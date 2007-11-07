2a0e479d8bacef46e0fec89a7bda0c98b59faa17
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-11-07 08:41:46 +0000

    first attempt to cell based test

---
+++ modules/dCacheJUnit/org/dcache/tests/namespace/PnfsManagerTest.java
@@ -0,0 +1,146 @@
+package org.dcache.tests.namespace;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import dmg.cells.nucleus.SystemCell;
+
+import org.dcache.chimera.FsInode;
+import org.dcache.chimera.JdbcFs;
+import org.dcache.chimera.XMLconfig;
+import org.dcache.chimera.posix.Stat;
+
+import diskCacheV111.namespace.PnfsManagerV3;
+import diskCacheV111.vehicles.PnfsCreateDirectoryMessage;
+import diskCacheV111.vehicles.PnfsCreateEntryMessage;
+
+public class PnfsManagerTest {
+
+    /*
+     * make Cells happy
+     */
+    private final static SystemCell _systemCell = new SystemCell("JUnitTestDomain");
+
+    private static PnfsManagerV3 _pnfsManager;
+    private static Connection _conn;
+    private static JdbcFs _fs;
+
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        /*
+         * init Chimera DB
+         */
+
+        Class.forName("org.hsqldb.jdbcDriver");
+
+        _conn = DriverManager.getConnection("jdbc:hsqldb:mem:chimeramem", "sa", "");
+
+        File sqlFile = new File("/home/tigran/work/cvs-work/eProject/Chimera/sql/create-hsqldb.sql");
+        StringBuilder sql = new StringBuilder();
+
+        BufferedReader dataStr = new BufferedReader(new FileReader(sqlFile));
+        String inLine = null;
+
+        while ((inLine = dataStr.readLine()) != null) {
+            sql.append(inLine);
+        }
+
+        Statement st = _conn.createStatement();
+
+        st.executeUpdate(sql.toString());
+
+        tryToClose(st);
+
+
+        String args = "org.dcache.chimera.namespace.ChimeraOsmStorageInfoExtractor " +
+                "-threads=1 " +
+                "-namespace-provider=org.dcache.chimera.namespace.ChimeraNameSpaceProviderFactory " +
+                "-storageinfo-provider=org.dcache.chimera.namespace.ChimeraNameSpaceProviderFactory " +
+                "-cachelocation-provider=org.dcache.chimera.namespace.ChimeraNameSpaceProviderFactory " +
+                "-chimeraConfig=/home/tigran/work/cvs-work/eProject/Chimera/test-config.xml";
+
+        _pnfsManager =  new PnfsManagerV3("testPnfsManager", args);
+
+
+        _fs = new JdbcFs(new XMLconfig(new File("/home/tigran/work/cvs-work/eProject/Chimera/test-config.xml")));
+        _fs.mkdir("/pnfs");
+        FsInode baseInode = _fs.mkdir("/pnfs/testRoot");
+        byte[] sGroupTagData = "chimera".getBytes();
+        byte[] osmTemplateTagData = "StoreName sql".getBytes();
+
+        _fs.createTag(baseInode, "sGroup");
+        _fs.createTag(baseInode, "OSMTemplate");
+
+        _fs.setTag(baseInode, "sGroup",sGroupTagData, 0, sGroupTagData.length);
+        _fs.setTag(baseInode, "OSMTemplate",osmTemplateTagData, 0, osmTemplateTagData.length);
+
+    }
+
+    @Test
+    public void testGetStorageInfo() {
+
+        PnfsCreateEntryMessage pnfsCreateEntryMessage = new PnfsCreateEntryMessage("/pnfs/testRoot/testGetStorageInfo");
+
+        _pnfsManager.createEntry(pnfsCreateEntryMessage);
+
+        assertTrue("failed to create an entry", pnfsCreateEntryMessage.getReturnCode() == 0 );
+        assertNotNull("failed to get StrageInfo", pnfsCreateEntryMessage.getStorageInfo());
+
+    }
+
+
+    @Test
+    public void testMkdirPermTest() throws Exception {
+
+        PnfsCreateDirectoryMessage pnfsCreateDirectoryMessage = new PnfsCreateDirectoryMessage("/pnfs/testRoot/testMkdirPermTest", 3750, 1000, 0750);
+
+        _pnfsManager.createDirectory(pnfsCreateDirectoryMessage);
+
+        assertTrue("failed to create a directory", pnfsCreateDirectoryMessage.getReturnCode() == 0 );
+
+        /*
+         * while we have a backdoor, let check what really happened
+         */
+
+        Stat stat = _fs.stat(new FsInode(_fs, pnfsCreateDirectoryMessage.getPnfsId().toString()));
+        assertEquals("new mode do not equal to specified one", (stat.getMode() & 0777) , 0750 );
+    }
+
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+
+        _systemCell.getNucleus().kill(_pnfsManager.getCellName());
+
+        _conn.createStatement().execute("SHUTDOWN;");
+        _conn.close();
+
+    }
+
+    static void tryToClose(Statement o) {
+        try {
+            if (o != null)
+                o.close();
+        } catch (SQLException e) {
+            // _logNamespace.error("tryToClose PreparedStatement", e);
+        }
+    }
+
+
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(PnfsManagerTest.class);
+    }
+}
