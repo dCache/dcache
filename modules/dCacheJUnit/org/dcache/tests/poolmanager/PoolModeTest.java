109dcd416d32753a14b7bad9e6822a0cedbadfbe
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-11-14 09:27:45 +0000

    mode tests

--- modules/dCacheJUnit/org/dcache/tests/poolmanager/PoolModeTest.java
+++ modules/dCacheJUnit/org/dcache/tests/poolmanager/PoolModeTest.java
@@ -56,4 +56,36 @@ public class PoolModeTest {
         assertFalse("DISABLED and ENABLED modes can't be equal", poolMode1.equals(poolMode2));
     }
 
+
+    @Test
+    public void testPartialDisable() {
+
+        PoolV2Mode poolMode = new PoolV2Mode(PoolV2Mode.DISABLED_RDONLY);
+
+        assertFalse("READ-ONLY disabled have return false on isDisabled(FETCH)", poolMode.isDisabled(PoolV2Mode.DISABLED_FETCH));
+        assertTrue("READ-ONLY disabled have return true on isDisabled(STAGE)", poolMode.isDisabled(PoolV2Mode.DISABLED_STAGE));
+        assertTrue("READ-ONLY disabled have return true on isDisabled(STORE)", poolMode.isDisabled(PoolV2Mode.DISABLED_STORE));
+        assertTrue("READ-ONLY disabled have return true on isDisabled(P2P_CLIENT)", poolMode.isDisabled(PoolV2Mode.DISABLED_P2P_CLIENT));
+        assertFalse("READ-ONLY disabled have return false on isDisabled(P2P_SERVER)", poolMode.isDisabled(PoolV2Mode.DISABLED_P2P_SERVER) );
+
+        assertTrue("READ-ONLY disabled have return true on isDisabled()", poolMode.isDisabled() );
+        assertFalse("READ-ONLY disabled have return false on isEnabled()", poolMode.isEnabled() );
+    }
+
+    @Test
+    public void testStrictDisable() {
+
+        PoolV2Mode poolMode = new PoolV2Mode(PoolV2Mode.DISABLED_STRICT);
+
+        assertTrue("STRICT disabled have return true on isDisabled(FETCH)", poolMode.isDisabled(PoolV2Mode.DISABLED_FETCH));
+        assertTrue("STRICT disabled have return true on isDisabled(STAGE)", poolMode.isDisabled(PoolV2Mode.DISABLED_STAGE));
+        assertTrue("STRICT disabled have return true on isDisabled(STORE)", poolMode.isDisabled(PoolV2Mode.DISABLED_STORE));
+        assertTrue("STRICT disabled have return true on isDisabled(P2P_CLIENT)", poolMode.isDisabled(PoolV2Mode.DISABLED_P2P_CLIENT));
+        assertTrue("STRICT disabled have return true on isDisabled(P2P_SERVER)", poolMode.isDisabled(PoolV2Mode.DISABLED_P2P_SERVER) );
+
+        assertTrue("STRICT disabled have return true on isDisabled()", poolMode.isDisabled() );
+        assertFalse("STRICT disabled have return false on isEnabled()", poolMode.isEnabled() );
+    }
+
+
 }
