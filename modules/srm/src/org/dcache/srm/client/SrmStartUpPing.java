d0a4e924a38325cad0652c08820a9ceac478c6b1
Dmitry Litvintsev <litvinse@fnal.gov>
2007-11-21 22:02:33 +0000

    testing $Id$ and $Author$ keywords

--- modules/srm/src/org/dcache/srm/client/SrmStartUpPing.java
+++ modules/srm/src/org/dcache/srm/client/SrmStartUpPing.java
@@ -1,7 +1,7 @@
 //______________________________________________________________________________
 //
 // $Id$
-// $Author: litvinse $
+// $Author$
 //
 // created 10/07 by Dmitry Litvintsev (litvinse@fnal.gov)
 // 
