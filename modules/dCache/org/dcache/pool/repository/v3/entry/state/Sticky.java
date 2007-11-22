5f2a88945ce1f0ac3f749ee0dc83890a5040ddfb
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-11-22 09:11:52 +0000

    added new line after each sticky record

--- modules/dCache/org/dcache/pool/repository/v3/entry/state/Sticky.java
+++ modules/dCache/org/dcache/pool/repository/v3/entry/state/Sticky.java
@@ -8,8 +8,8 @@ import java.util.Set;
 import org.dcache.pool.repository.StickyRecord;
 
 /**
- * 
- * file sticky status. 
+ *
+ * file sticky status.
  * @since 1.7.1
  *
  */
@@ -17,15 +17,15 @@ import org.dcache.pool.repository.StickyRecord;
 public class Sticky {
 
 	private final Set<StickyRecord> _records = new HashSet<StickyRecord>();
-	
-	
+
+
 	public boolean isSticky() {
-		
+
 		boolean isSticky = false;
-		
+
 		if( !_records.isEmpty() ) {
 			long now = System.currentTimeMillis();
-			
+
 			for( StickyRecord stickyRecord: _records ) {
 				if( stickyRecord.isValidAt(now) ) {
 					// even if only one is valid it's STICKY
@@ -34,43 +34,43 @@ public class Sticky {
 				}
 			}
 		}
-		return isSticky;		
+		return isSticky;
 	}
-		
+
 	public boolean isSet() {
 		return isSticky();
 	}
-	
+
 	public boolean addRecord(String owner, long expire) {
 		boolean isAdded = false;
 		if( expire == -1 || expire > System.currentTimeMillis() ) {
 			_records.add( new StickyRecord(owner, expire) );
 			isAdded = true;
 		}
-		
+
 		return isAdded;
 	}
-	
+
 	public boolean removeRecord(String owner, long expire) {
 		return _records.remove( new StickyRecord(owner, expire) );
 	}
-		
+
 	public String stringValue() {
-		
+
 		StringBuilder sb = new StringBuilder();
-		
+
 		long now = System.currentTimeMillis();
-		
-		for( StickyRecord record: _records ) {			
+
+		for( StickyRecord record: _records ) {
 			if( record.isValidAt(now) ) {
-				sb.append("sticky:").append(record.owner()).append(":").append(record.expire());
+				sb.append("sticky:").append(record.owner()).append(":").append(record.expire()).append("\n");
 			}
-		}		
-		
+		}
+
 		return sb.toString();
 	}
-	
-	public List<StickyRecord> records() {		
-		return new ArrayList<StickyRecord>(_records);		
-	}	
+
+	public List<StickyRecord> records() {
+		return new ArrayList<StickyRecord>(_records);
+	}
 }
