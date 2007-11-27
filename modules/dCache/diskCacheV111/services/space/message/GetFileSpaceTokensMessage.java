31971e18eae1d01d8c1fb30be50c1ea7c193656a
Dmitry Litvintsev <litvinse@fnal.gov>
2007-11-27 21:31:07 +0000

    rename GetFileTokens into GetFileSpaceTokensMessage in the name of clarity at the expense of verbosity

--- modules/dCache/diskCacheV111/services/space/message/GetFileSpaceTokensMessage.java
+++ modules/dCache/diskCacheV111/services/space/message/GetFileSpaceTokensMessage.java
@@ -18,21 +18,21 @@ import diskCacheV111.services.space.Space;
  *
  * @author timur
  */
-public class GetFileTokens extends Message{
+public class GetFileSpaceTokensMessage extends Message{
     private long[] spaceTokens;
     private String pnfsPath;
     private PnfsId pnfsId;
     /** Creates a new instance of Reserve */
-    public GetFileTokens(String pnfsPath) {
+    public GetFileSpaceTokensMessage(String pnfsPath) {
         this.pnfsPath = pnfsPath;
         setReplyRequired(true);
     }
 
-    public GetFileTokens(PnfsId pnfsId) {
+    public GetFileSpaceTokensMessage(PnfsId pnfsId) {
         this.pnfsId = pnfsId;
         setReplyRequired(true);
     }
-    public GetFileTokens(String pnfsPath, PnfsId pnfsId) {
+    public GetFileSpaceTokensMessage(String pnfsPath, PnfsId pnfsId) {
         this.pnfsPath = pnfsPath;
         this.pnfsId = pnfsId;
         setReplyRequired(true);
