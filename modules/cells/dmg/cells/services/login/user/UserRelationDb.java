8179347d00a1a20682a02b302d0241b4308b353f
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-05 12:10:26 +0000

    fixed potential null pointer accesses

--- modules/cells/dmg/cells/services/login/user/UserRelationDb.java
+++ modules/cells/dmg/cells/services/login/user/UserRelationDb.java
@@ -4,42 +4,42 @@ import java.util.* ;
 import java.io.* ;
 
 public class UserRelationDb {
- 
-    private class DEnumeration implements Enumeration {
+
+    private class DEnumeration<T> implements Enumeration<T> {
         public boolean hasMoreElements(){ return false ; }
-        public Object nextElement(){ return null ;}
+        public T nextElement(){ return null ;}
     }
     private class ElementItem {
-       private Hashtable _parents = null ;
-       private Hashtable _childs  = null ;
+       private Hashtable<String, String> _parents = null ;
+       private Hashtable<String, String> _childs  = null ;
        private void addParent(String parent){
-          if( _parents == null )_parents = new Hashtable() ;
+          if( _parents == null )_parents = new Hashtable<String, String>() ;
           _parents.put(parent,parent) ;
        }
        private void addChild( String child ){
-          if( _childs == null )_childs = new Hashtable() ;
+          if( _childs == null )_childs = new Hashtable<String, String>() ;
           _childs.put(child,child) ;
        }
-       private Enumeration parents(){ 
-           return _parents == null ? new DEnumeration() : _parents.keys() ;
+       private Enumeration<String> parents(){
+           return _parents == null ? new DEnumeration<String>() : _parents.keys() ;
        }
-       private Enumeration children(){ 
-           return _childs == null ? new DEnumeration() : _childs.keys() ;
+       private Enumeration<String> children(){
+           return _childs == null ? new DEnumeration<String>() : _childs.keys() ;
        }
     }
     private File      _dbDir    = null ;
-    private Hashtable _elements = null ;
+    private Hashtable<String, ElementItem> _elements = null ;
     public UserRelationDb( File dbDir )throws DatabaseException {
         if( ( ! dbDir.exists() ) || ( ! dbDir.isDirectory() ) )
           throw new
           DatabaseException( 11 , "Not a directory : "+dbDir ) ;
-          
+
         _dbDir = dbDir ;
-        
+
         _loadElements() ;
     }
-    public static Hashtable loadAcl( File aclFile ){
-       Hashtable      acl = new Hashtable() ;
+    public static  Map<String, Boolean> loadAcl( File aclFile ){
+       Map<String, Boolean>      acl = new Hashtable<String, Boolean>() ;
        BufferedReader br  = null ;
        try{
           br = new BufferedReader(
@@ -53,7 +53,7 @@ public class UserRelationDb {
              try{
                 name  = st.nextToken() ;
                 value = st.nextToken() ;
-                acl.put( name , 
+                acl.put( name ,
                          Boolean.valueOf( value.equals("allowed") )
                        ) ;
              }catch(Exception ee){
@@ -62,18 +62,17 @@ public class UserRelationDb {
           }
        }catch( IOException ie ){
        }finally{
-          try{ br.close() ; }catch(Exception ee){}
+          if( br != null ) { try{ br.close() ; }catch(IOException ee){} }
        }
        return acl ;
     }
     public void display(){
-       Enumeration all = _elements.keys() ;
+       Enumeration<String> all = _elements.keys() ;
        while( all.hasMoreElements() ){
-           String name = (String)all.nextElement() ;
-           ElementItem item = (ElementItem)_elements.get(name) ;
+           String name = all.nextElement() ;
+           ElementItem item = _elements.get(name) ;
            System.out.println(name);
-           Enumeration e = null ;
-           e = item.parents() ;
+           Enumeration<String> e = item.parents() ;
            while( e.hasMoreElements() ){
               System.out.println("   p:"+e.nextElement());
            }
@@ -84,18 +83,18 @@ public class UserRelationDb {
        }
     }
     private void _loadElements() throws DatabaseException{
-        String [] elements = _dbDir.list( 
-                     new FilenameFilter(){        
+        String [] elements = _dbDir.list(
+                     new FilenameFilter(){
                         public boolean accept( File dir , String name ){
                            return ! name.startsWith(".") ;
                         }
                      } ) ;
-        Hashtable hash = new Hashtable() ;
+        Hashtable<String,ElementItem > hash = new Hashtable<String,ElementItem >() ;
         for( int i = 0 ; i < elements.length ; i++ ){
             File file = new File( _dbDir , elements[i] ) ;
             BufferedReader br = null ;
             ElementItem item  = null , x = null;
-            if( ( item = (ElementItem)hash.get( elements[i] ) ) == null ){
+            if( ( item = hash.get( elements[i] ) ) == null ){
                 hash.put( elements[i] , item = new ElementItem() ) ;
             }
             try{
@@ -107,39 +106,39 @@ public class UserRelationDb {
                    if( name.length() == 0 )continue ;
                    if( name.charAt(0) == '#' )continue ;
                    item.addChild(name) ;
-                   if( ( x = (ElementItem)hash.get(name) ) == null ){
+                   if( ( x = hash.get(name) ) == null ){
                        hash.put(name , x = new ElementItem() ) ;
                    }
                    x.addParent( elements[i] ) ;
                }
             }catch( IOException ie ){
             }finally{
-               try{ br.close() ; }catch(Exception ee){}
+               if( br != null ) { try{ br.close() ; }catch(IOException ee){} }
             }
         }
         _elements = hash ;
     }
-    public boolean check( String user , Hashtable acl ){
+    public boolean check( String user ,  Map<String, Boolean> acl ){
         Boolean ok = null ;
-        if( ( ok = (Boolean)acl.get(user) ) != null )return ok.booleanValue() ;
-        
-        Vector v = new Vector() ;
+        if( ( ok = acl.get(user) ) != null )return ok.booleanValue() ;
+
+        Vector<String> v = new Vector<String>() ;
         String p = null ;
         ElementItem item = null ;
         Boolean     x    = null ;
         v.addElement( user ) ;
-        
+
         for( int i = 0 ; i < v.size() ; i++ ){
-            p = (String)v.elementAt(i) ;
-            if( ( x = (Boolean)acl.get(p) ) != null ){
+            p = v.elementAt(i) ;
+            if( ( x = acl.get(p) ) != null ){
                if( x.booleanValue() )return true ;
                continue ;
             }
-            if( (item = (ElementItem)_elements.get(p)) != null ){
-               Enumeration e = item.parents() ;
+            if( (item = _elements.get(p)) != null ){
+               Enumeration<String> e = item.parents() ;
                while( e.hasMoreElements() )
                   v.addElement(e.nextElement()) ;
-            } 
+            }
         }
         return false ;
     }
@@ -149,14 +148,14 @@ public class UserRelationDb {
           System.exit(4);
        }
        File   dbDir   = new File(args[0]);
-       
+
        UserRelationDb db = new UserRelationDb( dbDir ) ;
        if( args.length < 3 ){
           db.display();
        }else{
           File   aclFile = new File( args[1] ) ;
           String user    = args[2] ;
-          Hashtable acl = db.loadAcl( aclFile ) ;
+          Map<String, Boolean> acl = UserRelationDb.loadAcl( aclFile ) ;
           boolean rc = db.check( user , acl ) ;
           System.out.println( "user="+user+";acl="+aclFile+";allowed="+rc) ;
        }
