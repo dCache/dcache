eac789ff5ace471825827a5dc11efad0ec9f0b33
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-07 14:04:20 +0000

    added javadoc

--- modules/dCache/diskCacheV111/services/FileMetaDataSource.java
+++ modules/dCache/diskCacheV111/services/FileMetaDataSource.java
@@ -9,13 +9,40 @@ import diskCacheV111.util.FileMetaDataX;
 import diskCacheV111.util.PnfsId;
 
 public interface FileMetaDataSource {
-	
+
+    /**
+     *
+     * @param path
+     * @return metadata of the file specified by <i>path</i>
+     * @throws CacheException
+     */
 	public FileMetaData getMetaData(String path) throws CacheException ;
+
+	/**
+	 *
+	 * @param pnfsId
+	 * @return metadata of the file specified by <i>pnfsId</i>
+	 * @throws CacheException
+	 */
 	public FileMetaData getMetaData(PnfsId pnfsId) throws CacheException ;
-	
+
+	/**
+	 *
+	 * @param path
+	 * @return extended metadata of the file specified by <i>path</i>
+	 * @throws CacheException
+	 *
+	 * @since 1.8.0-7
+	 */
 	public FileMetaDataX getXMetaData(String path) throws CacheException ;
+
+	/**
+	 *
+	 * @param pnfsId
+	 * @return extended metadata of the file specified by <i>pnfsId</i>
+	 * @throws CacheException
+	 *
+	 * @since 1.8.0-7
+	 */
 	public FileMetaDataX getXMetaData(PnfsId pnfsId) throws CacheException ;
 }
-/*
- * $Log: not supported by cvs2svn $
- */
