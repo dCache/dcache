832fdfb480011a7be15e78d45e1c27df6bb6a006
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-10 13:52:19 +0000

    java5 cleanup more efficient map iteration in toString

--- modules/dCache/diskCacheV111/vehicles/PoolCheckMessage.java
+++ modules/dCache/diskCacheV111/vehicles/PoolCheckMessage.java
@@ -1,34 +1,32 @@
 // $Id: PoolCheckMessage.java,v 1.2 2004-11-05 12:07:19 tigran Exp $
 
 package diskCacheV111.vehicles;
-import java.util.*; 
+import java.util.*;
 
 
-public class PoolCheckMessage 
-       extends PoolMessage 
+public class PoolCheckMessage
+       extends PoolMessage
        implements PoolCheckable {
 
-    private Map _map = null ;
-    
+    private Map<String, String> _map = null ;
+
     private static final long serialVersionUID = 2300324248952047438L;
-    
+
     public PoolCheckMessage( String poolName ){
        super(poolName) ;
     }
-    public void setTagMap( Map map ){ _map = map ; }
-    public Map  getTagMap(){ return _map ; }
+    public void setTagMap( Map<String, String> map ){ _map = map ; }
+    public Map<String, String>  getTagMap(){ return _map ; }
     public String toString(){
       StringBuffer sb = new StringBuffer() ;
       sb.append(super.toString()).append(";");
       if( _map != null ){
           sb.append("tags={");
-          Iterator it = _map.entrySet().iterator() ;
-          while( it.hasNext() ){
-             Map.Entry entry = (Map.Entry)it.next() ;
-             sb.append((String)entry.getKey()).
+          for( Map.Entry<String, String> entry : _map.entrySet()){
+             sb.append(entry.getKey()).
                 append("=").
-                append((String)entry.getValue()).
-                append(";"); 
+                append(entry.getValue()).
+                append(";");
           }
           sb.append("};");
 
