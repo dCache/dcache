7df23d26f6bb4ebc0178a6b0fc19e9336d7a60f8
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-10 09:05:36 +0000

    more cost module related tests

--- modules/dCacheJUnit/org/dcache/tests/poolmanager/CostModuleTest.java
+++ modules/dCacheJUnit/org/dcache/tests/poolmanager/CostModuleTest.java
@@ -2,11 +2,18 @@ package org.dcache.tests.poolmanager;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import dmg.cells.nucleus.CellMessage;
+import dmg.cells.nucleus.CellPath;
+
 import static org.junit.Assert.*;
 
 import org.dcache.tests.cells.CellAdapterHelper;
 
 import diskCacheV111.poolManager.CostModuleV1;
+import diskCacheV111.pools.PoolCostInfo;
+import diskCacheV111.pools.PoolV2Mode;
+import diskCacheV111.vehicles.PoolManagerPoolUpMessage;
 
 public class CostModuleTest {
 
@@ -23,5 +30,93 @@ public class CostModuleTest {
 
         assertNull("should return null on non existing pool", _costModule.getPoolCostInfo("aPool"));
 
+    }
+
+
+    @Test
+    public void testPoolUp() throws Exception {
+
+        long serialId = System.currentTimeMillis();
+
+        PoolV2Mode poolMode = new PoolV2Mode(PoolV2Mode.ENABLED);
+
+        PoolManagerPoolUpMessage poolUpMessage = new PoolManagerPoolUpMessage("aPool",
+                serialId, poolMode);
+
+        CellMessage cellMessage = new CellMessage( new CellPath("CostModule"), poolUpMessage);
+
+        _costModule.messageArrived(cellMessage);
+
+        assertNull("should return null on non a pool without costInfo", _costModule.getPoolCostInfo("aPool"));
+
+    }
+
+    @Test
+    public void testPoolUpWithCost() throws Exception {
+
+        long serialId = System.currentTimeMillis();
+
+        PoolV2Mode poolMode = new PoolV2Mode(PoolV2Mode.ENABLED);
+
+        PoolCostInfo poolCost = new PoolCostInfo("aPool");
+
+        poolCost.setSpaceUsage(100, 20, 30, 50);
+
+
+        PoolManagerPoolUpMessage poolUpMessage = new PoolManagerPoolUpMessage("aPool",
+                serialId, poolMode, poolCost);
+
+        CellMessage cellMessage = new CellMessage( new CellPath("CostModule"), poolUpMessage);
+
+        _costModule.messageArrived(cellMessage);
+
+        PoolCostInfo recivedCost = _costModule.getPoolCostInfo("aPool");
+
+        assertNotNull("should return non null cost on a pool with costInfo", recivedCost);
+
+    }
+
+
+    @Test
+    public void testPoolUpAndDown() throws Exception {
+
+        CellMessage cellMessage = null;
+        PoolCostInfo recivedCost = null;
+
+        long serialId = System.currentTimeMillis();
+
+        PoolV2Mode poolMode = new PoolV2Mode(PoolV2Mode.ENABLED);
+
+        PoolCostInfo poolCost = new PoolCostInfo("aPool");
+
+        poolCost.setSpaceUsage(100, 20, 30, 50);
+
+
+        PoolManagerPoolUpMessage poolUpMessage = new PoolManagerPoolUpMessage("aPool",
+                serialId, poolMode, poolCost);
+
+        cellMessage = new CellMessage( new CellPath("CostModule"), poolUpMessage);
+
+        _costModule.messageArrived(cellMessage);
+
+        recivedCost = _costModule.getPoolCostInfo("aPool");
+
+        assertNotNull("should return non null cost on a pool with costInfo", recivedCost);
+
+
+        // set pool down
+
+        poolMode.setMode(PoolV2Mode.DISABLED);
+
+
+        PoolManagerPoolUpMessage poolDownMessage = new PoolManagerPoolUpMessage("aPool",
+                serialId, poolMode, poolCost);
+
+        cellMessage = new CellMessage( new CellPath("CostModule"), poolDownMessage);
+
+        _costModule.messageArrived(cellMessage);
+        recivedCost = _costModule.getPoolCostInfo("aPool");
+
+        assertNull("should return null cost on a DOWN pool", recivedCost);
     }
 }
