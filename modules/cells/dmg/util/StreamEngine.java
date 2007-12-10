dd804f9d55cb55b681c536c832d769ee6caddf21
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-10 14:18:10 +0000

    added javadoc

--- modules/cells/dmg/util/StreamEngine.java
+++ modules/cells/dmg/util/StreamEngine.java
@@ -10,11 +10,40 @@ import dmg.security.CellUser;
 
 public interface StreamEngine {
 
+    /**
+     *
+     * @return user associated with the connections
+     */
    public CellUser    getUserName() ;
+
+   /**
+    *
+    * @return Socket object associated with the connections
+    */
    public Socket      getSocket() ;
+
+   /**
+    *
+    * @return local InetAddress
+    */
    public InetAddress getLocalAddress();
+
+   /**
+    *
+    * @return remote InetAddress
+    */
    public InetAddress getInetAddress() ;
+
+   /**
+    *
+    * @return socket input stream
+    */
    public InputStream getInputStream() ;
+
+   /**
+    *
+    * @return return socket output stream
+    */
    public OutputStream getOutputStream() ;
    public Reader       getReader() ;
    public Writer       getWriter() ;
