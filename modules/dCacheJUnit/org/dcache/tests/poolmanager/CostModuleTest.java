82be6174d09b11bf2aa049916bd0604bd63d887c
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-11 11:11:54 +0000

    added cost evaluation ( commented out )

--- modules/dCacheJUnit/org/dcache/tests/poolmanager/CostModuleTest.java
+++ modules/dCacheJUnit/org/dcache/tests/poolmanager/CostModuleTest.java
@@ -13,6 +13,8 @@ import org.dcache.tests.cells.CellAdapterHelper;
 import diskCacheV111.poolManager.CostModuleV1;
 import diskCacheV111.pools.PoolCostInfo;
 import diskCacheV111.pools.PoolV2Mode;
+import diskCacheV111.vehicles.PoolCheckable;
+import diskCacheV111.vehicles.PoolCostCheckable;
 import diskCacheV111.vehicles.PoolManagerPoolUpMessage;
 
 public class CostModuleTest {
@@ -119,4 +121,49 @@ public class CostModuleTest {
 
         assertNull("should return null cost on a DOWN pool", recivedCost);
     }
+
+
+//    @Test
+//    public void testPoolDiskCost() throws Exception {
+//
+//        long serialId = System.currentTimeMillis();
+//
+//        PoolV2Mode poolMode = new PoolV2Mode(PoolV2Mode.ENABLED);
+//
+//        PoolCostInfo poolCost = new PoolCostInfo("aPool");
+//
+//        /*
+//         *  make it simple:
+//         *      total    : 100
+//         *      free     : 20
+//         *      precious : 30
+//         *      removable: 50
+//         */
+//        poolCost.setSpaceUsage(100, 20, 30, 50);
+//
+//
+//        PoolManagerPoolUpMessage poolUpMessage = new PoolManagerPoolUpMessage("aPool",
+//                serialId, poolMode, poolCost);
+//
+//        CellMessage cellMessage = new CellMessage( new CellPath("CostModule"), poolUpMessage);
+//
+//        _costModule.messageArrived(cellMessage);
+//
+//        PoolCostInfo recivedCost = _costModule.getPoolCostInfo("aPool");
+//
+//        assertNotNull("should return non null cost on a pool with costInfo", recivedCost);
+//
+//        PoolCostCheckable poolCostCheckable =  _costModule.getPoolCost("aPool", 18);
+//        System.out.println("Pool cost: " + poolCostCheckable.getSpaceCost() );
+//
+//        poolCostCheckable =  _costModule.getPoolCost("aPool", 40);
+//        System.out.println("Pool cost: " + poolCostCheckable.getSpaceCost() );
+//
+//        poolCostCheckable =  _costModule.getPoolCost("aPool", 60);
+//        System.out.println("Pool cost: " + poolCostCheckable.getSpaceCost() );
+//
+//        poolCostCheckable =  _costModule.getPoolCost("aPool", 120);
+//        System.out.println("Pool cost: " + poolCostCheckable.getSpaceCost() );
+//
+//    }
 }
