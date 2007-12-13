f3f3e8771e3c7a7c852613fe8ea6c9846f44863e
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-13 09:27:38 +0000

    added test of cell helper class

---
+++ modules/dCacheJUnit/org/dcache/tests/cells/CellAdapterHelperTest.java
@@ -0,0 +1,30 @@
+package org.dcache.tests.cells;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dmg.util.Args;
+
+
+public class CellAdapterHelperTest {
+
+    private CellAdapterHelper _sutCell;
+
+    @Test
+    public void testArgs() {
+
+        String args = "arg1 -opt1=opt1-value -opt2=opt2-value -flag1";
+        _sutCell = new CellAdapterHelper("testArgsCell", args );
+
+        Args cellArgs = _sutCell.getArgs();
+        assertNotNull("Args is NULL", cellArgs);
+
+
+        String opt1= cellArgs.getOpt("opt1");
+
+        assertEquals("Invalid option recieved", "opt1-value",opt1 );
+
+    }
+
+}
