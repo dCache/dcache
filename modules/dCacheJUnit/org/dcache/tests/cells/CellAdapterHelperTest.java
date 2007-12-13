21c990972ca7e230bbb47ba44298cb3ba8066cb3
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-13 09:46:18 +0000

    more real life test

--- modules/dCacheJUnit/org/dcache/tests/cells/CellAdapterHelperTest.java
+++ modules/dCacheJUnit/org/dcache/tests/cells/CellAdapterHelperTest.java
@@ -14,10 +14,11 @@ public class CellAdapterHelperTest {
     @Test
     public void testArgs() {
 
-        String args = "arg1" +
+        String args = "arg0" +
         		" -opt1=opt1-value" +
         		" -opt2=opt2-value" +
-        		" -flag1";
+        		" -flag1" +
+        		" arg1";
 
         _sutCell = new CellAdapterHelper("testArgsCell", args );
 
@@ -28,6 +29,14 @@ public class CellAdapterHelperTest {
         String opt1= cellArgs.getOpt("opt1");
 
         assertEquals("Invalid option received", "opt1-value",opt1 );
+        assertTrue("Flag is not set", cellArgs.getOpt("flag1") != null);
+
+        String arg0 = cellArgs.argv(0);
+        assertEquals("Invalid arg[0] received", "arg0", arg0 );
+
+        String arg1 = cellArgs.argv(1);
+        assertEquals("Invalid arg[1] received", "arg1", arg1 );
+
 
     }
 
