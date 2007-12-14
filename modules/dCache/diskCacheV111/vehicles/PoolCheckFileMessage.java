03ffe14fc90c68128bdf016b43baf871d67fe910
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-14 13:18:16 +0000

    telescopique constructor

--- modules/dCache/diskCacheV111/vehicles/PoolCheckFileMessage.java
+++ modules/dCache/diskCacheV111/vehicles/PoolCheckFileMessage.java
@@ -1,56 +1,59 @@
 // $Id: PoolCheckFileMessage.java,v 1.6 2004-11-05 12:07:19 tigran Exp $
 
 package diskCacheV111.vehicles;
-import  diskCacheV111.util.* ;
-import java.util.*; 
 
+import diskCacheV111.util.PnfsId;
 
-public class PoolCheckFileMessage 
-       extends PoolCheckMessage 
-       implements PoolFileCheckable {
+public class PoolCheckFileMessage extends PoolCheckMessage implements
+        PoolFileCheckable {
 
-    private PnfsId  _pnfsId;
+    private PnfsId _pnfsId;
     private boolean _have;
     private boolean _waiting;
 
     private static final long serialVersionUID = -642190491441448681L;
-    
-    public PoolCheckFileMessage(String poolName, String pnfsId){
-	super(poolName);
-	_pnfsId = new PnfsId( pnfsId );
-	setReplyRequired(true);
-    }
-    public PoolCheckFileMessage(String poolName, PnfsId pnfsId){
-	super(poolName);
-	_pnfsId = pnfsId;
-	setReplyRequired(true);
-    }
-    public String toString(){
-       return super.toString()+";PnfsId="+(_pnfsId==null?"<unknown>":_pnfsId.toString())+
-                ";have="+_have+";waiting="+_waiting ;
-    }
-    public void setPnfsId(String pnfsId){
-	_pnfsId = new PnfsId(pnfsId);
-    }
-    public void setPnfsId(PnfsId pnfsId){
-	_pnfsId = pnfsId ;
+
+    public PoolCheckFileMessage(String poolName, String pnfsId) {
+       this(poolName,  new PnfsId(pnfsId) );
     }
 
-    public PnfsId getPnfsId(){
-	return _pnfsId ;
+    public PoolCheckFileMessage(String poolName, PnfsId pnfsId) {
+        super(poolName);
+        _pnfsId = pnfsId;
+        setReplyRequired(true);
     }
-    
-    public boolean getHave(){
-	return _have;
+
+    public String toString() {
+        return super.toString() + ";PnfsId="
+                + (_pnfsId == null ? "<unknown>" : _pnfsId.toString())
+                + ";have=" + _have + ";waiting=" + _waiting;
     }
-    public void setHave(boolean have){
-	_have = have;
+
+    public void setPnfsId(String pnfsId) {
+        _pnfsId = new PnfsId(pnfsId);
     }
-    
-    public boolean getWaiting(){
-	return _waiting;
+
+    public void setPnfsId(PnfsId pnfsId) {
+        _pnfsId = pnfsId;
     }
-    public void setWaiting(boolean waiting){
-	_waiting = waiting;
+
+    public PnfsId getPnfsId() {
+        return _pnfsId;
+    }
+
+    public boolean getHave() {
+        return _have;
+    }
+
+    public void setHave(boolean have) {
+        _have = have;
+    }
+
+    public boolean getWaiting() {
+        return _waiting;
+    }
+
+    public void setWaiting(boolean waiting) {
+        _waiting = waiting;
     }
 }
