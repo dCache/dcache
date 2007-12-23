1384470d3e2d83c8950f729a662d80042c78e2d7
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-23 20:17:25 +0000

    java5 cleanup

--- modules/cells/org/pcells/services/connection/DomainConnectionAdapter.java
+++ modules/cells/org/pcells/services/connection/DomainConnectionAdapter.java
@@ -2,49 +2,60 @@
 //
 package org.pcells.services.connection ;
 //
-import java.util.*;
-import java.io.* ;
-import java.net.* ;
-import org.pcells.services.connection.DomainConnection ;
-import org.pcells.services.connection.DomainConnectionListener ;
-import org.pcells.services.connection.DomainEventListener ;
-import dmg.cells.applets.login.DomainObjectFrame ;
+import java.io.BufferedReader;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dmg.cells.applets.login.DomainObjectFrame;
 /**
  */
 public class DomainConnectionAdapter implements DomainConnection {
 
-     private HashMap   _packetHash = new HashMap() ;
-     private Object    _ioLock     = new Object() ; 
+     private Map<DomainObjectFrame, DomainConnectionListener>   _packetHash = new HashMap<DomainObjectFrame, DomainConnectionListener> () ;
+     private final Object    _ioLock     = new Object() ;
      private int       _ioCounter  = 100 ;
-     private ArrayList _listener   = new ArrayList() ;
+     private List<DomainEventListener> _listener   = new ArrayList<DomainEventListener>() ;
      private boolean   _connected  = false ;
-     
+
      private InputStream  _inputStream  = null ;
      private OutputStream _outputStream = null ;
      private Reader       _reader       = null ;
      private Writer       _writer       = null ;
-     private ObjectOutputStream _objOut = null ;     
+     private ObjectOutputStream _objOut = null ;
      private ObjectInputStream  _objIn  = null ;
-     
-     
+
+
      public String getAuthenticatedUser(){ return "Unknown" ; }
      public void setIoStreams( InputStream in , OutputStream out){
         setIoStreams( in , out, null , null ) ;
-     } 
+     }
      public void setIoStreams( InputStream in , OutputStream out ,
                                Reader reader , Writer writer    ){
-     
+
        _inputStream  = in ;
        _outputStream = out ;
        _reader       = reader ;
        _writer       = writer ;
-       
+
      }
      public void go() throws Exception {
         //System.out.println("runConnection started");
         runConnection() ;
         //System.out.println("runConnection OK");
-        
+
         informListenersOpened() ;
 
         //System.out.println("runReceiver starting");
@@ -54,13 +65,13 @@ public class DomainConnectionAdapter implements DomainConnection {
            //System.out.println("runReceiver finished");
            informListenersClosed() ;
         }
-      
-     
+
+
      }
      public void close()throws IOException {
         _objOut.close() ;
      }
-     private class MyFilter extends FilterInputStream {
+     private static class MyFilter extends FilterInputStream {
         public MyFilter( InputStream in ){
           super(in);
         }
@@ -79,26 +90,26 @@ public class DomainConnectionAdapter implements DomainConnection {
            data[0] = x[0];
            return r;
         }
-     } 
+     }
      private void runConnection() throws IOException {
-     
+
          InputStream inputstream = new MyFilter( _inputStream );
          BufferedReader reader = new BufferedReader(
                             _reader == null ?
                              new InputStreamReader(inputstream) :
                             _reader , 1 ) ;
-                                 
+
          PrintWriter writer = new PrintWriter( _writer == null ?
                                                new OutputStreamWriter(_outputStream) :
                                                _writer ) ;
-                              
+
          writer.println( "$BINARY$" ) ;
          writer.flush() ;
          String  check  = null ;
          do{
-         
-            check = reader.readLine()   ;  
-            //System.out.println(" >>"+check+"<<");    
+
+            check = reader.readLine()   ;
+            //System.out.println(" >>"+check+"<<");
 
          }while( ! check.equals( "$BINARY$" )  ) ;
          //System.out.println("opening object streams");
@@ -112,16 +123,16 @@ public class DomainConnectionAdapter implements DomainConnection {
         Object            obj   = null ;
         DomainObjectFrame frame = null ;
         DomainConnectionListener listener = null ;
-        
+
         while( true ){
-        
+
            if( ( obj = _objIn.readObject()  ) == null )break ;
            if( ! ( obj instanceof DomainObjectFrame ) )continue ;
-           
+
            synchronized( _ioLock ){
-           
+
               frame    = (DomainObjectFrame) obj ;
-              listener = (DomainConnectionListener)_packetHash.remove( frame ) ;
+              listener = _packetHash.remove( frame ) ;
               if( listener == null ){
                  System.err.println("Message without receiver : "+frame ) ;
                  continue ;
@@ -134,18 +145,18 @@ public class DomainConnectionAdapter implements DomainConnection {
            }
         }
      }
-     public int sendObject( Object obj , 
+     public int sendObject( Object obj ,
                             DomainConnectionListener listener ,
-                            int id 
+                            int id
                                                  ) throws IOException {
 
          synchronized( _ioLock ){
-         
+
              if( ! _connected )throw new IOException( "Not connected" ) ;
-             
-             DomainObjectFrame frame = 
+
+             DomainObjectFrame frame =
                      new DomainObjectFrame( obj , ++_ioCounter , id ) ;
-                     
+
              _objOut.writeObject( frame ) ;
              _objOut.reset() ;
              _packetHash.put( frame , listener ) ;
@@ -153,14 +164,14 @@ public class DomainConnectionAdapter implements DomainConnection {
          }
      }
      public int sendObject( String destination ,
-                            Object obj , 
+                            Object obj ,
                             DomainConnectionListener listener ,
-                            int id 
+                            int id
                                                  ) throws IOException {
 //         System.out.println("Sending : "+obj ) ;
          synchronized( _ioLock ){
              if( ! _connected )throw new IOException( "Not connected" ) ;
-             DomainObjectFrame frame = 
+             DomainObjectFrame frame =
                      new DomainObjectFrame( destination , obj , ++_ioCounter , id ) ;
              _objOut.writeObject( frame ) ;
              _objOut.reset() ;
@@ -185,12 +196,11 @@ public class DomainConnectionAdapter implements DomainConnection {
         }
      }
      private void informListenersOpened(){
-        ArrayList array = (ArrayList)_listener.clone();
+        List<DomainEventListener> array = new ArrayList<DomainEventListener>( _listener );
         synchronized( _ioLock ){
            _connected = true ;
-           Iterator e = array.iterator() ;
-           while( e.hasNext() ){
-               DomainEventListener listener = (DomainEventListener)e.next() ;
+           for ( DomainEventListener listener: array ){
+
                try{  listener.connectionOpened( this ) ;
                }catch( Throwable t ){
                   t.printStackTrace() ;
@@ -199,12 +209,10 @@ public class DomainConnectionAdapter implements DomainConnection {
         }
      }
      private void informListenersClosed(){
-        ArrayList array = (ArrayList)_listener.clone();
+         List<DomainEventListener> array = new ArrayList<DomainEventListener>( _listener );
         synchronized( _ioLock ){
            _connected = false ;
-           Iterator e = array.iterator() ;
-           while( e.hasNext() ){
-               DomainEventListener listener = (DomainEventListener)e.next() ;
+           for ( DomainEventListener listener: array ){
                try{  listener.connectionClosed( this ) ;
                }catch( Throwable t ){
                   t.printStackTrace() ;
