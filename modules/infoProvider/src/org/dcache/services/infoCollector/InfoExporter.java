6c021603d9b7ea82679f58bc20a604c353e0ee4e
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-23 19:15:46 +0000

    cleanup

--- modules/infoProvider/src/org/dcache/services/infoCollector/InfoExporter.java
+++ modules/infoProvider/src/org/dcache/services/infoCollector/InfoExporter.java
@@ -9,92 +9,96 @@ import dmg.util.Args ;
  * This class is instantiated by the <code>InfoCollector</code> to send
  * over a plain tcp socket a <code>Schema</code> object that carries out
  * dynamic information from dCache.<br><br>
- * Also this class is independent from the particular implementation of 
- * Schema used. As matter of fact, this class serializes a generic Schema   
+ * Also this class is independent from the particular implementation of
+ * Schema used. As matter of fact, this class serializes a generic Schema
  * object over a socket. It's a job of the client to know what particular
  * implementation of Schema was sent.<br><br>
  * Note that client needs only to know the specializing class of the Schema.
  */
 public class InfoExporter implements Runnable {
-	
+
 	/** InfoCollector link, to call say() **/
-	private InfoCollector _cell;
-	
+	private final InfoCollector _cell;
+
 	/** The SchemaFiller used **/
-	private SchemaFiller _filler;
-	
+	private final SchemaFiller _filler;
+
 	/** The Server Thread **/
-	private Thread   _sendThread   = null ;
-	
+	private final Thread   _sendThread   = null ;
+
 	/** Flag for strict-loop enabling **/
-	private boolean  _continue     = true ;	
-	
+	private boolean  _continue     = true ;
+
 	/** TCP port that the server listen **/
 	public  int _port = 22111;
-	
+
 	/** Server Socket reference**/
-	private ServerSocket _server;
-	
+	private final ServerSocket _server;
+
 	/** Constructor.<br>
 	 * Starts the thread and the server.
 	 * @param filler SchemaFiller created in the InfoCollector
 	 * @param cell InfoCollector reference
 	 * @throws Exception
-	 **/ 
+	 **/
 	public InfoExporter(SchemaFiller filler, InfoCollector cell) throws Exception{
 	   _filler = filler;
-	   _cell = cell;
-	   Args opt = _cell.getArgs() ;
-	   try{
+        _cell = cell;
+        Args opt = _cell.getArgs();
+        try {
 
-	      String option = opt.getOpt("listenPort");
-	      if( ( option != null ) && ( option.length() > 0 ) ){
-		try{ _port = Integer.parseInt(option) ; }catch(Exception e){}
-	      }
-	      _cell.say("InfoExporter: Using tcp listen port number : "+_port ) ;
+            String option = opt.getOpt("listenPort");
+            if ((option != null) && (option.length() > 0)) {
+                try {
+                    _port = Integer.parseInt(option);
+                } catch (NumberFormatException e) {
+                    // ignore bad values
+                }
+            }
+            _cell.say("InfoExporter: Using tcp listen port number : " + _port);
 
-	      _server = new ServerSocket(_port);
+            _server = new ServerSocket(_port);
 
-	      _sendThread  = new Thread( this ) ;
-	      _sendThread.start() ;
+            _sendThread = new Thread(this);
+            _sendThread.start();
 
-	   }catch(IOException ie){ 
-	      cell.say("InfoExporter: NOT STARTED!");
-	      throw ie ;
-	   }
+        } catch (IOException ie) {
+            cell.say("InfoExporter: NOT STARTED!");
+            throw ie;
+        }
 
 	}
-	
-	
+
+
 	/**
-	 * This method stops the loop and cleans the socket.<br> 
+	 * This method stops the loop and cleans the socket.<br>
 	 * It must to be called explicitly when the InfoCollector
 	 * is killed to force the socket closure.
 	 * If it is not called when InfoCollector is killed, the
 	 * port 22111 continues to be binded from the thread and
 	 * will not be more possible instanciates correctly a new
-	 * InfoCollector cell. 
+	 * InfoCollector cell.
 	 */
 	public void cleanUp(){
 		try{
 		      _continue = false;
 		      _server.close();
 		      _cell.say("InfoExporter: Finalized.");
-		   }catch(IOException ie){ 
+		   }catch(IOException ie){
 	              _cell.esay(ie);
 		   }
 	}
-	
-	
+
+
 	/**
 	 * Finalization: calls the cleaner.
 	 */
-	public void finalize(){
-		cleanUp();  
+	protected void finalize(){
+		cleanUp();
 		_cell.say("InfoExporter: Finalized.");
 	}
-	
-	
+
+
 	/**
 	 * Thread:<br>
 	 * In a strict-loop the server waits for a connection request.
@@ -104,9 +108,9 @@ public class InfoExporter implements Runnable {
 	 * and delivered on the stream.
 	 */
 	public void run(){
-		   
+
 	     if( Thread.currentThread() == _sendThread ){
-		synchronized( this ){            
+		synchronized( this ){
 		   while( _continue ){
 
 		      try{
@@ -136,10 +140,10 @@ public class InfoExporter implements Runnable {
 
 		   }
 	       }
-	    }     
+	    }
 	}
-	
-	
-	
+
+
+
 
 }
