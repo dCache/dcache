bba705f758ed6bd362c9e9572c1c6b4b1b7a78dc
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2007-12-28 11:02:22 +0000

    an attempt to test ActiveAdapter

---
+++ modules/dCacheJUnit/org/dcache/tests/ftp/ActiveAdapterTest.java
@@ -0,0 +1,49 @@
+package org.dcache.tests.ftp;
+
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import dmg.cells.nucleus.CellAdapter;
+
+import diskCacheV111.util.ActiveAdapter;
+
+public class ActiveAdapterTest {
+
+    private ActiveAdapter activeAdapter;
+
+    @Before
+    public void setUp() throws IOException {
+        activeAdapter = new ActiveAdapter((CellAdapter) null);
+    }
+
+    @After
+    public void  tearDown() {
+        activeAdapter.close();
+    }
+
+
+    @Test
+    public void testListenPort() throws Exception {
+
+        assertTrue("Failed to create a listen port", activeAdapter.getPoolListenerPort() > 0 );
+
+    }
+
+    @Test
+    public void testIsAlive() {
+
+        assertFalse("Can't be alive while not started", activeAdapter.isAlive() );
+
+        activeAdapter.start();
+
+        assertTrue("Failed to start", activeAdapter.isAlive() );
+
+    }
+
+}
