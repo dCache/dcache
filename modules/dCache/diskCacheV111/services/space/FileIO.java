922ad83c3ef9ee7af53087a93f99bab79e52703e
Dmitry Litvintsev <litvinse@fnal.gov>
2008-01-14 20:52:11 +0000

    merging files from SpaceManagerBranch

---
+++ modules/dCache/diskCacheV111/services/space/FileIO.java
@@ -0,0 +1,137 @@
+//______________________________________________________________________________
+//
+// $Id$ 
+// $Author$
+//
+// Infrastructure to retrieve objects from DB 
+//
+// created 11/07 by Dmitry Litvintsev (litvinse@fnal.gov)
+//
+//______________________________________________________________________________
+
+
+package diskCacheV111.services.space;
+import java.sql.*;
+import java.util.Set;
+import java.util.HashSet;
+import diskCacheV111.util.*;
+import diskCacheV111.util.IoPackage;
+import diskCacheV111.util.AccessLatency;
+import diskCacheV111.util.RetentionPolicy;
+import diskCacheV111.util.PnfsId;
+
+/*
+                Table "public.srmspacefile"
+       Column       |           Type           | Modifiers 
+--------------------+--------------------------+-----------
+ id                 | bigint                   | not null
+ vogroup            | character varying(32672) | 
+ vorole             | character varying(32672) | 
+ spacereservationid | bigint                   | 
+ sizeinbytes        | bigint                   | 
+ creationtime       | bigint                   | 
+ lifetime           | bigint                   | 
+ pnfspath           | character varying(32672) | 
+ pnfsid             | character varying(32672) | 
+ state              | integer                  | 
+Indexes:
+    "srmspacefile_pkey" PRIMARY KEY, btree (id)
+*/;
+
+
+
+public class FileIO extends IoPackage { 
+	
+	public static final String SRM_SPACEFILE_TABLE = ManagerSchemaConstants.SpaceFileTableName;
+	public static final String SELECT_BY_SPACERESERVATION_ID = 
+		"SELECT * FROM "+SRM_SPACEFILE_TABLE+" WHERE spacereservationid = ?";
+	public static final String SELECT_BY_ID="SELECT * FROM "+
+		SRM_SPACEFILE_TABLE+" WHERE  id = ?";
+	public static final String SELECT_BY_PNFSID =
+		"SELECT * FROM "+SRM_SPACEFILE_TABLE+" WHERE pnfsId=?";
+	public static final String SELECT_BY_PNFSPATH =
+		"SELECT * FROM "+SRM_SPACEFILE_TABLE+" WHERE pnfspath=?";
+	public static final String SELECT_BY_PNFSID_AND_PNFSPATH =
+		"SELECT * FROM "+SRM_SPACEFILE_TABLE+" WHERE pnfsid=? AND pnfspath=?";
+	public static final String SELECT_USED_SPACE_IN_SPACEFILES = "SELECT sum(sizeinbytes)  FROM "+
+		SRM_SPACEFILE_TABLE+" WHERE spacereservationid = ? AND state != ? "+FileState.FLUSHED.getStateId();
+
+	public static final String REMOVE_PNFSID_ON_SPACEFILE="UPDATE "+SRM_SPACEFILE_TABLE+
+		" SET pnfsid = NULL WHERE id=?";
+	public static final String REMOVE_PNFSID_AND_CHANGE_STATE_SPACEFILE="UPDATE "+SRM_SPACEFILE_TABLE+
+		" SET pnfsid = NULL, STATE=? WHERE id=?";
+	public static final String INSERT_WO_PNFSID  = "INSERT INTO "+SRM_SPACEFILE_TABLE+
+		" (id,vogroup,vorole,spacereservationid,sizeinbytes,creationtime,lifetime,pnfspath,pnfsid,state) "+
+		" VALUES  (?,?,?,?,?,?,?,?,NULL,?)";
+	public static final String INSERT_W_PNFSID  = "INSERT INTO "+SRM_SPACEFILE_TABLE+
+		" (id,vogroup,vorole,spacereservationid,sizeinbytes,creationtime,lifetime,pnfspath,pnfsid,state) "+
+		" VALUES  (?,?,?,?,?,?,?,?,?,?)";
+	public static final String DELETE="DELETE FROM "+SRM_SPACEFILE_TABLE+" WHERE id=?";
+
+	public static final String SELECT_FOR_UPDATE_BY_PNFSPATH = "SELECT * FROM "+SRM_SPACEFILE_TABLE+" WHERE  pnfspath=? FOR UPDATE ";
+	public static final String SELECT_FOR_UPDATE_BY_PNFSID   = "SELECT * FROM "+SRM_SPACEFILE_TABLE+" WHERE  pnfsid=?   FOR UPDATE ";
+	public static final String SELECT_FOR_UPDATE_BY_ID       = "SELECT * FROM "+SRM_SPACEFILE_TABLE+" WHERE  id=?       FOR UPDATE ";
+	
+	public static final String UPDATE = "UPDATE "+SRM_SPACEFILE_TABLE+
+		" SET vogroup=?, vorole=?, sizeinbytes=?, lifetime=?, pnfsid=?, state=? WHERE id=?";
+
+	public static final String UPDATE_WO_PNFSID = "UPDATE "+SRM_SPACEFILE_TABLE+
+		" SET vogroup=?, vorole=?, sizeinbytes=?, lifetime=?, state=? WHERE id=?";
+
+	public FileIO() {
+	}
+
+	public HashSet select( Connection connection,
+				String txt) throws SQLException {
+		HashSet<File>  container = new HashSet<File>();
+ 		Statement s = connection.createStatement();
+ 		ResultSet set = s.executeQuery(txt);
+ 		while (set.next()) { 
+			String pnfsIdString = set.getString("pnfsId");
+			PnfsId pnfsId = null;
+			if ( pnfsIdString != null ) {
+				pnfsId = new PnfsId( pnfsIdString );
+			}
+ 			container.add(
+ 				new File(set.getLong("id"),
+					 set.getString("vogroup"),
+					 set.getString("vorole"),
+					 set.getLong("spacereservationid"),
+					 set.getLong("sizeinbytes"),
+					 set.getLong("creationtime"),
+					 set.getLong("lifetime"),
+					 set.getString("pnfspath"),
+					 pnfsId, 
+					 FileState.getState(set.getInt("state"))));
+		}
+ 		s.close();
+		return container;
+	}
+	
+	public HashSet selectPrepared(Connection connection,
+				     PreparedStatement statement) 
+		throws SQLException {
+		HashSet<File>  container = new HashSet<File>();
+		ResultSet set = statement.executeQuery();
+ 		while (set.next()) { 
+			String pnfsIdString = set.getString("pnfsId");
+			PnfsId pnfsId = null;
+			if ( pnfsIdString != null ) {
+				pnfsId = new PnfsId( pnfsIdString );
+			}
+ 			container.add(
+ 				new File(set.getLong("id"),
+					 set.getString("vogroup"),
+					 set.getString("vorole"),
+					 set.getLong("spacereservationid"),
+					 set.getLong("sizeinbytes"),
+					 set.getLong("creationtime"),
+					 set.getLong("lifetime"),
+					 set.getString("pnfspath"),
+					 pnfsId, 
+					 FileState.getState(set.getInt("state"))));
+		}
+		return container;
+	}
+	
+}
--- modules/dCache/diskCacheV111/services/space/LinkGroup.java
+++ modules/dCache/diskCacheV111/services/space/LinkGroup.java
@@ -15,143 +15,179 @@ import diskCacheV111.util.VOInfo;
  * @author timur
  */
 public class LinkGroup implements java.io.Serializable{
-    private long id;
-    private String name;
-    private long freeSpace;
-    private boolean onlineAllowed ;
-    private boolean nearlineAllowed ;
-    private boolean replicaAllowed ;
-    private boolean outputAllowed;
-    private boolean custodialAllowed ;
-    private VOInfo[] vos;
-    private long updateTime;
-    /** Creates a new instance of Link */
-    public LinkGroup(){
-    }
-    
-    public LinkGroup(
-            long id,
-            String name,
-            long freeSpace,
-            VOInfo[] vos,
-            boolean onlineAllowed,
-            boolean nearlineAllowed,
-            boolean replicaAllowed,
-            boolean outputAllowed,
-            boolean custodialAllowed) {
-        this.id=id;
-        this.name=name;
-        this.freeSpace = freeSpace;
-        this.vos = vos;
-        this.onlineAllowed = onlineAllowed;
-        this.nearlineAllowed = nearlineAllowed;
-        this.replicaAllowed = replicaAllowed;
-        this.outputAllowed = outputAllowed;
-        this.custodialAllowed = custodialAllowed;
-    }
+	private long id;
+	private String name;
+	private long freeSpace;
+	private boolean onlineAllowed ;
+	private boolean nearlineAllowed ;
+	private boolean replicaAllowed ;
+	private boolean outputAllowed;
+	private boolean custodialAllowed ;
+	private VOInfo[] vos;
+	private long updateTime;
+	private long reservedSpaceInBytes;
 
-    public long getId() {
-        return id;
-    }
+	public LinkGroup(){
+	}
+	
+	public LinkGroup(
+		long id,
+		String name,
+		long freeSpace,
+		VOInfo[] vos,
+		boolean onlineAllowed,
+		boolean nearlineAllowed,
+		boolean replicaAllowed,
+		boolean outputAllowed,
+		boolean custodialAllowed,
+		long reseved) {
+		this.id=id;
+		this.name=name;
+		this.freeSpace = freeSpace;
+		this.vos = vos;
+		this.onlineAllowed = onlineAllowed;
+		this.nearlineAllowed = nearlineAllowed;
+		this.replicaAllowed = replicaAllowed;
+		this.outputAllowed = outputAllowed;
+		this.custodialAllowed = custodialAllowed;
+		this.reservedSpaceInBytes = reseved;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	//
+	// for backward compatibility
+	//
+	public LinkGroup(
+		long id,
+		String name,
+		long freeSpace,
+		VOInfo[] vos,
+		boolean onlineAllowed,
+		boolean nearlineAllowed,
+		boolean replicaAllowed,
+		boolean outputAllowed,
+		boolean custodialAllowed) {
+		this.id=id;
+		this.name=name;
+		this.freeSpace = freeSpace;
+		this.vos = vos;
+		this.onlineAllowed = onlineAllowed;
+		this.nearlineAllowed = nearlineAllowed;
+		this.replicaAllowed = replicaAllowed;
+		this.outputAllowed = outputAllowed;
+		this.custodialAllowed = custodialAllowed;
+		this.reservedSpaceInBytes = 0L;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public long getFreeSpace() {
+		return freeSpace;
+	}
+	
+	public void setFreeSpace(long freeSpace) {
+		this.freeSpace = freeSpace;
+	}
+	
+	
+	public String toString(){
+		StringBuffer sb = new StringBuffer();
+		toStringBuffer(sb);
+		return sb.toString();
+	}
+	public void toStringBuffer(StringBuffer sb){
+		sb.append(id).append(' ');
+		sb.append("Name:").append(name).append(' ');
+		sb.append("FreeSpace:").append(freeSpace).append(' ');
+		sb.append("ReservedSpace:").append(reservedSpaceInBytes).append(' ');
+		sb.append("VOs:");
+		for(int i = 0; i<vos.length; ++i) {
+			sb.append('{').append(vos[i]).append('}');
+		}
+		sb.append(' ');
+		sb.append("onlineAllowed:").append(onlineAllowed).append(' ');
+		sb.append("nearlineAllowed:").append(nearlineAllowed).append(' ');
+		sb.append("replicaAllowed:").append(replicaAllowed).append(' ');
+		sb.append("onlineAllowed:").append(onlineAllowed).append(' ');
+		sb.append("custodialAllowed:").append(custodialAllowed).append(' ');
+		sb.append("UpdateTime:").append(updateTime);
+	}
+	
+	
+	public VOInfo[] getVOs() {
+		return vos;
+	}
+	
+	public void setVOs(VOInfo[] vos) {
+		this.vos = vos;
+	}
+	
+	public long getUpdateTime() {
+		return updateTime;
+	}
+	
+	public void setUpdateTime(long updateTime) {
+		this.updateTime = updateTime;
+	}
+	
+	public boolean isOnlineAllowed() {
+		return onlineAllowed;
+	}
+	
+	public void setOnlineAllowed(boolean onlineAllowed) {
+		this.onlineAllowed = onlineAllowed;
+	}
+	
+	public boolean isNearlineAllowed() {
+		return nearlineAllowed;
+	}
+	
+	public void setNearlineAllowed(boolean nearlineAllowed) {
+		this.nearlineAllowed = nearlineAllowed;
+	}
+	
+	public boolean isReplicaAllowed() {
+		return replicaAllowed;
+	}
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getFreeSpace() {
-        return freeSpace;
-    }
-
-    public void setFreeSpace(long freeSpace) {
-        this.freeSpace = freeSpace;
-    }
-
-    
-    public String toString(){
-        StringBuffer sb = new StringBuffer();
-        toStringBuffer(sb);
-        return sb.toString();
-    }
-    public void toStringBuffer(StringBuffer sb){
-        sb.append(id).append(' ');
-        sb.append("Name:").append(name).append(' ');
-        sb.append("FreeSpace:").append(freeSpace).append(' ');
-        sb.append("VOs:");
-        for(int i = 0; i<vos.length; ++i) {
-            sb.append('{').append(vos[i]).append('}');
-        }
-        sb.append(' ');
-        sb.append("onlineAllowed:").append(onlineAllowed).append(' ');
-        sb.append("nearlineAllowed:").append(nearlineAllowed).append(' ');
-        sb.append("replicaAllowed:").append(replicaAllowed).append(' ');
-        sb.append("onlineAllowed:").append(onlineAllowed).append(' ');
-        sb.append("custodialAllowed:").append(custodialAllowed).append(' ');
-        sb.append("UpdateTime:").append(updateTime);
-    }
-
-
-    public VOInfo[] getVOs() {
-        return vos;
-    }
-
-    public void setVOs(VOInfo[] vos) {
-        this.vos = vos;
-    }
-
-    public long getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(long updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public boolean isOnlineAllowed() {
-        return onlineAllowed;
-    }
-
-    public void setOnlineAllowed(boolean onlineAllowed) {
-        this.onlineAllowed = onlineAllowed;
-    }
-
-    public boolean isNearlineAllowed() {
-        return nearlineAllowed;
-    }
-
-    public void setNearlineAllowed(boolean nearlineAllowed) {
-        this.nearlineAllowed = nearlineAllowed;
-    }
-
-    public boolean isReplicaAllowed() {
-        return replicaAllowed;
-    }
-
-    public void setReplicaAllowed(boolean replicaAllowed) {
-        this.replicaAllowed = replicaAllowed;
-    }
-
-    public boolean isOutputAllowed() {
-        return outputAllowed;
-    }
-
-    public void setOutputAllowed(boolean outputAllowed) {
-        this.outputAllowed = outputAllowed;
-    }
-
-    public boolean isCustodialAllowed() {
-        return custodialAllowed;
-    }
-
-    public void setCustodialAllowed(boolean custodialAllowed) {
-        this.custodialAllowed = custodialAllowed;
-    }
-    
+	public void setReplicaAllowed(boolean replicaAllowed) {
+		this.replicaAllowed = replicaAllowed;
+	}
+	
+	public boolean isOutputAllowed() {
+		return outputAllowed;
+	}
+	
+	public void setOutputAllowed(boolean outputAllowed) {
+		this.outputAllowed = outputAllowed;
+	}
+	
+	public boolean isCustodialAllowed() {
+		return custodialAllowed;
+	}
+	
+	public void setCustodialAllowed(boolean custodialAllowed) {
+		this.custodialAllowed = custodialAllowed;
+	}
+	
+	public long getReservedSpaceInBytes() {
+		return reservedSpaceInBytes;
+	}
+	
+	public void setReservedSpaceInBytes(long reserved) { 
+		this.reservedSpaceInBytes = reserved;
+	}
 }
