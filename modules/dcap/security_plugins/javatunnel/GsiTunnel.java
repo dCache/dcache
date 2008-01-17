68e00fbc2a816d38a3f3abe070da9963f085d77c
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2008-01-17 10:27:43 +0000

    use telescopic constructor

--- modules/dcap/security_plugins/javatunnel/GsiTunnel.java
+++ modules/dcap/security_plugins/javatunnel/GsiTunnel.java
@@ -26,84 +26,67 @@ import org.gridforum.jgss.ExtendedGSSManager;
 
 
 class GsiTunnel extends GssTunnel  {
-    
+
     private ExtendedGSSContext _e_context = null;
 
     MessageProp _prop =  new MessageProp(true);
-    
+
     private String service_key           = "/etc/grid-security/hostkey.pem";
     private String service_cert          = "/etc/grid-security/hostcert.pem";
-    private String service_trusted_certs = "/etc/grid-security/certificates";                   
-    
+    private String service_trusted_certs = "/etc/grid-security/certificates";
+
     // Creates a new instance of GssTunnel
     public GsiTunnel(String dummy) {
-
-        try {
-            GlobusCredential serviceCredential;
-            
-            serviceCredential =new GlobusCredential(service_cert, service_key);
-            
-            GSSCredential cred = new GlobusGSSCredentialImpl(serviceCredential, GSSCredential.ACCEPT_ONLY);
-            TrustedCertificates trusted_certs = TrustedCertificates.load(service_trusted_certs);
-            GSSManager manager = ExtendedGSSManager.getInstance();
-            _e_context = (ExtendedGSSContext) manager.createContext(cred);
-            
-            
-            _e_context.setOption(GSSConstants.GSS_MODE, GSIConstants.MODE_GSI);
-            _e_context.setOption(GSSConstants.TRUSTED_CERTIFICATES, trusted_certs);
-                        
-            _context = _e_context;
-            
-            // do not use cannel binding with GSIGSS
-            super.useChannelBinding(false);
-        }catch( Exception e ) {
-            e.printStackTrace();
-        }
+        this(dummy, true);
     }
-    
-    
+
+
     public GsiTunnel(String dummy, boolean init) {
         if( init ) {
             try {
                 GlobusCredential serviceCredential;
-                
+
                 serviceCredential =new GlobusCredential(service_cert, service_key);
-                
+
                 GSSCredential cred = new GlobusGSSCredentialImpl(serviceCredential, GSSCredential.ACCEPT_ONLY);
                 TrustedCertificates trusted_certs = TrustedCertificates.load(service_trusted_certs);
                 GSSManager manager = ExtendedGSSManager.getInstance();
                 _e_context = (ExtendedGSSContext) manager.createContext(cred);
                 _e_context.setOption(GSSConstants.GSS_MODE, GSIConstants.MODE_GSI);
-                _e_context.setOption(GSSConstants.TRUSTED_CERTIFICATES, trusted_certs);   
-                
+                _e_context.setOption(GSSConstants.TRUSTED_CERTIFICATES, trusted_certs);
+
                 _context = _e_context;
                 // do not use cannel binding with GSIGSS
                 super.useChannelBinding(false);
 
-            }catch( Exception e ) { }
-            
-        }        
+            }catch( Exception e ) {
+                e.printStackTrace();
+            }
+
+        }
     }
-    
+
+    @Override
     public boolean verify( InputStream in, OutputStream out, Object addon) {
-                
-        try {        	
+
+        try {
         	if( super.verify(in, out, addon) ) {
         		scanExtendedAttributes(_e_context);
         	}
         } catch( Exception e) { }
-        
+
         return _context.isEstablished();
     }
-    
-        
+
+
+    @Override
     public Convertable makeCopy() {
         return new GsiTunnel( null, true  );
     }
-        
+
     private void scanExtendedAttributes(ExtendedGSSContext gssContext)  {
-    	
-    	
+
+
 		String fqanValue = null;
 		try {
 
@@ -111,22 +94,22 @@ class GsiTunnel extends GssTunnel  {
 					.inquireByOid(GSSConstants.X509_CERT_CHAIN);
 
 			if(chain == null ) {
-				// some thing is grong here
+				// some thing is wrong here
 				return;
 			}
 
 			VOMSValidator validator = new VOMSValidator(chain);
 			validator.parse();
-			
+
 			List listOfAttributes = validator.getVOMSAttributes();
-	 
+
 			Iterator i = listOfAttributes.iterator();
-			
+
 			// currently take first one only
-			
+
 			if (i.hasNext()) {
-				
-				VOMSAttribute vomsAttribute = (VOMSAttribute) i.next();				
+
+				VOMSAttribute vomsAttribute = (VOMSAttribute) i.next();
 				List listOfFqans = vomsAttribute.getFullyQualifiedAttributes();
 				Iterator j = listOfFqans.iterator();
 
@@ -136,12 +119,12 @@ class GsiTunnel extends GssTunnel  {
 					FQAN fqan = new FQAN(fqanValue);
 					_role = fqanValue;
 				}
-			}			
-									
-		}catch(Exception e) {			
+			}
+
+		}catch(Exception e) {
 			e.printStackTrace();
 		}
 
-	}    
-    
+	}
+
 }
