cc7eb613d4c2e4d349e307ea69bef482bbf572ea
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2008-01-22 09:52:28 +0000

    more tests

--- modules/dCacheJUnit/org/dcache/tests/auth/VoRoleTest.java
+++ modules/dCacheJUnit/org/dcache/tests/auth/VoRoleTest.java
@@ -1,10 +1,11 @@
 package org.dcache.tests.auth;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import diskCacheV111.services.authorization.AuthorizationServiceException;
 import diskCacheV111.services.authorization.GPLAZMALiteVORoleAuthzPlugin;
 import diskCacheV111.util.UserAuthBase;
 
@@ -12,8 +13,9 @@ public class VoRoleTest {
 
     private GPLAZMALiteVORoleAuthzPlugin _voAuth;
     private static final String MY_DN = "/O=GermanGrid/OU=DESY/CN=Tigran Mkrtchyan";
+    private static final String FLAVIA = "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=flavia/CN=388195/CN=Flavia Donno";
     private static final String ROLE1 = "/dteam/Role=NULL/Capability=NULL";
-    private static final String ROLE2 = "/dteam/Role=NULL/Capability=NULL";
+    private static final String BADROLE = "/nonexist/Role=NULL/Capability=NULL";
 
     @Before
     public void setUp() throws Exception {
@@ -28,6 +30,25 @@ public class VoRoleTest {
 
         assertNotNull("can't find user record", pwdRecord);
 
+    }
+
+
+    @Test
+    public void testRegexp() throws Exception {
+        UserAuthBase pwdRecord = _voAuth.authorize(FLAVIA, ROLE1, null, null, null);
+        assertNotNull("can't find user record", pwdRecord);
+        assertEquals("Incorrect user record received", 1001, pwdRecord.UID);
+
+    }
+
+    @Test
+    public void testNonExisting() throws Exception {
+        try {
+            UserAuthBase pwdRecord = _voAuth.authorize(MY_DN, BADROLE, null, null, null);
+            fail("Record for non existing role returned");
+        }catch(AuthorizationServiceException ae) {
+            // OK
+        }
     }
 
 }
