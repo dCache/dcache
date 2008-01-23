5465d260723da8f2b76a614229de23c0cf44bd0e
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2008-01-23 09:47:17 +0000

    added grid-map based test

---
+++ modules/dCacheJUnit/org/dcache/tests/auth/GridMapFileTest.java
@@ -0,0 +1,37 @@
+package org.dcache.tests.auth;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import diskCacheV111.services.authorization.AuthorizationServicePlugin;
+import diskCacheV111.services.authorization.GridMapFileAuthzPlugin;
+import diskCacheV111.util.UserAuthBase;
+
+
+
+
+public class GridMapFileTest {
+
+    private static final String MY_DN = "/O=GermanGrid/OU=DESY/CN=Tigran Mkrtchyan";
+    private static final String ROLE1 = "/dteam/Role=NULL/Capability=NULL";
+    private AuthorizationServicePlugin _voAuth;
+
+    @Before
+    public void setUp() throws Exception {
+        _voAuth = new GridMapFileAuthzPlugin ("modules/dCacheJUnit/org/dcache/tests/auth/grid-mapfile",
+                "modules/dCacheJUnit/org/dcache/tests/auth/grid-mapfile-storage-authzdb", 1);
+    }
+
+
+    @Test
+    public void testVoRole() throws Exception {
+
+        UserAuthBase pwdRecord = _voAuth.authorize(MY_DN, ROLE1, null, null, null);
+
+        assertNotNull("can't find user record", pwdRecord);
+
+    }
+
+}
