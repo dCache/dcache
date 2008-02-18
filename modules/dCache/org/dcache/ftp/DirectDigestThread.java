fc64d1b8f506225e7fa27425c3f92ca30c309ec7
Gerd Behrmann <behrmann@nordu.net>
2008-02-18 18:29:50 +0000

    Do not rely on the current file position of the channel, since doing so is not safe when the file is used by multiple threads.

--- modules/dCache/org/dcache/ftp/DirectDigestThread.java
+++ modules/dCache/org/dcache/ftp/DirectDigestThread.java
@@ -8,12 +8,12 @@ public class DirectDigestThread extends DigestThread
 {
     public static final int BLOCK_SIZE = 4096;  // 4 kb
 
-    public DirectDigestThread(FileChannel channel, BlockLog log, MessageDigest digest) 
+    public DirectDigestThread(FileChannel channel, BlockLog log, MessageDigest digest)
     {
         super(channel, log, digest);
     }
 
-    public void run() 
+    public void run()
     {
 	try {
             ByteBuffer buffer = ByteBuffer.allocateDirect(BLOCK_SIZE);
@@ -21,7 +21,7 @@ public class DirectDigestThread extends DigestThread
             long read;
 
             advance(position + BLOCK_SIZE);
-            read = _channel.read(buffer);
+            read = _channel.read(buffer, position);
             while (read >= 0) {
                 buffer.flip();
                 _digest.update(buffer);
@@ -29,19 +29,9 @@ public class DirectDigestThread extends DigestThread
 
                 buffer.clear();
                 advance(position + BLOCK_SIZE);
-                read = _channel.read(buffer);
+                read = _channel.read(buffer, position);
             }
 	} catch (Exception e) {
-	    /* In theory we could attempt to resolve problems with the
-	     * map() call by trying to map a smaller memory
-	     * area. However, if we are so pressed in address space
-	     * that we cannot even map MAX_SIZE memory, then we may be
-	     * better of freeing some resources by killing the
-	     * thread. Over time, it may be better to coordinate the
-	     * computation of checksums from several transfer by using
-	     * a commong thread pool, thus limiting the number of
-	     * concurrent digest threads.
-	     */
 	    _error = e;
 	}
     }
