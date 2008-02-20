068b20dc3bc9b71844734385ae72cf1e8fd61aec
Dmitry Litvintsev <litvinse@fnal.gov>
2008-02-20 19:20:00 +0000

    syncronize with branch

--- modules/dCache/diskCacheV111/services/space/ManagerV2.java
+++ modules/dCache/diskCacheV111/services/space/ManagerV2.java
@@ -303,59 +303,65 @@ public class ManagerV2
 		                                    "                                                # valid examples of size: 1000, 100kB, 100KB, 100KiB, 100MB, 100MiB, 100GB, 100GiB, 10.5TB, 100TiB \n" +
                                                     "                                                 # see http://en.wikipedia.org/wiki/Gigabyte for explanation";
 
-	public String ac_update_space_reservation_$_2(Args args) throws Exception {
-		long reservationId = Long.parseLong(args.argv(0));
-		long size = 0L;
+	public final long stringToSize(String s)  throws Exception {
+		long size=0L;
 		int endIndex=0;
 		int startIndex=0;
-		String arg = args.argv(1);
-		if (arg.endsWith("kB") || arg.endsWith("KB")) { 
-			endIndex=arg.indexOf("KB");
+		if (s.endsWith("kB") || s.endsWith("KB")) { 
+			endIndex=s.indexOf("KB");
 			if (endIndex==-1) { 
-				endIndex=arg.indexOf("kB");	
+				endIndex=s.indexOf("kB");	
 			}
-			String sSize = arg.substring(startIndex,endIndex);
+			String sSize = s.substring(startIndex,endIndex);
 			size    = sSize.equals("") ? 1000L : (long)(Double.parseDouble(sSize)*1.e+3+0.5);
 		}
-		else if (arg.endsWith("KiB")) { 
-			endIndex=arg.indexOf("KiB");
-			String sSize = arg.substring(startIndex,endIndex);
+		else if (s.endsWith("KiB")) { 
+			endIndex=s.indexOf("KiB");
+			String sSize = s.substring(startIndex,endIndex);
 			size    = sSize.equals("") ? 1024L : (long)(Double.parseDouble(sSize)*1024.+0.5);
 		}
-		else if (arg.endsWith("MB")) { 
-			endIndex=arg.indexOf("MB");
-			String sSize = arg.substring(startIndex,endIndex);
+		else if (s.endsWith("MB")) { 
+			endIndex=s.indexOf("MB");
+			String sSize = s.substring(startIndex,endIndex);
 			size    = sSize.equals("") ? 1000000L : (long)(Double.parseDouble(sSize)*1.e+6+0.5);
 		}
-		else if (arg.endsWith("MiB")) { 
-			endIndex=arg.indexOf("MiB");
-			String sSize = arg.substring(startIndex,endIndex);
+		else if (s.endsWith("MiB")) { 
+			endIndex=s.indexOf("MiB");
+			String sSize = s.substring(startIndex,endIndex);
 			size    = sSize.equals("") ? 1048576L : (long)(Double.parseDouble(sSize)*1048576.+0.5);
 		}
-		else if (arg.endsWith("GB")) { 
-			endIndex=arg.indexOf("GB");
-			String sSize = arg.substring(startIndex,endIndex);
+		else if (s.endsWith("GB")) { 
+			endIndex=s.indexOf("GB");
+			String sSize = s.substring(startIndex,endIndex);
 			size    = sSize.equals("") ? 1000000000L : (long)(Double.parseDouble(sSize)*1.e+9+0.5);
 		}
-		else if (arg.endsWith("GiB")) { 
-			endIndex=arg.indexOf("GiB");
-			String sSize = arg.substring(startIndex,endIndex);
+		else if (s.endsWith("GiB")) { 
+			endIndex=s.indexOf("GiB");
+			String sSize = s.substring(startIndex,endIndex);
 			size    = sSize.equals("") ? 1073741824L : (long)(Double.parseDouble(sSize)*1073741824.+0.5);
 		}
-		else if (arg.endsWith("TB")) { 
-			endIndex=arg.indexOf("TB");
-			String sSize = arg.substring(startIndex,endIndex);
+		else if (s.endsWith("TB")) { 
+			endIndex=s.indexOf("TB");
+			String sSize = s.substring(startIndex,endIndex);
 			size    = sSize.equals("") ? 1000000000000L : (long)(Double.parseDouble(sSize)*1.e+12+0.5);
 		}
-		else if (arg.endsWith("TiB")) { 
-			endIndex=arg.indexOf("TiB");
-			String sSize = arg.substring(startIndex,endIndex);
+		else if (s.endsWith("TiB")) { 
+			endIndex=s.indexOf("TiB");
+			String sSize = s.substring(startIndex,endIndex);
 			size    = sSize.equals("") ? 1099511627776L : (long)(Double.parseDouble(sSize)*1099511627776.+0.5);
 		}
 		else {
-			size = Long.parseLong(arg);
-		}
-		try { 
+			size = Long.parseLong(s);
+		}		
+		return size;
+	}
+
+
+	public String ac_update_space_reservation_$_2(Args args) throws Exception {
+		long reservationId = Long.parseLong(args.argv(0));
+		long size = 0L;
+		try {
+			size = stringToSize(args.argv(1));
 			updateSpaceReservation(reservationId,
 					       null,
 					       null,
@@ -551,7 +557,13 @@ public class ManagerV2
 		" <sizeInBytes> <lifetimeInSecs (use quotes around negative one)>";   
 
 	public String ac_reserve_$_2(Args args) throws Exception {
-		long sizeInBytes = Long.parseLong(args.argv(0));
+		long sizeInBytes=0L;
+		try {
+			sizeInBytes=stringToSize(args.argv(0));
+		}
+		catch (Exception e) { 
+			return e.getMessage();	
+		}
 		long lifetime=Long.parseLong(args.argv(1));
 		if(lifetime > 0) {
 			lifetime *= 1000;
