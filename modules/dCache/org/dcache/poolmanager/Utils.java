d4e38a85f7297dc31bb083ce356da53ccd258c1b
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2008-04-07 10:22:10 +0000

    calculate pool size only once in case pool referenced by many links defined by the linkGroup

--- modules/dCache/org/dcache/poolmanager/Utils.java
+++ modules/dCache/org/dcache/poolmanager/Utils.java
@@ -1,8 +1,11 @@
 package org.dcache.poolmanager;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import diskCacheV111.poolManager.CostModule;
 import diskCacheV111.poolManager.PoolSelectionUnit;
@@ -23,14 +26,13 @@ public class Utils {
     public static Map<String, Long> linkGroupSize(PoolSelectionUnit selectionUnit, CostModule costModule ) {
 
         String [] linkGroups = selectionUnit.getLinkGroups();
-        //PoolLinkGroupInfo[] linkGroupInfo = new PoolLinkGroupInfo[linkGroups.length];
         Map<String, Long> linkGroupSize = new HashMap<String, Long>(linkGroups.length);
 
         /*
          * get list of all defined link groups
          * for each link group get list of links
          * for each link in the group find all active pools and
-         * calculate available space ( total - removable )
+         * calculate available space ( free + removable )
          */
 
 
@@ -39,6 +41,9 @@ public class Utils {
                 String[] links = selectionUnit.getLinksByGroupName(linkGroups[i_goup]);
                 long linkAvailableSpace = 0;
                 long linkTotalSpace = 0;
+
+                Set<String> referencedPools = new HashSet<String>();
+
                 for(int i_link = 0; i_link < links.length; i_link++ ) {
 
                     PoolSelectionUnit.SelectionLink selectionLink = selectionUnit.getLinkByName(links[i_link]);
@@ -47,10 +52,17 @@ public class Utils {
                         PoolSelectionUnit.SelectionPool pool = poolsIterator.next();
                         if ( pool.isEnabled() ) {
                             String poolName = pool.getName();
-                            PoolCostInfo poolCostInfo = costModule.getPoolCostInfo(poolName);
-                            if(poolCostInfo != null) {
-                                linkAvailableSpace += poolCostInfo.getSpaceInfo().getFreeSpace() + poolCostInfo.getSpaceInfo().getRemovableSpace();
-                                linkTotalSpace += poolCostInfo.getSpaceInfo().getTotalSpace();
+                            /*
+                             * calculate pool space only once. This can be an issue if pool exist in to different links
+                             * used by same pool group
+                             */
+                            if( !referencedPools.contains(poolName)) {
+                                referencedPools.add(poolName);
+                                PoolCostInfo poolCostInfo = costModule.getPoolCostInfo(poolName);
+                                if(poolCostInfo != null) {
+                                    linkAvailableSpace += poolCostInfo.getSpaceInfo().getFreeSpace() + poolCostInfo.getSpaceInfo().getRemovableSpace();
+                                    linkTotalSpace += poolCostInfo.getSpaceInfo().getTotalSpace();
+                                }
                             }
                         }
                     }
