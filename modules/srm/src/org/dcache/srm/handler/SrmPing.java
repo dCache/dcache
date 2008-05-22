29694d4464b4c44967d55923c28056762cfb3ea0
Dmitry Litvintsev <litvinse@fnal.gov>
2008-05-22 16:41:24 +0000

    add backend name to the faled response as well

--- modules/srm/src/org/dcache/srm/handler/SrmPing.java
+++ modules/srm/src/org/dcache/srm/handler/SrmPing.java
@@ -30,72 +30,74 @@ import org.dcache.srm.SRMProtocol;
  */
 
 public class SrmPing {
-    private final static String SFN_STRING="?SFN=";
-    AbstractStorageElement storage;
-    RequestUser            user;
-    RequestCredential      credential;
-    SrmPingRequest request;
-    SrmPingResponse        response;
+	private final static String SFN_STRING="?SFN=";
+	AbstractStorageElement storage;
+	RequestUser            user;
+	RequestCredential      credential;
+	SrmPingRequest request;
+	SrmPingResponse        response;
+	
+	public SrmPing(RequestUser user,
+		       RequestCredential credential,
+		       SrmPingRequest request,
+		       AbstractStorageElement storage,
+		       org.dcache.srm.SRM srm,
+		       String client_host) {
+		if (request == null) {
+			throw new NullPointerException("request is null");
+		}
+		this.request    = request;
+		this.user       = user;
+		this.credential = credential;
+		if (storage == null) {
+			throw new NullPointerException("storage is null");
+		}
+		this.storage = storage;
+	}
     
-    public SrmPing(RequestUser user,
-            RequestCredential credential,
-            SrmPingRequest request,
-            AbstractStorageElement storage,
-            org.dcache.srm.SRM srm,
-            String client_host) {
-        
-        if (request == null) {
-            throw new NullPointerException("request is null");
-        }
-        this.request    = request;
-        this.user       = user;
-        this.credential = credential;
-        if (storage == null) {
-            throw new NullPointerException("storage is null");
-        }
-        this.storage = storage;
-    }
+	private void say(String txt) {
+		if(storage!=null) {
+			storage.log("SrmPing "+txt);
+		}
+	}
     
-    private void say(String txt) {
-        if(storage!=null) {
-            storage.log("SrmPing "+txt);
-        }
-    }
+	private void esay(String txt) {
+		if(storage!=null) {
+			storage.elog("SrmPing "+txt);
+		}
+	}
     
-    private void esay(String txt) {
-        if(storage!=null) {
-            storage.elog("SrmPing "+txt);
-        }
-    }
+	private void esay(Throwable t) {
+		if(storage!=null) {
+			storage.elog(" SrmPing exception : ");
+			storage.elog(t);
+		}
+	}
     
-    private void esay(Throwable t) {
-        if(storage!=null) {
-            storage.elog(" SrmPing exception : ");
-            storage.elog(t);
-        }
-    }
+	public SrmPingResponse getResponse() {
+		if(response != null ) return response;
+		response = new SrmPingResponse();
+		response.setVersionInfo("v2.2");
+		response.setOtherInfo(new ArrayOfTExtraInfo(
+					      new TExtraInfo[]{
+						      new TExtraInfo("backend_type","dCache"),
+						      new TExtraInfo("backend_version", storage.getStorageBackendVersion())}));
+		return response;
+	}
     
-    public SrmPingResponse getResponse() {
-        if(response != null ) return response;
-        response = new SrmPingResponse();
-        response.setVersionInfo("v2.2");
-        response.setOtherInfo(new ArrayOfTExtraInfo(
-				  new TExtraInfo[]{
-				      new TExtraInfo("backend_type","dCache"),
-				      new TExtraInfo("backend_version", storage.getStorageBackendVersion())}));
-        return response;
-    }
+	public static final SrmPingResponse getFailedResponse(String text) {
+		return getFailedResponse(text,null);
+	}
     
-    public static final SrmPingResponse getFailedResponse(String text) {
-        return getFailedResponse(text,null);
-    }
-    
-    public static final SrmPingResponse getFailedResponse(String text, TStatusCode statusCode) {
-        if(statusCode == null) {
-            statusCode = TStatusCode.SRM_FAILURE;
-        }
-        SrmPingResponse response = new SrmPingResponse();
-        response.setVersionInfo("v2.2");
-        return response;
-    }
+	public static final SrmPingResponse getFailedResponse(String text, TStatusCode statusCode) {
+		if(statusCode == null) {
+			statusCode = TStatusCode.SRM_FAILURE;
+		}
+		SrmPingResponse response = new SrmPingResponse();
+		response.setVersionInfo("v2.2");
+		response.setOtherInfo(new ArrayOfTExtraInfo(
+					      new TExtraInfo[]{
+						      new TExtraInfo("backend_type","dCache")}));
+		return response;
+	}
 }
