cdf1e23350ed8706951a5087a8fb39a54aec2198
Owen Synge <owen.synge@desy.de>
2008-05-28 09:22:32 +0000

    Bug fixes to allow  node type to drive the configuration

--- skel/install/install.sh
+++ skel/install/install.sh
@@ -452,12 +452,14 @@ dcacheInstallGetIsAdminDoor()
     logmessage DEBUG "node is door so returns false"
     return 0
   fi
-  adminDoor=`printConfig adminDoor |  tr -s '[:upper:]' '[:lower:]'`
-  if [ "${adminDoor}" == "yes" ] ; then
-    return 1
-  fi
-  if [ "${adminDoor}" == "y" ] ; then
-    return 1
+  if [ "${NodeType}" == "custom" ] ; then
+    adminDoor=`printConfig adminDoor |  tr -s '[:upper:]' '[:lower:]'`
+    if [ "${adminDoor}" == "yes" ] ; then
+      return 1
+    fi
+    if [ "${adminDoor}" == "y" ] ; then
+      return 1
+    fi
   fi
   return 0
 }
@@ -481,12 +483,14 @@ dcacheInstallGetIsHttpDomain()
     logmessage DEBUG "node is door so returns false"
     return 0
   fi
-  httpDomain=`printConfig httpDomain |  tr -s '[:upper:]' '[:lower:]'`
-  if [ "${httpDomain}" == "yes" ] ; then
-    return 1
-  fi
-  if [ "${httpDomain}" == "y" ] ; then
-    return 1
+  if [ "${NodeType}" == "custom" ] ; then
+    httpDomain=`printConfig httpDomain |  tr -s '[:upper:]' '[:lower:]'`
+    if [ "${httpDomain}" == "yes" ] ; then
+      return 1
+    fi
+    if [ "${httpDomain}" == "y" ] ; then
+      return 1
+    fi
   fi
   return 0
 }
@@ -510,12 +514,14 @@ dcacheInstallGetIsLmDomain()
     logmessage DEBUG "node is door so returns false"
     return 0
   fi  
-  lmDomain=`printConfig lmDomain |  tr -s '[:upper:]' '[:lower:]'`
-  if [ "${lmDomain}" == "yes" ] ; then
-    return 1
-  fi
-  if [ "${lmDomain}" == "y" ] ; then
-    return 1
+  if [ "${NodeType}" == "custom" ] ; then
+    lmDomain=`printConfig lmDomain |  tr -s '[:upper:]' '[:lower:]'`
+    if [ "${lmDomain}" == "yes" ] ; then
+      return 1
+    fi
+    if [ "${lmDomain}" == "y" ] ; then
+      return 1
+    fi
   fi
   return 0
 }
@@ -538,12 +544,14 @@ dcacheInstallGetIsPoolManager()
     logmessage DEBUG "node is door so returns false"
     return 0
   fi
-  poolManager=`printConfig poolManager |  tr -s '[:upper:]' '[:lower:]'`
-  if [ "${poolManager}" == "yes" ] ; then
-    return 1
-  fi
-  if [ "${poolManager}" == "y" ] ; then
-    return 1
+  if [ "${NodeType}" == "custom" ] ; then
+    poolManager=`printConfig poolManager |  tr -s '[:upper:]' '[:lower:]'`
+    if [ "${poolManager}" == "yes" ] ; then
+      return 1
+    fi
+    if [ "${poolManager}" == "y" ] ; then
+      return 1
+    fi
   fi
   return 0
 }
@@ -566,12 +574,14 @@ dcacheInstallGetIsUtilityDomain()
     logmessage DEBUG "node is door so returns false"
     return 0
   fi
-  utilityDomain=`printConfig utilityDomain |  tr -s '[:upper:]' '[:lower:]'`
-  if [ "${utilityDomain}" == "yes" ] ; then
-    return 1
-  fi
-  if [ "${utilityDomain}" == "y" ] ; then
-    return 1
+  if [ "${NodeType}" == "custom" ] ; then
+    utilityDomain=`printConfig utilityDomain |  tr -s '[:upper:]' '[:lower:]'`
+    if [ "${utilityDomain}" == "yes" ] ; then
+      return 1
+    fi
+    if [ "${utilityDomain}" == "y" ] ; then
+      return 1
+    fi
   fi
   return 0
 }
@@ -594,12 +604,14 @@ dcacheInstallGetIsPnfsManager()
     logmessage DEBUG "node is door so returns false"
     return 0
   fi
-  pnfsManager=`printConfig pnfsManager |  tr -s '[:upper:]' '[:lower:]'`
-  if [ "${pnfsManager}" == "yes" ] ; then
-    return 1
-  fi
-  if [ "${pnfsManager}" == "y" ] ; then
-    return 1
+  if [ "${NodeType}" == "custom" ] ; then
+    pnfsManager=`printConfig pnfsManager |  tr -s '[:upper:]' '[:lower:]'`
+    if [ "${pnfsManager}" == "yes" ] ; then
+      return 1
+    fi
+    if [ "${pnfsManager}" == "y" ] ; then
+      return 1
+    fi
   fi
   return 0
 }
@@ -680,7 +692,6 @@ dcacheInstallPnfsMountPointClient()
       umount ${pnfsMountPoint}
       
     fi
-
     if [ -L "${pnfsMountPoint}" ] ; then
       logmessage INFO "Trying to remove symbolic link ${pnfsMountPoint} :"
       rm -f ${pnfsMountPoint}
@@ -692,7 +703,6 @@ dcacheInstallPnfsMountPointClient()
         exit 1
       fi
     fi
-
     if [ ! -d "${pnfsMountPoint}" ]; then
       if [ -e "${pnfsMountPoint}" ] ; then
         logmessage ABORT "The file ${pnfsMountPoint} is in the way. Please move it out of the way"
@@ -707,7 +717,6 @@ dcacheInstallPnfsMountPointClient()
     pnfsServer=$RET
     logmessage INFO "Will be mounted to ${pnfsServer}:/pnfsdoors by dcache-core start-up script."
   fi
-
   if [ ! -L "${PNFS_ROOT}/ftpBase" -a ! -e "${PNFS_ROOT}/ftpBase" ] ; then
     logmessage INFO "Creating link ${PNFS_ROOT}/ftpBase --> ${pnfsMountPoint} which is used by the GridFTP door."
     ln -s ${pnfsMountPoint} ${PNFS_ROOT}/ftpBase
@@ -724,7 +733,6 @@ dcacheInstallPnfsMountPointClient()
       exit 1
     fi
   fi
-
   if ! grep "^ftpBase=${PNFS_ROOT}/ftpBase" ${DCACHE_HOME}/config/dCacheSetup 2>/dev/null >/dev/null ; then
     logmessage WARNING "The file ${DCACHE_HOME}/config/dCacheSetup does not contain:"
     logmessage WARNING "   ftpBase=${PNFS_ROOT}/ftpBase"
@@ -889,13 +897,13 @@ dcacheInstallPnfsMountPoints()
   
   dcacheInstallGetIsPnfsManager
   isPnfsManager=$?
-
-  #echo admin=${isAdmin}${isGridFtp}${isPnfsManager}
-  if [ "${isPnfsManager}${isGridFtp}" == "01" -o "${isPnfsManager}${isSrm}" == "01" ] ; then
-    dcacheInstallPnfsMountPointClient
-  fi
   if [ "${isAdmin}" == "1" -o "${isPnfsManager}" == "1" ] ; then
     dcacheInstallPnfsMountPointServer
+  else 
+    if [ "${isPnfsManager}${isGridFtp}" == "01" -o "${isPnfsManager}${isSrm}" == "01" ] ; then
+      dcacheInstallPnfsMountPointClient
+    
+    fi
   fi  
   logmessage DEBUG "dcacheInstallPnfsMountPoints.stop"
 }
