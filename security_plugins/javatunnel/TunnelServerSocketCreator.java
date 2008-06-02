6e5a3ca17b5e6d560fda61bee1084ea494fb8d54
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2008-06-02 08:17:01 +0000

    Implemented SocketFactory interface

--- security_plugins/javatunnel/TunnelServerSocketCreator.java
+++ security_plugins/javatunnel/TunnelServerSocketCreator.java
@@ -4,33 +4,54 @@
 
 package javatunnel;
 
+import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.io.IOException;
 import java.lang.reflect.*;
 
-public class TunnelServerSocketCreator {
-    
-    
-    Convertable tunnel = null;
-    
+import javax.net.ServerSocketFactory;
+
+public class TunnelServerSocketCreator extends ServerSocketFactory {
+
+
+    Convertable _tunnel = null;
+
     public TunnelServerSocketCreator(String[] args) {
-                
+
+        super();
+
         try {
             Class c  = Class.forName(args[0]);
             Class [] classArgs = { java.lang.String.class } ;
             Constructor cc = c.getConstructor( classArgs );
             Object[] a = new Object[1];
-            a[0] = (Object)args[1];
-            tunnel = (Convertable)cc.newInstance(a);
+            a[0] = args[1];
+            _tunnel = (Convertable)cc.newInstance(a);
         }catch ( Exception e){
         }
 
     }
-    
-    
-    
-    
+
+
+    @Override
     public ServerSocket createServerSocket( int port ) throws java.io.IOException {
-        return new TunnelServerSocket(port, tunnel);
+        return new TunnelServerSocket(port, _tunnel);
     }
 
+    @Override
+    public ServerSocket createServerSocket() throws java.io.IOException {
+        return new TunnelServerSocket(_tunnel);
+    }
+
+    @Override
+    public ServerSocket createServerSocket(int port, int backlog)
+            throws IOException {
+        return new TunnelServerSocket(port, backlog, _tunnel);
+    }
+
+    @Override
+    public ServerSocket createServerSocket(int port, int backlog,
+            InetAddress ifAddress) throws IOException {
+        return new TunnelServerSocket(port, backlog, ifAddress, _tunnel);
+    }
 }
