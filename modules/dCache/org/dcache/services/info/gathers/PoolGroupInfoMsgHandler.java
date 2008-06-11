0de2020709ef2a678fbae8c3fa020aef6fac8d13
Paul Millar <paul.millar@desy.de>
2008-06-11 11:01:07 +0000

    Some cosmetic changes and support empty poolgroups by explicitly creating the branch (StateComposite) if no metrics are to be added.

--- modules/dCache/org/dcache/services/info/gathers/PoolGroupInfoMsgHandler.java
+++ modules/dCache/org/dcache/services/info/gathers/PoolGroupInfoMsgHandler.java
@@ -1,6 +1,7 @@
 package org.dcache.services.info.gathers;
 
 import org.apache.log4j.Logger;
+import org.dcache.services.info.base.StateComposite;
 import org.dcache.services.info.base.StateUpdate;
 import org.dcache.services.info.base.StatePath;
 
@@ -14,8 +15,6 @@ public class PoolGroupInfoMsgHandler extends CellMessageHandlerSkel {
 
 		_log.info( "processing new poolgroup information");
 		
-		StateUpdate update = new StateUpdate();
-		
 		if( !msgPayload.getClass().isArray()) {
 			_log.error( "received a message that isn't an array");
 			return;
@@ -27,13 +26,23 @@ public class PoolGroupInfoMsgHandler extends CellMessageHandlerSkel {
 			_log.error( "Unexpected array size: "+array.length);
 			return;
 		}
-		
+
+		// Map the array into slightly more meaningful components.
 		String poolgroupName = (String) array[0];
+		Object poolNames[] = (Object []) array[1];
+		Object linkNames[] = (Object []) array[2];
+		
+		StateUpdate update = new StateUpdate();
 
 		StatePath thisPoolGroupPath = POOLGROUPS_PATH.newChild( poolgroupName);
-				
-		addItems( update, thisPoolGroupPath.newChild("pools"), (Object []) array[1], metricLifetime);
-		addItems( update, thisPoolGroupPath.newChild("links"), (Object []) array[2], metricLifetime);
+
+		if( poolNames.length + linkNames.length == 0) {
+			// Add an entry, even though this poolgroup is empty.
+			update.appendUpdate( thisPoolGroupPath, new StateComposite( metricLifetime));
+		} else {		
+			addItems( update, thisPoolGroupPath.newChild("pools"), poolNames, metricLifetime);
+			addItems( update, thisPoolGroupPath.newChild("links"), linkNames, metricLifetime);
+		}
 
 		applyUpdates( update);
 	}
