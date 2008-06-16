8d5f26a05c4b24624240a902ce072fe457d5cdd0
Owen Synge <owen.synge@desy.de>
2008-06-16 12:38:49 +0000

    bug fix to support restarting chimera NFS server

--- modules/external/Chimera/chimera-nfs-run.sh
+++ modules/external/Chimera/chimera-nfs-run.sh
@@ -15,50 +15,61 @@ pfile=/var/run/chimera-nfsv3.pid
 . ${ourHomeDir}/classes/extern.classpath
 . ${ourHomeDir}/config/dCacheSetup
 
+
+dCacheChimeraStart()
+{
+  if [ -f ${pfile} ]
+  then
+    pid=`cat ${pfile}`
+    kill -0 ${pid} > /dev/null 2>&1
+    if [ $? -eq 0 ]
+    then
+      echo "Old NFS process still running"
+      exit 1
+    fi
+  fi
+
+  echo "Starting Chimera-NFSv3 interface"
+  ${java} ${java_options} -classpath ${externalLibsClassPath} \
+    -Xmx512M org.dcache.chimera.nfs.v3.Main2 \
+  ${ourHomeDir}/config/chimera-config.xml > ${log} 2>&1 &
+  echo $! > ${pfile}
+}
+
+
+dCacheChimeraStop()
+{
+  if [ -f ${pfile} ]
+  then
+    pid=`cat ${pfile}`
+    kill -0 ${pid} > /dev/null 2>&1
+    if [ $? -eq 0 ]
+    then
+      echo "Shuting down Chimera-NFSv3 interface"
+      kill `cat ${pfile}`
+      rm -f ${pfile}
+    else
+      echo "NFS process not running"
+      exit 1
+    fi
+  else
+    echo "Pid file missing. NFS process not running?"
+    exit 1;
+  fi
+}
 case $1 in
 
     start)
-        if [ -f ${pfile} ]
-        then
-            pid=`cat ${pfile}`
-            kill -0 ${pid} > /dev/null 2>&1
-            if [ $? -eq 0 ]
-            then
-                echo "Old NFS process still running"
-                exit 1
-            fi
-        fi
-
-        echo "Starting Chimera-NFSv3 interface"
-        ${java} ${java_options} -classpath ${externalLibsClassPath} \
-              -Xmx512M org.dcache.chimera.nfs.v3.Main2 \
-              ${ourHomeDir}/config/chimera-config.xml > ${log} 2>&1 &
-        echo $! > ${pfile}
+        dCacheChimeraStart
         ;;
 
     stop)
-        if [ -f ${pfile} ]
-        then
-            pid=`cat ${pfile}`
-            kill -0 ${pid} > /dev/null 2>&1
-            if [ $? -eq 0 ]
-            then
-                echo "Shuting down Chimera-NFSv3 interface"
-                kill `cat ${pfile}`
-                rm -f ${pfile}
-            else
-                echo "NFS process not running"
-                exit 1
-            fi
-        else
-            echo "Pid file missing. NFS process not running?"
-            exit 1;
-        fi
+        dCacheChimeraStop
         ;;
 
     restart)
-        stop
-        start
+        dCacheChimeraStop
+        dCacheChimeraStart
         ;;
     *)
         echo "Usage: `basename $0` [start | stop | restart]"
