eb447070a5a6b01c5d70b7a0e59da9af8364adf2
Gerd Behrmann <behrmann@nordu.net>
2008-06-19 13:28:19 +0000

    Added a helper cell to act as glue between the Cell framework and the Spring framework.

---
+++ modules/dCache/org/dcache/util/SpringCell.java
@@ -0,0 +1,145 @@
+package org.dcache.util;
+
+import dmg.cells.nucleus.CellAdapter;
+import dmg.util.Args;
+import java.util.Dictionary;
+import java.util.Properties;
+import java.util.Enumeration;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * Helper cell to bridge between the Cell framework and the Spring
+ * framework.
+ *
+ * This cell can be instantiated from a Cell batch file and takes a
+ * Spring XML configuration file as an argument, for instance:
+ *
+ * {@literal
+ * create org.dcache.util.SpringCell spring*  \
+ *   "file:${ourHomeDir}/config/pool.xml"
+ * }
+ *
+ * A ClassPathXmlApplicationContext is instantiated for this XML
+ * file. Based on the content of the XML file, Spring can instantiate
+ * other cells.
+ *
+ * The domain context and the cell arguments are made available as a
+ * Spring resource called 'domaincontext:' in Java Properties property
+ * file format. This can be used for setting up placeholder
+ * substitution in the Spring container, for instance:
+ *
+ * {@literal
+ * <bean class="org.springframework.beans.factory.config.PropertyPlaceholderConfigurer">
+ *   <property name="location" value="domaincontext:"/>
+ * </bean>
+ * }
+ *
+ * Options on the form '-key=value' are made available as 'key=value'
+ * properties.  The cell argument string is made available through a
+ * property with the key 'arguments'. This can be used to pass the
+ * argument string on to beans. The XML file resource name and any
+ * unresolved Cell placeholders are stripped from the argument string.
+ */
+public class SpringCell extends CellAdapter
+{
+    private void mergeDictionary(Properties properties, Dictionary dictionary)
+    {
+        Enumeration e = dictionary.keys();
+        while (e.hasMoreElements()) {
+            Object key = e.nextElement();
+            Object value = dictionary.get(key);
+            properties.setProperty(key.toString(),value.toString());
+        }
+    }
+
+    /**
+     * Converts a cell Args object into string form. All options
+     * containing a ${...} placeholder are filtered away, since they
+     * usually are artefacts from unresolved variables in the batch
+     * file and we don't want Spring to resolve them.
+     */
+    public String argsToString(Args args)
+    {
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < args.argc(); i++) {
+            s.append(args.argv(i)).append(' ');
+        }
+
+        Dictionary options = args.options();
+        Enumeration e = options.keys();
+        while (e.hasMoreElements()) {
+            String key = e.nextElement().toString();
+            String value = options.get(key).toString();
+            if (!key.matches("\\$\\{.*\\}") && !value.matches("\\$\\{.*\\}")) {
+                if (value.length() > 0) {
+                    s.append('-').append(key).append('=').append(value);
+                } else {
+                    s.append('-').append(key);
+                }
+                s.append(' ');
+            }
+        }
+
+        return s.toString();
+    }
+
+    public SpringCell(String name, String arg) throws Exception
+    {
+	super(name, arg, true);
+        try {
+            Args args = getArgs();
+            String setup = args.argv(0);
+            args.shift();
+
+            /* Merge domain context and options to a set of
+             * properties.
+             */
+            Properties properties = new Properties();
+
+            properties.setProperty("arguments", argsToString(args));
+            mergeDictionary(properties, getDomainContext());
+            mergeDictionary(properties, args.options());
+
+            /* Convert to byte array form such that we can make it
+             * available as a Spring resource.
+             */
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            properties.store(out, "");
+            final byte[] conf = out.toByteArray();
+
+            /* Create the application context with the configuration
+             * available as a Spring resource.
+             */
+            ApplicationContext context =
+                new ClassPathXmlApplicationContext(setup)
+                {
+                    public Resource getResource(String location)
+                    {
+                        if (location.startsWith("domaincontext:")) {
+                            return new ByteArrayResource(conf) {
+                                /** Fake file name to make
+                                 * PropertyPlaceholderConfigurer
+                                 * happy.
+                                 */
+                                public String getFilename()
+                                {
+                                    return "domaincontext.properties";
+                                }
+                            };
+                        } else {
+                            return super.getResource(location);
+                        }
+                    }
+                };
+        } finally {
+            kill();
+        }
+    }
+}
