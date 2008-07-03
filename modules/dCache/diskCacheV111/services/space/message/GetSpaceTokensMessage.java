b5d295cc2f8f9dd85ec8b62e6b608287ed852bd3
Dmitry Litvintsev <litvinse@fnal.gov>
2008-07-03 20:49:08 +0000

    implement messages and modify existing messages along the line of Paul's request. See here: https://srm.fnal.gov/twiki/bin/view/SrmProject/SrmSpaceManager#Work_for_info_provider

--- modules/dCache/diskCacheV111/services/space/message/GetSpaceTokensMessage.java
+++ modules/dCache/diskCacheV111/services/space/message/GetSpaceTokensMessage.java
@@ -1,3 +1,14 @@
+//______________________________________________________________________________
+//
+// Intended usage: specify requested space token id and get a set with one 
+//                 element back 
+//                 do not specify requested space token id and get the set 
+//                 containing all unexpired space tokens
+//
+//  $Id$
+//  $Author$
+//______________________________________________________________________________
+
 package diskCacheV111.services.space.message;
 
 import java.util.Set;
@@ -7,8 +18,21 @@ import diskCacheV111.services.space.Space;
 public class GetSpaceTokensMessage extends Message {
 
 	static final long serialVersionUID = -419540669938740860L;
+	private Long spacetokenId=null;
 
 	private Set<Space> list;
+
+	public void setSpaceTokenId(long id) { 
+		spacetokenId = new Long(id);
+	}
+
+	public void setSpaceTokenId(Long id) { 
+		spacetokenId = id;
+	}
+
+	public Long getSpaceTokenId() { 
+		return spacetokenId;
+	}
 
 	public GetSpaceTokensMessage() { 
 		setReplyRequired(true);
