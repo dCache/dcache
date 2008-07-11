ff2c53019b2d66038ab9f7b79cf4bc7d0d3c12e7
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2008-07-11 13:54:01 +0000

    javadoc update
    Sign-off-by: Gerd

--- modules/dCache/diskCacheV111/movers/MoverProtocol.java
+++ modules/dCache/diskCacheV111/movers/MoverProtocol.java
@@ -11,7 +11,7 @@ import  java.io.* ;
 public interface MoverProtocol {
    public static final int READ   =  1 ;
    public static final int WRITE  =  2 ;
-   //  
+   //
    // <init>( CellAdapter cell ) ;
    //
    public void runIO(
@@ -19,17 +19,40 @@ public interface MoverProtocol {
                   ProtocolInfo protocol ,
                   StorageInfo  storage ,
                   PnfsId       pnfsId  ,
-                  SpaceMonitor spaceMonitor , 
-                  int          access ) 
-                  
+                  SpaceMonitor spaceMonitor ,
+                  int          access )
+
           throws Exception ;
-   
+
    public void setAttribute( String name , Object attribute ) ;
    public Object getAttribute( String name ) ;
-        
+
+   /**
+    * Get number of bytes transfered. The number of bytes may exceed total file size if client
+    * does some seek requests in between.
+    *
+    * @return number of bytes
+    */
    public long getBytesTransferred() ;
+
+   /**
+    * Get time between transfers begin and end. If Mover is sill active, then current time used as end.
+    *
+    * @return transfer time in milliseconds.
+    */
    public long getTransferTime() ;
+
+   /**
+    * Get time of last transfer.
+    *
+    * @return last access time in milliseconds.
+    */
    public long getLastTransferred() ;
 
+   /**
+    * Get file modification status.
+    *
+    * @return true if file was changes.
+    */
    public boolean wasChanged() ;
 }
