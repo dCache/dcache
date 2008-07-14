387f05dc4126c89a410fffe40fb3bd0a1382b560
Gerd Behrmann <behrmann@nordu.net>
2008-07-14 07:34:37 +0000

    Extend CellEndpoint with more methods from CellAdapter.
    As I began to use CellEndpoint for more and more components, I realised
    that a number of methods are missing and hard to replace with
    alternatives. This patch adds some of these methods:
    
    - getDomainContext() to get the domain context
    - getArgs() to get the cell command line
    
    The names are chosen to match those already used in CellAdapter.
    
    The patch also adds some JavaDoc to the interface.
    
    Signed-off-by: Tigran
    Closes: 3136

--- modules/cells/dmg/cells/nucleus/CellEndpoint.java
+++ modules/cells/dmg/cells/nucleus/CellEndpoint.java
@@ -1,31 +1,99 @@
 package dmg.cells.nucleus;
 
 import java.io.NotSerializableException;
+import java.util.Dictionary;
+import dmg.util.Args;
 
 /**
- * Interface for sending messages.
+ * Interface encapsulating a cell as seen by cell components.
+ *
+ * The main difference between the <code>CellEndpoint</code> and the
+ * <code>Cell</code> interfaces is the intended client:
+ *
+ * The <code>Cell</code> interface is an abstraction of a cell as seen
+ * by the cell nucleus. It provides methods used by the nucleus to
+ * deliver messages to the cell.
+ *
+ * The <code>CellEndpoint</code> interface is an abstraction of a cell
+ * as seen by other components of the cell. It provides methods for
+ * sending messages and obtaining information about the cell.
  */
 public interface CellEndpoint
 {
+   /**
+    * Sends <code>envelope</code>.
+    *
+    * @param envelope the cell message to be sent.
+    * @throws NotSerializableException if the payload object of this
+    *         message is not serializable.
+    * @throws NoRouteToCellException if the destination could not be
+    *         reached.
+    */
     void sendMessage(CellMessage envelope)
         throws NotSerializableException,
                NoRouteToCellException;
 
+   /**
+    * Sends <code>envelope</code>. The <code>callback</code> argument
+    * (which has to be non-null) allows to specify an object which is
+    * informed as soon as an has answer arrived or if the timeout has
+    * expired.
+    *
+    * @param envelope the cell message to be sent.
+    * @param callback specifies an object class which will be informed
+    *                 as soon as the message arrives.
+    * @param timeout  is the timeout in msec.
+    * @exception NotSerializableException if the payload object of this
+    *            message is not serializable.
+    */
     void sendMessage(CellMessage envelope,
-                            CellMessageAnswerable callback,
-                            long    timeout)
+                     CellMessageAnswerable callback,
+                     long timeout)
         throws NotSerializableException;
 
-    void sendMessage(CellMessage msg,
-                            boolean locally, boolean remotely)
-        throws NotSerializableException,
-               NoRouteToCellException;
-
-    CellMessage sendAndWait(CellMessage msg,
-                                   long millisecs)
+   /**
+    * Sends <code>envelope</code> and waits <code>timeout</code>
+    * milliseconds for an answer to arrive.  The answer will bypass
+    * the ordinary queuing mechanism and will be delivered before any
+    * other asynchronous message.  The answer need to have the
+    * getLastUOID set to the UOID of the message send with
+    * sendAndWait. If the answer does not arrive withing the specified
+    * time interval, the method returns <code>null</code> and the
+    * answer will be handled as if it was an ordinary asynchronous
+    * message.
+    *
+    * @param envelope the cell message to be sent.
+    * @param timeout milliseconds to wait for an answer.
+    * @return the answer or null if the timeout was reached.
+    * @throws NotSerializableException if the payload object of this
+    *         message is not serializable.
+    * @throws NoRouteToCellException if the destination
+    *         couldnot be reached.
+    */
+    CellMessage sendAndWait(CellMessage envelope, long timeout)
         throws NotSerializableException,
                NoRouteToCellException,
                InterruptedException;
 
+    /**
+     * Provides information about the host cell.
+     *
+     * Depending on the cell, a subclass of CellInfo with additional
+     * information may be returned instead.
+     *
+     * @return The cell information encapsulated in a CellInfo object.
+     */
     CellInfo getCellInfo();
+
+    /**
+     * Returns the domain context. The domain context is shared by all
+     * cells in a domain.
+     */
+    Dictionary getDomainContext();
+
+    /**
+     * Returns the cell command line arguments provided when the cell
+     * was created.
+     */
+    Args getArgs();
 }
