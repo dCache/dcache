5abafac40251570885da07b6876bad5e15817439
Gerd Behrmann <behrmann@nordu.net>
2008-07-25 13:59:20 +0000

    Remove NotSerializableException from method signature.
    NotSerializableException is no longer thrown and thus should not be 
    declared.

--- modules/cells/dmg/cells/nucleus/Reply.java
+++ modules/cells/dmg/cells/nucleus/Reply.java
@@ -1,10 +1,7 @@
 package dmg.cells.nucleus;
 
-import java.io.NotSerializableException;
-
 public interface Reply
 {
     void deliver(CellEndpoint endpoint, CellMessage envelope)
-        throws NotSerializableException,
-               NoRouteToCellException;
+        throws NoRouteToCellException;
 }
