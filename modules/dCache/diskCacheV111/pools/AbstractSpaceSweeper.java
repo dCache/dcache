eef8c2abeb41451c8321103bba910ed2886c95b9
Gerd Behrmann <behrmann@nordu.net>
2008-07-28 10:01:25 +0000

    Forgot to call 'svn add' on these two files....

---
+++ modules/dCache/diskCacheV111/pools/AbstractSpaceSweeper.java
@@ -0,0 +1,19 @@
+package diskCacheV111.pools;
+
+import java.io.PrintWriter;
+import org.dcache.cell.CellSetupProvider;
+import diskCacheV111.util.event.AbstractCacheRepositoryListener;
+
+/**
+ * Abstract base class for space sweepers providing empty
+ * implementations of <code>printSetup</code> and
+ * <code>afterSetupExecuted</code>.
+ */
+public abstract class AbstractSpaceSweeper
+    extends AbstractCacheRepositoryListener
+    implements SpaceSweeper,
+               CellSetupProvider
+{
+    public void printSetup(PrintWriter pw) {}
+    public void afterSetupExecuted() {}
+}
