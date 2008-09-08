e865d29ec18213fb7179c3d1aca4602de2181050
Gerd Behrmann <behrmann@nordu.net>
2008-09-08 11:12:05 +0000

    Copied AbstractCell and related classes from trunk (log4j related updates). Notice that I changed the package of AbstractCell and CellMessageDispatcher.
    svn copy svn+ssh://svn.dcache.org/data/svn/dCache/trunk/modules/dCache/org/dcache/services/Option.java modules/dCache/org/dcache/cells/
    svn copy svn+ssh://svn.dcache.org/data/svn/dCache/trunk/modules/dCache/org/dcache/services/AbstractCell.java modules/dCache/org/dcache/cells/
    svn copy svn+ssh://svn.dcache.org/data/svn/dCache/trunk/modules/dCache/org/dcache/util/CellMessageDispatcher.java modules/dCache/org/dcache/cells/
    svn copy svn+ssh://svn.dcache.org/data/svn/dCache/trunk/modules/dCache/org/dcache/services/ReflectionUtils.java modules/dCache/org/dcache/util/
    svn copy svn+ssh://svn.dcache.org/data/svn/dCache/trunk/modules/dCache/org/dcache/services/PinboardAppender.java modules/dCache/org/dcache/cells/

---
+++ modules/dCache/org/dcache/util/PinboardAppender.java
@@ -0,0 +1,42 @@
+package org.dcache.util;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+import java.lang.ref.WeakReference;
+
+import dmg.cells.nucleus.CellAdapter;
+
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.AppenderSkeleton;
+
+public class PinboardAppender extends AppenderSkeleton
+{
+    static private Map<String,WeakReference<CellAdapter>> _cells =
+        Collections.synchronizedMap(new HashMap());
+
+    public static void addCell(CellAdapter cell)
+    {
+        _cells.put(cell.getCellName(), new WeakReference(cell));
+    }
+
+    protected void append(LoggingEvent event)
+    {
+        WeakReference<CellAdapter> ref = _cells.get(event.getMDC("cell"));
+        if (ref != null) {
+            CellAdapter adapter = ref.get();
+            if (adapter != null) {
+                adapter.pin(layout.format(event));
+            }
+        }
+    }
+
+    public boolean requiresLayout()
+    {
+        return true;
+    }
+
+    public void close()
+    {
+    }
+}
