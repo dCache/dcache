d807a692ce20a4cd945d33634b21d7c060cca9e3
Dmitry Litvintsev <litvinse@fnal.gov>
2008-10-08 20:25:45 +0000

    make sure we set TRetentionPolicy appropriately, based on client input
    1) no input -> do not set at all
    
    2) al specified, rp not specified -> throw exception
    
    3) rp specified -> set TRetentionPolicy
    
    patch : 
    
    http://www.dcache.org/rt/Ticket/Display.html?id=3587

--- modules/srmclient/gov/fnal/srm/util/SRMGetClientV2.java
+++ modules/srmclient/gov/fnal/srm/util/SRMGetClientV2.java
@@ -1,4 +1,4 @@
-// $Id: SRMGetClientV2.java 10230 2008-09-24 19:04:23Z litvinse $
+// $Id: SRMGetClientV2.java 10300 2008-10-08 20:16:12Z litvinse $
 // $Log: not supported by cvs2svn $
 
 /*
@@ -152,15 +152,21 @@ public class SRMGetClientV2 extends SRMClient implements Runnable {
             SrmPrepareToGetRequest srmPrepareToGetRequest = new SrmPrepareToGetRequest();
             srmPrepareToGetRequest.setDesiredTotalRequestTime(
                     new Integer((int)configuration.getRequestLifetime()));
-	    TRetentionPolicy rp   =  TRetentionPolicy.CUSTODIAL;
-	    TAccessLatency   al   =  TAccessLatency.NEARLINE;
+	    TRetentionPolicy rp   =  null;
+	    TAccessLatency   al   =  null;
 	    if(configuration.getRetentionPolicy() != null ) {
 		    rp = TRetentionPolicy.fromString(configuration.getRetentionPolicy());
             }
             if(configuration.getAccessLatency() != null ) {
                 al = TAccessLatency.fromString(configuration.getAccessLatency());
             }
-            srmPrepareToGetRequest.setTargetFileRetentionPolicyInfo(new TRetentionPolicyInfo(rp,al));
+            if ( (al!=null) && (rp==null)) { 
+                    throw new IllegalArgumentException("if access latency is specified, "+
+                                                       "then retention policy have to be specified as well");
+            }
+            else if ( rp!=null ) {
+                    srmPrepareToGetRequest.setTargetFileRetentionPolicyInfo(new TRetentionPolicyInfo(rp,al));
+            }
             srmPrepareToGetRequest.setArrayOfFileRequests(
                     new ArrayOfTGetFileRequest(fileRequests));
 	    TAccessPattern  ap = TAccessPattern.TRANSFER_MODE;
