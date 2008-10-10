0097f31faf32db8fb48d6732e3caafec93d22b07
Gerd Behrmann <behrmann@nordu.net>
2008-10-10 11:23:31 +0000

    Copied SpaceSweeper2 and base class from trunk r10307 to org.dcache.pool.classic.
    Per agreement with Tigran and Patrick to reduce shared code between the 
    two pool implementations.

---
+++ modules/dCache/org/dcache/pool/classic/AbstractSpaceSweeper.java
@@ -0,0 +1,20 @@
+package org.dcache.pool.classic;
+
+import java.io.PrintWriter;
+import org.dcache.cell.CellSetupProvider;
+import diskCacheV111.util.event.AbstractCacheRepositoryListener;
+import diskCacheV111.pools.SpaceSweeper;
+
+/**
+ * Abstract base class for space sweepers providing empty
+ * implementations of <code>printSetup</code> and
+ * <code>afterSetupExecuted</code>.
+ */
+public abstract class AbstractSpaceSweeper
+    extends AbstractCacheRepositoryListener
+    implements SpaceSweeper,
+               CellSetupProvider
+{
+    public void printSetup(PrintWriter pw) {}
+    public void afterSetupExecuted() {}
+}
