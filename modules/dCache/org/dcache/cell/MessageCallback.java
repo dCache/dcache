9553fc5d355ce5c7adf88563b8f9a82cae744936
Gerd Behrmann <behrmann@nordu.net>
2008-10-13 09:53:09 +0000

    Added more missing files from r10320.

---
+++ modules/dCache/org/dcache/cell/MessageCallback.java
@@ -0,0 +1,15 @@
+package org.dcache.cell;
+
+import diskCacheV111.vehicles.Message;
+
+/**
+ * Callback interface for asynchroneous message delivery. Similar to
+ * CellMessageAnswerable, but with a more dCache specific design.
+ */
+public interface MessageCallback<T extends Message>
+{
+    void success(T message);
+    void failure(int rc, Object error);
+    void noroute();
+    void timeout();
+}
