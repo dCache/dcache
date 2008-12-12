7debc84e88abc5aaec99cccd17f797d45dd7a9f1
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2008-12-12 12:03:55 +0000

    added missing commit (r9924)

--- modules/external/Chimera/sql/pgsql-procedures.sql
+++ modules/external/Chimera/sql/pgsql-procedures.sql
@@ -1,4 +1,4 @@
--- $Id: pgsql-procedures.sql 782 2008-07-21 18:50:15Z tigran $
+-- $Id: pgsql-procedures.sql 796 2008-09-05 13:40:45Z tigran $
 -- some procedures to push some work to SQL server
 
 CREATE OR REPLACE FUNCTION "public"."inode2path" (varchar) RETURNS varchar AS $$
@@ -60,3 +60,26 @@ $t_inodes_trash$ LANGUAGE plpgsql;
 --
 
 CREATE TRIGGER tgr_locationinfo_trash BEFORE DELETE ON t_inodes FOR EACH ROW EXECUTE PROCEDURE f_locationinfo2trash();
+
+
+
+
+---
+--- populate inhereted tags
+---
+CREATE OR REPLACE FUNCTION f_populate_tags() RETURNS TRIGGER AS $t_populate_tags$
+BEGIN
+	IF TG_OP = 'INSERT' AND NEW.iname = '..'
+    THEN
+	    INSERT INTO t_tags ( SELECT NEW.iparent, itagname, itagid, 0 from t_tags WHERE ipnfsid=NEW.ipnfsid );
+    END IF;
+
+	RETURN NEW;
+END;
+
+$t_populate_tags$ LANGUAGE plpgsql;
+--
+-- trigger to store removed inodes
+--
+
+CREATE TRIGGER tgr_populate_tags AFTER INSERT ON t_dirs FOR EACH ROW EXECUTE PROCEDURE f_populate_tags();
