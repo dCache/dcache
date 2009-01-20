6b6ce6f0c6edd978dd9c5923ee00f8f8101e68d9
Gerd Behrmann <behrmann@nordu.net>
2009-01-20 11:13:15 +0000

    Forgot to add this file in r10921.

---
+++ modules/dCache/diskCacheV111/vehicles/PnfsGetParentMessage.java
@@ -0,0 +1,24 @@
+package diskCacheV111.vehicles;
+
+import diskCacheV111.util.PnfsId;
+
+public class PnfsGetParentMessage extends PnfsMessage 
+{
+    private PnfsId _parent;
+   
+    public PnfsGetParentMessage(PnfsId pnfsId) 
+    {
+        super(pnfsId);
+        setReplyRequired(true);
+    }
+
+    public void setParent(PnfsId parent)
+    {
+        _parent = parent;
+    }
+
+    public PnfsId getParent()
+    {
+        return _parent;
+    }
+}
