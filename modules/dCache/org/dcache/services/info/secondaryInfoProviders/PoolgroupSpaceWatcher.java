f7c0a088abcb1423c9af4945ba93749d1b507c6a
Paul Millar <paul.millar@desy.de>
2009-01-21 10:49:51 +0000

    Improve debugging: tidy up existing debug info and add some more.
    Since debug-level logging should be switched off by default and, under those
    circumstances, the additional load from this patch should be insignificant
    (some additional "if" statements) I thought it would be useful to include
    these debugging statements in dCache proper.
    
    Signed-off by: Gerd
    Patch: #4228

--- modules/dCache/org/dcache/services/info/secondaryInfoProviders/PoolgroupSpaceWatcher.java
+++ modules/dCache/org/dcache/services/info/secondaryInfoProviders/PoolgroupSpaceWatcher.java
@@ -43,18 +43,23 @@ public class PoolgroupSpaceWatcher extends AbstractStateWatcher {
 		if( _log.isInfoEnabled())
 			_log.info( "Watcher " + this.getClass().getSimpleName() + " triggered");
 		
+		_log.debug( "Gathering state:");		
+		_log.debug( "  building current poolgroup membership.");		
 		Map <String,Set<String>> currentPoolgroupMembership = SetMapVisitor.getDetails( POOLGROUPS_PATH, POOL_MEMBERSHIP_REL_PATH); 
+
+		_log.debug( "  building future poolgroup membership.");		
 		Map <String,Set<String>> futurePoolgroupMembership = SetMapVisitor.getDetails( transition, POOLGROUPS_PATH, POOL_MEMBERSHIP_REL_PATH);
 		
+		_log.debug( "  establishing current pool space mapping.");		
 		Map<String, SpaceInfo> poolSpaceInfoPre = PoolSpaceVisitor.getDetails();		
+
+		_log.debug( "  establishing future pool space mapping.");		
 		Map<String, SpaceInfo> poolSpaceInfoPost = PoolSpaceVisitor.getDetails(transition);
 		
-		if( _log.isDebugEnabled())
-			_log.debug( "Looking for changes in poolgroup membership.");		
+		_log.debug( "Looking for changes in poolgroup membership.");		
 		updateTodoBasedOnMembership( recalcPoolgroup, transition, currentPoolgroupMembership, futurePoolgroupMembership);
 
-		if( _log.isDebugEnabled())
-			_log.debug( "Looking for changes in pool space information.");
+		_log.debug( "Looking for changes in pool space information.");
 		updateTodoBasedOnPoolSpace( recalcPoolgroup, transition, futurePoolgroupMembership, poolSpaceInfoPre, poolSpaceInfoPost);
 
 		if( recalcPoolgroup.size() == 0) {
@@ -89,6 +94,7 @@ public class PoolgroupSpaceWatcher extends AbstractStateWatcher {
 	private void buildNewMetrics( StateUpdate update, StatePath path, Set<String> pools, Map<String, SpaceInfo> poolsSpaceInfo) {
 		SpaceInfo pgSpaceInfo = new SpaceInfo();
 
+		
 		// Create an Ephemeral StateComposite for our data.
 		update.appendUpdate( path, new StateComposite()); 
 		
@@ -101,6 +107,9 @@ public class PoolgroupSpaceWatcher extends AbstractStateWatcher {
 			}
 		
 		pgSpaceInfo.addMetrics(update, path, false);
+		
+		if( _log.isDebugEnabled())
+			_log.debug( "  new info: " + pgSpaceInfo.toString());
 	}
 
 
@@ -118,7 +127,8 @@ public class PoolgroupSpaceWatcher extends AbstractStateWatcher {
 		for( Map.Entry<String, Set<String>> pgEntry : futurePoolgroupMembership.entrySet()) {
 			String thisPoolgroup = pgEntry.getKey();
 			Set<String> thisPoolgroupFuturePoolset = pgEntry.getValue();
-			_log.debug( "examining poolgroup "+ thisPoolgroup);
+			if( _log.isDebugEnabled())
+				_log.debug( "  examining poolgroup: "+ thisPoolgroup);
 			
 			Set<String> thisPoolgroupCurrentPoolset = currentPoolgroupMembership.get(thisPoolgroup);
 			
@@ -126,8 +136,38 @@ public class PoolgroupSpaceWatcher extends AbstractStateWatcher {
 			if( (thisPoolgroupCurrentPoolset == null) || !thisPoolgroupCurrentPoolset.equals( thisPoolgroupFuturePoolset)) {
 				recalcPoolgroup.add( thisPoolgroup);
 
-				if( _log.isDebugEnabled())
-					_log.debug( "poolgroup "+ thisPoolgroup + " is new or has altered membership");
+				if( _log.isDebugEnabled()) {
+					_log.debug( "    poolgroup "+ thisPoolgroup + " is new or has altered membership");
+
+					StringBuffer wasSb = new StringBuffer();
+					if( thisPoolgroupCurrentPoolset != null) {
+						for( String pool : thisPoolgroupCurrentPoolset) {
+							if( wasSb.length() > 0)
+								wasSb.append( ", ");
+							wasSb.append( pool);
+						}
+						if( wasSb.length() == 0)
+							wasSb.append( "<empty>");
+					} else {
+						wasSb.append( "<unknown>");						
+					}
+
+					_log.debug( "      was: " + wasSb.toString());
+					
+					StringBuffer nowSb = new StringBuffer();
+					if( thisPoolgroupFuturePoolset != null) {
+						for( String pool : thisPoolgroupFuturePoolset) {
+							if( nowSb.length() > 0)
+								nowSb.append( ", ");
+							nowSb.append( pool);
+						}
+						if( nowSb.length() == 0)
+							nowSb.append( "<empty>");
+					} else {
+						nowSb.append( "<unknown>");						
+					}
+					_log.debug( "      now: " + nowSb.toString());
+				}
 			}
 		}		
 	}
@@ -154,13 +194,13 @@ public class PoolgroupSpaceWatcher extends AbstractStateWatcher {
 			SpaceInfo thisPoolPostInfo = futurePoolSpaceInfo.get( thisPool);
 
 			if( _log.isDebugEnabled())
-				_log.debug( "examining pool "+ thisPool);
+				_log.debug( "  examining pool: "+ thisPool);
 
 			// Pool has disappeared or size has changed
 			if( thisPoolPostInfo == null || !thisPoolPostInfo.equals(thisPoolPreInfo)) {
 				changedPools.add( thisPool);
 				if( _log.isDebugEnabled())
-					_log.debug( "Pool " + thisPool + " has changed or disappeared");
+					_log.debug( "    pool " + thisPool + " has changed or disappeared");
 			}
 		}
 		
@@ -170,13 +210,13 @@ public class PoolgroupSpaceWatcher extends AbstractStateWatcher {
 				changedPools.add( thisPool);
 				
 				if( _log.isDebugEnabled())
-					_log.debug( "Pool " + thisPool + " has appeared");
+					_log.debug( "    pool " + thisPool + " has appeared");
 			}
 		
 		// 2.  Nothing doing?, do nothing more.
 		if( changedPools.size() == 0) {
 			if( _log.isDebugEnabled())
-				_log.debug( "No pools have changed");
+				_log.debug( "  no pools have changed");
 			return;
 		}
 		
@@ -209,7 +249,7 @@ public class PoolgroupSpaceWatcher extends AbstractStateWatcher {
 				recalcPoolgroup.add( poolgroup);
 				if( changedPools.size() == 0) {
 					if( _log.isDebugEnabled())
-						_log.debug( "Poolgroup " + poolgroup + " is marked as to be recalculated.");
+						_log.debug( "  poolgroup " + poolgroup + " is marked as to be recalculated.");
 					
 					return;
 				}
