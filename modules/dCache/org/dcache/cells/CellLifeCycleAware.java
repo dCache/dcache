c01be4008aaa4157725b697c265e441695768570
Gerd Behrmann <behrmann@nordu.net>
2009-02-18 11:30:16 +0000

    Forgot to add this file in r11203.

---
+++ modules/dCache/org/dcache/cells/CellLifeCycleAware.java
@@ -0,0 +1,18 @@
+package org.dcache.cells;
+
+/**
+ * Classes implementing this method receive Cell life cycle
+ * notifications.
+ */
+public interface CellLifeCycleAware
+{
+    /**
+     * Called just after the cell has been started. 
+     */
+    void afterStart();
+
+    /**
+     * Called just before the cell is killed.
+     */
+    void beforeStop();
+}
