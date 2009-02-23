9711b527e3f97ceb3b7d73f0092fcb23d00bbe75
Paul Millar <paul.millar@desy.de>
2009-02-23 15:53:41 +0000

    Add a delay when starting up a Chimera NFS server
    One problem with Chimera is that, because the JVM is run in the background, it may take an 
    unpredictable length of time to be scheduled to run by the underlying OS.  Because of this, there 
    may be a delay before the NFS server is actually available.  This leads to a race-condition when 
    starting a machine that may result in a mount being attempted before Chimera is ready.
    
    The solution will only work if Chimera is not registered with portmap before running.  This is true 
    when a machine is powered up, so should fix the start-up race condition.
    
    When stopping then starting Chimera, the portmap entry is not cleared.  This is a bug in the 
    Chimera NFS server: it should attempt to deregister itself when it goes down (at least, whenever 
    possible).
    
    (there's a free spelling fix, too)
    
    http://rb.dcache.org/r/67
    
    Acked-by: Tigran

--- modules/external/Chimera/chimera-nfs-run.sh
+++ modules/external/Chimera/chimera-nfs-run.sh
@@ -16,6 +16,29 @@ pfile=/var/run/chimera-nfsv3.pid
 . ${ourHomeDir}/config/dCacheSetup
 
 
+waitForChimera()
+{
+    echo -n "Waiting for NFS server to register itself: "
+    for try in 1 2 3 4 5 6 7 too-much; do 
+        rpcinfo -p localhost|grep 100003 >/dev/null
+        if [ $? == 0 ]; then
+            echo
+            break
+        else
+            echo -n "."
+        fi
+	
+        if [ "x$try"  = "xtoo-much" ]; then
+            echo
+            echo "Chimera is taking too long; giving up."
+            break
+        else
+            sleep 1
+        fi
+    done
+}
+
+
 dCacheChimeraStart()
 {
   if [ -f ${pfile} ]
@@ -34,6 +57,8 @@ dCacheChimeraStart()
     -Xmx512M org.dcache.chimera.nfs.v3.Main2 \
   ${ourHomeDir}/config/chimera-config.xml > ${log} 2>&1 &
   echo $! > ${pfile}
+
+  waitForChimera
 }
 
 
@@ -45,7 +70,7 @@ dCacheChimeraStop()
     kill -0 ${pid} > /dev/null 2>&1
     if [ $? -eq 0 ]
     then
-      echo "Shuting down Chimera-NFSv3 interface"
+      echo "Shutting down Chimera-NFSv3 interface"
       kill `cat ${pfile}`
       rm -f ${pfile}
     else
