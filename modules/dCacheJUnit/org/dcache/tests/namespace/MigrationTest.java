23950f7b6689bcd54a0bd87f2f7df3b4775ae13d
Paul Millar <paul.millar@desy.de>
2009-03-02 15:34:03 +0000

    Add comparator and alter unit tests to match; acked-by: Tigran

--- modules/dCacheJUnit/org/dcache/tests/namespace/MigrationTest.java
+++ modules/dCacheJUnit/org/dcache/tests/namespace/MigrationTest.java
@@ -1,9 +1,7 @@
 package org.dcache.tests.namespace;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import org.dcache.chimera.migration.Comparator;
+import org.dcache.chimera.migration.Comparator.MismatchException;
 import org.junit.Test;
 
 import diskCacheV111.util.FileMetaData;
@@ -13,35 +11,36 @@ import diskCacheV111.vehicles.StorageInfo;
 
 public class MigrationTest {
 
+    static final String DUMMY_ID_STRING = "dummy-ID";
 
     @Test
-    public void testEqualMetadataPass() {
+    public void testEqualMetadataPass() throws MismatchException {
         FileMetaData fileMetaData = new FileMetaData(3750, 1000, 0644);
         FileMetaData otherMetaData = new FileMetaData(3750, 1000, 0644);
-        assertTrue("equal metadata did not pass", Comparator.compare(fileMetaData, otherMetaData));
+        Comparator.assertEquals( DUMMY_ID_STRING, fileMetaData, otherMetaData);
     }
 
-    @Test
-    public void testEqualMetadataFail() {
+    @Test(expected=org.dcache.chimera.migration.Comparator.MismatchException.class)
+    public void testEqualMetadataFail() throws MismatchException {
         FileMetaData fileMetaData = new FileMetaData(3750, 1000, 0644);
         FileMetaData differMetaData = new FileMetaData(3751, 1001, 0666);
-        assertFalse("not equal metadata pass", Comparator.compare(fileMetaData, differMetaData));
+        Comparator.assertEquals( DUMMY_ID_STRING, fileMetaData, differMetaData);
     }
 
     @Test
-    public void testEqualsStorageInfoPass() {
+    public void testEqualsStorageInfoPass() throws MismatchException {
         StorageInfo storageInfo = new GenericStorageInfo("osm", "h1:raw");
         StorageInfo otherInfo = new GenericStorageInfo("osm", "h1:raw");
 
-        assertTrue("equal storageInfo did not pass", Comparator.compare(storageInfo, otherInfo));
+        Comparator.assertEquals( DUMMY_ID_STRING, storageInfo, otherInfo);
     }
 
-    @Test
-    public void testEqualsStorageInfoFail() {
+    @Test(expected=org.dcache.chimera.migration.Comparator.MismatchException.class)
+    public void testEqualsStorageInfoFail() throws MismatchException {
         StorageInfo storageInfo = new GenericStorageInfo("osm", "h1:raw");
         StorageInfo differInfo = new GenericStorageInfo("osm", "h1:rawd");
 
-        assertFalse("not equal storageInfo pass", Comparator.compare(storageInfo, differInfo));
+        Comparator.assertEquals( DUMMY_ID_STRING, storageInfo, differInfo);
     }
 
 }
