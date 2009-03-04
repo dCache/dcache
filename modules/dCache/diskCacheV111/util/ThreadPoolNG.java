9e9047cc640ca1eae203eebb11b57f0956f63f56
Gerd Behrmann <behrmann@nordu.net>
2009-03-04 17:43:03 +0000

    Propagate CDC in thread pool implementations.
    This patch adds CDC (diagnostic context) to ThreadManager, ThreadPoolNG
    and ThreadCounter. With this patch SpaceManager now propagates the CDC.
    
    Since the current SpaceManager propagates the wrong CDC (inherited at
    thread creation time) I suggest to apply this branch to both 1.9.1,
    1.9.2 and trunk.
    
    Target: 1.9.1
    Target: 1.9.2
    Target: trunk
    Signed-off-by: Gerd Behrmann <behrmann@ndgf.org>
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: http://rb.dcache.org/r/119/

--- modules/dCache/diskCacheV111/util/ThreadPoolNG.java
+++ modules/dCache/diskCacheV111/util/ThreadPoolNG.java
@@ -5,24 +5,26 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import dmg.cells.nucleus.CellAdapter;
+import dmg.cells.nucleus.CDC;
+
 /**
- * 
+ *
  * ThreadPoolNG ( Thread Pool New Generation is a
  * java concurrent based implementation of dCache
- * Thread pool. While it's nothing else than wrapper 
+ * Thread pool. While it's nothing else than wrapper
  * around ThreadPoolExecutor, it's better to replace all
  * instances of ThreadPool with pure  ThreadPoolExecutor.
- * 
+ *
  * @since 1.8
  */
 public class ThreadPoolNG implements ThreadPool {
 
 	private final CellAdapter _cell;
 	private final ThreadPoolExecutor _executor;
-	
+
 	public ThreadPoolNG(CellAdapter cell) {
 		_cell = cell;
-		
+
 		// we can get all options from batch file
 		_executor = new ThreadPoolExecutor(
 					0 , // core size
@@ -34,8 +36,8 @@ public class ThreadPoolNG implements ThreadPool {
 					// + rejection policy
 				);
 	}
-	
-	
+
+
 	public int getCurrentThreadCount() {
 		return _executor.getActiveCount();
 	}
@@ -48,16 +50,29 @@ public class ThreadPoolNG implements ThreadPool {
 		return 0;
 	}
 
-	public void invokeLater(Runnable runner, String name)
-			throws IllegalArgumentException {
-		_executor.execute(runner);
-	}
+    public void invokeLater(final Runnable runner, String name)
+    {
+        final CDC cdc = new CDC();
+        Runnable wrapper = new Runnable() {
+                public void run()
+                {
+                    cdc.apply();
+                    try {
+                        runner.run();
+                    } finally {
+                        CDC.clear();
+                    }
+                }
+            };
+
+        _executor.execute(wrapper);
+    }
 
 	public void setMaxThreadCount(int maxThreadCount)
-			throws IllegalArgumentException {		
+			throws IllegalArgumentException {
 		_executor.setMaximumPoolSize(maxThreadCount);
 	}
-	
+
 	public String toString() {
 		return "ThreadPoolNG $Revision: 1.4 $ max/active: " + getMaxThreadCount() + "/" + getCurrentThreadCount();
 	}
