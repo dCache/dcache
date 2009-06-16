4b5e79ef90918be6258676012ec2d7d3afa445d3
Gerd Behrmann <behrmann@nordu.net>
2009-06-16 07:22:52 +0000

    xrootd: Collapse double slashes
    rootd seems to have a concept of a home directory per user. A root URI
    is relative to that home directory. Hence
    http://root.cern.ch/root/NetFile.html describes a scheme in which an
    extra slash is inserted in order to specify a path from the root of the
    rootd servers name space. It seems xrootd copies this approach.
    
    The xrootd implementation in dCache has no concept of a per user home
    directory, and hence treated the double slashes literally. This has
    recently caused trouble for Alice.
    
    This patch adds a normalisation step to requests containing a path. This
    normalisation step eliminates double slashes.
    
    Target: 1.9.1
    Target: 1.9.2
    Target: trunk
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: http://rb.dcache.org/r/304/

--- modules/dCache/org/dcache/xrootd/protocol/messages/StatxRequest.java
+++ modules/dCache/org/dcache/xrootd/protocol/messages/StatxRequest.java
@@ -1,20 +1,26 @@
 package org.dcache.xrootd.protocol.messages;
 
+import diskCacheV111.util.FsPath;
+
 import org.dcache.xrootd.protocol.XrootdProtocol;
 
 public class StatxRequest extends AbstractRequestMessage {
 
     public StatxRequest(int[] h, byte[] d) {
         super(h, d);
-        
+
         if (getRequestID() != XrootdProtocol.kXR_statx)
             throw new IllegalArgumentException("doesn't seem to be a kXR_statx message");
     }
-    
+
     public String[] getPaths() {
-        
+
         readFromHeader(false);
-        
-        return new String(data).split("\n");
+
+        String[] paths = new String(data).split("\n");
+        for (int i = 0; i < paths.length; i++) {
+            paths[i] =  new FsPath(paths[i]).toString();
+        }
+        return paths;
     }
 }
