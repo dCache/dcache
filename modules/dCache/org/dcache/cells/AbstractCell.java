18762c4a004f5b5259a8dba454115ae74bccac22
Gerd Behrmann <behrmann@nordu.net>
2009-07-19 06:17:11 +0000

    core: Minor optimisation in AbstractCell
    Identified by findbugs.
    
    Target: trunk
    Patch: http://rb.dcache.org/r/416/
    Acked-by: Paul Millar <paul.millar@desy.de>

--- modules/dCache/org/dcache/cells/AbstractCell.java
+++ modules/dCache/org/dcache/cells/AbstractCell.java
@@ -532,7 +532,7 @@ public class AbstractCell extends CellAdapter
                     result = type.cast(i);
                 }
             } else if (type == Character.class || type == Character.TYPE) {
-                Character i = new Character(so.charAt(0));
+                Character i = Character.valueOf(so.charAt(0));
                 if (type == Character.TYPE) {
                     result = ((Class<T>) Character.class).cast(i); //avoid ClassCastException through autoboxing
                 } else {
