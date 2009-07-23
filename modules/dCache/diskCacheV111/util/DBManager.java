149c2cb9a0a55f69cd6c53e2e2edf6a3b346e38c
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2009-07-23 07:50:19 +0000

    spacemanager: robust connection handling
    in addition with: use log4j in space manager,
    remove use of Statatement in favor of PreparedStatement
    
    Signed-off-by: Dmitry Litvintsev
    Acked-by: Gerd
    Patch: http://rb.dcache.org/r/417
    Target: trunk, 1.9.4

--- modules/dCache/diskCacheV111/util/DBManager.java
+++ modules/dCache/diskCacheV111/util/DBManager.java
@@ -1,9 +1,9 @@
 //______________________________________________________________________________
 //
-// $Id$ 
+// $Id$
 // $Author$
 //
-// Infrastructure to retrieve objects from DB 
+// Infrastructure to retrieve objects from DB
 //
 // created 11/07 by Dmitry Litvintsev (litvinse@fnal.gov)
 //
@@ -21,13 +21,13 @@ import org.dcache.util.JdbcConnectionPool;
 public class DBManager {
 	private static DBManager _instance=null;
 	private JdbcConnectionPool connectionPool=null;
-	private DBManager() { 
+	private DBManager() {
 	}
 	private static Logger _logger =
 		Logger.getLogger("logger.org.dcache.db.sql");
 
 	synchronized public void initConnectionPool(String url,
-				       String driver, 
+				       String driver,
 				       String user,
 				       String password ) throws SQLException {
 		
@@ -37,31 +37,32 @@ public class DBManager {
 							  (password!=null?password:"srm"));
 		
 	}
-	synchronized public static final DBManager getInstance()  { 
-		if ( DBManager._instance==null) { 
+	synchronized public static final DBManager getInstance()  {
+		if ( DBManager._instance==null) {
 			DBManager._instance = new DBManager();	
 		}
 		return DBManager._instance;
 	}
 
-	public JdbcConnectionPool getConnectionPool() { 
+	public JdbcConnectionPool getConnectionPool() {
 		return connectionPool;
 	}
 	
 	
-	public HashSet select(IoPackage pkg, 
-			  String query) throws SQLException { 
+	public HashSet select(IoPackage pkg,
+			  String query) throws SQLException {
 		//
 		// Handling of connection is localized here
 		//
 		Connection connection = null;
 		HashSet set=null;
-		try { 
+		try {
 			connection = connectionPool.getConnection();
 			set =  pkg.select(connection,query);
+                        return set;
 		}
-		catch (SQLException e) { 
-			if (connection!=null){ 
+		catch (SQLException e) {
+			if (connection!=null){
 				connectionPool.returnFailedConnection(connection);
 				connection = null;
 			}
@@ -71,175 +72,237 @@ public class DBManager {
 			if(connection != null) {
 				connectionPool.returnConnection(connection);
 			}
-			
 		}
-		return set;
 	}
-
+        
 	public Object selectForUpdate(Connection connection,
 				      IoPackage pkg,
-				       String query,
-				       Object ... args) throws SQLException { 
-		PreparedStatement stmt = connection.prepareStatement(query);
-		for (int i = 0; i < args.length; i++) { 
-			stmt.setObject(i + 1, args[i]);
-		}
-		Object o =  pkg.selectForUpdate(connection,stmt);
-		return o;
-	}
-
-	public HashSet selectPrepared(IoPackage pkg, 
+                                      String query,
+                                      Object ... args) throws SQLException {
+		PreparedStatement stmt = null;
+                try {
+                        stmt = connection.prepareStatement(query);
+                        for (int i = 0; i < args.length; i++) {
+                                stmt.setObject(i + 1, args[i]);
+                        }
+                        Object o =  pkg.selectForUpdate(connection,stmt);
+                        return o;
+                }
+                finally {
+                        if (stmt != null) {
+                                try {
+                                        stmt.close();
+                                        stmt=null;
+                                }
+                                catch (SQLException e1) { } 
+                        }
+                }
+        }
+                        
+	public HashSet selectPrepared(IoPackage pkg,
 				      String query,
-				      Object ... args) throws SQLException { 
+				      Object ... args) throws SQLException {
 		Connection connection = null;
 		HashSet set=null;
-		try { 
+                PreparedStatement stmt=null;
+		try {
 			connection = connectionPool.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(query);
-			for (int i = 0; i < args.length; i++) { 
+			stmt = connection.prepareStatement(query);
+			for (int i = 0; i < args.length; i++) {
 				stmt.setObject(i + 1, args[i]);
 			}
 			set =  pkg.selectPrepared(connection,stmt);
-		}
-		catch (SQLException e) { 
-			if (connection!=null) { 
-				connectionPool.returnFailedConnection(connection);
-				connection = null;
-			}
-			throw e;
+                        stmt.close();
+                        connectionPool.returnConnection(connection);
+                        connection=null;
+                        stmt=null;
+                        return set;
 		}
 		finally {
 			if(connection != null) {
-				connectionPool.returnConnection(connection);
+                                if (stmt != null) {
+                                        try {
+                                                stmt.close();
+                                                stmt=null;
+                                        }
+                                        catch (SQLException e1) { } 
+                                }
+				connectionPool.returnFailedConnection(connection);
+                                connection=null;
 			}
-			
 		}
-		return set;
 	}
 
-
+        
 //------------------------------------------------------------------------------
-
+        
 	public Object selectPrepared(int columnIndex,
-				     String query, 
-				     Object ... args) throws SQLException { 
+				     String query,
+				     Object ... args) throws SQLException {
 		Connection connection = null;
 		Object obj=null;
-		try { 
+                PreparedStatement stmt=null;
+                ResultSet set = null;
+		try {
 			connection = connectionPool.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(query);
-			for (int i = 0; i < args.length; i++) { 
+			stmt = connection.prepareStatement(query);
+			for (int i = 0; i < args.length; i++) {
 				stmt.setObject(i + 1, args[i]);
 			}
-			ResultSet set = stmt.executeQuery();
-			if (set.next()) { 
+			set = stmt.executeQuery();
+			if (set.next()) {
 				obj=set.getObject(columnIndex);
 			}
-		}
-		catch (SQLException e) { 
-			if (connection!=null) { 
-				connectionPool.returnFailedConnection(connection);
-				connection = null;
-			}
-			throw e;
+                        //
+                        // No need to close set - it is closed if stmt is closed
+                        //
+                        stmt.close();
+                        connectionPool.returnConnection(connection);
+                        connection=null;
+                        stmt=null;
+                        return obj;
 		}
 		finally {
 			if(connection != null) {
-				connectionPool.returnConnection(connection);
+                                if (stmt != null) {
+                                        try {
+                                                stmt.close();
+                                                stmt=null;
+                                        }
+                                        catch (SQLException e1) { } 
+                                }
+				connectionPool.returnFailedConnection(connection);
+                                connection=null;
 			}
-			
 		}
-		return obj;
 	}
-
 
 	public void createTable( String name,
 				 String ... statements)
 		throws SQLException {		
-			Connection connection = null; 
-			try { 
+			Connection connection = null;
+                        ResultSet set = null;
+			try {
 				connection = connectionPool.getConnection();
 				DatabaseMetaData md = connection.getMetaData();
-				ResultSet set = md.getTables(null, null, name, null);
+				set = md.getTables(null, null, name, null);
 				if (!set.next()) {
-					try {
-						for (String statement : statements) {
-							Statement s = connection.createStatement();
-							if (_logger.isDebugEnabled()) {
-								_logger.debug("Executing  "+statement);
-							}
-							int result = s.executeUpdate(statement);
-							connection.commit();
-							s.close();
-						}
-					} 
-					catch (SQLException e) {
-						if (_logger.isDebugEnabled()) {
-							_logger.debug("SQL Exception (relation could already exist): "+e.getMessage());
-						}
-						throw e;
-					}
-				}
-				else { 
+                                        for (String statement : statements) {
+                                                Statement s=null;
+                                                try {
+                                                        s = connection.createStatement();
+                                                        if (_logger.isDebugEnabled()) {
+                                                                _logger.debug("Executing  "+statement);
+                                                        }
+                                                        int result = s.executeUpdate(statement);
+                                                        connection.commit();
+                                                        s.close();
+                                                        s=null;
+                                                }
+                                                catch (SQLException e) {
+                                                        try {
+                                                                connection.rollback();
+                                                                if (s!=null) {
+                                                                        s.close();
+                                                                        s=null;
+                                                                }
+                                                        }
+                                                        catch (SQLException e1) { } 
+                                                        if (_logger.isDebugEnabled()) {
+                                                                _logger.debug("Failed: "+e.getMessage());
+                                                        }
+                                                }
+                                        }
+                                }
+                                else {
 					throw new SQLException("Table \""+name+"\" already exist");
 				}
 			}
-			catch (SQLException e) {
-				if (connection!=null) { 
-					connectionPool.returnFailedConnection(connection);
-					connection = null;
-				}
-				throw e;
-			}
 			finally {
-				if(connection != null) {
-					connectionPool.returnConnection(connection);
-				}
-				connection=null;
-			}
-	}
+				if(connection!= null) {
+                                        if (set!=null) {
+                                                try {
+                                                        set.close();
+                                                        set=null;
+                                                }
+                                                catch (SQLException e1) { } 
+                                        }
+                                        connectionPool.returnConnection(connection);
+                                        connection=null;
+                                }
+                        }
+        }
 
 	public void createIndexes( String name,
-				   String ... columns) 
-		throws SQLException {		
-		Connection connection = null; 
-		try { 
-				connection = connectionPool.getConnection();
+				   String ... columns)
+		throws SQLException {
+		Connection connection = null;
+                ResultSet set = null;
+		try {
+				connection          = connectionPool.getConnection();
 				DatabaseMetaData md = connection.getMetaData();
-				ResultSet set       = md.getIndexInfo(null, 
-								      null, 
+				set                 = md.getIndexInfo(null,
+								      null,
 								      name,
-								      false, 
+								      false,
 								      false);
 				HashSet<String> listOfColumnsToBeIndexed = new HashSet<String>();
 				for (String column : columns) {
 					listOfColumnsToBeIndexed.add(column.toLowerCase());
 				}
-				while(set.next()) { 
+				while(set.next()) {
 					String s = set.getString("column_name").toLowerCase();
-					if (listOfColumnsToBeIndexed.contains(s)) { 
+					if (listOfColumnsToBeIndexed.contains(s)) {
 						listOfColumnsToBeIndexed.remove(s);
 					}
 				}
-				for (Iterator<String> i=listOfColumnsToBeIndexed.iterator();i.hasNext();) { 
+				for (Iterator<String> i=listOfColumnsToBeIndexed.iterator();i.hasNext();) {
 					String column = i.next();
 					String indexName=name.toLowerCase()+"_"+column+"_idx";
 					String createIndexStatementText = "CREATE INDEX "+indexName+" ON "+name+" ("+column+")";
-					Statement s = connection.createStatement();
-					int result = s.executeUpdate(createIndexStatementText);
-					connection.commit();
-					s.close();
-				}
-		}
+                                        Statement s=null;
+                                        try {
+                                                s = connection.createStatement();
+                                                int result = s.executeUpdate(createIndexStatementText);
+                                                connection.commit();
+                                                s.close();
+                                                s=null;
+                                        }
+                                        catch (SQLException e) {
+                                                try {
+                                                        connection.rollback();
+                                                        if (s!=null) {
+                                                                s.close();
+                                                                s=null;
+                                                        }
+                                                }
+                                                catch (SQLException e1) { } 
+                                        }
+                                }
+                }
 		catch (SQLException e) {
-			if (connection!=null) { 
+			if (connection!=null) {
+                                if (set!=null) {
+                                        try {
+                                                set.close();
+                                                set=null;
+                                        }
+                                        catch (SQLException e1) { } 
+                                }
 				connectionPool.returnFailedConnection(connection);
 				connection = null;
-			}
+                        }
 			throw e;
 		}
 		finally {
 			if(connection != null) {
+                                if (set!=null) {
+                                        try {
+                                                set.close();
+                                                set=null;
+                                        }
+                                        catch (SQLException e1) { } 
+                                }
 				connectionPool.returnConnection(connection);
 				connection = null;
 			}
@@ -247,61 +310,62 @@ public class DBManager {
 	}
 
 	public int update(String query,
-			  Object ... args) throws SQLException { 
+			  Object ... args) throws SQLException {
 		Connection connection = null;
 		PreparedStatement stmt = null;
-		try { 
+		try {
 			connection = connectionPool.getConnection();
 			stmt = connection.prepareStatement(query);
 			for (int i = 0; i < args.length; i++)
 				stmt.setObject(i + 1, args[i]);
 			int result = stmt.executeUpdate();
+			connection.commit();
 			stmt.close();
-			connection.commit();	
 			connectionPool.returnConnection(connection);
 			connection = null;
+                        stmt=null;
 			return result;
 		}
-		catch (SQLException e) { 
-			if (connection!=null) { 
-				connection.rollback();
-				connectionPool.returnFailedConnection(connection);
-				connection = null;
-			}
-			throw e;
-		}
 		finally {
-			if(connection != null) {
+			if (connection!=null) {
+                                try {
+                                        connection.rollback();
+                                        if (stmt != null) {
+                                                stmt.close();
+                                                stmt=null;
+                                        }
+                                }
+                                catch (SQLException e1) { } 
 				connectionPool.returnConnection(connection);
+                                connection=null;
 			}
-			
 		}
 	}
 	
-	public int delete(String query,Object ... args)  throws SQLException { 
+	public int delete(String query,Object ... args)  throws SQLException {
 		return update(query,args);
 	}
 	
-	public int insert(String query, Object ... args)  throws SQLException { 
+	public int insert(String query, Object ... args)  throws SQLException {
 		return update(query,args);
 	}
 
-	public void batchUpdates(String ... statements) throws SQLException { 
+	public void batchUpdates(String ... statements) throws SQLException {
 		for (String statement : statements) {
 			try {
 				update(statement);
 			}
-			catch (SQLException sql) { 
+			catch (SQLException sql) {
 				throw new SQLException("Statement "+statement+ " failed");
 			}
 		}
 	}
 
-	public void batchDeletes(String ... statements) throws SQLException { 
+	public void batchDeletes(String ... statements) throws SQLException {
 		 batchUpdates(statements);
 	}
 
-	public void batchInserts(String ... statements) throws SQLException { 
+	public void batchInserts(String ... statements) throws SQLException {
 		batchUpdates(statements);
 	}
 
@@ -309,26 +373,36 @@ public class DBManager {
 //    below the connection is exposed
 //------------------------------------------------------------------------------
 
-	public int update(Connection connection, 
-			   String query, 
-			   Object ... args)  throws SQLException { 
-		PreparedStatement stmt =  connection.prepareStatement(query);
-		for (int i = 0; i < args.length; i++)
-			stmt.setObject(i + 1, args[i]);
-		int result = stmt.executeUpdate();
-		stmt.close();
-		return result;
-	}
+	public int update(Connection connection,
+			   String query,
+			   Object ... args)  throws SQLException {
+                PreparedStatement stmt=null;
+                try {
+                        stmt =  connection.prepareStatement(query);
+                        for (int i = 0; i < args.length; i++)
+                                stmt.setObject(i + 1, args[i]);
+                        return stmt.executeUpdate();
+                }
+                finally {
+                        if (stmt != null) {
+                                try {
+                                        stmt.close();
+                                        stmt=null;
+                                }
+                                catch (SQLException e1) { } 
+                        }
+                }
+        }
 
-	public int delete(Connection connection, 
-			  String query, 
-			   Object ... args)  throws SQLException { 
+        public int delete(Connection connection,
+			  String query,
+                          Object ... args)  throws SQLException {
 		return update(connection, query, args);
 	}
 
-	public int insert(Connection connection, 
-			  String query, 
-			  Object ... args)  throws SQLException { 
+	public int insert(Connection connection,
+			  String query,
+			  Object ... args)  throws SQLException {
 		return update(connection, query, args);
 	}
 }
