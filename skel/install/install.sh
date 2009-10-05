0a091d2a6ad95fc76ca62036dc15bdd07f6772c8
Owen Synge <owen.synge@desy.de>
2009-10-05 16:26:08 +0000

    Fixing typos in install.sh which broke mounting correctly
    Their is a nasty typo in "skel/install/install.sh" much all versions going back to 1.9.1 (I don't fix older versions) 
    
    When this is fixed, it exposes a second more obvious "typo" bug which was masked by the first.
    
    This patch should be applied at to all the above branches, applies cleanly to head and trunk, but less cleanly to older branches where it should only be applied where it has an effect.
    
    The second fix, is to skel/share/lib/namespace.sh this is very much dependent on how far Gerd has go in removing the need to mount PNFS. If the "dir" domain is needed to mount pnfs/chimera still then it shoudl be applied, which branches this should be applied to is dependent on Gerds knowledge.
    
    Signed-off by: Paul
    http://rb.dcache.org/r/808/

--- skel/install/install.sh
+++ skel/install/install.sh
@@ -247,6 +247,8 @@ dcacheInstallGetNameSpaceType()
     logmessage INFO "NAMESPACE=${NODE_CONFIG_NAMESPACE}"
     nameServerFormat="pnfs"
     logmessage WARNING "Defaulting node_config to pnfs. This behaviour will change in future dCache releases."
+  else
+    nameServerFormat=$NODE_CONFIG_NAMESPACE
   fi
   RET=$nameServerFormat
 }
@@ -569,7 +571,7 @@ dcacheInstallChimeraMountPoints()
   if contains chimeraDomain $DOMAINS; then
       /etc/init.d/portmap restart    
       dcacheInstallChimeraMountPointServer
-  elif isNameSpaceMountNeedeed $DOMAINS; then
+  elif isNameSpaceMountNeeded $DOMAINS; then
       dcacheInstallChimeraMountPointServer
   fi  
   
