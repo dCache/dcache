0039c6886be72e3027240ec082dad50231424773
Gerd Behrmann <behrmann@nordu.net>
2009-10-29 11:39:06 +0000

    UniversalSpringCell: Fix argument parsing in the presence of -${<number>}
    Due to how the pool batch file is constructed, cell arguments may
    contain unexpanded arguments like -${7}. UniversalSpringCell contains
    code to strip those, however the regular expression used had a bug: It
    used a greedy match for the argument name. Hence an argument string like
    -${7} ..... -${8} would be stripped not only of the unexpanded
    arguments, but also of all arguments in between.
    
    This patch fixes the regular expression and limits the scope to only
    replacing unexpanded arguments with a numeric name.
    
    One side effect of the bug is that the tag.hostname= parameter was not
    always parsed by the pool and thus broke the suppression of replication
    to the same host.
    
    Target: 1.9.1
    Target: 1.9.2
    Target: 1.9.3
    Target: 1.9.4
    Target: 1.9.5
    Target: trunk
    Ticket: #5198
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: http://rb.dcache.org/r/981/

--- modules/dCache/org/dcache/cells/UniversalSpringCell.java
+++ modules/dCache/org/dcache/cells/UniversalSpringCell.java
@@ -776,7 +776,7 @@ public class UniversalSpringCell
 
             Properties properties = new Properties();
             String arguments =
-                args.toString().replaceAll("-?\\$\\{.*\\}", "");
+                args.toString().replaceAll("-\\$\\{[0-9]+\\}", "");
             properties.setProperty("arguments", arguments);
             properties.setProperty("thisCell", getCellName());
             properties.setProperty("thisDomain", getCellDomainName());
