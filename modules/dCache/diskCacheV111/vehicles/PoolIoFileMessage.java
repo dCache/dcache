9d5001ee38885a6824215f3638ee306924d0789c
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2009-11-11 11:19:23 +0000

    core: cleanup of PoolIoFileMessage.java
    remove dead ( commented ) code
    remove trailing whitespaces
    
    Target: Trunk
    Acked-By: Gerd
    Patch: http://rb.dcache.org/r/1063/

--- modules/dCache/diskCacheV111/vehicles/PoolIoFileMessage.java
+++ modules/dCache/diskCacheV111/vehicles/PoolIoFileMessage.java
@@ -7,14 +7,13 @@ public class PoolIoFileMessage extends PoolMessage {
     private StorageInfo  _storageInfo  = null ;
     private ProtocolInfo _protocolInfo = null ;
     private PnfsId       _pnfsId       = null ;
-//    private boolean      _isReply      = false ;
     private boolean      _isPool2Pool  = false ;
     private String       _ioQueueName  = null ;
     private int          _moverId      = 0;
     private String       _initiator = "<undefined>";
-    
+
     private static final long serialVersionUID = -6549886547049510754L;
-    
+
     public PoolIoFileMessage( String pool , 
                               PnfsId pnfsId ,
                               ProtocolInfo protocolInfo ,
@@ -22,7 +21,7 @@ public class PoolIoFileMessage extends PoolMessage {
        super( pool ) ;
        _storageInfo  = storageInfo ;
        _protocolInfo = protocolInfo ;
-       _pnfsId       = pnfsId ;                      
+       _pnfsId       = pnfsId ;
     }
 
     public PoolIoFileMessage( String pool , 
@@ -30,16 +29,15 @@ public class PoolIoFileMessage extends PoolMessage {
                               ProtocolInfo protocolInfo  ){
        super( pool ) ;
        _protocolInfo = protocolInfo ;
-       _pnfsId       = pnfsId ;                      
+       _pnfsId       = pnfsId ;
     }
     public PnfsId       getPnfsId(){ return _pnfsId ; }
     public StorageInfo  getStorageInfo(){ return _storageInfo ; }
     public ProtocolInfo getProtocolInfo(){ return _protocolInfo ; }
-//    public void setReply(){ _isReply = true ; }
-//    public boolean isReply(){ return _isReply ; }
+
     public boolean isPool2Pool(){ return _isPool2Pool ; }
     public void setPool2Pool(){ _isPool2Pool = true ; }
-                           
+
     public void setIoQueueName( String ioQueueName ){
        _ioQueueName = ioQueueName ;
     }
@@ -52,8 +50,8 @@ public class PoolIoFileMessage extends PoolMessage {
      */
     public int getMoverId() {
         return _moverId;
-    }    
-                              
+    }
+
     /**
      * Setter for property moverId.
      * @param moverId New value of property moverId.
@@ -61,12 +59,12 @@ public class PoolIoFileMessage extends PoolMessage {
     public void setMoverId(int moverId) {
         this._moverId = moverId;
     }
-    
-    
+
+
     public void setInitiator(String initiator) {
         _initiator = initiator;
     }
-    
+
     public String getInitiator() {
         return _initiator;
     }
