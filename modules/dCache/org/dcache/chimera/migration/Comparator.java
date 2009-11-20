01c70e4ebcd36405e6375d3a054fbcd46a43f292
Paul Millar <paul.millar@desy.de>
2009-11-20 15:15:48 +0000

    Revert code checked in by mistake
    The following patch fixes code that was checking in by mistake.
    
    Acked-by: Tigran (verbally)

--- modules/dCache/org/dcache/chimera/migration/Comparator.java
+++ modules/dCache/org/dcache/chimera/migration/Comparator.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 import org.dcache.chimera.namespace.ChimeraNameSpaceProvider;
 
@@ -16,43 +14,29 @@ import diskCacheV111.util.PnfsId;
 import dmg.util.Args;
 
 public class Comparator {
-    
-    private static final String OPTION_CHIMIERA_CONFIG = "config";
-    private static final String OPTION_PNFS_MOUNT = "pnfsMount";
-    
-    private static final String FILENAME_CHIMERA_CONFIG_DEFAULT = "/opt/d-cache/config/chimera-config.xml";
 
 	public static void main(String args[]) throws Exception {
 
 	    Args parsedArgs = new Args( args);
 
-        if( parsedArgs.argc() != 1 || parsedArgs.isOneCharOption( 'h')) {
+        if( parsedArgs.argc() != 1 || parsedArgs.optc() > 1 ||
+	            (parsedArgs.optc() == 1 && !parsedArgs.optv( 0).equals( "k"))) {
 			System.err.println("");
-			System.err.println("    Usage Comparator [-k] [-" + OPTION_CHIMIERA_CONFIG + "=<file>] <file>");
+			System.err.println("    Usage Comparator [-k] <file>");
 			System.err.println("");
 			System.err.println("where <file> contains a list of PNFS IDs of files to verify.");
-            System.err.println( "-k continue when an inconsistency is discovered.\n");
-            System.err.println( "-" + OPTION_CHIMIERA_CONFIG + "=<file> is the Chimera XML configuration file.\n");
-            System.err.println( "     ("+FILENAME_CHIMERA_CONFIG_DEFAULT + " by default)\n");
+                        System.err.println( "-k continue when an inconsistency is discovered.\n");
 			System.exit(2);
 		}
-        
-        String suppliedConfigFilename = parsedArgs.getOpt( OPTION_CHIMIERA_CONFIG);
-        String chimeraConfigFilename = suppliedConfigFilename != null ?
-                suppliedConfigFilename :
-                FILENAME_CHIMERA_CONFIG_DEFAULT;
-        
-        String pnfsMount = parsedArgs.getOpt( OPTION_PNFS_MOUNT);
 
 		String file = parsedArgs.argv( 0);
 		boolean showAllErrors = parsedArgs.isOneCharOption( 'k');
 
 		String pnfsArgs = "diskCacheV111.util.GenericInfoExtractor "
 				+ "-delete-registration=dummyLocation -delete-registration-jdbcDrv=foo "
-				+ "-delete-registration-dbUser=dummyUser -delete-registration-dbPass=dummyPass "
-				+ (pnfsMount != null ? "-pnfs="+pnfsMount : "");
+				+ "-delete-registration-dbUser=dummyUser -delete-registration-dbPass=dummyPass ";
 		String chimeraArgs = "org.dcache.chimera.namespace.ChimeraOsmStorageInfoExtractor "
-				+ "-chimeraConfig=" + chimeraConfigFilename;
+				+ "-chimeraConfig=/opt/d-cache/config/chimera-config.xml";
 
 		NameSpaceProvider chimeraNamespace = new ChimeraNameSpaceProvider(
 				new Args(chimeraArgs), null);
@@ -72,28 +56,49 @@ public class Comparator {
             System.exit(2);
         }
 
-        int errorCount = 0;
+        int idCount = 0, errorCount = 0;
 
-        TerminableBlockingQueue<PnfsId> queue = new TerminableBlockingQueueWrapper<PnfsId>( 
-        		new ArrayBlockingQueue<PnfsId>( 1, true),
-        		new PnfsId("0000000000000000000000"), 1);
+		try {
+			String rawLine;
 
-        PnfsIdProducer producer = new PnfsIdProducer( br, queue);
-        
-        producer.start();
-        
-        
-        PnfsId id;
-        while( (id = queue.take()) != queue.getPoisonItem()) {
-        	if( !combinedComparator.isOK( id)) {
-        		errorCount++;
-        		if( !showAllErrors)
-        			break;
-        	}
-        }
+			while( (rawLine = br.readLine()) != null) {
 
-        producer.emitFinalStatistics();
+			    String trimmedLine = rawLine.trim();
 
-		System.out.println((errorCount == 0 ? "All OK." : (errorCount + " failures.")));
+                if( trimmedLine.startsWith( "#"))
+                    continue;
+
+                if( trimmedLine.length() == 0)
+                    continue;
+
+				if (idCount % 1000 == 0)
+					System.out.print("\nChecked " + idCount + " files: ");
+				else if (idCount % 20 == 0)
+					System.out.print(".");
+				System.out.flush();
+
+				idCount++;
+
+				PnfsId pnfsid = new PnfsId(trimmedLine);
+
+				if( !combinedComparator.isOK( pnfsid)) {
+				    errorCount++;
+				    if( !showAllErrors)
+				        break;
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("Cannot read file: " + e.getMessage());
+			System.exit(2);
+		} finally {
+			try {
+			    br.close();
+			} catch (IOException e) {
+				// Ignore this one: it doesn't really matter and shouldn't happen with local files, right?
+			}
+		}
+
+		System.out.println("\n" + idCount + " IDs verified, " +
+		                   (errorCount == 0 ? "all OK." : (errorCount + " failures.")));
 	}
 }
