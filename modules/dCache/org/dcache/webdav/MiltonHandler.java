c5cb5a9e9bd621fef5dc33cc7b0624bb30de27d6
Gerd Behrmann <behrmann@nordu.net>
2009-12-01 12:50:11 +0000

    webdav: Add CDC session to WebDAV requests
    Target: trunk
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: http://rb.dcache.org/r/1175/

--- modules/dCache/org/dcache/webdav/MiltonHandler.java
+++ modules/dCache/org/dcache/webdav/MiltonHandler.java
@@ -13,31 +13,61 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 
 import java.io.IOException;
 
+import org.dcache.cells.CellMessageSender;
+
+import dmg.cells.nucleus.CDC;
+import dmg.cells.nucleus.CellInfo;
+import dmg.cells.nucleus.CellEndpoint;
+import org.apache.log4j.MDC;
+import org.apache.log4j.NDC;
+
 /**
  * A Jetty handler that wraps a Milton HttpManager. Makes it possible
  * to embed Milton in Jetty without using the Milton servlet.
  */
-public class MiltonHandler extends AbstractHandler
+public class MiltonHandler
+    extends AbstractHandler
+    implements CellMessageSender
 {
     private HttpManager _httpManager;
+    private String _cellName;
+    private String _domainName;
 
     public void setHttpManager(HttpManager httpManager)
     {
         _httpManager = httpManager;
     }
 
+    public void setCellEndpoint(CellEndpoint endpoint)
+    {
+        CellInfo info = endpoint.getCellInfo();
+        _cellName = info.getCellName();
+        _domainName = info.getDomainName();
+    }
+
     public void handle(String target, Request baseRequest,
                        HttpServletRequest request,HttpServletResponse response)
         throws IOException, ServletException
     {
+        MDC.put(CDC.MDC_CELL, _cellName);
+        MDC.put(CDC.MDC_DOMAIN, _domainName);
+        CDC.createSession();
+        NDC.push(CDC.getSession().toString());
         try {
-            ServletRequest req = new ServletRequest(request);
-            ServletResponse resp = new ServletResponse(response);
-            baseRequest.setHandled(true);
-            _httpManager.process(req, resp);
+            try {
+                ServletRequest req = new ServletRequest(request);
+                ServletResponse resp = new ServletResponse(response);
+                baseRequest.setHandled(true);
+                _httpManager.process(req, resp);
+            } finally {
+                response.getOutputStream().flush();
+                response.flushBuffer();
+            }
         } finally {
-            response.getOutputStream().flush();
-            response.flushBuffer();
+            MDC.remove(CDC.MDC_DOMAIN);
+            MDC.remove(CDC.MDC_CELL);
+            MDC.remove(CDC.MDC_SESSION);
+            NDC.remove();
         }
     }
 }
