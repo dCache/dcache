b1110d22bd526f2792a6a82f1b184c2cd9271242
Gerd Behrmann <behrmann@nordu.net>
2010-01-15 09:01:13 +0000

    doors: Get rid of PnfsGetFileMetaDataMessage in FileMetaDataSource
    We aim at removing PnfsGetFileMetaDataMessage, so this patch removes
    some of the uses of that message.
    
    Also removes two unused classes: FsPermissionHandler and FileMetaDataX.
    
    Target: trunk
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: http://rb.dcache.org/r/1388/

--- modules/dCacheJUnit/org/dcache/tests/namespace/FileMetaDataProviderHelper.java
+++ modules/dCacheJUnit/org/dcache/tests/namespace/FileMetaDataProviderHelper.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import diskCacheV111.services.FileMetaDataSource;
 import diskCacheV111.util.CacheException;
 import diskCacheV111.util.FileMetaData;
-import diskCacheV111.util.FileMetaDataX;
 import diskCacheV111.util.FileNotFoundCacheException;
 import diskCacheV111.util.PnfsId;
 import dmg.cells.nucleus.CellEndpoint;
@@ -22,13 +21,10 @@ public class FileMetaDataProviderHelper implements FileMetaDataSource {
         private static final Map<PnfsId, FileMetaData> _metadataById = new HashMap<PnfsId, FileMetaData>();
         private static final Map<String, FileMetaData> _metadataByPath = new HashMap<String, FileMetaData>();
 
-        private static final Map<PnfsId, FileMetaDataX> _metadataXById = new HashMap<PnfsId, FileMetaDataX>();
-        private static final Map<String, FileMetaDataX> _metadataXByPath = new HashMap<String, FileMetaDataX>();
-
         public FileMetaDataProviderHelper(CellEndpoint cell) {
         	// forced by use in dcache .batch files
         }
-        
+
         public FileMetaData getMetaData(String path) throws CacheException {
 
             FileMetaData metaData = _metadataByPath.get(path);
@@ -49,27 +45,6 @@ public class FileMetaDataProviderHelper implements FileMetaDataSource {
             return metaData;
         }
 
-
-        public FileMetaDataX getXMetaData(String path) throws CacheException {
-
-            FileMetaDataX metaData = _metadataXByPath.get(path);
-
-            if( metaData == null ) {
-                throw new FileNotFoundCacheException(path + " not found");
-            }
-            return metaData;
-        }
-
-        public FileMetaDataX getXMetaData(PnfsId pnfsId) throws CacheException {
-
-            FileMetaDataX metaData = _metadataXById.get(pnfsId);
-
-            if( metaData == null ) {
-                throw new FileNotFoundCacheException(pnfsId + " not found");
-            }
-            return metaData;
-        }
-
         public void setMetaData(PnfsId pnfsId,FileMetaData metaData ) {
             _metadataById.put(pnfsId, metaData);
         }
@@ -79,21 +54,8 @@ public class FileMetaDataProviderHelper implements FileMetaDataSource {
             _metadataByPath.put(path, metaData);
         }
 
-        public void setXMetaData(PnfsId pnfsId,FileMetaDataX metaData ) {
-            _metadataXById.put(pnfsId, metaData);
-        }
-
-
-        public void setXMetaData(String path,FileMetaDataX metaData ) {
-            _metadataXByPath.put(path, metaData);
-        }
-
         public void cleanAll() {
             _metadataById.clear();
             _metadataByPath.clear();
-            _metadataXById.clear();
-            _metadataXByPath.clear();
-
         }
-
     }
