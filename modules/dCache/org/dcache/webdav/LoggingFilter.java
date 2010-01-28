7191a237552db69a3f46320f0e7450bdf3ce5a0b
Gerd Behrmann <behrmann@nordu.net>
2010-01-28 07:51:14 +0000

    WebDAV: Fix error handling in filters
    MiltonHandler would commit the response in a finally block. Thus even
    when some of the filters would throw a RuntimException, the response
    back to the client would be 200 OK.
    
    This patch removes the finally block allowing Jetty to handle the
    exception and generate a 500 Server Error.
    
    LoggingFilter was updated to extract user information directly from the
    request object. Thus the filter can be placed in front of the
    SecurityFilter and hence will log failures that happen during
    authentication.
    
    Target: trunk
    Target: 1.9.6
    Ticket: #5416
    Patch: http://rb.dcache.org/r/1444/
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- modules/dCache/org/dcache/webdav/LoggingFilter.java
+++ modules/dCache/org/dcache/webdav/LoggingFilter.java
@@ -4,12 +4,15 @@ import com.bradmcevoy.http.Filter;
 import com.bradmcevoy.http.FilterChain;
 import com.bradmcevoy.http.Request;
 import com.bradmcevoy.http.Response;
+import com.bradmcevoy.http.ServletRequest;
 
 import org.dcache.auth.Subjects;
 
 import com.sun.security.auth.UserPrincipal;
 import javax.security.auth.Subject;
+import javax.servlet.http.HttpServletRequest;
 import java.security.AccessController;
+import java.security.cert.X509Certificate;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -21,6 +24,9 @@ import org.apache.log4j.Logger;
 public class LoggingFilter implements Filter
 {
     private final Logger _log = Logger.getLogger(LoggingFilter.class);
+
+    private static final String X509_CERTIFICATE_ATTRIBUTE =
+        "javax.servlet.request.X509Certificate";
 
     @Override
     public void process(final FilterChain filterChain,
@@ -58,17 +64,15 @@ public class LoggingFilter implements Filter
 
     private String getUser()
     {
-        Subject subject = Subject.getSubject(AccessController.getContext());
-        if (subject != null) {
-            String user = Subjects.getDn(subject);
-            if (user == null)  {
-                Set<UserPrincipal> principals =
-                    subject.getPrincipals(UserPrincipal.class);
-                if (!principals.isEmpty()) {
-                    return principals.iterator().next().getName();
-                }
+        HttpServletRequest servletRequest = ServletRequest.getRequest();
+        Object object =
+            servletRequest.getAttribute(X509_CERTIFICATE_ATTRIBUTE);
+        if (object instanceof X509Certificate[]) {
+            X509Certificate[] chain = (X509Certificate[]) object;
+            if (chain.length >= 1) {
+                return chain[0].getSubjectX500Principal().getName();
             }
         }
         return "";
-    }
+   }
 }
