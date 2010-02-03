cad6751a9847f39de3c6296ddfd81e2a5905aae3
Gerd Behrmann <behrmann@nordu.net>
2010-02-03 11:43:11 +0000

    Merged r13401,r13402
    svn merge -c 13401,13402 svn+ssh://svn.dcache.org/data/svn/dCache/trunk

--- skel/share/lib/services.sh
+++ skel/share/lib/services.sh
@@ -282,13 +282,19 @@ getPidOfDomain() # in $1 = Domain name, out $2 = pid
     local domain
     local pidFile
     local pidDir
+    local service
     local ret
 
     domain="$1"
 
     getConfigurationValue "$domain" pidDir pidDir || return
+    getService "$domain" service || return
 
-    pidFile="${pidDir:-$DCACHE_PID}/dcache.$domain-daemon.pid"
+    if [ "$service" = "srm" ]; then
+        pidFile="${pidDir:-$DCACHE_PID}/dcache.$domain.pid"
+    else
+        pidFile="${pidDir:-$DCACHE_PID}/dcache.$domain-daemon.pid"
+    fi
     [ -f "${pidFile}" ] || return
 
     ret=$(cat "${pidFile}")
@@ -304,13 +310,19 @@ getJavaPidOfDomain() # in $1 = Domain name, out $2 = pid
     local domain
     local pidFile
     local pidDir
+    local service
     local ret
 
     domain="$1"
 
     getConfigurationValue "$domain" pidDir pidDir || return
+    getService "$domain" service || return
 
-    pidFile="${pidDir:-$DCACHE_PID}/dcache.$domain-java.pid"
+    if [ "$service" = "srm" ]; then
+        pidFile="${pidDir:-$DCACHE_PID}/dcache.$domain.pid"
+    else
+        pidFile="${pidDir:-$DCACHE_PID}/dcache.$domain-java.pid"
+    fi
     [ -f "${pidFile}" ] || return
 
     ret=$(cat "${pidFile}")
