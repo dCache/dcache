1e6499a79f46b1ff8e3a812627a357bdbf6dbd6f
Gerd Behrmann <behrmann@nordu.net>
2010-02-05 11:25:51 +0000

    pool: Fix reuse of TCP port in xrootd mover
    The xrootd mover uses a private TCP port for accepting the connection
    from the client. As only a single connection is expected, it would close
    the server socket as soon as the connection has been established. The
    rational was that we would avoid responding on the port for longer than
    absolutely necessary and we would free up ports in the limited port
    range for other movers.
    
    The problem we ran into is when the same client would read multiple
    files from the same pool. It could happen that both movers would pick
    the same port. This is as designed as the first mover releases the port
    as soon as it got its connection. However the xrootd client has a clever
    optimisation where it recognizes that the second redirect goes to the
    same pool and the same port. In that case it reuses the existing TCP
    connection for opening the second file. This however fails as the
    request goes to the wrong mover.
    
    The solution is to not close the server socket until the file has been
    transferred.
    
    Target: trunk
    Target: 1.9.6
    Target: 1.9.5
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: http://rb.dcache.org/r/1489/

--- modules/dCache/org/dcache/xrootd2/pool/XrootdProtocol_3.java
+++ modules/dCache/org/dcache/xrootd2/pool/XrootdProtocol_3.java
@@ -223,6 +223,11 @@ public class XrootdProtocol_3
     private final CountDownLatch _closeLatch = new CountDownLatch(1);
 
     /**
+     * Hangup handler signals closure of the client connection.
+     */
+    private final HangupHandler _hangupHandler = new HangupHandler(_closeLatch);
+
+    /**
      * Next response message used during read operations. Provides a
      * simplistic read-ahead buffer.
      */
@@ -467,7 +472,7 @@ public class XrootdProtocol_3
                         pipeline.addLast("logger", new LoggingHandler(XrootdProtocol_3.class));
                     }
                     pipeline.addLast("handshake", new XrootdHandshakeHandler(XrootdProtocol.DATA_SERVER));
-                    pipeline.addLast("hangup", new HangupHandler(_closeLatch));
+                    pipeline.addLast("hangup", _hangupHandler);
                     pipeline.addLast("executor", new ExecutionHandler(_diskExecutor));
                     pipeline.addLast("transfer", XrootdProtocol_3.this);
                     return pipeline;
