c63dc41a89c6f7b1a87335905a39ee45a391c68d
Paul Millar <paul.millar@desy.de>
2010-02-18 14:33:04 +0000

    Strip trailing white-space

--- modules/dCache/org/dcache/services/info/serialisation/SimpleTextSerialiser.java
+++ modules/dCache/org/dcache/services/info/serialisation/SimpleTextSerialiser.java
@@ -20,49 +20,49 @@ import org.dcache.services.info.base.StringStateValue;
  * <p>
  * NB, instances of this Class are not thread-safe: the caller is responsible for
  * ensuring no concurrent calls to serialise().
- * 
- * @see PrettyPrintTextSerialiser 
- * 
+ *
+ * @see PrettyPrintTextSerialiser
+ *
  * @author Paul Millar <paul.millar@desy.de>
  */
 public class SimpleTextSerialiser implements StateVisitor, StateSerialiser {
-	
+
 	public static final String NAME = "simple";
-		
+
 	private static final String LIST_TYPE = "List item";
-	
+
 	private StringBuilder _result = new StringBuilder();
 	private StatePath _lastStateComponentPath=null;
 	private StatePath _startPath;
 	private final StateExhibitor _exhibitor;
-	
+
 	public SimpleTextSerialiser( StateExhibitor exhibitor) {
 		_exhibitor = exhibitor;
 	}
-	
+
 	public String serialise( StatePath start) {
 		_result = new StringBuilder();
 		_startPath = start;
-		
+
 		if( start != null) {
 			_result.append( start.toString());
 			_result.append(">\n");
 		}
-		
+
 		_exhibitor.visitState( this, start);
-		
+
 		return _result.toString();
 	}
-	
+
 	public String serialise() {
 		return serialise( null);
 	}
-	
-	
+
+
 	public String getName() {
 		return NAME;
 	}
-	
+
 	public void visitBoolean(StatePath path, BooleanStateValue value) {
 		outputMetric( path, value.toString(), value.getTypeName());
 		_lastStateComponentPath = path;
@@ -82,16 +82,16 @@ public class SimpleTextSerialiser implements StateVisitor, StateSerialiser {
 		outputMetric( path, "\""+value+"\"", value.getTypeName());
 		_lastStateComponentPath = path;
 	}
-	
+
 	public void visitCompositePreDescend(StatePath path, Map<String, String> metadata) {
 		_lastStateComponentPath = path;
 	}
 	public void visitCompositePostDescend(StatePath path, Map<String, String> metadata) {
-		
+
 		// If we just traversed a path without it containing any elements, treat it as a list.
 		if( path != null && path.equals(_lastStateComponentPath) && !path.isSimplePath()) {
 			String type = LIST_TYPE;
-			
+
 			if( metadata != null) {
 				String className = metadata.get( State.METADATA_BRANCH_CLASS_KEY);
 				if( className != null)
@@ -100,29 +100,29 @@ public class SimpleTextSerialiser implements StateVisitor, StateSerialiser {
 			outputMetric( path, "", type);
 		}
 	}
-	
+
 	public void visitCompositePreLastDescend(StatePath path, Map<String, String> metadata) {}
 	public void visitCompositePreSkipDescend(StatePath path, Map<String, String> metadata) {}
 	public void visitCompositePostSkipDescend(StatePath path, Map<String, String> metadata) {}
-	
+
 	/**
 	 * Output a single line, corresponding to a metric value.
-	 * @param path  The StatePath for this metric 
+	 * @param path  The StatePath for this metric
 	 * @param metricValue the String representing this metric's current value.
 	 * @param metricType the type of metric.
 	 */
 	private void outputMetric( StatePath path, String metricValue, String metricType) {
-		
+
 		if( _startPath != null)
 			_result.append( "  ");
-		
+
 		if( path != null)
 			_result.append( path.toString( _startPath));
-		
+
 		_result.append( ":  ");
 		_result.append( metricValue);
 		_result.append( " [");
 		_result.append( metricType);
-		_result.append( "]\n");		
+		_result.append( "]\n");
 	}
 }
