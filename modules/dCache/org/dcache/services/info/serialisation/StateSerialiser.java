f9453b762a59a642ed80cd447dcef43d61152456
Paul Millar <paul.millar@desy.de>
2010-02-23 18:05:43 +0000

    Fixing white-space

--- modules/dCache/org/dcache/services/info/serialisation/StateSerialiser.java
+++ modules/dCache/org/dcache/services/info/serialisation/StateSerialiser.java
@@ -5,16 +5,16 @@ import org.dcache.services.info.base.StatePath;
 /**
  * Classes that implement StateSerialiser provide a representations of dCache's
  * current state as a String.
- *  
+ *
  * @author Paul Millar <paul.millar@desy.de>
  */
 public interface StateSerialiser {
-	
-	/** Return a simple name for this serialiser */
-	String getName();
-	
-	/** Return serialised version of dCache's current state */
-	String serialise();
-	
-	String serialise( StatePath start);
+
+    /** Return a simple name for this serialiser */
+    String getName();
+
+    /** Return serialised version of dCache's current state */
+    String serialise();
+
+    String serialise( StatePath start);
 }
