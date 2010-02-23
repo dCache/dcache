0ae2b05686e4defe1e08accc0d15a74c5e772b32
Paul Millar <paul.millar@desy.de>
2010-02-23 18:03:49 +0000

    Fixing whitespace

--- modules/dCache/org/dcache/services/info/stateInfo/PoolgroupToPoolsVisitor.java
+++ modules/dCache/org/dcache/services/info/stateInfo/PoolgroupToPoolsVisitor.java
@@ -22,74 +22,74 @@ import org.dcache.services.info.base.StringStateValue;
  */
 public class PoolgroupToPoolsVisitor implements StateVisitor {
 
-	private static Logger _log = LoggerFactory.getLogger( PoolgroupToPoolsVisitor.class);
+    private static Logger _log = LoggerFactory.getLogger( PoolgroupToPoolsVisitor.class);
 
-	private static final StatePath POOLGROUPS_PATH = new StatePath( "poolgroups");
+    private static final StatePath POOLGROUPS_PATH = new StatePath( "poolgroups");
 
-	/**
-	 * Obtain a Map between a poolgroup and the pools that are currently members of this poolgroup.
-	 * @return
-	 */
-	public static Map <String,Set<String>> getDetails( StateExhibitor exhibitor) {
-		if( _log.isInfoEnabled())
-			_log.info( "Gathering current status");
+    /**
+     * Obtain a Map between a poolgroup and the pools that are currently members of this poolgroup.
+     * @return
+     */
+    public static Map <String,Set<String>> getDetails( StateExhibitor exhibitor) {
+        if( _log.isInfoEnabled())
+            _log.info( "Gathering current status");
 
-		PoolgroupToPoolsVisitor visitor = new PoolgroupToPoolsVisitor();
-		exhibitor.visitState(visitor, POOLGROUPS_PATH);
-		return visitor._poolgroups;
-	}
+        PoolgroupToPoolsVisitor visitor = new PoolgroupToPoolsVisitor();
+        exhibitor.visitState(visitor, POOLGROUPS_PATH);
+        return visitor._poolgroups;
+    }
 
-	/**
-	 * Obtain a Map between a poolgroup and the pools that will be members of this poolgroup after
-	 * the given StateTransition
-	 * @param transition the StateTransition to consider
-	 * @return
-	 */
-	public static Map <String,Set<String>> getDetails( StateExhibitor exhibitor, StateTransition transition) {
-		if( _log.isInfoEnabled())
-			_log.info( "Gathering status after transition");
+    /**
+     * Obtain a Map between a poolgroup and the pools that will be members of this poolgroup after
+     * the given StateTransition
+     * @param transition the StateTransition to consider
+     * @return
+     */
+    public static Map <String,Set<String>> getDetails( StateExhibitor exhibitor, StateTransition transition) {
+        if( _log.isInfoEnabled())
+            _log.info( "Gathering status after transition");
 
-		PoolgroupToPoolsVisitor visitor = new PoolgroupToPoolsVisitor();
-		exhibitor.visitState(transition, visitor, POOLGROUPS_PATH);
-		return visitor._poolgroups;
-	}
+        PoolgroupToPoolsVisitor visitor = new PoolgroupToPoolsVisitor();
+        exhibitor.visitState(transition, visitor, POOLGROUPS_PATH);
+        return visitor._poolgroups;
+    }
 
 
-	Map <String,Set<String>> _poolgroups = new HashMap<String,Set<String>>();
-	Set<String> _currentPoolgroupPools;
-	StatePath _poolMembershipPath;
+    Map <String,Set<String>> _poolgroups = new HashMap<String,Set<String>>();
+    Set<String> _currentPoolgroupPools;
+    StatePath _poolMembershipPath;
 
-	public void visitCompositePreDescend( StatePath path, Map<String,String> metadata) {
-		if( _log.isDebugEnabled())
-			_log.debug( "Examining "+path);
+    public void visitCompositePreDescend( StatePath path, Map<String,String> metadata) {
+        if( _log.isDebugEnabled())
+            _log.debug( "Examining "+path);
 
-		// If something like poolgroups.<some poolgroup>
-		if( POOLGROUPS_PATH.isParentOf( path)) {
-			if( _log.isDebugEnabled())
-				_log.debug( "Found poolgroup "+path.getLastElement());
+        // If something like poolgroups.<some poolgroup>
+        if( POOLGROUPS_PATH.isParentOf( path)) {
+            if( _log.isDebugEnabled())
+                _log.debug( "Found poolgroup "+path.getLastElement());
 
-			_currentPoolgroupPools = new HashSet<String>();
-			_poolgroups.put( path.getLastElement(), _currentPoolgroupPools);
+            _currentPoolgroupPools = new HashSet<String>();
+            _poolgroups.put( path.getLastElement(), _currentPoolgroupPools);
 
-			_poolMembershipPath = path.newChild("pools");
-		}
+            _poolMembershipPath = path.newChild("pools");
+        }
 
-		// If something like poolgroups.<some poolgroup>.pools.<some pool>
-		if( _poolMembershipPath != null && _poolMembershipPath.isParentOf(path)) {
-			if( _log.isDebugEnabled())
-				_log.debug( "Found pool "+path.getLastElement());
+        // If something like poolgroups.<some poolgroup>.pools.<some pool>
+        if( _poolMembershipPath != null && _poolMembershipPath.isParentOf(path)) {
+            if( _log.isDebugEnabled())
+                _log.debug( "Found pool "+path.getLastElement());
 
-			_currentPoolgroupPools.add( path.getLastElement());
-		}
-	}
+            _currentPoolgroupPools.add( path.getLastElement());
+        }
+    }
 
-	public void visitCompositePostDescend( StatePath path, Map<String,String> metadata) {}
-	public void visitCompositePreSkipDescend( StatePath path, Map<String,String> metadata) {}
-	public void visitCompositePostSkipDescend( StatePath path, Map<String,String> metadata) {}
-	public void visitString( StatePath path, StringStateValue value) {}
-	public void visitBoolean( StatePath path, BooleanStateValue value) {}
-	public void visitInteger( StatePath path, IntegerStateValue value) {}
-	public void visitFloatingPoint( StatePath path, FloatingPointStateValue value) {}
+    public void visitCompositePostDescend( StatePath path, Map<String,String> metadata) {}
+    public void visitCompositePreSkipDescend( StatePath path, Map<String,String> metadata) {}
+    public void visitCompositePostSkipDescend( StatePath path, Map<String,String> metadata) {}
+    public void visitString( StatePath path, StringStateValue value) {}
+    public void visitBoolean( StatePath path, BooleanStateValue value) {}
+    public void visitInteger( StatePath path, IntegerStateValue value) {}
+    public void visitFloatingPoint( StatePath path, FloatingPointStateValue value) {}
 }
 
 
