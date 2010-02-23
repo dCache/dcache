51996004d1659d305820fb59379facda6ac7ebff
Paul Millar <paul.millar@desy.de>
2010-02-23 18:02:51 +0000

    Whitespace fix

--- modules/dCache/org/dcache/services/info/stateInfo/SkeletonListVisitor.java
+++ modules/dCache/org/dcache/services/info/stateInfo/SkeletonListVisitor.java
@@ -24,96 +24,96 @@ import org.dcache.services.info.base.StringStateValue;
  */
 public class SkeletonListVisitor implements StateVisitor {
 
-	private static Logger _log = LoggerFactory.getLogger( SkeletonListVisitor.class);
+    private static Logger _log = LoggerFactory.getLogger( SkeletonListVisitor.class);
 
-	final private StatePath _pathToList;
+    final private StatePath _pathToList;
 
-	/** The key of the current branch */
-	private String _thisKey;
+    /** The key of the current branch */
+    private String _thisKey;
 
-	/**
-	 * Instantiate the list over the items underneath pathToList.
-	 * @param pathToList the StatePath representing the parent object for this list.
-	 */
-	protected SkeletonListVisitor( StatePath pathToList) {
-		if( _log.isDebugEnabled())
-			_log.debug( "Searching on path " + pathToList);
+    /**
+     * Instantiate the list over the items underneath pathToList.
+     * @param pathToList the StatePath representing the parent object for this list.
+     */
+    protected SkeletonListVisitor( StatePath pathToList) {
+        if( _log.isDebugEnabled())
+            _log.debug( "Searching on path " + pathToList);
 
-		_pathToList = pathToList;
-	}
+        _pathToList = pathToList;
+    }
 
-	/**
-	 * The super-Class should override one of the following four methods
-	 */
-	public void visitBoolean(StatePath path, BooleanStateValue value) {}
-	public void visitFloatingPoint(StatePath path, FloatingPointStateValue value) {}
-	public void visitInteger(StatePath path, IntegerStateValue value) {}
-	public void visitString(StatePath path, StringStateValue value) {}
+    /**
+     * The super-Class should override one of the following four methods
+     */
+    public void visitBoolean(StatePath path, BooleanStateValue value) {}
+    public void visitFloatingPoint(StatePath path, FloatingPointStateValue value) {}
+    public void visitInteger(StatePath path, IntegerStateValue value) {}
+    public void visitString(StatePath path, StringStateValue value) {}
 
-	public void visitCompositePreSkipDescend(StatePath path, Map<String, String> metadata) {}
-	public void visitCompositePostSkipDescend(StatePath path, Map<String, String> metadata) {}
+    public void visitCompositePreSkipDescend(StatePath path, Map<String, String> metadata) {}
+    public void visitCompositePostSkipDescend(StatePath path, Map<String, String> metadata) {}
 
-	public void visitCompositePreDescend(StatePath path, Map<String, String> metadata) {
-		if( _pathToList.isParentOf( path)) {
-			if( _log.isDebugEnabled())
-				_log.debug( "Entering " + path);
+    public void visitCompositePreDescend(StatePath path, Map<String, String> metadata) {
+        if( _pathToList.isParentOf( path)) {
+            if( _log.isDebugEnabled())
+                _log.debug( "Entering " + path);
 
-			newListItem( path.getLastElement());
-		}
-	}
+            newListItem( path.getLastElement());
+        }
+    }
 
-	public void visitCompositePostDescend(StatePath path, Map<String, String> metadata) {
-		if( _pathToList.isParentOf( path)) {
-			if( _log.isDebugEnabled())
-				_log.debug( "Leaving " + path);
+    public void visitCompositePostDescend(StatePath path, Map<String, String> metadata) {
+        if( _pathToList.isParentOf( path)) {
+            if( _log.isDebugEnabled())
+                _log.debug( "Leaving " + path);
 
-			exitingListItem( path.getLastElement());
-		}
-	}
+            exitingListItem( path.getLastElement());
+        }
+    }
 
-	/**
-	 * Method called whenever a new list item is visited.
-	 * @param listItemName the name of the list item to record.
-	 * @see the getKey() method.
-	 */
-	protected void newListItem( String listItemName) {
-		if( _log.isDebugEnabled())
-			_log.debug( "Assigning _thisKey to " + listItemName);
+    /**
+     * Method called whenever a new list item is visited.
+     * @param listItemName the name of the list item to record.
+     * @see the getKey() method.
+     */
+    protected void newListItem( String listItemName) {
+        if( _log.isDebugEnabled())
+            _log.debug( "Assigning _thisKey to " + listItemName);
 
-		_thisKey = listItemName;
-	}
+        _thisKey = listItemName;
+    }
 
-	/**
-	 * Method called whenever the visitor is leaving a list item.
-	 * @param listItemName the name of the list item that is being left.
-	 */
-	protected void exitingListItem( String listItemName) {
-		if( _log.isDebugEnabled())
-			_log.debug( "Resetting _thisKey to null on leaving " + listItemName);
+    /**
+     * Method called whenever the visitor is leaving a list item.
+     * @param listItemName the name of the list item that is being left.
+     */
+    protected void exitingListItem( String listItemName) {
+        if( _log.isDebugEnabled())
+            _log.debug( "Resetting _thisKey to null on leaving " + listItemName);
 
-		_thisKey = null;
-	}
+        _thisKey = null;
+    }
 
-	/**
-	 * Obtain the StatePath to the parent object for all list items.
-	 */
-	protected StatePath getPathToList() {
-		return _pathToList;
-	}
+    /**
+     * Obtain the StatePath to the parent object for all list items.
+     */
+    protected StatePath getPathToList() {
+        return _pathToList;
+    }
 
-	/**
-	 * @return the name of the last item in the list, or null if not currently within a list item.
-	 */
-	protected String getKey() {
-		return _thisKey;
-	}
+    /**
+     * @return the name of the last item in the list, or null if not currently within a list item.
+     */
+    protected String getKey() {
+        return _thisKey;
+    }
 
-	/**
-	 * Check whether the visitor is within (underneath) the list item.  If so,
-	 * then getKey() will return the valid key.
-	 * @return true if visitor is within a list item, false otherwise.
-	 */
-	protected boolean isInListItem() {
-		return _thisKey != null;
-	}
+    /**
+     * Check whether the visitor is within (underneath) the list item.  If so,
+     * then getKey() will return the valid key.
+     * @return true if visitor is within a list item, false otherwise.
+     */
+    protected boolean isInListItem() {
+        return _thisKey != null;
+    }
 }
