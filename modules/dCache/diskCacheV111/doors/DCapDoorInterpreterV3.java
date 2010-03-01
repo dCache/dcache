cbd7625d9aac4c1055de1f939d3575ec0b9ef51e
Gerd Behrmann <behrmann@nordu.net>
2010-03-01 16:14:36 +0000

    DCAP: Fix subject and access mask handling
    When permission checking is delegated to PnfsManager, read checks have
    to be explicitly requested by the door. The code for doing this was
    present in the DCAP door, however when transfering by path the code
    would never be executed as the door relies on an exception being
    generated in the contructor of PnfsId and this exception would render
    the code to set the access mask dead.
    
    To check permissions in PnfsManager, the subject has to be specified in
    the message. This is usually set by PnfsHandler, however the DCap door
    would in several occasions bypass the PnfsHandler and send a message
    directlry to PnfsManager. In those cases the subject would not be set,
    thus bypassing permissions checks.
    
    Target: trunk
    Target: 1.9.6
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: http://rb.dcache.org/r/1573/

--- modules/dCache/diskCacheV111/doors/DCapDoorInterpreterV3.java
+++ modules/dCache/diskCacheV111/doors/DCapDoorInterpreterV3.java
@@ -1278,7 +1278,6 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
             try{
                 _pnfsId         = new PnfsId(_vargs.argv(0)) ;
                 _getStorageInfo = new PnfsGetStorageInfoMessage( _pnfsId ) ;
-                _getStorageInfo.setAccessMask(_accessMask);
             }catch(Exception ee ){
                 //
                 // seems not to be a pnfsId, might be a url.
@@ -1299,15 +1298,11 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
 
             say( "Requesting storageInfo for "+_getStorageInfo ) ;
 
+            _getStorageInfo.setAccessMask(_accessMask);
             _getStorageInfo.setId(_sessionId) ;
 
-            synchronized( _messageLock ){
-                _cell.sendMessage(
-                new CellMessage(
-                new CellPath( _pnfsManagerName )  ,
-                _getStorageInfo
-                )
-                ) ;
+            synchronized (_messageLock) {
+                _pnfs.send(_getStorageInfo);
             }
             setStatus( "WaitingForPnfs" ) ;
         }
@@ -1317,7 +1312,6 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
             try{
                 _pnfsId          = new PnfsId(_vargs.argv(0)) ;
                 _getFileMetaData = new PnfsGetFileMetaDataMessage( _pnfsId ) ;
-                _getFileMetaData.setAccessMask(_accessMask);
             }catch(Exception ee ){
                 //
                 // seems not to be a pnfsId, might be a url.
@@ -1337,16 +1331,13 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
 
             say( "Requesting FileMetaData for "+_getFileMetaData ) ;
 
+            _getFileMetaData.setAccessMask(_accessMask);
             _getFileMetaData.setId(_sessionId) ;
 
-            synchronized( _messageLock ){
-                _cell.sendMessage(
-                new CellMessage(
-                new CellPath( _pnfsManagerName )  ,
-                _getFileMetaData
-                )
-                ) ;
+            synchronized (_messageLock) {
+                _pnfs.send(_getFileMetaData);
             }
+
             setStatus( "WaitingForPnfs (FileMetaData)" ) ;
         }
         public void
@@ -2521,12 +2512,7 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
                 flag.setReplyRequired(false) ;
                 flag.setValue(checksumString);
 
-                _cell.sendMessage(
-                new CellMessage(
-                new CellPath("PnfsManager") ,
-                flag
-                )
-                );
+                _pnfs.send(flag);
             }catch(Exception eee ){
                 _log.error("Failed to send crc to PnfsManager : "+eee ) ;
             }
