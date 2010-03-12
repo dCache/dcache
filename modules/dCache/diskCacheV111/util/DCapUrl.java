9401010761fc2e09f90431225afbbf26af0093e2
Paul Millar <paul.millar@desy.de>
2010-03-12 11:10:47 +0000

    Whitespace-only fix

--- modules/dCache/diskCacheV111/util/DCapUrl.java
+++ modules/dCache/diskCacheV111/util/DCapUrl.java
@@ -4,27 +4,27 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class DCapUrl {
-	
+
 	private final URI _uri;
-		
+
 	/**
 	 * construct a DCapUrl from given string ( dcap://host:port/path/file )
-	 * 
+	 *
 	 * @param dCapUrl
 	 * @throws IllegalArgumentException If the given string violates RFC 2396, as augmented by the above deviations
 	 */
 	public DCapUrl(String dCapUrl) throws IllegalArgumentException {
-		
+
 		try {
 			_uri = new URI(dCapUrl);
 		} catch (URISyntaxException ue) {
 			// be complaint with dCache API specification
 			throw new IllegalArgumentException("Invalid dcap url : " + ue);
-		}		
+		}
 	}
 
 	/**
-	 * 
+	 *
 	 * @return The decoded path component of this DCapUrl, or null if the path is undefined
 	 */
 	public String getFilePart() {
@@ -32,7 +32,7 @@ public class DCapUrl {
 	}
 
 	/**
-	 * 
+	 *
 	 * @return The protocol component of this DCapUrl, or null if the protocol is undefined
 	 */
 	public String getProtocol() {
