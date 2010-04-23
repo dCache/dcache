e7f7490aae6a1bb489d9498795e9c73b9e521bef
Dmitry Litvintsev <litvinse@fnal.gov>
2010-04-23 21:57:10 +0000

    Added support for pattern matching in LinkGroupAuthorizationFile
    http://rb.dcache.org/r/1217/
    
    Signed off by Timur

--- modules/dCache/diskCacheV111/services/space/Manager.java
+++ modules/dCache/diskCacheV111/services/space/Manager.java
@@ -382,8 +382,7 @@ public final class Manager
                                 for (VOInfo info : lg.getVOs()) {
                                         String group = info.getVoGroup();
                                         String role  = info.getVoRole();
-                                        if ((group.equals(voGroup)||group.equals("*"))&&
-                                            (role==null||role.equals(voRole)||role.equals("*"))) {
+                                        if (info.match(voGroup,voRole)) {
                                                 foundMatch=true;
                                                 break;
                                         }
@@ -4930,64 +4929,68 @@ public final class Manager
 			decrementReservedSpaceInLinkGroup(connection,group.getId(),size);
 		}
 	}
-    public void checkReleasePermission(AuthorizationRecord authRecord, Space space)
-        throws SpaceAuthorizationException {
-        String spaceGroup = space.getVoGroup();
-        String spaceRole = space.getVoRole();
 
-        if((spaceGroup == null || spaceGroup.equals(authRecord.getVoGroup())) &&
-            (spaceRole == null || spaceRole.equals(authRecord.getVoRole()))) {
-            logger.debug("userGroup : "+authRecord.getVoGroup()+", userRole : "+
-                    authRecord.getVoRole()+ " have permission to release ");
-            return;
-        }
+        public void checkReleasePermission(AuthorizationRecord authRecord,
+                                           Space space)
+                throws SpaceAuthorizationException {
+                String spaceGroup = space.getVoGroup();
+                String spaceRole = space.getVoRole();
 
-        for(GroupList groupList: authRecord.getGroupLists()) {
-            FQAN voAttribute = new FQAN(groupList.getAttribute());
-            String userGroup = voAttribute.getGroup();
-            String userRole = voAttribute.getRole();
-            if((spaceGroup == null || spaceGroup.equals(userGroup)) &&
-                (spaceRole == null || spaceRole.equals(userRole))) {
-                logger.debug("userGroup : "+userGroup+", userRole : "+userRole+
-                        " have permission to release ");
-                return;
-            }
-        }
-
-        throw new SpaceAuthorizationException("user with "+authRecord+
-                " has no permission to release "+space);
-    }
-
-    public VOInfo checkReservePermission(AuthorizationRecord authRecord, LinkGroup linkGroup)
-        throws SpaceAuthorizationException {
-        VOInfo[] voInfos = linkGroup.getVOs();
-        for(VOInfo voInfo:voInfos) {
-            String lgGroup=voInfo.getVoGroup();
-            String lgRole=voInfo.getVoRole();
-            if( (lgGroup.equals("*") ||lgGroup.equals(authRecord.getVoGroup()))  &&
-                (lgRole.equals("*") ||lgRole.equals(authRecord.getVoRole()))) {
-                String userGroup = authRecord.getVoGroup();
-                String userRole = authRecord.getVoRole();
-                logger.debug("userGroup : "+userGroup+", userRole : "+userRole+
-                        " have permission to reserve ");
-                return new VOInfo(userGroup,userRole );
-            }
-
-            for(GroupList groupList: authRecord.getGroupLists()) {
-                FQAN voAttribute = new FQAN(groupList.getAttribute());
-                String userGroup = voAttribute.getGroup();
-                String userRole = voAttribute.getRole();
-                if((lgGroup.equals("*") ||lgGroup.equals(userGroup))  &&
-                   (lgRole.equals("*")  ||lgRole.equals(userRole))) {
-                    logger.debug("userGroup : "+userGroup+", userRole : "+userRole+
-                            " have permission to reserve ");
-                    return new VOInfo(userGroup,userRole );
+                if((spaceGroup == null || spaceGroup.equals(authRecord.getVoGroup())) &&
+                   (spaceRole == null || spaceRole.equals(authRecord.getVoRole()))) {
+                        logger.debug("userGroup : "+authRecord.getVoGroup()+", userRole : "+
+                                     authRecord.getVoRole()+ " have permission to release ");
+                        return;
                 }
-            }
-        }
-        throw new SpaceAuthorizationException("user with "+authRecord+
-                " has no permission to reserve in "+linkGroup);
 
-    }
+                for(GroupList groupList: authRecord.getGroupLists()) {
+                        FQAN voAttribute = new FQAN(groupList.getAttribute());
+                        String userGroup = voAttribute.getGroup();
+                        String userRole = voAttribute.getRole();
+                        if((spaceGroup == null || spaceGroup.equals(userGroup)) &&
+                           (spaceRole == null || spaceRole.equals(userRole))) {
+                                logger.debug("userGroup : "+userGroup+", userRole : "+userRole+
+                                             " have permission to release ");
+                                return;
+                        }
+                }
+
+                throw new SpaceAuthorizationException("user with "+authRecord+
+                                                      " has no permission to release "+space);
+        }
+
+        public VOInfo checkReservePermission(AuthorizationRecord authRecord,
+                                             LinkGroup linkGroup)
+                throws SpaceAuthorizationException {
+                VOInfo[] voInfos = linkGroup.getVOs();
+                for(VOInfo voInfo:voInfos) {
+                        String userGroup = authRecord.getVoGroup();
+                        String userRole = authRecord.getVoRole();
+                        if (VOInfo.match(voInfo,userGroup,userRole)) {
+                                if (logger.isDebugEnabled()) {
+                                        logger.debug("userGroup : "+userGroup+", userRole : "+userRole+
+                                                     " have permission to reserve ");
+                                }
+                                return new VOInfo(userGroup,userRole );
+                        }
+
+                        for(GroupList groupList: authRecord.getGroupLists()) {
+                                if (groupList.getAttribute()==null) continue;
+                                FQAN voAttribute = new FQAN(groupList.getAttribute());
+                                userGroup = voAttribute.getGroup();
+                                userRole = voAttribute.getRole();
+                                if (VOInfo.match(voInfo,userGroup,userRole)) {
+                                        if (logger.isDebugEnabled()) {
+                                                logger.debug("userGroup : "+userGroup+", userRole : "+userRole+
+                                                             " have permission to reserve ");
+                                                return new VOInfo(userGroup,userRole );
+                                        }
+                                }
+                        }
+                }
+                throw new SpaceAuthorizationException("user with "+authRecord+
+                                                      " has no permission to reserve in "+linkGroup);
+        }
+
 
 }
