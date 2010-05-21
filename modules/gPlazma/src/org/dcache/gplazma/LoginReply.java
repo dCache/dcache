e98223118f14f00f260355353be6a1f9501f98b5
Paul Millar <paul.millar@desy.de>
2010-05-21 10:32:43 +0000

    gPlazma: updates to LoginReply
    This patch enforces the getSessionAttributes method always returns a non-null Set.  
    Client code then doesn't need to handle the case when it returns null.  It also 
    protects the internal storage Set by returning a read-only view of the Set.
    
    The patch also provides a couple of methods for extracting SessionAttribute values by 
    name and by type.
    
    Acked-by: Timur
    Acked-by: Thomas
    Patch: http://rb.dcache.org/r/1858/
    Target: trunk

--- modules/gPlazma/src/org/dcache/gplazma/LoginReply.java
+++ modules/gPlazma/src/org/dcache/gplazma/LoginReply.java
@@ -1,5 +1,7 @@
 package org.dcache.gplazma;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.security.auth.Subject;
@@ -10,7 +12,7 @@ import javax.security.auth.Subject;
  */
 public class LoginReply {
     private Subject _subject;
-    private Set<SessionAttribute> _sessionAttributes;
+    private Set<SessionAttribute> _sessionAttributes = Collections.emptySet();
 
     public void setSubject(Subject subject) {
         _subject = subject;
@@ -21,10 +23,38 @@ public class LoginReply {
     }
 
     public void setSessionAttributes(Set<SessionAttribute> sessionAttributes) {
+        if( sessionAttributes == null) {
+            throw new IllegalArgumentException("Not allowed to pass null as a parameter");
+        }
+
         _sessionAttributes = sessionAttributes;
     }
 
     public Set<SessionAttribute> getSessionAttributes() {
-        return _sessionAttributes;
+        return Collections.unmodifiableSet( _sessionAttributes);
+    }
+
+    public Set<SessionAttribute> getSessionAttributeByName( String name) {
+        Set<SessionAttribute> resultSet = new HashSet<SessionAttribute>();
+
+        for( SessionAttribute attribute : _sessionAttributes) {
+            if( name.equals( attribute.getName())) {
+                resultSet.add( attribute);
+            }
+        }
+
+        return resultSet;
+    }
+
+    public Set<SessionAttribute> getSessionAttributesByType( Class<? extends SessionAttribute> type) {
+        Set<SessionAttribute> resultSet = new HashSet<SessionAttribute>();
+
+        for( SessionAttribute attribute : _sessionAttributes) {
+            if( type.isInstance( attribute)) {
+                resultSet.add( attribute);
+            }
+        }
+
+        return resultSet;
     }
 }
