23805a2f868a792deea9b1d7ed225581558f31d0
Gerd Behrmann <behrmann@nordu.net>
2010-06-02 09:26:07 +0000

    cells: Implement escaping and quoting in argument parser
    The argument parser of cells had rudimentary support for qouting, but
    not for escaping. This means it was not possible to include quote
    symbols within quoted strings, among other things.
    
    This patch replaces the argument parser of cells. The new parser is a
    recursive decent parser. It implements single and double quoting,
    backslash escaping and the end of options double dash sequence.
    
    The toString method of Args is updated to escape special characters.
    This makes it possible to parse the output of toString and obtain an
    equivalent instance of Args.
    
    This patch is a prerequisite to another patch for CellShell that will
    make it easier to include spaces in cell arguments.
    
    Testing done: Verified that dCache starts and that transfer tests
    succeed. Unit tests pass.
    
    Target: trunk
    Acked-by: Paul Millar <paul.millar@desy.de>
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: http://rb.dcache.org/r/1913/

--- modules/cells/dmg/util/Args.java
+++ modules/cells/dmg/util/Args.java
@@ -3,6 +3,9 @@ package dmg.util ;
 import java.util.* ;
 import java.io.Serializable;
 
+/**
+ * TODO: Write JavaDoc!
+ */
 public class Args
     implements Serializable, Cloneable
 {
@@ -11,10 +14,10 @@ public class Args
    private final Map<String, String> _optHash = CollectionFactory.newHashMap();
    private final List<String>    _optv  = new Vector<String>();
    private final List<String>    _argv  = new Vector<String>();
-   private String    _oneChar = null ;
+   private String _oneChar;
    public Args( String args ) {
 
-      scanLine( args ) ;
+       new Scanner(args).scan();
    }
    public Args( String [] args ) {
 
@@ -22,13 +25,13 @@ public class Args
       for( int i = 0 ; i < args.length ; i++ )
          sb.append(args[i]).append(" ");
 
-      scanLine( sb.toString() ) ;
+      new Scanner(sb).scan();
    }
    Args( Args in ){
      _argv.addAll(in._argv);
      _optv.addAll( in._optv );
      _optHash.putAll( in._optHash );
-     _line = in._line ;
+     _oneChar = in._oneChar;
    }
    public boolean isOneCharOption( char c ){
      return _oneChar.indexOf(c) > -1 ;
@@ -70,6 +73,53 @@ public class Args
 
    public Object clone(){ return new Args( this ) ; }
 
+    public boolean equals(Object other)
+    {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof Args)) {
+            return false;
+        }
+
+        Args args = (Args) other;
+        return args._optHash.equals(_optHash)
+            && args._optv.equals(_optv)
+            && args._argv.equals(_argv)
+            && args._oneChar.equals(_oneChar);
+    }
+
+    public int hashCode()
+    {
+        return _optHash.hashCode() ^ _argv.hashCode();
+    }
+
+    private void quote(String in, StringBuilder out)
+    {
+        for (int i = 0; i < in.length(); i++) {
+            switch (in.charAt(i)) {
+            case '\\':
+                out.append("\\\\");
+                break;
+            case '"':
+                out.append("\\\"");
+                break;
+            case '\'':
+                out.append("\\'");
+                break;
+            case '=':
+                out.append("\\=");
+                break;
+            case ' ':
+                out.append("\\ ");
+                break;
+            default:
+                out.append(in.charAt(i));
+                break;
+            }
+        }
+    }
+
     public String toString()
     {
         StringBuilder s = new StringBuilder();
@@ -77,16 +127,23 @@ public class Args
         for (Map.Entry<String,String> e: _optHash.entrySet()) {
             String key = e.getKey();
             String value = e.getValue();
+
+            s.append('-');
+            quote(key, s);
             if (value.length() > 0) {
-                s.append('-').append(key).append('=').append(value);
-            } else {
-                s.append('-').append(key);
+                s.append('=');
+                quote(value, s);
             }
             s.append(' ');
         }
 
+        if (s.length() > 0) {
+            s.append("-- ");
+        }
+
         for (int i = 0; i < argc(); i++) {
-            s.append(argv(i)).append(' ');
+            quote(argv(i), s);
+            s.append(' ');
         }
 
         return s.toString();
@@ -111,127 +168,7 @@ public class Args
 
       return sb.toString() ;
    }
-   private static final int IDLE          = 0 ;
-   private static final int PLAIN_STRING  = 1 ;
-   private static final int QUOTED_STRING = 2 ;
-   private static final int OPT_KEY       = 3 ;
-   private static final int OPT_VALUE     = 4 ;
-   private static final int OPT_QUOTED    = 5 ;
-   private static final int OPT_PLAIN     = 6 ;
 
-   private void undo( char r ){ _res = r ; _undo = true ; }
-
-   private boolean _undo    = false ;
-   private char    _res     = 0 ;
-   private int     _current = 0 ;
-   private String  _line    = null ;
-   private char nextChar() {
-      if( _undo ){ _undo = false  ; return _res ; }
-      else
-         return _current >= _line.length() ?
-                END_OF_INFO :
-                _line.charAt(_current++) ;
-
-   }
-
-   private final static char   END_OF_INFO = (char)-1 ;
-   private void scanLine( String line ){
-      _line = line ;
-      int  state = IDLE ;
-      char c ;
-      StringBuilder key = null , value = null ;
-      StringBuilder oneChar = new StringBuilder() ;
-      do{
-         c = nextChar() ;
-         switch( state ){
-            case IDLE :
-               if( ( c == END_OF_INFO ) || ( c == ' ' ) || ( c == '\t' ) ){
-                  // nothing to do
-               }else if( c == '"' ){
-                  state = QUOTED_STRING ;
-                  value = new StringBuilder() ;
-               }else if( c == '-' ){
-                  state = OPT_KEY ;
-                  key   = new StringBuilder() ;
-               }else{
-                  value = new StringBuilder() ;
-                  value.append(c);
-                  state = PLAIN_STRING ;
-               }
-            break ;
-            case PLAIN_STRING :
-               if( ( c == END_OF_INFO ) || ( c == ' ' ) || ( c == '\t' ) ){
-                  _argv.add( value.toString() ) ;
-                  state = IDLE ;
-               }else{
-                  value.append(c) ;
-               }
-            break ;
-            case QUOTED_STRING :
-               if( ( c == END_OF_INFO ) ||
-                   ( c == '"'         )    ){
-                  _argv.add( value.toString() ) ;
-                  state = IDLE ;
-               }else{
-                  value.append(c) ;
-               }
-            break ;
-            case OPT_KEY :
-               if( ( c == END_OF_INFO ) || ( c == ' ' ) || ( c == '\t' ) ){
-                  if( key.length() != 0 ){
-                     _optv.add(key.toString()) ;
-                     _optHash.put( key.toString() , "" ) ;
-                     oneChar.append(key.toString());
-                  }
-                  state = IDLE ;
-               }else if( c == '=' ){
-                  value = new StringBuilder() ;
-                  state = OPT_VALUE ;
-               }else{
-                  key.append(c) ;
-               }
-            break ;
-            case OPT_VALUE :
-               if( ( c == END_OF_INFO ) || ( c == ' ' ) || ( c == '\t' ) ){
-                  if( key.length() != 0 ){
-                     _optv.add(key.toString()) ;
-                     _optHash.put( key.toString() , "" ) ;
-                  }
-                  state = IDLE ;
-               }else if( c == '"' ){
-                  value = new StringBuilder() ;
-                  state = OPT_QUOTED ;
-               }else{
-                  state = OPT_PLAIN ;
-                  value = new StringBuilder() ;
-                  value.append(c) ;
-               }
-            break ;
-            case OPT_QUOTED :
-               if( ( c == END_OF_INFO ) || ( c == '"' ) ){
-                  _optv.add( key.toString() ) ;
-                  _optHash.put( key.toString() , value.toString() ) ;
-                  state =IDLE ;
-               }else{
-                  value.append(c) ;
-               }
-            break ;
-            case OPT_PLAIN :
-               if( ( c == END_OF_INFO ) || ( c == ' ' ) || ( c == '\t' ) ){
-                  _optv.add( key.toString() ) ;
-                  _optHash.put( key.toString() , value.toString() ) ;
-                  state =IDLE ;
-               }else{
-                  value.append(c) ;
-               }
-            break ;
-
-
-         }
-      }while( c != END_OF_INFO ) ;
-      _oneChar = oneChar.toString() ;
-
-   }
    public static void main( String [] args )throws Exception {
       if( args.length < 1 ){
          System.err.println( "Usage : ... <parseString>" ) ;
@@ -246,4 +183,199 @@ public class Args
       System.out.println( "pvr="+lineArgs.getOpt( "pvr" ) ) ;
 
    }
+
+    /**
+     * Scanner for parsing strings of white space separated
+     * words. Characters may be escaped with a backslash and character
+     * sequences may be quoted. Options begin with an unescaped dash.
+
+     * A -- signals the end of options and disables further option
+     * processing.  Any arguments after the -- are treated as regular
+     * arguments.
+     */
+    class Scanner
+    {
+        private final CharSequence _line;
+        private int _position;
+
+        public Scanner(CharSequence line)
+        {
+            _line = line;
+        }
+
+        private char peek()
+        {
+            return isEof() ? (char) 0 : _line.charAt(_position);
+        }
+
+        private char readChar()
+        {
+            char c = peek();
+            _position++;
+            return c;
+        }
+
+        private boolean isEof()
+        {
+            return (_position >= _line.length());
+        }
+
+        private boolean isWhitespace()
+        {
+            return Character.isWhitespace(peek());
+        }
+
+        private void scanWhitespace()
+        {
+            while (isWhitespace()) {
+                readChar();
+            }
+        }
+
+        public void scan()
+        {
+            StringBuilder oneChar = new StringBuilder();
+            boolean isAtEndOfOptions = false;
+            scanWhitespace();
+            while (!isEof()) {
+                if (!isAtEndOfOptions && peek() == '-') {
+                    readChar();
+                    String key = scanKey();
+                    if (key.isEmpty()) {
+                        _argv.add("-");
+                    } else if (peek() == '=') {
+                        readChar();
+                        _optv.add(key) ;
+                        _optHash.put(key, scanWord());
+                    } else if (key.equals("-")) {
+                        isAtEndOfOptions = true;
+                    } else {
+                        _optv.add(key) ;
+                        _optHash.put(key, "");
+                        oneChar.append(key);
+                    }
+                } else {
+                    _argv.add(scanWord());
+                }
+                scanWhitespace();
+            }
+            _oneChar = oneChar.toString();
+        }
+
+        /**
+         * Scans an option key. An option key is terminated by an
+         * unescaped white space character or - for non-empty keys -
+         * by an unescaped equal sign.
+         */
+        private String scanKey()
+        {
+            StringBuilder key = new StringBuilder();
+            do {
+                scanWordElement(key);
+            } while (!isEof() && !isWhitespace() && peek() != '=');
+            return key.toString();
+        }
+
+        /**
+         * Scans the next word. A word is a sequence of non-white
+         * space characters and escaped or quoted white space
+         * characters. The unescaped and unquoted word is returned.
+         */
+        private String scanWord()
+        {
+            StringBuilder word = new StringBuilder();
+            while (!isEof() && !isWhitespace()) {
+                scanWordElement(word);
+            }
+            return word.toString();
+        }
+
+        /**
+         * Scans the next element of a word. Elements of a word are
+         * non-white space characters, escaped characters and quoted
+         * strings. The unescaped and unquoted element is added to word.
+         */
+        private void scanWordElement(StringBuilder word)
+        {
+            if (!isEof() && !isWhitespace()) {
+                switch (peek()) {
+                case '\'':
+                    scanSingleQuotedString(word);
+                    break;
+                case '"':
+                    scanDoubleQuotedString(word);
+                    break;
+                case '\\':
+                    scanEscapedCharacter(word);
+                    break;
+                default:
+                    word.append(readChar());
+                    break;
+                }
+            }
+        }
+
+        /**
+         * Scans a single quoted string. Escaped characters are not
+         * recognized. The unquoted string is added to word.
+         */
+        private void scanSingleQuotedString(StringBuilder word)
+        {
+            if (readChar() != '\'') {
+                throw new IllegalStateException("Parse failure");
+            }
+
+            while (!isEof()) {
+                char c = readChar();
+                switch (c) {
+                case '\'':
+                    return;
+                default:
+                    word.append(c);
+                    break;
+                }
+            }
+        }
+
+        /**
+         * Scans a double quoted string. Escaped characters are
+         * recognized. The unquoted and unescaped string is added to
+         * word.
+         */
+        private void scanDoubleQuotedString(StringBuilder word)
+        {
+            if (readChar() != '"') {
+                throw new IllegalStateException("Parse failure");
+            }
+
+            while (!isEof()) {
+                switch (peek()) {
+                case '\\':
+                    scanEscapedCharacter(word);
+                    break;
+                case '"':
+                    readChar();
+                    return;
+                default:
+                    word.append(readChar());
+                    break;
+                }
+            }
+        }
+
+        /**
+         * Scans a backslash escaped character. The escaped character
+         * without the escape symbol is added to word.
+         */
+        private void scanEscapedCharacter(StringBuilder word)
+        {
+            if (readChar() != '\\') {
+                throw new IllegalStateException("Parse failure");
+            }
+
+            if (!isEof()) {
+                word.append(readChar());
+            }
+        }
+    }
 }
--- modules/dCacheJUnit/org/dcache/tests/util/ArgsTest.java
+++ modules/dCacheJUnit/org/dcache/tests/util/ArgsTest.java
@@ -61,5 +61,305 @@ public class ArgsTest {
 
     }
 
+    @Test
+    public void testLocationManager()
+    {
+        Args args = new Args("do -serial=118 * nl d:dCacheDomain c:dCacheDomain");
+        assertEquals(5, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("do", args.argv(0));
+        assertEquals("*", args.argv(1));
+        assertEquals("nl", args.argv(2));
+        assertEquals("d:dCacheDomain", args.argv(3));
+        assertEquals("c:dCacheDomain", args.argv(4));
+        assertEquals("118", args.getOpt("serial"));
 
+        assertEquals(args, new Args(args.toString()));
+    }
+
+    @Test
+    public void testDoubleQuoteArgument()
+    {
+        Args args = new Args("-foo \"bar\"");
+        assertEquals(1, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("bar", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testDoubleQuoteArgumentWithEscape()
+    {
+        Args args = new Args("-foo \"b\\\"a\\\"r\"");
+        assertEquals(1, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("b\"a\"r", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testDoubleQuoteArgumentWithSingleQuote()
+    {
+        Args args = new Args("-foo \"bar''\"");
+        assertEquals(1, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("bar''", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testDoubleQuoteInsideArgument()
+    {
+        Args args = new Args("-foo b\"a\"r");
+        assertEquals(1, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("bar", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testDoubleQuoteOption()
+    {
+        Args args = new Args("\"-foo\" bar");
+        assertEquals(2, args.argc());
+        assertEquals("-foo", args.argv(0));
+        assertEquals("bar", args.argv(1));
+        assertFalse(args.isOneCharOption('-'));
+        assertFalse(args.isOneCharOption('f'));
+        assertFalse(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testDoubleQuoteOptionValue()
+    {
+        Args args = new Args("-foo=\"bar bar\"");
+        assertEquals(0, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("bar bar", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertFalse(args.isOneCharOption('f'));
+        assertFalse(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testSingleQuoteArgument()
+    {
+        Args args = new Args("-foo 'bar'");
+        assertEquals(1, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("bar", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testSingleQuoteArgumentWithEscape()
+    {
+        Args args = new Args("-foo 'b\\\"a\\\"r'");
+        assertEquals(1, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("b\\\"a\\\"r", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testSingleQuoteArgumentWithDoubleQuote()
+    {
+        Args args = new Args("-foo 'bar\"\"'");
+        assertEquals(1, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("bar\"\"", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testSingleQuoteInsideArgument()
+    {
+        Args args = new Args("-foo b'a'r");
+        assertEquals(1, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("bar", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testSingleQuoteOption()
+    {
+        Args args = new Args("'-foo' bar");
+        assertEquals(2, args.argc());
+        assertEquals(0, args.optc());
+        assertEquals("-foo", args.argv(0));
+        assertEquals("bar", args.argv(1));
+        assertFalse(args.isOneCharOption('-'));
+        assertFalse(args.isOneCharOption('f'));
+        assertFalse(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testSingleQuoteOptionValue()
+    {
+        Args args = new Args("-foo='bar bar'");
+        assertEquals(0, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("bar bar", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertFalse(args.isOneCharOption('f'));
+        assertFalse(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testEscapedOption()
+    {
+        Args args = new Args("\\-foo");
+        assertEquals(1, args.argc());
+        assertEquals(0, args.optc());
+        assertEquals("-foo", args.argv(0));
+        assertFalse(args.isOneCharOption('-'));
+        assertFalse(args.isOneCharOption('f'));
+        assertFalse(args.isOneCharOption('o'));
+    }
+
+    @Test
+    public void testEscapedSpaceOption()
+    {
+        Args args = new Args("-foo=bar\\ bar");
+        assertEquals(0, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("bar bar", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertFalse(args.isOneCharOption('f'));
+        assertFalse(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+    }
+
+    @Test
+    public void testEscapedSpaceArgument()
+    {
+        Args args = new Args("bar\\ bar");
+        assertEquals(1, args.argc());
+        assertEquals(0, args.optc());
+        assertEquals("bar bar", args.argv(0));
+    }
+
+    @Test
+    public void testEscapedBackslash()
+    {
+        Args args = new Args("bar\\\\bar");
+        assertEquals(1, args.argc());
+        assertEquals(0, args.optc());
+        assertEquals("bar\\bar", args.argv(0));
+    }
+
+    @Test
+    public void testTrailingEscape()
+    {
+        Args args = new Args("bar\\");
+        assertEquals(1, args.argc());
+        assertEquals(0, args.optc());
+        assertEquals("bar", args.argv(0));
+    }
+
+    @Test
+    public void testEmptyOptionKey()
+    {
+        Args args = new Args("-");
+        assertEquals(1, args.argc());
+        assertEquals(0, args.optc());
+        assertEquals("-", args.argv(0));
+        assertFalse(args.isOneCharOption('-'));
+    }
+
+    @Test
+    public void testTrailingHyphen()
+    {
+        Args args = new Args("aa bb -");
+        assertEquals(3, args.argc());
+        assertEquals(0, args.optc());
+        assertEquals("aa", args.argv(0));
+        assertEquals("bb", args.argv(1));
+        assertEquals("-", args.argv(2));
+        assertFalse(args.isOneCharOption('-'));
+    }
+
+    @Test
+    public void testOptionKeyStartingWithAssignment()
+    {
+        Args args = new Args("-=bar");
+        assertEquals(0, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("", args.getOpt("=bar"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('='));
+        assertTrue(args.isOneCharOption('b'));
+        assertTrue(args.isOneCharOption('a'));
+        assertTrue(args.isOneCharOption('r'));
+    }
+
+    @Test
+    public void testEndOfOptions()
+    {
+        Args args = new Args("-foo -- -bar");
+        assertEquals(1, args.argc());
+        assertEquals(1, args.optc());
+        assertEquals("-bar", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertFalse(args.isOneCharOption('b'));
+        assertFalse(args.isOneCharOption('a'));
+        assertFalse(args.isOneCharOption('r'));
+    }
+
+    @Test
+    public void testEscapedEndOfOptions()
+    {
+        Args args = new Args("-foo \\-- -bar");
+        assertEquals(1, args.argc());
+        assertEquals(2, args.optc());
+        assertEquals("--", args.argv(0));
+        assertEquals("", args.getOpt("foo"));
+        assertEquals("", args.getOpt("bar"));
+        assertFalse(args.isOneCharOption('-'));
+        assertTrue(args.isOneCharOption('f'));
+        assertTrue(args.isOneCharOption('o'));
+        assertTrue(args.isOneCharOption('b'));
+        assertTrue(args.isOneCharOption('a'));
+        assertTrue(args.isOneCharOption('r'));
+    }
 }
