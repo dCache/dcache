4c7717b224a3cf65e8eae1c64495e0f3c123f688
Paul Millar <paul.millar@desy.de>
2010-06-08 12:58:11 +0000

    gplazma: add plugin XML metadata parser
    As part of the loader, gPlazma will use XML data to determine static information about 
    plugins.  This information includes the names of plugins, the class that implements the 
    plugin and potentially other information.
    
    This patch adds support for parsing this XML data.  It also includes tests to verify correct 
    behaviour.
    
    Patch is for trunk.
    
    Patch: http://rb.dcache.org/r/1919/
    Acked-by: Timur
    Target: trunk

---
+++ modules/gPlazma/junit/org/dcache/gplazma/loader/XmlParserTests.java
@@ -0,0 +1,354 @@
+package org.dcache.gplazma.loader;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.dcache.gplazma.plugins.GPlazmaPlugin;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public class XmlParserTests {
+    private static final TransformerFactory TRANSFORMER_FACTORY =
+            TransformerFactory.newInstance();
+    private static final DocumentBuilderFactory BUILDER_FACTORY =
+            DocumentBuilderFactory.newInstance();
+
+    private Document _emptyDocument;
+
+    private Document _document;
+    private Node _pluginsElement;
+
+    private Set<PluginMetadata> _plugins;
+
+    @Before
+    public void setUp() throws ParserConfigurationException {
+        DocumentBuilder builder = BUILDER_FACTORY.newDocumentBuilder();
+
+        _emptyDocument = builder.newDocument();
+
+        _document = builder.newDocument();
+        _pluginsElement = _document.createElement( "plugins");
+        _document.appendChild( _pluginsElement);
+    }
+
+    @Test
+    public void testEmpty() {
+        // Note: as the XML is not well-formed, the support library generates
+        // output on stderr. It's unclear how to suppress this.
+        parseAndGetPlugins( _emptyDocument);
+        assertEquals( "check plugins empty", _plugins, Collections.emptySet());
+    }
+
+    @Test
+    public void testBrokenXml() {
+        // Note: as the XML is not well-formed, the support library generates
+        // output on stderr. It's unclear how to suppress this.
+        parseAndGetPlugins( "<plugins>");
+        assertEquals( "check plugins empty", _plugins, Collections.emptySet());
+    }
+
+    @Test
+    public void testXmlWithExtraField() {
+        parseAndGetPlugins( "<plugins><plugin><class>org.dcache.gplazma.loader.XmlParserTests$DummyPlugin</class><name>test</name><fruit>banana</fruit></plugin></plugins>");
+
+        assertEquals( "check plugin count", 1, _plugins.size());
+        PluginMetadata plugin = _plugins.iterator().next();
+
+        assertPluginMetadata( plugin, Collections.singleton( "test"),
+                DummyPlugin.class, "test", null);
+    }
+
+    @Test
+    public void testOnlyRoot() {
+        parseAndGetPlugins();
+        assertEquals( "check plugins empty", _plugins, Collections.emptySet());
+    }
+
+    @Test
+    public void testPluginsAndEmptyPlugin() {
+        addEmptyPlugin();
+
+        parseAndGetPlugins();
+
+        assertEquals( "check plugins empty", _plugins, Collections.emptySet());
+    }
+
+    @Test
+    public void testPluginsAndValidPluginWithSingleName() {
+        String pluginName = "foo";
+        Set<String> names = Collections.singleton( pluginName);
+        addPlugin( names, DummyPlugin.class.getName());
+
+        parseAndGetPlugins();
+
+        assertEquals( "check plugin count", 1, _plugins.size());
+
+        PluginMetadata plugin = _plugins.iterator().next();
+
+        assertPluginMetadata( plugin, names, DummyPlugin.class, pluginName,
+                null);
+    }
+
+    @Test
+    public void testPluginsAndValidPluginWithMultipleNames() {
+        String pluginShortestName = "foo";
+        Set<String> names =
+                new HashSet<String>( Arrays.asList( pluginShortestName,
+                        "foo-o-matic", "original-foo"));
+
+        addPlugin( names, DummyPlugin.class.getName());
+
+        parseAndGetPlugins();
+
+        assertEquals( "check plugin count", 1, _plugins.size());
+
+        PluginMetadata plugin = _plugins.iterator().next();
+
+        assertPluginMetadata( plugin, names, DummyPlugin.class,
+                pluginShortestName, null);
+    }
+
+    @Test
+    public void testPluginsAndTwoValidPlugins() {
+        String plugin1Name = "foo";
+        Set<String> plugin1Names = Collections.singleton( plugin1Name);
+        addPlugin( plugin1Names, DummyPlugin.class.getName());
+
+        String plugin2Name = "bar";
+        Set<String> plugin2Names = Collections.singleton( plugin2Name);
+        addPlugin( plugin2Names, AnotherDummyPlugin.class.getName());
+
+        parseAndGetPlugins();
+
+        assertEquals( "check plugin count", 2, _plugins.size());
+
+        Map<String, PluginMetadata> results =
+                new HashMap<String, PluginMetadata>();
+        for( PluginMetadata plugin : _plugins) {
+            results.put( plugin.getShortestName(), plugin);
+        }
+
+        PluginMetadata plugin1Result = results.get( plugin1Name);
+        assertPluginMetadata( plugin1Result, plugin1Names, DummyPlugin.class,
+                plugin1Name, null);
+
+        PluginMetadata plugin2Result = results.get( plugin2Name);
+        assertPluginMetadata( plugin2Result, plugin2Names,
+                AnotherDummyPlugin.class, plugin2Name, null);
+    }
+
+    @Test
+    public void testPluginWithDefaultControl() {
+        String pluginName = "foo";
+        Set<String> names = Collections.singleton( pluginName);
+        String defaultControl = "required";
+        addPlugin( names, DummyPlugin.class.getName(), defaultControl);
+
+        parseAndGetPlugins();
+
+        assertEquals( "check plugin count", 1, _plugins.size());
+
+        PluginMetadata plugin = _plugins.iterator().next();
+
+        assertPluginMetadata( plugin, names, DummyPlugin.class, pluginName,
+                defaultControl);
+    }
+
+    @Test
+    public void testTwoPluginsWithSameClassAcceptsNeither() {
+        String className = DummyPlugin.class.getName();
+
+        String plugin1Name = "foo";
+        Set<String> plugin1Names = Collections.singleton( plugin1Name);
+        addPlugin( plugin1Names, className);
+
+        String plugin2Name = "bar";
+        Set<String> plugin2Names = Collections.singleton( plugin2Name);
+        addPlugin( plugin2Names, className);
+
+        parseAndGetPlugins();
+
+        assertEquals( "check plugin count", 0, _plugins.size());
+    }
+
+    @Test
+    public void testThreePluginsWithSameClassAcceptsNone() {
+        String className = DummyPlugin.class.getName();
+
+        String plugin1Name = "foo";
+        Set<String> plugin1Names = Collections.singleton( plugin1Name);
+        addPlugin( plugin1Names, className);
+
+        String plugin2Name = "bar";
+        Set<String> plugin2Names = Collections.singleton( plugin2Name);
+        addPlugin( plugin2Names, className);
+
+        String plugin3Name = "baz";
+        Set<String> plugin3Names = Collections.singleton( plugin3Name);
+        addPlugin( plugin3Names, className);
+
+        parseAndGetPlugins();
+
+        assertEquals( "check plugin count", 0, _plugins.size());
+    }
+
+    @Test
+    public void testPluginWithTwoClassesNotAccepted() {
+        String class1Name = DummyPlugin.class.getName();
+        String class2Name = AnotherDummyPlugin.class.getName();
+
+        String xmlData = String.format(  "<plugins><plugin>"
+                                       + "<name>test</name>"
+                                       + "<class>%s</class>"
+                                       + "<class>%s</class>"
+                                       + "</plugin></plugins>",
+                                       class1Name, class2Name);
+        parseAndGetPlugins( xmlData);
+
+        assertEquals( "check plugin count", 0, _plugins.size());
+    }
+
+    /*
+     * COMPOUND ASSERTIONS
+     */
+
+    private void assertPluginMetadata( PluginMetadata plugin,
+                                       Set<String> assignedNames,
+                                       Class<?> expectedClass,
+                                       String expectedShortestName,
+                                       String expectedDefaultControl) {
+        assertNotNull( plugin);
+        assertTrue( plugin.isValid());
+
+        // We always add the class name to the list of names
+        Set<String> expectedNames = new HashSet<String>( assignedNames);
+        expectedNames.add( expectedClass.getName());
+
+        assertEquals( expectedNames, plugin.getPluginNames());
+        assertEquals( expectedClass, plugin.getPluginClass());
+        assertEquals( expectedShortestName, plugin.getShortestName());
+        assertEquals( expectedDefaultControl, plugin.getDefaultControl());
+    }
+
+    /*
+     * SUPPORT METHODS
+     */
+
+    private void addEmptyPlugin() {
+        Set<String> names = Collections.emptySet();
+        addPlugin( names);
+    }
+
+    private void addPlugin( Set<String> names) {
+        addPlugin( names, null);
+    }
+
+    private void addPlugin( Set<String> names, String pluginClass) {
+        addPlugin( names, pluginClass, null);
+    }
+
+    private void addPlugin( Set<String> names, String pluginClass,
+                            String defaultControl) {
+        Node pluginNode = _document.createElement( "plugin");
+        _pluginsElement.appendChild( pluginNode);
+
+        for( String name : names) {
+            addTextElement( pluginNode, "name", name);
+        }
+
+        if( pluginClass != null) {
+            addTextElement( pluginNode, "class", pluginClass);
+        }
+
+        if( defaultControl != null) {
+            addTextElement( pluginNode, "default-control", defaultControl);
+        }
+    }
+
+    private void addTextElement( Node parentNode, String localName,
+                                 String contents) {
+        Node childNode = _document.createElement( localName);
+        Node textNode = _document.createTextNode( contents);
+        childNode.appendChild( textNode);
+        parentNode.appendChild( childNode);
+    }
+
+    private void parseAndGetPlugins() {
+        parseAndGetPlugins( _document);
+    }
+
+    private void parseAndGetPlugins( Document document) {
+        String xmlData = serialiseXml( document);
+        parseAndGetPlugins( xmlData);
+    }
+
+    private String serialiseXml( Document document) {
+        Source source = new DOMSource( document);
+
+        StringWriter writer = new StringWriter();
+        Result result = new StreamResult( writer);
+
+        Transformer identityTransformer;
+        try {
+            identityTransformer = TRANSFORMER_FACTORY.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            throw new RuntimeException( "Unable to create identity transformation",
+                                        e);
+        }
+
+        try {
+            identityTransformer.transform( source, result);
+        } catch (TransformerException e) {
+            throw new RuntimeException( "Identity transformation failed", e);
+        }
+
+        return writer.toString();
+    }
+
+    private void parseAndGetPlugins( String xmlData) {
+        Reader xmlSrc = new StringReader( xmlData);
+        XmlParser parser = new XmlParser( xmlSrc);
+        parser.parse();
+        _plugins = parser.getPlugins();
+    }
+
+    /**
+     * Dummy implementation of a GPlazmaPlugin
+     */
+    public static final class DummyPlugin implements GPlazmaPlugin {
+        // no content as the class isn't meant to be used.
+    }
+
+    /**
+     * Another dummy implementation of a GPlazmaPlugin
+     */
+    public static final class AnotherDummyPlugin implements GPlazmaPlugin {
+        // no content as the class isn't meant to be used.
+    }
+
+}
---
+++ modules/gPlazma/src/org/dcache/gplazma/loader/XmlParser.java
@@ -0,0 +1,320 @@
+package org.dcache.gplazma.loader;
+
+import java.io.Reader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.dcache.gplazma.plugins.GPlazmaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXParseException;
+
+/**
+ * Class that parses the XML configuration file and returns useful
+ * information. Plugin XML metadata has a root <tt>plugins</tt> element and
+ * zero or more <tt>plugin</tt> child elements. Each <tt>plugin</tt> element
+ * has metadata for that plugin as child elements of that <tt>plugin</tt>
+ * element.
+ * <p>
+ * The following is a list of possible child elements (their local-name
+ * values) along with the cardinality and a brief description.
+ * <table>
+ * <tr>
+ * <th>local-name</th>
+ * <th>cardinality</th>
+ * <th>description</th>
+ * </tr>
+ * <tr>
+ * <td>name</td>
+ * <td>1..*</td>
+ * <td>a name that may be used for this plugin. Names must be unique</td>
+ * </tr>
+ * <tr>
+ * <td>class</td>
+ * <td>1</td>
+ * <td>the class name for this plugin (the result of
+ * <code>Class.getName</code>)</td>
+ * </tr>
+ * <tr>
+ * <td>default-control</td>
+ * <td>0..1</td>
+ * <td>currently unused</td>
+ * </tr>
+ * </table>
+ *
+ * The following is an example XML describing two plugins. One has a single
+ * name, the other has two names.
+ *
+ * <pre>
+ *     &lt;plugins>
+ *         &lt;plugin>
+ *             &lt;name>foo&lt;/name>
+ *             &lt;class>org.dcache.foo.plugins.plugin1&lt;/class>
+ *             &lt;default-control>required&lt;/default-control>
+ *         &lt;/plugin>
+ *         &lt;plugin>
+ *             &lt;name>bar&lt;/name>
+ *             &lt;name>bar-o-matic&lt;/name>
+ *             &lt;class>org.dcache.foo.plugins.plugin2&lt;/class>
+ *             &lt;default-control>optional&lt;/default-control>
+ *         &lt;/plugin>
+ *     &lt;/plugins>
+ * </pre>
+ *
+ * The parser will generate a set of PluginMetadata objects, one for each
+ * valid plugin described in the XML.
+ */
+public class XmlParser {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger( XmlParser.class);
+
+    private static final XPathFactory XPATH_FACTORY =
+            XPathFactory.newInstance();
+    private static final String XPATH_EXPRESSION_PLUGINS_PLUGIN =
+            "/plugins/plugin";
+
+    /**
+     * An enumeration of allowed XML elements that are children of the
+     * <tt>/plugins/plugin</tt> elements. The enumeration also holds the
+     * local-name of the elements and allows easy access to that information.
+     */
+    private enum XML_CHILD_NODE {
+        NAME("name"), CLASS("class"), DEFAULT_CONTROL("default-control");
+
+        static private Map<String, XML_CHILD_NODE> LOCAL_NAME_STORE =
+                new HashMap<String, XML_CHILD_NODE>();
+
+        private String _localName;
+
+        XML_CHILD_NODE( String localName) {
+            _localName = localName;
+        }
+
+        public String getLocalName() {
+            return _localName;
+        }
+
+        static {
+            for( XML_CHILD_NODE node : XML_CHILD_NODE.values()) {
+                LOCAL_NAME_STORE.put( node.getLocalName(), node);
+            }
+        }
+
+        static boolean hasLocalName( String localName) {
+            return LOCAL_NAME_STORE.containsKey( localName);
+        }
+
+        static XML_CHILD_NODE byLocalName( String localName) {
+            return LOCAL_NAME_STORE.get( localName);
+        }
+    }
+
+    private final InputSource _is;
+    private final Set<PluginMetadata> _plugins = new HashSet<PluginMetadata>();
+
+    /**
+     * If the same class is used by multiple plugin descriptions then we
+     * cannot know which plugin description is correct. Because of this, we
+     * remove all plugin descriptions that use this class.
+     */
+    private final Set<Class<? extends GPlazmaPlugin>> _bannedClasses =
+            new HashSet<Class<? extends GPlazmaPlugin>>();
+
+    public XmlParser( Reader source) {
+        _is = new InputSource( source);
+    }
+
+    public void parse() {
+        LOGGER.debug( "starting parse");
+        NodeList nodes = buildPluginNodeList();
+        LOGGER.debug( "NodeList has {} entries", nodes.getLength());
+        addPluginsFromNodeList( nodes);
+        LOGGER.debug( "Created {} plugin metadata entries", _plugins.size());
+    }
+
+    public Set<PluginMetadata> getPlugins() {
+        return Collections.unmodifiableSet( _plugins);
+    }
+
+    private NodeList buildPluginNodeList() {
+        XPath xpath = XPATH_FACTORY.newXPath();
+
+        XPathExpression expression;
+        try {
+            expression = xpath.compile( XPATH_EXPRESSION_PLUGINS_PLUGIN);
+        } catch (XPathExpressionException e) {
+            throw new RuntimeException( "Unable to compile XPath expression" +
+                                        XPATH_EXPRESSION_PLUGINS_PLUGIN, e);
+        }
+
+        Object result;
+        try {
+            result = expression.evaluate( _is, XPathConstants.NODESET);
+        } catch (XPathExpressionException e) {
+            Throwable genericCause = e.getCause();
+
+            if( genericCause instanceof org.xml.sax.SAXParseException) {
+                SAXParseException cause = (SAXParseException) genericCause;
+                LOGGER.error( "Unable to parse plugin metadata: [{},{}] {}",
+                        new Object[] { cause.getLineNumber(),
+                                      cause.getColumnNumber(),
+                                      cause.getMessage() });
+            } else {
+                LOGGER.error( "Unable to parse plugin metadata: {}",
+                        genericCause.getMessage());
+            }
+            return new EmptyNodeList();
+        }
+
+        NodeList nodes = (NodeList) result;
+
+        return nodes;
+    }
+
+    private void addPluginsFromNodeList( NodeList nodes) {
+        for( int i = 0; i < nodes.getLength(); i++) {
+            Node item = nodes.item( i);
+            tryToAddPluginFromNode( item);
+        }
+    }
+
+    private void tryToAddPluginFromNode( Node pluginRootNode) {
+        try {
+            PluginMetadata plugin = processPluginNode( pluginRootNode);
+            LOGGER.debug( "Adding plugin {}", plugin.getPluginNames());
+            _plugins.add( plugin);
+        } catch (IllegalArgumentException e) {
+            LOGGER.error( "Unable register new plugin: {}", e.getMessage());
+        }
+    }
+
+    private PluginMetadata processPluginNode( Node pluginRootNode) {
+        PluginMetadata metadata = new PluginMetadata();
+        addMetadataFromPluginNodeChildren( metadata, pluginRootNode);
+        validMetadataGuard( metadata);
+        return metadata;
+    }
+
+    private void validMetadataGuard( PluginMetadata metadata) {
+        if( !metadata.isValid()) {
+            throw new IllegalArgumentException( pluginName( metadata) +
+                                                " metadata is incomplete");
+        }
+
+        Class<? extends GPlazmaPlugin> thisPluginClass =
+                metadata.getPluginClass();
+        LOGGER.debug( "examining plugin with class {}", thisPluginClass);
+
+        if( removeIfClassAlreadyRegistered( thisPluginClass)) {
+            _bannedClasses.add( thisPluginClass);
+
+            throw new IllegalArgumentException( "Plugin '" +
+                                                metadata.getShortestName() +
+                                                "' uses class " +
+                                                thisPluginClass.getName() +
+                                                " which is already registered");
+        }
+
+        if( _bannedClasses.contains( thisPluginClass)) {
+            throw new IllegalArgumentException( "Plugin '" +
+                                                metadata.getShortestName() +
+                                                "' uses class " +
+                                                thisPluginClass.getName() +
+                                                " which was used by another plugin");
+        }
+    }
+
+    private boolean removeIfClassAlreadyRegistered( Class<? extends GPlazmaPlugin> pluginClass) {
+        Iterator<PluginMetadata> itr = _plugins.iterator();
+
+        while (itr.hasNext()) {
+            PluginMetadata registeredPlugin = itr.next();
+            Class<? extends GPlazmaPlugin> registeredPluginClass =
+                    registeredPlugin.getPluginClass();
+
+            LOGGER.debug("comparing plugin class {} against registered plugin with class {}",
+                         pluginClass, registeredPluginClass);
+
+            if( pluginClass.equals( registeredPluginClass)) {
+                itr.remove();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void addMetadataFromPluginNodeChildren( PluginMetadata metadata,
+                                                    Node pluginRootNode) {
+        NodeList childNodes = pluginRootNode.getChildNodes();
+
+        for( int i = 0; i < childNodes.getLength(); i++) {
+            Node childNode = childNodes.item( i);
+            addMetadataFromChildNode( metadata, childNode);
+        }
+    }
+
+    private void addMetadataFromChildNode( PluginMetadata metadata, Node node) {
+        String nodeName = node.getLocalName();
+        String value = node.getTextContent();
+
+        if( !XML_CHILD_NODE.hasLocalName( nodeName)) {
+            LOGGER.warn( pluginName( metadata) + ": ignoring unknown field " +
+                         nodeName);
+            return;
+        }
+
+        switch (XML_CHILD_NODE.byLocalName( nodeName)) {
+        case NAME:
+            metadata.addName( value);
+            break;
+        case CLASS:
+            metadata.setPluginClass( value);
+            break;
+        case DEFAULT_CONTROL:
+            metadata.setDefaultControl( value);
+            break;
+        }
+    }
+
+    private String pluginName( PluginMetadata metadata) {
+        String name;
+
+        if( metadata.hasPluginName()) {
+            name = "Plugin " + metadata.getShortestName();
+        } else {
+            name = "Plugin";
+        }
+
+        return name;
+    }
+
+    /**
+     * A trivial implementation of NodeList that is always empty.
+     */
+    private static class EmptyNodeList implements NodeList {
+        @Override
+        public int getLength() {
+            return 0;
+        }
+
+        @Override
+        public Node item( int index) {
+            return null;
+        }
+    }
+}
