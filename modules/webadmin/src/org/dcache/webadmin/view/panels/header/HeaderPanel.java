66a87ae864a59b5271168011080c3893e9861ee7
Jan Schaefer <jan.schaefer@desy.de>
2010-06-17 12:34:30 +0000

    Webadmin: Create a reusable Header Panel for all pages. This is a reusable component for every page. It will be put into use within another patch.
    http://rb.dcache.org/r/2005/
    
    acked by Thomas

---
+++ modules/webadmin/src/org/dcache/webadmin/view/panels/header/HeaderPanel.java
@@ -0,0 +1,14 @@
+package org.dcache.webadmin.view.panels.header;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * The Header of a Page
+ * @author jans
+ */
+public class HeaderPanel extends Panel {
+
+    public HeaderPanel(String id) {
+        super(id);
+    }
+}
