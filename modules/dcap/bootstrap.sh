df9663e14c2f74297d94c93338b1897888cf344b
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2010-07-08 15:40:07 +0000

    libdcap: do not create bunch of empty files as a documentation.
    The current bootstrap.sh creates a bunch of empty files, that are later
    installed as documentation. Installing empty documentation files does
    not make sense.
    
    Patch: http://rb.dcache.org/r/1645
    Acked-By: Tigran
    Signed-off-by: Mattias Ellert

--- modules/dcap/bootstrap.sh
+++ modules/dcap/bootstrap.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -x
 mkdir -p config
-touch AUTHORS ChangeLog NEWS README COPYING
 aclocal -I config
 #aclocal-1.10 -I config
 autoheader
 libtoolize --automake
-automake  --add-missing --copy
+automake  --add-missing --copy --foreign
 #automake-1.10  --add-missing --copy
 autoconf
 
