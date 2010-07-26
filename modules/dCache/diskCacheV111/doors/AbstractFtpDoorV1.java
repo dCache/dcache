5a684d93daba0d693054ea6b65d4cbed572f446d
Paul Millar <paul.millar@desy.de>
2010-07-26 16:30:47 +0000

    ftp and pnfsmanager: Check file type on list
    PermissionHandlerNameSpaceProvider checks whether the caller is allowed
    to list a directory. It does however not check whether the name space
    entry is actually a directory (in contrast to a file). Thus if one
    attempts to list a file and the permission check fails, then the wrong
    error message is generated. The wrong error message is then propagated
    to the client.
    
    Debugging this, we also found the error message generated by the FTP
    door for NLST and MLSD command when performed on files to be rather
    misleading. The error message was updated.
    
    This bug causes compatibility problems with ARC clients and the fix is 
    thus proposed for merge into stable branches.
    
    Patch: http://rb.dcache.org/r/1924/
    Acked-by: Paul
    Target: trunk (14042)
    Target: 1.9.8 (proposed)
    Target: 1.9.7 (proposed)
    Target: 1.9.6 (proposed)
    Target: 1.9.5 (proposed)
    Request: http://rt.dcache.org/Ticket/Display.html?id=5660

--- modules/dCache/diskCacheV111/doors/AbstractFtpDoorV1.java
+++ modules/dCache/diskCacheV111/doors/AbstractFtpDoorV1.java
@@ -3751,7 +3751,7 @@ public abstract class AbstractFtpDoorV1
         } catch (FileNotFoundCacheException e) {
             reply("550 Directory not found");
         } catch (NotDirCacheException e) {
-            reply("550 Directory not found");
+            reply("550 Not a directory");
         } catch (PermissionDeniedCacheException e) {
             reply("550 Permission denied");
         } catch (CacheException e) {
@@ -3853,7 +3853,7 @@ public abstract class AbstractFtpDoorV1
         } catch (FileNotFoundCacheException e) {
             reply("501 Directory not found");
         } catch (NotDirCacheException e) {
-            reply("501 Directory not found");
+            reply("501 Not a directory");
         } catch (PermissionDeniedCacheException e) {
             reply("550 Permission denied");
         } catch (CacheException e) {
