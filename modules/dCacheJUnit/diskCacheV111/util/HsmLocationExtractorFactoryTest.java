ae63b88506eb41e7343ea786d2a972b9565ad80c
Paul Millar <paul.millar@desy.de>
2010-08-24 13:56:07 +0000

    add a couple of extra unit-tests for documented example HSM URIs
    The wiki page:
    
        http://trac.dcache.org/projects/dcache/wiki/manuals/MultiHsmSupport
    
    contains a couple of example HSM URIs.  This patch adds them to the unit tests.
    
    Target: trunk
    Patch: http://rb.dcache.org/r/2243/
    Acked-by: Tigran

--- modules/dCacheJUnit/diskCacheV111/util/HsmLocationExtractorFactoryTest.java
+++ modules/dCacheJUnit/diskCacheV111/util/HsmLocationExtractorFactoryTest.java
@@ -55,4 +55,14 @@ public class HsmLocationExtractorFactoryTest {
     public void testValidateOk() throws Exception {
         HsmLocationExtractorFactory.validate(new URI("osm://osm/?group=1&store=2&bfid=3"));
     }
+
+    @Test
+    public void testExample1ValidateOk() throws Exception {
+        HsmLocationExtractorFactory.validate(new URI("osm://desy-main/?store=sql&group=chimera&bfid=3434.0.994.1188400818542"));
+    }
+
+    @Test
+    public void testExample2ValidateOk() throws Exception {
+        HsmLocationExtractorFactory.validate(new URI("osm://desy-dup/?store=sql&group=chimera&bfid=3434.0.994.1188400818542"));
+    }
 }
