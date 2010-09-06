2d7cd92a731c5c4d2fa2fd9b76daf2bb7099996b
Jan Schaefer <jan.schaefer@desy.de>
2010-09-06 07:45:58 +0000

    Webadmin: Adding a login and a logout link to webadmin-homepage
    http://rb.dcache.org/r/2362/
    
    acked by Tigran

--- modules/webadmin/src/org/dcache/webadmin/view/pages/dcacheservices/DCacheServices.java
+++ modules/webadmin/src/org/dcache/webadmin/view/pages/dcacheservices/DCacheServices.java
@@ -1,7 +1,13 @@
 package org.dcache.webadmin.view.pages.dcacheservices;
 
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.dcache.webadmin.view.pages.basepage.BasePage;
+import org.dcache.webadmin.view.pages.login.LogIn;
+import org.dcache.webadmin.view.util.CustomLink;
 
 /**
  * Main overview of all dCache-Services
@@ -10,7 +16,27 @@ import org.dcache.webadmin.view.pages.basepage.BasePage;
 public class DCacheServices extends BasePage {
 
     public DCacheServices() {
+        add(new FeedbackPanel("feedback"));
         add(new Label("dCacheInstanceName",
                 getWebadminApplication().getDcacheName()));
+        Link login = new CustomLink("loginLink", LogIn.class);
+        login.add(new Image("loginImage", new ResourceReference(
+                DCacheServices.class, "login.gif")));
+        add(login);
+        Link logout = new Link("logoutLink") {
+
+            @Override
+            public void onClick() {
+                if (getWebadminSession().isSignedIn()) {
+                    getWebadminSession().logoutUser();
+                    info(getStringResource("user.logout"));
+                } else {
+                    info(getStringResource("user.notLoggedIn"));
+                }
+            }
+        };
+        logout.add(new Image("logoutImage", new ResourceReference(
+                DCacheServices.class, "logout.gif")));
+        add(logout);
     }
 }
