1b59c2975717b06a7bd009c604a3d8650f11ce03
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2010-09-21 07:59:08 +0000

    webadmin: hide select box for non admins on space usage page.
    Signed-off-by: Jan Schaefer
    merge: trunk@14613

--- modules/webadmin/src/org/dcache/webadmin/view/panels/poollist/PoolListPanel.java
+++ modules/webadmin/src/org/dcache/webadmin/view/panels/poollist/PoolListPanel.java
@@ -1,6 +1,7 @@
 package org.dcache.webadmin.view.panels.poollist;
 
 import java.util.List;
+import org.apache.wicket.authorization.strategies.role.metadata.MetaDataRoleAuthorizationStrategy;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
@@ -12,6 +13,7 @@ import org.dcache.webadmin.view.beans.PoolSpaceBean;
 import org.dcache.webadmin.view.panels.basepanel.BasePanel;
 import org.dcache.webadmin.view.panels.layout.LayoutItemPanel;
 import org.dcache.webadmin.view.panels.layout.LayoutHeaderPanel;
+import org.dcache.webadmin.view.util.Role;
 
 /**
  * Reusable Panel for the Listview on Poolspaces
@@ -28,6 +30,8 @@ public class PoolListPanel extends BasePanel {
         Label selectBoxHeaderLabel = new Label("selectBoxHeader",
                 getStringResource("PoolPanel.selected.header"));
         selectBoxHeaderLabel.setVisibilityAllowed(_showCheckbox);
+        MetaDataRoleAuthorizationStrategy.authorize(selectBoxHeaderLabel,
+                RENDER, Role.ADMIN);
         add(selectBoxHeaderLabel);
         add(new LayoutHeaderPanel("PoolPanel.layoutHeaderPanel"));
         add(new PoolBeanListView("poolPanelListview", model));
@@ -42,12 +46,14 @@ public class PoolListPanel extends BasePanel {
         @Override
         protected void populateItem(final ListItem<PoolSpaceBean> item) {
             PoolSpaceBean poolBean = item.getModelObject();
-            WebMarkupContainer checkboxRow = new WebMarkupContainer("PoolPanel.checkboxRow");
-            checkboxRow.setVisibilityAllowed(_showCheckbox);
+            WebMarkupContainer checkboxColumn = new WebMarkupContainer("PoolPanel.checkboxRow");
+            checkboxColumn.setVisibilityAllowed(_showCheckbox);
+            MetaDataRoleAuthorizationStrategy.authorize(checkboxColumn,
+                    RENDER, Role.ADMIN);
             CheckBox checkbox = new CheckBox("PoolPanel.selected",
                     new PropertyModel<Boolean>(poolBean, "selected"));
-            checkboxRow.add(checkbox);
-            item.add(checkboxRow);
+            checkboxColumn.add(checkbox);
+            item.add(checkboxColumn);
             item.add(new Label("PoolPanel.name", poolBean.getName()));
             item.add(new Label("PoolPanel.domainName",
                     poolBean.getDomainName()));
