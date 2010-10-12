e6b1849f54cbb8c3c4d5a49ea4c7815ffdbaa35a
Paul Millar <paul.millar@desy.de>
2010-10-12 15:09:59 +0000

    dcap: fix bootstrap.sh to work on Solaris
    Solaris packaging provides tools like aclocal and automake as filenames with a 
    version-specific suffix (e.g., aclocal-1.11, automake-1.9, etc); moreover, we cannot 
    necessarily mix-and-match.  We must use tools with a consistent version; for example, 
    running v1.11 of aclocal with automake.
    
    The current implementation of bootstrap.sh simply invokes "aclocal" and "automake" 
    without any version-specific suffix.  This doesn't work on Solaris.  There is 
    commented-out lines in the current bootstrap.sh that include a version-specific suffix, 
    suggesting this issue is known but not addressed.
    
    This patch fixes the problem.  It looks for a set of consistently named tools with a 
    version-specific prefix, in decreasing version numbers (1.11, then 1.10, then 1.9).  If 
    these all fail, it tries again without any version-specific suffix.  If none of these 
    succeed then the script fails with a suitable error message.
    
    The output (on the terminal) from running bootstrap.sh should be identical to 
    previously.  The behaviour on running bootstrap.sh should be identical, including that 
    it terminates immediately after any failing step.
    
    Patch: http://rb.dcache.org/r/2465/
    Acked-by: Owen
    Target: trunk

--- modules/dcap/bootstrap.sh
+++ modules/dcap/bootstrap.sh
@@ -1,11 +1,60 @@
 #!/bin/sh
-set -x
-mkdir -p config
-aclocal -I config
-#aclocal-1.10 -I config
-autoheader
-libtoolize --automake
-automake  --add-missing --copy --foreign
-#automake-1.10  --add-missing --copy
-autoconf
 
+#  This version of bootstrap.sh will attempt to locate a consistent
+#  set of aclocal and automake.  "Consistent" here means that they
+#  have the same suffix; for example, aclocal-1.9 and automake-1.9.
+#
+#  This identification of a common set of aclocal and automake is a
+#  work-around.  It is needed because of a packaging issue with
+#  OpenSolaris, which does not always provide "aclocal" and "automake"
+#  that work.  Instead, there are aclocal-nn and automake-nn (where
+#  "nn" is the corresponding version number) allows external scripts
+#  to deduce which versions will work together.  This problem does not
+#  exists on the Linux distributions tried so far (Scientific Linux
+#  and Debian).
+#
+#  Unfortunately, it is unclear how to submit bugs against OpenSolais,
+#  so there is no indication if and when the problem will be fixed.
+
+function haveToolsWithSuffix() # in $1 - version suffix
+{
+  found_count=0
+  for prog in aclocal automake; do
+    which $prog$1 >/dev/null 2>&1 || break
+    found_count=$(( $found_count + 1 ))
+  done
+
+  [ $found_count -eq 2 ]
+}
+
+#  Echo a command then run it.  If it fails terminate script.
+function run() # in $1 - program to run, $2.. arguments
+{
+  echo "+ $*"
+  "$@"
+  rc=$?
+  if [ $rc -ne 0 ]; then
+      exit $rc
+  fi
+}
+
+for suffix in -1.11 -1.10 -1.9 ""; do
+  if haveToolsWithSuffix "$suffix"; then
+      ACLOCAL=aclocal$suffix
+      AUTOMAKE=automake$suffix
+      break;
+  fi
+done
+
+if [ ! "$ACLOCAL" ]; then
+    echo "No suitable autotools found."
+    exit 1
+fi
+
+mkdir -p config
+
+run $ACLOCAL -I config
+run autoheader
+run libtoolize --automake
+run $AUTOMAKE --add-missing --copy --foreign
+run autoconf
