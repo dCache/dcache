cbba9d5186b546ae4448024deb032faa60fbc211
Gerd Behrmann <behrmann@nordu.net>
2010-10-14 08:13:11 +0000

    JGlobus 1.8.0 source code.
    The previous commit added the untared source package instead of the 
    original tarball. Fixed.

--- modules/external/cog/cog-jglobus-1.8.0/etc/windows/java.cfg.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set LOCALCLASSPATH=
-for %%i in ("%COG_INSTALL_PATH%\lib\*.jar") do call "%COG_INSTALL_PATH%\etc\lcp.bat" "%%i"
-set LOCALCLASSPATH="%COG_INSTALL_PATH%\etc";%LOCALCLASSPATH%
--- modules/external/cog/cog-jglobus-1.8.0/setenv.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-for %%i in (lib\*.jar) do call etc\windows\lcp.bat %%i
-set CLASSPATH=build\classes;etc;%LOCALCLASSPATH%
