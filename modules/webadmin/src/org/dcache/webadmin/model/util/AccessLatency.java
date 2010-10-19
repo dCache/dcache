3c6a5533d71ece3d5fed5e0a17212232824f8239
Jan Schaefer <jan.schaefer@desy.de>
2010-10-19 14:03:17 +0000

    Put Enum for AccessLatency and RetentionPolicy into webadmin
    http://rb.dcache.org/r/2543/
    
    acked by Paul

---
+++ modules/webadmin/src/org/dcache/webadmin/model/util/AccessLatency.java
@@ -0,0 +1,40 @@
+package org.dcache.webadmin.model.util;
+
+/**
+ * An utility version of Access Latency to easily convert Strings and Latencies
+ * back and forth
+ */
+public enum AccessLatency {
+
+    ONLINE("ONLINE", "O"), NEARLINE("NEARLINE", "N"), OFFLINE("OFFLINE", "F");
+    private final String _name;
+    private final String _shortcut;
+
+    AccessLatency(String name, String shortcut) {
+        _name = name;
+        _shortcut = shortcut;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public String getShortcut() {
+        return _shortcut;
+    }
+
+    /**
+     * Look up the AccessLatency that matches given String
+     *
+     * @param name
+     * @return the corresponding AccessLatency, if valid, null otherwise.
+     */
+    static public AccessLatency parseStringValue(String name) {
+        for (AccessLatency accessLatency : AccessLatency.values()) {
+            if (accessLatency.getName().equals(name)) {
+                return accessLatency;
+            }
+        }
+        return null;
+    }
+};
---
+++ modules/webadmin/src/org/dcache/webadmin/model/util/RetentionPolicy.java
@@ -0,0 +1,40 @@
+package org.dcache.webadmin.model.util;
+
+/**
+ * An utility version of Retention Policy to easily convert Strings and Policies
+ * back and forth
+ */
+public enum RetentionPolicy {
+
+    REPLICA("REPLICA", "R"), OUTPUT("OUTPUT", "P"), CUSTODIAL("CUSTODIAL", "C");
+    private final String _name;
+    private final String _shortcut;
+
+    RetentionPolicy(String name, String shortcut) {
+        _name = name;
+        _shortcut = shortcut;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public String getShortcut() {
+        return _shortcut;
+    }
+
+    /**
+     * Look up the RetentionPolicy that matches given String
+     *
+     * @param name
+     * @return the corresponding RetentionPolicy, if valid, null otherwise.
+     */
+    static public RetentionPolicy parseStringValue(String name) {
+        for (RetentionPolicy policy : RetentionPolicy.values()) {
+            if (policy.getName().equals(name)) {
+                return policy;
+            }
+        }
+        return null;
+    }
+}
