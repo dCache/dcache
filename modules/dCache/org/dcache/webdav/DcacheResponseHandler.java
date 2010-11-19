4db2f2ef7f4d9c5d16c9e9a64ab1e6a4f7cae3a1
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2010-11-19 08:28:36 +0000

    webdav: fix HEAD command
    Signed-off-by: Gerd Behrmann
    merge: trunk@14815

---
+++ modules/dCache/org/dcache/webdav/DcacheResponseHandler.java
@@ -0,0 +1,51 @@
+package org.dcache.webdav;
+
+import com.bradmcevoy.http.Resource;
+import com.bradmcevoy.http.GetableResource;
+import com.bradmcevoy.http.Response;
+import com.bradmcevoy.http.Request;
+import com.bradmcevoy.http.AuthenticationService;
+import com.bradmcevoy.http.http11.DefaultHttp11ResponseHandler;
+import com.bradmcevoy.http.webdav.DefaultWebDavResponseHandler;
+import com.bradmcevoy.http.webdav.WebDavResourceTypeHelper;
+import com.bradmcevoy.http.webdav.PropFindXmlGenerator;
+import com.bradmcevoy.http.values.ValueWriters;
+
+/**
+ * Response handler that contains workarounds for bugs in Milton.
+ */
+public class DcacheResponseHandler extends DefaultWebDavResponseHandler
+{
+    public DcacheResponseHandler(AuthenticationService authenticationService)
+    {
+        super(new Http11ResponseHandler(authenticationService),
+              new WebDavResourceTypeHelper(),
+              new PropFindXmlGenerator(new ValueWriters()));
+    }
+
+    protected static class Http11ResponseHandler extends DefaultHttp11ResponseHandler
+    {
+        public Http11ResponseHandler(AuthenticationService authenticationService)
+        {
+            super(authenticationService);
+        }
+
+        @Override
+        public void respondHead(Resource resource, Response response, Request request ) {
+            setRespondContentCommonHeaders(response, resource);
+
+            if (resource instanceof GetableResource) {
+                GetableResource gr = (GetableResource) resource;
+                Long contentLength = gr.getContentLength();
+                if (contentLength != null) {
+                    response.setContentLengthHeader(contentLength);
+                }
+                String acc = request.getAcceptHeader();
+                String ct = gr.getContentType(acc);
+                if (ct != null) {
+                    response.setContentTypeHeader(ct);
+                }
+            }
+        }
+    }
+}
