eba541d7b3ce0a025be1957964e0476b995c1bad
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2010-11-22 14:54:05 +0000

    util: utility class for booleans
    dCache code base full of constructions like:
        (_debug?"on":"off")
    or
         if( args.argv(0).equals("on") ){ _magic = true ; }
         else if( args.argv(0).equals("off") ){ _magic = false ; }
         else throw new IllegalArgumentException("on|off") ;
    
    This helper class tried to clenup such cases and make it more readable
    
    Acked-By: Paul Millar
    Acked-By: Gerd Behrmann
    Patch: http://rb.dcache.org/r/2648/

---
+++ modules/dCache/org/dcache/util/Booleans.java
@@ -0,0 +1,112 @@
+package org.dcache.util;
+
+public class Booleans {
+
+    private final static String[] trueStrings = {"true", "yes", "on", "enable"};
+    private final static String[] falseStrings = {"false", "no", "off", "disable"};
+
+    private Booleans() {
+    }
+
+    /**
+     * Convert boolean value into Strings "yes" or "no"
+     * @param value
+     * @return "yes" if <code>value</code> is <code>true</code> and "no" otherwise.
+     */
+    public static String toYesNoString(boolean value) {
+        return toPredefinedString(value, "yes", "no");
+    }
+
+    /**
+     * Convert boolean value into Strings "on" or "off"
+     * @param value
+     * @return "on" if <code>value</code> is <code>true</code> and "off" otherwise.
+     */
+    public static String toOnOffString(boolean value) {
+        return toPredefinedString(value, "on", "off");
+    }
+
+    /**
+     * Convert boolean value into Strings "true" or "false"
+     * @param value
+     * @return "true" if <code>value</code> is <code>true</code> and "false" otherwise.
+     */
+    public static String toTrueFalseString(boolean value) {
+        return toPredefinedString(value, "true", "false");
+    }
+
+        /**
+     * Convert boolean value into Strings "enable" or "disable"
+     * @param value
+     * @return "enable" if <code>value</code> is <code>true</code> and "disable" otherwise.
+     */
+    public static String toEnableDisableString(boolean value) {
+        return toPredefinedString(value, "enable", "disable");
+    }
+
+    /**
+     * Convert boolean value into corresponding string.
+     * @param value
+     * @param ifTrue String representation for <code>true</code>
+     * @param ifFalse String representation for <code>false</code>
+     * @return value of <code>ifTrue</code> if value is <code>true</code> and ifFalse otherwise.
+     */
+    public static String toPredefinedString(boolean value, String ifTrue, String ifFalse) {
+        return value ? ifTrue : ifFalse;
+    }
+
+    /**
+     * Get boolean value of specified string.
+     * @param s string to check
+     * @throws IllegalArgumentException if provided string do not corresponds to
+     * any of <code>true</code> or <code>false</code> string.
+     * @return true if <code>s</code> has corresponding value.
+     */
+    public static boolean of(String s) {
+        if (contains(trueStrings, s, true)) {
+            return true;
+        }
+        if (contains(falseStrings, s, true)) {
+            return false;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * Get boolean value of specified string. If provided string is <code>null</code>
+     * then <code>defaultValue</code> is returned.
+     * @param s string to check
+     * @param defaultValue
+     * @throws IllegalArgumentException if provided string do not corresponds to
+     * any of <code>true</code> or <code>false</code> string.
+     * @return true if <code>s</code> has corresponding value.
+     */
+    public static boolean of(String s, boolean defaultValue) {
+        if( s == null) return defaultValue;
+
+        if (contains(trueStrings, s, true)) {
+            return true;
+        }
+        if (contains(falseStrings, s, true)) {
+            return false;
+        }
+        throw new IllegalArgumentException(s + " is not in supported boolean identifier");
+    }
+
+    /**
+     * Test the array.
+     * @param array where to test
+     * @param value to test
+     * @param ignoreCase
+     * @return <code>true</code> is <code>array</code> contains <code>value</code>
+     * and <code>false</code> otherwise.
+     */
+    private static boolean contains(String[] array, String value, boolean ignoreCase) {
+        for (String s : array) {
+            if (ignoreCase && s.equalsIgnoreCase(value) || s.equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ modules/dCacheJUnit/org/dcache/util/BooleansTest.java
@@ -0,0 +1,76 @@
+package org.dcache.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BooleansTest {
+
+    @Test
+    public void testYes() {
+        assertEquals("yes", Booleans.toYesNoString(true));
+    }
+
+    @Test
+    public void testNo() {
+        assertEquals("no", Booleans.toYesNoString(false));
+    }
+
+    @Test
+    public void testOn() {
+        assertEquals("on", Booleans.toOnOffString(true));
+    }
+
+    @Test
+    public void testEnable() {
+        assertEquals("enable", Booleans.toEnableDisableString(true));
+    }
+
+    @Test
+    public void testOff() {
+        assertEquals("off", Booleans.toOnOffString(false));
+    }
+
+    @Test
+    public void testTrue() {
+        assertEquals("true", Booleans.toTrueFalseString(true));
+    }
+
+    @Test
+    public void testFalse() {
+        assertEquals("false", Booleans.toTrueFalseString(false));
+    }
+
+    @Test
+    public void testDisable() {
+        assertEquals("disable", Booleans.toEnableDisableString(false));
+    }
+
+    @Test
+    public void testTrueFromString() {
+        assertTrue(Booleans.of("yes"));
+        assertTrue(Booleans.of("on"));
+        assertTrue(Booleans.of("true"));
+        assertTrue(Booleans.of("enable"));
+    }
+
+    @Test
+    public void testFalseFromString() {
+        assertFalse(Booleans.of("no"));
+        assertFalse(Booleans.of("off"));
+        assertFalse(Booleans.of("false"));
+        assertFalse(Booleans.of("disable"));
+    }
+
+    @Test
+    public void testFalseFromStringIgnoreCase() {
+        assertFalse(Booleans.of("No"));
+        assertFalse(Booleans.of("OFF"));
+        assertFalse(Booleans.of("faLse"));
+        assertFalse(Booleans.of("DisAblE"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFromBadString() {
+        assertTrue(Booleans.of("bla"));
+    }
+}
