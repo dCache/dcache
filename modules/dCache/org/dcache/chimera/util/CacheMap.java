4e34bbf97f4d4d292ec8b892bd84de0a2e5a2974
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2010-12-10 13:19:16 +0000

    chimera: add classes missed during the merge
    Acked-By: Gerd Behrmann (jabber)

---
+++ modules/dCache/org/dcache/chimera/util/CacheMap.java
@@ -0,0 +1,91 @@
+/*
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program (see the file COPYING.LIB for more
+ * details); if not, write to the Free Software Foundation, Inc.,
+ * 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.dcache.chimera.util;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+
+public class CacheMap<K, V> {
+
+    private static final long serialVersionUID = -446090383728952825L;
+    private static final Logger _log = Logger.getLogger(CacheMap.class.getName());
+    private final Map<K, V> _elements = new ConcurrentHashMap<K, V>();
+    private final ScheduledExecutorService _cleaner = new ScheduledThreadPoolExecutor(2);
+
+    /**
+     * @param key     :
+     *                key
+     * @param value   :
+     *                value
+     * @param timeout :
+     *                timeout in seconds
+     * @return : Object associated with this key
+     */
+    public V put(final K key, final V value, long timeout) {
+
+        V ce = _elements.put(key, value);
+        _cleaner.schedule(new Runnable()  {
+
+            public void run() {
+                if (_elements.remove(key) != null) {
+                    _log.debug("removing cache value for key" + key);
+                }
+            }
+        }, timeout, TimeUnit.SECONDS);
+
+        return ce;
+    }
+
+    public V remove(K key) {
+        return _elements.remove(key);
+    }
+
+    public V get(K key) {
+        return _elements.get(key);
+    }
+
+    public boolean isEmpty() {
+        return _elements.isEmpty();
+    }
+
+    public void clear() {
+        _elements.clear();
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            CacheMap<String, String> map = new CacheMap<String, String>();
+
+            map.put("o1", "o1", 10);
+
+            boolean b1 = map.isEmpty();
+            Thread.sleep(11000);
+
+            boolean b2 = map.isEmpty();
+            System.out.println("b1 = " + b1);
+            System.out.println("b2 = " + b2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ modules/dCache/org/dcache/chimera/util/DirectoryListCache.java
@@ -0,0 +1,55 @@
+/*
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program (see the file COPYING.LIB for more
+ * details); if not, write to the Free Software Foundation, Inc.,
+ * 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.dcache.chimera.util;
+
+import org.apache.log4j.Logger;
+
+public class DirectoryListCache<K, E> {
+
+    private final CacheMap<K, E> _cache = new CacheMap<K, E>();
+    private static final Logger _log = Logger.getLogger(DirectoryListCache.class.getName());
+
+    public void add(K key, E list) {
+        add(key, list, 0);
+    }
+
+    /**
+     * @param inode
+     * @param list
+     * @param timeout in seconds
+     */
+    public void add(K key, E list, long timeout) {
+        try {
+            // be smart (I am not sure it's a good idea or not
+            //long timeout = list.length / 500 ; // ( 1 second per 500 entries )
+            _cache.put(key, list, timeout == 0 ? 10 : timeout); // cache entries 10 seconds
+        } catch (OutOfMemoryError oops) {
+            // PANIC! try to free some memory :(
+            _cache.clear();
+            _log.error("DirectoryCacheList: Out of memory !");
+        }
+    }
+
+    public E get(K key) {
+        return _cache.get(key);
+    }
+
+    public void remove(K key) {
+        _cache.remove(key);
+    }
+}
+
