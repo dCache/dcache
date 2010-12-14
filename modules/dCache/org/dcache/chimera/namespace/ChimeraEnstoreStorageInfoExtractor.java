3f1aba00d4351c45e969c117eb8007c62b99e9c7
Dmitry Litvintsev <litvinse@fnal.gov>
2010-12-14 16:35:42 +0000

    introduce ChimerEnstoreStorageInfoExtratcor, so dCache works with enstrore
    http://rb.dcache.org/r/2769
    Acked by Tigran

--- modules/dCache/org/dcache/chimera/namespace/ChimeraEnstoreStorageInfoExtractor.java
+++ modules/dCache/org/dcache/chimera/namespace/ChimeraEnstoreStorageInfoExtractor.java
@@ -24,41 +24,31 @@ import diskCacheV111.util.FileNotFoundCacheException;
 import diskCacheV111.util.HsmLocationExtractorFactory;
 import diskCacheV111.util.RetentionPolicy;
 import diskCacheV111.util.AccessLatency;
-import diskCacheV111.vehicles.OSMStorageInfo;
+import diskCacheV111.vehicles.EnstoreStorageInfo;
 import diskCacheV111.vehicles.StorageInfo;
 
 
-public class ChimeraOsmStorageInfoExtractor extends ChimeraHsmStorageInfoExtractor {
+public class ChimeraEnstoreStorageInfoExtractor extends ChimeraHsmStorageInfoExtractor {
 
-    public ChimeraOsmStorageInfoExtractor(AccessLatency defaultAL,
-                                          RetentionPolicy defaultRP) {
+    public ChimeraEnstoreStorageInfoExtractor(AccessLatency defaultAL,
+                                              RetentionPolicy defaultRP) {
         super(defaultAL,defaultRP);
     }
 
     public StorageInfo getFileStorageInfo(FsInode inode) throws CacheException {
-
-        OSMStorageInfo info;
-
+        EnstoreStorageInfo info;
         Stat stat = null;
         FsInode level2 = new FsInode(inode.getFs(), inode.toString(), 2);
-
         try {
-
-            List<StorageLocatable> locations = inode.getFs().getInodeLocations(inode, StorageGenericLocation.TAPE );
-
-            if ( locations.isEmpty() ) {
-                info = (OSMStorageInfo) getDirStorageInfo(inode);
+            List<StorageLocatable> locations = inode.getFs().getInodeLocations(inode,
+                                                                               StorageGenericLocation.TAPE );
+            EnstoreStorageInfo parentStorageInfo = (EnstoreStorageInfo) getDirStorageInfo(inode);
+            if (locations.isEmpty()) {
+                info = parentStorageInfo;
                 AccessLatency al = inode.getFs().getAccessLatency(inode);
-
-                /*
-                 * currently storage class and AL and RP are asymmetric:
-                 *   storage class of the file is bound to directory as log as file is not stored on tape,
-                 *   while AL and RP are bound to file
-                 */
                 if(al != null) {
                     info.setAccessLatency(AccessLatency.getAccessLatency(al.getId()));
                 }
-
                 RetentionPolicy rp = inode.getFs().getRetentionPolicy(inode);
                 if( rp != null ) {
                     info.setRetentionPolicy(RetentionPolicy.getRetentionPolicy(rp.getId()));
@@ -66,32 +56,48 @@ public class ChimeraOsmStorageInfoExtractor extends ChimeraHsmStorageInfoExtract
             }
             else {
                 InodeStorageInformation inodeStorageInfo = inode.getFs().getSorageInfo(inode);
-
-                info = new OSMStorageInfo( inodeStorageInfo.storageGroup(),
-                                           inodeStorageInfo.storageSubGroup() );
-
+                info = new EnstoreStorageInfo(parentStorageInfo.getStorageGroup(),
+                                              parentStorageInfo.getFileFamily());
                 info.setIsNew(false);
                 info.setAccessLatency(inodeStorageInfo.accessLatency());
                 info.setRetentionPolicy(inodeStorageInfo.retentionPolicy());
-
                 for(StorageLocatable location: locations) {
                     if( location.isOnline() ) {
                         try {
-                            info.addLocation( new URI(location.location()) );
-                        } catch (URISyntaxException e) {
+                            URI uri = new URI(location.location());
+                            info.addLocation(uri);
+                            for (String part : uri.getQuery().split("&")) {
+                                String[] data = part.split("=");
+                                String key    = data[0];
+                                String value  = (data.length == 2 ? data[1] : "");
+                                if (key.equals("bfid")) {
+                                    info.setBitfileId(value);
+                                }
+                                else if (key.equals("volume")) {
+                                    info.setVolume(value);
+                                }
+                                else if (key.equals("location_cookie")) {
+                                    info.setLocation(value);
+                                }
+                                else if (key.equals("original_name")) {
+                                    info.setPath(value);
+                                }
+                            }
+                        }
+                        catch (URISyntaxException e) {
                             // bad URI
                         }
                     }
                 }
             }
-
-        } catch (ChimeraFsException e) {
+        }
+        catch (ChimeraFsException e) {
             throw new CacheException(e.getMessage());
         }
-
         try {
             stat = inode.stat();
-        }catch( ChimeraFsException hfe) {
+        }
+        catch( ChimeraFsException hfe) {
             throw new CacheException(hfe.getMessage());
         }
         info.setFileSize(stat.getSize());
@@ -100,7 +106,7 @@ public class ChimeraOsmStorageInfoExtractor extends ChimeraHsmStorageInfoExtract
     }
 
     public StorageInfo getDirStorageInfo(FsInode inode) throws CacheException {
-        FsInode dirInode = null;
+        FsInode dirInode;
         if (!inode.isDirectory()) {
             dirInode = inode.getParent();
         }
@@ -109,9 +115,11 @@ public class ChimeraOsmStorageInfoExtractor extends ChimeraHsmStorageInfoExtract
         }
         try {
             HashMap<String, String> hash = new HashMap<String, String>();
-            String store = null;
-            String group = null;
-            String[] OSMTemplate = getTag(dirInode, "OSMTemplate");
+            String [] OSMTemplate = getTag(dirInode,"OSMTemplate") ;
+            String [] group       = getTag(dirInode,"storage_group" ) ;
+            String [] family      = getTag(dirInode,"file_family" ) ;
+            String [] spaceToken  = getTag(dirInode,"WriteToken");
+
             if (OSMTemplate != null) {
                 for ( String line: OSMTemplate) {
                     StringTokenizer st = new StringTokenizer(line);
@@ -119,18 +127,12 @@ public class ChimeraOsmStorageInfoExtractor extends ChimeraHsmStorageInfoExtract
                         continue;
                     hash.put(st.nextToken(), st.nextToken());
                 }
-                store = hash.get("StoreName");
-                if (store == null) {
-                    throw new CacheException(37, "StoreName not found in template");
-                }
             }
-            String [] sGroup = getTag(dirInode, "sGroup");
-            if( sGroup != null ) {
-                group = sGroup[0].trim();
-            }
-            OSMStorageInfo info = new OSMStorageInfo(store, group);
+            String sg = (group==null||group.length == 0)||(sg=group[0].trim()).equals("") ? "none" : sg;
+            String ff = (family==null||family.length == 0)||(ff=family[0].trim()).equals("") ? "none" : ff;
+            EnstoreStorageInfo info = new EnstoreStorageInfo(sg,ff);
             info.addKeys(hash);
-            String[] accessLatency = getTag(dirInode, "AccessLatency");
+            String[] accessLatency   = getTag(dirInode, "AccessLatency");
             String[] retentionPolicy = getTag(dirInode, "RetentionPolicy");
             if(accessLatency != null) {
                 try {
@@ -138,22 +140,21 @@ public class ChimeraOsmStorageInfoExtractor extends ChimeraHsmStorageInfoExtract
                     info.isSetAccessLatency(true);
                 }
                 catch(IllegalArgumentException iae) {
-                    // TODO: do we fail here or not?
                 }
             }
-            else {
-	         // force default
+            else{
                 info.setAccessLatency(getDefaultAccessLatency());
             }
 
             if(retentionPolicy != null) {
                 try {
-                    info.setRetentionPolicy( diskCacheV111.util.RetentionPolicy.getRetentionPolicy(retentionPolicy[0].trim()));
+                    info.setRetentionPolicy(RetentionPolicy.getRetentionPolicy(retentionPolicy[0].trim()));
                     info.isSetRetentionPolicy(true);
                 }
                 catch(IllegalArgumentException iae) {
                 }
-            }else{
+            }
+            else{
                 info.setRetentionPolicy(getDefaultRetentionPolicy());
             }
             return info;
---
+++ modules/dCache/org/dcache/chimera/namespace/ChimeraHsmStorageInfoExtractor.java
@@ -0,0 +1,200 @@
+package org.dcache.chimera.namespace;
+
+import java.io.BufferedReader;
+import java.io.CharArrayReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.dcache.chimera.ChimeraFsException;
+import org.dcache.chimera.FileNotFoundHimeraFsException;
+import org.dcache.chimera.FsInode;
+import org.dcache.chimera.FsInode_TAG;
+import org.dcache.chimera.StorageGenericLocation;
+import org.dcache.chimera.StorageLocatable;
+import org.dcache.chimera.posix.Stat;
+import org.dcache.chimera.store.InodeStorageInformation;
+
+import diskCacheV111.util.CacheException;
+import diskCacheV111.util.FileNotFoundCacheException;
+import diskCacheV111.util.HsmLocationExtractorFactory;
+import diskCacheV111.util.RetentionPolicy;
+import diskCacheV111.util.AccessLatency;
+import diskCacheV111.vehicles.StorageInfo;
+
+public abstract class ChimeraHsmStorageInfoExtractor implements
+       ChimeraStorageInfoExtractable {
+
+    /**
+     * default access latency for newly created files
+     */
+    private final AccessLatency _defaultAccessLatency;
+
+    /**
+     * default retention policy for newly created files
+     */
+    private final RetentionPolicy _defaultRetentionPolicy;
+
+
+    public ChimeraHsmStorageInfoExtractor(AccessLatency defaultAL,
+                                          RetentionPolicy defaultRP) {
+
+        _defaultAccessLatency = defaultAL;
+        _defaultRetentionPolicy = defaultRP;
+    }
+
+    public final AccessLatency getDefaultAccessLatency() {
+        return _defaultAccessLatency;
+    }
+
+    public final RetentionPolicy getDefaultRetentionPolicy() {
+        return _defaultRetentionPolicy;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see diskCacheV111.util.StorageInfoExtractable#getStorageInfo(java.lang.String,
+     *      diskCacheV111.util.PnfsId)
+     */
+
+    public StorageInfo getStorageInfo(FsInode inode)
+            throws CacheException {
+
+        if( !inode.exists() ) {
+            throw new FileNotFoundCacheException(inode.toString() + " does not exists");
+        }
+
+        StorageInfo info;
+        FsInode dirInode;
+
+        if (inode.isDirectory()) {
+            info =  getDirStorageInfo(inode);
+            dirInode = inode;
+        } else {
+            info =  getFileStorageInfo(inode);
+            dirInode = inode.getParent();
+        }
+
+        try {
+            // overwrite hsm type with hsmInstance tag
+            String[] hsmInstance;
+            hsmInstance = getTag(dirInode, "hsmInstance");
+            if( hsmInstance != null ) {
+                info.setHsm( hsmInstance[0].toLowerCase().trim());
+            }
+
+            String[] cacheClass = getTag(dirInode, "cacheClass");
+            if( cacheClass != null ) {
+                info.setCacheClass( cacheClass[0].trim());
+            }
+
+            String [] spaceToken = getTag(dirInode, "WriteToken");
+            if( spaceToken != null ) {
+                info.setKey("writeToken", spaceToken[0].trim());
+            }
+        } catch (IOException e) {
+            throw new CacheException( 37, "Unable to fetch tags: " + e.getMessage());
+        }
+
+        return info;
+    }
+
+    public abstract StorageInfo getFileStorageInfo(FsInode inode) throws CacheException;
+    public abstract StorageInfo getDirStorageInfo(FsInode inode) throws CacheException;
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see diskCacheV111.util.StorageInfoExtractable#setStorageInfo(java.lang.String,
+     *      diskCacheV111.util.PnfsId, diskCacheV111.vehicles.StorageInfo, int)
+     */
+    public void setStorageInfo(FsInode inode, StorageInfo dCacheStorageInfo,
+            int arg3) throws CacheException {
+
+        try {
+
+            if( dCacheStorageInfo.isSetAccessLatency() ) {
+                AccessLatency accessLatency = dCacheStorageInfo.getAccessLatency();
+                inode.getFs().setAccessLatency(inode, accessLatency);
+            }
+
+            if( dCacheStorageInfo.isSetRetentionPolicy() ) {
+                RetentionPolicy retentionPolicy = dCacheStorageInfo.getRetentionPolicy();
+                inode.getFs().setRetentionPolicy(inode, retentionPolicy);
+            }
+
+            if(dCacheStorageInfo.isSetAddLocation() ) {
+                List<URI> locationURIs = dCacheStorageInfo.locations();
+
+                if( !locationURIs.isEmpty() ) {
+                    InodeStorageInformation storageInfo = new InodeStorageInformation(inode,
+                    dCacheStorageInfo.getHsm(),
+                    dCacheStorageInfo.getKey("store"),
+                    dCacheStorageInfo.getKey("group"));
+                    inode.getFs().setStorageInfo(inode, storageInfo);
+                }
+
+                for(URI location : locationURIs) {
+                    // skip bad URI's if the get here
+                    if(location.toString().isEmpty()) continue;
+                    URI validatedUri = HsmLocationExtractorFactory.validate(location);
+                    inode.getFs().addInodeLocation(inode, StorageGenericLocation.TAPE, validatedUri.toString());
+                }
+            }
+
+        }catch(FileNotFoundHimeraFsException e) {
+            throw new FileNotFoundCacheException(e.getMessage());
+        }catch(ChimeraFsException he ) {
+            throw new CacheException(he.getMessage() );
+        }
+    }
+
+    /**
+     *
+     * get content of a virtual file named .(tag)(&lt;tagname&gt;).
+     *
+     * @param dirInode inode of directory
+     * @param tag tag name
+     * @return array of strings corresponding to lines of tag file or null
+     * if tag does not exist or empty.
+     * @throws IOException
+     */
+    public static String[] getTag(FsInode dirInode, String tag)
+        throws IOException {
+
+        FsInode_TAG tagInode = new FsInode_TAG(dirInode.getFs(), dirInode
+                .toString(), tag);
+
+
+        if( !tagInode.exists() ) {
+            return null;
+        }
+
+        byte[] buff = new byte[256];
+
+        int len = tagInode.read(0, buff, 0, buff.length);
+        /* empty and bad tags are treated as non existing tags */
+        if( len <= 0 ) {
+            return null;
+        }
+
+        List<String> lines = new ArrayList<String>();
+        CharArrayReader ca = new CharArrayReader(new String(buff, 0, len)
+                .toCharArray());
+
+        BufferedReader br = new BufferedReader(ca);
+
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+        }
+
+        return lines.toArray(new String[lines.size()]);
+
+    }
+}
