fbad7e1c1a3a9fd3be7a1b5ee11e23a2fcfe1bb8
Gerd Behrmann <behrmann@nordu.net>
2011-02-28 16:48:29 +0000

    pinmanager: Refactor LiquiBase Spring integration
    LiquiBase comes with Spring integration, however the functionality is
    rather limited: The only way to disable automatic schema migration is by
    setting a system property. This makes it impossible to disable schema
    migration per service. When schema migration is disabled, the LiquiBase
    spring integration is disabled completely rather than falling back to
    verification mode.
    
    This patch adds a subclass of the SpringLiquibase class. It allows the
    automatic schema management to be managed per instance and when disabled
    it will check whether unapplied changes exist.
    
    Finally, the class will now output a few extra log messages to keep the
    user informed about these important events.
    
    Target: trunk
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: http://rb.dcache.org/r/3110/

---
+++ modules/dCache/org/dcache/util/SpringLiquibase.java
@@ -0,0 +1,80 @@
+package org.dcache.util;
+
+import java.util.List;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+import liquibase.Liquibase;
+import liquibase.changelog.ChangeSet;
+import liquibase.exception.LiquibaseException;
+import liquibase.exception.MigrationFailedException;
+import liquibase.exception.DatabaseException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.dao.DataAccessException;
+
+public class SpringLiquibase
+    extends liquibase.integration.spring.SpringLiquibase
+{
+    private final static Logger _log =
+        LoggerFactory.getLogger(SpringLiquibase.class);
+
+    private boolean _shouldUpdate = true;
+
+    public void setShouldUpdate(boolean shouldUpdate)
+    {
+        _shouldUpdate = shouldUpdate;
+    }
+
+    public boolean getShouldUpdate()
+    {
+        return _shouldUpdate;
+    }
+
+    @Override
+    public void afterPropertiesSet()
+    {
+        getJdbcTemplate().execute(new SchemaMigrator());
+    }
+
+    protected JdbcTemplate getJdbcTemplate()
+    {
+        return new JdbcTemplate(getDataSource());
+    }
+
+    private class SchemaMigrator implements ConnectionCallback<Void>
+    {
+        public Void doInConnection(Connection c)
+            throws SQLException, DataAccessException
+        {
+            try {
+                Liquibase liquibase = createLiquibase(c);
+                try {
+                    List<ChangeSet> changeSets =
+                        liquibase.listUnrunChangeSets(getContexts());
+                    for (ChangeSet changeSet: changeSets) {
+                        _log.warn("Unapplied database schema change found: {}",
+                                  changeSet.getDescription());
+                    }
+
+                    if (_shouldUpdate) {
+                        _log.warn("Applying database schema changes");
+                        liquibase.update(getContexts());
+                    } else if (!changeSets.isEmpty()) {
+                        throw new MigrationFailedException(changeSets.get(0),
+                                                           "Automatic schema migration is disabled. Please apply missing changes.");
+                    }
+                } finally {
+                    liquibase.forceReleaseLocks();
+                }
+            } catch (LiquibaseException e) {
+                throw new SQLException("Schema migration failed", e);
+            }
+            return null;
+        }
+    }
+}
