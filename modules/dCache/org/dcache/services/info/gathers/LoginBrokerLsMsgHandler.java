822b33155c5036970559a593450df2d4ce761ab4
Paul Millar <paul.millar@desy.de>
2011-04-26 13:59:08 +0000

    Whitespace-only fix

--- modules/dCache/org/dcache/services/info/gathers/LoginBrokerLsMsgHandler.java
+++ modules/dCache/org/dcache/services/info/gathers/LoginBrokerLsMsgHandler.java
@@ -20,7 +20,7 @@ import dmg.cells.services.login.LoginBrokerInfo;
 /**
  * Parse the reply messages from sending the LoginBroker CellMessages with "ls -binary".
  * These replies are an array of LoginBrokerInfo objects.
- * 
+ *
  * @author Paul Millar <paul.millar@desy.de>
  */
 public class LoginBrokerLsMsgHandler extends CellMessageHandlerSkel {
@@ -39,7 +39,7 @@ public class LoginBrokerLsMsgHandler extends CellMessageHandlerSkel {
 
 		if( !msgPayload.getClass().isArray()) {
 			_log.error( "unexpected received non-array payload");
-			return;			
+			return;
 		}
 
 		Object[] array = (Object []) msgPayload;
@@ -50,20 +50,20 @@ public class LoginBrokerLsMsgHandler extends CellMessageHandlerSkel {
 		StateUpdate update = new StateUpdate();
 
 		for( int i = 0; i < array.length; i++) {
-			
+
 			if( !(array [i] instanceof LoginBrokerInfo)) {
 				_log.warn( "Skipping array element that is not LoginBrokerInfo");
 				continue;
 			}
-			
+
 			LoginBrokerInfo info = (LoginBrokerInfo) array[i];
 			addDoorInfo( update, PATH_TO_DOORS.newChild( info.getIdentifier()), info, metricLifetime);
 		}
-		
+
 		applyUpdates( update);
 	}
-	
-	
+
+
 	/**
 	 * Add additional state-update to record information about a door.
 	 * @param update the StateUpdate we are to add metrics to.
@@ -74,16 +74,16 @@ public class LoginBrokerLsMsgHandler extends CellMessageHandlerSkel {
 	private void addDoorInfo( StateUpdate update, StatePath pathToDoor, LoginBrokerInfo info, long lifetime) {
 
 		StatePath pathToProtocol = pathToDoor.newChild( "protocol");
-		
+
 		conditionalAddString( update, pathToProtocol, "engine",  info.getProtocolEngine(), lifetime);
 		conditionalAddString( update, pathToProtocol, "family",  info.getProtocolFamily(), lifetime);
 		conditionalAddString( update, pathToProtocol, "version", info.getProtocolVersion(), lifetime);
-		
+
 		update.appendUpdate( pathToDoor.newChild("load"),
 					new FloatingPointStateValue( info.getLoad(), lifetime));
 		update.appendUpdate( pathToDoor.newChild( "port"),
 					new IntegerStateValue( info.getPort(), lifetime));
-		
+
 		update.appendUpdate( pathToDoor.newChild( "cell"),
 					new StringStateValue( info.getCellName(), lifetime));
 
@@ -94,9 +94,9 @@ public class LoginBrokerLsMsgHandler extends CellMessageHandlerSkel {
 					new IntegerStateValue( info.getUpdateTime(), lifetime));
 
 		StatePath pathToInterfaces = pathToDoor.newChild("interfaces");
-		
+
 		String[] interfaceNames = info.getHosts();
-		
+
 		if( interfaceNames != null) {
 			for( int i = 0; i < interfaceNames.length; i++) {
 				if( interfaceNames[i] != null)
@@ -104,11 +104,11 @@ public class LoginBrokerLsMsgHandler extends CellMessageHandlerSkel {
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * Add a string metric at a specific point in the State tree if the value is not NULL.
-	 * @param update the StateUpdate to append with the metric definition 
+	 * @param update the StateUpdate to append with the metric definition
 	 * @param parentPath the path to the parent branch for this metric
 	 * @param name the name of the metric
 	 * @param value the metric's value, or null if the metric should not be added.
@@ -120,7 +120,7 @@ public class LoginBrokerLsMsgHandler extends CellMessageHandlerSkel {
 						new StringStateValue( value, storeTime));
 		}
 	}
-	
+
 
 	/**
 	 * Add a standardised amount of information about an interface.  This is in the form:
@@ -136,32 +136,32 @@ public class LoginBrokerLsMsgHandler extends CellMessageHandlerSkel {
 	 *       |   +-- "address" (string metric: the host's address; e.g., "127.0.0.1")
 	 *       |   +-- "address-type"    (string metric: "IPv4", "IPv6" or "unknown")
 	 *       |
-	 * </pre> 
+	 * </pre>
 	 * @param update The StateUpdate to append the new metrics.
-	 * @param parentPath the path that the id branch will be added.  
+	 * @param parentPath the path that the id branch will be added.
 	 * @param name something that identifies the interface (e.g., IP address or simple name).
 	 * @param order the order in which the interfaces should be considered: 1 is the lowest number.
 	 * @param lifetime how long the created metrics should last.
 	 */
 	private void addInterfaceInfo( StateUpdate update, StatePath parentPath, String name, int order, long lifetime) {
-		
+
 		StatePath pathToInterfaceBranch = parentPath.newChild(name);
-		
+
 		// Always add the name
 		update.appendUpdate( pathToInterfaceBranch.newChild( "name"), new StringStateValue( name, lifetime));
 
 		// Attempt to add information after resolving the interface
 		try {
 			InetAddress address = InetAddress.getByName(name);
-			
+
 			update.appendUpdate( pathToInterfaceBranch.newChild( "FQDN"), new StringStateValue( address.getCanonicalHostName(), lifetime));
-					
+
 			update.appendUpdate( pathToInterfaceBranch.newChild( "address"), new StringStateValue( address.getHostAddress(), lifetime));
 			update.appendUpdate( pathToInterfaceBranch.newChild( "address-type"),
 								new StringStateValue( (address instanceof Inet4Address) ? "IPv4" : (address instanceof Inet6Address) ? "IPv6" : "unknown", lifetime));
-			
+
 			update.appendUpdate( pathToInterfaceBranch.newChild( "order"), new IntegerStateValue( order, lifetime));
-			
+
 		} catch( UnknownHostException e) {
 			/**
 			 *  If interface is (for whatever reason) unknown, simply skip publishing the
