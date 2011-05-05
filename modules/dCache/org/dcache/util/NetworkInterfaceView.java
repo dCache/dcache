9c1304216d6f8896d61f57d03cc32a5045db1f06
Paul Millar <paul.millar@desy.de>
2011-05-05 12:59:54 +0000

    utils: add NetworkInterfaceView
    Add a serializable snapshot of a NetworkInterface object.  The 
    NetworkInterfaceView provides a serializable object allowing remote 
    components to know about the interfaces of a door.
    
    Unfortunately, InterfaceAddress is also not serialisable and has no 
    accessible constructor, so the patch also provides a 
    InterfaceAddressView class that is a serialisable version of 
    InterfaceAddress.
    
    The NetworkInterfaceView object will walk down the parent-child tree 
    to record information about all sub-interfaces but does not follow the 
    parent link.  This places a requirement that the source 
    NetworkInterface object is the root of the parent-child tree.
    
    The patch is intended to provide a mechanism to allow doors to relay 
    more detailed information about their interface to the LoginBroker, 
    which can then provide more detailed information about the doors 
    available in dCache.
    
    Target: trunk
    Patch: http://rb.dcache.org/r/2943/
    Acked-by: Gerd Behrmann

---
+++ modules/dCache/org/dcache/util/NetworkInterfaceView.java
@@ -0,0 +1,289 @@
+package org.dcache.util;
+
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterators;
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+
+/**
+ * This class provides an unmodifiable, serializable snapshot of a
+ * {@link NetworkInterface}. It provides much the same functionality
+ * as NetworkInterface; however, {@link #getParent} and {@link #getSubInterfaces}
+ * return different types and the public methods will never throw a
+ * {@code SocketException}.
+ * <p>
+ * {@link #getInterfaceAddresses} returns a list of {@link InterfaceAddressView} objects.
+ */
+public class NetworkInterfaceView implements Serializable {
+    private static final long serialVersionUID = 5589652882519395824L;
+    private static final Logger _log = LoggerFactory.getLogger(NetworkInterfaceView.class);
+
+    private final String _displayName;
+    private final byte[] _hardwareAddress;
+    private final List<InterfaceAddressView> _interfaceAddresses;
+    private final List<InetAddress> _inetAddresses;
+    private final int _mtu;
+    private final String _name;
+    private final List<NetworkInterfaceView> _children;
+    private final boolean _isLoopback;
+    private final boolean _isPointToPoint;
+    private final boolean _isUp;
+    private final boolean _isVirtual;
+    private final boolean _hasMulticastSupport;
+    private final NetworkInterfaceView _parent;
+
+    /**
+     * Create a {@link NetworkInterfaceView} snapshot of a source
+     * {@link NetworkInterface} object.  {@code source} not be a
+     * sub-interface; i.e., a call to {@link NetworkInterface#getParent}
+     * on {@code source} must return {@code null}.
+     * <p>
+     * When creating a {@code NetworkInterfaceView} object from a
+     * {@code NetworkInterface} object, a {@code NetworkInterfaceView} object is created
+     * for each sub-interface.  These {@code NetworkInterfaceView} objects are
+     * accessible via {@link #getSubInterfaces}.
+     * <p>
+     * If, when creating the list of {@code NetworkInterfaceView} objects for the
+     * sub-interfaces, a {@code NetworkInterfaceView} object cannot be created then
+     * that sub-interface is silently skipped.
+     * <p>
+     * @param source the {@code NetworkInterface} from which to create a snapshot.
+     * @throws SocketException if any of the {@code getHardwareAddress}, {@code getMTU},
+     * {@code isLoopback}, {@code isPointToPoint}, {@code isUp} and
+     * {@code supportsMulticast} methods of {@code source} throws a {@code SocketException}.
+     */
+    public NetworkInterfaceView(NetworkInterface source) throws SocketException {
+        this(source, null);
+
+        Preconditions.checkArgument(source.getParent() == null,
+                "Cannot create NetworkInterfaceView from interface %s",
+                source.getName());
+    }
+
+    private NetworkInterfaceView(NetworkInterface ni,
+                                 NetworkInterfaceView parent) throws SocketException  {
+        _displayName = ni.getDisplayName();
+        _hardwareAddress = ni.getHardwareAddress();
+        _mtu = ni.getMTU();
+        _name = ni.getName();
+        _isLoopback = ni.isLoopback();
+        _isPointToPoint = ni.isPointToPoint();
+        _isUp = ni.isUp();
+        _isVirtual = ni.isVirtual();
+        _hasMulticastSupport = ni.supportsMulticast();
+        _parent = parent;
+        _interfaceAddresses = viewOfInterfaceAddresses(ni);
+        _inetAddresses = viewOfInetAddresses(ni);
+        _children = viewOfChildren(ni);
+    }
+
+    private List<InterfaceAddressView> viewOfInterfaceAddresses(NetworkInterface ni) {
+
+        ImmutableList.Builder<InterfaceAddressView> builder = ImmutableList.builder();
+
+        for(InterfaceAddress address : ni.getInterfaceAddresses()) {
+            builder.add(new InterfaceAddressView(address));
+        }
+
+        return builder.build();
+    }
+
+    private List<InetAddress> viewOfInetAddresses(NetworkInterface ni) {
+        ImmutableList.Builder<InetAddress> builder = ImmutableList.builder();
+
+        Enumeration<InetAddress> addresses = ni.getInetAddresses();
+
+        while(addresses.hasMoreElements()) {
+            InetAddress address = addresses.nextElement();
+            builder.add(address);
+        }
+
+        return builder.build();
+    }
+
+    private List<NetworkInterfaceView> viewOfChildren(NetworkInterface ni) {
+        ImmutableList.Builder<NetworkInterfaceView> builder = ImmutableList.builder();
+
+        Enumeration<NetworkInterface> subInterfaces = ni.getSubInterfaces();
+
+        while( subInterfaces.hasMoreElements()) {
+
+            NetworkInterface child = subInterfaces.nextElement();
+
+            try {
+                NetworkInterfaceView childView = new NetworkInterfaceView(child, this);
+                builder.add(childView);
+            } catch (SocketException e) {
+                _log.debug("Unable to add child {} of interface {}: {}",
+                        new Object[]{child.getName(), ni.getName(), e.getMessage()});
+            }
+        }
+
+        return builder.build();
+    }
+
+    public List<NetworkInterfaceView> getSubInterfaces() {
+        return _children;
+    }
+
+    public Enumeration<InetAddress> getInetAddresses() {
+        return Iterators.asEnumeration(_inetAddresses.iterator());
+    }
+
+    public List<InetAddress> getInetAddressList() {
+        return _inetAddresses;
+    }
+
+    public boolean hasSubInterfaces() {
+        return !_children.isEmpty();
+    }
+
+    public String getDisplayName() {
+        return _displayName;
+    }
+
+    public byte[] getHardwareAddress() {
+        return _hardwareAddress;
+    }
+
+    public List<InterfaceAddressView> getInterfaceAddresses() {
+        return _interfaceAddresses;
+    }
+
+    public int getMTU() {
+        return _mtu;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public NetworkInterfaceView getParent() {
+        return _parent;
+    }
+
+    public boolean isLoopback() {
+        return _isLoopback;
+    }
+
+    public boolean isPointToPoint() {
+        return _isPointToPoint;
+    }
+
+    public boolean isUp() {
+        return _isUp;
+    }
+
+    public boolean isVirtual() {
+        return _isVirtual;
+    }
+
+    public boolean supportsMulticast() {
+        return _hasMulticastSupport;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("name:");
+
+        if( getName() != null) {
+            sb.append(getName());
+        } else {
+            sb.append("null");
+        }
+
+        if(getDisplayName() != null) {
+            sb.append(" (").append(getDisplayName()).append(")");
+        }
+
+
+        List<InterfaceAddressView> addresses = getInterfaceAddresses();
+
+        /*
+         * NetworkInterface provides an "index" field, which isn't exposed
+         * by any other method; because of this, NetworkInterfaceView
+         * doesn't record the index and that part of the toString output
+         * is skipped.
+         */
+
+        sb.append(" addresses:\n");
+
+        for(InterfaceAddressView address : addresses) {
+            sb.append(address.getAddress()).append(";\n");
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * This class provides the same functionality as InterfaceAddress
+     * but is serializable.
+     */
+    public static class InterfaceAddressView implements Serializable {
+        private static final long serialVersionUID = 467290761384687925L;
+
+        private final InetAddress _address;
+        private final InetAddress _broadcast;
+        private final short _maskLength;
+
+        public InterfaceAddressView( InterfaceAddress ifAddress) {
+            _address = ifAddress.getAddress();
+            _broadcast = ifAddress.getBroadcast();
+            _maskLength = ifAddress.getNetworkPrefixLength();
+        }
+
+        public InetAddress getAddress() {
+            return _address;
+        }
+
+        public InetAddress getBroadcast() {
+            return _broadcast;
+        }
+
+        public short getNetworkPrefixLength() {
+            return _maskLength;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(getNetworkPrefixLength(), getAddress(), getBroadcast());
+        }
+
+        @Override
+        public boolean equals(Object rawOther) {
+
+            if( rawOther == this) {
+                return true;
+            }
+
+            if(rawOther.getClass().equals(InterfaceAddressView.class)) {
+                InterfaceAddressView other = (InterfaceAddressView) rawOther;
+
+                return _maskLength == other.getNetworkPrefixLength() &&
+                    Objects.equal(_address, other.getAddress()) &&
+                    Objects.equal(_broadcast, other.getBroadcast());
+            }
+
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return _address.toString() + "/" + _maskLength + " [" + _broadcast + "]";
+        }
+    }
+}
---
+++ modules/dCacheJUnit/org/dcache/util/NetworkInterfaceViewTests.java
@@ -0,0 +1,229 @@
+package org.dcache.util;
+
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assume.assumeNoException;
+import static org.junit.Assume.assumeTrue;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Set of tests for the NetworkInterfaceView class
+ * <p>
+ * We are somewhat limited in what we can test as NetworkInterface
+ * is a final class that doesn't implement any interface and there
+ * are no static methods for creating NetworkInterface objects other
+ * than those for returning information about what is available.
+ * <p>
+ * These tests will use all the interfaces of the test-machine from
+ * which we can create a NetworkInterfaceView snapshot as the fixture.
+ * Any interface that we cannot create a snapshot of is silently ignored.
+ * See {@link NetworkInterfaceView} for reasons why creating a snapshot
+ * might fail.
+ * <p>
+ * These tests are bypassed (they are not run but without reporting a
+ * failure) if we can't enumerate the available interfaces or if we
+ * can't take a snapshot of at least one interface.
+ */
+public class NetworkInterfaceViewTests {
+
+    List<InterfaceAndViewPair> _interfaces;
+
+    @Before
+    public void setUp()
+    {
+        ImmutableList.Builder<InterfaceAndViewPair> builder = ImmutableList.builder();
+
+        Enumeration<NetworkInterface> interfaces;
+        try {
+            interfaces = NetworkInterface.getNetworkInterfaces();
+        } catch (SocketException e) {
+            assumeNoException(e);
+            throw new RuntimeException("Code cannot reach this point"); // work-around for Java
+        }
+
+        while(interfaces.hasMoreElements()) {
+            NetworkInterface ni = interfaces.nextElement();
+
+            try {
+                NetworkInterfaceView niv = new NetworkInterfaceView(ni);
+                builder.add(new InterfaceAndViewPair(ni, niv));
+            } catch( SocketException e) {
+                // Silently skip interfaces where no snapshot can be created.
+            }
+        }
+
+        _interfaces = builder.build();
+        assumeTrue(_interfaces.size() > 0);
+    }
+
+    @Test
+    public void testGetDisplayName()  {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+
+            assertEquals(ni.getDisplayName(), niv.getDisplayName());
+        }
+    }
+
+    @Test
+    public void testGetHardwareAddress() throws SocketException {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+
+            assertArrayEquals(ni.getHardwareAddress(), niv.getHardwareAddress());
+        }
+    }
+
+    @Test
+    public void testGetInetAddresses() {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+
+            List<InetAddress> niInetAddresses = Collections.list(ni.getInetAddresses());
+            List<InetAddress> nivInetAddresses = Collections.list(niv.getInetAddresses());
+
+            assertEquals(niInetAddresses, nivInetAddresses);
+        }
+    }
+
+    @Test
+    public void testGetInterfaceAddresses() {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+
+            List<InterfaceAddress> sourceAddresses = ni.getInterfaceAddresses();
+            List<NetworkInterfaceView.InterfaceAddressView> viewAddresses = niv.getInterfaceAddresses();
+
+            assertEquals(sourceAddresses.size(), viewAddresses.size());
+
+            for( int i = 0; i < sourceAddresses.size(); i++) {
+                InterfaceAddress source = sourceAddresses.get(i);
+                NetworkInterfaceView.InterfaceAddressView expected = new NetworkInterfaceView.InterfaceAddressView(source);
+                NetworkInterfaceView.InterfaceAddressView actual = viewAddresses.get(i);
+
+                assertEquals(expected, actual);
+                assertEquals(expected.toString(), actual.toString());
+            }
+        }
+    }
+
+    @Test
+    public void testGetMTU() throws SocketException {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+
+            assertEquals(ni.getMTU(), niv.getMTU());
+        }
+    }
+
+    @Test
+    public void testGetName() {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+
+            assertEquals(ni.getName(), niv.getName());
+        }
+    }
+
+    @Test
+    public void testGetParent() {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+
+            assertNull(niv.getParent());
+            assertAllSubInterfacesHaveParent(niv);
+        }
+    }
+
+    @Test
+    public void testIsLoopback() throws SocketException {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+            assertEquals(ni.isLoopback(), niv.isLoopback());
+        }
+    }
+
+    @Test
+    public void testIsPointToPoint() throws SocketException {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+            assertEquals(ni.isPointToPoint(), niv.isPointToPoint());
+        }
+    }
+
+    @Test
+    public void testIsUp() throws SocketException {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+            assertEquals(ni.isUp(), niv.isUp());
+        }
+    }
+
+    @Test
+    public void testIsVirtual() {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+            assertEquals(ni.isVirtual(), niv.isVirtual());
+        }
+    }
+
+    @Test
+    public void testSupportsMulticast() throws SocketException {
+        for(InterfaceAndViewPair pair : _interfaces) {
+            NetworkInterface ni = pair.getNetworkInterface();
+            NetworkInterfaceView niv = pair.getNetworkInterfaceView();
+            assertEquals(ni.supportsMulticast(), niv.supportsMulticast());
+        }
+    }
+
+    private void assertAllSubInterfacesHaveParent(NetworkInterfaceView parent) {
+        for( NetworkInterfaceView subInterface : parent.getSubInterfaces()) {
+            assertEquals(parent, subInterface.getParent());
+            assertAllSubInterfacesHaveParent(subInterface);
+        }
+    }
+
+    /**
+     * A simple data class to hold a network interface and
+     * the corresponding NetworkInterfaceView snapshot.
+     */
+    private static class InterfaceAndViewPair {
+        private final NetworkInterface _ni;
+        private final NetworkInterfaceView _niv;
+        public InterfaceAndViewPair(NetworkInterface ni, NetworkInterfaceView niv) {
+            _ni = ni;
+            _niv = niv;
+        }
+
+        public NetworkInterface getNetworkInterface() {
+            return _ni;
+        }
+
+        public NetworkInterfaceView getNetworkInterfaceView() {
+            return _niv;
+        }
+    }
+}
