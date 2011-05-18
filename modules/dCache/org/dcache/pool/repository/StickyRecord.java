596439765de4c4c95046b3eaec94313b3bcc6c10
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2011-05-18 10:57:22 +0000

    pool: fix how sticky record describes itself
    Signed-off-by: Paul Millar
    merge: trunk@15612

--- modules/dCache/org/dcache/pool/repository/StickyRecord.java
+++ modules/dCache/org/dcache/pool/repository/StickyRecord.java
@@ -1,54 +1,74 @@
 package org.dcache.pool.repository;
 
 import java.io.Serializable;
+import java.text.DateFormat;
 
 public class StickyRecord implements Serializable {
 
     static final long serialVersionUID = 8235126040387514086L;
-	
+
+    private static final DateFormat df = DateFormat.getInstance();
+    public static final long NON_EXPIRING = -1;
+
 	private final String _owner;
-	private final long   _expire;	
-	
-	public StickyRecord(String owner, long expire) {
+	private final long   _expire;
+
+
+	public StickyRecord(String owner, long expire)
+	{
 		_owner = owner;
 		_expire = expire;
-	}	
-	
-	public boolean isValid() {
-		return isValidAt( System.currentTimeMillis() );
 	}
-	
-	public boolean isValidAt(long time) {
-		return _expire == -1 || _expire > time;
+
+	public boolean isValid()
+	{
+		return isValidAt(System.currentTimeMillis());
 	}
-	
-	public long expire() {
+
+	public boolean isValidAt(long time)
+	{
+		return isNonExpiring() || _expire > time;
+	}
+
+	private boolean isNonExpiring()
+	{
+	    return _expire == NON_EXPIRING;
+	}
+
+	public long expire()
+	{
 		return _expire;
 	}
-	
-	public String owner() {
+
+	public String owner()
+	{
 		return _owner;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		
-		if ( ! (obj instanceof StickyRecord) ) return false;
-		
-		
-		return ((StickyRecord)obj).owner().equals(_owner) && ((StickyRecord)obj).expire() == _expire;
+	public boolean equals(Object obj)
+	{
+		if( !(obj instanceof StickyRecord)) {
+		    return false;
+		}
+
+		StickyRecord other = (StickyRecord) obj;
+
+		return other.owner().equals(_owner) && other.expire() == _expire;
 	}
 
 
 	@Override
-	public int hashCode() {		
+	public int hashCode()
+	{
 		return _owner.hashCode();
 	}
 
 
 	@Override
-	public String toString() {	
-		return _owner + ":" + _expire;
+	public synchronized String toString()
+	{
+	    String expiryLabel = isNonExpiring() ? "never expires" : "expires " + df.format(_expire);
+		return _owner + " : " + expiryLabel;
 	}
-		
 }
