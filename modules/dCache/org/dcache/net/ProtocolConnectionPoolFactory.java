22cfce944fa089da427a13b0558c1d7b8e0efb70
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2011-06-09 08:21:50 +0000

    pool: Fix socket option handling in DCAP mover Signed-off-by: Gerd Behrmann merge: trunk@15731

--- modules/dCache/org/dcache/net/ProtocolConnectionPoolFactory.java
+++ modules/dCache/org/dcache/net/ProtocolConnectionPoolFactory.java
@@ -7,30 +7,32 @@ import java.io.IOException;
 
 public class ProtocolConnectionPoolFactory {
 
-	
+
 	private static Object _initLock = new Object();
 	private static ProtocolConnectionPool _protocolConnectionPool = null;
-	
+
 	private int _port;
 	ChallengeReader _challengeReader = null;
-	
+
 	public ProtocolConnectionPoolFactory(int port, ChallengeReader challengeReader) {
 		_port = port;
 		_challengeReader = challengeReader;
 	}
-	
-	public ProtocolConnectionPool getConnectionPool() throws IOException {
+
+	public ProtocolConnectionPool getConnectionPool(int receiveBufferSize)
+            throws IOException
+        {
 		synchronized(_initLock){
 			if( _protocolConnectionPool == null ) {
-				_protocolConnectionPool = new ProtocolConnectionPool(_port, _challengeReader);
+                                _protocolConnectionPool = new ProtocolConnectionPool(_port, receiveBufferSize, _challengeReader);
 				_protocolConnectionPool.start();
 			}
 		}
-		
+
 		return _protocolConnectionPool;
 	}
-	
-	
+
+
 }
 /*
  * $Log: not supported by cvs2svn $
