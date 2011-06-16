46b4d9031fae7b955ed164f20b3f7c55751b915e
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2011-06-16 12:42:10 +0000

    nfsv4: optionally start v3 if required
    Required to run v3 and v4 at the same time.
    small cleanups in v3.
    
    
    In a long run dedicated v4 door have to be removed.
    
    Acked-by: Tatjana Baranova
    Patch: http://rb.dcache.org/r/3466/
    Ticket: #5871

--- modules/dCache/org/dcache/chimera/nfsv41/door/NFSv3.java
+++ modules/dCache/org/dcache/chimera/nfsv41/door/NFSv3.java
@@ -1,9 +1,5 @@
 package org.dcache.chimera.nfsv41.door;
 
-import java.net.InetAddress;
-import org.acplt.oncrpc.OncRpcPortmapClient;
-import org.acplt.oncrpc.OncRpcProtocols;
-import org.acplt.oncrpc.apps.jportmap.OncRpcEmbeddedPortmap;
 import org.dcache.chimera.FileSystemProvider;
 import org.dcache.chimera.nfs.ExportFile;
 import org.dcache.chimera.nfs.v3.MountServer;
@@ -12,6 +8,7 @@ import org.dcache.chimera.nfs.v3.xdr.mount_prot;
 import org.dcache.chimera.nfs.v3.xdr.nfs3_prot;
 import org.dcache.xdr.OncRpcProgram;
 import org.dcache.xdr.OncRpcSvc;
+import org.dcache.xdr.portmap.OncRpcEmbeddedPortmap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,49 +18,26 @@ import org.slf4j.LoggerFactory;
 public class NFSv3 {
 
     private final static Logger _log = LoggerFactory.getLogger(NFSv3.class);
-    static final int DEFAULT_PORT = 2049;
 
     private FileSystemProvider _fs;
     private ExportFile _exports;
     private OncRpcSvc _service;
 
+    /**
+     * TCP port number to bind.
+     */
+    private int _port;
+
     public void init() throws Exception {
 
-        _log.info("starting NFSv3 on: {}", DEFAULT_PORT);
+        _log.info("starting NFSv3 on: {}", _port);
 
-        new OncRpcEmbeddedPortmap(2000);
-
-        OncRpcPortmapClient portmap = new OncRpcPortmapClient(InetAddress.getByName("127.0.0.1"));
-        portmap.getOncRpcClient().setTimeout(2000);
-
-        if (!portmap.setPort(mount_prot.MOUNT_PROGRAM, mount_prot.MOUNT_V3, OncRpcProtocols.ONCRPC_TCP, DEFAULT_PORT)) {
-            _log.error("Failed to register mountv1 service within portmap.");
-        }
-
-        if (!portmap.setPort(mount_prot.MOUNT_PROGRAM, mount_prot.MOUNT_V3, OncRpcProtocols.ONCRPC_UDP, DEFAULT_PORT)) {
-            _log.error("Failed to register mountv1 service within portmap.");
-        }
-
-        if (!portmap.setPort(mount_prot.MOUNT_PROGRAM, mount_prot.MOUNT_V1, OncRpcProtocols.ONCRPC_TCP, DEFAULT_PORT)) {
-            _log.error("Failed to register mountv3 service within portmap.");
-        }
-
-        if (!portmap.setPort(mount_prot.MOUNT_PROGRAM, mount_prot.MOUNT_V1, OncRpcProtocols.ONCRPC_UDP, DEFAULT_PORT)) {
-            _log.error("Failed to register mountv3 service within portmap.");
-        }
-
-        if (!portmap.setPort(nfs3_prot.NFS_PROGRAM, nfs3_prot.NFS_V3, OncRpcProtocols.ONCRPC_TCP, DEFAULT_PORT)) {
-            _log.error("Failed to register NFSv3 service within portmap.");
-        }
-
-        if (!portmap.setPort(nfs3_prot.NFS_PROGRAM, nfs3_prot.NFS_V3, OncRpcProtocols.ONCRPC_UDP, DEFAULT_PORT)) {
-            _log.error("Failed to register NFSv3 service within portmap.");
-        }
+        new OncRpcEmbeddedPortmap();
 
         NfsServerV3 nfs3 = new NfsServerV3(_exports, _fs);
         MountServer ms = new MountServer(_exports, _fs);
 
-        _service = new OncRpcSvc(DEFAULT_PORT);
+        _service = new OncRpcSvc(_port);
         _service.register(new OncRpcProgram(nfs3_prot.NFS_PROGRAM, nfs3_prot.NFS_V3), nfs3);
         _service.register(new OncRpcProgram(mount_prot.MOUNT_PROGRAM, mount_prot.MOUNT_V3), ms);
         _service.start();
@@ -80,4 +54,9 @@ public class NFSv3 {
     public void setExportFile(ExportFile export) {
         _exports = export;
     }
+
+    public void setPortNumber(int port) {
+        _port = port;
+    }
+
 }
