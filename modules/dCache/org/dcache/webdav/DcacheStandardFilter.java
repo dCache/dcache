8c4ea633ad6bb2839aa0b35ca17c2503f82bd597
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2011-06-27 08:31:39 +0000

    webdav: improving webdav style pages.
    Signed-off-by: Tatjana Baranova
    merge: trunk@15853
    merge: trunk@15859

--- modules/dCache/org/dcache/webdav/DcacheStandardFilter.java
+++ modules/dCache/org/dcache/webdav/DcacheStandardFilter.java
@@ -1,9 +1,5 @@
 package org.dcache.webdav;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
