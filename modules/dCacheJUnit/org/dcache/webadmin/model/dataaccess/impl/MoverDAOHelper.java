70b5c3bf506ced344368c3571f9d5ce316ec07af
Jan Schaefer <jan.schaefer@desy.de>
2011-06-30 09:34:17 +0000

    forgot svn add with patch http://rb.dcache.org/r/3546/

---
+++ modules/dCacheJUnit/org/dcache/webadmin/model/dataaccess/impl/MoverDAOHelper.java
@@ -0,0 +1,25 @@
+package org.dcache.webadmin.model.dataaccess.impl;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import org.dcache.admin.webadmin.datacollector.datatypes.MoverInfo;
+import org.dcache.webadmin.model.dataaccess.MoverDAO;
+import org.dcache.webadmin.model.exceptions.DAOException;
+
+/**
+ *
+ * @author jans
+ */
+public class MoverDAOHelper implements MoverDAO {
+
+    @Override
+    public List<MoverInfo> getActiveTransfers() throws DAOException {
+        return Collections.EMPTY_LIST;
+    }
+
+    @Override
+    public void killMoversOnSinglePool(Set<Integer> jobids, String targetPool) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
