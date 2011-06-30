d55b143a6e3361a880e3ff5eadccac23bd12b719
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2011-06-30 10:22:05 +0000

    nfsv4: add nfs.domain property
    The local NFSv4 domain name. An NFSv4 domain is a namespace
    with a unique username<->UID and groupname<->GID mapping. Used
    in multi-domain environment. Currently we simply strip them away.
    
    Acked-By: Gerd Behrmann
    Patch: http://rb.dcache.org/r/3552/

--- modules/dCache/org/dcache/chimera/nfsv41/door/StrategyIdMapper.java
+++ modules/dCache/org/dcache/chimera/nfsv41/door/StrategyIdMapper.java
@@ -1,5 +1,6 @@
 package org.dcache.chimera.nfsv41.door;
 
+import com.google.common.base.Strings;
 import diskCacheV111.util.CacheException;
 import java.security.Principal;
 import java.util.Set;
@@ -17,9 +18,11 @@ public class StrategyIdMapper implements NfsIdMapping {
     private final String NOBODY = "nobody";
     private final LoginStrategy _remoteLoginStrategy;
     private final static Logger _log = LoggerFactory.getLogger(StrategyIdMapper.class);
+    private final String _domain;
 
-    public StrategyIdMapper(LoginStrategy remoteLoginStrategy) {
+    public StrategyIdMapper(LoginStrategy remoteLoginStrategy, String domain) {
         _remoteLoginStrategy = remoteLoginStrategy;
+        _domain = Strings.emptyToNull(domain);
     }
 
     @Override
@@ -33,37 +36,39 @@ public class StrategyIdMapper implements NfsIdMapping {
             Set<Principal> principals = _remoteLoginStrategy.reverseMap(new GidPrincipal(id, false));
             for (Principal principal : principals) {
                 if (principal instanceof GroupNamePrincipal) {
-                    return principal.getName();
+                    return addDomain(principal.getName());
                 }
             }
         } catch (CacheException e) {
-            _log.warn("Failed to reverseMap for gid: " + id, e);
+            _log.warn("Failed to reverseMap for gid {} : {}", id, e);
         }
         return NOBODY;
     }
 
     @Override
-    public int principalToGid(String principal) {
+    public int principalToGid(String name) {
         try {
+            String principal = stripDomain(name);
             Principal gidPrincipal = _remoteLoginStrategy.map(new GroupNamePrincipal(principal));
             if (gidPrincipal instanceof GidPrincipal) {
                 return (int) ((GidPrincipal) gidPrincipal).getGid();
             }
         } catch (CacheException e) {
-            _log.warn("Failed to map pringipal: " + principal, e);
+            _log.warn("Failed to map pringipal {} : {}", name, e);
         }
         return -1;
     }
 
     @Override
-    public int principalToUid(String principal) {
+    public int principalToUid(String name) {
         try {
+            String principal = stripDomain(name);
             Principal uidPrincipal = _remoteLoginStrategy.map(new UserNamePrincipal(principal));
             if (uidPrincipal instanceof UidPrincipal) {
                 return (int) ((UidPrincipal) uidPrincipal).getUid();
             }
         } catch (CacheException e) {
-             _log.warn("Failed to map pringipal: " + principal, e);
+             _log.warn("Failed to map pringipal {} : {}", name, e);
         }
         return -1;
     }
@@ -79,12 +84,24 @@ public class StrategyIdMapper implements NfsIdMapping {
             Set<Principal> principals = _remoteLoginStrategy.reverseMap(new UidPrincipal(id));
             for (Principal principal : principals) {
                 if (principal instanceof UserNamePrincipal) {
-                    return principal.getName();
+                    return addDomain(principal.getName());
                 }
             }
         } catch (CacheException e) {
-             _log.warn("Failed to reverseMap for uid: " + id, e);
+             _log.warn("Failed to reverseMap for uid {} : {}", id, e);
         }
         return NOBODY;
+    }
+
+    private String stripDomain(String s) {
+        int n = s.indexOf('@');
+        if (n != -1) {
+            return s.substring(0, n);
+        }
+        return s;
+    }
+
+    private String addDomain(String s) {
+        return _domain == null? s : s + "@" + _domain;
     }
 }
