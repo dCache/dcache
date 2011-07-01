1ec9d0a1b21a650957bb012550b90af1af449b57
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2011-07-01 14:01:31 +0000

    nfsv40: fix setclientid reply
    Signed-off-by: Tigran Mkrtchyan
    merge: trunk@15947

--- modules/dCache/org/dcache/chimera/nfs/v4/OperationSETCLIENTID.java
+++ modules/dCache/org/dcache/chimera/nfs/v4/OperationSETCLIENTID.java
@@ -17,6 +17,7 @@
 
 package org.dcache.chimera.nfs.v4;
 
+import java.net.Inet6Address;
 import org.dcache.chimera.nfs.v4.xdr.nfsstat4;
 import org.dcache.chimera.nfs.v4.xdr.verifier4;
 import org.dcache.chimera.nfs.v4.xdr.uint64_t;
@@ -26,6 +27,8 @@ import org.dcache.chimera.nfs.v4.xdr.nfs_opnum4;
 import org.dcache.chimera.nfs.v4.xdr.SETCLIENTID4resok;
 import org.dcache.chimera.nfs.v4.xdr.SETCLIENTID4res;
 import org.dcache.chimera.nfs.ChimeraNFSException;
+import org.dcache.chimera.nfs.v4.xdr.clientaddr4;
+import org.dcache.chimera.nfs.v4.xdr.netaddr4;
 import org.dcache.utils.net.InetSocketAddresses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,18 +49,23 @@ public class OperationSETCLIENTID extends AbstractNFSv4Operation {
 
 	        try {
 
-
 	            verifier4 verifier = _args.opsetclientid.client.verifier;
-
-	            if( context.getStateHandler().getClientByVerifier(verifier) != null ) {
-                    throw new ChimeraNFSException(nfsstat4.NFS4ERR_CLID_INUSE, "Client Id In use");
-	            }
+                    NFS4Client client = context.getStateHandler().getClientByVerifier(verifier);
+                    if (client != null) {
+                        netaddr4 addr = new netaddr4();
+                        addr.na_r_netid = client.getRemoteAddress().getAddress() instanceof Inet6Address ?
+                                "tcp6" : "tcp";
+                        addr.na_r_addr = InetSocketAddresses.uaddrOf(client.getRemoteAddress());
+                        res.status = nfsstat4.NFS4ERR_CLID_INUSE;
+                        res.client_using = new clientaddr4(addr);
+                        throw new ChimeraNFSException(nfsstat4.NFS4ERR_CLID_INUSE, "Client Id In use");
+                    }
 
 		        String r_addr = _args.opsetclientid.callback.cb_location.na_r_addr;
 		        String r_netid = _args.opsetclientid.callback.cb_location.na_r_netid;
 		        int program = _args.opsetclientid.callback.cb_program.value;
 
-                        NFS4Client client = new NFS4Client(context.getRpcCall().getTransport().getRemoteSocketAddress(),
+                        client = new NFS4Client(context.getRpcCall().getTransport().getRemoteSocketAddress(),
                                 context.getRpcCall().getTransport().getLocalSocketAddress(),
                                 _args.opsetclientid.client.id,_args.opsetclientid.client.verifier, null );
 		        try {
