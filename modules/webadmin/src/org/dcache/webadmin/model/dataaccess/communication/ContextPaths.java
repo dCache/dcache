e5d7ec130973fe91f38d6ef9dd6f03a6b15e6fbc
Jan Schaefer <jan.schaefer@desy.de>
2011-08-23 15:20:45 +0000

    Webadmin: Introducing new Collector for Linkgroups and SpaceTokens
    This collector is getting it's information from the SrmSpaceManager. Nothing special, just like the other 3. Later it may make sense to refactor to ScheduledExecutors maybe. This makes the SpaceToken Page ready to be taken away from info as datasource.
    
    Patch: http://rb.dcache.org/r/3648/
    
    Acked-By: Karsten
    
    Target: trunk

--- modules/webadmin/src/org/dcache/webadmin/model/dataaccess/communication/ContextPaths.java
+++ modules/webadmin/src/org/dcache/webadmin/model/dataaccess/communication/ContextPaths.java
@@ -9,4 +9,6 @@ public interface ContextPaths {
     public static final String MOVER_LIST = "transfers.list";
     public static final String CELLINFO_LIST = "cellinfo.list";
     public static final String POOLMONITOR = "PoolMonitor";
+    public static final String SPACETOKENS = "Spacetokens";
+    public static final String LINKGROUPS = "LinkGroups";
 }
---
+++ modules/webadmin/src/org/dcache/webadmin/model/dataaccess/communication/collectors/SpaceTokenCollector.java
@@ -0,0 +1,62 @@
+package org.dcache.webadmin.model.dataaccess.communication.collectors;
+
+import diskCacheV111.services.space.message.GetLinkGroupsMessage;
+import diskCacheV111.services.space.message.GetSpaceTokensMessage;
+import diskCacheV111.util.CacheException;
+import org.dcache.webadmin.model.dataaccess.communication.ContextPaths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author jans
+ */
+public class SpaceTokenCollector extends Collector {
+
+    private final static Logger _log = LoggerFactory.getLogger(SpaceTokenCollector.class);
+
+    @Override
+    public void run() {
+
+        try {
+            for (;;) {
+                try {
+                    collectSpaceTokens();
+                    collectLinkGroups();
+//                  catch everything - maybe next round it works out
+                } catch (RuntimeException e) {
+                    _log.debug(e.toString(), e);
+                }
+                Thread.sleep(10000);
+            }
+        } catch (InterruptedException e) {
+            _log.info("Space token Collector interrupted");
+        }
+    }
+
+    private void collectSpaceTokens() throws InterruptedException {
+        try {
+            _log.debug("Retrieving space tokens");
+            GetSpaceTokensMessage reply = _cellStub.sendAndWait(
+                    new GetSpaceTokensMessage());
+            _pageCache.put(ContextPaths.SPACETOKENS,
+                    reply.getSpaceTokenSet());
+            _log.debug("Space tokens retrieved successfully");
+        } catch (CacheException ex) {
+            _log.debug("Could not retrieve Space tokens ", ex);
+        }
+    }
+
+    private void collectLinkGroups() throws InterruptedException {
+        try {
+            _log.debug("Retrieving linkgroups");
+            GetLinkGroupsMessage reply = _cellStub.sendAndWait(
+                    new GetLinkGroupsMessage());
+            _pageCache.put(ContextPaths.LINKGROUPS,
+                    reply.getLinkGroupSet());
+            _log.debug("Linkgroups retrieved successfully");
+        } catch (CacheException ex) {
+            _log.debug("Could not retrieve linkgroups ", ex);
+        }
+    }
+}
