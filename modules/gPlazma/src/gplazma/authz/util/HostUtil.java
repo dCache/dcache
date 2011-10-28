1033d8a0f63c997bf8369ed29eed61933ed39b2c
Karsten Schwank <karsten.schwank@desy.de>
2011-10-28 10:13:13 +0000

    IPv6: HostUtil
    Refactoring of HostUtil.
    
    * Reorganised Imports
    * getHosts does not return null any more, which btw would have led to a NPE in XACMLAuthorizationPlugin
    * improved readability in boolean expressions (positive formulation)
    ! Removed check for IPv4 address -> also IPv6 addresses are accepted now
    * removed faulty toArray call
    * some code reformatting (sorry)
    
    Patch: http://rb.dcache.org/r/3874/
    
    Acked-by: Jan Schaefer
    
    Target: default

--- modules/gPlazma/src/gplazma/authz/util/HostUtil.java
+++ modules/gPlazma/src/gplazma/authz/util/HostUtil.java
@@ -1,24 +1,23 @@
 package gplazma.authz.util;
 
-import org.ietf.jgss.GSSContext;
-import org.ietf.jgss.GSSException;
-import org.ietf.jgss.GSSCredential;
-import org.ietf.jgss.GSSManager;
-import org.globus.gsi.GlobusCredential;
-import org.globus.gsi.GlobusCredentialException;
-import org.globus.gsi.TrustedCertificates;
-import org.globus.gsi.GSIConstants;
-import org.globus.gsi.gssapi.GlobusGSSCredentialImpl;
-import org.globus.gsi.gssapi.GSSConstants;
-import org.gridforum.jgss.ExtendedGSSManager;
-import org.gridforum.jgss.ExtendedGSSContext;
-
 import java.net.InetAddress;
 import java.net.NetworkInterface;
-import java.net.Inet4Address;
 import java.net.SocketException;
 import java.util.Enumeration;
 import java.util.Vector;
+
+import org.globus.gsi.GSIConstants;
+import org.globus.gsi.GlobusCredential;
+import org.globus.gsi.GlobusCredentialException;
+import org.globus.gsi.TrustedCertificates;
+import org.globus.gsi.gssapi.GSSConstants;
+import org.globus.gsi.gssapi.GlobusGSSCredentialImpl;
+import org.gridforum.jgss.ExtendedGSSContext;
+import org.gridforum.jgss.ExtendedGSSManager;
+import org.ietf.jgss.GSSContext;
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
+import org.ietf.jgss.GSSManager;
 
 /**
  * HostUtil.java.
@@ -31,9 +30,6 @@ public class HostUtil {
 
     public static String[] getHosts() throws SocketException {
         InetAddress[] addresses = getInetAddress();
-
-        if( (addresses == null) || ( addresses.length == 0 ) ) return null;
-
         String[] hosts = new String[addresses.length];
 
         /**
@@ -46,77 +42,66 @@ public class HostUtil {
         for( int i = 0; i < addresses.length; i++) {
             InetAddress addr = addresses[i];
 
-            if( !addr.isLinkLocalAddress() && !addr.isLoopbackAddress() &&
-                    !addr.isSiteLocalAddress() && !addr.isMulticastAddress()) {
-                hosts [nextExternalIfIndex++] = addr.getHostName();
-            } else {
+            if( addr.isLinkLocalAddress()
+                    || addr.isLoopbackAddress()
+                    || addr.isSiteLocalAddress()
+                    || addr.isMulticastAddress()) {
                 hosts [nextInternalIfIndex--] = addr.getHostName();
+            } else {
+                hosts [nextExternalIfIndex++] = addr.getHostName();
             }
         }
-
         return hosts;
     }
 
     public static InetAddress[] getInetAddress() throws SocketException {
-        InetAddress[] addresses = null;
-
         /**
          *  put all local Ip addresses, except loopback
          */
-
         Enumeration<NetworkInterface> ifList = NetworkInterface.getNetworkInterfaces();
-
         Vector<InetAddress> v = new Vector<InetAddress>();
         while( ifList.hasMoreElements() ) {
-
             NetworkInterface ne = ifList.nextElement();
 
-            Enumeration<InetAddress> ipList = ne.getInetAddresses();
-            while( ipList.hasMoreElements() ) {
-                InetAddress ia = ipList.nextElement();
-                // Currently we do not handle ipv6
-                if( ! (ia instanceof Inet4Address) ) continue;
-                if( ! ia.isLoopbackAddress() ) {
-                    v.add( ia ) ;
-                }
+            Enumeration<InetAddress> ipEnumeration = ne.getInetAddresses();
+            while( ipEnumeration.hasMoreElements() ) {
+                InetAddress ia = ipEnumeration.nextElement();
+                if( ! ia.isLoopbackAddress() ) { v.add( ia ) ; }
             }
         }
-        addresses = v.toArray( new InetAddress[ v.size() ] );
-
-        return addresses;
+        return v.toArray( new InetAddress[0] );
     }
 
     public static GSSContext getServiceContext() throws GSSException {
-        return getServiceContext(X509CertUtil.default_service_cert, X509CertUtil.default_service_key, X509CertUtil.default_trusted_cacerts);
+        return getServiceContext(
+                X509CertUtil.default_service_cert,
+                X509CertUtil.default_service_key,
+                X509CertUtil.default_trusted_cacerts);
     }
 
-    public static GSSContext getServiceContext(String service_cert, String service_key, String service_trusted_certs) throws GSSException {
+    public static GSSContext getServiceContext(
+            String service_cert,
+            String service_key,
+            String service_trusted_certs) throws GSSException {
 
         GlobusCredential serviceCredential;
         try {
-            serviceCredential =new GlobusCredential(
-                    service_cert,
-                    service_key
-                    );
+            serviceCredential = new GlobusCredential( service_cert, service_key );
         } catch(GlobusCredentialException gce) {
-            throw new GSSException(GSSException.NO_CRED ,
-                    0,
+            throw new GSSException(GSSException.NO_CRED, 0,
                     "could not load host globus credentials "+gce.toString());
         }
 
-
-        GSSCredential cred = new GlobusGSSCredentialImpl(serviceCredential,
+        GSSCredential cred = new GlobusGSSCredentialImpl(
+                serviceCredential,
                 GSSCredential.INITIATE_AND_ACCEPT);
-        TrustedCertificates trusted_certs =
-                TrustedCertificates.load(service_trusted_certs);
-        GSSManager manager = ExtendedGSSManager.getInstance();
-        ExtendedGSSContext context =
-                (ExtendedGSSContext) manager.createContext(cred);
 
-        context.setOption(GSSConstants.GSS_MODE,
-                GSIConstants.MODE_GSI);
-        context.setOption(GSSConstants.TRUSTED_CERTIFICATES,
-                trusted_certs);
+        TrustedCertificates trusted_certs = TrustedCertificates.load(service_trusted_certs);
+        GSSManager manager = ExtendedGSSManager.getInstance();
+        ExtendedGSSContext context = (ExtendedGSSContext) manager.createContext(cred);
+
+        context.setOption(GSSConstants.GSS_MODE, GSIConstants.MODE_GSI);
+        context.setOption(GSSConstants.TRUSTED_CERTIFICATES, trusted_certs);
 
         return context;
     }
