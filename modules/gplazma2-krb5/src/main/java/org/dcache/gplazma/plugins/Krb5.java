575378918e9dcdb70e3597745e3e8bcfe28853e9
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2011-11-30 09:33:50 +0000

    gplazma2: implement GPlazmaAuthenticationPlugin in krb5 plugin
    fix ConcurrentModificationException
    
    Requires working kerberos5 installation on gPlazma node.
    
    Acked-by: Jan Schaefer
    Patch: http://rb.dcache.org/r/3989/
    
    RELEASE NOTES:
    
      gplazma2 support password authentication with kerberos5

--- modules/gplazma2-krb5/src/main/java/org/dcache/gplazma/plugins/Krb5.java
+++ modules/gplazma2-krb5/src/main/java/org/dcache/gplazma/plugins/Krb5.java
@@ -3,23 +3,35 @@ package org.dcache.gplazma.plugins;
 import java.security.Principal;
 import java.util.Properties;
 import java.util.Set;
+import javax.security.auth.callback.*;
 import javax.security.auth.kerberos.KerberosPrincipal;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import org.dcache.auth.PasswordCredential;
 import org.dcache.auth.UserNamePrincipal;
 import org.dcache.gplazma.AuthenticationException;
 import org.dcache.gplazma.SessionID;
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterables.getFirst;
+import java.util.HashSet;
 
 /**
- * A {@link GPlazmaMappingPlugin} implementation which converts {@code user@DOMAIN.COM}
- * to {@link UserNamePrincipal} corresponding to  {@code user} ( without domain ).
+ * A {@link GPlazmaMappingPlugin} and {@link GPlazmaAuthenticationPlugin}
+ * implementation which verifies user/pasword combination against kerberos5
+ * server and converts {@code user@DOMAIN.COM} to {@link UserNamePrincipal}
+ * corresponding to {@code user} ( without domain ).
  * For more complex mappings, like {@code remte-user@DOMAIN.COM} to {@code local-user},
  * {@link GridMapFilePlugin} can be used.
  *
  * To enable, add following likes into gplazma.conf:
  * <pre>
+ *     <b>auth optional krb5</b>
  *     <b>map requisite krb5</b>
  * </pre>
  */
-public class Krb5 implements GPlazmaMappingPlugin {
+public class Krb5 implements GPlazmaMappingPlugin, GPlazmaAuthenticationPlugin {
+
+    private static final String JAAS_CONFIG_NAME = "Krb5Gplazma";
 
     public Krb5(Properties properties) {
         /*
@@ -28,12 +40,48 @@ public class Krb5 implements GPlazmaMappingPlugin {
     }
 
     @Override
+    public void authenticate(SessionID sID, Set<Object> publicCredentials, Set<Object> privateCredentials, Set<Principal> identifiedPrincipals) throws AuthenticationException {
+        LoginContext loginContext = null;
+
+        PasswordCredential password =
+                getFirst(filter(privateCredentials, PasswordCredential.class), null);
+        if (password == null) {
+            throw new AuthenticationException("No login name provided");
+        }
+
+        try {
+
+            loginContext = new LoginContext(JAAS_CONFIG_NAME, new PasswordCallbackHandler(password) );
+
+            loginContext.login();
+            identifiedPrincipals.addAll(loginContext.getSubject().getPrincipals());
+            tryToLogout(loginContext);
+
+        } catch (LoginException e) {
+            throw new AuthenticationException("Failed to autorize:", e);
+        }
+    }
+
+    private static void tryToLogout(LoginContext loginContext) {
+        if(loginContext == null)
+            return;
+
+        try {
+            loginContext.logout();
+        } catch (LoginException e) {
+            // ignored
+        }
+    }
+
+    @Override
     public void map(SessionID sID, Set<Principal> principals, Set<Principal> authorizedPrincipals) throws AuthenticationException {
+        Set<Principal> kerberosPrincipals = new HashSet<Principal>();
         for (Principal principal : principals) {
             if (principal instanceof KerberosPrincipal) {
-                principals.add(new UserNamePrincipal(stripDomain(principal.getName())));
+                kerberosPrincipals.add(new UserNamePrincipal(stripDomain(principal.getName())));
             }
         }
+        principals.addAll(kerberosPrincipals);
     }
 
     private String stripDomain(String s) {
@@ -42,5 +90,31 @@ public class Krb5 implements GPlazmaMappingPlugin {
             return s.substring(0, n);
         }
         return s;
+    }
+
+    private static class PasswordCallbackHandler implements CallbackHandler {
+
+        private final String _userName;
+        private final char _password[];
+
+        public PasswordCallbackHandler(PasswordCredential credential) {
+            _userName = credential.getUsername();
+            _password = credential.getPassword().toCharArray();
+        }
+
+        @Override
+        public void handle(Callback callbacks[]) throws UnsupportedCallbackException {
+            for (Callback callback : callbacks) {
+                if (callback instanceof NameCallback) {
+                    ((NameCallback) callback).setName(_userName);
+                } else if (callback instanceof PasswordCallback) {
+                    ((PasswordCallback) callback).setPassword(_password);
+                } else if (callback instanceof TextOutputCallback) {
+                    // do nothing
+                } else {
+                    throw new UnsupportedCallbackException(callback, "Unrecognized Callback");
+                }
+            }
+        }
     }
 }
