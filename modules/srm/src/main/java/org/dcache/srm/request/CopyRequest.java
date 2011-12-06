cdca4350f7a63a2e347f888ff1ba0b62a7f9bf21
Dmitry Litvintsev <litvinse@fnal.gov>
2011-12-06 16:31:10 +0000

    change error log level to debug log level. srmcp 3rd party transfer generate extraneous error log messages. This patch fixes it.
    Target: trunk
    Patch: http://rb.dcache.org/r/4014/
    Acked-by: Gerd Behrmann

--- modules/srm/src/main/java/org/dcache/srm/request/CopyRequest.java
+++ modules/srm/src/main/java/org/dcache/srm/request/CopyRequest.java
@@ -940,7 +940,7 @@ public final class CopyRequest extends ContainerRequest implements PropertyChang
 
             TurlGetterPutter a_getter_putter = getGetter_putter();
             if(a_getter_putter != null) {
-                logger.error("copyRequest getter_putter is non null, stopping");
+                logger.debug("copyRequest getter_putter is non null, stopping");
                 a_getter_putter.stop();
             }
             logger.debug("copy request state changed to "+state);
@@ -950,7 +950,7 @@ public final class CopyRequest extends ContainerRequest implements PropertyChang
                     if(!(State.isFinalState(fr_state)))
                     {
 
-                        logger.error("changing fr#"+request.getId()+" to "+state);
+                        logger.debug("changing fr#"+request.getId()+" to "+state);
                             request.setState(state,"Request state changed, changing file state");
                     }
                 }
