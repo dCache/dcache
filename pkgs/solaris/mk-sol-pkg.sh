1cebeab4af80e2b62fcb2364542c19e0629de4f7
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2011-12-14 09:48:32 +0000

    build: fix solaris packageing
    due to new structure 'build' directory is not
    created any more This patch adds the creation
    of the directory into packaging step.
    
    Acked-by: Paul Millar
    Patch: http://rb.dcache.org//r/4057/

--- pkgs/solaris/mk-sol-pkg.sh
+++ pkgs/solaris/mk-sol-pkg.sh
@@ -11,6 +11,7 @@ echo 'i pkginfo' > prototype
 find . | grep -v '^./prototype$' | grep -v '^./pkginfo$' | pkgproto \
     | awk '{ print $1, $2, $3, $4, "root", "bin" }' >> prototype
 
+mkdir -p ${BUILDDIR}
 pkgmk -o -d ${BUILDDIR} -r .
 pkgtrans -s ${BUILDDIR} ${DISTDIR}/dcache-server-${VERSION}.pkg dCache
 
