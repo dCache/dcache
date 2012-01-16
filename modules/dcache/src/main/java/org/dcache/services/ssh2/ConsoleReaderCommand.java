79142efff7589bae79beddf5c6bf4ea73d6ea8c1
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2012-01-16 17:28:23 +0000

    ssh2: use colors only if the client connected with terminal
    Signed-off-by: Tigran Mkrtchyan
    merge: trunk@16954

--- modules/dcache/src/main/java/org/dcache/services/ssh2/ConsoleReaderCommand.java
+++ modules/dcache/src/main/java/org/dcache/services/ssh2/ConsoleReaderCommand.java
@@ -57,7 +57,7 @@ public class ConsoleReaderCommand implements Command, Runnable {
     private Thread _adminShellThread;
     private ConsoleReader _console;
     private History _history;
-    private boolean _useColors = true;
+    private boolean _useColors;
 
     public ConsoleReaderCommand(String username, CellEndpoint cellEndpoint,
             File historyFile) {
@@ -101,6 +101,7 @@ public class ConsoleReaderCommand implements Command, Runnable {
 
     @Override
     public void start(Environment env) throws IOException {
+        _useColors = env.getEnv().containsKey(Environment.ENV_TERM);
         _console = new ConsoleReader(_in, _outWriter, null, new ConsoleReaderTerminal(env));
         _adminShellThread = new Thread(this);
         _adminShellThread.start();
