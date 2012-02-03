00396662ff9e46f189f41f5fbdb0397e43757b8d
Daniel Becker <d.becker@student.htw-berlin.de>
2012-02-03 13:10:42 +0000

    Fixed NSSwitch root group membership test for Mac OS X.
    The NSSwitch root group membership test failed on Mac
    OS X as there is no group called 'root'. Instead the
    default group of root is called 'wheel'. So this version
    of the test checks if root is a member of at least one
    of the groups.
    
    Patch: http://rb.dcache.org/r/4226
    Acked-by: Tigran Mkrtchyan
    Target: trunk

--- modules/gplazma2-nsswitch/src/test/java/org/dcache/gplazma/plugins/NsswitchTest.java
+++ modules/gplazma2-nsswitch/src/test/java/org/dcache/gplazma/plugins/NsswitchTest.java
@@ -2,6 +2,7 @@ package org.dcache.gplazma.plugins;
 
 import java.security.Principal;
 import java.util.Properties;
+import java.util.Set;
 
 import org.dcache.auth.GidPrincipal;
 import org.dcache.auth.GroupNamePrincipal;
@@ -14,8 +15,9 @@ import static org.junit.Assert.*;
 
 public class NsswitchTest {
 
-    private static final Principal ROOT_UNAME = new UserNamePrincipal("root");
-    private static final Principal ROOT_GNAME = new GroupNamePrincipal("root");
+    private static final Principal USER_ROOT = new UserNamePrincipal("root");
+    private static final Principal GROUP_ROOT = new GroupNamePrincipal("root");
+    private static final Principal GROUP_WHEEL = new GroupNamePrincipal("wheel");
     private static final Principal ROOT_UID = new UidPrincipal(0);
     private static final Principal ROOT_GID = new GidPrincipal(0, false);
     private final static Properties EMPTY_PROPERTIES = new Properties();
@@ -30,17 +32,19 @@ public class NsswitchTest {
 
     @Test
     public void testUidByName() throws NoSuchPrincipalException {
-         assertEquals(ROOT_UID, _identityPlugin.map(ROOT_UNAME));
+         assertEquals(ROOT_UID, _identityPlugin.map(USER_ROOT));
     }
 
     @Test
     public void testUnameByUid() throws NoSuchPrincipalException {
-        assertTrue(_identityPlugin.reverseMap(ROOT_UID).contains(ROOT_UNAME));
+        assertTrue(_identityPlugin.reverseMap(ROOT_UID).contains(USER_ROOT));
     }
 
     @Test
     public void testGnameByUid() throws NoSuchPrincipalException {
-        assertTrue(_identityPlugin.reverseMap(ROOT_GID).contains(ROOT_GNAME));
+        Set<Principal> principals = _identityPlugin.reverseMap(ROOT_GID);
+        assertTrue(principals.contains(GROUP_ROOT) ||
+                principals.contains(GROUP_WHEEL));
     }
 
 }
