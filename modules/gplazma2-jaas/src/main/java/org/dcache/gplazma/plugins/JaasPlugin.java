eb7446f0f69bf95fcb3501c92b4702eaec476e17
Gerd Behrmann <behrmann@nordu.net>
2012-02-07 18:04:51 +0000

    gplazma2: Split JAAS functionality of krb5 out into separate plugin
    The krb5 plugin has two functions: To perform username/password
    credential check using a callout to JAAS and KerberosPrincipal
    translation.
    
    The JAAS part is not strictly kerberos related; this all depends on the
    particular JAAS setup used.
    
    This patch splits the JAAS authentication functionality out from the
    krb5 plugin. The new jaas plugin allows the JAAS application name to be
    configured. It can be used as an auth plugin in place of the krb5 auth
    plugin.
    
    Note that the default application name is different.
    
    Release notes: The krb5 plugin was split into two plugins: The jaas
    plugin performs password credential check through JAAS and the krb5
    provides name translation by stripping the domain name from
    KerberosPrincipals. gplazma.conf has to be updated by replacing:
    
        auth optional krb5
    
    with
    
        auth optional jaas gplazma.jaas.name=Krb5Gplazma
    
    Target: trunk
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: http://rb.dcache.org/r/4218/

---
+++ modules/gplazma2-jaas/src/main/java/org/dcache/gplazma/plugins/JaasPlugin.java
@@ -0,0 +1,100 @@
+package org.dcache.gplazma.plugins;
+
+import java.security.Principal;
+import java.util.Properties;
+import java.util.Set;
+import javax.security.auth.callback.*;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import org.dcache.auth.PasswordCredential;
+import org.dcache.gplazma.AuthenticationException;
+import org.dcache.gplazma.SessionID;
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterables.getFirst;
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * A {@link GPlazmaAuthenticationPlugin} implementation which verifies
+ * user/pasword credentials using JAAS, the Jave Authentication and
+ * Authorization Services.
+ *
+ * A valid JAAS setup for password authentication has to be in place.
+ */
+public class JaasPlugin implements GPlazmaAuthenticationPlugin
+{
+    private static final String NAME = "gplazma.jaas.name";
+
+    private final String _name;
+
+    public JaasPlugin(Properties properties)
+    {
+        String name = properties.getProperty(NAME);
+        checkArgument(name != null, "Undefined property: " + NAME);
+        _name = name;
+    }
+
+    @Override
+    public void authenticate(SessionID sID,
+                             Set<Object> publicCredentials,
+                             Set<Object> privateCredentials,
+                             Set<Principal> identifiedPrincipals)
+        throws AuthenticationException
+    {
+        PasswordCredential password =
+            getFirst(filter(privateCredentials, PasswordCredential.class), null);
+        if (password == null) {
+            throw new AuthenticationException("No login name provided");
+        }
+
+        try {
+            LoginContext loginContext =
+                new LoginContext(_name,
+                                 new PasswordCallbackHandler(password));
+            loginContext.login();
+            identifiedPrincipals.addAll(loginContext.getSubject().getPrincipals());
+            tryToLogout(loginContext);
+        } catch (LoginException e) {
+            throw new AuthenticationException("Failed to authorize:", e);
+        }
+    }
+
+    private static void tryToLogout(LoginContext loginContext) {
+        if (loginContext == null) {
+            return;
+        }
+        try {
+            loginContext.logout();
+        } catch (LoginException e) {
+            // ignored
+        }
+    }
+
+    private static class PasswordCallbackHandler implements CallbackHandler
+    {
+        private final String _userName;
+        private final char _password[];
+
+        public PasswordCallbackHandler(PasswordCredential credential)
+        {
+            _userName = credential.getUsername();
+            _password = credential.getPassword().toCharArray();
+        }
+
+        @Override
+        public void handle(Callback callbacks[])
+            throws UnsupportedCallbackException
+        {
+            for (Callback callback: callbacks) {
+                if (callback instanceof NameCallback) {
+                    ((NameCallback) callback).setName(_userName);
+                } else if (callback instanceof PasswordCallback) {
+                    ((PasswordCallback) callback).setPassword(_password);
+                } else if (callback instanceof TextOutputCallback) {
+                    // do nothing
+                } else {
+                    throw new UnsupportedCallbackException(callback, "Unrecognized Callback");
+                }
+            }
+        }
+    }
+}
--- modules/gplazma2-krb5/src/main/java/org/dcache/gplazma/plugins/Krb5.java
+++ modules/gplazma2-krb5/src/main/java/org/dcache/gplazma/plugins/Krb5.java
@@ -3,74 +3,30 @@ package org.dcache.gplazma.plugins;
 import java.security.Principal;
 import java.util.Properties;
 import java.util.Set;
-import javax.security.auth.callback.*;
 import javax.security.auth.kerberos.KerberosPrincipal;
-import javax.security.auth.login.LoginContext;
-import javax.security.auth.login.LoginException;
-import org.dcache.auth.PasswordCredential;
 import org.dcache.auth.UserNamePrincipal;
 import org.dcache.gplazma.AuthenticationException;
 import org.dcache.gplazma.SessionID;
-import static com.google.common.collect.Iterables.filter;
-import static com.google.common.collect.Iterables.getFirst;
 import java.util.HashSet;
 
 /**
- * A {@link GPlazmaMappingPlugin} and {@link GPlazmaAuthenticationPlugin}
- * implementation which verifies user/pasword combination against kerberos5
- * server and converts {@code user@DOMAIN.COM} to {@link UserNamePrincipal}
- * corresponding to {@code user} ( without domain ).
- * For more complex mappings, like {@code remte-user@DOMAIN.COM} to {@code local-user},
- * {@link GridMapFilePlugin} can be used.
+ * A {@link GPlazmaMappingPlugin} converts {@code user@DOMAIN.COM} to
+ * {@link UserNamePrincipal} corresponding to {@code user} ( without
+ * domain ).  For more complex mappings, like {@code
+ * remte-user@DOMAIN.COM} to {@code local-user}, {@link
+ * GridMapFilePlugin} can be used.
  *
  * To enable, add following likes into gplazma.conf:
  * <pre>
- *     <b>auth optional krb5</b>
  *     <b>map requisite krb5</b>
  * </pre>
  */
-public class Krb5 implements GPlazmaMappingPlugin, GPlazmaAuthenticationPlugin {
-
-    private static final String JAAS_CONFIG_NAME = "Krb5Gplazma";
-
+public class Krb5 implements GPlazmaMappingPlugin
+{
     public Krb5(Properties properties) {
         /*
          * enforced by pluggin interface
          */
-    }
-
-    @Override
-    public void authenticate(SessionID sID, Set<Object> publicCredentials, Set<Object> privateCredentials, Set<Principal> identifiedPrincipals) throws AuthenticationException {
-        LoginContext loginContext = null;
-
-        PasswordCredential password =
-                getFirst(filter(privateCredentials, PasswordCredential.class), null);
-        if (password == null) {
-            throw new AuthenticationException("No login name provided");
-        }
-
-        try {
-
-            loginContext = new LoginContext(JAAS_CONFIG_NAME, new PasswordCallbackHandler(password) );
-
-            loginContext.login();
-            identifiedPrincipals.addAll(loginContext.getSubject().getPrincipals());
-            tryToLogout(loginContext);
-
-        } catch (LoginException e) {
-            throw new AuthenticationException("Failed to autorize:", e);
-        }
-    }
-
-    private static void tryToLogout(LoginContext loginContext) {
-        if(loginContext == null)
-            return;
-
-        try {
-            loginContext.logout();
-        } catch (LoginException e) {
-            // ignored
-        }
     }
 
     @Override
@@ -90,31 +46,5 @@ public class Krb5 implements GPlazmaMappingPlugin, GPlazmaAuthenticationPlugin {
             return s.substring(0, n);
         }
         return s;
-    }
-
-    private static class PasswordCallbackHandler implements CallbackHandler {
-
-        private final String _userName;
-        private final char _password[];
-
-        public PasswordCallbackHandler(PasswordCredential credential) {
-            _userName = credential.getUsername();
-            _password = credential.getPassword().toCharArray();
-        }
-
-        @Override
-        public void handle(Callback callbacks[]) throws UnsupportedCallbackException {
-            for (Callback callback : callbacks) {
-                if (callback instanceof NameCallback) {
-                    ((NameCallback) callback).setName(_userName);
-                } else if (callback instanceof PasswordCallback) {
-                    ((PasswordCallback) callback).setPassword(_password);
-                } else if (callback instanceof TextOutputCallback) {
-                    // do nothing
-                } else {
-                    throw new UnsupportedCallbackException(callback, "Unrecognized Callback");
-                }
-            }
-        }
     }
 }
