fa98df214ee13b847d874de4aee0320761ff5a1b
Paul Millar <paul.millar@desy.de>
2012-02-20 16:39:11 +0000

    webdav: add subject to logging
    WebDAV door will log (at info level) the requests that it receives.  
    These requests include the X509 certificate, if one is provided.  
    However, the mapped user is not recorded.  Since the uid/gid of the 
    request might not have anything to do with the X509 certificate (e.g., 
    username+pw from basic auth) this is insufficient information for 
    diagnosing permission problems.
    
    This patch updates the logging so that uid and gid information also 
    gets logged.
    
    RELEASE NOTES:
    
    WebDAV logging (available from admin interface 'show pinboard' cmd) 
    now includes the uid and gid to which the user has been mapped.
    
    Target: trunk
    Patch: http://rb.dcache.org/r/4289/
    Acked-by: Tigran Mkrtchyan

--- modules/dcache-webdav/src/main/java/org/dcache/webdav/LoggingFilter.java
+++ modules/dcache-webdav/src/main/java/org/dcache/webdav/LoggingFilter.java
@@ -1,20 +1,21 @@
 package org.dcache.webdav;
 
+import com.bradmcevoy.http.Auth;
 import com.bradmcevoy.http.Filter;
 import com.bradmcevoy.http.FilterChain;
 import com.bradmcevoy.http.Request;
 import com.bradmcevoy.http.Response;
 import com.bradmcevoy.http.ServletRequest;
 
-import org.dcache.auth.Subjects;
-
-import com.sun.security.auth.UserPrincipal;
+import com.google.common.base.Joiner;
+import com.google.common.primitives.Longs;
 import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
-import java.security.AccessController;
 import java.security.cert.X509Certificate;
-import java.util.Set;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.dcache.auth.Subjects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,37 +44,99 @@ public class LoggingFilter implements Filter
                           new Object[] { request.getFromAddress(),
                                          request.getMethod(),
                                          request.getAbsolutePath(),
-                                         getUser(),
+                                         getUser(request),
                                          status.code });
             } else {
                 _log.info("{} {} {} {}",
                           new Object[] { request.getFromAddress(),
                                          request.getMethod(),
                                          request.getAbsolutePath(),
-                                         getUser() });
+                                         getUser(request) });
             }
         } catch (RuntimeException e) {
             _log.warn(String.format("%s %s %s %s",
                                     request.getFromAddress(),
                                     request.getMethod(),
                                     request.getAbsolutePath(),
-                                    getUser()),
+                                    getUser(request)),
                                     e);
             throw e;
         }
     }
 
-    private String getUser()
+     private String getUser(Request request)
+     {
+         StringBuilder sb = new StringBuilder();
+
+         String certificateName = getCertificateName();
+         String subjectName = getSubjectName(request);
+
+         sb.append("[");
+         if(certificateName.isEmpty() && subjectName.isEmpty()) {
+             sb.append("ANONYMOUS");
+         } else {
+             sb.append(certificateName);
+
+             if(!certificateName.isEmpty() && !subjectName.isEmpty()) {
+                 sb.append("; ");
+             }
+
+             sb.append(subjectName);
+         }
+         sb.append("]");
+
+         return sb.toString();
+    }
+
+    private String getCertificateName()
     {
         HttpServletRequest servletRequest = ServletRequest.getRequest();
+
         Object object =
             servletRequest.getAttribute(X509_CERTIFICATE_ATTRIBUTE);
+
         if (object instanceof X509Certificate[]) {
             X509Certificate[] chain = (X509Certificate[]) object;
+
             if (chain.length >= 1) {
                 return chain[0].getSubjectX500Principal().getName();
             }
         }
+
         return "";
-   }
+    }
+
+    private String getSubjectName(Request request)
+    {
+        Auth auth = request.getAuthorization();
+
+        if(auth == null) {
+            return "";
+        }
+
+        Subject subject = (Subject) auth.getTag();
+
+        if(subject == null) {
+            return "";
+        }
+
+        if(subject.equals(Subjects.NOBODY)) {
+            return "NOBODY";
+        }
+
+        if(subject.equals(Subjects.ROOT)) {
+            return "ROOT";
+        }
+
+        String uid;
+        try {
+            uid = Long.toString(Subjects.getUid(subject));
+        } catch(NoSuchElementException e) {
+            uid="unknown";
+        }
+
+        List<Long> gids = Longs.asList(Subjects.getGids(subject));
+
+        return "uid=" + uid + ", gid={" + Joiner.on(",").join(gids)+"}";
+    }
 }
