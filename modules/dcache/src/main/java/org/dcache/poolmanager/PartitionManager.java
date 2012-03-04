53e4f1d52b43c0af5bb58c6d84b9890299ba51dc
Gerd Behrmann <behrmann@nordu.net>
2012-03-04 10:00:39 +0000

    poolmanager: Make default partition type configurable
    The default partition was hardcoded to the type 'classic'. This patch
    allows the type of the default partition to be changed.
    
    
    The approach taken is to allow partitions to be recreated with a
    different type. The existing partition is overwritten. This allows the
    default partition to be recreated with a different type.
    
    
    One complication was that we for reasons of backwards compatibility
    didn't distinguish between the set of inherited properties and the
    properties of the default partition. In 1.9.12 there was no need to
    distinguish because all partitions were of the same type and thus
    accepted the same set of parameters. Now that several partition types
    exist, inheriting from the default partition doesn't make sense, which
    is why we distinguish between the inherited properties and the
    properties of the default partition. To provide compatibility with the
    old scheme, setting a property on the default partition was silently
    treated as an update to the inherited properties. This has now been
    changed such that the properties of the default partition and the
    inherited properties can be updated individually. This change however
    only applies if a default partition was explicitly created - otherwise
    an update to the default partition's properties is still applied to the
    inherited set. This ensure backwards compatibility with 1.9.12. This
    feature can be removed once we move to dCache 2.3.
    
    
    The default setup embedded into the service definition batch file is
    updated to create an explicit default partition. Thus a fresh
    installation will always use an explicitly defined default.
    
    The patch updates the name of the _properties field to better reflect
    the meaning of this set.
    
    The patch also adds help text to all PartitionManager commands.
    
    Target: trunk
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: http://rb.dcache.org/r/4212/

--- modules/dcache/src/main/java/org/dcache/poolmanager/PartitionManager.java
+++ modules/dcache/src/main/java/org/dcache/poolmanager/PartitionManager.java
@@ -64,8 +64,26 @@ public class PartitionManager
      * Properties inherited by all partitions. Each partition may
      * override any of the properties.
      */
-    private ImmutableMap<String,String> _properties =
+    private ImmutableMap<String,String> _inheritedProperties =
         ImmutableMap.of();
+
+    /**
+     * When true the default partition's properties and the set of
+     * inherited properties are the same. Ie. the default partition
+     * cannot override any of the inherited properties.
+     *
+     * Once an explicit default partition is created, this field is set
+     * to false.
+     *
+     * This feature provides backwards compatibility with dCache
+     * 1.9.12: That version wrote PoolManager.conf files with the
+     * assumption that a property on the default partition would be
+     * inherited by other partitions. Once the PoolManager.conf file
+     * is regenerated with a newer version of dCache, an explicit
+     * default partition is created. Support for this feature can be
+     * removed in dCache 2.3.
+     */
+    private volatile boolean _hasImplicitDefaultPartition;
 
     /**
      * Partition manager manages a set of named partitions.
@@ -84,7 +102,8 @@ public class PartitionManager
 
     public synchronized void clear()
     {
-        _properties = ImmutableMap.of();
+        _hasImplicitDefaultPartition = true;
+        _inheritedProperties = ImmutableMap.of();
         _partitions =
             ImmutableMap.of(DEFAULT, (Partition) new ClassicPartition());
     }
@@ -109,14 +128,15 @@ public class PartitionManager
                                            Map<String,String> properties)
         throws IllegalArgumentException
     {
-        if (name == null || name.equals(DEFAULT)) {
-            _properties =
+        if (name == null || (_hasImplicitDefaultPartition && name.equals(DEFAULT))) {
+            _inheritedProperties =
                 ImmutableMap.<String,String>builder()
-                .putAll(filterKeys(_properties, not(in(properties.keySet()))))
+                .putAll(filterKeys(_inheritedProperties,
+                                   not(in(properties.keySet()))))
                 .putAll(filterValues(properties, notNull()))
                 .build();
             _partitions =
-                ImmutableMap.copyOf(transformValues(_partitions, updateInherited(_properties)));
+                ImmutableMap.copyOf(transformValues(_partitions, updateInherited(_inheritedProperties)));
         } else if (_partitions.containsKey(name)) {
             _partitions =
                 ImmutableMap.<String,Partition>builder()
@@ -132,27 +152,26 @@ public class PartitionManager
         createPartition(PartitionFactory factory, String name)
         throws IllegalArgumentException, NoSuchElementException
     {
-        if (_partitions.containsKey(name)) {
-            throw new IllegalArgumentException("Partition " + name + " already exists.");
-        }
-
-        Partition partition = factory.createPartition(_properties);
+        Partition partition = factory.createPartition(_inheritedProperties);
         _partitions =
             ImmutableMap.<String,Partition>builder()
-            .putAll(_partitions)
+            .putAll(filterKeys(_partitions, not(equalTo(name))))
             .put(name, partition)
             .build();
+        if (name.equals(DEFAULT)) {
+            _hasImplicitDefaultPartition = false;
+        }
     }
 
     public synchronized void destroyPartition(String name)
         throws IllegalArgumentException
     {
         if (name.equals(DEFAULT)) {
-            throw new IllegalArgumentException("Can't destroy default parameter partition");
+            throw new IllegalArgumentException("Cannot destroy default partition");
         }
 
         if (!_partitions.containsKey(name)) {
-            throw new IllegalArgumentException("No such parameter partition " + name);
+            throw new IllegalArgumentException("No such partition: " + name);
         }
 
         _partitions =
@@ -178,7 +197,17 @@ public class PartitionManager
         return partition;
     }
 
-    public final static String hh_pmx_get_map = "";
+    public final static String fh_pmx_get_map =
+        "Name:\n" +
+        "    pmx get map - get partition map\n" +
+        "\n" +
+        "Synopsis:\n" +
+        "    pmx get map\n" +
+        "\n" +
+        "Description:\n" +
+        "    Internal command to query the internal representation of all\n" +
+        "    partitions.\n";
+    public final static String hh_pmx_get_map = "# get partition map";
     public Object ac_pmx_get_map(Args args)
     {
        return _partitions;
@@ -190,32 +219,59 @@ public class PartitionManager
     }
 
     public final static String fh_pm_set =
-       "pm set [<partitionName>] OPTIONS\n"+
-       "    OPTIONS\n"+
-       "       -spacecostfactor=<scf>|off\n"+
-       "       -cpucostfactor=<ccf>|off\n"+
-       "       -max-copies=<max-replicas>|off\n"+
-       "       -idle=<value>|off\n"+
-       "       -p2p=<value>|off\n"+
-       "       -alert=<value>|off\n"+
-       "       -panic=<value>|off\n"+
-       "       -fallback=<value>|off\n"+
-       "       -slope=<value>|off\n"+
-       "       -p2p-allowed=yes|no|off\n"+
-       "       -p2p-oncost=yes|no|off\n"+
-       "       -p2p-fortransfer=yes|no|off\n"+
-       "       -stage-allowed=yes|no|off\n"+
-       "       -stage-oncost=yes|no|off\n"+
-       "";
+        "Name:\n" +
+        "    pm set - set partition parameters\n" +
+        "\n" +
+        "Synopsis:\n" +
+        "    pm set [<partition>] OPTION...\n" +
+        "\n" +
+        "Description:\n" +
+        "    Set one or more parameters on a partition.\n" +
+        "\n" +
+        "    If no partition name is provided, then the set of inherited\n" +
+        "    parameters is updated. These parameters are inherited by all\n" +
+        "    partitions except for those that explicitly redefine the\n" +
+        "    parameters.\n" +
+        "\n" +
+        "    Setting a parameter to the value 'off' resets it back to\n" +
+        "    inherited value or back to the default parameter value.\n" +
+        "\n" +
+        "    Available parameters depend on the partition type. Use\n" +
+        "    'pm ls -a' to see available parameters and their current\n" +
+        "    value. A small set of parameters are common to all partition\n" +
+        "    types. These are:\n" +
+        "\n" +
+        "       -p2p-allowed=yes|no|off\n"+
+        "       -p2p-oncost=yes|no|off\n"+
+        "       -p2p-fortransfer=yes|no|off\n"+
+        "       -stage-allowed=yes|no|off\n"+
+        "       -stage-oncost=yes|no|off\n";
     public final static String hh_pm_set =
-        "[<partitionName>] OPTIONS #  help pm set" ;
+        "[<partition>] OPTION... # set partition parameters";
     public String ac_pm_set_$_0_1(Args args)
         throws IllegalArgumentException
     {
-        setProperties(args.argv(0), scanParameter(args));
+        setProperties(args.argv(0), scanProperties(args));
         return "";
     }
 
+    public final static String fh_pm_types =
+        "Name:\n" +
+        "    pm types - list available partition types\n" +
+        "\n" +
+        "Synopsis:\n" +
+        "    pm types\n" +
+        "\n" +
+        "Description:\n" +
+        "    List partition types that can be used when creating new\n" +
+        "    partitions. The pool selection algorithm, the configuration\n" +
+        "    parameters, and the default values are defined by the\n" +
+        "    partition type.\n" +
+        "\n" +
+        "    Partition types are pluggable and new partition types can\n" +
+        "    be added through a plugin mechanism.";
+    public final static String hh_pm_types =
+        "# list available partition types";
     public String ac_pm_types_$_0(Args args)
     {
         final String format = "%-16s %s\n";
@@ -230,7 +286,32 @@ public class PartitionManager
     }
 
     public final static String hh_pm_create =
-        "[-type=<partitionType>] <partitionName>";
+        "[-type=<type>] <partition> # create new partition";
+    public final static String fh_pm_create =
+        "Name:\n" +
+        "    pm create - create new partition\n" +
+        "\n" +
+        "Synopsis:\n" +
+        "    pm create [-type=<type>] <partition>\n" +
+        "\n" +
+        "Description:\n" +
+        "    Creates a pool manager partition named <partition> of <type>.\n" +
+        "    If no <type> is specified then 'classic' is used as a default\n" +
+        "    type.\n" +
+        "\n" +
+        "    A partition encapsulates configuration parameters and pool\n" +
+        "    selection logic. Each pool manager link identifies a partition\n" +
+        "    to use for transfers in that link.\n" +
+        "\n" +
+        "    The partition type defines the pool selection logic and the\n" +
+        "    available configuration parameters. 'pm types' lists available\n" +
+        "    partition types." +
+        "\n" +
+        "    A default partition named 'default' exists and is used for\n" +
+        "    links that do not explicitly define which partition to use.\n" +
+        "    Creating a partition reusing an existing name overwrites that\n" +
+        "    partition. This allows the type of the default partition to be\n" +
+        "    redefined. Any parameter values on the partition are lost.";
     public String ac_pm_create_$_1(Args args)
     {
         String type = args.getOption("type");
@@ -240,61 +321,105 @@ public class PartitionManager
         return "";
     }
 
+    public final static String fh_pm_destroy =
+        "Name:\n" +
+        "    pm destroy - remove partition\n" +
+        "\n" +
+        "Synopsis:\n" +
+        "    pm destroy <partition>\n" +
+        "\n" +
+        "Description:\n" +
+        "    Remove the specified pool manager partition. Links using the\n" +
+        "    partition will fall back to the default partition. Any\n" +
+        "    parameter values associated with the partition are lost.";
     public final static String hh_pm_destroy =
-        "<partitionName> # destroys parameter partition";
+        "<partition> # remove partition";
     public String ac_pm_destroy_$_1(Args args)
     {
         destroyPartition(args.argv(0));
         return "";
     }
 
-    public final static String hh_pm_ls = "[-l] [<partitionName>]";
+    public final static String fh_pm_ls =
+        "Name:\n" +
+        "    pm ls - list partitions\n" +
+        "\n" +
+        "Synopsis:\n" +
+        "    pm ls [-l] [-a] [<partition>]\n" +
+        "\n" +
+        "Description:\n" +
+        "    List information about the <partition>. If no <partition> is\n" +
+        "    specified then information about all partitions is given.\n" +
+        "\n" +
+        "    -l  List parameters (default when <partition> is specified).\n" +
+        "    -a  List all parameters. The default is not to list inherited\n" +
+        "        and default parameters.\n";
+    public final static String hh_pm_ls =
+        "[-l] [-a] [<partition>] # list partitions";
     public String ac_pm_ls_$_0_1(Args args)
         throws IllegalArgumentException
     {
+        boolean showAll = args.hasOption("a");
         StringBuilder sb = new StringBuilder();
         if (args.argc() != 0) {
             String name = args.argv(0);
             Partition partition = _partitions.get(name);
             if (partition == null) {
-                throw new IllegalArgumentException("Section not found: " + name);
+                throw new IllegalArgumentException("Partition not found: " + name);
             }
 
             sb.append(name).append(" (").append(partition.getType()).append(")\n");
-            printProperties(sb, partition);
+            printProperties(sb, partition, showAll);
         } else {
+            boolean showProperties = args.hasOption("l") || showAll;
+            if (showProperties) {
+                printInheritedProperties(sb);
+            }
             for (Map.Entry<String,Partition> entry: _partitions.entrySet()) {
                 sb.append(entry.getKey())
                     .append(" (")
                     .append(entry.getValue().getType())
                     .append(")\n");
-                if (args.hasOption("l")) {
-                    printProperties(sb, entry.getValue());
+                if (showProperties) {
+                    printProperties(sb, entry.getValue(), showAll);
                 }
             }
         }
         return sb.toString();
     }
 
-    private void printProperties(StringBuilder sb, Partition partition)
+    private void printInheritedProperties(StringBuilder sb)
+    {
+        Set<String> keys = _inheritedProperties.keySet();
+        sb.append("Inherited by all partitions\n");
+        for (String key: Ordering.<String>natural().sortedCopy(keys)) {
+            sb.append("    -").append(key)
+                .append("=").append(_inheritedProperties.get(key)).append("\n");
+        }
+    }
+
+    private void printProperties(StringBuilder sb, Partition partition,
+                                 boolean showAll)
     {
         Set<String> all = partition.getAllProperties().keySet();
         Set<String> defined = partition.getProperties().keySet();
 
         for (String key: Ordering.<String>natural().sortedCopy(all)) {
-            sb.append("    -").append(key)
-                .append("=").append(partition.getProperty(key));
-            if (defined.contains(key)) {
-                sb.append("\n");
-            } else if (_properties.containsKey(key)) {
-                sb.append(" [inherited]\n");
-            } else {
-                sb.append(" [default]\n");
+            if (defined.contains(key) || showAll) {
+                sb.append("    -").append(key)
+                    .append("=").append(partition.getProperty(key));
+                if (defined.contains(key)) {
+                    sb.append("\n");
+                } else if (_inheritedProperties.containsKey(key)) {
+                    sb.append(" [inherited]\n");
+                } else {
+                    sb.append(" [default]\n");
+                }
             }
         }
     }
 
-    private Map<String,String> scanParameter(Args args)
+    private Map<String,String> scanProperties(Args args)
     {
         Map<String,String> map = newHashMap();
         for (Map.Entry<String,String> entry: args.optionsAsMap().entrySet()) {
@@ -323,12 +448,10 @@ public class PartitionManager
     @Override
     public synchronized void printSetup(PrintWriter pw)
     {
-        dumpInfo(pw, "", _properties);
+        dumpInfo(pw, "", _inheritedProperties);
         for (Map.Entry<String,Partition> entry: _partitions.entrySet()) {
-            if (!entry.getKey().equals(DEFAULT)) {
-                pw.format("pm create -type=%s %s\n",
-                          entry.getValue().getType(), entry.getKey());
-            }
+            pw.format("pm create -type=%s %s\n",
+                      entry.getValue().getType(), entry.getKey());
             dumpInfo(pw, entry.getKey(), entry.getValue().getProperties());
         }
     }
