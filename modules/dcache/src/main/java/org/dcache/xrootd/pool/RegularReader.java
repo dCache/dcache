22dccc7ebf14353b129423f7ccd9c2eb1034eccf
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2012-06-19 14:21:06 +0000

    xrootd: Fix race condition and vector read framing bugs
    Signed-off-by: Gerd Behrmann
    merge: trunk@17301

--- modules/dcache/src/main/java/org/dcache/xrootd/pool/RegularReader.java
+++ modules/dcache/src/main/java/org/dcache/xrootd/pool/RegularReader.java
@@ -1,9 +1,12 @@
 package org.dcache.xrootd.pool;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import org.dcache.pool.repository.RepositoryChannel;
 import org.dcache.xrootd.protocol.messages.ReadResponse;
+
+import static org.jboss.netty.buffer.ChannelBuffers.wrappedBuffer;
 
 /**
  * Reader for kXR_read requests.
@@ -24,6 +27,7 @@ public class RegularReader implements Reader
         _descriptor = descriptor;
     }
 
+    @Override
     public int getStreamID()
     {
         return _id;
@@ -33,6 +37,7 @@ public class RegularReader implements Reader
      * Returns the next response message for this read
      * request. Returns null if all data has been read.
      */
+    @Override
     public ReadResponse read(int maxFrameSize)
         throws IOException
     {
@@ -43,9 +48,8 @@ public class RegularReader implements Reader
         RepositoryChannel channel = _descriptor.getChannel();
 
         int length = Math.min(_length, maxFrameSize);
-        ReadResponse response = new ReadResponse(_id, length);
-        channel.position(_position);
-        length = response.writeBytes(channel, length);
+        RegularReadResponse response = new RegularReadResponse(_id);
+        length = response.write(channel, _position, length);
         _position += length;
         _length -= length;
         response.setIncomplete(_length != 0);
@@ -55,6 +59,43 @@ public class RegularReader implements Reader
         mover.addTransferredBytes(length);
         mover.updateLastTransferred();
         return response;
+    }
+
+    /**
+     * Specialized response for regular read requests.
+     *
+     * In contrast to ReadResponse, RegularReadResponse supports position
+     * independent read from a RepositoryChannel.
+     */
+    private static class RegularReadResponse extends ReadResponse
+    {
+        public RegularReadResponse(int sId) {
+            super(sId, 0);
+        }
+
+        public int write(RepositoryChannel channel, long srcIndex, int length)
+                throws IOException
+        {
+            int remaining = length;
+            byte[] chunkArray = new byte[length];
+            ByteBuffer chunk = ByteBuffer.wrap(chunkArray);
+
+            while (remaining > 0) {
+                /* use position independent thread safe call */
+                int bytes = channel.read(chunk, srcIndex);
+                if (bytes < 0) {
+                    break;
+                }
+
+                srcIndex += bytes;
+                remaining -= bytes;
+            }
+
+            _buffer = wrappedBuffer(_buffer,
+                    wrappedBuffer(chunkArray, 0, length - remaining));
+
+            return length - remaining;
+        }
     }
 }
 
