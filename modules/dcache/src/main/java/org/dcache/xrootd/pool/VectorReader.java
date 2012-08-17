fb66c27955184750f49049eb8f3c4678e9e4172e
Paul Millar <paul.millar@desy.de>
2012-08-17 09:49:39 +0000

    xrootd: Fix race condition and vector read framing bugs
    Merged: trunk@17301
    Signed-off-by: Gerd Behrmann
    Acked-by: Paul Millar
    Patch: http://rb.dcache.org/r/4529
    Merge-request: http://rt.dcache.org/Ticket/Display.html?id=7189

--- modules/dcache/src/main/java/org/dcache/xrootd/pool/VectorReader.java
+++ modules/dcache/src/main/java/org/dcache/xrootd/pool/VectorReader.java
@@ -1,5 +1,6 @@
 package org.dcache.xrootd.pool;
 
+import java.nio.ByteBuffer;
 import java.util.List;
 
 import java.io.IOException;
@@ -7,6 +8,10 @@ import java.io.IOException;
 import org.dcache.pool.repository.RepositoryChannel;
 import org.dcache.xrootd.protocol.messages.ReadResponse;
 import org.dcache.xrootd.protocol.messages.GenericReadRequestMessage.EmbeddedReadRequest;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+
+import static org.jboss.netty.buffer.ChannelBuffers.wrappedBuffer;
 
 /**
  * Reader for kXR_readv requests.
@@ -34,49 +39,61 @@ public class VectorReader implements Reader
         return _id;
     }
 
-    private int getSizeOfNextFrame(int maxFrameSize)
+    private int getLengthOfRequest(EmbeddedReadRequest request)
+            throws IOException
     {
-        int length = 0;
-        for (int i = _index; i < _requests.length; i++) {
-            EmbeddedReadRequest req = _requests[_index];
-            int sizeOfNextBlock =
-                req.BytesToRead() + ReadResponse.READ_LIST_HEADER_SIZE;
-            if (length + sizeOfNextBlock > maxFrameSize) {
-                break;
-            }
-            length += sizeOfNextBlock;
-        }
-
-        if (length == 0) {
-            throw new IllegalStateException("Maximum chunk size exceeded");
-        }
-
-        return length;
+        FileDescriptor descriptor = _descriptors.get(request.getFileHandle());
+        RepositoryChannel channel = descriptor.getChannel();
+        return (int) Math.min(request.BytesToRead(), channel.size() - request.getOffset());
     }
 
-    private int readBlock(ReadResponse response, EmbeddedReadRequest request)
+    private int getChunksInNextFrame(int maxFrameSize) throws IOException
+    {
+        long length = 0;
+        int count = 0;
+        for (int i = _index; i < _requests.length && length < maxFrameSize; i++) {
+            length += ReadResponse.READ_LIST_HEADER_SIZE;
+            length += getLengthOfRequest(_requests[i]);
+            count++;
+        }
+        if (length > maxFrameSize) {
+            count--;
+        }
+        if (count == 0) {
+            throw new IllegalStateException("Maximum chunk size exceeded");
+        }
+        return count;
+    }
+
+    private ChannelBuffer readBlock(EmbeddedReadRequest request)
         throws IOException
     {
         FileDescriptor descriptor = _descriptors.get(request.getFileHandle());
         RepositoryChannel channel = descriptor.getChannel();
-        long position = request.getOffset();
-        long end = position + request.BytesToRead();
 
-        int length = response.writeBytes(request);
-        channel.position(position);
-        while (position < end) {
-            int read = response.writeBytes(channel, (int) (end - position));
-            position += read;
-            length += read;
+        long position = request.getOffset();
+        int remaining = request.BytesToRead();
+        byte[] chunkArray = new byte[remaining];
+        ByteBuffer chunk = ByteBuffer.wrap(chunkArray);
+
+        while (remaining > 0) {
+            /* use position independent thread safe call */
+            int bytes = channel.read(chunk, position);
+            if (bytes < 0) {
+                break;
+            }
+
+            position += bytes;
+            remaining -= bytes;
         }
 
         /* inform the mover about transferred bytes and update last
          * transfer information */
         XrootdProtocol_3 mover = descriptor.getMover();
-        mover.addTransferredBytes(length);
+        mover.addTransferredBytes(chunkArray.length - remaining);
         mover.updateLastTransferred();
 
-        return length;
+        return wrappedBuffer(chunkArray, 0, chunkArray.length - remaining);
     }
 
     /**
@@ -91,15 +108,52 @@ public class VectorReader implements Reader
             return null;
         }
 
-        int length = getSizeOfNextFrame(maxFrameSize);
-        ReadResponse response = new ReadResponse(_id, length);
-        while (length > 0 && _index < _requests.length) {
-            length -= readBlock(response, _requests[_index]);
-            _index = _index + 1;
+        int count = getChunksInNextFrame(maxFrameSize);
+        ChannelBuffer[] chunks = new ChannelBuffer[_requests.length];
+        for (int i = _index; i < _index + count; i++) {
+            chunks[i] = readBlock(_requests[i]);
         }
 
-        response.setIncomplete(_index < _requests.length);
+        VectorReadResponse response = new VectorReadResponse(_id);
+        response.write(_requests, chunks, _index, count);
+        response.setIncomplete(_index + count < _requests.length);
+        _index += count;
 
         return response;
+    }
+
+    /**
+     * Specialized response for vector read requests.
+     *
+     * In contrast to ReadResponse, VectorReadResponse supports zero-copy
+     * appending the response data.
+     */
+    public static class VectorReadResponse extends ReadResponse
+    {
+        public VectorReadResponse(int sId) {
+            super(sId, 0);
+        }
+
+        private ChannelBuffer getReadListHeader(EmbeddedReadRequest request, int actualLength)
+        {
+            ChannelBuffer buffer = ChannelBuffers.buffer(16);
+            buffer.writeInt(request.getFileHandle());
+            buffer.writeInt(actualLength);
+            buffer.writeLong(request.getOffset());
+            return buffer;
+        }
+
+        public void write(EmbeddedReadRequest[] requests,
+                          ChannelBuffer[] buffers,
+                          int offset, int length)
+        {
+            ChannelBuffer[] reply = new ChannelBuffer[2 * length + 1];
+            reply[0] = _buffer;
+            for (int i = 0; i < length; i++) {
+                reply[2 * i + 1] = getReadListHeader(requests[offset + i], buffers[offset + i].readableBytes());
+                reply[2 * i + 2] = buffers[offset + i];
+            }
+            _buffer = wrappedBuffer(reply);
+        }
     }
 }
