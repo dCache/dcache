1ae2ad332d5f49da8c9799c7ad40339831ac0d2b
Gerd Behrmann <behrmann@nordu.net>
2012-08-17 16:57:07 +0000

    pool: Fix xrootd vector read bug affecting reads of more than one file
    A single xrootd connection can open several files. Each file gets
    its own file handle. Vector reads can read from several open files
    at once.
    
    The xrootd mover contained a bug that would cause an IndexOutOfBoundsException
    to be thrown if attempting to vector reads from files with a file
    handle bigger than 0.
    
    In fact, the bug could even cause data to be read from the wrong files
    without any error being thrown. This would happen when a single readv request
    would refer to several different files.
    
    We have probably not encountered this bug as most clients only read from
    a single file.
    
    Require-book: no
    Require-notes: yes
    Target: trunk
    Request: 2.3
    Request: 2.2
    Request: 2.1
    Request: 1.9.12
    Acked-by: Paul Millar <paul.millar@desy.de>
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: http://rb.dcache.org/r/4751/

--- modules/dcache-xrootd/src/main/java/org/dcache/xrootd/pool/XrootdPoolRequestHandler.java
+++ modules/dcache-xrootd/src/main/java/org/dcache/xrootd/pool/XrootdPoolRequestHandler.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.UUID;
 
+import com.google.common.collect.Lists;
 import org.dcache.pool.movers.IoMode;
 import org.dcache.pool.movers.MoverChannel;
 import org.dcache.vehicles.XrootdProtocolInfo;
@@ -424,9 +425,6 @@ public class XrootdPoolRequestHandler extends XrootdRequestHandler
             throw new XrootdException(kXR_ArgMissing, "Request contains no vector");
         }
 
-        List<FileDescriptor> vectorDescriptors =
-            new ArrayList<FileDescriptor>();
-
         for (EmbeddedReadRequest req : list) {
             int fd = req.getFileHandle();
 
@@ -437,8 +435,6 @@ public class XrootdPoolRequestHandler extends XrootdRequestHandler
                                           + "does not refer to an open file.");
             }
 
-            FileDescriptor descriptor = _descriptors.get(fd);
-
             int totalBytesToRead = req.BytesToRead() +
                 ReadResponse.READ_LIST_HEADER_SIZE;
 
@@ -448,12 +444,10 @@ public class XrootdPoolRequestHandler extends XrootdRequestHandler
                           MAX_FRAME_SIZE);
                 throw new XrootdException(kXR_ArgInvalid, "Single readv transfer is too large");
             }
-
-            vectorDescriptors.add(fd, descriptor);
         }
 
         _readers.add(new VectorReader(msg.getStreamId(),
-                                      vectorDescriptors,
+                                      Lists.newArrayList(_descriptors),
                                       list));
         sendToClient(event.getChannel());
         return null;
