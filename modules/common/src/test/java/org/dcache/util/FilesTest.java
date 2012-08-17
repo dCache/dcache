e57f9f1f2345747fb21f068fc3b192129609117d
Paul Millar <paul.millar@desy.de>
2012-08-17 10:43:35 +0000

    gsiftp/gsidcap: provide better errors when file permissions are wrong
    This patch is needed by another patch that is requested to be 
    back-ported to this branch.
    
    Merged: 2.2@17074
    Signed-off-by: Paul Millar
    Acked-by: Tigran Mkrtchyan
    Acked-by: Karsten Schwank
    Patch: http://rb.dcache.org/r/4290

---
+++ modules/common/src/test/java/org/dcache/util/FilesTest.java
@@ -0,0 +1,125 @@
+package org.dcache.util;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+
+/**
+ *  Verify correct behaviour of files class.
+ */
+public class FilesTest
+{
+    File _directory;
+    File _test;
+
+    @Before
+    public void setup() throws IOException
+    {
+        _directory = File.createTempFile("test-FilesTest-", "-dir");
+        _directory.delete();
+        _directory.mkdir();
+    }
+
+    @After
+    public void teardown()
+    {
+        deleteEverythingIn(_directory);
+        _directory.delete();
+    }
+
+    private void deleteEverythingIn(File dir)
+    {
+        for(File file : dir.listFiles()) {
+            if(file.isDirectory()) {
+                deleteEverythingIn(file);
+            }
+
+            file.delete();
+        }
+    }
+
+    @Test
+    public void checkFileShouldNotThrowExceptionForFile() throws IOException
+    {
+        givenANormalFile();
+
+        checkFile();
+    }
+
+    @Test(expected=IOException.class)
+    public void checkFileShouldThrowExceptionForMissingFile()
+            throws IOException
+    {
+        givenAnAbsentFile();
+
+        checkFile();
+    }
+
+    @Test(expected=IOException.class)
+    public void checkFileShouldThrowExceptionForDirectory() throws IOException
+    {
+        givenANormalDirectory();
+
+        checkFile();
+    }
+
+    @Test
+    public void checkDirectoryShouldNotThrowExceptionForDirectory()
+            throws IOException
+    {
+        givenANormalDirectory();
+
+        checkDirectory();
+    }
+
+    @Test(expected=IOException.class)
+    public void checkDirectoryShouldThrowExceptionForMissingDirectory()
+            throws IOException
+    {
+        givenAnAbsentFile();
+
+        checkDirectory();
+    }
+
+    @Test(expected=IOException.class)
+    public void checkDirectoryShouldThrowExceptionForFile() throws IOException
+    {
+        givenANormalFile();
+
+        checkDirectory();
+    }
+
+    public void checkFile() throws IOException
+    {
+        Files.checkFile(_test);
+    }
+
+    public void checkDirectory() throws IOException
+    {
+        Files.checkDirectory(_test);
+    }
+
+    public void givenAnAbsentFile()
+    {
+        _test = new File(_directory, "missing");
+    }
+
+    public void givenANormalFile() throws IOException
+    {
+        _test = new File(_directory, "present");
+        if(!_test.createNewFile()) {
+            fail("Unable to create file " + _test);
+        }
+    }
+
+    public void givenANormalDirectory() throws IOException
+    {
+        _test = new File(_directory, "present");
+        _test.mkdir();
+    }
+
+}
