955e7411f8d90262f23f12b669592d49f7b1ba79
Paul Millar <paul.millar@desy.de>
2012-12-17 13:03:02 +0000

    chimera-cli: fix output of 'ls' cmd
    The chimera-cli may be used as a diagnostic command for investigating 
    problems with dCache's namespace.  In particular, it may be used by an 
    admin who is new to dCache to "see what's happening".
    
    Although the current output presents all the necessary information the 
    format is alien.  This is quite off-putting for the new admin.
    
    This patch changes the output to follow what 'ls -l' shows.  It also 
    forces '.' and '..' to appear as the first and second items 
    (respectively).  Although this is purely cosmetic, I think it helps 
    the output look more familiar.
    
    FOR RELEASE NOTES
    
    Output from chimera-cli ls command has been updated to match 'ls -l'
    
    Target: trunk
    Request: 2.5
    Require-book: yes
    Require-notes: yes
    Patch: http://rb.dcache.org/r/5037

--- modules/chimera/src/main/java/org/dcache/chimera/cli/Ls.java
+++ modules/chimera/src/main/java/org/dcache/chimera/cli/Ls.java
@@ -16,18 +16,42 @@
  */
 package org.dcache.chimera.cli;
 
+import static com.google.common.base.Strings.padStart;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.Formatter;
-
-import org.dcache.chimera.DirectoryStreamB;
-import org.dcache.chimera.FileSystemProvider;
-import org.dcache.chimera.FsInode;
-import org.dcache.chimera.HimeraDirectoryEntry;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.dcache.chimera.*;
 import org.dcache.chimera.posix.Stat;
 
-public class Ls {
+public class Ls
+{
+    /* The block size is purely nominal; we use 1k here as historically
+     * filesystems have used a 1k block size. */
+    private static final int BLOCK_SIZE = 1024;
 
-    public static void main(String[] args) throws Exception {
+    private static final  int[] INT_SIZE_TABLE = {9, 99, 999, 9999, 99999,
+        999999, 9999999, 99999999, 999999999, Integer.MAX_VALUE};
+
+    private static final DateFormat WITH_YEAR =
+            new SimpleDateFormat("MMM dd  yyyy");
+
+    private static final DateFormat WITHOUT_YEAR =
+            new SimpleDateFormat("MMM dd HH:mm");
+
+    private static int nlinkWidth = 0;
+    private static int uidWidth = 0;
+    private static int gidWidth = 0;
+    private static int sizeWidth = 0;
+
+    private static final long sixMonthsInPast = sixMonthsInPast();
+    private static final long oneHourInFuture = oneHourInFuture();
+
+    public static void main(String[] args) throws Exception
+    {
 
         if (args.length != FsFactory.ARGC + 1) {
             System.err.println("Usage : " + Ls.class.getName() + " " + FsFactory.USAGE
@@ -37,25 +61,168 @@ public class Ls {
 
         FileSystemProvider fs = FsFactory.createFileSystem(args);
         FsInode inode = fs.path2inode(args[FsFactory.ARGC]);
+
+        List<HimeraDirectoryEntry> entries = new LinkedList<>();
+
+        long totalBlocks = 0;
+
+        HimeraDirectoryEntry dot = null;
+        HimeraDirectoryEntry dotdot = null;
+
         try {
-            int count = 0;
             try (DirectoryStreamB<HimeraDirectoryEntry> dirStream = inode
                     .newDirectoryStream()) {
                 for (HimeraDirectoryEntry entry : dirStream) {
+                    String name = entry.getName();
                     Stat stat = entry.getStat();
-                    String s =
-                            String.format("%o %6d %6d %6d %6d %s %s",
-                                    stat.getMode(), stat.getNlink(),
-                                    stat.getUid(), stat.getGid(),
-                                    stat.getSize(), new Date(stat.getMTime()),
-                                    entry.getName());
-                    System.out.println(s);
+
+                    if(name.equals(".")) {
+                        dot = entry;
+                    } else if(name.equals("..")) {
+                        dotdot = entry;
+                    } else {
+                        entries.add(entry);
+                    }
+
+                    totalBlocks = updateTotalBlocks(totalBlocks, stat);
+                    nlinkWidth = updateMaxWidth(nlinkWidth, stat.getNlink());
+                    uidWidth = updateMaxWidth(uidWidth, stat.getUid());
+                    gidWidth = updateMaxWidth(gidWidth, stat.getGid());
+                    sizeWidth = updateMaxWidth(sizeWidth, stat.getSize());
                 }
             }
 
-            System.out.println("Total: " + count);
         } finally {
             fs.close();
+        }
+
+        System.out.println("total " + totalBlocks);
+        printEntry(dot);
+        printEntry(dotdot);
+        for(HimeraDirectoryEntry entry : entries) {
+            printEntry(entry);
+        }
+    }
+
+    private static void printEntry(HimeraDirectoryEntry entry)
+    {
+        if(entry != null) {
+            Stat stat = entry.getStat();
+            String s = String.format("%s %s %s %s %s %s %s",
+                        permissionsFor(stat),
+                        pad(stat.getNlink(), nlinkWidth),
+                        pad(stat.getUid(), uidWidth),
+                        pad(stat.getGid(), gidWidth),
+                        pad(stat.getSize(), sizeWidth),
+                        dateOf(stat.getMTime()),
+                        entry.getName());
+
+            System.out.println(s);
+        }
+    }
+
+    // For files with a time that is more than 6 months old or more than 1
+    // hour into the future, the timestamp contains the year instead of the
+    // time of day.
+    private static String dateOf(long time)
+    {
+        Date d = new Date(time);
+
+        if(time < sixMonthsInPast || time > oneHourInFuture) {
+            return WITH_YEAR.format(d);
+        } else {
+            return WITHOUT_YEAR.format(d);
+        }
+    }
+
+    private static long sixMonthsInPast()
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, -6);
+        return calendar.getTimeInMillis();
+    }
+
+    private static long oneHourInFuture()
+    {
+        return System.currentTimeMillis() + TimeUnit.HOURS.toMillis(1);
+    }
+
+    private static String pad(int value, int width)
+    {
+        String str = String.valueOf(value);
+        return padStart(str, width, ' ');
+    }
+
+    private static String pad(long value, int width)
+    {
+        String str = String.valueOf(value);
+        return padStart(str, width, ' ');
+    }
+
+    private static long updateTotalBlocks(long total, Stat stat)
+    {
+        // calculate number of blocks, but rounding up
+        long nBlocks = 1 + (stat.getSize() -1)/ BLOCK_SIZE;
+        return total + nBlocks;
+    }
+
+    private static int updateMaxWidth(int max, int value)
+    {
+        int width = widthOf(value);
+        return width > max ? width : max;
+    }
+
+    private static int updateMaxWidth(int max, long value)
+    {
+        int width = widthOf(value);
+        return width > max ? width : max;
+    }
+
+    private static String permissionsFor(Stat stat)
+    {
+        return new UnixPermission(stat.getMode()).toString();
+    }
+
+
+    // Requires positive x
+    private static int widthOf(int x)
+    {
+        for (int i=0; ; i++) {
+            if (x <= INT_SIZE_TABLE[i]) {
+                return i+1;
+            }
+        }
+    }
+
+    // Requires positive x
+    private static int widthOf(long x)
+    {
+        if(x <= Integer.MAX_VALUE) {
+            return widthOf((int) x);
+        }
+
+        // x is more than 0x7fffffff or 2147483647
+
+        if (x < 1000000000000L) { // from 10 to 12 digits
+            if (x < 10000000000L) {
+                return 10;
+            } else {
+                return x < 100000000000L ? 11 : 12;
+            }
+        } else { // 13 or more digits
+            if (x < 10000000000000000L) {
+                if (x < 100000000000000L) {
+                    return x < 10000000000000L ? 13 : 14;
+                } else {
+                    return x < 1000000000000000L ? 15 : 16;
+                }
+            } else {
+                if (x < 1000000000000000000L) {
+                    return x < 100000000000000000L ? 17 : 18;
+                } else {
+                    return 19;
+                }
+            }
         }
     }
 }
