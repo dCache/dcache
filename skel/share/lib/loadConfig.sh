f2e64c4a9f1d8eaad48ba94ff400276c58285601
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2013-02-01 10:22:14 +0000

    scripts: fix dcache cmd for zero-length cache file
    Signed-off-by: Paul Millar
    merge: trunk@17969

--- skel/share/lib/loadConfig.sh
+++ skel/share/lib/loadConfig.sh
@@ -84,7 +84,7 @@ if ! findJava || ! "$JAVA" -version 2>&1 | egrep -e 'version "1\.[7]' >/dev/null
     exit 1
 fi
 
-if [ -e $DCACHE_CACHED_CONFIG ]; then
+if [ -s $DCACHE_CACHED_CONFIG ]; then
     . $DCACHE_CACHED_CONFIG
    if ! eval isCacheValidForFiles $(getProperty dcache.config.files) /etc/hostname ||
       ! eval isCacheValidForDirs $(getProperty dcache.config.dirs); then
