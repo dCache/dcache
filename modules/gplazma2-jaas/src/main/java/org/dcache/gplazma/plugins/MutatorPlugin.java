6ed643bb77e8822f9ef6f8c2c932fd6043acb368
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2013-04-24 11:55:43 +0200

    gplazma2-jaas: add principal mutator plugin
    JAAS plug in can be configured to use any jaas login module. As a
    result we may get principas which can't be handles by gplazma. New
    MutatorPlugin allows to convert some principals to supported equivalents.
    
    in gplazma.conf:
    auth jaas
    ...
    map optional mutator gplazma.mutator.accept=org.apache.activemq.jaas.UserPrincipal gplazma.mutator.produce=username
    map sufficient nsswitch
    ...
    
    The supported output types:
       dn
       name
       username
       kerberos
       fqan
    
    Ticket: #7763
    Acked-by: Paul Millar
    Target: trunk, 2.6, 2.2
    Require-book: yes
    Require-notes: yes
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

---
+++ modules/gplazma2-jaas/src/main/java/org/dcache/gplazma/plugins/MutatorPlugin.java
@@ -0,0 +1,80 @@
+package org.dcache.gplazma.plugins;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+import javax.security.auth.kerberos.KerberosPrincipal;
+import org.dcache.auth.FQANPrincipal;
+import org.dcache.auth.LoginNamePrincipal;
+import org.dcache.auth.UserNamePrincipal;
+import org.globus.gsi.jaas.GlobusPrincipal;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Predicates.instanceOf;
+import static com.google.common.collect.Iterables.filter;
+
+/**
+ * A {@link GPlazmaMappingPlugin} that maps a one principal type to an other one.
+ * This is required to convert equivalent principals into principals provided by
+ * {@code org.dcache.auth} package.
+ * NOTICE, this plugin modifies the original set of principals.
+ */
+public class MutatorPlugin implements GPlazmaMappingPlugin {
+
+    private final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+    final static String IN_OPTION = "gplazma.mutator.accept";
+    final static String OUT_OPTION = "gplazma.mutator.produce";
+    private final Class<?> inPrincipal;
+    private final Class<?> outPrincipal;
+    private Constructor<? extends Principal> outConstructor;
+
+    public MutatorPlugin(Properties properties) throws ClassNotFoundException, NoSuchMethodException {
+        String inClass = properties.getProperty(IN_OPTION);
+        String outClass = properties.getProperty(OUT_OPTION);
+
+        checkArgument(inClass != null, "Undefined property: " + IN_OPTION);
+        checkArgument(outClass != null, "Undefined property: " + OUT_OPTION);
+
+        inPrincipal = classLoader.loadClass(inClass);
+        checkArgument(Principal.class.isAssignableFrom(inPrincipal), inClass + " is not a Principal");
+
+        outPrincipal = classOf(outClass);
+        outConstructor = (Constructor<? extends Principal>) outPrincipal.getConstructor(String.class);
+    }
+
+    @Override
+    public void map(Set<Principal> principals, Set<Principal> authorizedPrincipals) {
+        Set<Principal> mutated = new HashSet<>();
+        for (Principal p : filter(principals, instanceOf(inPrincipal))) {
+            try {
+                Principal out = outConstructor.newInstance(p.getName());
+                mutated.add(out);
+            } catch (IllegalAccessException | IllegalArgumentException |
+                    InstantiationException | InvocationTargetException e) {
+            }
+        }
+        principals.addAll(mutated);
+    }
+
+    public static Class<? extends Principal> classOf(String type) {
+
+        switch (type) {
+            case "dn":
+                return GlobusPrincipal.class;
+            case "kerberos":
+                return KerberosPrincipal.class;
+            case "fqan":
+                return FQANPrincipal.class;
+            case "name":
+                return LoginNamePrincipal.class;
+            case "username":
+                return UserNamePrincipal.class;
+            default:
+                throw new IllegalArgumentException("unknown type: " + type);
+        }
+    }
+
+}
