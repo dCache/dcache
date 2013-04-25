1addecdbda0541c68eaee6bb17fc53578f6f7b58
alrossi <arossi@otfrid.fnal.gov>
2013-04-25 11:14:03 -0500

    billing: add flag to turn plotting on/off; fix SQL for migration scripts
    module: dcache services billing
    
    When running the liquibase changesets on the billing database from 1.9.12+, there may be interference from the plotting thread which runs in a separate thread and already begins to execute queries on non-existent views.  This might actually cause the creation of the additional triggers to hang inside of the liquibase thread.
    
    This version of billing never added the ability to turn plot generation on or off.  We add that capability here, and recommend that plots be turned off when running the upgraded dCache for the first time.
    
    It also came to my attention that a patch fixing some typos in the migration scripts provided in shared/migration was never merged to this branch, so I have included those fixes as well (see http://rb.dcache.org/r/4389, trunk@17253).
    
    Target: 2.2
    Patch: http://rb.dcache.org/r/5500/
    Merge-req: http://rt.dcache.org/Ticket/Display.html?id=7784
    Require-book: no
    Require-notes: yes
    Acked-by: Tigran
    
    RELEASE NOTES:  Adds ability, via the "generatePlots" property (found in later versions), to turn plotting off or on (on by default).  This is recommended when running the first time, since the plotting thread may interfere with the liquibase migration.  The sql migration scripts (in shared/migration) also had a some typographical problems which have been corrected.
    
    Tested on testbed.
    
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- modules/dcache/src/main/java/org/dcache/services/billing/html/HttpBillingHistoryEngine.java
+++ modules/dcache/src/main/java/org/dcache/services/billing/html/HttpBillingHistoryEngine.java
@@ -12,59 +12,56 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Servlet stand-in {@link org.dcache.services.httpd.HttpServiceCell}. <br><br>
+ * Servlet stand-in {@link org.dcache.services.httpd.HttpServiceCell}. <br>
+ * <br>
  * Constructs dynamic html page with table of plots.
  *
  * @see BillingHistory
  * @author arossi
  */
 public final class HttpBillingHistoryEngine extends BillingHistory implements
-HttpResponseEngine {
+                HttpResponseEngine {
 
     private static final Logger _log = LoggerFactory.getLogger(HttpBillingHistoryEngine.class);
     private static final String fileSep = System.getProperty("file.separator");
-
     private static final String THUMBNAIL_W = "120";
     private static final String THUMBNAIL_H = "60";
+    private static final String GENERATE_PLOTS = "generatePlots";
 
     private CellNucleus nucleus;
-    private final Thread _billingHistory;
+    private final boolean generatePlots;
+    private Thread _billingHistory;
 
-    /**
-     * @param nucleus
-     * @param args
-     */
     public HttpBillingHistoryEngine(CellNucleus nucleus, String[] args) {
         super(new Args(args));
         this.nucleus = nucleus;
-        _billingHistory = nucleus.newThread(this, "billing-history");
+        generatePlots = Boolean.valueOf(getArgs().getOpt(GENERATE_PLOTS));
+        if (generatePlots) {
+            _billingHistory = nucleus.newThread(this, "billing-history");
+        }
     }
 
     @Override
-    public void startup()
-    {
-        _billingHistory.start();
+    public void startup() {
+        if (generatePlots) {
+            _billingHistory.start();
+        }
     }
 
-
     @Override
-    public void shutdown()
-    {
-        setRunning(false);
-        _billingHistory.interrupt();
-        try {
-            _billingHistory.join();
-        } catch (InterruptedException e) {
-            _log.warn("Interrupted while waiting for BillingHistory thread to terminate");
+    public void shutdown() {
+        if (generatePlots) {
+            setRunning(false);
+            _billingHistory.interrupt();
+            try {
+                _billingHistory.join();
+            } catch (InterruptedException e) {
+                _log.warn("Interrupted while waiting for BillingHistory thread to terminate");
+            }
         }
         close();
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see dmg.util.HttpResponseEngine#queryUrl(dmg.util.HttpRequest)
-     */
     @Override
     public void queryUrl(HttpRequest request) throws HttpException {
         request.printHttpHeader(0);
@@ -72,46 +69,52 @@ HttpResponseEngine {
                         this.nucleus.getDomainContext()));
     }
 
-    /**
-     * Constructs the html page.
-     *
-     * @param contents
-     */
     public void emit(HTMLWriter contents) {
         contents.addHeader("/styles/common.css", "Billing History Plots");
-        contents.println("<table align='center'>");
-        contents.println("<tr>");
-        contents.println("<th>Plot Type</th>");
-        contents.println("<th>24-Hour</th>");
-        contents.println("<th>Weekly</th>");
-        contents.println("<th>Monthly</th>");
-        contents.println("<th>Yearly</th>");
-        contents.println("</tr>");
-        for (int t = 0; t < BillingHistory.TYPE.length; t++) {
-            printRow(contents, BillingHistory.TITLE[t], BillingHistory.TYPE[t]);
+        if (!generatePlots) {
+            printWarning(contents);
+        } else {
+            contents.println("<table align='center'>");
+            contents.println("<tr>");
+            contents.println("<th>Plot Type</th>");
+            contents.println("<th>24-Hour</th>");
+            contents.println("<th>Weekly</th>");
+            contents.println("<th>Monthly</th>");
+            contents.println("<th>Yearly</th>");
+            contents.println("</tr>");
+            for (int t = 0; t < BillingHistory.TYPE.length; t++) {
+                printRow(contents, BillingHistory.TITLE[t],
+                                BillingHistory.TYPE[t]);
+            }
+            contents.println("</table>");
         }
-        contents.println("</table>");
         contents.addFooter(this.getClass().getName());
         contents.flush();
     }
 
     /**
+     * Generates warning message that plots are turned off.
+     */
+    private void printWarning(HTMLWriter contents) {
+        contents.println("<table align='center'>");
+        contents.println("<tr><td>");
+        contents.println("<h2>Billing Plots Have been Disabled</h2>");
+        contents.println("</td></tr>");
+        contents.println("</table>");
+    }
+
+    /**
      * Generates row in the table containing link to the image file for plot.
-     *
-     * @param contents
-     * @param title
-     * @param type
      */
     private void printRow(HTMLWriter contents, String title, String type) {
         contents.println("<tr>");
         contents.println("<td align='right'><i>" + title + "</i></td>");
-        String prefix =  subDir + fileSep + type;
+        String prefix = subDir + fileSep + type;
         for (String tag : BillingHistory.EXT) {
             String file = prefix + tag + imgExt;
             contents.println("<td><a href='" + file + "'> <img src='" + file
                             + "' width=" + THUMBNAIL_W + " height="
-                            + THUMBNAIL_H
-                            + "> </a></td>");
+                            + THUMBNAIL_H + "> </a></td>");
         }
         contents.println("</tr>");
     }
