dCache/dcache

commit 969903b5ef39ac6a0f8a9394e23822a32661f86e

A step towards configurable cell names for gPlazma and billing
Addresses several cases of hardcoded cell names for the gplazma and
billing services.

The patch does not address all hardcoded names. The patch moves
some hardcoded names from the Java code to the batch file. A subsequent
patch should introduce a property to define the name of the billing
cell to use.

Target: trunk
Require-notes: no
Require-book: no
Ticket: http://rt.dcache.org/Ticket/Display.html?id=6836
Acked-by: Albert Rossi <arossi@fnal.gov>
Patch: http://rb.dcache.org/r/5494/

modules/dcache-dcap/src/main/java/diskCacheV111/doors/DCapDoorInterpreterV3.java
@@ -156,6 +156,7 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
     private String  _poolManagerName;
     private String  _pnfsManagerName;
 
+    private final CellStub _gPlazmaStub;
     private CellStub _pinManagerStub;
     private CellPath _poolMgrPath;
 
@@ -201,7 +202,7 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
 
     private final UnionLoginStrategy.AccessLevel _anonymousAccessLevel;
 
-    protected final CellPath _billingCellPath = new CellPath("billing");
+    private final CellPath _billingCellPath;
     private final InetAddress _clientAddress;
 
     /**
@@ -283,6 +284,8 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
 
         _poolMgrPath     = new CellPath( _poolManagerName ) ;
         _pinManagerStub = new CellStub(cell, new CellPath(_args.getOpt("pinManager")));
+        _gPlazmaStub = new CellStub(_cell, new CellPath(_args.getOpt("gplazma")), 30000);
+        _billingCellPath = new CellPath(_args.getOpt("billing"));
 
         _checkStrict     = _args.hasOption("check") &&
         ( _args.getOpt("check").equals("strict") ) ;
@@ -349,9 +352,7 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
         UnionLoginStrategy union = new UnionLoginStrategy();
 
         if (_authorizationStrong || _authorizationRequired) {
-            LoginStrategy gplazma =
-                    new RemoteLoginStrategy(new CellStub(_cell, new CellPath("gPlazma"), 30000));
-            union.setLoginStrategies(Collections.singletonList(gplazma));
+            union.setLoginStrategies(Collections.<LoginStrategy>singletonList(new RemoteLoginStrategy(_gPlazmaStub)));
         }
 
         if (!_authorizationStrong ) {

modules/dcache-ftp/src/main/java/diskCacheV111/doors/AbstractFtpDoorV1.java
@@ -382,6 +382,16 @@ public abstract class AbstractFtpDoorV1
     )
     protected String _pnfsManager;
 
+    @Option(name = "gplazma",
+            description = "Cell path to gPlazma",
+            defaultValue = "gPlazma")
+    protected String _gPlazma;
+
+    @Option(name = "billing",
+            description = "Cell path to billing",
+            defaultValue = "billing")
+    protected String _billing;
+
     @Option(
         name = "clientDataPortRange"
     )
@@ -618,6 +628,7 @@ public abstract class AbstractFtpDoorV1
     protected CellStub _billingStub;
     protected CellStub _poolManagerStub;
     protected CellStub _poolStub;
+    protected CellStub _gPlazmaStub;
     protected TransferRetryPolicy _readRetryPolicy;
     protected TransferRetryPolicy _writeRetryPolicy;
 
@@ -1166,8 +1177,7 @@ public abstract class AbstractFtpDoorV1
         }
 
         if (_useLoginService) {
-            _loginStrategy =
-                new RemoteLoginStrategy(new CellStub(this, new CellPath("gPlazma"), 30000));
+            _loginStrategy = new RemoteLoginStrategy(_gPlazmaStub);
         } else {
             /* Use kpwd file if login service is not enabled.
              */
@@ -1202,12 +1212,15 @@ public abstract class AbstractFtpDoorV1
                              _engine.getInetAddress());
 
         _billingStub =
-            new CellStub(this, new CellPath("billing"));
+            new CellStub(this, new CellPath(_billing));
         _poolManagerStub =
             new CellStub(this, new CellPath(_poolManager),
                          _poolManagerTimeout * 1000);
         _poolStub =
             new CellStub(this, null, _poolTimeout * 1000);
+
+        _gPlazmaStub =
+                new CellStub(this, new CellPath(_gPlazma), 30000);
 
         _readRetryPolicy =
             new TransferRetryPolicy(_maxRetries, _retryWait * 1000,

modules/dcache-webdav/src/main/resources/org/dcache/webdav/webdav.xml
@@ -44,7 +44,7 @@
 
   <bean id="billing-stub" class="org.dcache.cells.CellStub">
       <description>Billing communication stub</description>
-      <property name="destination" value="billing"/>
+      <property name="destination" value="${billing}"/>
   </bean>
 
 

modules/dcache-xrootd/src/main/resources/org/dcache/xrootd/door/xrootd.xml
@@ -22,7 +22,7 @@
 
   <bean id="billing-stub" class="org.dcache.cells.CellStub">
     <description>Billing communication stub</description>
-    <property name="destination" value="billing"/>
+    <property name="destination" value="${billing}"/>
   </bean>
 
   <bean id="gPlazma-stub" class="org.dcache.cells.CellStub">

modules/dcache/src/main/java/org/dcache/pool/classic/HsmStorageHandler2.java
@@ -7,6 +7,7 @@ import com.google.common.base.Throwables;
 import com.google.common.io.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Required;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -56,6 +57,7 @@ import dmg.cells.nucleus.CellPath;
 import dmg.cells.nucleus.NoRouteToCellException;
 
 import org.dcache.cells.AbstractCellComponent;
+import org.dcache.cells.CellStub;
 import org.dcache.namespace.FileAttribute;
 import org.dcache.pool.repository.EntryState;
 import org.dcache.pool.repository.IllegalTransitionException;
@@ -93,6 +95,7 @@ public class HsmStorageHandler2
     private long _maxRemoveRun = _maxRuntime;
     private int _maxLines = 200;
     private String _flushMessageTarget;
+    private CellStub _billingStub;
 
     ////////////////////////////////////////////////////////////////////////////////
     //
@@ -222,6 +225,12 @@ public class HsmStorageHandler2
 	    throw new IllegalArgumentException("Illegal value of max active restores: " + e.getMessage(),
 					       e);
 	}
+    }
+
+    @Required
+    public void setBillingStub(CellStub billingStub)
+    {
+        _billingStub = billingStub;
     }
 
     @Override
@@ -503,7 +512,7 @@ public class HsmStorageHandler2
         private void sendBillingInfo()
         {
             try {
-                sendMessage(new CellMessage(new CellPath("billing"), _infoMsg));
+                _billingStub.send(_infoMsg);
             } catch (NoRouteToCellException e) {
                 _log.error("Failed to send message to billing: " + e.getMessage());
             }
@@ -834,7 +843,7 @@ public class HsmStorageHandler2
         private void sendBillingInfo()
         {
             try {
-                sendMessage(new CellMessage(new CellPath("billing"), _infoMsg));
+                _billingStub.send(_infoMsg);
             } catch (NoRouteToCellException e) {
                 _log.error("Failed to send message to billing: " + e.getMessage());
             }

modules/dcache/src/main/resources/org/dcache/chimera/nfsv41/door/nfsv41-common.xml
@@ -22,7 +22,7 @@
 
     <bean id="billing-stub" class="org.dcache.cells.CellStub">
         <description>Billing communication stub</description>
-        <property name="destination" value="billing"/>
+        <property name="destination" value="${billing}"/>
     </bean>
 
     <bean id="scheduled-thread-pool"
@@ -77,7 +77,7 @@
 
     <bean id="login-stub" class="org.dcache.cells.CellStub">
         <description>Login cell communication stub</description>
-        <property name="destination" value="gPlazma"/>
+        <property name="destination" value="${gplazma}"/>
         <property name="timeout" value="3000"/>
     </bean>
 

modules/dcache/src/main/resources/org/dcache/pool/classic/pool.xml
@@ -192,6 +192,7 @@
     <constructor-arg ref="hsmset"/>
     <constructor-arg ref="csm"/>
     <property name="flushMessageTarget" value="${flushMessageTarget}"/>
+    <property name="billingStub" ref="billing-stub"/>
   </bean>
 
   <bean id="storage-interpreter" class="org.dcache.pool.classic.HsmStorageInterpreter">
@@ -213,7 +214,7 @@
 
   <bean id="billing-stub" class="org.dcache.cells.CellStub">
       <description>Billing communication stub</description>
-      <property name="destination" value="billing"/>
+      <property name="destination" value="${billing}"/>
   </bean>
 
   <bean id="pnfsStub" class="org.dcache.cells.CellStub">

skel/share/services/authdcap.batch
@@ -9,6 +9,7 @@ check -strong dcapIoQueueOverwrite
 check -strong port
 check -strong pnfsmanager
 check -strong poolmanager
+check -strong gplazma
 check -strong srmSpaceManagerEnabled
 check -strong loginBroker
 check -strong truncate
@@ -54,6 +55,8 @@ create dmg.cells.services.login.LoginManager ${cell.name} \
              -poolManager=${myPoolManager} \
              -poolProxy=${myPoolManager} \
              -pinManager=PinManager \
+             -gplazma=\"${gplazma}\" \
+             -billing=billing \
              -stageConfigurationFilePath=${stageConfigurationFilePath} \
              -io-queue=${dcapIoQueue} \
              -io-queue-overwrite=${dcapIoQueueOverwrite} \

commit 26adacd6d7bd3a50126699c604d207b8a3fdcc7b

gsidcap: fix regression introduced by 969903b5
The patch entitled "a step towards configurable cell names for gPlazma
and billing", committed master@969903b5 added support in various doors
for configuring the gPlazma and billing cell names.

Unfortunately, the part touching the gsidcap door was broken as the
resulting code attempts to create the LoginStrategy before the gPlazma
CellStub is defined.  This results in an NPE any time a client attempts
to connect to dCache.

This patch fixes that issue by creating the LoginStrategy after the
required gPlazma CellStub is defined.

Patch: http://rb.dcache.org/r/5537/
Acked-by: Gerd Behrmann
Target: master

modules/dcache-dcap/src/main/java/diskCacheV111/doors/DCapDoorInterpreterV3.java
@@ -262,8 +262,6 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
             UnionLoginStrategy.AccessLevel.READONLY;
         _log.debug("Anonymous access level : {}", _anonymousAccessLevel);
 
-        _loginStrategy = createLoginStrategy();
-
         _pnfsManagerName = _args.getOpt("pnfsManager");
         _poolManagerName = _args.getOpt("poolManager");
         _poolProxy = _args.getOpt("poolProxy");
@@ -345,6 +343,8 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
         _stageConfigurationFilePath = _args.getOpt("stageConfigurationFilePath");
         _checkStagePermission = new CheckStagePermission(_stageConfigurationFilePath);
         _log.debug("Check : {}", _checkStrict ? "Strict" : "Fuzzy");
+
+        _loginStrategy = createLoginStrategy();
     }
 
     private LoginStrategy createLoginStrategy()