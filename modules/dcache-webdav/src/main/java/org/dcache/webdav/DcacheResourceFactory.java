dCache/dcache

commit b460302934a6dab2cde1202a38883552d62ac65d

implement ability to chroot to user root directory extracted from login record.
FOR RELEASE NOTES:

    add variable called 'webdavChrootToUserRoot' which is false by
    default. If set to true would limit access to namespace only to 
    user root directory extracted from login record. 

Target: trunk
Request: 2.2 
Request: 1.9.12 
Require-notes: yes
Require-book: yes
Patch: http://rb.dcache.org/r/5201/
Acked-by: Paul Millar

modules/dcache-webdav/src/main/java/org/dcache/webdav/DcacheResourceFactory.java
@@ -95,6 +95,11 @@ import static org.dcache.namespace.FileAttribute.*;
 
 import org.dcache.missingfiles.AlwaysFailMissingFileStrategy;
 import org.dcache.missingfiles.MissingFileStrategy;
+
+import org.dcache.auth.attributes.HomeDirectory;
+import org.dcache.auth.attributes.RootDirectory;
+
+
 /**
  * This ResourceFactory exposes the dCache name space through the
  * Milton WebDAV framework.
@@ -163,6 +168,7 @@ public class DcacheResourceFactory
     private String _path;
     private boolean _doRedirectOnRead = true;
     private boolean _doRedirectOnWrite = true;
+    private boolean _doChrootToUserRoot = false;
     private boolean _isOverwriteAllowed;
     private boolean _isAnonymousListingAllowed;
 
@@ -324,6 +330,17 @@ public class DcacheResourceFactory
     public void setIoQueue(String ioQueue)
     {
         _ioQueue = (ioQueue != null && !ioQueue.isEmpty()) ? ioQueue : null;
+    }
+
+    /**
+     * Sers whether chroot to user root extracted from user login record
+     */
+    public void setChrootToUserRoot(boolean chroot) {
+        _doChrootToUserRoot = chroot;
+    }
+
+    public boolean isChrootToUserRoot() {
+        return _doChrootToUserRoot;
     }
 
     /**
@@ -504,22 +521,28 @@ public class DcacheResourceFactory
         if (_log.isDebugEnabled()) {
             _log.debug("Resolving " + HttpManager.request().getAbsoluteUrl());
         }
-
-        return getResource(getFullPath(path));
+        return getResource(new FsPath(path));
     }
 
     /**
      * Returns the resource object for a path.
      *
-     * @param path The full path
+     * @param requestPath request path
      */
-    public DcacheResource getResource(FsPath path)
+    public DcacheResource getResource(FsPath requestPath)
     {
-        if (!isAllowedPath(path)) {
+
+        FsPath fullPath;
+        try {
+            fullPath = getFullPath(requestPath);
+        } catch (IllegalArgumentException e) {
+            _log.debug(e.getMessage());
             return null;
         }
 
-        FsPath requestPath = getRequestPath(path);
+        if (!isAllowedPath(fullPath)) {
+            return null;
+        }
 
         boolean haveRetried = false;
         Subject subject = getSubject();
@@ -534,14 +557,14 @@ public class DcacheResourceFactory
                         requestedAttributes.add(CHECKSUM);
                     }
                     FileAttributes attributes =
-                        pnfs.getFileAttributes(path.toString(), requestedAttributes);
-                    return getResource(path, attributes);
+                        pnfs.getFileAttributes(fullPath.toString(), requestedAttributes);
+                    return getResource(fullPath, attributes);
                 } catch (FileNotFoundCacheException e) {
                     if(haveRetried) {
                         return null;
                     } else {
                         switch(_missingFileStrategy.recommendedAction(subject,
-                                path, requestPath)) {
+                                fullPath, requestPath)) {
                         case FAIL:
                             return null;
                         case RETRY:
@@ -954,12 +977,45 @@ public class DcacheResourceFactory
     }
 
     /**
+     * Calculate root path based on _rootPath and User Root directory
+     * extracted from login record and taking into account _rootPath
+     * if necessary
+     */
+
+    private FsPath getUserRootPath()
+    {
+        FsPath path = _rootPath;
+        if (!_doChrootToUserRoot) {
+            return path;
+        }
+        Request request = HttpManager.request();
+        FsPath userRootDirectory = (request.getAttributes().containsKey(SecurityFilter.USER_ROOT_DIRECTORY)) ?
+            (FsPath)request.getAttributes().get(SecurityFilter.USER_ROOT_DIRECTORY) : null;
+        if (userRootDirectory!=null)  {
+            if (userRootDirectory.startsWith(_rootPath)) {
+                path=userRootDirectory;
+            } else {
+                if (!_rootPath.startsWith(userRootDirectory)) {
+                    throw new
+                        IllegalArgumentException("webdavRootPath = "+
+                                                 _rootPath+
+                                                 " and user root path = "+
+                                                 userRootDirectory +
+                                                 " are not subpaths of each other");
+                }
+            }
+        }
+        return path;
+    }
+
+    /**
      * Given a path relative to the root path, this method returns a
      * full PNFS path.
      */
-    private FsPath getFullPath(String path)
+    private FsPath getFullPath(FsPath path)
     {
-        return new FsPath(_rootPath, new FsPath(path));
+        FsPath rootPath = getUserRootPath();
+        return new FsPath(rootPath, new FsPath(path));
     }
 
     /**
@@ -968,7 +1024,8 @@ public class DcacheResourceFactory
      */
     private FsPath getRequestPath(FsPath internalPath)
     {
-        return _rootPath.relativize(internalPath);
+        FsPath rootPath = getUserRootPath();
+        return rootPath.relativize(internalPath);
     }
 
     /**

modules/dcache-webdav/src/main/java/org/dcache/webdav/SecurityFilter.java
@@ -20,9 +20,12 @@ import org.dcache.auth.LoginReply;
 import org.dcache.auth.PasswordCredential;
 import org.dcache.auth.attributes.LoginAttribute;
 import org.dcache.auth.attributes.ReadOnly;
+import org.dcache.auth.attributes.HomeDirectory;
+import org.dcache.auth.attributes.RootDirectory;
 import org.dcache.auth.Origin;
 
 import diskCacheV111.util.CacheException;
+import diskCacheV111.util.FsPath;
 import diskCacheV111.util.PermissionDeniedCacheException;
 
 import org.slf4j.Logger;
@@ -51,9 +54,10 @@ import org.slf4j.LoggerFactory;
 public class SecurityFilter implements Filter
 {
     private final Logger _log = LoggerFactory.getLogger(SecurityFilter.class);
-
     private static final String X509_CERTIFICATE_ATTRIBUTE =
         "javax.servlet.request.X509Certificate";
+    public static final String USER_ROOT_DIRECTORY="org.dcache.user.root";
+    public static final String USER_HOME_DIRECTORY="org.dcache.user.home";
 
     private String _realm;
     private boolean _isReadOnly;
@@ -86,7 +90,18 @@ public class SecurityFilter implements Filter
             if (!isAuthorizedMethod(request.getMethod(), login)) {
                 throw new PermissionDeniedCacheException("Permission denied");
             }
-
+            /*
+             * Add user root and home directory to request
+             */
+            for (LoginAttribute attribute: login.getLoginAttributes()) {
+                if (attribute instanceof RootDirectory) {
+                    request.getAttributes().put(USER_ROOT_DIRECTORY,
+                                                new FsPath(((RootDirectory) attribute).getRoot()));
+                } else if (attribute instanceof HomeDirectory) {
+                    request.getAttributes().put(USER_HOME_DIRECTORY,
+                                                new FsPath(((HomeDirectory) attribute).getHome()));
+                }
+            }
             /* Add the origin of the request to the subject. This
              * ought to be processed in the LoginStrategy, but our
              * LoginStrategies currently do not process the Origin.

modules/dcache-webdav/src/main/resources/org/dcache/webdav/webdav.xml
@@ -110,6 +110,7 @@
       <property name="missingFileStrategy" ref="missing-file-strategy"/>
       <property name="listHandler" ref="list-handler"/>
       <property name="rootPath" value="${webdavRootPath}"/>
+      <property name="chrootToUserRoot" value="${webdavChrootToUserRoot}"/>
       <property name="allowedPaths" value="${webdavAllowedPaths}"/>
       <property name="ioQueue" value="${webdavIoQueue}"/>
       <property name="executor" ref="scheduled-thread-pool"/>

skel/share/defaults/webdav.properties
@@ -299,6 +299,14 @@ webdav.templates.html=file:${dcache.paths.share}/webdav/templates/html.stg
 #
 (immutable)webdav/net.ports.tcp=${port}
 
+#  ---- Whether chroot to user root directory extracted from user login record
+#
+#   This paramerer specified whether the WebDAV door will expose full
+#   namespace (relative to webdavRootPath) or only relative to user
+#   root directory.
+#
+(one-of?true|false)webdavChrootToUserRoot=false
+
 #  ---- Artwork elements
 #
 #  These properties are obsolete. Modify the template to customize the

skel/share/services/webdav.batch
@@ -129,6 +129,7 @@ create org.dcache.cells.UniversalSpringCell ${cell.name} \
     -webdavTransferConfirmationTimeout=${webdavTransferConfirmationTimeout} \
     -webdavWantClientAuth=${webdavWantClientAuth} \
     -webdavNeedClientAuth=${webdavNeedClientAuth} \
+    -webdavChrootToUserRoot=${webdavChrootToUserRoot} \
     -hostCertificateRefreshPeriod=${hostCertificateRefreshPeriod} \
     -trustAnchorRefreshPeriod=${trustAnchorRefreshPeriod} \
     -pnfsmanager=${pnfsmanager} \

commit e48a670d502815203704af59a2dedfa7307c13ba

webdav: Fix problems with root paths
Addresses several issues in the webdav door:

- HTTP PUT without redirect is broken in earlier versions of 2.6. This
  regression was introduced in b460302934a6dab2cde1202a38883552d62ac65d.

  The problem is that the the resource path after upload is produced by
  concatenating the server's root path with a full dCache name space path,
  thus the root path appear twice in the generated path.

- The URI name space depends on who authenticates, which means that the
  same URI can have several meanings. This makes bookmarking and linking
  difficult.

- The CSS refers to a non-existing background image.

The solution to these problems are related. The patch removes the
webdavChrootToUserRoot property. An authenticated users root directory
is now always enforced, but only by checking that the request is within
the users root path. If a user has a non-null root and accesses the
root of the webdav door, a redirect to the users home directory is
generated.

The above change resulted in repeated authentication requests in the
browser, because the browser would try to access a non-existing
background image and a non-existing favicon.ico file. Access to these
would be denied, as they are outside the users root directory, and
thus the browser would ask for another user name and password. To
solve this problem, the patch fixes the CSS and adds a favicon.ico file.

An earlier version of this patch was submitted as http://rb.dcache.org/r/5201/

Target: trunk
Request: 2.6
Require-book: yes
Require-notes: yes
Acked-by: Dmitry Litvintsev <litvinse@fnal.gov>
Patch: http://rb.dcache.org/r/5672/

modules/dcache-webdav/src/main/java/org/dcache/webdav/DcacheResourceFactory.java
@@ -92,7 +92,6 @@ import static java.util.Arrays.asList;
 import static org.dcache.namespace.FileAttribute.*;
 import static org.dcache.namespace.FileType.*;
 
-
 /**
  * This ResourceFactory exposes the dCache name space through the
  * Milton WebDAV framework.
@@ -151,7 +150,6 @@ public class DcacheResourceFactory
     private String _path;
     private boolean _doRedirectOnRead = true;
     private boolean _doRedirectOnWrite = true;
-    private boolean _doChrootToUserRoot = false;
     private boolean _isOverwriteAllowed;
     private boolean _isAnonymousListingAllowed;
 
@@ -313,17 +311,6 @@ public class DcacheResourceFactory
     public void setIoQueue(String ioQueue)
     {
         _ioQueue = (ioQueue != null && !ioQueue.isEmpty()) ? ioQueue : null;
-    }
-
-    /**
-     * Sers whether chroot to user root extracted from user login record
-     */
-    public void setChrootToUserRoot(boolean chroot) {
-        _doChrootToUserRoot = chroot;
-    }
-
-    public boolean isChrootToUserRoot() {
-        return _doChrootToUserRoot;
     }
 
     /**
@@ -494,28 +481,21 @@ public class DcacheResourceFactory
         if (_log.isDebugEnabled()) {
             _log.debug("Resolving " + HttpManager.request().getAbsoluteUrl());
         }
-        return getResource(new FsPath(path));
+         return getResource(getFullPath(path));
     }
 
     /**
      * Returns the resource object for a path.
      *
-     * @param requestPath request path
+     * @param path The full path
      */
-    public DcacheResource getResource(FsPath requestPath)
+    public DcacheResource getResource(FsPath path)
     {
-        FsPath fullPath;
-        try {
-            fullPath = getFullPath(requestPath);
-        } catch (IllegalArgumentException e) {
-            _log.debug(e.getMessage());
+        if (!isAllowedPath(path)) {
             return null;
         }
 
-        if (!isAllowedPath(fullPath)) {
-            return null;
-        }
-
+        FsPath requestPath = getRequestPath(path);
         boolean haveRetried = false;
         Subject subject = getSubject();
 
@@ -529,14 +509,14 @@ public class DcacheResourceFactory
                         requestedAttributes.add(CHECKSUM);
                     }
                     FileAttributes attributes =
-                        pnfs.getFileAttributes(fullPath.toString(), requestedAttributes);
-                    return getResource(fullPath, attributes);
+                        pnfs.getFileAttributes(path.toString(), requestedAttributes);
+                    return getResource(path, attributes);
                 } catch (FileNotFoundCacheException e) {
                     if(haveRetried) {
                         return null;
                     } else {
                         switch(_missingFileStrategy.recommendedAction(subject,
-                                fullPath, requestPath)) {
+                                path, requestPath)) {
                         case FAIL:
                             return null;
                         case RETRY:
@@ -949,45 +929,12 @@ public class DcacheResourceFactory
     }
 
     /**
-     * Calculate root path based on _rootPath and User Root directory
-     * extracted from login record and taking into account _rootPath
-     * if necessary
-     */
-
-    private FsPath getUserRootPath()
-    {
-        FsPath path = _rootPath;
-        if (!_doChrootToUserRoot) {
-            return path;
-        }
-        Request request = HttpManager.request();
-        FsPath userRootDirectory = (request.getAttributes().containsKey(SecurityFilter.USER_ROOT_DIRECTORY)) ?
-            (FsPath)request.getAttributes().get(SecurityFilter.USER_ROOT_DIRECTORY) : null;
-        if (userRootDirectory!=null)  {
-            if (userRootDirectory.startsWith(_rootPath)) {
-                path=userRootDirectory;
-            } else {
-                if (!_rootPath.startsWith(userRootDirectory)) {
-                    throw new
-                        IllegalArgumentException("webdavRootPath = "+
-                                                 _rootPath+
-                                                 " and user root path = "+
-                                                 userRootDirectory +
-                                                 " are not subpaths of each other");
-                }
-            }
-        }
-        return path;
-    }
-
-    /**
      * Given a path relative to the root path, this method returns a
      * full PNFS path.
      */
-    private FsPath getFullPath(FsPath path)
+    private FsPath getFullPath(String path)
     {
-        FsPath rootPath = getUserRootPath();
-        return new FsPath(rootPath, new FsPath(path));
+        return new FsPath(_rootPath, new FsPath(path));
     }
 
     /**
@@ -996,9 +943,9 @@ public class DcacheResourceFactory
      */
     private FsPath getRequestPath(FsPath internalPath)
     {
-        FsPath rootPath = getUserRootPath();
-        return rootPath.relativize(internalPath);
+        return _rootPath.relativize(internalPath);
     }
+
 
     /**
      * Returns true if access to path is allowed through the WebDAV

modules/dcache-webdav/src/main/java/org/dcache/webdav/SecurityFilter.java
@@ -14,6 +14,8 @@ import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
 
 import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.security.PrivilegedAction;
 import java.security.cert.X509Certificate;
@@ -56,13 +58,12 @@ public class SecurityFilter implements Filter
     private final Logger _log = LoggerFactory.getLogger(SecurityFilter.class);
     private static final String X509_CERTIFICATE_ATTRIBUTE =
         "javax.servlet.request.X509Certificate";
-    public static final String USER_ROOT_DIRECTORY="org.dcache.user.root";
-    public static final String USER_HOME_DIRECTORY="org.dcache.user.home";
 
     private String _realm;
     private boolean _isReadOnly;
     private boolean _isBasicAuthenticationEnabled;
     private LoginStrategy _loginStrategy;
+    private FsPath _rootPath = new FsPath();
 
     @Override
     public void process(final FilterChain filterChain,
@@ -90,18 +91,9 @@ public class SecurityFilter implements Filter
             if (!isAuthorizedMethod(request.getMethod(), login)) {
                 throw new PermissionDeniedCacheException("Permission denied");
             }
-            /*
-             * Add user root and home directory to request
-             */
-            for (LoginAttribute attribute: login.getLoginAttributes()) {
-                if (attribute instanceof RootDirectory) {
-                    request.getAttributes().put(USER_ROOT_DIRECTORY,
-                                                new FsPath(((RootDirectory) attribute).getRoot()));
-                } else if (attribute instanceof HomeDirectory) {
-                    request.getAttributes().put(USER_HOME_DIRECTORY,
-                                                new FsPath(((HomeDirectory) attribute).getHome()));
-                }
-            }
+
+            checkRootPath(request, login);
+
             /* Add the origin of the request to the subject. This
              * ought to be processed in the LoginStrategy, but our
              * LoginStrategies currently do not process the Origin.
@@ -127,12 +119,52 @@ public class SecurityFilter implements Filter
                             return null;
                         }
                 });
+        } catch (RedirectException e) {
+            manager.getResponseHandler().respondRedirect(response, request, e.getUrl());
         } catch (PermissionDeniedCacheException e) {
-            _log.warn("Login failed for " + subject);
+            _log.warn("{} for path {} and {}", e.getMessage(), request.getAbsolutePath(), subject);
             manager.getResponseHandler().respondUnauthorised(new EmptyResource(request), response, request);
         } catch (CacheException e) {
             _log.error("Internal server error: " + e);
             manager.getResponseHandler().respondServerError(request, response, e.getMessage());
+        }
+    }
+
+    /**
+     * Verifies that the request is within the root directory of the given user.
+     *
+     * @throws RedirectException If the request should be redirected
+     * @throws PermissionDeniedCacheException If the request is denied
+     * @throws CacheException If the request fails
+     */
+    private void checkRootPath(Request request, LoginReply login) throws CacheException
+    {
+        FsPath userRoot = new FsPath();
+        FsPath userHome = new FsPath();
+        for (LoginAttribute attribute: login.getLoginAttributes()) {
+            if (attribute instanceof RootDirectory) {
+                userRoot = new FsPath(((RootDirectory) attribute).getRoot());
+            } else if (attribute instanceof HomeDirectory) {
+                userHome = new FsPath(((HomeDirectory) attribute).getHome());
+            }
+        }
+
+        String path = request.getAbsolutePath();
+        FsPath fullPath = new FsPath(_rootPath, new FsPath(path));
+        if (!fullPath.startsWith(userRoot)) {
+            if (!path.equals("/")) {
+                throw new PermissionDeniedCacheException("Permission denied");
+            }
+
+            try {
+                FsPath redirectFullPath = new FsPath(userRoot, userHome);
+                String redirectPath = _rootPath.relativize(redirectFullPath).toString();
+                URI uri = new URI(request.getAbsoluteUrl());
+                URI redirect = new URI(uri.getScheme(), uri.getAuthority(), redirectPath, null, null);
+                throw new RedirectException(null, redirect.toString());
+            } catch (URISyntaxException e) {
+                throw new CacheException(e.getMessage(), e);
+            }
         }
     }
 
@@ -236,5 +268,15 @@ public class SecurityFilter implements Filter
     public void setLoginStrategy(LoginStrategy loginStrategy)
     {
         _loginStrategy = loginStrategy;
+    }
+
+    public String getRootPath()
+    {
+        return _rootPath.toString();
+    }
+
+    public void setRootPath(String path)
+    {
+        _rootPath = new FsPath(path);
     }
 }

modules/dcache-webdav/src/main/resources/org/dcache/webdav/webdav.xml
@@ -89,6 +89,7 @@
       <property name="readOnly" value="${webdavReadOnly}"/>
       <property name="loginStrategy" ref="login-strategy"/>
       <property name="enableBasicAuthentication" value="${webdavBasicAuthentication}"/>
+      <property name="rootPath" value="${webdavRootPath}"/>
   </bean>
 
 
@@ -111,7 +112,6 @@
       <property name="missingFileStrategy" ref="missing-file-strategy"/>
       <property name="listHandler" ref="list-handler"/>
       <property name="rootPath" value="${webdavRootPath}"/>
-      <property name="chrootToUserRoot" value="${webdavChrootToUserRoot}"/>
       <property name="allowedPaths" value="${webdavAllowedPaths}"/>
       <property name="ioQueue" value="${webdavIoQueue}"/>
       <property name="executor" ref="scheduled-thread-pool"/>

skel/share/defaults/webdav.properties
@@ -299,14 +299,6 @@ webdav.templates.html=file:${dcache.paths.share}/webdav/templates/html.stg
 #
 (immutable)webdav/net.ports.tcp=${port}
 
-#  ---- Whether chroot to user root directory extracted from user login record
-#
-#   This paramerer specified whether the WebDAV door will expose full
-#   namespace (relative to webdavRootPath) or only relative to user
-#   root directory.
-#
-(one-of?true|false)webdavChrootToUserRoot=false
-
 #  Security related properties
 webdav.security.ciphers=${dcache.security.ciphers}
 

skel/share/webdav/static/styles/webdav.css
@@ -176,7 +176,6 @@ table.sortable thead tr th, table.sortable tfoot tr th {
 
 table.sortable thead tr .header {
 
-	background-image: url(/.webdav/images/bg.gif);
 	background-repeat: no-repeat;
 	background-position: center right;
 	cursor: pointer;

skel/share/webdav/templates/html.stg
@@ -125,6 +125,7 @@ header() ::= <<
     <head>
         <title>dCache.org - File system</title>
         <link rel="stylesheet" type="text/css" href="$static$/styles/webdav.css"/>
+        <link rel="icon" type="image/x-icon" href="$static$/images/favicon.ico"/>
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
         <script type="text/javascript" src="$static$/tablesorter/jquery.tablesorter.min.js"></script>
         <script type="text/javascript">

packages/system-test/pom.xml
@@ -45,6 +45,7 @@
                                       <filename name="**/*.jpg" />
                                       <filename name="**/*.png" />
                                       <filename name="**/*.jar" />
+                                      <filename name="**/*.ico" />
                                   </or>
                               </selector>
                               <copy todir="${filtered-skel}" overwrite="true" preservelastmodified="true">