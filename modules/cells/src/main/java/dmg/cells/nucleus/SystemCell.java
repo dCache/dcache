dCache/dcache

commit ce8196b2e51d4843ebfd35a264e78f1af39d2fb3

System cell: fix kill command
The patch http://rb.dcache.org/r/4278/, committed to master@7071473dc27df13
(pre-2.2) and merged 1.9.12@51e03907399a510 fixes the problem that a
kill command may take time to complete. In particular, when working
around a configuration issue, an existing gPlazma is killed before a
fresh gPlazma is started. If the new gPlazma is started before the old
cell has died then the new gPlazma will not start. This is the
race-condition the patch fixes.

Unfortunately, the patch also introduces a deadlock. When attempting
to kill the System cell, the message-processing thread is blocked
waiting for a signal that the cell has died. This signal is generated
when processing an event by the message-processing thread which,
because the kill command is blocking, will never happen.

This patch fixes the problem by replying with a DelayedReply; an
implementation of the future Reply. The patch also adds support for
replying with a Reply to SystemCell.

Target: trunk
Request: 2.2
Request: 1.9.12
Require-notes: yes
Require-book: no
Patch: http://rb.dcache.org/r/5115/
Acked-by: Gerd Behrmann
Committed: master@d20662a349d6a520fa1dd3b5dba61d7b905b9d97

modules/cells/src/main/java/dmg/cells/nucleus/SystemCell.java
@@ -241,42 +241,49 @@ public class      SystemCell
             return ;
          }
         Object obj  = msg.getMessageObject() ;
+        Object reply = null; // dummy value needed for Java, not used.
+        boolean processed = false;
+
         if( obj instanceof String ){
            String command = (String)obj ;
            if( command.length() < 1 )return ;
-           Object reply = null ;
            _log.info( "Command : "+command ) ;
            reply = _cellShell.objectCommand2( command ) ;
-           _log.info( "Reply : "+reply ) ;
-           msg.setMessageObject( reply ) ;
-           _packetsAnswered ++ ;
+           processed = true;
         }else if( obj instanceof AuthorizedString ){
            AuthorizedString as = (AuthorizedString)obj ;
            String command = as.toString() ;
            if( command.length() < 1 )return ;
-           Object reply = null ;
            _log.info( "Command(p="+as.getAuthorizedPrincipal()+") : "+command ) ;
            reply = _cellShell.objectCommand2( command ) ;
-           _log.info( "Reply : "+reply ) ;
-           msg.setMessageObject( reply ) ;
-           _packetsAnswered ++ ;
+           processed = true;
         }else if( obj instanceof CommandRequestable ){
            CommandRequestable request = (CommandRequestable)obj ;
-           Object reply = null ;
            try{
               _log.info( "Command : "+request.getRequestCommand() ) ;
               reply = _cellShell.command( request ) ;
            }catch( CommandException cee ){
               reply = cee ;
            }
-           _log.info( "Reply : "+reply ) ;
-           msg.setMessageObject( reply ) ;
-           _packetsAnswered ++ ;
+           processed = true;
         }
+
+        if (processed) {
+            _log.debug("Reply : {}", reply);
+            _packetsAnswered++;
+        }
+
+        msg.revertDirection();
+
         try{
-           msg.revertDirection() ;
-           sendMessage( msg ) ;
-           _log.info( "Sending : "+msg ) ;
+            if (processed && reply instanceof Reply) {
+                ((Reply)reply).deliver(this, msg);
+            } else {
+                if (processed) {
+                    sendMessage(msg);
+                    _log.debug("Sending : {}", msg);
+                }
+            }
            _packetsReplied++ ;
         }catch( Exception e ){
            _exceptionCounter ++ ;

modules/cells/src/main/java/dmg/cells/nucleus/CellShell.java
@@ -600,12 +600,50 @@ public class      CellShell
           " Syntax : kill <cellName>\n"+
           "          Starts the killl mechanism on the specified cell\n"+
           "         and removes it from the cell list\n" ;
-   public String ac_kill_$_1( Args args )
+   public Reply ac_kill_$_1( Args args )
          throws IllegalArgumentException, InterruptedException
    {
-      _nucleus.kill( args.argv(0) );
-      _nucleus.join(args.argv(0), 0);
-      return "\n" ;
+       // The killing of a cell requires delivery of a message to the targeted
+       // cell.  If CellShell is running in the targeted shell then the call
+       // to _nucleus.join will never return unless we free up the
+       // message-processing thread by returning from this method.  We return a
+       // delayed reply to achieve this while also not delivering the reply
+       // until after the cell has terminated.
+       final DelayedReply future = new DelayedReply();
+       final String cell = args.argv(0);
+
+       Thread thread = new Thread("kill " + cell + " command") {
+           @Override
+           public void run()
+           {
+               Serializable response = "";
+               try {
+                   try {
+                       _nucleus.kill(cell);
+                       _nucleus.join(cell, 0);
+                   } catch (IllegalArgumentException e) {
+                       response = e;
+                   }
+
+                   try {
+                       future.send(response);
+                   } catch (NoRouteToCellException e) {
+                       _log.error("problem sending result of 'kill {}' " +
+                               "command: {}", cell, e.getMessage());
+                   } catch (SerializationException e) {
+                       _log.error("problem sending result of 'kill {}' " +
+                               "command: {}", cell, e.getMessage());
+                   }
+               } catch (InterruptedException e) {
+                   // Do nothing. dCache is shutting down.
+               }
+           }
+       };
+
+       thread.setDaemon(true);
+       thread.start();
+
+       return future;
    }
    ////////////////////////////////////////////////////////////
    //

commit ecbb90e39be3257136e8cddb9e05cc7840041b74

cells: fix handling of ascii commands in SystemCell
regression introduced by ce8196b

Ticket: #7961, #7962
Acked-by: Karsten Schwank
Target: 2.2
Require-book: no
Require-notes: yes

modules/cells/src/main/java/dmg/cells/nucleus/SystemCell.java
@@ -280,6 +280,7 @@ public class      SystemCell
                 ((Reply)reply).deliver(this, msg);
             } else {
                 if (processed) {
+                    msg.setMessageObject( reply ) ;
                     sendMessage(msg);
                     _log.debug("Sending : {}", msg);
                 }