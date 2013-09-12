9f347d1d2020a0175110732ace463e4d530f6b69
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2013-09-12 11:09:23 +0200

    namespace: remove dead FileAttribute to NFS atts method
    Acked-by: Paul Millar
    Target: master
    Require-book: no
    Require-notes: no

--- modules/dcache/src/main/java/org/dcache/namespace/FileAttribute.java
+++ modules/dcache/src/main/java/org/dcache/namespace/FileAttribute.java
@@ -1,7 +1,5 @@
 package org.dcache.namespace;
 
-import static org.dcache.acl.enums.FileAttribute.*;
-
 /**
  *
  * File attributes supported by dCache
@@ -30,37 +28,4 @@ public enum FileAttribute {
     TYPE,
     SIMPLE_TYPE,
     PNFSID;
-
-    /**
-     * Returns the matching NFS4 attribute, or null if there is
-     * no matching attribute in NFS4.
-     */
-    public org.dcache.acl.enums.FileAttribute toNfs4Attribute()
-    {
-        switch (this) {
-        case ACL:
-            return FATTR4_ACL;
-        case ACCESS_TIME:
-            return FATTR4_TIME_ACCESS;
-        case CREATION_TIME:
-            return FATTR4_TIME_CREATE;
-        case MODE:
-            return FATTR4_MODE;
-        case MODIFICATION_TIME:
-            return FATTR4_TIME_MODIFY;
-        case OWNER:
-            return FATTR4_OWNER;
-        case OWNER_GROUP:
-            return FATTR4_OWNER_GROUP;
-        case SIZE:
-            return FATTR4_SIZE;
-        case TYPE:
-        case SIMPLE_TYPE:
-            return FATTR4_TYPE;
-        default:
-            return null;
-        }
-    }
-
-    // parent
 }
