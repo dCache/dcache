f43759f95d688f3f0c03d8ee4eeb53b60c89423e
alrossi <arossi@otfrid.fnal.gov>
2013-09-20 09:35:36 -0500

    (2.2) dcache (billing): rewrite triggers to eliminate multiple repeated updates
    This is the 2.2 version of patch http://rb.dcache.org/r/5983
    
    Please refer to it for full discussion.
    
    Testing:
    
    as with http://rb.dcache.org/r/5983 (excepting system-test, which does not exist)
    
    Target: 2.2
    Request: 2.2
    Patch: http://rb.dcache.org/r/5984/
    Bug: http://rt.dcache.org/Ticket/Display.html?id=7915
    Require-book: no
    Require-notes: yes
    Acked-by: Dmitry
    
    RELEASE NOTES:
    
    Fixes a problem with the billing database triggers which under continuous heavy loads (> 300 Hz of billing messages) led eventually to an Out-Of-Memory error in the billing domain.

--- modules/dcache/src/main/java/org/dcache/services/billing/db/data/DcacheTimeDaily.java
+++ modules/dcache/src/main/java/org/dcache/services/billing/db/data/DcacheTimeDaily.java
@@ -4,88 +4,51 @@ import java.util.Map;
 
 /**
  * @author arossi
- *
  */
 public final class DcacheTimeDaily extends BaseDaily implements IPlotData {
-
     public static final String MIN_TIME = "minimum";
     public static final String MAX_TIME = "maximum";
     public static final String AVG_TIME = "average";
 
-    private Long totalTime = Long.MAX_VALUE;
     private Long minimum= 0L;
     private Long maximum= 0L;
+    private Double average = 0.0;
 
     public String toString() {
         return "(" + dateString() + "," + count + "," + minimum + "," + maximum
-                        + "," + average() + ")";
+                        + "," + average + ")";
     }
 
-    /**
-     * @return the totalTime
-     */
-    public Long getTotalTime() {
-        return totalTime;
+    public Double getAverage() {
+        return average;
     }
 
-    /**
-     * @param totalTime
-     *            the totalTime to set
-     */
-    public void setTotalTime(Long totalTime) {
-        this.totalTime = totalTime;
+    public void setAverage(Double average) {
+        this.average = average;
     }
 
-    /**
-     * @return the minimum
-     */
     public Long getMinimum() {
         return minimum;
     }
 
-    /**
-     * @param minimum
-     *            the minimum to set
-     */
     public void setMinimum(Long minimum) {
         this.minimum = minimum;
     }
 
-    /**
-     * @return the maximum
-     */
     public Long getMaximum() {
         return maximum;
     }
 
-    /**
-     * @param maximum
-     *            the maximum to set
-     */
     public void setMaximum(Long maximum) {
         this.maximum = maximum;
     }
 
-    /**
-     * @return the average
-     */
-    public Double average() {
-        if (count == 0)
-            return 0.0;
-        return totalTime / (double) count;
-    }
-
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.dcache.billing.data.ITimestampedData#data()
-     */
     @Override
     public Map<String, Double> data() {
         Map<String, Double> dataMap = super.data();
         dataMap.put(MIN_TIME, minimum.doubleValue());
         dataMap.put(MAX_TIME, maximum.doubleValue());
-        dataMap.put(AVG_TIME, average().doubleValue());
+        dataMap.put(AVG_TIME, average);
         return dataMap;
     }
 }
