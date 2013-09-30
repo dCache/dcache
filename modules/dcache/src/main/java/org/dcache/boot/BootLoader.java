91975d16daef0dff5f3f6d896ae14db01ef0247c
Gerd Behrmann <behrmann@gmail.com>
2013-09-30 21:04:56 +0200

    Refactor BootLoader
    The patch moves the part for loading the dCache configuration into
    the new LayoutBuilder class.
    
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: http://rb.dcache.org/r/6059/

--- modules/dcache/src/main/java/org/dcache/boot/BootLoader.java
+++ modules/dcache/src/main/java/org/dcache/boot/BootLoader.java
@@ -1,37 +1,23 @@
 package org.dcache.boot;
 
-import com.google.common.base.Function;
-import com.google.common.base.Joiner;
-import com.google.common.collect.Sets;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Set;
 import java.util.logging.LogManager;
 
 import dmg.util.Args;
 import dmg.util.CommandException;
 
-import org.dcache.util.ConfigurationProperties;
 import org.dcache.util.ConfigurationProperties.DefaultProblemConsumer;
 import org.dcache.util.ConfigurationProperties.ProblemConsumer;
 
 import static com.google.common.base.Throwables.getRootCause;
-import static com.google.common.collect.Iterables.transform;
-import static org.dcache.boot.Properties.*;
 
 /**
  * Boot loader for dCache. This class contains the main method of
@@ -51,9 +37,6 @@ public class BootLoader
     private static final String CMD_CHECK = "check-config";
 
     private static final char OPT_SILENT = 'q';
-
-    private static Set<File> _sourceFiles = Sets.newHashSet();
-    private static Set<File> _sourceDirectories = Sets.newHashSet();
 
     private BootLoader()
     {
@@ -84,151 +67,6 @@ public class BootLoader
         System.exit(1);
     }
 
-    private static ConfigurationProperties loadSystemProperties()
-        throws UnknownHostException
-    {
-        ConfigurationProperties config =
-            new ConfigurationProperties(System.getProperties());
-        InetAddress localhost = InetAddress.getLocalHost();
-        config.setProperty(PROPERTY_HOST_NAME,
-                           localhost.getHostName().split("\\.")[0]);
-        config.setProperty(PROPERTY_HOST_FQDN,
-                           localhost.getCanonicalHostName());
-        return config;
-    }
-
-    private static ConfigurationProperties
-        loadConfiguration(ConfigurationProperties config, File path)
-        throws IOException
-    {
-        config = new ConfigurationProperties(config);
-        if (path.isFile()) {
-	    _sourceFiles.add(path);
-            config.loadFile(path);
-        } else if (path.isDirectory()) {
-	    _sourceDirectories.add(path);
-            File[] files = path.listFiles();
-            if (files != null) {
-                Arrays.sort(files);
-                for (File file: files) {
-                    if (file.isFile() && file.getName().endsWith(".properties")) {
-			_sourceFiles.add(file);
-                        config.loadFile(file);
-                    }
-                }
-            }
-        }
-        return config;
-    }
-
-    private static ConfigurationProperties
-        loadConfiguration(ConfigurationProperties config, String property)
-        throws IOException
-    {
-        String paths = config.getValue(property);
-        if (paths != null) {
-            for (String path: paths.split(PATH_DELIMITER)) {
-                config = loadConfiguration(config, new File(path));
-            }
-        }
-        return config;
-    }
-
-    /**
-     * Loads plugins in a plugin directory.
-     *
-     * A plugin directory contains a number of plugins. Each plugin is
-     * stored in a sub-directory containing that one plugin.
-     */
-    private static ConfigurationProperties
-        loadPlugins(ConfigurationProperties config, File directory)
-        throws IOException
-    {
-        _sourceDirectories.add(directory);
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file: files) {
-                if (file.isDirectory()) {
-                    config = loadConfiguration(config, file);
-                }
-            }
-        }
-        return config;
-    }
-
-    private static ConfigurationProperties
-        loadConfiguration(ProblemConsumer consumer)
-        throws UnknownHostException, IOException, URISyntaxException
-    {
-        /* Configuration properties are loaded from several
-         * sources, starting with importing Java system
-         * properties...
-         */
-        ConfigurationProperties config = loadSystemProperties();
-        config.setProblemConsumer(consumer);
-
-        /* ... and a chain of properties files. */
-        config = loadConfiguration(config, PROPERTY_DEFAULTS_PATH);
-        for (String dir: getPluginDirs()) {
-            config = loadPlugins(config, new File(dir));
-        }
-        config = loadConfiguration(config, PROPERTY_SETUP_PATH);
-
-        return config;
-    }
-
-    private final static Function<File, String> QUOTE_FILE =
-	new Function<File, String>()
-        {
-	    @Override
-	    public String apply(File input)
-	    {
-		return '"' + input.getPath() + '"';
-	    }
-	};
-
-    private static Layout loadLayout(ConfigurationProperties config)
-        throws IOException, URISyntaxException
-    {
-        String path = config.getValue(PROPERTY_DCACHE_LAYOUT_URI);
-        if (path == null) {
-            throw new IOException("Undefined property: " + PROPERTY_DCACHE_LAYOUT_URI);
-        }
-        URI uri = new URI(path);
-        Layout layout = new Layout(config);
-        layout.load(uri);
-
-        if (Objects.equals(uri.getScheme(), "file")) {
-            _sourceFiles.add(new File(uri.getPath()));
-        } else {
-            layout.properties().setProperty(PROPERTY_DCACHE_CONFIG_CACHE, "false");
-        }
-        layout.properties().setProperty(PROPERTY_DCACHE_CONFIG_FILES,
-					Joiner.on(" ").join(transform(_sourceFiles, QUOTE_FILE)));
-        layout.properties().setProperty(PROPERTY_DCACHE_CONFIG_DIRS,
-					Joiner.on(" ").join(transform(_sourceDirectories, QUOTE_FILE)));
-        return layout;
-    }
-
-    /**
-     * Returns the top-level plugin directory.
-     *
-     * To allow the plugin directory to be configurable, we first have
-     * to load all the configuration files without the plugins.
-     */
-    private static String[] getPluginDirs()
-        throws IOException, URISyntaxException
-    {
-        ConfigurationProperties config = loadSystemProperties();
-        ProblemConsumer silentConsumer =
-            new OutputStreamProblemConsumer(new ByteArrayOutputStream());
-        config.setProblemConsumer(silentConsumer);
-        config = loadConfiguration(config, PROPERTY_DEFAULTS_PATH);
-        config = loadConfiguration(config, PROPERTY_SETUP_PATH);
-        config = loadLayout(config).properties();
-        String dir = config.getValue(PROPERTY_PLUGIN_PATH);
-        return (dir == null) ? new String[0] : dir.split(PATH_DELIMITER);
-    }
 
     public static void main(String arguments[])
     {
@@ -264,7 +102,7 @@ public class BootLoader
             /* The layout contains all configuration information, and
              * all domains and services of this node.
              */
-            Layout layout = loadLayout(loadConfiguration(problemConsumer));
+            Layout layout = new LayoutBuilder().setProblemConsumer(problemConsumer).build();
 
             /* The BootLoader is not limitted to starting dCache. The
              * behaviour is controlled by a command provided as a
---
+++ modules/dcache/src/main/java/org/dcache/boot/LayoutBuilder.java
@@ -0,0 +1,208 @@
+package org.dcache.boot;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Sets;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Set;
+
+import org.dcache.util.ConfigurationProperties;
+
+import static com.google.common.collect.Iterables.transform;
+import static org.dcache.boot.Properties.*;
+
+public class LayoutBuilder
+{
+    private static final Function<File, String> QUOTE_FILE =
+            new Function<File, String>()
+            {
+                @Override
+                public String apply(File input)
+                {
+                    return '"' + input.getPath() + '"';
+                }
+            };
+
+    private final Set<File> _sourceFiles = Sets.newHashSet();
+    private final Set<File> _sourceDirectories = Sets.newHashSet();
+    private ConfigurationProperties.ProblemConsumer problemConsumer =
+            new SilentProblemConsumer();
+
+    public LayoutBuilder setProblemConsumer(ConfigurationProperties.ProblemConsumer problemConsumer)
+    {
+        this.problemConsumer = problemConsumer;
+        return this;
+    }
+
+    public Layout build() throws IOException, URISyntaxException
+    {
+        return loadLayout(loadConfiguration());
+    }
+
+    private ConfigurationProperties loadSystemProperties()
+            throws UnknownHostException
+    {
+        ConfigurationProperties config =
+                new ConfigurationProperties(System.getProperties());
+        InetAddress localhost = InetAddress.getLocalHost();
+        config.setProperty(PROPERTY_HOST_NAME,
+                localhost.getHostName().split("\\.")[0]);
+        config.setProperty(PROPERTY_HOST_FQDN,
+                localhost.getCanonicalHostName());
+        return config;
+    }
+
+    private ConfigurationProperties loadConfigurationFromPath(
+            ConfigurationProperties defaults, File path)
+            throws IOException
+    {
+        ConfigurationProperties config = new ConfigurationProperties(defaults);
+        if (path.isFile()) {
+            _sourceFiles.add(path);
+            config.loadFile(path);
+        } else if (path.isDirectory()) {
+            _sourceDirectories.add(path);
+            File[] files = path.listFiles();
+            if (files != null) {
+                Arrays.sort(files);
+                for (File file: files) {
+                    if (file.isFile() && file.getName().endsWith(".properties")) {
+                        _sourceFiles.add(file);
+                        config.loadFile(file);
+                    }
+                }
+            }
+        }
+        return config;
+    }
+
+    private ConfigurationProperties loadConfigurationByProperty(
+            ConfigurationProperties defaults, String property)
+            throws IOException
+    {
+        ConfigurationProperties config = defaults;
+        String paths = config.getValue(property);
+        if (paths != null) {
+            for (String path: paths.split(PATH_DELIMITER)) {
+                config = loadConfigurationFromPath(config, new File(path));
+            }
+        }
+        return config;
+    }
+
+    /**
+     * Loads plugins in a plugin directory.
+     *
+     * A plugin directory contains a number of plugins. Each plugin is
+     * stored in a sub-directory containing that one plugin.
+     */
+    private ConfigurationProperties loadPlugins(
+            ConfigurationProperties defaults, File directory)
+            throws IOException
+    {
+        ConfigurationProperties config = defaults;
+        _sourceDirectories.add(directory);
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file: files) {
+                if (file.isDirectory()) {
+                    config = loadConfigurationFromPath(config, file);
+                }
+            }
+        }
+        return config;
+    }
+
+    private ConfigurationProperties loadConfiguration()
+            throws UnknownHostException, IOException, URISyntaxException
+    {
+        /* Configuration properties are loaded from several
+         * sources, starting with importing Java system
+         * properties...
+         */
+        ConfigurationProperties config = loadSystemProperties();
+        config.setProblemConsumer(problemConsumer);
+
+        /* ... and a chain of properties files. */
+        config = loadConfigurationByProperty(config, PROPERTY_DEFAULTS_PATH);
+        for (String dir: getPluginDirs()) {
+            config = loadPlugins(config, new File(dir));
+        }
+        config = loadConfigurationByProperty(config, PROPERTY_SETUP_PATH);
+
+        return config;
+    }
+
+    private Layout loadLayout(ConfigurationProperties config)
+            throws IOException, URISyntaxException
+    {
+        String path = config.getValue(PROPERTY_DCACHE_LAYOUT_URI);
+        if (path == null) {
+            throw new IOException("Undefined property: " + PROPERTY_DCACHE_LAYOUT_URI);
+        }
+        URI uri = new URI(path);
+        Layout layout = new Layout(config);
+        layout.load(uri);
+
+        if (Objects.equals(uri.getScheme(), "file")) {
+            _sourceFiles.add(new File(uri.getPath()));
+        } else {
+            layout.properties().setProperty(PROPERTY_DCACHE_CONFIG_CACHE, "false");
+        }
+        layout.properties().setProperty(PROPERTY_DCACHE_CONFIG_FILES,
+                Joiner.on(" ").join(transform(_sourceFiles, QUOTE_FILE)));
+        layout.properties().setProperty(PROPERTY_DCACHE_CONFIG_DIRS,
+                Joiner.on(" ").join(transform(_sourceDirectories, QUOTE_FILE)));
+        return layout;
+    }
+
+    /**
+     * Returns the top-level plugin directory.
+     *
+     * To allow the plugin directory to be configurable, we first have
+     * to load all the configuration files without the plugins.
+     */
+    private String[] getPluginDirs()
+            throws IOException, URISyntaxException
+    {
+        ConfigurationProperties config = loadSystemProperties();
+        config.setProblemConsumer(new SilentProblemConsumer());
+        config = loadConfigurationByProperty(config, PROPERTY_DEFAULTS_PATH);
+        config = loadConfigurationByProperty(config, PROPERTY_SETUP_PATH);
+        config = loadLayout(config).properties();
+        String dir = config.getValue(PROPERTY_PLUGIN_PATH);
+        return (dir == null) ? new String[0] : dir.split(PATH_DELIMITER);
+    }
+
+    private static class SilentProblemConsumer implements ConfigurationProperties.ProblemConsumer
+    {
+        @Override
+        public void setFilename(String name)
+        {
+        }
+
+        @Override
+        public void setLineNumberReader(LineNumberReader reader)
+        {
+        }
+
+        @Override
+        public void error(String message)
+        {
+        }
+
+        @Override
+        public void warning(String message)
+        {
+        }
+    }
+}
