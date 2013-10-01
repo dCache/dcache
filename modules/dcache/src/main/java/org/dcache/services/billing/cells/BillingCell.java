dCache/dcache

commit 8549a349db94f133b45fca8c1687cbc5ea190aee

billing: Comply with new configuration policy
RELEASE NOTES:
BOOK:

NEW:

billing.cell.name
billing.enable.text=${billing.enable.text.when-disable-is-${billingDisableTxt}}
billing.enable.db=${billing.enable.db.when-toDb-is-${billingToDb}}

billing.service.poolmanager.timeout=${poolConnectTimeout}
billing.service.poolmanager.timeout.unit=MILLISECONDS

billing.text.dir=${billingLogsDir}
billing.text.flat-dir
billing.text.format.mover-info-message=${billing.format.MoverInfoMessage}
billing.text.format.remove-file-info-message=${billing.format.RemoveFileInfoMessage}
billing.text.format.door-request-info-message=${billing.format.DoorRequestInfoMessage}
billing.text.format.storage-info-message=${billing.format.StorageInfoMessage}

billing.db.config.path=${billingInfoAccessPropertiesFile}
billing.db.inserts.max-before-commit=${billingMaxInsertsBeforeCommit}
billing.db.inserts.timeout-before-commit={billingMaxTimeBeforeCommitInSecs}
billing.db.inserts.timeout-before-commit.unit=SECONDS
billing.db.driver=${billingDbDriver}
billing.db.url=${billingDbUrl}
billing.db.host=${billingDbHost}
billing.db.user=${billingDbUser}
billing.db.password=${billingDbPass}
billing.db.password.file=${billingDbPgPassFileName}
billing.db.name=${billingDbName}

billing.db.connections.max-per-partition=30
billing.db.connections.min-per-partition=3
billing.db.schema.auto=${updateBillingDb}
billing.db.schema.changelog=${billing.db.schema.changelog-when-${billing.enable.db}}

DEPRECATED:

billingLogsDir
billingDisableTxt
billingToDb

poolConnectTimeout

billing.format.MoverInfoMessage
billing.format.RemoveFileInfoMessage
billing.format.DoorRequestInfoMessage
billing.format.StorageInfoMessage

updateBillingDb
billingInfoAccessPropertiesFile
billingMaxInsertsBeforeCommit
billingMaxTimeBeforeCommitInSecs
billingDbDriver
billingDbUrl
billingDbHost
billingDbUser
billingDbPass
billingDbPgPassFileName
billingDbName

OBSOLETE:

billingInfoAccess
billingChangelog

TO BE REMOVED (to httpd.properties):

modules/dcache/src/main/java/org/dcache/services/billing/cells/BillingCell.java
@@ -1,5 +1,7 @@
 package org.dcache.services.billing.cells;
 
+import com.google.common.base.CaseFormat;
+
 import com.google.common.base.Function;
 import com.google.common.collect.Maps;
 import com.google.common.io.Files;
@@ -34,6 +36,7 @@ import dmg.util.Args;
 import org.dcache.cells.CellCommandListener;
 import org.dcache.cells.CellMessageReceiver;
 import org.dcache.cells.CellStub;
+import org.dcache.commons.util.Strings;
 
 import static com.google.common.collect.Iterables.toArray;
 import static com.google.common.collect.Iterables.transform;
@@ -75,8 +78,8 @@ public final class BillingCell
     private Map<String,Object> _environment;
     private CellStub _poolManagerStub;
     private File _logsDir;
-    private int _printMode;
-    private boolean _disableTxt;
+    private boolean _enableText;
+    private boolean _flatTextDir;
 
     public BillingCell()
     {
@@ -135,7 +138,7 @@ public final class BillingCell
         }
         _log.info(output);
 
-        if (!_disableTxt) {
+        if (_enableText) {
             String ext = getFilenameExtension(new Date(info.getTimestamp()));
             logInfo(output, ext);
             if (info.getResultCode() != 0) {
@@ -150,13 +153,15 @@ public final class BillingCell
 
         _log.info(output);
 
-        if (!_disableTxt) {
+        if (_enableText) {
             logInfo(output, getFilenameExtension(now));
         }
     }
 
     private String getFormattedMessage(InfoMessage msg) {
-        String property = "billing.format." + msg.getClass().getSimpleName();
+        String type = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_HYPHEN,
+                                                msg.getClass().getSimpleName());
+        String property = "billing.format." + type;
         Object format = _environment.get(property);
         if (format == null) {
             return msg.toString();
@@ -281,7 +286,7 @@ public final class BillingCell
 
     private String getFilenameExtension(Date dateOfEvent)
     {
-        if (_printMode == 0) {
+        if (_flatTextDir) {
             _currentDbFile = _logsDir;
             return _fileNameFormat.format(dateOfEvent);
         } else {
@@ -392,12 +397,12 @@ public final class BillingCell
         _logsDir = dir;
     }
 
-    public void setPrintMode(int printMode) {
-        _printMode = printMode;
+    public void setFlatTextDir(boolean flatTextDir) {
+        _flatTextDir = flatTextDir;
     }
 
     @Required
-    public void setDisableTxt(boolean disableTxt) {
-        _disableTxt = disableTxt;
+    public void setEnableTxt(boolean enableText) {
+        _enableText = enableText;
     }
 }

modules/dcache/src/main/java/org/dcache/services/billing/db/impl/BaseBillingInfoAccess.java
@@ -59,10 +59,12 @@ documents or software obtained from this server.
  */
 package org.dcache.services.billing.db.impl;
 
+import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.dcache.services.billing.db.IBillingInfoAccess;
 import org.dcache.services.billing.db.exceptions.BillingInitializationException;
@@ -78,21 +80,17 @@ public abstract class BaseBillingInfoAccess implements IBillingInfoAccess {
 
     protected static final int DUMMY_VALUE = -1;
 
-    protected static final String MAX_INSERTS_PROPERTY
-        = "dbAccessMaxInsertsBeforeCommit";
-    protected static final String MAX_TIMEOUT_PROPERTY
-        = "dbAccessMaxTimeBeforeCommit";
-
     /**
      * Daemon which periodically flushes to the persistent store.
      */
     protected class TimedCommitter extends Thread {
         @Override
         public void run() {
+            long sleep = maxTimeBeforeCommitUnit.toMillis(maxTimeBeforeCommit);
             while (isRunning()) {
                 try {
                     logger.trace("TimedCommitter thread sleeping");
-                    Thread.sleep(maxTimeBeforeCommit * 1000L);
+                    Thread.sleep(sleep);
                     logger.trace("TimedCommitter thread calling doCommitIfNeeded");
                 } catch (InterruptedException ignored) {
                     logger.trace("TimedCommitter thread sleep interrupted");
@@ -145,6 +143,7 @@ public abstract class BaseBillingInfoAccess implements IBillingInfoAccess {
     protected int insertCount;
     protected int maxInsertsBeforeCommit = 1;
     protected int maxTimeBeforeCommit;
+    protected TimeUnit maxTimeBeforeCommitUnit = TimeUnit.SECONDS;
 
     private Thread flushD;
     private boolean running;
@@ -160,6 +159,7 @@ public abstract class BaseBillingInfoAccess implements IBillingInfoAccess {
 
         logger.trace("maxInsertsBeforeCommit {}", maxInsertsBeforeCommit);
         logger.trace("maxTimeBeforeCommit {}", maxTimeBeforeCommit);
+        logger.trace("maxTimeBeforeCommitUnit {}", maxTimeBeforeCommitUnit);
 
         /*
          * if using delayed commits, run a flush thread
@@ -234,6 +234,11 @@ public abstract class BaseBillingInfoAccess implements IBillingInfoAccess {
 
     public void setMaxTimeBeforeCommit(int maxTimeBeforeCommit) {
         this.maxTimeBeforeCommit = maxTimeBeforeCommit;
+    }
+
+    public void setMaxTimeBeforeCommitUnit(TimeUnit timeUnit) {
+        Preconditions.checkNotNull(timeUnit);
+        this.maxTimeBeforeCommitUnit = timeUnit;
     }
 
     public void setPropertiesPath(String propetiesPath) {

modules/dcache/src/main/resources/org/dcache/services/billing/cells/billing.xml
@@ -18,34 +18,33 @@
   <bean id="poolmanager-stub" class="org.dcache.cells.CellStub">
     <description>Pool manager communication stub</description>
     <property name="destination" value="${dcache.service.poolmanager}"/>
-    <property name="timeout" value="${poolConnectTimeout}"/>
+    <property name="timeout" value="${billing.service.poolmanager.timeout}"/>
+    <property name="timeoutUnit" value="${billing.service.poolmanager.timeout.unit}"/>
   </bean>
 
   <bean id="billingcell"
     class="org.dcache.services.billing.cells.BillingCell">
-    <property name="logsDir" value="${billingLogsDir}"/>
-    <property name="disableTxt" value="${billingDisableTxt}"/>
-    <property name="printMode" value="${printMode}"/>
+    <property name="logsDir" value="${billing.text.dir}"/>
+    <property name="enableTxt" value="${billing.enable.text}"/>
+    <property name="flatTextDir" value="${billing.text.flat-dir}"/>
     <property name="poolManagerStub" ref="poolmanager-stub"/>
   </bean>
 
   <!-- Set of beans instantiated if database is enabled -->
-  <beans profile="db-yes">
-    <bean id="jdbc-billing-info-access" class="${billingInfoAccess}"
+  <beans profile="db-true">
+    <bean id="jdbc-billing-info-access" class="org.dcache.services.billing.db.impl.datanucleus.DataNucleusBillingInfo"
           init-method="initialize" destroy-method="close">
-      <property name="jdbcUrl" value="${db.url}"/>
-      <property name="jdbcDriver" value="${db.driver}"/>
-      <property name="jdbcUser" value="${db.user}"/>
-      <property name="jdbcPassword"
-        value="#{ T(diskCacheV111.util.Pgpass).getPassword('${db.password.file}', '${db.url}', '${db.user}', '${db.password}') }"/>
-      <property name="maxInsertsBeforeCommit"
-        value="${billingMaxInsertsBeforeCommit}"/>
-      <property name="maxTimeBeforeCommit"
-        value="${billingMaxTimeBeforeCommitInSecs}"/>
-      <property name="propertiesPath" value="${billingInfoAccessPropertiesFile}"/>
-      <property name="partitionCount" value="${db.connections.partition-count}"/>
-      <property name="maxConnectionsPerPartition" value="${db.connections.max-per-partition}"/>
-      <property name="minConnectionsPerPartition" value="${db.connections.min-per-partition}"/>
+      <property name="jdbcUrl" value="${billing.db.url}"/>
+      <property name="jdbcDriver" value="${billing.db.driver}"/>
+      <property name="jdbcUser" value="${billing.db.user}"/>
+      <property name="jdbcPassword" value="#{ T(diskCacheV111.util.Pgpass).getPassword('${billing.db.password.file}', '${billing.db.url}', '${billing.db.user}', '${billing.db.password}') }"/>
+      <property name="maxInsertsBeforeCommit" value="${billing.db.inserts.max-before-commit}"/>
+      <property name="maxTimeBeforeCommit" value="${billing.db.inserts.timeout-before-commit}"/>
+      <property name="maxTimeBeforeCommitUnit" value="${billing.db.inserts.timeout-before-commit.unit}"/>
+      <property name="propertiesPath" value="${billing.db.config.path}"/>
+      <property name="partitionCount" value="${billing.db.connections.partition-count}"/>
+      <property name="maxConnectionsPerPartition" value="${billing.db.connections.max-per-partition}"/>
+      <property name="minConnectionsPerPartition" value="${billing.db.connections.min-per-partition}"/>
     </bean>
 
     <bean id="histogram-service" class="org.dcache.services.billing.histograms.data.JDOTimeFrameHistogramDataService">
@@ -64,15 +63,15 @@
       <description>Database schema manager</description>
       <property name="dataSource">
         <bean class="org.springframework.jdbc.datasource.DriverManagerDataSource">
-          <property name="driverClassName" value="${db.driver}"/>
-          <property name="url" value="${db.url}"/>
-          <property name="username" value="${db.user}"/>
+          <property name="driverClassName" value="${billing.db.driver}"/>
+          <property name="url" value="${billing.db.url}"/>
+          <property name="username" value="${billing.db.user}"/>
           <property name="password"
-                    value="#{ T(diskCacheV111.util.Pgpass).getPassword('${db.password.file}', '${db.url}', '${db.user}', '${db.password}') }"/>
+                    value="#{ T(diskCacheV111.util.Pgpass).getPassword('${billing.db.password.file}', '${billing.db.url}', '${billing.db.user}', '${billing.db.password}') }"/>
         </bean>
       </property>
-      <property name="changeLog" value="classpath:${db.schema.changelog}"/>
-      <property name="shouldUpdate" value="${db.schema.auto}"/>
+      <property name="changeLog" value="classpath:${billing.db.schema.changelog}"/>
+      <property name="shouldUpdate" value="${billing.db.schema.auto}"/>
     </bean>
   </beans>
 </beans>

skel/share/defaults/billing.properties
@@ -9,13 +9,15 @@
 
 #  ---- Cell name of billing service
 #
-billing/cell.name=billing
+billing.cell.name=billing
+billing/cell.name=${billing.cell.name}
 
 #  ---- Directory for billing logs
 #
 #   The directory within which the billing logs are to be written.
 #
-billingLogsDir=${dcache.paths.billing}
+(deprecated)billingLogsDir=${dcache.paths.billing}
+billing.text.dir=${billingLogsDir}
 
 #  ---- Disable billing to plain text file
 #
@@ -23,7 +25,16 @@ billingLogsDir=${dcache.paths.billing}
 #   the property is set to 'false' then activity is logged in text files.
 #   If set to 'true' then there is no logging to text files.
 #
-(one-of?true|false)billingDisableTxt=false
+(deprecated,one-of?true|false)billingDisableTxt=false
+(immutable)billing.enable.text.when-disable-is-true=false
+(immutable)billing.enable.text.when-disable-is-false=true
+billing.enable.text=${billing.enable.text.when-disable-is-${billingDisableTxt}}
+
+#
+#  ---- whether logs are stored in flattened directory; default:
+#       they are stored in year/month subdirectory
+#
+(one-of?true|false)billing.text.flat-dir=false
 
 #  -----------------------------------------------------------------------
 #     Format of billing entries in plain text billing files
@@ -196,25 +207,34 @@ billingLogsDir=${dcache.paths.billing}
 #           Please overwrite the default if you wish to be able to distinguish
 #           pool-to-pool transfers from door-initiated uploads or downloads.
 #
-billing.format.MoverInfoMessage=$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$storage.storageClass$@$storage.hsm$$else$<Unknown>$endif$ $transferred$ $connectionTime$ $created$ {$protocol$} [$initiator$] {$rc$:"$message$"}
+(deprecated)billing.format.MoverInfoMessage=$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$storage.storageClass$@$storage.hsm$$else$<Unknown>$endif$ $transferred$ $connectionTime$ $created$ {$protocol$} [$initiator$] {$rc$:"$message$"}
+billing.text.format.mover-info-message=${billing.format.MoverInfoMessage}
 
 #  ---- RemoveFileInfoMessage
 #
 #    Submitted by PnfsManager on file removal.
 #
-billing.format.RemoveFileInfoMessage=$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$storage.storageClass$@$storage.hsm$$else$<Unknown>$endif$ {$rc$:"$message$"}
+(deprecated)billing.format.RemoveFileInfoMessage=$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$storage.storageClass$@$storage.hsm$$else$<Unknown>$endif$ {$rc$:"$message$"}
+billing.text.format.remove-file-info-message=${billing.format.RemoveFileInfoMessage}
 
 #  ---- DoorRequestInfoMessage
 #
 #    Submitted by doors for each file transfer.
 #
-billing.format.DoorRequestInfoMessage=$date$ [$cellType$:$cellName$:$type$] ["$owner$":$uid$:$gid$:$client$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$storage.storageClass$@$storage.hsm$$else$<Unknown>$endif$ $transactionTime$ $queuingTime$ {$rc$:"$message$"}
+(deprecated)billing.format.DoorRequestInfoMessage=$date$ [$cellType$:$cellName$:$type$] ["$owner$":$uid$:$gid$:$client$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$storage.storageClass$@$storage.hsm$$else$<Unknown>$endif$ $transactionTime$ $queuingTime$ {$rc$:"$message$"}
+billing.text.format.door-request-info-message=${billing.format.DoorRequestInfoMessage}
 
 #  ---- StorageInfoMessage
 #
 #    Submitted by pools for each flush to and fetch from tape.
 #
-billing.format.StorageInfoMessage=$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$storage.storageClass$@$storage.hsm$$else$<Unknown>$endif$ $transferTime$ $queuingTime$ {$rc$:"$message$"}
+(deprecated)billing.format.StorageInfoMessage=$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$storage.storageClass$@$storage.hsm$$else$<Unknown>$endif$ $transferTime$ $queuingTime$ {$rc$:"$message$"}
+billing.text.format.storage-info-message=${billing.format.StorageInfoMessage}
+
+# ---- communcation timeout
+(deprecated)poolConnectTimeout=3600000
+billing.service.poolmanager.timeout=${poolConnectTimeout}
+(one-of?MILLISECONDS|SECONDS|MINUTES|HOURS|DAYS)billing.service.poolmanager.timeout.unit=MILLISECONDS
 
 #  -----------------------------------------------------------------------
 #     Store billing data in database
@@ -235,89 +255,70 @@ billing.format.StorageInfoMessage=$date$ [$cellType$:$cellName$:$type$] [$pnfsid
 #       createdb -O srmdcache -U postgres billing
 #       createlang -U srmdcache plpgsql billing
 #
-(one-of?yes|no)billingToDb=no
+(deprecated,one-of?yes|no)billingToDb=no
+(immutable)billing.enable.db.when-toDb-is-yes=true
+(immutable)billing.enable.db.when-toDb-is-no=false
+(one-of?true|false|${billing.enable.db.when-toDb-is-${billingToDb}})billing.enable.db=${billing.enable.db.when-toDb-is-${billingToDb}}
 
 # ---- Use DAO access layer to persistence
 #
-billingInfoAccess=org.dcache.services.billing.db.impl.datanucleus.DataNucleusBillingInfo
+(obsolete)billingInfoAccess=set internally
 
 # ---- If this is set, it overrides the jar-resident configuration resource
 #      for the DAO implementation, if any
-billingInfoAccessPropertiesFile=
+(deprecated)billingInfoAccessPropertiesFile=
+billing.db.config.path=${billingInfoAccessPropertiesFile}
 
 # ---- Commit optimizations: in-memory caching thresholds
 #
-billingMaxInsertsBeforeCommit=10000
-billingMaxTimeBeforeCommitInSecs=5
+(deprecated)billingMaxInsertsBeforeCommit=10000
+billing.db.inserts.max-before-commit=${billingMaxInsertsBeforeCommit}
+(deprecated)billingMaxTimeBeforeCommitInSecs=5
+billing.db.inserts.timeout-before-commit=${billingMaxTimeBeforeCommitInSecs}
+(one-of?MILLISECONDS|SECONDS|MINUTES)billing.db.inserts.timeout-before-commit.unit=SECONDS
 
 # ---- liquibase changelog
-billingChangelog=org/dcache/services/billing/db/sql/billing.changelog-master.xml
+(obsolete)billingChangelog=
 
 # ---- liquibase update
-(one-of?true|false)updateBillingDb=true
-
-# ---- communcation timeout
-poolConnectTimeout=3600000
+(deprecated)updateBillingDb=true
+(one-of?true|false|${updateBillingDb})billing.db.schema.auto=${updateBillingDb}
 
 # ---- RDBMS/JDBC Driver
 #
-billingDbDriver=org.postgresql.Driver
+(deprecated)billingDbDriver=org.postgresql.Driver
+billing.db.driver=${billingDbDriver}
 
 # ---- RDBMS/JDBC URL
 #
-billingDbUrl=jdbc:postgresql://${billingDbHost}/${billingDbName}
+(deprecated)billingDbUrl=jdbc:postgresql://${billing.db.host}/${billing.db.name}
+billing.db.url=${billingDbUrl}
 
 # ---- RDBMS/JDBC Database host name
 #
-billingDbHost=localhost
+(deprecated)billingDbHost=localhost
+billing.db.host=${billingDbHost}
 
 # ---- RDBMS/JDBC Database user name
 #
-billingDbUser=srmdcache
+(deprecated)billingDbUser=srmdcache
+billing.db.user=${billingDbUser}
 
 # ---- RDBMS/JDBC Database user password
 #
-billingDbPass=srmdcache
+(deprecated)billingDbPass=
+billing.db.password=${billingDbPass}
 
 #  The following enables using pgfile, which is disabled by default
 #  billingDbPgPassFileName=/root/.pgpass
 #
-billingDbPgPassFileName=
+(deprecated)billingDbPgPassFileName=
+billing.db.password.file=${billingDbPgPassFileName}
 
 # ---- Database name
 #
-billingDbName=billing
-
-# ---- Whether to generate histograms or not (only if billingToDb=yes)
-#
-(one-of?true|false)generatePlots=false
-
-# ---- Type of image file to produce (JAIDA property)
-#
-billingPlotsExportType=png
-
-#   -- refresh the plots after this many minutes
-#
-webadmin.billing-plot.refresh-threshold=5
-
-#  ---- Height of the plot image in pixels
-#
-webadmin.billing-plot.height=800
-
-#  ---- Width of the plot image in pixels
-#
-webadmin.billing-plot.width=1200
-
-#   -- the default style for the plots
-#      connected draws a line between each data point
-#      outline draws the first histogram as filled, the others as empty
-#      filled draws all histograms as filled, and overlapping
-#
-(one-of?CONNECTED|OUTLINE|FILLED)webadmin.billing-plot.default-style=CONNECTED
-
-#   -- the default scale for the plots (lin = linear, log = logarithmic)
-#
-(one-of?lin|log)webadmin.billing-plot.default-scale=log
+(deprecated)billingDbName=billing
+billing.db.name=${billingDbName}
 
 #  ---- Configuration for database connection pool
 #
@@ -354,7 +355,8 @@ webadmin.billing-plot.width=1200
 #  Note that DataNucleus currently prevents this value from being set.
 #  The default value is recorded here
 #
-(immutable)billing/db.connections.partition-count = 1
+(immutable)billing.db.connections.partition-count=1
+(immutable)billing/db.connections.partition-count=${billing.db.connections.partition-count}
 
 #
 #  The maximum number of connections per partition.
@@ -363,41 +365,89 @@ webadmin.billing-plot.width=1200
 #  billing is the product of db.connections.parition-count and
 #  db.connections.paritions.max-per-partition.
 #
-billing/db.connections.max-per-partition = 30
+billing.db.connections.max-per-partition=30
+billing/db.connections.max-per-partition=${billing.db.connections.max-per-partition}
 
 #
 #  The minimum number of connections per partition.  Billing will
 #  have at least the product of db.connections.partition-count and
 #  db.connections.paritions.min-per-partition.
 #
-billing/db.connections.min-per-partition = 3
+billing.db.connections.min-per-partition=3
+billing/db.connections.min-per-partition=${billing.db.connections.min-per-partition}
 
 #
 #  To enable conditional presence of database for the dcache database commands
 #
-(immutable)billing-db-name-when-yes=${billingDbName}
-(immutable)billing-db-name-when-no=
-(immutable)billing-db-scheme-changelog-when-yes=${billingChangelog}
-(immutable)billing-db-scheme-changelog-when-no=
+
+(immutable)billing.db.name-when-true=${billing.db.name}
+(immutable)billing.db.name-when-false=
+(immutable)billing.db.schema.changelog-when-true=org/dcache/services/billing/db/sql/billing.changelog-master.xml
+(immutable)billing.db.schema.changelog-when-false=
 
 #
 # Database related settings reserved for internal use.
 #
-(immutable)billing/db.name=${billing-db-name-when-${billingToDb}}
-billing/db.user=${billingDbUser}
-(immutable)billing/db.host=${billingDbHost}
-billing/db.password=${billingDbPass}
-billing/db.password.file=${billingDbPgPassFileName}
-billing/db.driver=${billingDbDriver}
-billing/db.url=${billingDbUrl}
-billing/db.schema.auto=${updateBillingDb}
-billing/db.schema.changelog=${billing-db-scheme-changelog-when-${billingToDb}}
+(not-for-services)billing.db.name-resolved=${billing.db.name-when-${billing.enable.db}}
+#
+# Note: when db is not enabled, the name needs to be empty, or else
+#       the dcache database command will attempt to connect
+#       Hence this cannot be equal to billing.db.name in this case.
+#
+billing/db.name=${billing.db.name-resolved}
+billing/db.user=${billing.db.user}
+billing/db.host=${billing.db.host}
+billing/db.password=${billing.db.password}
+billing/db.password.file=${billing.db.password.file}
+billing/db.driver=${billing.db.driver}
+billing/db.url=${billing.db.url}
+billing/db.schema.auto=${billing.db.schema.auto}
+billing.db.schema.changelog=${billing.db.schema.changelog-when-${billing.enable.db}}
+billing/db.schema.changelog=${billing.db.schema.changelog}
 
 #
 #  The following properties are Obsolete or Forbidden.
 #
-(forbidden)billingDb=use billingLogsDir instead
-(obsolete)billingDbCommitRows=use billingMaxInsertsBeforeCommit instead
-(obsolete)billingDbCommitIntervalInMilliseconds=use billingMaxTimeBeforeCommitInSecs instead
+(forbidden)billingDb=use billing.text.dir instead
+(obsolete)billingDbCommitRows=use billing.db.inserts.max-before-commit instead
+(obsolete)billingDbCommitIntervalInMilliseconds=use billing.db.inserts.timeout-before-commit instead
 (obsolete)billingPlotPropertiesFile=no longer supported
 (forbidden)poolManager=Use poolmanager instead
+
+#
+#  --------------------------------------------------------------------------
+#      THESE PROPERTIES WILL BE REMOVED AND REDEFINED IN httpd.properties
+#      BY A SUBSEQUENT PATCH
+#  --------------------------------------------------------------------------
+#
+
+# ---- Whether to generate histograms or not (only if billingToDb=yes)
+#
+(one-of?true|false)generatePlots=false
+
+# ---- Type of image file to produce (JAIDA property)
+#
+billingPlotsExportType=png
+
+#   -- refresh the plots after this many minutes
+#
+webadmin.billing-plot.refresh-threshold=5
+
+#  ---- Height of the plot image in pixels
+#
+webadmin.billing-plot.height=800
+
+#  ---- Width of the plot image in pixels
+#
+webadmin.billing-plot.width=1200
+
+#   -- the default style for the plots
+#      connected draws a line between each data point
+#      outline draws the first histogram as filled, the others as empty
+#      filled draws all histograms as filled, and overlapping
+#
+(one-of?CONNECTED|OUTLINE|FILLED)webadmin.billing-plot.default-style=CONNECTED
+
+#   -- the default scale for the plots (lin = linear, log = logarithmic)
+#
+(one-of?lin|log)webadmin.billing-plot.default-scale=log

skel/share/services/billing.batch
@@ -2,39 +2,44 @@
 
 onerror shutdown
 
-check -strong cell.name
+check -strong billing.cell.name
 check -strong billingLogsDir
-check billingDisableTxt
-check -strong billingToDb
+check -strong billing.enable.text
+check -strong billing.enable.db
 check -strong dcache.service.poolmanager
-check poolConnectTimeout
+check -strong billing.service.poolmanager.timeout
+check -strong billing.text.dir
+check billing.text.flat-dir
 
-# Additional params, if 'billingToDb' is 'yes'
+# Additional params, if 'billing.enable.db' is true
 define env billingToDb.exe endExe
   onerror shutdown
 
-  check -strong db.url
-  check -strong db.driver
-  check -strong db.user
-  check db.password
-  check db.password.file
-  check -strong db.schema.changelog
-  check -strong db.schema.auto
-
-  check -strong billingInfoAccess
-  check billingInfoAccessPropertiesFile
-  check billingMaxInsertsBeforeCommit
-  check billingMaxTimeBeforeCommitInSecs
+  check -strong billing.db.url
+  check -strong billing.db.driver
+  check -strong billing.db.host
+  check -strong billing.db.user
+  check -strong billing.db.schema.changelog
+  check -strong billing.db.schema.auto
+  check billing.db.name
+  check billing.db.password
+  check billing.db.password.file
+  check billing.db.config.path
+  check billing.db.inserts.max-before-commit
+  check billing.db.inserts.timeout-before-commit
+  check billing.db.connections.max-per-partition
+  check billing.db.connections.min-per-partition
+  check billing.db.schema.auto
+  check billing.db.schema.changelog
 endExe
 
 onerror continue
-eval ${billingToDb} yes ==
+eval ${billing.enable.db} true ==
 exec env billingToDb.exe -ifok
 onerror shutdown
 
 create org.dcache.cells.UniversalSpringCell ${cell.name} \
         "classpath:org/dcache/services/billing/cells/billing.xml \
-        -profiles=db-${billingToDb} \
+        -profiles=db-${billing.enable.db} \
         -export \
-        -billingCellName=${cell.name}.alias \
-        -printMode=2"
+        -billingCellName=${cell.name}.alias"

commit 9bbb1822454d14cab0b64f2d3195d939b0ba2351

billing: fix invalid property handling
fix regression introduced by 8549a34.
The changed property required code update.

Acked-by: Albert Rossi
Target: master, 2.7
Require-book: no
Require-notes: no

modules/dcache/src/main/java/org/dcache/services/billing/cells/BillingCell.java
@@ -161,7 +161,7 @@ public final class BillingCell
     private String getFormattedMessage(InfoMessage msg) {
         String type = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_HYPHEN,
                                                 msg.getClass().getSimpleName());
-        String property = "billing.format." + type;
+        String property = "billing.text.format." + type;
         Object format = _environment.get(property);
         if (format == null) {
             return msg.toString();