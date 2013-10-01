dCache/dcache

commit 8b7c60e6fe9f534a38e9575bbf955da24259b384

httpd: fix access to context based pages
The contextHandler was bound to last requested page and
was rejecting access to other values. The internal logic
is change to bound only for 'named' contexts.

RELEASE NOTES:
fixed access to active transfers html and txt pages

Ticket: #7975
Acked-by: Albert Rossi
Target: master, 2.7, 2.6
Require-book: no
Require-notes: yes

modules/dcache/src/main/java/org/dcache/services/httpd/handlers/ContextHandler.java
@@ -26,7 +26,7 @@ import org.dcache.services.httpd.util.StandardHttpRequest;
  */
 public class ContextHandler extends AbstractHandler {
 
-    private String specificName;
+    private final String specificName;
     private final Map<String, Object> context;
 
     public ContextHandler(String specificName, Map<String, Object> context) {
@@ -43,25 +43,23 @@ public class ContextHandler extends AbstractHandler {
             final HttpRequest proxy = new StandardHttpRequest(request, response);
             String html;
             final String[] tokens = proxy.getRequestTokens();
-            if ((tokens.length > 1) && (tokens[1].equals("index.html"))) {
+            if ((tokens.length < 2) || tokens[1].equals("index.html")) {
                 html = createContextDirectory();
             } else {
-                if (tokens.length > 1) {
-                    final String contextName = tokens[1];
-                    if (!contextName.startsWith(specificName)) {
-                        throw new HttpException(HttpServletResponse.SC_FORBIDDEN,
+                if(!specificName.equals("*") && !specificName.equals(tokens[1])) {
+                    throw new HttpException(HttpServletResponse.SC_FORBIDDEN,
                                                 "Forbidden");
-                    }
-                    specificName = contextName;
                 }
-                html = (String) context.get(specificName);
+                Object value = context.get(tokens[1]);
+                if (value == null) {
+                    throw new OnErrorException();
+                }
+                html = String.valueOf(value);
             }
-            if (html == null) {
-                throw new OnErrorException();
-            } else {
-                proxy.getPrintWriter().println(html);
-                proxy.getPrintWriter().flush();
-            }
+
+            proxy.getPrintWriter().println(html);
+            proxy.getPrintWriter().flush();
+
         } catch (final Exception t) {
             throw new ServletException("ContextHandler", t);
         }

modules/dcache/src/main/java/org/dcache/services/httpd/util/AliasEntry.java
@@ -116,10 +116,6 @@ public class AliasEntry {
                                 + specific + ")";
                 break;
             case CONTEXT:
-                int pos = specific.indexOf("*");
-                if (pos > -1) {
-                    specific = specific.substring(0, pos);
-                }
                 handler = new ContextHandler(specific, cell.getDomainContext());
                 entry = new AliasEntry(alias, aliasType, handler, specific);
                 entry.setOnError(args.getOpt("onError"));

commit 82bd3fd12aec0a3c9c39c8fb8a68a9d9d5b0b82b

http: fix context based pages
regression introduced by 8b7c60e

Acked-by: Albert Rossi
Ticket: #8024
Target: master
Require-book: no
Require-notes: no

modules/dcache/src/main/java/org/dcache/services/httpd/handlers/ContextHandler.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import java.util.SortedMap;
 
 import dmg.util.CollectionFactory;
-import dmg.util.HttpException;
 import dmg.util.HttpRequest;
 
 import org.dcache.services.httpd.exceptions.OnErrorException;
@@ -41,21 +40,24 @@ public class ContextHandler extends AbstractHandler {
 
         try {
             final HttpRequest proxy = new StandardHttpRequest(request, response);
-            String html;
-            final String[] tokens = proxy.getRequestTokens();
-            if ((tokens.length < 2) || tokens[1].equals("index.html")) {
-                html = createContextDirectory();
+
+            Object value;
+            if (specificName.equals("*")) {
+                final String[] tokens = proxy.getRequestTokens();
+                if (tokens.length < 2 || tokens[1].equals("index.html")) {
+                    value = createContextDirectory();
+                } else {
+                    value = context.get(tokens[1]);
+                }
+
             } else {
-                if(!specificName.equals("*") && !specificName.equals(tokens[1])) {
-                    throw new HttpException(HttpServletResponse.SC_FORBIDDEN,
-                                                "Forbidden");
-                }
-                Object value = context.get(tokens[1]);
-                if (value == null) {
-                    throw new OnErrorException();
-                }
-                html = String.valueOf(value);
+                 value = context.get(specificName);
             }
+
+            if (value == null) {
+                throw new OnErrorException();
+            }
+            String html = String.valueOf(value);
 
             proxy.getPrintWriter().println(html);
             proxy.getPrintWriter().flush();