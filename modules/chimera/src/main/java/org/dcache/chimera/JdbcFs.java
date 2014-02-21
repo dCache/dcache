dCache/dcache

commit 119ecfe92c1a69925ad31934f7517421e3f74405

chimera: fix rename into a file
fix confitions when file is remove into an existing file
as a parent.

Ticket: #8250
Acked-by: Paul Millar
Target: master, 2.8, 2.7, 2.6
Require-book: no
Require-notes: yes

modules/chimera/src/main/java/org/dcache/chimera/JdbcFs.java
@@ -1329,34 +1329,7 @@ public class JdbcFs implements FileSystemProvider {
 
     @Override
     public void setFileName(FsInode dir, String oldName, String newName) throws ChimeraFsException {
-
-        checkNameLength(newName);
-
-        Connection dbConnection;
-        try {
-            // get from pool
-            dbConnection = _dbConnectionsPool.getConnection();
-        } catch (SQLException e) {
-            throw new BackEndErrorHimeraFsException(e.getMessage());
-        }
-
-        try {
-            // read/write only
-            dbConnection.setAutoCommit(false);
-
-            _sqlDriver.setFileName(dbConnection, dir, oldName, newName);
-            dbConnection.commit();
-        } catch (Exception e) {
-            _log.error("setFileName", e);
-            try {
-                dbConnection.rollback();
-            } catch (SQLException e1) {
-                _log.error("setFileName rollback", e1);
-            }
-            throw new IOHimeraFsException(e.getMessage());
-        } finally {
-            tryToClose(dbConnection);
-        }
+	move(dir, oldName, dir, newName);
     }
 
     @Override
@@ -1787,11 +1760,45 @@ public class JdbcFs implements FileSystemProvider {
             // read/write only
             dbConnection.setAutoCommit(false);
 
+            Stat destStat = _sqlDriver.stat(dbConnection, destDir);
+            if ((destStat.getMode() & UnixPermission.S_IFDIR) == 0) {
+                throw new NotDirChimeraException();
+            }
+
+            FsInode destInode = _sqlDriver.inodeOf(dbConnection, destDir, dest);
+            FsInode srcInode = _sqlDriver.inodeOf(dbConnection, srcDir, source);
+            if (srcInode == null) {
+                throw new FileNotFoundHimeraFsException();
+            }
+
+            if (destInode != null) {
+                Stat statDest = _sqlDriver.stat(dbConnection, destInode);
+                Stat statSrc = _sqlDriver.stat(dbConnection, srcInode);
+                if (destInode.equals(srcInode)) {
+                   // according to POSIX, we are done
+                   return true;
+                }
+
+               /*
+                * renaming only into existing same type is allowed
+                */
+                if ((statSrc.getMode() & UnixPermission.S_TYPE) != (statDest.getMode() & UnixPermission.S_TYPE)) {
+                    throw new FileExistsChimeraFsException();
+                }
+
+                _sqlDriver.remove(dbConnection, destDir, dest);
+            }
+
             if (!srcDir.equals(destDir)) {
                 _sqlDriver.move(dbConnection, srcDir, source, destDir, dest);
+                _sqlDriver.incNlink(dbConnection, destDir);
+                _sqlDriver.decNlink(dbConnection, srcDir);
             } else {
                 // same directory
-                _sqlDriver.setFileName(dbConnection, srcDir, source, dest);
+		long now = System.currentTimeMillis();
+		_sqlDriver.setFileName(dbConnection, srcDir, source, dest);
+		_sqlDriver.setFileMTime(dbConnection, destDir, 0, now);
+		_sqlDriver.setFileCTime(dbConnection, destDir, 0, now);
             }
 
             dbConnection.commit();

modules/chimera/src/main/java/org/dcache/chimera/FsSqlDriver.java
@@ -463,21 +463,7 @@ class FsSqlDriver {
 
         try {
 
-            FsInode destInode = inodeOf(dbConnection, destDir, dest);
             FsInode srcInode = inodeOf(dbConnection, srcDir, source);
-
-            if (destInode != null) {
-
-                if (destInode.equals(srcInode)) {
-                    // according to POSIX, we are done
-                    return;
-                }
-
-                // remove old entry if exist
-                remove(dbConnection, destDir, dest);
-            }
-            incNlink(dbConnection, destDir);
-
             stMove = dbConnection.prepareStatement(sqlMove);
 
             stMove.setString(1, destDir.toString());
@@ -496,8 +482,6 @@ class FsSqlDriver {
                 stParentMove.setString(2, srcInode.toString());
                 stParentMove.executeUpdate();
             }
-
-            decNlink(dbConnection, srcDir);
 
         } finally {
             SqlHelper.tryToClose(stMove);
@@ -1048,29 +1032,12 @@ class FsSqlDriver {
 
         try {
 
-            FsInode destInode = inodeOf(dbConnection, dir, newName);
-            FsInode srcInode = inodeOf(dbConnection, dir, oldName);
-
-            if (destInode != null) {
-
-                if (destInode.equals(srcInode)) {
-                    // according to POSIX, we are done
-                    return;
-                }
-
-                // remove old entry if exist
-                remove(dbConnection, dir, newName);
-            }
-
             ps = dbConnection.prepareStatement(sqlSetFileName);
 
             ps.setString(1, newName);
             ps.setString(2, oldName);
             ps.setString(3, dir.toString());
             ps.executeUpdate();
-
-            // update parent modification time
-            setFileMTime(dbConnection, dir, 0, System.currentTimeMillis());
 
         } finally {
             SqlHelper.tryToClose(ps);

modules/chimera/src/test/java/org/dcache/chimera/BasicTest.java
@@ -845,6 +845,36 @@ public class BasicTest extends ChimeraTestCaseHelper {
         assertEquals("Invalid parent", dir13, newInode.inodeOf(".."));
     }
 
+    @Test(expected = NotDirChimeraException.class)
+    public void testMoveIntoFile() throws Exception {
+
+	FsInode src = _rootInode.create("testMoveIntoFile1", 0, 0, 0644);
+	FsInode dest = _rootInode.create("testMoveIntoFile2", 0, 0, 0644);
+	_fs.move(_rootInode, "testMoveIntoFile1", dest, "testMoveIntoFile3");
+    }
+
+    @Test(expected = FileExistsChimeraFsException.class)
+    public void testMoveIntoDir() throws Exception {
+
+	FsInode src = _rootInode.create("testMoveIntoDir", 0, 0, 0644);
+	FsInode dir = _rootInode.mkdir("dir", 0, 0, 0755);
+	_fs.move(_rootInode, "testMoveIntoDir", _rootInode, "dir");
+    }
+
+    @Test(expected = FileNotFoundHimeraFsException.class)
+    public void testMoveNotExists() throws Exception {
+        _fs.move(_rootInode, "foo", _rootInode, "bar");
+    }
+
+    @Test(expected = DirNotEmptyHimeraFsException.class)
+    public void testMoveNotEmptyDir() throws Exception {
+
+	FsInode dir1 = _rootInode.mkdir("dir1", 0, 0, 0755);
+	FsInode dir2 = _rootInode.mkdir("dir2", 0, 0, 0755);
+	FsInode src = dir2.create("testMoveIntoDir", 0, 0, 0644);
+	_fs.move(_rootInode, "dir1", _rootInode, "dir2");
+    }
+
     @Test
     public void testMoveExistingWithLevel() throws Exception {
 

commit 1287a28a057c233b9e9c7d93876e4593d50343f4

chimera: fix transaction leak
introduced in 119ecfe92c1a69925ad31934f7517421e3f74405

Acked-by: Paul Millar
Target: master
Require-book: no
Require-notes: no

modules/chimera/src/main/java/org/dcache/chimera/JdbcFs.java
@@ -1776,6 +1776,7 @@ public class JdbcFs implements FileSystemProvider {
                 Stat statSrc = _sqlDriver.stat(dbConnection, srcInode);
                 if (destInode.equals(srcInode)) {
                    // according to POSIX, we are done
+                   dbConnection.commit();
                    return true;
                 }
 