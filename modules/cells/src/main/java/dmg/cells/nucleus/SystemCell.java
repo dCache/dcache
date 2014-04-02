dCache/dcache

commit 6250598677f3a3edccabfed9a28368b6d02db764

cells: move command support into commons-cli and create skeleton CLI client
This patch moves the classes needed to support a CLI from cells into
commons-cli.

The Interpretable interface is removed as it had no effect.

The CommandRequestable interface is removed.  Its purpose was to allow
arbitrary arguments; however, it was used for a single command (which
was never used in dCache) and the same effect is achievable via
escaping.

The chimera shell is split into two parts: the generic CLI handling part
is now the ShellApplication class in commons-cli, from which the
Chimera shell class now inherits.

Target: master
Requires-notes: no
Requires-book: no
Patch: http://rb.dcache.org/r/6552
Acked-by: Gerd Behrmann

modules/cells/src/main/java/dmg/cells/nucleus/SystemCell.java
@@ -11,8 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dmg.util.AuthorizedString;
-import dmg.util.CommandException;
-import dmg.util.CommandRequestable;
 import dmg.util.DomainInterruptHandler;
 import dmg.util.Gate;
 import dmg.util.logback.FilterShell;
@@ -276,15 +274,6 @@ public class      SystemCell
            }
            _log.info( "Command(p="+as.getAuthorizedPrincipal()+") : "+command ) ;
            reply = _cellShell.objectCommand2( command ) ;
-           processed = true;
-        }else if( obj instanceof CommandRequestable ){
-           CommandRequestable request = (CommandRequestable)obj ;
-           try{
-               _log.info( "Command : "+request.getRequestCommand() ) ;
-              reply = _cellShell.command( request ) ;
-           }catch( CommandException cee ){
-              reply = cee ;
-           }
            processed = true;
         }
 

modules/cells/pom.xml
@@ -32,6 +32,11 @@
         <version>${project.version}</version>
     </dependency>
     <dependency>
+        <groupId>org.dcache</groupId>
+        <artifactId>common-cli</artifactId>
+        <version>${project.version}</version>
+    </dependency>
+    <dependency>
         <groupId>com.google.code.findbugs</groupId>
         <artifactId>annotations</artifactId>
     </dependency>

modules/cells/src/main/java/dmg/cells/applets/spy/ContextPanel.java
@@ -20,6 +20,7 @@ import java.awt.event.ItemListener;
 import dmg.cells.network.CellDomainNode;
 import dmg.cells.services.MessageObjectFrame;
 
+import org.dcache.util.Args;
 
 
 class ContextPanel
@@ -171,12 +172,9 @@ class ContextPanel
               return;
           }
 
-          SpyCommandRequest req =
-            new SpyCommandRequest( "set context" ,
-                                   _contextName ,
-                                   _contextText.getText() ) ;
-          _connection.send( _domainNode.getAddress() , req , this ) ;
-
+          String command = "set context " + _contextName + " " +
+                  Args.quote(_contextText.getText());
+          _connection.send(_domainNode.getAddress(), command, this);
        }
    }
    @Override

modules/cells/src/main/java/dmg/cells/applets/spy/SpyCommandRequest.java
@@ -1,24 +1,0 @@
-package dmg.cells.applets.spy ;
-
-import dmg.util.CommandRequestable;
-
-public class SpyCommandRequest implements CommandRequestable {
-   private static final long serialVersionUID = 7012972757477500153L;
-   private String    _command ;
-   private Object [] _params ;
-   public SpyCommandRequest( String command , Object key , Object value ){
-       _command   = command ;
-       _params    = new Object[2] ;
-       _params[0] = key ;
-       _params[1] = value ;
-   }
-   @Override
-   public String getRequestCommand(){ return _command ; }
-   @Override
-   public int    getArgc(){ return 2 ; }
-   @Override
-   public Object getArgv(int position){ return _params[position] ; }
-
-   @Override
-   public String toString() { return _command + " " + _params[0] + " " + _params[1]; }
-}

modules/cells/src/main/java/dmg/cells/nucleus/CellAdapter.java
@@ -25,14 +25,12 @@ import dmg.util.CommandException;
 import dmg.util.CommandExitException;
 import dmg.util.CommandInterpreter;
 import dmg.util.CommandPanicException;
-import dmg.util.CommandRequestable;
 import dmg.util.CommandSyntaxException;
 import dmg.util.CommandThrowableException;
 import dmg.util.Gate;
 import dmg.util.Pinboard;
 import dmg.util.command.Argument;
 import dmg.util.command.Command;
-import dmg.util.command.HelpFormat;
 import dmg.util.command.Option;
 import dmg.util.logback.FilterShell;
 
@@ -65,8 +63,7 @@ import org.dcache.util.Version;
  * @version 0.2.11, 10/22/1998
  */
 
-public class   CellAdapter
-    extends CommandInterpreter
+public class   CellAdapter extends CommandInterpreter
     implements Cell, CellEventListener, CellEndpoint
 {
     private final static Logger _log =
@@ -130,7 +127,6 @@ public class   CellAdapter
                        String  cellType,
                        Args    args,
                        boolean startNow) {
-
         _args      = args;
         _nucleus   = new CellNucleus(this, cellName, cellType);
         _autoSetup = cellName + "Setup";
@@ -194,6 +190,23 @@ public class   CellAdapter
         executeSetupContext();
         _startGate.open();
     }
+
+    @Override
+    public Serializable command(Args args) throws CommandException {
+        if (args.argc() == 0) {
+            return "";
+        }
+
+        //
+        // check for the NOOP command.
+        //
+        if (args.argc() > 0 && args.argv(0).equals("xyzzy")) {
+            return "Nothing happens.";
+        }
+
+        return super.command(args);
+    }
+
     /**
      *  Executes the ContextVariable :
      *  &lt;cellName&gt;Setup and "!&lt;setupContextName&gt;"
@@ -947,8 +960,7 @@ public class   CellAdapter
             if (msg.isFinalDestination()) {
                 if (_useInterpreter && (! msg.isReply()) &&
                     ((obj instanceof String) ||
-                     (obj instanceof AuthorizedString) ||
-                     (obj instanceof CommandRequestable))) {
+                     (obj instanceof AuthorizedString))) {
 
                     Serializable o;
                     UOID uoid = msg.getUOID();
@@ -1028,7 +1040,8 @@ public class   CellAdapter
         if (command instanceof Authorizable) {
 
             if (_returnCommandException) {
-                return command(new AuthorizedArgs((Authorizable)command));
+                AuthorizedArgs args = new AuthorizedArgs((Authorizable)command);
+                return command(args);
             } else {
                 return autoCommand(command);
             }
@@ -1036,13 +1049,11 @@ public class   CellAdapter
         } else if (command instanceof String) {
 
             if (_returnCommandException) {
-                return command(new Args((String)command));
+                Args args = new Args((String)command);
+                return command(args);
             } else {
                 return autoCommand(command);
             }
-
-        } else if (command instanceof CommandRequestable) {
-            return command((CommandRequestable)command);
         } else {
             throw new
                     CommandPanicException("Illegal CommandClass detected",
@@ -1055,9 +1066,11 @@ public class   CellAdapter
 
         try {
             if (command instanceof String) {
+                Args args = new Args((String) command);
                 return command(new Args((String) command));
             } else if (command instanceof AuthorizedString) {
-                return command(new AuthorizedArgs((AuthorizedString) command));
+                AuthorizedArgs args = new AuthorizedArgs((AuthorizedString) command);
+                return command(args);
             } else {
                 return "Panic : internal server error 14345";
             }
@@ -1083,9 +1096,20 @@ public class   CellAdapter
             return "??? : "+e.toString();
         }
     }
+
+    @Override
+    protected Serializable doExecute(CommandEntry entry, Args args,
+            String[] acls) throws CommandException
+    {
+        if (args instanceof Authorizable) {
+            checkAclPermission((Authorizable) args, args, acls);
+        }
+
+        return super.doExecute(entry, args, acls);
+    }
+
     private CellPath _aclPath    = new CellPath("acm");
     private long     _aclTimeout = 10000L;
-    @Override
     protected void checkAclPermission(Authorizable auth, Object command, String [] acls) throws CommandException {
 
         String user = auth.getAuthorizedPrincipal();

modules/cells/src/main/java/dmg/cells/nucleus/CellShell.java
@@ -43,7 +43,6 @@ import dmg.util.CommandException;
 import dmg.util.CommandExitException;
 import dmg.util.CommandInterpreter;
 import dmg.util.CommandPanicException;
-import dmg.util.CommandRequestable;
 import dmg.util.CommandSyntaxException;
 import dmg.util.CommandThrowableException;
 import dmg.util.Exceptions;
@@ -62,8 +61,7 @@ import org.dcache.util.Args;
   * @author Patrick Fuhrmann
   * @version 0.1, 15 Feb 1998
   */
-public class      CellShell
-       extends    CommandInterpreter
+public class CellShell extends CommandInterpreter
        implements Replaceable, ClassDataProvider
 {
     private final static Logger _log =
@@ -215,12 +213,17 @@ public class      CellShell
           return "";
       }
       try{
+          Args args = new Args(str);
+
+          if (args.argc() == 0) {
+              return "";
+          }
 
          Serializable o;
          if( _externalInterpreter != null ){
-            o =  _externalInterpreter.command( new Args( str ) ) ;
+            o =  _externalInterpreter.command(args);
          }else{
-            o =  command( new Args( str ) ) ;
+            o =  command(args);
          }
          _errorCode = 0 ;
          _errorMsg  = null ;
@@ -240,12 +243,17 @@ public class      CellShell
           return "";
       }
       try{
+          Args args = new Args(strin);
+
+          if (args.argc() == 0) {
+              return "";
+          }
 
          Object o;
          if( _externalInterpreter != null ){
-            o =  _externalInterpreter.command( new Args( str ) ) ;
+            o =  _externalInterpreter.command(args) ;
          }else{
-            o =  command( new Args( str ) ) ;
+            o =  command(args) ;
          }
          _errorCode = 0 ;
          _errorMsg  = null ;
@@ -1095,33 +1103,7 @@ public class      CellShell
    public String ac_id( Args args ){
       return _nucleus.getCellDomainName()+"\n" ;
    }
-   ////////////////////////////////////////////////////////////
-   //
-   // setting the context/environment
-   //
-   public Object ac_set_context_$_2( CommandRequestable request )
-          throws CommandException {
 
-        Map<String,Object> dict = _nucleus.getDomainContext() ;
-        Object contextName = request.getArgv(0) ;
-        if( ! ( contextName instanceof String ) ) {
-            throw new CommandException(67, "ContextName not a string");
-        }
-
-        dict.put((String) contextName , request.getArgv(1) ) ;
-
-        Object o = dict.get( contextName ) ;
-        if( o == null ) {
-            throw new CommandException(68, "Setting of " + contextName + " failed");
-        }
-
-        Object [] answer = new Object[2] ;
-        answer[0] = contextName ;
-        answer[1] = o ;
-
-        return answer ;
-
-   }
    public static final String fh_check =
       " check [-strong] <var1> [<var2> [] ... ]\n"+
       "        checks if all of the specified variables are set.\n"+

modules/cells/src/main/java/dmg/cells/services/CommandTaskCell.java
@@ -415,6 +415,9 @@ public class CommandTaskCell extends CellAdapter {
    }
    @Override
    public Serializable command( Args args )throws CommandException {
+      if (args.argc() == 0) {
+          return "";
+      }
       Args copyArgs = new Args(args);
       try{
           return super.command(args) ;
@@ -424,7 +427,9 @@ public class CommandTaskCell extends CellAdapter {
       }
    }
    private Serializable executeLocalCommand( Args args ) throws CommandException {
-
+       if (args.argc() == 0) {
+           return "";
+       }
       ClientInfo client = _clientHandler.getThisClient() ;
       if( ! client.isAttached() ) {
           throw new

modules/cells/src/main/java/dmg/cells/services/LocationManager.java
@@ -412,8 +412,8 @@ public class LocationManager extends CellAdapter {
           }
           String message = new String( data , 0 , datalen ) ;
           _log.info( "server query : ["+address+"] "+"("+message.length()+") "+message) ;
-
-          message = (String)command( new Args( message ) ) ;
+          Args args = new Args(message);
+          message = args.argc() == 0 ? "" : (String)command(args);
 
           _log.info( "server reply : "+message ) ;
           data = message.getBytes() ;

commit c09d8546b466f2d87f6984bfabe4a631c0b3573c

admin: Fix cd to System cell
6250598677f3a3edccabfed9a28368b6d02db764 introduced a regression by move the
xyzzy noop command to CellAdapter and thus removing it from the System cell.
As a result, cd to the System cell resulted in an error reply. UserAdminShell
however silently ignored this error and nothing bad happened.

1e706795bfe43482d949c5dc1102fb7e513227c9 ported UserAdminShell to CellStub
which rethrows exception replies. As a consequence the UserAdminShell's cell
check would now fail, claiming that the System cell doesn't exist.

The fix adds the xyzzy command to System cell. The patch also changes
UserAdminShell to only consider TimeoutCacheException (which also covers no
route to cell errors) as a non-existing cell. Other errors are logged and
otherwise ignored.

Target: trunk
Require-notes: no
Require-book: no
Acked-by: Paul Millar <paul.millar@desy.de>
Patch: http://rb.dcache.org/r/6841/

modules/cells/src/main/java/dmg/cells/nucleus/SystemCell.java
@@ -260,11 +260,15 @@ public class      SystemCell
 
         if(obj instanceof String) {
            String command = (String) obj;
-           if(command.length() < 1) {
+           if (command.isEmpty()) {
                return;
            }
-           _log.info( "Command : "+command ) ;
-           reply = _cellShell.objectCommand2( command ) ;
+           _log.info("Command: {}", command);
+           if (command.equals("xyzzy")) {
+               reply = "Nothing happens.";
+           } else {
+               reply = _cellShell.objectCommand2(command);
+           }
            processed = true;
         }else if( obj instanceof AuthorizedString ){
            AuthorizedString as = (AuthorizedString)obj ;

modules/dcache/src/main/java/diskCacheV111/admin/UserAdminShell.java
@@ -1282,8 +1282,11 @@ public class UserAdminShell
     private void checkCellExists( CellPath remoteCell) {
         try {
             _cellStub.sendAndWait(remoteCell, ADMIN_COMMAND_NOOP, Object.class, CD_PROBE_MESSAGE_TIMEOUT_MS);
+        } catch (TimeoutCacheException e) {
+            throw new IllegalArgumentException("Cannot cd to this cell as it doesn't exist.");
         } catch (CacheException e) {
-            throw new IllegalArgumentException("Cannot cd to this cell as it doesn't exist");
+            // Some other failure, but apparently the cell exists
+            _log.info("Cell probe failed: {}", e.getMessage());
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }