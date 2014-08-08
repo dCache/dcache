dCache/dcache

commit ff5e0423492a4931b188e48e2599962f1013a377

alarms: decorate data source to capture connector errors as alarms
This patch converts the alarm definitions which filter
database connection failures by creating a wrapper/decorator
class which logs these as Alarm types, and by substituting
the decorator class for its delegate classes in the
object field types and spring injection files.

It could be argued that a more elegant solution would
have been to do this using aspect (a natural fit), but
my experience with AspectJ is limited (coding examples
written during a workshop class), so I adopted this
approach as the shorter/quicker path.

Target: master
Patch: https://rb.dcache.org/r/7138/
Require-notes: yes
Require-book: no
Acked-by: Dmitry

RELEASE NOTES

Note: this patch ought to be applied together with 7137.
Since it falls under the general category of those
changes, no special notice need be given on the release notes.

modules/common/pom.xml
@@ -46,5 +46,10 @@
       <groupId>org.bouncycastle</groupId>
       <artifactId>${bouncycastle.bcprov}</artifactId>
     </dependency>
+
+    <dependency>
+      <groupId>com.zaxxer</groupId>
+      <artifactId>HikariCP</artifactId>
+    </dependency>
   </dependencies>
 </project>

modules/common/src/main/java/org/dcache/db/AlarmEnabledDataSource.java
@@ -1,0 +1,187 @@
+/*
+COPYRIGHT STATUS:
+Dec 1st 2001, Fermi National Accelerator Laboratory (FNAL) documents and
+software are sponsored by the U.S. Department of Energy under Contract No.
+DE-AC02-76CH03000. Therefore, the U.S. Government retains a  world-wide
+non-exclusive, royalty-free license to publish or reproduce these documents
+and software for U.S. Government purposes.  All documents and software
+available from this server are protected under the U.S. and Foreign
+Copyright Laws, and FNAL reserves all rights.
+
+Distribution of the software available from this server is free of
+charge subject to the user following the terms of the Fermitools
+Software Legal Information.
+
+Redistribution and/or modification of the software shall be accompanied
+by the Fermitools Software Legal Information  (including the copyright
+notice).
+
+The user is asked to feed back problems, benefits, and/or suggestions
+about the software to the Fermilab Software Providers.
+
+Neither the name of Fermilab, the  URA, nor the names of the contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+DISCLAIMER OF LIABILITY (BSD):
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED  WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL FERMILAB,
+OR THE URA, OR THE U.S. DEPARTMENT of ENERGY, OR CONTRIBUTORS BE LIABLE
+FOR  ANY  DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY  OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE  POSSIBILITY OF SUCH DAMAGE.
+
+Liabilities of the Government:
+
+This software is provided by URA, independent from its Prime Contract
+with the U.S. Department of Energy. URA is acting independently from
+the Government and in its own private capacity and is not acting on
+behalf of the U.S. Government, nor as its contractor nor its agent.
+Correspondingly, it is understood and agreed that the U.S. Government
+has no connection to this software and in no manner whatsoever shall
+be liable for nor assume any responsibility or obligation for any claim,
+cost, or damages arising out of or resulting from the use of the software
+available from this server.
+
+Export Control:
+
+All documents and software available from this server are subject to U.S.
+export control laws.  Anyone downloading information from this server is
+obligated to secure any necessary Government licenses before exporting
+documents or software obtained from this server.
+ */
+package org.dcache.db;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+import org.dcache.alarms.AlarmMarkerFactory;
+import org.dcache.alarms.Severity;
+import org.dcache.util.NetworkUtils;
+
+/**
+ * A decorator which allows exception thrown by the connection
+ * factory to be marked as alarms.
+ *
+ * @author arossi
+ */
+public class AlarmEnabledDataSource implements DataSource, Closeable {
+
+    private static final org.slf4j.Logger LOGGER =
+                    LoggerFactory.getLogger(DataSource.class);
+    private static final String DB_CONNECTION_FAILURE = "DB_CONNECTION_FAILURE";
+
+    private final DataSource delegate;
+    private final String connectorName;
+    private final String url;
+
+    /**
+     * @param serviceName alarms will be identified by a combination of
+     *        this name, the connection url and the canonical name of
+     *        this client host.
+     */
+    public AlarmEnabledDataSource(String url,
+                                  String connectorName,
+                                  DataSource delegate) {
+        this.connectorName = connectorName;
+        this.url = url;
+        this.delegate = delegate;
+    }
+
+    public PrintWriter getLogWriter() throws SQLException {
+        return delegate.getLogWriter();
+    }
+
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return delegate.unwrap(iface);
+    }
+
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        delegate.setLogWriter(out);
+    }
+
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return delegate.isWrapperFor(iface);
+    }
+
+    public Connection getConnection() throws SQLException {
+        try {
+            return delegate.getConnection();
+        } catch (SQLException sql) {
+            LOGGER.error(AlarmMarkerFactory.getMarker(Severity.CRITICAL,
+                                                      DB_CONNECTION_FAILURE,
+                                                      url,
+                                                      connectorName,
+                                                      NetworkUtils.getCanonicalhostname()),
+                         "Could not get connection to database", sql);
+            throw sql;
+        }
+    }
+
+    public void setLoginTimeout(int seconds) throws SQLException {
+        delegate.setLoginTimeout(seconds);
+    }
+
+    public Connection getConnection(String username, String password)
+                    throws SQLException {
+
+        try {
+            return delegate.getConnection(username, password);
+        } catch (SQLException sql) {
+            LOGGER.error(AlarmMarkerFactory.getMarker(Severity.CRITICAL,
+                                                      DB_CONNECTION_FAILURE,
+                                                      url,
+                                                      connectorName,
+                                                      NetworkUtils.getCanonicalhostname()),
+                         "Could not get connection to database", sql);
+            throw sql;
+        }
+    }
+
+    public int getLoginTimeout() throws SQLException {
+        return delegate.getLoginTimeout();
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return delegate.getParentLogger();
+    }
+
+    public DataSource getDelegate() {
+        return delegate;
+    }
+
+    public void close() throws IOException {
+        if (delegate instanceof HikariDataSource) {
+            ((HikariDataSource) delegate).shutdown();
+        } else if (delegate instanceof Closeable) {
+            ((Closeable) delegate).close();
+        }
+    }
+
+    public void shutdown() {
+        try {
+            close();
+        } catch (IOException t) {
+            LOGGER.warn("{}: shutdown encountered a problem: {}",
+                            connectorName,
+                            t.getMessage() );
+        }
+    }
+}

modules/dcache-chimera/src/main/java/org/dcache/chimera/namespace/ChimeraCleaner.java
@@ -43,6 +43,7 @@ import dmg.cells.nucleus.NoRouteToCellException;
 import org.dcache.cells.AbstractCell;
 import org.dcache.cells.CellStub;
 import org.dcache.cells.Option;
+import org.dcache.db.AlarmEnabledDataSource;
 import org.dcache.services.hsmcleaner.PoolInformationBase;
 import org.dcache.services.hsmcleaner.RequestTracker;
 import org.dcache.services.hsmcleaner.Sink;
@@ -180,7 +181,7 @@ public class ChimeraCleaner extends AbstractCell implements Runnable
     private ScheduledExecutorService _executor;
     private ScheduledFuture<?> _cleanerTask;
     private PoolInformationBase _pools = new PoolInformationBase();
-    private HikariDataSource _dataSource;
+    private AlarmEnabledDataSource _dataSource;
     private JdbcTemplate _db;
     private BroadcastRegistrationTask _broadcastRegistration;
     private CellStub _broadcasterStub;
@@ -276,7 +277,9 @@ public class ChimeraCleaner extends AbstractCell implements Runnable
         config.setMinimumIdle(1);
         config.setMaximumPoolSize(10);
 
-        _dataSource = new HikariDataSource(config);
+        _dataSource = new AlarmEnabledDataSource(jdbcUrl,
+                                                 ChimeraCleaner.class.getSimpleName(),
+                                                 new HikariDataSource(config));
         _db = new JdbcTemplate(_dataSource);
 
         _log.info("Database connection with jdbcUrl={}; user={}",

modules/dcache-chimera/src/main/resources/diskCacheV111/namespace/pnfsmanager-chimera.xml
@@ -36,7 +36,7 @@
       <property name="queueMaxSize" value="${pnfsmanager.limits.queue-length}"/>
   </bean>
 
-   <bean id="chimera-data-source" class="com.zaxxer.hikari.HikariDataSource"
+   <bean id="data-source-delegate" class="com.zaxxer.hikari.HikariDataSource"
           destroy-method="shutdown">
         <description>Database connection pool</description>
         <constructor-arg>
@@ -58,7 +58,7 @@
                 </property>
             </bean>
         </constructor-arg>
-    </bean>
+   </bean>
 
   <bean id="liquibase" class="org.dcache.util.SpringLiquibase">
       <description>Database schema manager</description>
@@ -74,7 +74,14 @@
   </bean>
 
   <bean id="file-system" class="org.dcache.chimera.JdbcFs" depends-on="liquibase">
-      <constructor-arg ref="chimera-data-source"/>
+      <constructor-arg>
+        <bean class="org.dcache.db.AlarmEnabledDataSource" destroy-method="shutdown">
+            <description>Connection pool decorator</description>
+            <constructor-arg value="${pnfsmanager.db.url}"/>
+            <constructor-arg value="JdbcFs"/>
+            <constructor-arg ref="data-source-delegate"/>
+        </bean>
+      </constructor-arg>
       <constructor-arg value="${pnfsmanager.db.dialect}"/>
   </bean>
 

modules/dcache-nfs/src/main/resources/org/dcache/chimera/nfsv41/door/nfsv41-common.xml
@@ -53,7 +53,7 @@
         <constructor-arg value="1"/>
     </bean>
 
-    <bean id="dataSource" class="com.zaxxer.hikari.HikariDataSource"
+    <bean id="data-source-delegate" class="com.zaxxer.hikari.HikariDataSource"
           destroy-method="shutdown">
         <description>Database connection pool</description>
         <constructor-arg>
@@ -77,6 +77,12 @@
         </constructor-arg>
     </bean>
 
+    <bean id="dataSource" class="org.dcache.db.AlarmEnabledDataSource" destroy-method="shutdown">
+        <description>Connection pool decorator</description>
+        <constructor-arg value="${nfs.db.url}"/>
+        <constructor-arg value="DCacheAwareJdbcFs"/>
+        <constructor-arg ref="data-source-delegate"/>
+    </bean>
 
     <bean id="liquibase" class="org.dcache.util.SpringLiquibase">
         <description>Database schema manager</description>

modules/dcache/src/main/java/diskCacheV111/services/TransferManager.java
@@ -25,7 +25,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -33,7 +32,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import diskCacheV111.util.CacheException;
-import diskCacheV111.util.MessageEventTimer;
 import diskCacheV111.util.Pgpass;
 import diskCacheV111.util.PnfsId;
 import diskCacheV111.vehicles.DoorTransferFinishedMessage;
@@ -44,11 +42,10 @@ import diskCacheV111.vehicles.transferManager.TransferStatusQueryMessage;
 
 import dmg.cells.nucleus.CellMessage;
 import dmg.cells.nucleus.CellPath;
-import dmg.cells.nucleus.DelayedReply;
-import dmg.cells.nucleus.Reply;
 
 import org.dcache.cells.AbstractCell;
 import org.dcache.cells.CellStub;
+import org.dcache.db.AlarmEnabledDataSource;
 import org.dcache.srm.request.sql.RequestsPropertyStorage;
 import org.dcache.srm.scheduler.JobIdGenerator;
 import org.dcache.srm.scheduler.JobIdGeneratorFactory;
@@ -90,7 +87,7 @@ public abstract class TransferManager extends AbstractCell
     private final Map<Long, TimerTask> _moverTimeoutTimerTasks =
             new ConcurrentHashMap<>();
     private String _ioQueueName; // multi io queue option
-    private HikariDataSource ds;
+    private AlarmEnabledDataSource ds;
     private JobIdGenerator idGenerator;
     public final Set<PnfsId> justRequestedIDs = new HashSet<>();
     private String _poolProxy;
@@ -196,7 +193,9 @@ public abstract class TransferManager extends AbstractCell
             config.setDataSource(new DriverManagerDataSource(_jdbcUrl, _user, _pass));
             config.setMaximumPoolSize(50);
             config.setMinimumIdle(1);
-            ds = new HikariDataSource(config);
+            ds = new AlarmEnabledDataSource(_jdbcUrl,
+                                            TransferManager.class.getSimpleName(),
+                                            new HikariDataSource(config));
 
             RequestsPropertyStorage.initPropertyStorage(
                     new DataSourceTransactionManager(ds), ds, "srmnextrequestid");
@@ -291,7 +290,9 @@ public abstract class TransferManager extends AbstractCell
         config.setDataSource(new DriverManagerDataSource(_jdbcUrl, _user, _pass));
         JDOPersistenceManagerFactory pmf = new JDOPersistenceManagerFactory(
                 Maps.<String, Object>newHashMap(Maps.fromProperties(properties)));
-        pmf.setConnectionFactory(new HikariDataSource(config));
+        pmf.setConnectionFactory(new AlarmEnabledDataSource(_jdbcUrl,
+                                                            TransferManager.class.getSimpleName(),
+                                                            new HikariDataSource(config)));
         return pmf.getPersistenceManager();
     }
 

modules/dcache/src/main/resources/diskCacheV111/services/space/spacemanager.xml
@@ -48,7 +48,7 @@
     <constructor-arg ref="pnfs-stub"/>
   </bean>
 
-    <bean id="data-source" class="com.zaxxer.hikari.HikariDataSource"
+    <bean id="data-source-delegate" class="com.zaxxer.hikari.HikariDataSource"
             destroy-method="shutdown">
         <constructor-arg>
             <bean class="com.zaxxer.hikari.HikariConfig">
@@ -71,16 +71,23 @@
         </constructor-arg>
     </bean>
 
+    <bean id="data-source" class="org.dcache.db.AlarmEnabledDataSource" destroy-method="shutdown">
+            <description>Connection pool decorator</description>
+            <constructor-arg value="${spacemanager.db.url}"/>
+            <constructor-arg value="SpaceManager"/>
+            <constructor-arg ref="data-source-delegate"/>
+    </bean>
+
     <bean id="liquibase" class="org.dcache.util.SpringLiquibase">
         <description>Database schema manager</description>
-        <property name="dataSource" ref="data-source"/>
+        <property name="dataSource" ref="data-source-delegate"/>
         <property name="changeLog" value="classpath:${spacemanager.db.schema.changelog}"/>
         <property name="shouldUpdate" value="${spacemanager.db.schema.auto}"/>
     </bean>
 
   <bean id="tx-manager" class="org.springframework.jdbc.datasource.DataSourceTransactionManager">
-    <description>Database transaction manager</description>
-    <property name="dataSource" ref="data-source"/>
+        <description>Database transaction manager</description>
+        <property name="dataSource" ref="data-source"/>
   </bean>
 
   <bean class="org.dcache.util.aspects.PerInstanceAnnotationTransactionBeanPostProcessor">

commit 4b10fe5d1ea40fb8bdeb3ec832e114668a9e7308

Fix several issues in recent alarms changes
The patch fixes several issues introduced in ff5e0423492a4931b188e48e2599962f1013a377:

- dcache-common imports hikaricp, which means that several components that
  don't make use of databases now pull in database related code (like the srm client).

- Missing @Override annotations in AlarmEnabledDataSource

- Redundant shutdown method in AlarmEnabledDataSource (use close)

- Infinite loop in uncaught exception handler (on IOException that isn't an
  out of file descriptor error, the code would loop forever)

- Complex code before kill on fatal JVM error (which in case of OOM may lead to
  another OOM rather than a shutdown)

To fix the HikariCP dependency I had to update to HikariCP 2.0.1.

Target: trunk
Require-notes: no
Require-book: no
Acked-by: Albert Rossi <arossi@fnal.gov>
Patch: https://rb.dcache.org/r/7188/

modules/acl/pom.xml
@@ -24,7 +24,7 @@
     </dependency>
     <dependency>
       <groupId>com.zaxxer</groupId>
-      <artifactId>HikariCP</artifactId>
+      <artifactId>HikariCP-java6</artifactId>
     </dependency>
     <dependency>
       <groupId>org.dcache</groupId>

modules/cells/src/main/java/dmg/cells/nucleus/SystemCell.java
@@ -1,9 +1,10 @@
 package dmg.cells.nucleus ;
 
+import com.google.common.base.Throwables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.Serializable;
 import java.net.InetAddress;
@@ -320,27 +321,25 @@ public class      SystemCell
          */
         if (e instanceof VirtualMachineError) {
             _oomSafetyBuffer = null;
+            kill();
             _log.error(AlarmMarkerFactory.getMarker(Severity.CRITICAL,
                                                     "FATAL_JVM_ERROR",
                                                     getCellDomainName(),
                                                     getCellName()),
-                                                    "Fatal JVM error", e);
-            _log.error("Shutting down...");
-            kill();
+                       "Restarting due to fatal JVM error", e);
+            return;
         }
 
-        if (e instanceof IOException) {
-            Throwable cause = e;
-            while(cause != null) {
-                if (cause.toString().contains("Too many open files")) {
-                    _log.error(AlarmMarkerFactory.getMarker(Severity.CRITICAL,
-                                                            "OUT_OF_FILE_DESCRIPTORS",
-                                                            getCellDomainName(),
-                                                            getCellName()),
-                                     "Uncaught exception in thread " + t.getName(),
-                                     e);
-                    return;
-                }
+        Throwable root = Throwables.getRootCause(e);
+        if (root instanceof FileNotFoundException) {
+            if (root.getMessage().contains("Too many open files")) {
+                _log.error(AlarmMarkerFactory.getMarker(Severity.CRITICAL,
+                                                        "OUT_OF_FILE_DESCRIPTORS",
+                                                        getCellDomainName(),
+                                                        getCellName()),
+                           "Uncaught exception in thread " + t.getName(),
+                           e);
+                return;
             }
         }
 

modules/common/pom.xml
@@ -46,10 +46,5 @@
       <groupId>org.bouncycastle</groupId>
       <artifactId>${bouncycastle.bcprov}</artifactId>
     </dependency>
-
-    <dependency>
-      <groupId>com.zaxxer</groupId>
-      <artifactId>HikariCP</artifactId>
-    </dependency>
   </dependencies>
 </project>

modules/common/src/main/java/org/dcache/db/AlarmEnabledDataSource.java
@@ -59,7 +59,6 @@ documents or software obtained from this server.
  */
 package org.dcache.db;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
@@ -75,6 +74,8 @@ import java.util.logging.Logger;
 import org.dcache.alarms.AlarmMarkerFactory;
 import org.dcache.alarms.Severity;
 import org.dcache.util.NetworkUtils;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * A decorator which allows exception thrown by the connection
@@ -93,34 +94,46 @@ public class AlarmEnabledDataSource implements DataSource, Closeable {
     private final String url;
 
     /**
-     * @param serviceName alarms will be identified by a combination of
+     * @param connectorName alarms will be identified by a combination of
      *        this name, the connection url and the canonical name of
      *        this client host.
      */
     public AlarmEnabledDataSource(String url,
                                   String connectorName,
                                   DataSource delegate) {
-        this.connectorName = connectorName;
-        this.url = url;
-        this.delegate = delegate;
+        this.connectorName = checkNotNull(connectorName);
+        this.url = checkNotNull(url);
+        this.delegate = checkNotNull(delegate);
     }
 
+    @Override
     public PrintWriter getLogWriter() throws SQLException {
         return delegate.getLogWriter();
     }
 
+    @Override
+    @SuppressWarnings("unchecked")
     public <T> T unwrap(Class<T> iface) throws SQLException {
+        if (iface.isInstance(delegate)) {
+            return (T) delegate;
+        }
         return delegate.unwrap(iface);
     }
 
+    @Override
     public void setLogWriter(PrintWriter out) throws SQLException {
         delegate.setLogWriter(out);
     }
 
+    @Override
     public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        if (iface.isInstance(delegate)) {
+            return true;
+        }
         return delegate.isWrapperFor(iface);
     }
 
+    @Override
     public Connection getConnection() throws SQLException {
         try {
             return delegate.getConnection();
@@ -135,10 +148,12 @@ public class AlarmEnabledDataSource implements DataSource, Closeable {
         }
     }
 
+    @Override
     public void setLoginTimeout(int seconds) throws SQLException {
         delegate.setLoginTimeout(seconds);
     }
 
+    @Override
     public Connection getConnection(String username, String password)
                     throws SQLException {
 
@@ -155,33 +170,20 @@ public class AlarmEnabledDataSource implements DataSource, Closeable {
         }
     }
 
+    @Override
     public int getLoginTimeout() throws SQLException {
         return delegate.getLoginTimeout();
     }
 
+    @Override
     public Logger getParentLogger() throws SQLFeatureNotSupportedException {
         return delegate.getParentLogger();
     }
 
-    public DataSource getDelegate() {
-        return delegate;
-    }
-
+    @Override
     public void close() throws IOException {
-        if (delegate instanceof HikariDataSource) {
-            ((HikariDataSource) delegate).shutdown();
-        } else if (delegate instanceof Closeable) {
+        if (delegate instanceof Closeable) {
             ((Closeable) delegate).close();
-        }
-    }
-
-    public void shutdown() {
-        try {
-            close();
-        } catch (IOException t) {
-            LOGGER.warn("{}: shutdown encountered a problem: {}",
-                            connectorName,
-                            t.getMessage() );
         }
     }
 }

modules/dcache-chimera/pom.xml
@@ -49,7 +49,7 @@
     </dependency>
     <dependency>
       <groupId>com.zaxxer</groupId>
-      <artifactId>HikariCP</artifactId>
+      <artifactId>HikariCP-java6</artifactId>
     </dependency>
     <dependency>
         <groupId>org.postgresql</groupId>

modules/dcache-chimera/src/main/java/org/dcache/chimera/namespace/ChimeraCleaner.java
@@ -13,6 +13,7 @@ import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -299,7 +300,11 @@ public class ChimeraCleaner extends AbstractCell implements Runnable
             _broadcastRegistration.unregister();
         }
         if (_dataSource != null) {
-            _dataSource.shutdown();
+            try {
+                _dataSource.close();
+            } catch (IOException e) {
+                _log.debug("Failed to shutdown database connection pool: {}", e.getMessage());
+            }
         }
     }
 

modules/dcache-chimera/src/main/resources/diskCacheV111/namespace/pnfsmanager-chimera.xml
@@ -75,7 +75,7 @@
 
   <bean id="file-system" class="org.dcache.chimera.JdbcFs" depends-on="liquibase">
       <constructor-arg>
-        <bean class="org.dcache.db.AlarmEnabledDataSource" destroy-method="shutdown">
+        <bean class="org.dcache.db.AlarmEnabledDataSource" destroy-method="close">
             <description>Connection pool decorator</description>
             <constructor-arg value="${pnfsmanager.db.url}"/>
             <constructor-arg value="JdbcFs"/>

modules/dcache-nfs/src/main/resources/org/dcache/chimera/nfsv41/door/nfsv41-common.xml
@@ -77,7 +77,7 @@
         </constructor-arg>
     </bean>
 
-    <bean id="dataSource" class="org.dcache.db.AlarmEnabledDataSource" destroy-method="shutdown">
+    <bean id="dataSource" class="org.dcache.db.AlarmEnabledDataSource" destroy-method="close">
         <description>Connection pool decorator</description>
         <constructor-arg value="${nfs.db.url}"/>
         <constructor-arg value="DCacheAwareJdbcFs"/>

modules/dcache/pom.xml
@@ -76,7 +76,7 @@
     </dependency>
     <dependency>
       <groupId>com.zaxxer</groupId>
-      <artifactId>HikariCP</artifactId>
+      <artifactId>HikariCP-java6</artifactId>
     </dependency>
     <dependency>
         <groupId>org.postgresql</groupId>

modules/dcache/src/main/java/diskCacheV111/services/TransferManager.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Transaction;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -181,7 +182,11 @@ public abstract class TransferManager extends AbstractCell
     {
         super.cleanUp();
         if (ds != null) {
-            ds.shutdown();
+            try {
+                ds.close();
+            } catch (IOException e) {
+                log.debug("Failed to shutdown database connection pool: {}", e.getMessage());
+            }
         }
         executor.shutdown();
     }

modules/dcache/src/main/resources/diskCacheV111/services/space/spacemanager.xml
@@ -71,7 +71,7 @@
         </constructor-arg>
     </bean>
 
-    <bean id="data-source" class="org.dcache.db.AlarmEnabledDataSource" destroy-method="shutdown">
+    <bean id="data-source" class="org.dcache.db.AlarmEnabledDataSource" destroy-method="close">
             <description>Connection pool decorator</description>
             <constructor-arg value="${spacemanager.db.url}"/>
             <constructor-arg value="SpaceManager"/>

modules/dcache/src/main/resources/diskCacheV111/srm/srm.xml
@@ -457,7 +457,7 @@
         </constructor-arg>
     </bean>
 
-    <bean id="data-source" class="org.dcache.db.AlarmEnabledDataSource" destroy-method="shutdown">
+    <bean id="data-source" class="org.dcache.db.AlarmEnabledDataSource" destroy-method="close">
             <description>Connection pool decorator</description>
             <constructor-arg value="${srm.db.url}"/>
             <constructor-arg value="Srm"/>

modules/srm-server/pom.xml
@@ -104,7 +104,7 @@
     </dependency>
     <dependency>
       <groupId>com.zaxxer</groupId>
-      <artifactId>HikariCP</artifactId>
+      <artifactId>HikariCP-java6</artifactId>
     </dependency>
 
     <dependency>

pom.xml
@@ -286,8 +286,8 @@
             </dependency>
             <dependency>
                 <groupId>com.zaxxer</groupId>
-                <artifactId>HikariCP</artifactId>
-                <version>1.3.9</version>
+                <artifactId>HikariCP-java6</artifactId>
+                <version>2.0.1</version>
             </dependency>
             <dependency>
                 <groupId>org.postgresql</groupId>