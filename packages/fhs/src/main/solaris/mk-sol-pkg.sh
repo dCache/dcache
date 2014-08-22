0f1cea448091c58ec0a492fd5b91b9b0e8ab5121
Paul Millar <paul.millar@desy.de>
2014-08-22 18:39:01 +0200

    solaris: fix solaris package script and add work-around for pkgmk bug
    When building a tagged version of dCache, one of the directories is "-P
    pkg".  The space in the directory name breaks the script and pkgmk
    application.
    
    This patch fixes the script by adding quotes to paths and adds a
    work-around for the pkgmk bug.
    
    Target: master
    Patch: https://rb.dcache.org/r/7212/
    Acked-by: Tigran Mkrtchyan
    Request: 2.10
    Request: 2.9
    Request: 2.8
    Request: 2.7

--- packages/fhs/src/main/solaris/mk-sol-pkg.sh
+++ packages/fhs/src/main/solaris/mk-sol-pkg.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+SRCDIR=`pwd`
 BUILDDIR=$1
 DISTDIR=$2
 VERSION=$3
@@ -9,6 +10,29 @@ echo 'i pkginfo' > prototype
 find . | grep -v '^./prototype$' | grep -v '^./pkginfo$' | pkgproto \
     | awk '{ print $1, $2, $3, $4, "root", "bin" }' >> prototype
 
-mkdir -p ${BUILDDIR}
-pkgmk -o -d ${BUILDDIR} -r .
-pkgtrans -s ${BUILDDIR} ${DISTDIR}/${NAME}-${VERSION}.pkg dCache
+mkdir -p "${BUILDDIR}"
+
+#  Work-around pkgmk brokenness.
+#
+#  When building a package, one of the directories is "-P pkg".  When
+#  processing the prototype file, pkgmk will prepend the path to the
+#  files.  Unfortunately, it cannot cope with a space being present in
+#  the resulting path.  Further, it also cannot cope with cwd
+#  containing a space, perhaps as it must find the prototype file.
+#
+#  As a work-around, we copy everything into a temporary directory
+#  that (on Solaris) will not contain a space and change to that
+#  directory for building the package.
+
+TEMP_STORAGE=`mktemp -d`
+cp -r * "${TEMP_STORAGE}"
+cd "${TEMP_STORAGE}"
+
+pkgmk -o -d "${BUILDDIR}" -r .
+
+cd "${SRCDIR}"
+rm -rf "${TEMP_STORAGE}"
+
+pkgtrans -s "${BUILDDIR}" "${DISTDIR}/${NAME}-${VERSION}.pkg" dCache
+
+rm -rf "${BUILDDIR}"
