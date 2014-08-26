e1cc8208d3314091fa3444d372617263b981ca27
Gerd Behrmann <behrmann@gmail.com>
2014-08-26 21:53:13 +0200

    chimera: Move new CLI to chimera and delete old CLI
    The patch moves the chimera shell to the chimera module. The patch
    removes the old CLI classes.
    
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: https://rb.dcache.org/r/7221/

--- modules/chimera/src/main/java/org/dcache/chimera/cli/Shell.java
+++ modules/chimera/src/main/java/org/dcache/chimera/cli/Shell.java
@@ -1,19 +1,18 @@
-/* dCache - http://www.dcache.org/
- *
- * Copyright (C) 2014 Deutsches Elektronen-Synchrotron
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
+/*
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU Library General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program (see the file COPYING.LIB for more
+ * details); if not, write to the Free Software Foundation, Inc.,
+ * 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 package org.dcache.chimera.cli;
 
