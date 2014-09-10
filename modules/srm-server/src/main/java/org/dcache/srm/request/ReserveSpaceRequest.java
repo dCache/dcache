a663f44d7e6e0bd2e6ca2abf60d13dc6fb3d8115
Paul Millar <paul.millar@desy.de>
2014-09-10 17:24:29 +0200

    Add missing import

--- modules/srm-server/src/main/java/org/dcache/srm/request/ReserveSpaceRequest.java
+++ modules/srm-server/src/main/java/org/dcache/srm/request/ReserveSpaceRequest.java
@@ -77,6 +77,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.dcache.srm.SRMInvalidRequestException;
 import org.dcache.srm.SRMUser;
