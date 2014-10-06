4dfca009d9b2490d3ad15197cbbb21cecb375993
Gerd Behrmann <behrmann@gmail.com>
2014-10-06 11:39:39 +0200

    pool: Add JavaDoc to migration JobDefinition
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: https://rb.dcache.org/r/7337/

--- modules/dcache/src/main/java/org/dcache/pool/migration/JobDefinition.java
+++ modules/dcache/src/main/java/org/dcache/pool/migration/JobDefinition.java
@@ -12,21 +12,88 @@ import org.dcache.util.expression.Expression;
  */
 public class JobDefinition
 {
+    /**
+     * Selection criteria defining which replicas to include in the migration job.
+     */
     public final List<CacheEntryFilter> filters;
+
+    /**
+     * New mode of the source replica to apply after successful migration.
+     */
     public final CacheEntryMode sourceMode;
+
+    /**
+     * Mode of the target replica to apply after successful migration.
+     */
     public final CacheEntryMode targetMode;
+
+    /**
+     * Strategy object for selection a target pool.
+     */
     public final PoolSelectionStrategy selectionStrategy;
+
+    /**
+     * Sort order for the initial replica list.
+     */
     public final Comparator<CacheEntry> comparator;
+
+    /**
+     * Pool information about the source pool. Must contain exactly one
+     * pool.
+     */
     public final RefreshablePoolList sourceList;
+
+    /**
+     * Pool information about target pools.
+     */
     public final RefreshablePoolList poolList;
+
+    /**
+     * How often to refresh pool information.
+     */
     public final long refreshPeriod;
+
+    /**
+     * Whether the job is permanent. Permanent jobs add new replicas to the job if
+     * they match the selection criteria.
+     */
     public final boolean isPermanent;
+
+    /**
+     * Whether the job is eager. Eager jobs proceed by creating new replicas if
+     * existing replicas are inaccessible.
+     */
     public final boolean isEager;
+
+    /**
+     * Whether to move pins to the target pool after successful migration.
+     */
     public final boolean mustMovePins;
+
+    /**
+     * Whether to verify the checksum when reusing existing target replicas.
+     */
     public final boolean computeChecksumOnUpdate;
+
+    /**
+     * Condition when to pause the migration job.
+     */
     public final Expression pauseWhen;
+
+    /**
+     * Condition when to stop the migration job.
+     */
     public final Expression stopWhen;
+
+    /**
+     * Whether the migration job overrides the mode of the source pool, that is,
+     * whether to allow migration even if the source pool is disabled.
+     */
     public final boolean forceSourceMode;
+
+    /**
+     * Number of replicas to create.
+     */
     public final int replicas;
 
     public JobDefinition(List<CacheEntryFilter> filters,
