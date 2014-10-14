b09bb69ccf479359aba2384ac2c0b7dc8557d2b6
Gerd Behrmann <behrmann@gmail.com>
2014-10-14 10:16:47 +0200

    billing: Add file size to request records
    Request records submitted by doors lack the file size (they always
    appeared as 0 in billing records). This patch adds the file size for
    all protocols except for NFS.
    
    Target: trunk
    Request: 2.10
    Request: 2.9
    Request: 2.8
    Request: 2.7
    Request: 2.6
    Require-notes: yes
    Require-book: yes
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: https://rb.dcache.org/r/7360/
    (cherry picked from commit ef3d069dd73275189543be30160ed29484e7c2ec)

--- modules/dcache-dcap/src/main/java/diskCacheV111/doors/DCapDoorInterpreterV3.java
+++ modules/dcache-dcap/src/main/java/diskCacheV111/doors/DCapDoorInterpreterV3.java
@@ -2191,6 +2191,8 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
                                                 _readPoolSelectionContext,
                                                 allowedStates);
                getPoolMessage.setIoQueueName(_ioQueueName );
+
+                _info.setFileSize(_fileAttributes.getSize());
             }
 
             if( _verbose ) {
@@ -2398,6 +2400,9 @@ public class DCapDoorInterpreterV3 implements KeepAliveListener,
                                 break ;
                             }
                         }
+                    }
+                    if (_ioMode.contains("w")) {
+                        _info.setFileSize(filesize);
                     }
                     sendReply( "doorTransferArrived" , 0 , "" ) ;
                 }else{
