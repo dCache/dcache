6885adc8c6c566c83b1611bc7d94585ea5d193f9
Gerd Behrmann <behrmann@gmail.com>
2014-10-27 11:49:13 +0100

    chimera: Refine IOException to ChimeraFsException
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: https://rb.dcache.org/r/7404/

--- modules/dcache-chimera/src/main/java/org/dcache/chimera/namespace/ChimeraNameSpaceProvider.java
+++ modules/dcache-chimera/src/main/java/org/dcache/chimera/namespace/ChimeraNameSpaceProvider.java
@@ -659,7 +659,7 @@ public class ChimeraNameSpaceProvider
     }
 
     private FileAttributes getFileAttributesForPermissionHandler(@Nullable FsInode inode)
-        throws IOException, CacheException
+        throws ChimeraFsException, CacheException
     {
         return (inode == null)
                 ? null
@@ -667,13 +667,13 @@ public class ChimeraNameSpaceProvider
     }
 
     private FileAttributes getFileAttributesForPermissionHandler(@Nonnull ExtendedInode inode)
-        throws IOException, CacheException
+        throws ChimeraFsException, CacheException
     {
         return getFileAttributes(inode, _permissionHandler.getRequiredAttributes());
     }
 
     private FileAttributes getFileAttributes(ExtendedInode inode, Set<FileAttribute> attr)
-        throws IOException, ChimeraFsException, CacheException
+        throws ChimeraFsException, CacheException
     {
         if (!inode.exists()) {
             throw new FileNotFoundHimeraFsException();
@@ -1279,7 +1279,7 @@ public class ChimeraNameSpaceProvider
             removeRecursively(uploadDirInode, temporaryDir.getName());
 
             return new PnfsId(inodeOfFile.toString());
-        } catch (IOException e) {
+        } catch (ChimeraFsException e) {
             throw new CacheException(CacheException.UNEXPECTED_SYSTEM_EXCEPTION,
                                      e.getMessage());
         }
