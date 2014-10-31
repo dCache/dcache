60f8bb4b367487aedb1b70e8408bcbd82f4aa3b7
Gerd Behrmann <behrmann@gmail.com>
2014-10-31 02:18:53 +0100

    xrootd: Initialize session
    Initializes a session string for xrootd.
    
    Target: master
    Request: 2.11
    Request: 2.10
    Require-notes: yes
    Require-book: yes
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: https://rb.dcache.org/r/7437/
    (cherry picked from commit 2fda3350c67486f871a1c22e73e6f2f23e380596)

--- modules/dcache-xrootd/src/main/java/org/dcache/xrootd/door/ConnectionTracker.java
+++ modules/dcache-xrootd/src/main/java/org/dcache/xrootd/door/ConnectionTracker.java
@@ -1,23 +1,24 @@
 package org.dcache.xrootd.door;
 
+import com.google.common.collect.Ordering;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandler.Sharable;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelStateEvent;
-import org.jboss.netty.channel.SimpleChannelHandler;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 
 import java.io.PrintWriter;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import dmg.cells.nucleus.CDC;
 import dmg.cells.nucleus.CellCommandListener;
 import dmg.cells.nucleus.CellInfo;
 import dmg.cells.nucleus.CellInfoProvider;
 
 import org.dcache.util.Args;
-
-import static org.jboss.netty.channel.Channels.close;
 
 /**
  * Channel handler that keeps track of connected channels. Provides
@@ -25,35 +26,31 @@ import static org.jboss.netty.channel.Channels.close;
  */
 @Sharable
 public class ConnectionTracker
-    extends SimpleChannelHandler
-    implements CellCommandListener,
-               CellInfoProvider
+    extends SimpleChannelUpstreamHandler
+    implements CellCommandListener, CellInfoProvider
 {
-    private Map<Integer, Channel> _channels = new ConcurrentHashMap<>();
-    private AtomicInteger _counter = new AtomicInteger();
-
-    public ConnectionTracker()
-    {
-    }
+    private Map<Channel,String> sessions = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger();
 
     @Override
-    public void channelConnected(ChannelHandlerContext ctx,
-                                 ChannelStateEvent e)
+    public void channelOpen(ChannelHandlerContext ctx, ChannelStateEvent e)
         throws Exception
     {
-        super.channelConnected(ctx, e);
         Channel channel = e.getChannel();
-        _channels.put(channel.getId(), channel);
-        _counter.getAndIncrement();
+        sessions.put(channel, CDC.getSession());
+        counter.getAndIncrement();
+        super.channelOpen(ctx, e);
     }
 
     @Override
-    public void channelDisconnected(ChannelHandlerContext ctx,
-                                    ChannelStateEvent e)
+    public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e)
         throws Exception
     {
-        _channels.remove(e.getChannel().getId());
-        super.channelDisconnected(ctx, e);
+        try {
+            super.channelClosed(ctx, e);
+        } finally {
+            sessions.remove(e.getChannel());
+        }
     }
 
     @Override
@@ -65,31 +62,29 @@ public class ConnectionTracker
     @Override
     public void getInfo(PrintWriter pw)
     {
-        pw.println(String.format("Active : %d", _channels.size()));
-        pw.println(String.format("Created: %d", _counter.get()));
+        pw.println(String.format("Active : %d", sessions.size()));
+        pw.println(String.format("Created: %d", counter.get()));
     }
 
     public String ac_connections(Args args)
     {
         StringBuilder s = new StringBuilder();
-        for (Map.Entry<Integer,Channel> e: _channels.entrySet()) {
-            Channel c = e.getValue();
-            s.append(e.getKey()).append(" ").append(c.getRemoteAddress())
-                    .append("\n");
+        for (Map.Entry<Channel, String> e: sessions.entrySet()) {
+            s.append(e.getValue()).append(' ').append(e.getKey()).append("\n");
         }
         return s.toString();
     }
 
     public String ac_kill_$_1(Args args)
     {
-        int id = Integer.parseInt(args.argv(1));
-        Channel channel = _channels.get(id);
-        if (channel == null) {
-            return "No such connection";
+        String session = args.argv(0);
+        Iterator<String> iterator = sessions.values().iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(session)) {
+                iterator.remove();
+                return "";
+            }
         }
-
-        close(channel);
-
-        return "";
+        return "No such connection";
     }
 }
