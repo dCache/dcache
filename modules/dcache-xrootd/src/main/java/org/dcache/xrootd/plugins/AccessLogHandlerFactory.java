c07e0cf75e677f4e94f9dee8499d184b73b47168
Gerd Behrmann <behrmann@gmail.com>
2014-11-03 13:06:20 +0100

    xrootd: Add access-log plugin
    Allows xrootd doors to generate an access log similar to those of SRM, FTP and WebDAV
    doors. May optionally be used on pools too.
    
    In contrast to 2.12, the plugin is not enabled by default.
    
    Target: trunk
    Request: 2.11
    Request: 2.10
    Require-notes: yes
    Require-book: no
    Acked-by: Karsten Schwank <karsten.schwank@desy.de>
    Patch: https://rb.dcache.org/r/7442/
    (cherry picked from commit 25250c09e8288b18ee4a0a569676a9a9db51e3b1)

---
+++ modules/dcache-xrootd/src/main/java/org/dcache/xrootd/plugins/AccessLogHandlerFactory.java
@@ -0,0 +1,45 @@
+/* dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2014 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.xrootd.plugins;
+
+import org.jboss.netty.channel.ChannelHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AccessLogHandlerFactory implements ChannelHandlerFactory
+{
+    private final Logger accessLogger = LoggerFactory.getLogger("org.dcache.access.xrootd");
+
+    @Override
+    public String getName()
+    {
+        return AccessLogHandlerProvider.NAME;
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "Generates an access log";
+    }
+
+    @Override
+    public ChannelHandler createHandler()
+    {
+        return new AccessLogHandler(accessLogger);
+    }
+}
