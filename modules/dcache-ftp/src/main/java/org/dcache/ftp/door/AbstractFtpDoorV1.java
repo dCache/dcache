7990ab6cbec37b6bc12059b7addd19ba5cb1b3c7
Paul Millar <paul.millar@desy.de>
2014-12-08 17:39:37 +0100

    ftp: fix help output
    The HELP command was added to allow better interoperability with
    ncftp and Globus clients.  Unfortunately, the output for 'SITE HELP'
    and 'HELP SITE' was incorrect.
    
    While ncftp proved robust against this bug (it wasn't detected
    during previous testing) and Globus code is likely OK too, I
    request back-porting so dCache output conforms with RFC 959.
    
    Target: master
    Patch: https://rb.dcache.org/r/7575/
    Acked-by: Gerd Behrmann
    Request: 2.11
    Request: 2.10
    Request: 2.9

--- modules/dcache-ftp/src/main/java/org/dcache/ftp/door/AbstractFtpDoorV1.java
+++ modules/dcache-ftp/src/main/java/org/dcache/ftp/door/AbstractFtpDoorV1.java
@@ -2176,6 +2176,7 @@ public abstract class AbstractFtpDoorV1
                     } else {
                         pw.print("214 " + line);
                     }
+                    isFirstLine = false;
                 }
             } else {
                 pw.print("501 Unknown command " + command);
