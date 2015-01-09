3c70868ebd582789e2b99081a9548cc27a1f723d
Gerd Behrmann <behrmann@gmail.com>
2015-01-09 09:06:36 +0100

    pool: Drop StorageInfo from CacheRepositoryEntryInfo
    The storage info isn't used anyway. In the quest to reduce the
    use of StorageInfo, this patch drop the StorageInfo from the
    message.
    
    Also cleans up the rest of the message a bit.
    
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: https://rb.dcache.org/r/7645/

--- modules/dcache/src/main/java/diskCacheV111/repository/CacheRepositoryEntryInfo.java
+++ modules/dcache/src/main/java/diskCacheV111/repository/CacheRepositoryEntryInfo.java
@@ -1,24 +1,11 @@
-// $Id:  $
-/*
- * CacheRepositoryEntryInfo.java
- *
- * Created on January 18, 2008, 7:47 PM
- *
- */
-
 package diskCacheV111.repository;
 
 import java.io.Serializable;
 
 import diskCacheV111.util.PnfsId;
-import diskCacheV111.vehicles.StorageInfo;
 
 import org.dcache.pool.repository.CacheEntry;
 
-/**
- *
- * @author timur
- */
 public class CacheRepositoryEntryInfo implements Serializable {
     private static final long serialVersionUID = -4494188511917602601L;
 
@@ -37,69 +24,45 @@ public class CacheRepositoryEntryInfo implements Serializable {
     private  int bitmask;
     private long lastAccessTime;
     private long creationTime;
-    private StorageInfo storageInfo;
     private long size;
-
-
-    /**
-     * Creates a new instance of CacheRepositoryEntryInfo
-     */
-    public CacheRepositoryEntryInfo() {
-    }
 
     public CacheRepositoryEntryInfo(CacheEntry entry)
     {
         pnfsId = entry.getPnfsId();
         lastAccessTime = entry.getLastAccessTime();
         creationTime = entry.getCreationTime();
-        storageInfo = entry.getFileAttributes().getStorageInfo();
         size = entry.getReplicaSize();
         switch (entry.getState()) {
         case PRECIOUS:
-            setBit(PRECIOUS_BIT, true);
+            bitmask = 1 << PRECIOUS_BIT;
             break;
         case CACHED:
-            setBit(CACHED_BIT, true);
+            bitmask = 1 << CACHED_BIT;
             break;
         case FROM_CLIENT:
-            setBit(RECEIVINGFROMCLIENT_BIT, true);
+            bitmask = 1 << RECEIVINGFROMCLIENT_BIT;
             break;
         case FROM_POOL:
         case FROM_STORE:
-            setBit(RECEIVINGFROMSTORE_BIT, true);
+            bitmask = 1 << RECEIVINGFROMSTORE_BIT;
             break;
         case BROKEN:
-            setBit(BAD_BIT, true);
+            bitmask = 1 << BAD_BIT;
             break;
         case REMOVED:
-            setBit(REMOVED_BIT, true);
+            bitmask = 1 << REMOVED_BIT;
             break;
         case NEW:
         case DESTROYED:
             throw new RuntimeException("Bug. An entry should never be in NEW or DESTROYED.");
         }
-        setBit(STICKY_BIT, entry.isSticky());
-    }
-
-    private void setBit(int bitnum, boolean val) {
-        if (val) {
-            bitmask |= 1<<bitnum ;
-        } else {
-            bitmask &= ~(1 << bitnum);
+        if (entry.isSticky()) {
+            bitmask |= 1<< STICKY_BIT;
         }
     }
 
     private boolean getBit(int bitnum) {
-        return (bitmask & ( 1 <<bitnum)) != 0;
-    }
-
-    private String getBitMaskString() {
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=31; i>=0; i--) {
-            sb.append(getBit(i) ? '1':'0');
-        }
-        return sb.toString();
+        return (bitmask & (1 << bitnum)) != 0;
     }
 
     public int getBitMask() {
@@ -121,11 +84,6 @@ public class CacheRepositoryEntryInfo implements Serializable {
     public boolean isReceivingFromStore() {
         return getBit(RECEIVINGFROMSTORE_BIT);
     }
-
-    public boolean isSendingToStore(){
-        return getBit(SENDINGTOSTORE_BIT);
-    }
-
 
     public boolean isBad() {
         return getBit(BAD_BIT);
@@ -152,12 +110,6 @@ public class CacheRepositoryEntryInfo implements Serializable {
         return lastAccessTime;
     }
 
-
-    public StorageInfo getStorageInfo()  {
-        return storageInfo;
-
-    }
-
     public PnfsId getPnfsId() {
         return pnfsId;
     }
@@ -165,22 +117,4 @@ public class CacheRepositoryEntryInfo implements Serializable {
     public long getSize() {
         return size;
     }
-
-    public static void main(String[] args) {
-        CacheRepositoryEntryInfo info = new CacheRepositoryEntryInfo();
-        System.out.printf(" bitmask = %1$S\n",info.getBitMaskString());
-        info.setBit(0,true);
-        info.setBit(1,true);
-        info.setBit(15,true);
-        System.out.printf(" bitmask = %1$S\n",info.getBitMaskString());
-        System.out.printf(" bit 0 = %1$b",info.getBit(0));
-        System.out.printf(" bit 1 = %1$b",info.getBit(1));
-        System.out.printf(" bit 3 = %1$b",info.getBit(3));
-        System.out.printf(" bit 14 = %1$b",info.getBit(14));
-        System.out.printf(" bit 15 = %1$b",info.getBit(15));
-        System.out.printf(" bit 16 = %1$b",info.getBit(16));
-
-    }
-
-
 }
