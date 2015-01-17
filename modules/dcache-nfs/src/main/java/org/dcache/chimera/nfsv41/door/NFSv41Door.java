a14c10b74ef1d974d3f1ae157b41a25a7967600b
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2015-01-17 14:41:34 +0100

    pool: update NFS transfer service to validate inactive movers
    The NFS protocol provides a mechanism to detect dead client. In dCache,
    this functionality implemented by binding mover to a nfs session on a first
    IO operation. When a pool detects expired session, the corresponding mover
    is terminated.
    
    While this mechanism works in most situations, there are at least two conner
    cases which must be covered:
      a) client never connects to a pool and, as a result, mover is not bound to
        a nfs session. In such situations, we will observe NFS movers, with
        zero bytes transfered size, which never get accessed.
    
      b) due to network errors the client my loose the connection to the pool,
      but still maintain its state with a door.  In such situation, client will assume
      that mover still exists and will attempt to use. Nevertheless, the pool may already
      killed the corresponding mover, and reject IO request with NFSERR_BAD_STATEID .
      By receiving NFSERR_BAD_STATEID the client will issue a new OPEN requests, but
      won't ask for new mover. This will endup into an infinite pool, pool will not accept
      this transfers.
    
    To cover this conditions, the nfs transfer service is update to change his behavior.
    The mover still get associated with a nfs session, but not killed when session expires.
    In case of session expires, mover get decoupled from a session, but stays active.
    A special task periodically scans for movers without sessions as queries corresponding
    door for a validity. A mover get killed only if door does not held a valid open-state.
    
    The change introduces a new message, which is generic enough to be used by other protocols
    as well.
    
    Acked-by: Gerd Behrmann
    Target: master
    (cherry picked from commit feea8e9f17069ac8f3806e6293a7e961957f2d90)
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/NFSv41Door.java
+++ modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/NFSv41Door.java
@@ -57,7 +57,6 @@ import org.dcache.chimera.FsInode;
 import org.dcache.chimera.FsInodeType;
 import org.dcache.chimera.JdbcFs;
 import org.dcache.chimera.nfsv41.door.proxy.DcapProxyIoFactory;
-import org.dcache.chimera.nfsv41.door.proxy.ProxyIoAdapter;
 import org.dcache.chimera.nfsv41.door.proxy.ProxyIoMdsOpFactory;
 import org.dcache.chimera.nfsv41.mover.NFS4ProtocolInfo;
 import org.dcache.commons.stats.RequestExecutionTimeGauges;
@@ -67,6 +66,7 @@ import org.dcache.nfs.ExportFile;
 import org.dcache.nfs.status.DelayException;
 import org.dcache.nfs.status.LayoutTryLaterException;
 import org.dcache.nfs.status.NfsIoException;
+import org.dcache.nfs.status.BadStateidException;
 import org.dcache.nfs.v3.MountServer;
 import org.dcache.nfs.v3.NfsServerV3;
 import org.dcache.nfs.v3.xdr.mount_prot;
@@ -99,6 +99,7 @@ import org.dcache.util.RedirectedTransfer;
 import org.dcache.util.Transfer;
 import org.dcache.util.TransferRetryPolicy;
 import org.dcache.utils.Bytes;
+import org.dcache.vehicles.DoorValidateMoverMessage;
 import org.dcache.xdr.OncRpcException;
 import org.dcache.xdr.OncRpcProgram;
 import org.dcache.xdr.OncRpcSvc;
@@ -374,6 +375,24 @@ public class NFSv41Door extends AbstractCellComponent implements
                 transfer.notifyBilling(transferFinishedMessage.getReturnCode(), "");
                 _vfs.invalidateStatCache(transfer.getInode());
         }
+    }
+
+    public DoorValidateMoverMessage messageArrived(DoorValidateMoverMessage<org.dcache.chimera.nfs.v4.xdr.stateid4> message) {
+        org.dcache.chimera.nfs.v4.xdr.stateid4 legacyStateid = message.getChallenge();
+        stateid4 stateid = new stateid4(legacyStateid.other, legacyStateid.seqid.value);
+
+        boolean isValid = false;
+        try {
+            NFS4Client nfsClient = _nfs4.getStateHandler().getClientIdByStateId(stateid);
+            // will throw exception if state does not exists
+            nfsClient.state(stateid);
+            isValid = true;
+        } catch (BadStateidException e) {
+        } catch (ChimeraNFSException e) {
+            _log.warn("Unexpected NFS exception: {}", e.getMessage() );
+        }
+        message.setIsValid(isValid);
+        return message;
     }
 
     private int nextDeviceID() {
--- modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/mover/NfsTransferService.java
+++ modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/mover/NfsTransferService.java
@@ -23,7 +23,6 @@ import dmg.cells.nucleus.CellCommandListener;
 import dmg.cells.nucleus.CellPath;
 import dmg.cells.nucleus.NoRouteToCellException;
 import java.net.Inet4Address;
-import java.util.Arrays;
 
 import org.dcache.cells.CellStub;
 import org.dcache.commons.stats.RequestExecutionTimeGauges;
@@ -76,7 +75,8 @@ public class NfsTransferService extends AbstractCellComponent
             portRange = new PortRange(0);
         }
 
-        _nfsIO = new NFSv4MoverHandler(portRange, _withGss, getCellName());
+        _door = new CellStub(getCellEndpoint());
+        _nfsIO = new NFSv4MoverHandler(portRange, _withGss, getCellName(), _door, _bootVerifier);
         _localSocketAddresses = localSocketAddresses(NetworkUtils.getLocalAddresses(), _nfsIO.getLocalAddress().getPort());
 
         /*
@@ -90,8 +90,6 @@ public class NfsTransferService extends AbstractCellComponent
             }
         }
         _sortMultipathList = ipv4Count > 1;
-
-        _door = new CellStub(getCellEndpoint());
     }
 
     @Required
