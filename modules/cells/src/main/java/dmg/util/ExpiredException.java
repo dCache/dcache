822e4796d61ead0588db5c2ec763936540dc493d
Gerd Behrmann <behrmann@gmail.com>
2015-04-03 12:01:44 +0200

    cells: Drop unused utility classes
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: https://rb.dcache.org/r/8078/

--- modules/cells/src/main/java/dmg/util/ExpiredException.java
+++
@@ -1,6 +0,0 @@
-package dmg.util ;
-
-public class ExpiredException extends Exception {
-   private static final long serialVersionUID = 6043689361608502931L;
-   public ExpiredException( String message ){ super( message ) ; }
-}
