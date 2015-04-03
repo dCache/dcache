39060a8dcff51add6abe1361e69a81d1a6ccd975
Gerd Behrmann <behrmann@gmail.com>
2015-04-03 02:08:16 +0200

    cells: Remove more old GUI code
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Patch: https://rb.dcache.org/r/8072/

--- modules/cells/src/main/java/dmg/cells/applets/login/DomainEventListener.java
+++
@@ -1,9 +0,0 @@
-package dmg.cells.applets.login ;
-
-public interface DomainEventListener {
-
-   public void connectionOpened( DomainConnection connection ) ;
-   public void connectionClosed( DomainConnection connection ) ;
-   public void connectionOutOfBand( DomainConnection connection ,
-                                    Object subject                ) ;
-}
--- modules/cells/src/main/java/dmg/cells/applets/spy/FrameArrivable.java
+++
@@ -1,9 +0,0 @@
-package dmg.cells.applets.spy ;
-
-import dmg.cells.services.MessageObjectFrame;
-
-public interface FrameArrivable {
-
-    void frameArrived( MessageObjectFrame frame ) ;
-
-}
