a705340941b20090358755e47e4ae4287c1c260f
Gerd Behrmann <behrmann@gmail.com>
2015-05-10 00:31:57 +0200

    dcache: Delete unused MessageEventTimer
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: https://rb.dcache.org/r/8202/

--- modules/dcache/src/main/java/diskCacheV111/util/MessageTimerEvent.java
+++
@@ -1,9 +0,0 @@
-// $Id: MessageTimerEvent.java,v 1.1 2002-01-21 09:00:06 cvs Exp $
-package diskCacheV111.util ;
-
-public interface MessageTimerEvent {
-
-    public void event( MessageEventTimer timer ,
-                       Object eventObject ,
-		       int eventType ) ;
-}
