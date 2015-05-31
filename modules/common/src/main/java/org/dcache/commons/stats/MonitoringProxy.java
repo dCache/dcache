5d24d9bfe2b0bb4d886107bbf6a1951ea9df1b7b
Gerd Behrmann <behrmann@gmail.com>
2015-05-31 14:05:49 +0200

    Revert "common: Extend execution time gauge proxy to asynchronous results"
    This reverts commit c3ebccdd4754cd969457cd393f62c900d3c2f394.

--- modules/common/src/main/java/org/dcache/commons/stats/MonitoringProxy.java
+++ modules/common/src/main/java/org/dcache/commons/stats/MonitoringProxy.java
@@ -1,8 +1,5 @@
 package org.dcache.commons.stats;
 
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.MoreExecutors;
-import com.google.common.util.concurrent.Uninterruptibles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,9 +7,6 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.concurrent.ExecutionException;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * MonitoringProxy can be used to decorate a any class implementing an interface
@@ -31,72 +25,52 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public class MonitoringProxy  <T> implements InvocationHandler {
     private static final Logger logger = LoggerFactory.getLogger(MonitoringProxy.class);
-
     private final T monitoredObject;
     private final RequestCounters<Method> counter;
     private final RequestExecutionTimeGauges<Method> gauge;
-
     private MonitoringProxy(T monitoredObject,
             RequestCounters<Method> counter,
             RequestExecutionTimeGauges<Method> gauge) {
-        this.monitoredObject = checkNotNull(monitoredObject);
-        this.counter = checkNotNull(counter);
-        this.gauge = checkNotNull(gauge);
+        this.monitoredObject = monitoredObject;
+        this.counter = counter;
+        this.gauge = gauge;
     }
 
     @Override
-    public Object invoke(Object proxy, final Method method, Object[] args)
+    public Object invoke(Object proxy, Method method, Object[] args)
             throws Throwable {
-        counter.incrementRequests(method);
-        final long startTimeStamp = System.currentTimeMillis();
-        Object result = null;
+        long startTimeStamp = System.currentTimeMillis();
+        if(counter != null) {
+            counter.incrementRequests(method);
+        }
         try {
-            result = method.invoke(monitoredObject, args);
+            Object result = method.invoke(monitoredObject, args);
+            return result;
         } catch (InvocationTargetException e) {
-            counter.incrementFailed(method);
+            if(counter != null) {
+                counter.incrementFailed(method);
+            }
             throw e.getTargetException();
-        } catch (Error | RuntimeException e) {
-            counter.incrementFailed(method);
-            throw e;
         } finally {
-            if (result instanceof ListenableFuture) {
-                final ListenableFuture future = (ListenableFuture) result;
-                future.addListener(
-                        new Runnable()
-                        {
-                            @Override
-                            public void run()
-                            {
-                                try {
-                                    Uninterruptibles.getUninterruptibly(future);
-                                } catch (ExecutionException | Error | RuntimeException e) {
-                                    counter.incrementFailed(method);
-                                }
-                                updateTime(method, startTimeStamp);
-                            }
-                        },
-                        MoreExecutors.directExecutor());
-            } else {
-                updateTime(method, startTimeStamp);
+            long execTime = System.currentTimeMillis() - startTimeStamp;
+            if (logger.isDebugEnabled()) {
+                logger.debug("invocation of "+method+" took "+execTime+" ms");
+            }
+            if(gauge != null) {
+                gauge.update(method, execTime);
             }
         }
-        return result;
     }
 
-    private void updateTime(Method method, long startTimeStamp)
-    {
-        long execTime = System.currentTimeMillis() - startTimeStamp;
-        logger.debug("invocation of {} took {} ms", method, execTime);
-        gauge.update(method, execTime);
-    }
-
-    public static <T> T decorateWithMonitoringProxy(Class<?>[] interfaces,
-                                                    T monitoringObject,
-                                                    RequestCounters<Method> counter,
-                                                    RequestExecutionTimeGauges<Method> gauge) {
-        MonitoringProxy <T> monitoringHandler = new MonitoringProxy<>(monitoringObject,counter,gauge);
+    public static <T> T decorateWithMonitoringProxy( Class<?>[] interfaces,
+            T monitoringObject,
+            RequestCounters<Method> counter,
+            RequestExecutionTimeGauges<Method> gauge) {
+        MonitoringProxy <T> monitoringHandler = new MonitoringProxy<>(
+                monitoringObject,counter,gauge);
         return (T) Proxy.newProxyInstance(monitoringObject.getClass().getClassLoader(),
-                                          interfaces, monitoringHandler);
+                                                         interfaces,
+                                                          monitoringHandler);
 
 
     }
