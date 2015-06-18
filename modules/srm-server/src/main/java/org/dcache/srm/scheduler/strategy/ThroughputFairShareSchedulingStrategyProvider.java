91fa27bb2b107e208575017ea54afc72b730f50c
Gerd Behrmann <behrmann@gmail.com>
2015-06-18 09:52:38 +0200

    srm: Reimplement scheduling policy to resolve scalability issues
    Motivation:
    
    The srm has a pluggable scheduling policy system, but the policy is based on
    the plugin assigning a dynamically allocated priority for each job. Thus this
    requires reevaluating the pririty for all queued jobs whenever a new request
    has to be sheduld. With O(n^2) complexity, this does not scale to large request
    queues.
    
    Previous patches have added various minor improvements without fixing the
    core issues.
    
    Modification:
    
    The existing plugin system is ripped out and replaced with the new scheduling
    strategy SPI. The new interface embeds the entire queuing into the plugin,
    which thus is free to use an appropriate internal representation.
    
    Several plugins are provided by dCache. The default one, inprogress-fair-share,
    has a similar goal to the old one, but without the need to configure a per user
    job soft limit. Two fair share, a fifo and a lifo strategy are provided. The
    two fair share strategies have configurable discriminators (which themselves
    are pluggable through a SPI) to allow the grouping to be changed (e.g.
    fair share by DN or by FQAN).
    
    I deliberately avoided Java 8 constructs, and also left the existing uses of
    ModifiableQueue in place. Subsequent patches for master will refactor this
    further.
    
    Result:
    
    Fixes #1533. Obsoletes srm.request.max-by-same-user and per request-type
    variants of this property. Introduces srm.plugins.scheduler,
    srm.plugins.discriminator, srm.scheduler.inprogress-fair-share!discriminator,
    and srm.scheduler.throughput-fair-share!discriminator.
    
    Given the severity of the scalability issue, I request this patch to be
    backported. Although large, the changes to the existing code are minor.
    Initially I request backport to 2.13 and once field tested I request it for
    older versions too.
    
    Target: trunk
    Request: 2.13
    Require-notes: yes
    Require-book: yes
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: https://rb.dcache.org/r/8334/
    (cherry picked from commit 214963f393ef089ad9cf5a1102c7f377e7b1b832)
    (cherry picked from commit 5af2eb93e62b6117a665f0f85138d05e614a9cfb)

---
+++ modules/srm-server/src/main/java/org/dcache/srm/scheduler/strategy/ThroughputFairShareSchedulingStrategyProvider.java
@@ -0,0 +1,47 @@
+/* dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2015 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.srm.scheduler.strategy;
+
+import java.util.Map;
+
+import org.dcache.srm.scheduler.Scheduler;
+import org.dcache.srm.scheduler.spi.SchedulingStrategy;
+import org.dcache.srm.scheduler.spi.SchedulingStrategyProvider;
+
+public class ThroughputFairShareSchedulingStrategyProvider implements SchedulingStrategyProvider
+{
+    private String discriminator;
+
+    @Override
+    public String getName()
+    {
+        return "throughput-fair-share";
+    }
+
+    @Override
+    public void setConfiguration(Map<String, String> configuration)
+    {
+        discriminator = configuration.get("discriminator");
+    }
+
+    @Override
+    public SchedulingStrategy createStrategy(Scheduler scheduler)
+    {
+        return new ThroughputFairShareSchedulingStrategy(discriminator);
+    }
+}
