dCache/dcache

commit 633308bb56a8fa3d43b8325c7a88b42ec41f0d85

srm,dcap: Respect cadir and vomsdir settings
Motivation:

A recent upgrade of the VOMS library used by dCache left the cadir and vomsdir
configuraion to be ignored by SRM and DCAP services.

Modification:

Restore the use of these properties. Relocate relevant utility classes to avoid
that specific doors have direct dependencies on gplazma plugins.

Result:

The services once again use the configured cadir and vomsdir.

Target: trunk
Request: 2.13
Request: 2.12
Request: 2.11
Request: 2.10
Require-notes: yes
Require-book: no
Acked-by: Paul Millar <paul.millar@desy.de>
Patch: https://rb.dcache.org/r/8473/
(cherry picked from commit 0f77a7a700ea8351b251e0ab15364f4f5baae7dc)

modules/dcache-ftp/src/main/java/org/dcache/ftp/door/GsiFtpDoorV1.java
@@ -23,18 +23,16 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
-import diskCacheV111.doors.FTPTransactionLog;
 import diskCacheV111.util.CacheException;
 import diskCacheV111.util.PermissionDeniedCacheException;
 
 import org.dcache.auth.LoginNamePrincipal;
 import org.dcache.auth.Subjects;
-import org.dcache.auth.util.X509Utils;
 import org.dcache.cells.Option;
 import org.dcache.gplazma.AuthenticationException;
+import org.dcache.gplazma.util.X509Utils;
 import org.dcache.util.CertificateFactories;
 import org.dcache.util.Crypto;
-import org.dcache.util.NetLoggerBuilder;
 
 import static java.util.Arrays.asList;
 

modules/gplazma2-grid/pom.xml
@@ -35,11 +35,6 @@
     </dependency>
     <dependency>
         <groupId>org.dcache</groupId>
-        <artifactId>gplazma2-gsi</artifactId>
-        <version>${project.version}</version>
-    </dependency>
-    <dependency>
-        <groupId>org.dcache</groupId>
         <artifactId>gplazma2</artifactId>
         <version>${project.version}</version>
     </dependency>

modules/gplazma2-grid/src/main/java/org/dcache/gplazma/plugins/X509Plugin.java
@@ -7,8 +7,8 @@ import java.security.cert.CertPath;
 import java.util.Properties;
 import java.util.Set;
 
-import org.dcache.auth.util.X509Utils;
 import org.dcache.gplazma.AuthenticationException;
+import org.dcache.gplazma.util.X509Utils;
 import org.dcache.util.CertPaths;
 
 import static org.dcache.gplazma.util.Preconditions.checkAuthentication;

modules/gplazma2-voms/pom.xml
@@ -28,11 +28,6 @@
     </dependency>
     <dependency>
         <groupId>org.dcache</groupId>
-        <artifactId>gplazma2-gsi</artifactId>
-        <version>${project.version}</version>
-    </dependency>
-    <dependency>
-        <groupId>org.dcache</groupId>
         <artifactId>gplazma2</artifactId>
         <version>${project.version}</version>
     </dependency>

modules/gplazma2-xacml/pom.xml
@@ -54,11 +54,6 @@
         </dependency>
         <dependency>
             <groupId>org.dcache</groupId>
-            <artifactId>gplazma2-gsi</artifactId>
-            <version>${project.version}</version>
-        </dependency>
-        <dependency>
-            <groupId>org.dcache</groupId>
             <artifactId>gplazma2</artifactId>
             <version>${project.version}</version>
         </dependency>

modules/gplazma2-xacml/src/main/java/org/dcache/gplazma/plugins/XACMLPlugin.java
@@ -19,6 +19,8 @@ import org.opensciencegrid.authz.xacml.common.XACMLConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.security.auth.x500.X500Principal;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketException;
@@ -34,13 +36,12 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import javax.security.auth.x500.X500Principal;
 
 import org.dcache.auth.LoginGidPrincipal;
 import org.dcache.auth.LoginNamePrincipal;
 import org.dcache.auth.UserNamePrincipal;
-import org.dcache.auth.util.X509Utils;
 import org.dcache.gplazma.AuthenticationException;
+import org.dcache.gplazma.util.X509Utils;
 import org.dcache.util.CertPaths;
 import org.dcache.util.NetworkUtils;
 

modules/gplazma2/src/main/java/org/dcache/gplazma/util/GSSUtils.java
@@ -1,6 +1,7 @@
-package org.dcache.auth.util;
+package org.dcache.gplazma.util;
 
 
+import eu.emi.security.authn.x509.X509CertChainValidatorExt;
 import org.globus.gsi.gssapi.GSSConstants;
 import org.globus.gsi.gssapi.auth.AuthorizationException;
 import org.gridforum.jgss.ExtendedGSSContext;
@@ -16,6 +17,11 @@ import java.util.Set;
 
 import org.italiangrid.voms.VOMSAttribute;
 import org.italiangrid.voms.VOMSValidators;
+import org.italiangrid.voms.store.VOMSTrustStore;
+import org.italiangrid.voms.store.VOMSTrustStores;
+import org.italiangrid.voms.util.CertificateValidatorBuilder;
+
+import static java.util.Arrays.asList;
 
 /**
  * Extraction and conversion methods useful when dealing with GSS/VOMS
@@ -29,8 +35,6 @@ import org.italiangrid.voms.VOMSValidators;
  */
 public class GSSUtils {
 
-    static final String SYS_VOMSDIR = "VOMSDIR";
-    static final String SYS_CADIR = "CADIR";
     static final String CAPNULL = "/Capability=NULL";
     static final String ROLENULL = "/Role=NULL";
 
@@ -77,7 +81,9 @@ public class GSSUtils {
 
     public static Iterable<String> extractFQANs(String vomsDir, String caDir, X509Certificate[] chain)
     {
-        VOMSACValidator validator = VOMSValidators.newValidator();
+        VOMSTrustStore vomsTrustStore = VOMSTrustStores.newTrustStore(asList(vomsDir));
+        X509CertChainValidatorExt certChainValidator = new CertificateValidatorBuilder().trustAnchorsDir(caDir).build();
+        VOMSACValidator validator = VOMSValidators.newValidator(vomsTrustStore, certChainValidator);
         List<VOMSAttribute> listOfAttributes = validator.validate(chain);
         return getFQANSfromVOMSAttributes(listOfAttributes);
     }

modules/gplazma2/src/main/java/org/dcache/gplazma/util/X509Utils.java
@@ -1,4 +1,4 @@
-package org.dcache.auth.util;
+package org.dcache.gplazma.util;
 
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.ASN1Set;

commit 8a0cb806b0e99c5af33a7f6d2588e2028899ff87

ftp: Add missing import
Fixes a regression introduced in 633308bb56a8fa3d43b8325c7a88b42ec41f0d85.

modules/dcache-ftp/src/main/java/org/dcache/ftp/door/GsiFtpDoorV1.java
@@ -23,6 +23,7 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
+import diskCacheV111.doors.FTPTransactionLog;
 import diskCacheV111.util.CacheException;
 import diskCacheV111.util.PermissionDeniedCacheException;
 