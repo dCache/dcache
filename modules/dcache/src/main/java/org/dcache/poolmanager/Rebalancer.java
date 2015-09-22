d5714c02be88c9294af2aef9b01ca30040bc14bc
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2015-09-22 12:13:30 +0200

    poolmanager: implementing annotated command syntax for admin commands (Rebalancer)
    Motivation:
    Documentation of all admin commands in Rebalancer by implementing the command infrastructure.
    
    Modification & Result:
    Implemenmted the new command infrastructure to these following commands:
    	1. rebalance pgroup
    	2. rebalance cancel pgroup
    for better documentation of the admin interface commands.
    
    Acked-by: Gerd Behrmann <behrmann@ndgf.org>
    Target: trunk
    Require-book: no
    Require-notes: no
    Patch: https://rb.dcache.org/r/8610/

--- modules/dcache/src/main/java/org/dcache/poolmanager/Rebalancer.java
+++ modules/dcache/src/main/java/org/dcache/poolmanager/Rebalancer.java
@@ -5,6 +5,8 @@ import com.google.common.base.Joiner;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.concurrent.Callable;
 
 import diskCacheV111.poolManager.CostModule;
 import diskCacheV111.poolManager.PoolSelectionUnit;
@@ -16,9 +18,12 @@ import diskCacheV111.util.CacheException;
 import dmg.cells.nucleus.CellCommandListener;
 import dmg.cells.nucleus.CellPath;
 
+import dmg.util.command.Argument;
+import dmg.util.command.Command;
+import dmg.util.command.Option;
+
 import org.dcache.cells.CellStub;
 import org.dcache.pool.migration.PoolMigrationJobCancelMessage;
-import org.dcache.util.Args;
 
 /**
  * Implements commands to generate migration jobs to rebalance pools.
@@ -60,124 +65,128 @@ public class Rebalancer
         }
     }
 
-    public static final String hh_rebalance_pgroup =
-        "[-metric=relative|free] [-refresh=<period>] <pgroup>";
-    public static final String fh_rebalance_pgroup =
-        "Moves files between pools of a pool group to balance space usage.\n\n" +
-
-        "A migration job will be submitted to each pool in the pool group.\n" +
-        "The combined effect of these migration jobs is to move files until\n" +
-        "either the relative space usage (used space relative to the total\n" +
-        "size of the pool) is the same or the space cost is the same; which\n" +
-        "depends on the metric used. The default is balance relative space\n" +
-        "usage.\n\n" +
-
-        "A pool can only  be the source of  one rebalance  run at a time.\n" +
-        "Previous  rebalancing jobs  will be cancelled.  The PoolManager\n" +
-        "maintains no state for the rebalancing job, however migration jobs\n" +
-        "created by the rebalancer have a well known name (rebalance).\n\n" +
-
-        "Migration jobs periodically query PoolManager about how much space\n" +
-        "is used on each pool and about the space cost. There will thus be\n" +
-        "a delay between  files being moved between pools  and the metric\n" +
-        "being updated. It is expected that rebalancing jobs will overshoot\n" +
-        "the target slightly. For very small pools on test instances this\n" +
-        "effect will be more profound than on large pools. The effect can\n" +
-        "be reduced by specifying a shorter refresh period via the refresh\n" +
-        "option, which accepts an integer number of seconds. The default\n" +
-        "period is 30 seconds.\n\n" +
-
-        "The migration jobs created by the rebalancer will not survive a\n" +
-        "pool restart. If the lots of files are written, deleted or moved\n" +
-        "while the rebalancing job runs, then the pool group may not be\n" +
-        "completely balanced when the jobs terminate. Run the rebalancer\n" +
-        "a second time to improve the balance further.\n\n" +
-
-        "If 'relative' metric is used then files are moved around so that\n" +
-        "pools in the poolgroup have about the same fractional usage (e.g.,\n" +
-        "each pool is 30% full).  If pools have different capacities then\n" +
-        "bigger pools will have more free space.\n\n" +
-
-        "If metric is 'free' then files are moved around so that pools in\n" +
-        "the poolgroup have about the same amount of free space.  If pools\n" +
-        "have different capacities then bigger pools will store a larger\n" +
-        "fraction of the files.\n\n" +
-
-        "By default the 'relative' metric is used.  If all pools in the\n" +
-        "poolgroup have identical capacities then the metric used does not\n" +
-        "matter.";
-    public String ac_rebalance_pgroup_$_1(Args args)
-        throws CacheException, InterruptedException
+    @Command(name = "rebalance pgroup",
+            hint = "rearrange files to balance space usage",
+            description = "A migration job will be submitted to each pool in the pool group. " +
+                    "The combined effect of these migration jobs is to move files until " +
+                    "either the relative space usage (used space relative to the total " +
+                    "size of the pool) is the same or the space cost is the same; which " +
+                    "depends on the metric used. The default is balance relative space " +
+                    "usage.\n\n" +
+                    "A pool can only be the source of one rebalance run at a time. " +
+                    "Previous rebalancing jobs will be cancelled. The PoolManager " +
+                    "maintains no state for the rebalancing job, however migration jobs " +
+                    "created by the rebalancer have a well known name (rebalance).\n\n" +
+                    "Migration jobs periodically query PoolManager about how much space " +
+                    "is used on each pool and about the space cost. There will thus be " +
+                    "a delay between files being moved between pools and the metric " +
+                    "being updated. It is expected that rebalancing jobs will overshoot " +
+                    "the target slightly. For very small pools on test instances this " +
+                    "effect will be more profound than on large pools. The effect can " +
+                    "be reduced by specifying a shorter refresh period via the refresh " +
+                    "option, which accepts an integer number of seconds. The default " +
+                    "period is 30 seconds.\n\n" +
+                    "The migration jobs created by the rebalancer will not survive a " +
+                    "pool restart. If the lots of files are written, deleted or moved " +
+                    "while the rebalancing job runs, then the pool group may not be " +
+                    "completely balanced when the jobs terminate. Run the rebalancer " +
+                    "a second time to improve the balance further.\n\n" +
+                    "If 'relative' metric is used then files are moved around so that " +
+                    "pools in the poolgroup have about the same fractional usage (e.g., " +
+                    "each pool is 30% full). If pools have different capacities then " +
+                    "bigger pools will have more free space.\n\n" +
+                    "If metric is 'free' then files are moved around so that pools in " +
+                    "the poolgroup have about the same amount of free space. If pools " +
+                    "have different capacities then bigger pools will store a larger " +
+                    "fraction of the files.\n\n" +
+                    "By default the 'relative' metric is used.  If all pools in the " +
+                    "poolgroup have identical capacities then the metric used does not " +
+                    "matter.")
+    public class RebalancePgroupCommand implements Callable<String>
     {
-        String metric = args.getOpt("metric");
-        String refresh = args.getOpt("refresh");
-        String poolGroup = args.argv(0);
-        int period = (refresh == null) ? 30 : Integer.parseInt(refresh);
+        @Argument(usage = "The name of the pool group to balance.")
+        String poolGroup;
 
-        if (metric == null) {
-            metric = METRIC_RELATIVE;
-        }
+        @Option(name = "metric", values = {"relative","free"})
+        String metric = METRIC_RELATIVE;
 
-        long used = 0;
-        long total = 0;
-        Collection<SelectionPool> pools = new ArrayList<>();
-        Collection<String> names = new ArrayList<>();
-        for (SelectionPool pool: _psu.getPoolsByPoolGroup(poolGroup)) {
-            PoolCostInfo cost = _cm.getPoolCostInfo(pool.getName());
-            if (pool.getPoolMode().isEnabled() && cost != null) {
-                PoolSpaceInfo spaceInfo = cost.getSpaceInfo();
-                used += spaceInfo.getUsedSpace();
-                total += spaceInfo.getTotalSpace();
-                pools.add(pool);
-                names.add(pool.getName());
+        @Option(name="refresh", metaVar="seconds")
+        int period = 30;
+
+        @Override
+        public String call() throws CacheException, InterruptedException,
+                NoSuchElementException, IllegalArgumentException
+        {
+            long used = 0;
+            long total = 0;
+            Collection<SelectionPool> pools = new ArrayList<>();
+            Collection<String> names = new ArrayList<>();
+            for (SelectionPool pool: _psu.getPoolsByPoolGroup(poolGroup)) {
+                PoolCostInfo cost = _cm.getPoolCostInfo(pool.getName());
+                if (pool.getPoolMode().isEnabled() && cost != null) {
+                    PoolSpaceInfo spaceInfo = cost.getSpaceInfo();
+                    used += spaceInfo.getUsedSpace();
+                    total += spaceInfo.getTotalSpace();
+                    pools.add(pool);
+                    names.add(pool.getName());
+                }
             }
-        }
 
-        String command;
-        switch (metric) {
-        case METRIC_RELATIVE:
-            double factor = (double) used / (double) total;
-            command =
-                    String.format(Locale.US, "migration move -id=%s -include-when='target.used < %2$f * target.total' -stop-when='targets == 0 or source.used <= %2$f * source.total' -refresh=%3$d %4$s",
-                            JOB_NAME, factor, period, Joiner.on(" ").join(names));
-            break;
-        case METRIC_FREE_COST:
-            command =
-                    String.format(Locale.US, "migration move -id=%s -include-when='target.free > source.free' -stop-when='targets == 0' -refresh=%d %s",
-                            JOB_NAME, period, Joiner.on(" ").join(names));
-            break;
-        default:
-            throw new IllegalArgumentException("Unsupported value for -metric: " + metric);
-        }
-
-        cancelAll(pools);
-
-        boolean success = false;
-        try {
-            // TODO: Use SpreadAndWait
-            for (SelectionPool pool: pools) {
-                _poolStub.sendAndWait(new CellPath(pool.getName()), command,
-                                      String.class);
+            String command;
+            switch (metric) {
+                case METRIC_RELATIVE:
+                    double factor = (double) used / (double) total;
+                    command =
+                            String.format(Locale.US, "migration move -id=%s -include-when='target.used " +
+                                            "< %2$f * target.total' -stop-when='targets == 0 or source." +
+                                            "used <= %2$f * source.total' -refresh=%3$d %4$s",
+                                    JOB_NAME, factor, period, Joiner.on(" ").join(names));
+                    break;
+                case METRIC_FREE_COST:
+                    command =
+                            String.format(Locale.US, "migration move -id=%s -include-when='target.free > " +
+                                            "source.free' -stop-when='targets == 0' -refresh=%d %s",
+                                    JOB_NAME, period, Joiner.on(" ").join(names));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported value for -metric: " + metric);
             }
-            success = true;
-        } finally {
-            if (!success) {
-                cancelAll(pools);
-            }
-        }
 
-        return "Rebalancing jobs have been submitted to " +
-            Joiner.on(", ").join(names) + ".";
+            cancelAll(pools);
+
+            boolean success = false;
+            try {
+                // TODO: Use SpreadAndWait
+                for (SelectionPool pool: pools) {
+                    _poolStub.sendAndWait(new CellPath(pool.getName()), command,
+                            String.class);
+                }
+                success = true;
+            } finally {
+                if (!success) {
+                    cancelAll(pools);
+                }
+            }
+
+            return "Rebalancing jobs have been submitted to " +
+                    Joiner.on(", ").join(names) + ".";
+        }
     }
 
-    public static final String hh_rebalance_cancel_pgroup =
-        "<pgroup>";
-    public static final String fh_rebalance_cancel_pgroup =
-        "Cancels migration jobs created by the rebalancer.";
-    public String ac_rebalance_cancel_pgroup_$_1(Args args)
-        throws CacheException, InterruptedException
+    @Command(name = "rebalance cancel pgroup",
+            hint = "cancel rebalancing operation",
+            description = "Cancels migration jobs created by the rebalancer.")
+    public class rebalance_cancel_pgroupCommand implements Callable<String>
     {
-        cancelAll(_psu.getPoolsByPoolGroup(args.argv(0)));
-        return "";
+        @Argument(usage = "The name of the pool group.")
+        String poolGroup;
+
+        @Override
+        public String call() throws CacheException, InterruptedException,
+                NoSuchElementException
+        {
+            cancelAll(_psu.getPoolsByPoolGroup(poolGroup));
+            return "";
+        }
     }
 }
