3b70e4ef4f5a2e5b9c15632cd1259f4499f88012
Anupam Ashish <anupamashish@outlook.com>
2015-12-09 14:38:12 +0100

    dcache-webdav: moved logging functionality from Milton filter to its own handler
    Motivation:
    
    The addition of more handlers to the webdav.xml, e.g. webdav dos handler, requires the authentication to happen outside of MiltonHandler. Currently, the MiltonHandler provides the logging of all requests, handling of authentication etc. This is the first step to get the functionalities that are not specific to Milton out of its filter.
    
    Modification:
    
    Deleted the org.dcache.webdav.LoggingFilter.java and moved the functionality with slight adaptation to org.dcache.webdav.LoggingHandler.java
    
    Ticket:
    Acked-by: Paul Millar <paul.millar@desy.de>
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    Target: trunk
    Require-book: no
    Require-notes: no
    Patch: https://rb.dcache.org/r/8861/

---
+++ modules/dcache-webdav/src/main/java/org/dcache/webdav/LoggingHandler.java
@@ -0,0 +1,105 @@
+package org.dcache.webdav;
+
+import com.google.common.net.InetAddresses;
+import dmg.cells.nucleus.CDC;
+import org.dcache.util.NetLoggerBuilder;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.server.handler.HandlerWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.security.auth.Subject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.security.cert.X509Certificate;
+
+import static org.dcache.webdav.DcacheResourceFactory.TRANSACTION_ATTRIBUTE;
+import static org.dcache.webdav.SecurityFilter.DCACHE_SUBJECT_ATTRIBUTE;
+
+
+public class LoggingHandler extends HandlerWrapper {
+
+    private final Logger ACCESS_LOGGER =
+            LoggerFactory.getLogger("org.dcache.access.webdav");
+
+    private static final String X509_CERTIFICATE_ATTRIBUTE =
+            "javax.servlet.request.X509Certificate";
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        if (isStarted() && !baseRequest.isHandled()) {
+            super.handle(target, baseRequest, request, response);
+
+            int statusCode = response.getStatus();
+
+            NetLoggerBuilder log = new NetLoggerBuilder(logLevel(statusCode),
+                    "org.dcache.webdav.request").omitNullValues();
+            log.add("session", CDC.getSession());
+            log.add("transaction", getTransaction(request));
+            log.add("request.method", request.getMethod());
+            log.add("request.url", request.getRequestURL());
+            log.add("response.code", statusCode);
+            log.add("response.reason", getReason(response));
+            log.add("location", response.getHeader("Location"));
+            HttpServletRequest r = request;
+            InetAddress addr = InetAddresses.forString(r.getRemoteAddr());
+            log.add("user-agent", request.getHeader("User-Agent"));
+
+            log.add("user.dn", getCertificateName(request));
+            log.add("user.mapped", getSubject(request));
+            log.toLogger(ACCESS_LOGGER);
+        }
+    }
+
+    private static String getReason(HttpServletResponse response)
+    {
+        if (response instanceof Response) {
+            return ((Response) response).getReason();
+        } else {
+            return HttpStatus.getMessage(response.getStatus());
+        }
+    }
+
+    private static NetLoggerBuilder.Level logLevel(int code) {
+        if (code >= 500) {
+            return NetLoggerBuilder.Level.ERROR;
+        } else if (code >= 400) {
+            return NetLoggerBuilder.Level.WARN;
+        } else {
+            return NetLoggerBuilder.Level.INFO;
+        }
+    }
+
+    private static String getCertificateName(HttpServletRequest request) {
+
+        Object object = request.getAttribute(X509_CERTIFICATE_ATTRIBUTE);
+
+        if (object instanceof X509Certificate[]) {
+            X509Certificate[] chain = (X509Certificate[]) object;
+
+            if (chain.length >= 1) {
+                return chain[0].getSubjectX500Principal().getName();
+            }
+        }
+
+        return null;
+    }
+
+    private static String getTransaction(HttpServletRequest request) {
+        Object object = request.getAttribute(TRANSACTION_ATTRIBUTE);
+
+        return object == null ? null : String.valueOf(object);
+    }
+
+    private static Subject getSubject(HttpServletRequest request) {
+        Object object = request.getAttribute(DCACHE_SUBJECT_ATTRIBUTE);
+
+        return (object instanceof Subject) ? (Subject) object : null;
+    }
+}
