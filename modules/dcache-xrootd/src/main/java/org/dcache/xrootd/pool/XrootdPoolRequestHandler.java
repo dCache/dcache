9aedb72841d1cbc19c4021ac3103bc72aeba4663
Gerd Behrmann <behrmann@gmail.com>
2016-01-07 22:06:38 +0100

    pool: Upgrade xrootd4j to 3.0.2
    Motivation:
    
    The pool suffers from xrootd buffer leaks and lacks support for endsess.
    
    Modification:
    
    Upgrade xrootd4j and implement the buffer leaks fixes in our custom
    chunked responses.
    
    Also add a noop implementation of endsess to avoid spamming the logs -
    pools automatically ends the session when the client disconnects.
    
    Result:
    
    Fixes buffer leaks in the xrootd mover.
    
    Target: trunk
    Request: 2.14
    Request: 2.13
    Request: 2.12
    Require-notes: yes
    Require-book: no
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: https://rb.dcache.org/r/8923/
    (cherry picked from commit 5c35fcdc481787c48ed03ee8e7d67024ff3b9c3a)

--- modules/dcache-xrootd/src/main/java/org/dcache/xrootd/pool/XrootdPoolRequestHandler.java
+++ modules/dcache-xrootd/src/main/java/org/dcache/xrootd/pool/XrootdPoolRequestHandler.java
@@ -54,6 +54,7 @@ import org.dcache.xrootd.protocol.XrootdProtocol;
 import org.dcache.xrootd.protocol.messages.AuthenticationRequest;
 import org.dcache.xrootd.protocol.messages.CloseRequest;
 import org.dcache.xrootd.protocol.messages.DirListRequest;
+import org.dcache.xrootd.protocol.messages.EndSessionRequest;
 import org.dcache.xrootd.protocol.messages.GenericReadRequestMessage.EmbeddedReadRequest;
 import org.dcache.xrootd.protocol.messages.LoginRequest;
 import org.dcache.xrootd.protocol.messages.MkDirRequest;
@@ -610,6 +611,12 @@ public class XrootdPoolRequestHandler extends AbstractXrootdRequestHandler
         default:
             return unsupported(ctx, msg);
         }
+    }
+
+    @Override
+    protected Object doOnEndSession(ChannelHandlerContext ctx, EndSessionRequest request) throws XrootdException
+    {
+        return withOk(request);
     }
 
     /**
