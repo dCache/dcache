1c4b9ea55d382b8a0d024bbb8e6296f86740b5e6
Paul Millar <paul.millar@desy.de>
2016-01-12 17:34:25 +0100

    voms: add diagnostic information if validation fails
    Motivation:
    
    Allow the admin to distinguish easily between a client authenticating
    with no attribute certificate (a non-voms proxy) and a client
    authenticating with attribute certificate(s) that do not validate.
    
    Also provide sufficient information to allow the admin to diagnose why
    an attribute certificate failed to validate.
    
    Modification:
    
    The descriptive message, included in AuthenticationMessage now
    distingishes between no attribute certificate ("no FQANs") and some
    validation error ("validation failure").  Unfortunately the VOMS library
    is rather verbose in describing such errors, so its output cannot be
    included directly in the AuthenticationMessage.  Instead, each
    validation error is logged with a corresponding id and the id(s) are
    described in the AuthenticationMessage.
    
    The plugin is considered successful if at least one FQAN is extracted,
    independent of other validation errors; however, all validation errors
    are logged.
    
    Result:
    
    Validation errors are obvious in log file and (in most cases) in the
    LoginResultPrinter.  The admin should have sufficient information to
    understand the issue.
    
    Target: master
    Requires-notes: yes
    Requires-book: yes
    Patch: https://rb.dcache.org/r/8930/
    Acked-by: Gerd Behrmann

--- modules/gplazma2-voms/src/main/java/org/dcache/gplazma/plugins/VomsPlugin.java
+++ modules/gplazma2-voms/src/main/java/org/dcache/gplazma/plugins/VomsPlugin.java
@@ -4,17 +4,22 @@ import eu.emi.security.authn.x509.X509CertChainValidatorExt;
 import org.italiangrid.voms.VOMSAttribute;
 import org.italiangrid.voms.VOMSValidators;
 import org.italiangrid.voms.ac.VOMSACValidator;
+import org.italiangrid.voms.ac.VOMSValidationResult;
 import org.italiangrid.voms.store.VOMSTrustStore;
 import org.italiangrid.voms.store.VOMSTrustStores;
 import org.italiangrid.voms.util.CertificateValidatorBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.security.Principal;
 import java.security.cert.CRLException;
 import java.security.cert.CertPath;
 import java.security.cert.CertificateException;
+import java.util.Base64;
 import java.util.List;
 import java.util.Properties;
+import java.util.Random;
 import java.util.Set;
 
 import org.dcache.auth.FQANPrincipal;
@@ -29,13 +34,16 @@ import static org.dcache.gplazma.util.Preconditions.checkAuthentication;
  * Validates and extracts FQANs from any X509Certificate certificate chain in
  * the public credentials.
  */
-public class VomsPlugin implements GPlazmaAuthenticationPlugin {
+public class VomsPlugin implements GPlazmaAuthenticationPlugin
+{
+    private static final Logger LOG = LoggerFactory.getLogger(VomsPlugin.class);
 
     private static final String CADIR = "gplazma.vomsdir.ca";
     private static final String VOMSDIR = "gplazma.vomsdir.dir";
     private final String caDir;
     private final String vomsDir;
     private VOMSACValidator validator;
+    private final Random random = new Random();
 
     public VomsPlugin(Properties properties)
             throws CertificateException, CRLException, IOException
@@ -64,25 +72,47 @@ public class VomsPlugin implements GPlazmaAuthenticationPlugin {
     public void authenticate(Set<Object> publicCredentials,
                     Set<Object> privateCredentials,
                     Set<Principal> identifiedPrincipals)
-                    throws AuthenticationException {
+                    throws AuthenticationException
+    {
         boolean primary = true;
         boolean hasX509 = false;
         boolean hasFQANs = false;
+        String ids = null;
+        boolean multipleIds = false;
 
         for (Object credential : publicCredentials) {
             if (CertPaths.isX509CertPath(credential)) {
                 hasX509 = true;
-                List<VOMSAttribute> attrs = validator.validate(CertPaths.getX509Certificates((CertPath) credential));
-                for (VOMSAttribute attr : attrs) {
-                    for (String fqan : attr.getFQANs()) {
-                        hasFQANs = true;
-                        identifiedPrincipals.add(new FQANPrincipal(fqan, primary));
-                        primary = false;
+                List<VOMSValidationResult> results = validator.validateWithResult(CertPaths.getX509Certificates((CertPath) credential));
+                for (VOMSValidationResult result : results) {
+                    if (result.isValid()) {
+                        VOMSAttribute attr = result.getAttributes();
+
+                        for (String fqan : attr.getFQANs()) {
+                            hasFQANs = true;
+                            identifiedPrincipals.add(new FQANPrincipal(fqan, primary));
+                            primary = false;
+                        }
+                    } else {
+                        byte[] rawId = new byte[3]; // a Base64 char represents 6 bits; 4 chars represent 3 bytes.
+                        random.nextBytes(rawId);
+                        String id = Base64.getEncoder().withoutPadding().encodeToString(rawId);
+                        LOG.warn("Validation failure {}: {}", id, result.getValidationErrors());
+                        if (ids == null) {
+                            ids = id;
+                        } else {
+                            ids = ids + ", " + id;
+                            multipleIds = true;
+                        }
                     }
                 }
             }
         }
 
+        if (ids != null && !hasFQANs) {
+            String failure = multipleIds ? "failures" : "failure";
+            throw new AuthenticationException("validation " + failure + ": " + ids);
+        }
         checkAuthentication(hasX509, "no X509 certificate chain");
         checkAuthentication(hasFQANs, "no FQANs");
     }
