dCache/dcache

commit a8edc2642417aee14ae7944956d5fdedc2cfee1b

dcache-webdav: Moved handling of authentication from Milton Security Filter to it separate handler
Motivation:

Currently, the authentication of incoming requests to Webdav module occurs in the Security Filter of the Milton Handler. In order to make the authentication also available to other handlers, it was important that it happens outside of the filter chain.

Ticket:
Acked-by: Paul Millar <paul.millar@desy.de>
Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
Target: trunk
Require-book: no
Require-notes: no
Patch: https://rb.dcache.org/r/8870/

modules/dcache-webdav/src/main/java/org/dcache/webdav/AuthenticationHandler.java
@@ -1,0 +1,302 @@
+package org.dcache.webdav;
+
+import com.google.common.base.Throwables;
+import diskCacheV111.util.CacheException;
+import diskCacheV111.util.FsPath;
+import diskCacheV111.util.PermissionDeniedCacheException;
+import org.dcache.auth.LoginReply;
+import org.dcache.auth.LoginStrategy;
+import org.dcache.auth.Origin;
+import org.dcache.auth.Subjects;
+import org.dcache.auth.LoginNamePrincipal;
+import org.dcache.auth.PasswordCredential;
+import org.dcache.auth.attributes.HomeDirectory;
+import org.dcache.auth.attributes.LoginAttribute;
+import org.dcache.auth.attributes.ReadOnly;
+import org.dcache.auth.attributes.RootDirectory;
+import org.dcache.util.NetLoggerBuilder;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.HandlerWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.security.auth.Subject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.security.PrivilegedAction;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
+
+import static java.util.Arrays.asList;
+
+public class AuthenticationHandler extends HandlerWrapper {
+
+    private final static Logger LOG = LoggerFactory.getLogger(AuthenticationHandler.class);
+    public static final String X509_CERTIFICATE_ATTRIBUTE =
+            "javax.servlet.request.X509Certificate";
+    public static final String DCACHE_SUBJECT_ATTRIBUTE =
+            "org.dcache.subject";
+
+    private String _realm;
+    private boolean _isReadOnly;
+    private boolean _isBasicAuthenticationEnabled;
+    private LoginStrategy _loginStrategy;
+    private FsPath _rootPath = new FsPath();
+
+    private CertificateFactory _cf;
+    private FsPath _uploadPath;
+
+    public void setCertificateFactory(CertificateFactory _cf) {
+        this._cf = _cf;
+    }
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        if (isStarted() && !baseRequest.isHandled()) {
+            Subject subject = new Subject();
+
+            if (!isReadMethodsAllowed(request.getMethod())) {
+                LOG.debug("Failing {} from {} as door is read-only",
+                        request.getMethod(), request.getRemoteAddr());
+                response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                return;
+            }
+
+            try {
+                addX509ChainToSubject(request, subject);
+                addOriginToSubject(request, subject);
+                addPasswordCredentialToSubject(request, subject);
+
+                LoginReply login = _loginStrategy.login(subject);
+                subject = login.getSubject();
+
+                request.setAttribute(DCACHE_SUBJECT_ATTRIBUTE, subject);
+
+                if (!isAuthorizedMethod(request.getMethod(), login)) {
+                    throw new PermissionDeniedCacheException("Permission denied: " +
+                            "read-only user");
+                }
+                checkRootPath(request, login);
+                /* Process the request as the authenticated user.*/
+                Exception problem = Subject.doAs(subject, (PrivilegedAction<Exception>) () -> {
+                    try {
+                        AuthenticationHandler.super.handle(target, baseRequest, request, response);
+                    } catch (IOException | ServletException e) {
+                        return e;
+                    }
+                    return null;
+                });
+                if (problem != null) {
+                    Throwables.propagateIfInstanceOf(problem, IOException.class);
+                    Throwables.propagateIfInstanceOf(problem, ServletException.class);
+                    throw Throwables.propagate(problem);
+                }
+            } catch (RedirectException e) {
+                LOG.warn("{} for path {} to url:{}", e.getMessage(), baseRequest.getServletPath(), e.getUrl());
+                response.sendRedirect(e.getUrl());
+            } catch (PermissionDeniedCacheException e) {
+                LOG.warn("{} for path {} and user {}", e.getMessage(), baseRequest.getServletPath(),
+                        NetLoggerBuilder.describeSubject(subject));
+                if (Subjects.isNobody(subject)) {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                } else {
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                }
+            } catch (CacheException e) {
+                LOG.error("Internal server error: " + e);
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
+
+
+    /**
+     * Verifies that the request is within the root directory of the given user.
+     *
+     * @throws RedirectException If the request should be redirected
+     * @throws PermissionDeniedCacheException If the request is denied
+     * @throws CacheException If the request fails
+     */
+    private void checkRootPath(HttpServletRequest request, LoginReply login)
+            throws CacheException, RedirectException {
+        FsPath userRoot = new FsPath();
+        FsPath userHome = new FsPath();
+        for (LoginAttribute attribute: login.getLoginAttributes()) {
+            if (attribute instanceof RootDirectory) {
+                userRoot = new FsPath(((RootDirectory) attribute).getRoot());
+            } else if (attribute instanceof HomeDirectory) {
+                userHome = new FsPath(((HomeDirectory) attribute).getHome());
+            }
+        }
+
+        String path = request.getServletPath();
+        FsPath fullPath = new FsPath(_rootPath, new FsPath(path));
+        if (!fullPath.startsWith(userRoot) &&
+                (_uploadPath == null || !fullPath.startsWith(_uploadPath))) {
+            if (!path.equals("/")) {
+                throw new PermissionDeniedCacheException("Permission denied: " +
+                        "path outside user's root");
+            }
+
+            try {
+                FsPath redirectFullPath = new FsPath(userRoot, userHome);
+                String redirectPath = _rootPath.relativize(redirectFullPath).toString();
+                URI uri = new URI(request.getRequestURL().toString());
+                URI redirect = new URI(uri.getScheme(), uri.getAuthority(), redirectPath, null, null);
+                throw new RedirectException(null, redirect.toString());
+            } catch (URISyntaxException e) {
+                throw new CacheException(e.getMessage(), e);
+            }
+        }
+    }
+
+    private void addX509ChainToSubject(HttpServletRequest request, Subject subject)
+            throws CacheException {
+        Object object = request.getAttribute(X509_CERTIFICATE_ATTRIBUTE);
+        if (object instanceof X509Certificate[]) {
+            try {
+                subject.getPublicCredentials().add(_cf.generateCertPath(asList((X509Certificate[]) object)));
+            } catch (CertificateException e) {
+                throw new CacheException("Failed to generate X.509 certificate path: " + e.getMessage(), e);
+            }
+        }
+    }
+
+    private void addOriginToSubject(HttpServletRequest request, Subject subject) {
+        String address = request.getRemoteAddr();
+        try {
+            subject.getPrincipals().add(new Origin(address));
+        } catch (UnknownHostException e) {
+            LOG.warn("Failed to resolve " + address + ": " + e.getMessage());
+        }
+    }
+
+
+    private void addPasswordCredentialToSubject(HttpServletRequest request, Subject subject) {
+        if (!_isBasicAuthenticationEnabled) {
+            return;
+        }
+
+        String header = request.getHeader("Authorization");
+        if (header == null) {
+            LOG.trace("No credentials found in Authorization header");
+            return;
+        }
+
+        if (header.length() == 0) {
+            LOG.trace("Credentials in Authorization header are not-null, but are empty");
+            return;
+        }
+
+        int space = header.indexOf(" ");
+        String authScheme = space >= 0 ? header.substring(0, space).toUpperCase() : HttpServletRequest.BASIC_AUTH;
+        String authData = space >= 0 ? header.substring(space + 1) : header;
+
+        switch (authScheme) {
+        case HttpServletRequest.BASIC_AUTH:
+            try {
+                byte[] bytes = Base64.getDecoder().decode(authData.getBytes(StandardCharsets.US_ASCII));
+                String credential = new String(bytes, StandardCharsets.UTF_8);
+                int colon = credential.indexOf(":");
+                if (colon >= 0) {
+                    String user = credential.substring(0, colon);
+                    String password = credential.substring(colon + 1);
+                    subject.getPrivateCredentials().add(new PasswordCredential(user, password));
+                } else {
+                    subject.getPrincipals().add(new LoginNamePrincipal(credential));
+                }
+            } catch (IllegalArgumentException e) {
+                LOG.warn("Authentication Data in the header received is not Base64 encoded {}", header);
+            }
+            break;
+        default:
+            LOG.trace("Unknown authentication scheme {}", authScheme);
+        }
+    }
+
+    private boolean isAuthorizedMethod(String method, LoginReply login) {
+        return !isUserReadOnly(login) || isReadMethod(method);
+    }
+
+
+    private boolean isUserReadOnly(LoginReply login) {
+        for (LoginAttribute attribute: login.getLoginAttributes()) {
+            if (attribute instanceof ReadOnly) {
+                return ((ReadOnly) attribute).isReadOnly();
+            }
+        }
+        return false;
+    }
+
+    private boolean isReadMethodsAllowed(String method) {
+        return !_isReadOnly || isReadMethod(method);
+    }
+
+    private boolean isReadMethod(String method) {
+        switch (method) {
+        case "GET":
+        case "HEAD":
+        case "OPTIONS":
+        case "PROPFIND":
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    public String getRealm() {
+        return _realm;
+    }
+
+    /**
+     * Sets the HTTP realm used for basic authentication.
+     */
+    public void setRealm(String realm) {
+        _realm = realm;
+    }
+
+    public boolean isReadOnly() {
+        return _isReadOnly;
+    }
+
+    /**
+     * Specifies whether the door is read only.
+     */
+    public void setReadOnly(boolean isReadOnly) {
+        _isReadOnly = isReadOnly;
+    }
+
+    public void setEnableBasicAuthentication(boolean isEnabled) {
+        _isBasicAuthenticationEnabled = isEnabled;
+    }
+
+    public void setLoginStrategy(LoginStrategy loginStrategy) {
+        _loginStrategy = loginStrategy;
+    }
+
+    public void setRootPath(String path) {
+        _rootPath = new FsPath(path);
+    }
+
+    public String getRootPath() {
+        return _rootPath.toString();
+    }
+
+    public void setUploadPath(File uploadPath) {
+        this._uploadPath = uploadPath.isAbsolute() ? new FsPath(uploadPath.getPath()) : null;
+    }
+
+    public File getUploadPath() {
+        return (_uploadPath == null) ? null : new File(_uploadPath.toString());
+    }
+}

modules/dcache-webdav/src/main/java/org/dcache/webdav/LoggingHandler.java
@@ -20,7 +20,7 @@ import java.net.InetSocketAddress;
 import java.security.cert.X509Certificate;
 
 import static org.dcache.webdav.DcacheResourceFactory.TRANSACTION_ATTRIBUTE;
-import static org.dcache.webdav.SecurityFilter.DCACHE_SUBJECT_ATTRIBUTE;
+import static org.dcache.webdav.AuthenticationHandler.DCACHE_SUBJECT_ATTRIBUTE;
 
 
 public class LoggingHandler extends HandlerWrapper {
@@ -48,9 +48,8 @@ public class LoggingHandler extends HandlerWrapper {
             log.add("response.code", statusCode);
             log.add("response.reason", getReason(response));
             log.add("location", response.getHeader("Location"));
-            HttpServletRequest r = request;
-            InetAddress addr = InetAddresses.forString(r.getRemoteAddr());
-            log.add("socket.remote", new InetSocketAddress(addr, r.getRemotePort()));
+            InetAddress addr = InetAddresses.forString(request.getRemoteAddr());
+            log.add("socket.remote", new InetSocketAddress(addr, request.getRemotePort()));
             log.add("user-agent", request.getHeader("User-Agent"));
 
             log.add("user.dn", getCertificateName(request));

modules/dcache-webdav/src/main/java/org/dcache/webdav/MiltonHandler.java
@@ -1,12 +1,15 @@
 package org.dcache.webdav;
 
+import io.milton.http.Auth;
 import io.milton.http.HttpManager;
 import io.milton.servlet.ServletRequest;
 import io.milton.servlet.ServletResponse;
+import org.dcache.auth.Subjects;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.ContextHandler;
 
+import javax.security.auth.Subject;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.AccessController;
 
 import dmg.cells.nucleus.CDC;
 import dmg.cells.nucleus.CellEndpoint;
@@ -62,8 +66,16 @@ public class MiltonHandler
                 response.sendError(501, "Not implemented");
                 break;
             default:
+                Subject subject = Subject.getSubject(AccessController.getContext());
                 ServletRequest req = new DcacheServletRequest(request, context);
                 ServletResponse resp = new DcacheServletResponse(response);
+
+                /* Although we don't rely on the authorization tag
+                 * ourselves, Milton uses it to detect that the request
+                 * was preauthenticated.
+                 */
+                req.setAuthorization(new Auth(Subjects.getUserName(subject), subject));
+
                 baseRequest.setHandled(true);
                 _httpManager.process(req, resp);
             }

modules/dcache-webdav/src/main/java/org/dcache/webdav/SecurityFilter.java
@@ -1,309 +1,0 @@
-package org.dcache.webdav;
-
-import io.milton.http.Auth;
-import io.milton.http.Filter;
-import io.milton.http.FilterChain;
-import io.milton.http.HttpManager;
-import io.milton.http.Request;
-import io.milton.http.Response;
-import io.milton.servlet.ServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.security.auth.Subject;
-import javax.servlet.http.HttpServletRequest;
-
-import java.io.File;
-import java.net.InetAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.UnknownHostException;
-import java.security.PrivilegedAction;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-
-import diskCacheV111.util.CacheException;
-import diskCacheV111.util.FsPath;
-import diskCacheV111.util.PermissionDeniedCacheException;
-
-import org.dcache.auth.LoginReply;
-import org.dcache.auth.LoginStrategy;
-import org.dcache.auth.Origin;
-import org.dcache.auth.PasswordCredential;
-import org.dcache.auth.Subjects;
-import org.dcache.auth.attributes.HomeDirectory;
-import org.dcache.auth.attributes.LoginAttribute;
-import org.dcache.auth.attributes.ReadOnly;
-import org.dcache.auth.attributes.RootDirectory;
-import org.dcache.util.CertificateFactories;
-import org.dcache.util.NetLoggerBuilder;
-
-import static java.util.Arrays.asList;
-
-/**
- * SecurityFilter for WebDAV door.
- *
- * This filter must be injected into the Milton HttpManager. Milton
- * provides its own SecurityManager infrastructure, however it has
- * several shortcomings:
- *
- * - It relies on access to a Resource object, meaning authentication
- *   is performed after we resolve a path to a DcacheResource. Thus
- *   lookup permissions cannot be handled.
- *
- * - Milton performs authentication after it checks whether a request
- *   should be redirected (I assume the idea is that the target of
- *   the redirect performs authorisation). For dCache we want to
- *   authorise the request before redirecting to a pool.
- *
- * For these reasons we have our own SecurityFilter for performing
- * authentication. The rest of the request processing is performed
- * with the priviledges of the authenticated user using JAAS.
- */
-public class SecurityFilter implements Filter
-{
-    private final Logger _log = LoggerFactory.getLogger(SecurityFilter.class);
-    public static final String X509_CERTIFICATE_ATTRIBUTE =
-        "javax.servlet.request.X509Certificate";
-    public static final String DCACHE_SUBJECT_ATTRIBUTE =
-            "org.dcache.subject";
-
-    private String _realm;
-    private boolean _isReadOnly;
-    private boolean _isBasicAuthenticationEnabled;
-    private LoginStrategy _loginStrategy;
-    private FsPath _rootPath = new FsPath();
-    private CertificateFactory _cf;
-    private FsPath _uploadPath;
-
-    public SecurityFilter()
-    {
-        _cf = CertificateFactories.newX509CertificateFactory();
-    }
-
-    @Override
-    public void process(final FilterChain filterChain,
-                        final Request request,
-                        final Response response)
-    {
-        HttpManager manager = filterChain.getHttpManager();
-        Subject subject = new Subject();
-
-        if (!isAllowedMethod(request.getMethod())) {
-            _log.debug("Failing {} from {} as door is read-only",
-                    request.getMethod(), request.getRemoteAddr());
-            manager.getResponseHandler().respondMethodNotAllowed(new EmptyResource(request), response, request);
-            return;
-        }
-
-        try {
-            HttpServletRequest servletRequest = ServletRequest.getRequest();
-
-            addX509ChainToSubject(servletRequest, subject);
-            addOriginToSubject(servletRequest, subject);
-            addPasswordCredentialToSubject(request, subject);
-
-            LoginReply login = _loginStrategy.login(subject);
-            subject = login.getSubject();
-
-            servletRequest.setAttribute(DCACHE_SUBJECT_ATTRIBUTE, subject);
-
-            if (!isAuthorizedMethod(request.getMethod(), login)) {
-                throw new PermissionDeniedCacheException("Permission denied: " +
-                        "read-only user");
-            }
-
-            checkRootPath(request, login);
-
-            /* Although we don't rely on the authorization tag
-             * ourselves, Milton uses it to detect that the request
-             * was preauthenticated.
-             */
-            request.setAuthorization(new Auth(Subjects.getUserName(subject), subject));
-
-            /* Process the request as the authenticated user.
-             */
-            Subject.doAs(subject, new PrivilegedAction<Void>() {
-                    @Override
-                    public Void run()
-                        {
-                            filterChain.process(request, response);
-                            return null;
-                        }
-                });
-        } catch (RedirectException e) {
-            manager.getResponseHandler().respondRedirect(response, request, e.getUrl());
-        } catch (PermissionDeniedCacheException e) {
-            _log.warn("{} for path {} and user {}", e.getMessage(), request.getAbsolutePath(),
-                    NetLoggerBuilder.describeSubject(subject));
-            manager.getResponseHandler().respondUnauthorised(new EmptyResource(request), response, request);
-        } catch (CacheException e) {
-            _log.error("Internal server error: " + e);
-            manager.getResponseHandler().respondServerError(request, response, e.getMessage());
-        }
-    }
-
-    /**
-     * Verifies that the request is within the root directory of the given user.
-     *
-     * @throws RedirectException If the request should be redirected
-     * @throws PermissionDeniedCacheException If the request is denied
-     * @throws CacheException If the request fails
-     */
-    private void checkRootPath(Request request, LoginReply login) throws CacheException
-    {
-        FsPath userRoot = new FsPath();
-        FsPath userHome = new FsPath();
-        for (LoginAttribute attribute: login.getLoginAttributes()) {
-            if (attribute instanceof RootDirectory) {
-                userRoot = new FsPath(((RootDirectory) attribute).getRoot());
-            } else if (attribute instanceof HomeDirectory) {
-                userHome = new FsPath(((HomeDirectory) attribute).getHome());
-            }
-        }
-
-        String path = request.getAbsolutePath();
-        FsPath fullPath = new FsPath(_rootPath, new FsPath(path));
-        if (!fullPath.startsWith(userRoot) &&
-                (_uploadPath == null || !fullPath.startsWith(_uploadPath))) {
-            if (!path.equals("/")) {
-                throw new PermissionDeniedCacheException("Permission denied: " +
-                        "path outside user's root");
-            }
-
-            try {
-                FsPath redirectFullPath = new FsPath(userRoot, userHome);
-                String redirectPath = _rootPath.relativize(redirectFullPath).toString();
-                URI uri = new URI(request.getAbsoluteUrl());
-                URI redirect = new URI(uri.getScheme(), uri.getAuthority(), redirectPath, null, null);
-                throw new RedirectException(null, redirect.toString());
-            } catch (URISyntaxException e) {
-                throw new CacheException(e.getMessage(), e);
-            }
-        }
-    }
-
-    private void addX509ChainToSubject(HttpServletRequest request, Subject subject)
-            throws CacheException
-    {
-        Object object = request.getAttribute(X509_CERTIFICATE_ATTRIBUTE);
-        if (object instanceof X509Certificate[]) {
-            try {
-                subject.getPublicCredentials().add(_cf.generateCertPath(asList((X509Certificate[]) object)));
-            } catch (CertificateException e) {
-                throw new CacheException("Failed to generate X.509 certificate path: " + e.getMessage(), e);
-            }
-        }
-    }
-
-    private void addOriginToSubject(HttpServletRequest request, Subject subject)
-    {
-        String address = request.getRemoteAddr();
-        try {
-            subject.getPrincipals().add(new Origin(address));
-        } catch (UnknownHostException e) {
-            _log.warn("Failed to resolve " + address + ": " + e.getMessage());
-        }
-    }
-
-    private void addPasswordCredentialToSubject(Request request, Subject subject)
-    {
-        Auth auth = request.getAuthorization();
-        if (auth != null && auth.getScheme().equals(Auth.Scheme.BASIC) &&
-            _isBasicAuthenticationEnabled) {
-            PasswordCredential credential =
-                new PasswordCredential(auth.getUser(), auth.getPassword());
-            subject.getPrivateCredentials().add(credential);
-        }
-    }
-
-    private boolean isAllowedMethod(Request.Method method)
-    {
-        return !_isReadOnly || isReadMethod(method);
-    }
-
-    private boolean isAuthorizedMethod(Request.Method method, LoginReply login)
-    {
-        return !isUserReadOnly(login) || isReadMethod(method);
-    }
-
-    private boolean isUserReadOnly(LoginReply login)
-    {
-        for (LoginAttribute attribute: login.getLoginAttributes()) {
-            if (attribute instanceof ReadOnly) {
-                return ((ReadOnly) attribute).isReadOnly();
-            }
-        }
-        return false;
-    }
-
-    private boolean isReadMethod(Request.Method method)
-    {
-        switch (method) {
-        case GET:
-        case HEAD:
-        case OPTIONS:
-        case PROPFIND:
-            return true;
-        default:
-            return false;
-        }
-    }
-
-    public String getRealm()
-    {
-        return _realm;
-    }
-
-    /**
-     * Sets the HTTP realm used for basic authentication.
-     */
-    public void setRealm(String realm)
-    {
-        _realm = realm;
-    }
-
-    public boolean isReadOnly()
-    {
-        return _isReadOnly;
-    }
-
-    /**
-     * Specifies whether the door is read only.
-     */
-    public void setReadOnly(boolean isReadOnly)
-    {
-        _isReadOnly = isReadOnly;
-    }
-
-    public void setEnableBasicAuthentication(boolean isEnabled)
-    {
-        _isBasicAuthenticationEnabled = isEnabled;
-    }
-
-    public void setLoginStrategy(LoginStrategy loginStrategy)
-    {
-        _loginStrategy = loginStrategy;
-    }
-
-    public void setRootPath(String path)
-    {
-        _rootPath = new FsPath(path);
-    }
-
-    public String getRootPath()
-    {
-        return _rootPath.toString();
-    }
-
-    public void setUploadPath(File uploadPath)
-    {
-        this._uploadPath = uploadPath.isAbsolute() ? new FsPath(uploadPath.getPath()) : null;
-    }
-
-    public File getUploadPath()
-    {
-        return (_uploadPath == null) ? null : new File(_uploadPath.toString());
-    }
-}

commit 97444868ba6cbef5aa3b4e59e66da89c5adabfcd

webdav: fix root directory permission check and logging
A recent patch, committed as a8edc264, mistakenly used the
servlet-path (which is always the empty string) both for
checking the requested resource against the user's root
path and for logging the request.

This patch fixes this by using the path-info, which contains
the correct information.

Target: master
Requires-notes: no
Requires-book: no
Patch: https://rb.dcache.org/r/8960
Acked-by: Gerd Behrmann

modules/dcache-webdav/src/main/java/org/dcache/webdav/AuthenticationHandler.java
@@ -114,10 +114,10 @@ public class AuthenticationHandler extends HandlerWrapper {
                     throw Throwables.propagate(problem);
                 }
             } catch (RedirectException e) {
-                LOG.warn("{} for path {} to url:{}", e.getMessage(), baseRequest.getServletPath(), e.getUrl());
+                LOG.warn("{} for path {} to url:{}", e.getMessage(), request.getPathInfo(), e.getUrl());
                 response.sendRedirect(e.getUrl());
             } catch (PermissionDeniedCacheException e) {
-                LOG.warn("{} for path {} and user {}", e.getMessage(), baseRequest.getServletPath(),
+                LOG.warn("{} for path {} and user {}", e.getMessage(), request.getPathInfo(),
                         NetLoggerBuilder.describeSubject(subject));
                 if (Subjects.isNobody(subject)) {
                     response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
@@ -151,7 +151,7 @@ public class AuthenticationHandler extends HandlerWrapper {
             }
         }
 
-        String path = request.getServletPath();
+        String path = request.getPathInfo();
         FsPath fullPath = new FsPath(_rootPath, new FsPath(path));
         if (!fullPath.startsWith(userRoot) &&
                 (_uploadPath == null || !fullPath.startsWith(_uploadPath))) {