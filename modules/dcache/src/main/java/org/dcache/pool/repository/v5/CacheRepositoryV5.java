dCache/dcache

commit c4d4e4f233506e1399e17ef474d64214486c1401

pool: Refactor representation of pool size
Motivation:

Pool size is composed from statically configured (layout file), runtime
configured (pool setup file or admin shell) and file system size. The
current implementation uses "Infinity" as a marker for an unspecified
value, triggering one of the other sources to be used. This is confusing
as the pool size is not infinite. It is true that the actual pool size
can be thought of as the minimum of the three different sources and this
was the original idea behind using Infinity, but in practice it just
seems odd.

Modification:

Refactored the UnitInteger class into DiskSpace. A dash is used as the
string representation for an absent value, although Infinity is still
supported for backwards compatibility.

set max diskspace and set gap commands are updated to use the new class
and rewritten to use the annotated command syntax.

Shell scripts are updated to recognize the new value for an absent
size. The default of the pool.size property changes from Infinity to
an empty string, both representing absense of a statically configured
size.

Result:

The use of Infinity as a value for the pool.size and set max diskspace
command is deprecated.

Target: trunk
Require-notes: yes
Require-book: yes
Acked-by: Paul Millar <paul.millar@desy.de>
Patch: https://rb.dcache.org/r/9108/

modules/dcache/src/main/java/org/dcache/pool/repository/v5/CacheRepositoryV5.java
@@ -14,8 +14,8 @@ import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ScheduledExecutorService;
@@ -31,11 +31,13 @@ import diskCacheV111.util.FileNotInCacheException;
 import diskCacheV111.util.LockedCacheException;
 import diskCacheV111.util.PnfsHandler;
 import diskCacheV111.util.PnfsId;
-import diskCacheV111.util.UnitInteger;
+import diskCacheV111.util.DiskSpace;
 import diskCacheV111.vehicles.PnfsAddCacheLocationMessage;
 
 import dmg.cells.nucleus.AbstractCellComponent;
 import dmg.cells.nucleus.CellCommandListener;
+import dmg.util.command.Argument;
+import dmg.util.command.Command;
 
 import org.dcache.pool.FaultAction;
 import org.dcache.pool.FaultEvent;
@@ -58,13 +60,10 @@ import org.dcache.pool.repository.StateChangeEvent;
 import org.dcache.pool.repository.StateChangeListener;
 import org.dcache.pool.repository.StickyChangeEvent;
 import org.dcache.pool.repository.StickyRecord;
-import org.dcache.util.Args;
 import org.dcache.util.CacheExceptionFactory;
 import org.dcache.vehicles.FileAttributes;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.Preconditions.*;
 import static org.dcache.namespace.FileAttribute.PNFSID;
 import static org.dcache.namespace.FileAttribute.STORAGEINFO;
 import static org.dcache.pool.repository.EntryState.*;
@@ -186,19 +185,19 @@ public class CacheRepositoryV5
      * Pool size configured through the 'max disk space' command.
      */
     @GuardedBy("_stateLock")
-    private long _runtimeMaxSize = Long.MAX_VALUE;
+    private DiskSpace _runtimeMaxSize = DiskSpace.UNSPECIFIED;
 
     /**
      * Pool size configured in the configuration files.
      */
     @GuardedBy("_stateLock")
-    private long _staticMaxSize = Long.MAX_VALUE;
+    private DiskSpace _staticMaxSize = DiskSpace.UNSPECIFIED;
 
     /**
      * Pool size gap to report to pool manager.
      */
     @GuardedBy("_stateLock")
-    private Optional<Long> _gap = Optional.empty();
+    private DiskSpace _gap = DiskSpace.UNSPECIFIED;
 
     /**
      * Throws an IllegalStateException if the repository has been
@@ -421,14 +420,11 @@ public class CacheRepositoryV5
 
     public void setMaxDiskSpaceString(String size)
     {
-        setMaxDiskSpace(UnitInteger.parseUnitLong(size));
+        setMaxDiskSpace(size.isEmpty() ? DiskSpace.UNSPECIFIED : new DiskSpace(size));
     }
 
-    public void setMaxDiskSpace(long size)
+    public void setMaxDiskSpace(DiskSpace size)
     {
-        if (size < 0) {
-            throw new IllegalArgumentException("Negative value is not allowed");
-        }
         _stateLock.writeLock().lock();
         try {
             _staticMaxSize = size;
@@ -891,7 +887,7 @@ public class CacheRepositoryV5
             long gap = space.getGap();
 
             pw.println("Disk space");
-            pw.println("    Total    : " + UnitInteger.toUnitString(total));
+            pw.println("    Total    : " + DiskSpace.toUnitString(total));
             pw.println("    Used     : " + used + "    ["
                        + (((float) used) / ((float) total)) + "]");
             pw.println("    Free     : " + (total - used) + "    Gap : " + gap);
@@ -908,8 +904,8 @@ public class CacheRepositoryV5
                        "    [" + (((float) fsFree) / fsTotal) + "]");
             pw.println("Limits for maximum disk space");
             pw.println("    File system          : " + (fsFree + used));
-            pw.println("    Statically configured: " + UnitInteger.toUnitString(_staticMaxSize));
-            pw.println("    Runtime configured   : " + UnitInteger.toUnitString(_runtimeMaxSize));
+            pw.println("    Statically configured: " + _staticMaxSize);
+            pw.println("    Runtime configured   : " + _runtimeMaxSize);
         } finally {
             _stateLock.readLock().unlock();
         }
@@ -1085,51 +1081,69 @@ public class CacheRepositoryV5
          return getCellName();
     }
 
-    public static final String hh_set_max_diskspace =
-        "<bytes>[<unit>]|Infinity # unit = k|m|g|t";
-    public static final String fh_set_max_diskspace =
-        "Sets the maximum disk space to be used by this pool. Overrides\n" +
-        "whatever maximum was defined in the configuration file. The value\n" +
-        "will be saved to the pool setup file if the save command is\n" +
-        "executed. If inf is specified, then the pool will return to the\n" +
-        "size configured in the configuration file, or no maximum if such a\n" +
-        "size is not defined.";
-    public String ac_set_max_diskspace_$_1(Args args)
+    @Command(name = "set max diskspace",
+            hint = "set size of pool",
+            description = "Sets the maximum disk space to be used by this pool. Overrides " +
+                          "whatever maximum was defined in the configuration file. The value " +
+                          "will be saved to the pool setup file if the save command is " +
+                          "executed.")
+    class SetMaxDiskspaceCommand implements Callable<String>
     {
-        long size = UnitInteger.parseUnitLong(args.argv(0));
-        if (size < 0) {
-            throw new IllegalArgumentException("Negative value is not allowed");
-        }
-        _stateLock.writeLock().lock();
-        try {
-            _runtimeMaxSize = size;
-            if (_state == State.OPEN) {
-                updateAccountSize();
+        @Argument(valueSpec = "-|BYTES[k|m|g|t]",
+                usage = "Disk space in bytes, kibibytes, mebibytes, gibibytes, or tebibytes. If " +
+                        "- is specified, then the pool will return to the size configured in " +
+                        "the configuration file, or no maximum if such a size is not defined.")
+        DiskSpace size;
+
+        @Override
+        public String call() throws IllegalArgumentException
+        {
+            _stateLock.writeLock().lock();
+            try {
+                _runtimeMaxSize = size;
+                if (_state == State.OPEN) {
+                    updateAccountSize();
+                }
+            } finally {
+                _stateLock.writeLock().unlock();
             }
-        } finally {
-            _stateLock.writeLock().unlock();
+            return "";
         }
-        return "";
     }
 
-    public static final String hh_set_gap = "<always removable gap>/size[<unit>] # unit = k|m|g";
-    public String ac_set_gap_$_1(Args args)
+    @Command(name = "set gap",
+            hint = "set minimum free space target",
+            description = "New transfers will not be assigned to a pool once it has less free space than the " +
+                          "gap. This is to ensure that there is a reasonable chance for ongoing transfers to " +
+                          "complete. To prevent that writes will fail due to lack of space, the gap should be " +
+                          "in the order of the expected largest file size multiplied by the largest number of " +
+                          "concurrent writes expected to a pool, although a smaller value will often do.\n\n" +
+                          "It is not an error for a pool to have less free space than the gap.")
+    class SetGapCommand implements Callable<String>
     {
-        long gap = UnitInteger.parseUnitLong(args.argv(0));
-        _stateLock.writeLock().lock();
-        try {
-            _gap = Optional.of(gap);
-        } finally {
-            _stateLock.writeLock().unlock();
+        @Argument(valueSpec = "BYTES[k|m|g|t]", required = false,
+                usage = "The gap in bytes, kibibytes, mebibytes, gibibytes or tebibytes. If not specified the " +
+                        "default is the smaller of 4 GiB or 25% of the pool size.")
+        DiskSpace gap = DiskSpace.UNSPECIFIED;
+
+        @Override
+        public String call() throws Exception
+        {
+            _stateLock.writeLock().lock();
+            try {
+                _gap = gap;
+            } finally {
+                _stateLock.writeLock().unlock();
+            }
+            return "Gap set to " + gap;
         }
-        return "Gap set to " + gap;
     }
 
     @Override
     public void printSetup(PrintWriter pw)
     {
-        long runtimeMaxSize;
-        Optional<Long> gap;
+        DiskSpace runtimeMaxSize;
+        DiskSpace gap;
 
         _stateLock.readLock().lock();
         try {
@@ -1139,19 +1153,19 @@ public class CacheRepositoryV5
             _stateLock.readLock().unlock();
         }
 
-        if (runtimeMaxSize < Long.MAX_VALUE) {
+        if (runtimeMaxSize.isSpecified()) {
             pw.println("set max diskspace " + runtimeMaxSize);
         }
-        if (gap.isPresent()) {
-            pw.println("set gap " + gap.get());
+        if (gap.isSpecified()) {
+            pw.println("set gap " + gap);
         }
     }
 
-    private long getConfiguredMaxSize()
+    private DiskSpace getConfiguredMaxSize()
     {
         _stateLock.readLock().lock();
         try {
-            return (_runtimeMaxSize < Long.MAX_VALUE) ? _runtimeMaxSize : _staticMaxSize;
+            return _runtimeMaxSize.orElse(_staticMaxSize);
         } finally {
             _stateLock.readLock().unlock();
         }
@@ -1195,30 +1209,28 @@ public class CacheRepositoryV5
     {
         Account account = _account;
         synchronized (account) {
-            long configuredMaxSize = getConfiguredMaxSize();
-            long fileSystemMaxSize = getFileSystemMaxSize();
-            boolean hasConfiguredMaxSize = (configuredMaxSize < Long.MAX_VALUE);
+            DiskSpace configuredPoolSize = getConfiguredMaxSize();
+            long maxPoolSize = getFileSystemMaxSize();
             long used = account.getUsed();
 
             if (!isTotalSpaceReported()) {
                 LOGGER.warn("Java reported the file system size as 0. This typically happens on Solaris with a 32-bit JVM. Please use a 64-bit JVM.");
-                if (!hasConfiguredMaxSize) {
+                if (!configuredPoolSize.isSpecified()) {
                     throw new IllegalStateException("Failed to determine file system size. A pool size must be configured.");
                 }
             }
 
-            if (hasConfiguredMaxSize && fileSystemMaxSize < configuredMaxSize) {
-                LOGGER.warn("Configured pool size ({}) is larger than what is available on disk ({})", configuredMaxSize,
-                            fileSystemMaxSize);
+            long newSize;
+
+            if (configuredPoolSize.isLargerThan(maxPoolSize)) {
+                LOGGER.warn("Configured pool size ({}) is larger than what is available on disk ({}).",
+                            configuredPoolSize, maxPoolSize);
+            } else if (configuredPoolSize.isLessThan(used)) {
+                LOGGER.warn("Configured pool size ({}) is less than what is used already ({}).",
+                            configuredPoolSize, used);
             }
 
-            if (configuredMaxSize < used) {
-                LOGGER.warn("Configured pool size ({}) is smaller than what is used already ({})", configuredMaxSize,
-                            used);
-            }
-
-            long newSize =
-                Math.max(used, Math.min(configuredMaxSize, fileSystemMaxSize));
+            newSize = Math.max(used, configuredPoolSize.orElse(maxPoolSize);
             if (newSize != account.getTotal()) {
                 LOGGER.info("Adjusting pool size to {}", newSize);
                 account.setTotal(newSize);

modules/dcache/src/main/java/diskCacheV111/util/DiskSpace.java
@@ -1,0 +1,159 @@
+package diskCacheV111.util;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * Immutable quantity of disk space.
+ *
+ * Essentially just an integer, but can parse suffixes for kibibytes, mebibytes,
+ * gibibytes, and tebibytes.
+ *
+ * The quantity can be unspecified, with the string representation being a dash.
+ * This is represented as Long.MAX_VALUE bytes, the quantity larger than any other
+ * disk space.
+ *
+ * Disk space is always positive.
+ */
+public class DiskSpace
+{
+    public static final DiskSpace UNSPECIFIED = new DiskSpace(Long.MAX_VALUE);
+
+    private static final long TEBI = (1L << 40);
+    private static final long GIBI = (1L << 30);
+    private static final long MEBI = (1L << 20);
+    private static final long KIBI = (1L << 10);
+
+    private final long _value;
+
+    public DiskSpace(long value)
+    {
+        checkArgument(value >= 0, "Negative value is not allowed");
+        _value = value;
+    }
+
+    public DiskSpace(String s)
+    {
+        this(parseUnitLong(s));
+    }
+
+    private static long parseUnitLong(String s)
+    {
+        checkArgument(!s.isEmpty(), "Argument must not be empty");
+
+        if (s.equals("Infinity") || s.equals("-")) {
+            return Long.MAX_VALUE;
+        }
+
+        String num;
+        long multi;
+        switch (s.charAt(s.length() - 1)) {
+        case 'k':
+        case 'K':
+            multi = KIBI;
+            num = s.substring(0, s.length() - 1);
+            break;
+        case 'm':
+        case 'M':
+            multi = MEBI;
+            num = s.substring(0, s.length() - 1);
+            break;
+        case 'g':
+        case 'G':
+            multi = GIBI;
+            num = s.substring(0, s.length() - 1);
+            break;
+        case 't':
+        case 'T':
+            multi = TEBI;
+            num = s.substring(0, s.length() - 1);
+            break;
+        default:
+            multi = 1;
+            num = s;
+        }
+        return Long.parseLong(num) * multi;
+    }
+
+    public String toString()
+    {
+        return toUnitString();
+    }
+
+    public String toUnitString()
+    {
+        return toUnitString(_value);
+    }
+
+    public static String toUnitString(long value)
+    {
+        long tmp;
+        if (value == Long.MAX_VALUE) {
+            return "-";
+        }
+        if (((tmp = (value / TEBI)) > 0) && ((value % TEBI) == 0)) {
+            return Long.toString(tmp) + "T";
+        }
+        if (((tmp = (value / GIBI)) > 0) && ((value % GIBI) == 0)) {
+            return Long.toString(tmp) + "G";
+        }
+        if (((tmp = (value / MEBI)) > 0) && ((value % MEBI) == 0)) {
+            return Long.toString(tmp) + "M";
+        }
+        if (((tmp = (value / KIBI)) > 0) && ((value % KIBI) == 0)) {
+            return Long.toString(tmp) + "k";
+        }
+        return Long.toString(value);
+    }
+
+    public long longValue()
+    {
+        return _value;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        DiskSpace that = (DiskSpace) o;
+        return _value == that._value;
+
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return (int) (_value ^ (_value >>> 32));
+    }
+
+    public boolean isLargerThan(long value)
+    {
+        return value < _value && _value < Long.MAX_VALUE;
+    }
+
+    public boolean isLessThan(long value)
+    {
+        return _value < value;
+    }
+
+    public boolean isSpecified()
+    {
+        return _value != Long.MAX_VALUE;
+    }
+
+    public long orElse(long other)
+    {
+        return isSpecified() ? _value : other;
+    }
+
+    public DiskSpace orElse(DiskSpace other)
+    {
+        return isSpecified() ? this : other;
+    }
+}
+

modules/dcache/src/main/java/diskCacheV111/util/UnitInteger.java
@@ -1,91 +1,0 @@
-package diskCacheV111.util ;
-
-public class UnitInteger {
-
-    private static final String INFINITY = "Infinity";
-    private static final long TEBI = (1L << 40);
-    private static final long GIBI = (1L << 30);
-    private static final long MEBI = (1L << 20);
-    private static final long KIBI = (1L << 10);
-
-    private final long _value;
-    public UnitInteger( long value ){
-       _value = value ;
-    }
-    public UnitInteger( String stringRep ){
-       _value = parseUnitLong( stringRep ) ;
-    }
-    public static long parseUnitLong( String stringRep ){
-       if( stringRep.length() < 1 ) {
-           throw new
-                   IllegalArgumentException("Empty String");
-       }
-
-       if (stringRep.equals(INFINITY)) {
-           return Long.MAX_VALUE;
-       }
-
-       String num;
-       long   multi;
-       switch( stringRep.charAt(stringRep.length()-1) ){
-
-          case 'k' :
-          case 'K' :
-             multi = KIBI;
-             num   = stringRep.substring(0,stringRep.length()-1) ;
-             break ;
-          case 'm' :
-          case 'M' :
-             multi = MEBI;
-             num   = stringRep.substring(0,stringRep.length()-1) ;
-             break ;
-          case 'g' :
-          case 'G' :
-             multi = GIBI;
-             num   = stringRep.substring(0,stringRep.length()-1) ;
-             break ;
-          case 't' :
-          case 'T' :
-             multi = TEBI;
-             num   = stringRep.substring(0,stringRep.length()-1) ;
-             break ;
-          default :
-             multi = 1 ;
-             num   = stringRep ;
-       }
-       return  Long.parseLong(num) * multi ;
-    }
-    public String toString(){ return ""+_value ; }
-    public String toUnitString(){
-       return toUnitString( _value ) ;
-    }
-    public static String toUnitString( long value ){
-       long tmp;
-       if (value == Long.MAX_VALUE) {
-           return INFINITY;
-       }
-       if( ( ( tmp = ( value / TEBI ) ) > 0 ) && ( ( value % TEBI ) == 0 ) ) {
-           return Long.toString(tmp) + "T";
-       }
-       if( ( ( tmp = ( value / GIBI ) ) > 0 ) && ( ( value % GIBI ) == 0 ) ) {
-           return Long.toString(tmp) + "G";
-       }
-       if( ( ( tmp = ( value / MEBI ) ) > 0 ) && ( ( value % MEBI ) == 0 ) ) {
-           return Long.toString(tmp) + "M";
-       }
-       if( ( ( tmp = ( value / KIBI ) ) > 0 ) && ( ( value % KIBI ) == 0 ) ) {
-           return Long.toString(tmp) + "K";
-       }
-       return Long.toString(value);
-    }
-    public long longValue(){ return _value ; }
-    public static void main( String [] args ){
-        for (String arg : args) {
-            UnitInteger ui = new UnitInteger(arg);
-            System.out.println(arg + " -> " + ui
-                    .toUnitString() + "  (" + ui + ")");
-        }
-       System.exit(0);
-    }
-}
-

modules/dcache/src/test/java/diskCacheV111/util/DiskSpaceTest.java
@@ -1,0 +1,60 @@
+package diskCacheV111.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DiskSpaceTest
+{
+    private final static long TEBI = (1L << 40);
+    private final static long GIBI = (1L << 30);
+    private final static long MEBI = (1L << 20);
+    private final static long KIBI = (1L << 10);
+
+    @Test
+    public void testParsing()
+    {
+        assertEquals(-1, new DiskSpace("-1").longValue());
+        assertEquals(0, new DiskSpace("0").longValue());
+        assertEquals(1, new DiskSpace("1").longValue());
+
+        assertEquals(-KIBI, new DiskSpace("-1k").longValue());
+        assertEquals(0, new DiskSpace("0k").longValue());
+        assertEquals(KIBI, new DiskSpace("1k").longValue());
+        assertEquals(-KIBI, new DiskSpace("-1K").longValue());
+        assertEquals(0, new DiskSpace("0K").longValue());
+        assertEquals(KIBI, new DiskSpace("1K").longValue());
+
+        assertEquals(-MEBI, new DiskSpace("-1m").longValue());
+        assertEquals(0, new DiskSpace("0m").longValue());
+        assertEquals(MEBI, new DiskSpace("1m").longValue());
+        assertEquals(-MEBI, new DiskSpace("-1M").longValue());
+        assertEquals(0, new DiskSpace("0M").longValue());
+        assertEquals(MEBI, new DiskSpace("1M").longValue());
+
+        assertEquals(-GIBI, new DiskSpace("-1g").longValue());
+        assertEquals(0, new DiskSpace("0g").longValue());
+        assertEquals(GIBI, new DiskSpace("1g").longValue());
+        assertEquals(-GIBI, new DiskSpace("-1G").longValue());
+        assertEquals(0, new DiskSpace("0G").longValue());
+        assertEquals(GIBI, new DiskSpace("1G").longValue());
+
+        assertEquals(-TEBI, new DiskSpace("-1t").longValue());
+        assertEquals(0, new DiskSpace("0t").longValue());
+        assertEquals(TEBI, new DiskSpace("1t").longValue());
+        assertEquals(-TEBI, new DiskSpace("-1T").longValue());
+        assertEquals(0, new DiskSpace("0T").longValue());
+        assertEquals(TEBI, new DiskSpace("1T").longValue());
+    }
+
+    @Test
+    public void testToString()
+    {
+        assertEquals("0", DiskSpace.toUnitString(0));
+        assertEquals("1", DiskSpace.toUnitString(1));
+        assertEquals("1K", DiskSpace.toUnitString(KIBI));
+        assertEquals("1M", DiskSpace.toUnitString(MEBI));
+        assertEquals("1G", DiskSpace.toUnitString(GIBI));
+        assertEquals("1T", DiskSpace.toUnitString(TEBI));
+    }
+}

modules/dcache/src/test/java/diskCacheV111/util/UnitIntegerTest.java
@@ -1,60 +1,0 @@
-package diskCacheV111.util;
-
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
-public class UnitIntegerTest
-{
-    private final static long TEBI = (1L << 40);
-    private final static long GIBI = (1L << 30);
-    private final static long MEBI = (1L << 20);
-    private final static long KIBI = (1L << 10);
-
-    @Test
-    public void testParsing()
-    {
-        assertEquals(-1, UnitInteger.parseUnitLong("-1"));
-        assertEquals(0, UnitInteger.parseUnitLong("0"));
-        assertEquals(1, UnitInteger.parseUnitLong("1"));
-
-        assertEquals(-KIBI, UnitInteger.parseUnitLong("-1k"));
-        assertEquals(0, UnitInteger.parseUnitLong("0k"));
-        assertEquals(KIBI, UnitInteger.parseUnitLong("1k"));
-        assertEquals(-KIBI, UnitInteger.parseUnitLong("-1K"));
-        assertEquals(0, UnitInteger.parseUnitLong("0K"));
-        assertEquals(KIBI, UnitInteger.parseUnitLong("1K"));
-
-        assertEquals(-MEBI, UnitInteger.parseUnitLong("-1m"));
-        assertEquals(0, UnitInteger.parseUnitLong("0m"));
-        assertEquals(MEBI, UnitInteger.parseUnitLong("1m"));
-        assertEquals(-MEBI, UnitInteger.parseUnitLong("-1M"));
-        assertEquals(0, UnitInteger.parseUnitLong("0M"));
-        assertEquals(MEBI, UnitInteger.parseUnitLong("1M"));
-
-        assertEquals(-GIBI, UnitInteger.parseUnitLong("-1g"));
-        assertEquals(0, UnitInteger.parseUnitLong("0g"));
-        assertEquals(GIBI, UnitInteger.parseUnitLong("1g"));
-        assertEquals(-GIBI, UnitInteger.parseUnitLong("-1G"));
-        assertEquals(0, UnitInteger.parseUnitLong("0G"));
-        assertEquals(GIBI, UnitInteger.parseUnitLong("1G"));
-
-        assertEquals(-TEBI, UnitInteger.parseUnitLong("-1t"));
-        assertEquals(0, UnitInteger.parseUnitLong("0t"));
-        assertEquals(TEBI, UnitInteger.parseUnitLong("1t"));
-        assertEquals(-TEBI, UnitInteger.parseUnitLong("-1T"));
-        assertEquals(0, UnitInteger.parseUnitLong("0T"));
-        assertEquals(TEBI, UnitInteger.parseUnitLong("1T"));
-    }
-
-    @Test
-    public void testToString()
-    {
-        assertEquals("0", UnitInteger.toUnitString(0));
-        assertEquals("1", UnitInteger.toUnitString(1));
-        assertEquals("1K", UnitInteger.toUnitString(KIBI));
-        assertEquals("1M", UnitInteger.toUnitString(MEBI));
-        assertEquals("1G", UnitInteger.toUnitString(GIBI));
-        assertEquals("1T", UnitInteger.toUnitString(TEBI));
-    }
-}

modules/dcache/src/test/java/org/dcache/tests/repository/RepositorySubsystemTest.java
@@ -26,6 +26,7 @@ import diskCacheV111.util.FileNotInCacheException;
 import diskCacheV111.util.LockedCacheException;
 import diskCacheV111.util.PnfsHandler;
 import diskCacheV111.util.PnfsId;
+import diskCacheV111.util.DiskSpace;
 import diskCacheV111.vehicles.GenericStorageInfo;
 import diskCacheV111.vehicles.PnfsAddCacheLocationMessage;
 import diskCacheV111.vehicles.PnfsClearCacheLocationMessage;
@@ -196,7 +197,7 @@ public class RepositorySubsystemTest
         repository.setSynchronousNotification(true);
         repository.addListener(this);
         repository.setSpaceSweeperPolicy(sweeper);
-        repository.setMaxDiskSpace(5120);
+        repository.setMaxDiskSpace(new DiskSpace(5120));
     }
 
     @Before
@@ -611,7 +612,7 @@ public class RepositorySubsystemTest
         repository.load();
         stateChangeEvents.clear();
 
-        repository.setMaxDiskSpace(3072);
+        repository.setMaxDiskSpace(new DiskSpace(3072));
         assertSpaceRecord(3072, 0, 1024, 1024);
     }
 
@@ -623,7 +624,7 @@ public class RepositorySubsystemTest
         repository.load();
         stateChangeEvents.clear();
 
-        repository.setMaxDiskSpace(-1);
+        repository.setMaxDiskSpace(new DiskSpace(-1));
     }
 
     @Test
@@ -925,7 +926,7 @@ public class RepositorySubsystemTest
         repository.load();
         stateChangeEvents.clear();
 
-        repository.setMaxDiskSpace(3072);
+        repository.setMaxDiskSpace(new DiskSpace(3072));
         createEntry4(0, false, false, FROM_CLIENT, PRECIOUS);
         assertCanOpen(id4, size4, PRECIOUS);
         assertSpaceRecord(3072, 0, 2048, 0);

skel/bin/dcache
@@ -532,13 +532,7 @@ case "$1" in
                                         ;;
                                 esac
 
-                                max=$(getSizeOfPool "$path")
-                                if [ "$max" = "Infinity" ]; then
-                                    max="-"
-                                else
-                                    max="${max}G"
-                                fi
-                                printf "${name}\t${domain}\t${meta}\t${max}\t$(getFreeSpace ${path}/data)G\t${path}\n"
+                                printf "${name}\t${domain}\t${meta}\t$(getSizeOfPool "$path")\t$(getFreeSpace ${path}/data)G\t${path}\n"
                             fi
                         done
                     done

skel/share/defaults/pool.properties
@@ -41,11 +41,11 @@ pool.queues =
 
 #  ---- Maximum amount of space to be used by a pool
 #
-#   In bytes or 'Infinity'. May also be configured at runtime through
-#   the admin interface. If 'Infinity', then the pool size is
+#   In bytes. May also be configured at runtime through
+#   the admin interface. If not set, the pool size is
 #   determined from the size of the file system.
 #
-pool.size = Infinity
+pool.size =
 
 #  ---- Pool tags
 #

commit 6ad7cd3d7569c7392c4a7273746ace2011940247

pool: Fix compilation failure
Fixes a regression introduced in c4d4e4f233506e1399e17ef474d64214486c1401.

Patch: none

modules/dcache/src/main/java/org/dcache/pool/repository/v5/CacheRepositoryV5.java
@@ -1230,7 +1230,7 @@ public class CacheRepositoryV5
                             configuredPoolSize, used);
             }
 
-            newSize = Math.max(used, configuredPoolSize.orElse(maxPoolSize);
+            newSize = Math.max(used, configuredPoolSize.orElse(maxPoolSize));
             if (newSize != account.getTotal()) {
                 LOGGER.info("Adjusting pool size to {}", newSize);
                 account.setTotal(newSize);