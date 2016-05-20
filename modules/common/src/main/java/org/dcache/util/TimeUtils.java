818da05809201b941ec776698ef73968a61e6b7d
alrossi <arossi@otfrid.fnal.gov>
2016-05-20 11:03:07 -0500

    common: add parsing and formatting utilities for time duration
    Motivation:
    
    Being able to express a duration value as a formatted string
    is useful in several places in dCache code.
    
    Modification:
    
    Add a generalized method to TimeUtils to return an object
    encapsulating duration values accessible like a map.
    
    Add a formatting class which allows for the expression of
    duration in terms of days, hours, minutes, seconds and millis/nanos.
    
    Result:
    
    Standard methods for achieving this.
    
    Target: master
    Require-book: no
    Require-notes: no
    Acked-by: Dmitry

--- modules/common/src/main/java/org/dcache/util/TimeUtils.java
+++ modules/common/src/main/java/org/dcache/util/TimeUtils.java
@@ -1,15 +1,20 @@
 package org.dcache.util;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.concurrent.TimeUnit.*;
-
 
 /**
  * Utility classes for dealing with time and durations, mostly with pretty-
@@ -18,6 +23,275 @@ import static java.util.concurrent.TimeUnit.*;
 public class TimeUtils
 {
     public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd' 'HH:mm:ss.SSS";
+
+    /**
+     * <p>Compares time units such that the larger unit is
+     *      ordered before the smaller.</p>
+     */
+    public static class DecreasingTimeUnitComparator
+                    implements Comparator<TimeUnit>
+    {
+        @Override
+        public int compare(TimeUnit unit1, TimeUnit unit2) {
+            if (unit1 == null) {
+                return -1;
+            }
+
+            if (unit2 == null) {
+                return 1;
+            }
+
+            long nanos1 = unit1.toNanos(1);
+            long nanos2 = unit2.toNanos(1);
+
+            if (nanos1 == nanos2) {
+                return 0;
+            }
+
+            if (nanos1 < nanos2) {
+                return 1;
+            }
+
+            return -1;
+        }
+    }
+
+    /**
+     * <p>Parses a duration of a given dimension into other dimensions.</p>
+     *
+     * <p>The parsed out dimensions are held in an internal map.</p>
+     *
+     * <p>There can be gaps between the various time dimensions, but it
+     *    is understood that successive calls to parse should be
+     *    strictly decreasing. Calls to compute a larger unit than
+     *    the current unit will throw an exception.</p>
+     *
+     * <p>The dimensions can be recomputed by calling clear(), but
+     *    the original duration given to the parser is fixed.</p>
+     */
+    public static class DurationParser
+    {
+        private final Map<TimeUnit, Long> durations;
+        private final Long duration;
+        private final TimeUnit durationUnit;
+
+        private TimeUnit current;
+        private long remainder;
+
+        public DurationParser(Long duration, TimeUnit durationUnit) {
+            this.duration = Preconditions.checkNotNull(duration,
+                            "duration was null");
+            this.durationUnit = Preconditions.checkNotNull(durationUnit,
+                            "durationUnit was null");
+            durations = new HashMap<>();
+            remainder = durationUnit.toNanos(duration);
+        }
+
+        public DurationParser parseAll() {
+            parse(TimeUnit.DAYS);
+            parse(TimeUnit.HOURS);
+            parse(TimeUnit.MINUTES);
+            parse(TimeUnit.SECONDS);
+            parse(TimeUnit.MILLISECONDS);
+            parse(TimeUnit.MICROSECONDS);
+            parse(TimeUnit.NANOSECONDS);
+            return this;
+        }
+
+        public void parse(TimeUnit unit) throws IllegalStateException {
+            checkStrictlyDecreasing(unit);
+            long durationForUnit = unit.convert(remainder, TimeUnit.NANOSECONDS);
+            long durationInNanos = unit.toNanos(durationForUnit);
+            remainder = remainder - durationInNanos;
+            durations.put(unit, durationForUnit);
+            current = unit;
+        }
+
+        public void clear() {
+            durations.clear();
+            current = null;
+            remainder = durationUnit.toNanos(duration);
+        }
+
+        public long get(TimeUnit unit) {
+            Long duration = durations.get(unit);
+            return duration == null ? 0L: duration;
+        }
+
+        private void checkStrictlyDecreasing(TimeUnit next)
+                        throws IllegalStateException {
+            /*
+             * Because this is a decreasing order comparator,
+             * it returns -1 when the first element is larger than the
+             * second.
+             */
+            if (comparator.compare(next, current) <= 0) {
+                throw new IllegalStateException(next + " is not strictly "
+                                + "smaller than " + current);
+            }
+        }
+    }
+
+    /**
+     * <p>Returns a given duration broken down into constituent units of all
+     *    dimensions and formatted according to the duration format string.</p>
+     *
+     * <p>The format markers are:</p>
+     *
+     * <table>
+     *     <tr><td>%D</td><td>days</td></tr>
+     *     <tr><td>%H</td><td>hours (no leading zero)</td></tr>
+     *     <tr><td>%HH</td><td>hours with one leading zero</td></tr>
+     *     <tr><td>%m</td><td>minutes (no leading zero)</td></tr>
+     *     <tr><td>%mm</td><td>minutes with one leading zero</td></tr>
+     *     <tr><td>%s</td><td>seconds (no leading zero)</td></tr>
+     *     <tr><td>%ss</td><td>seconds with one leading zero</td></tr>
+     *     <tr><td>%S</td><td>milliseconds</td></tr>
+     *     <tr><td>%N</td><td>milliseconds+nanoseconds</td></tr>
+     * </table>
+     *
+     * <p>Note that '%' is used here as a reserved symbol as in String formatting.
+     *    %s, however, has a different meaning.  This formatting string should
+     *    not contain any other placeholders than the ones above and cannot
+     *    be combined with normal string formatting.</p>
+     */
+    public static class DurationFormatter
+    {
+        private final String format;
+        private DurationParser durations;
+
+        public DurationFormatter(String format)
+        {
+            Preconditions.checkNotNull(format,
+                            "Format string must be specified.");
+            this.format = format;
+        }
+
+        public String format(long duration, TimeUnit unit)
+        {
+            Preconditions.checkNotNull(unit,
+                            "Duration time unit must be specified.");
+            TimeUnit[] sortedDimensions = getSortedDimensions();
+            durations = new DurationParser(duration, unit);
+            for (TimeUnit dimension: sortedDimensions) {
+                durations.parse(dimension);
+            }
+            StringBuilder builder = new StringBuilder();
+            replace(builder);
+            return builder.toString();
+        }
+
+        private TimeUnit[] getSortedDimensions() {
+            Set<TimeUnit> units = new HashSet<>();
+            char[] sequence = format.toCharArray();
+            for (int c = 0; c < sequence.length; c++) {
+                switch (sequence[c]) {
+                    case '%':
+                        ++c;
+                        switch (sequence[c]) {
+                            case 'D':
+                                units.add(TimeUnit.DAYS);
+                                break;
+                            case 'H':
+                                units.add(TimeUnit.HOURS);
+                                break;
+                            case 'm':
+                                units.add(TimeUnit.MINUTES);
+                                break;
+                            case 's':
+                                units.add(TimeUnit.SECONDS);
+                                break;
+                            case 'S':
+                                units.add(TimeUnit.MILLISECONDS);
+                                break;
+                            case 'N':
+                                units.add(TimeUnit.MILLISECONDS);
+                                units.add(TimeUnit.MICROSECONDS);
+                                units.add(TimeUnit.NANOSECONDS);
+                                break;
+                            default:
+                                throw new IllegalArgumentException(
+                                                "No such formatting symbol " + c);
+                        }
+                        break;
+                    default:
+                }
+            }
+
+            TimeUnit[] sorted = units.toArray(new TimeUnit[units.size()]);
+            Arrays.sort(sorted, comparator);
+            return sorted;
+        }
+
+        private void replace(StringBuilder builder) {
+            char[] sequence = format.toCharArray();
+            for (int c = 0; c < sequence.length; c++) {
+                switch (sequence[c]) {
+                    case '%':
+                        c = handlePlaceholder(++c,
+                                        sequence,
+                                        builder);
+                        break;
+                    default:
+                        builder.append(sequence[c]);
+                }
+            }
+        }
+
+        private int handlePlaceholder(int c, char[] sequence, StringBuilder builder)
+        {
+            switch (sequence[c]) {
+                case 'D':
+                    builder.append(durations.get(TimeUnit.DAYS));
+                    break;
+                case 'H':
+                    if (sequence[c+1] == 'H') {
+                        ++c;
+                        builder.append(leadingZero(durations.get(TimeUnit.HOURS)));
+                    } else {
+                        builder.append(durations.get(TimeUnit.HOURS));
+                    }
+                    break;
+                case 'm':
+                    if (sequence[c+1] == 'm') {
+                        ++c;
+                        builder.append(leadingZero(durations.get(TimeUnit.MINUTES)));
+                    } else {
+                        builder.append(durations.get(TimeUnit.MINUTES));
+                    }
+                    break;
+                case 's':
+                    if (sequence[c+1] == 's') {
+                        ++c;
+                        builder.append(leadingZero(durations.get(TimeUnit.SECONDS)));
+                    } else {
+                        builder.append(durations.get(TimeUnit.SECONDS));
+                    }
+                    break;
+                case 'S':
+                    builder.append(durations.get(TimeUnit.MILLISECONDS));
+                    break;
+                case 'N':
+                    builder.append(durations.get(TimeUnit.MILLISECONDS))
+                           .append(durations.get(TimeUnit.MICROSECONDS))
+                           .append(durations.get(TimeUnit.NANOSECONDS));
+                    break;
+                default:
+                    throw new IllegalArgumentException
+                                    ("No such formatting symbol " + c);
+            }
+
+            return c;
+        }
+
+        private String leadingZero(Long value) {
+            String valueString = String.valueOf(value);
+            if (valueString.length() < 2) {
+                return "0" + valueString;
+            }
+            return valueString;
+        }
+    }
 
     public enum TimeUnitFormat
     {
@@ -54,6 +328,9 @@ public class TimeUtils
                     put(DAYS, "days").
                     build();
 
+    private static final DecreasingTimeUnitComparator comparator
+                    = new DecreasingTimeUnitComparator();
+
     private TimeUtils()
     {
         // Prevent instantiation.
@@ -64,6 +341,20 @@ public class TimeUtils
         return appendDuration(new StringBuilder(), duration, units, unitFormat);
     }
 
+    /**
+     * @see DurationFormatter
+     *
+     * @param duration to be expressed
+     * @param unit of the duration
+     * @param format as specified above.
+     * @return formatted string
+     */
+    public static String getFormattedDuration(long duration,
+                                              TimeUnit unit,
+                                              String format)
+    {
+        return new DurationFormatter(format).format(duration, unit);
+    }
 
     /**
      * Returns short sting form for given {@ link TimeUnit}.
@@ -79,7 +370,8 @@ public class TimeUtils
      * {@code <number> <space> <units>}, where {@code <number>}
      * is an integer and {@code <units>} is defined by the value of unitFormat.
      */
-    public static StringBuilder appendDuration(StringBuilder sb, long duration, TimeUnit units, TimeUnitFormat unitFormat)
+    public static StringBuilder appendDuration(StringBuilder sb, long duration,
+                    TimeUnit units, TimeUnitFormat unitFormat)
     {
         checkArgument(duration >= 0);
 
@@ -124,7 +416,6 @@ public class TimeUtils
 
         return sb;
     }
-
 
     /**
      * Returns a description of some point in time using some reference point.
--- modules/common/src/test/java/org/dcache/util/TimeUtilsTest.java
+++ modules/common/src/test/java/org/dcache/util/TimeUtilsTest.java
@@ -1,7 +1,11 @@
 package org.dcache.util;
 
-import java.util.concurrent.TimeUnit;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.dcache.util.TimeUtils.DurationParser;
+
 import static org.junit.Assert.*;
 
 
@@ -54,5 +58,78 @@ public class TimeUtilsTest {
         long duration = TimeUnit.DAYS.toNanos(13);
         CharSequence cs = TimeUtils.duration(duration, TimeUnit.NANOSECONDS, TimeUtils.TimeUnitFormat.SHORT);
         assertEquals(cs.toString(), "13 days");
+    }
+
+    @Test
+    public void testFormattingFromDaysNoLeadingZeros() {
+        String formatted = getFormattedDuration("%D %H:%m:%s.%N");
+        assertEquals(formatted, "9 19:3:57.55100");
+    }
+
+    @Test
+    public void testFormattingFromDays() {
+        String formatted = getFormattedDuration("%D %HH:%mm:%ss.%N");
+        assertEquals(formatted, "9 19:03:57.55100");
+    }
+
+    @Test
+    public void testFormattingFromHours() {
+        String formatted = getFormattedDuration("%HH:%mm:%ss.%N");
+        assertEquals(formatted, "235:03:57.55100");
+    }
+
+    @Test
+    public void testFormattingFromMinutes() {
+        String formatted = getFormattedDuration("%mm minutes, %ss.%N seconds");
+        assertEquals(formatted, "14103 minutes, 57.55100 seconds");
+    }
+
+    @Test
+    public void testFormattingFromSeconds() {
+        String formatted = getFormattedDuration("%ss.%N seconds");
+        assertEquals(formatted, "846237.55100 seconds");
+    }
+
+    @Test
+    public void testScrambledFormatting() {
+        String formatted = getFormattedDuration("%HH hours, %D days, %s.%N seconds, %m minutes");
+        assertEquals(formatted, "19 hours, 9 days, 57.55100 seconds, 3 minutes");
+    }
+
+    @Test
+    public void testRepeatedFormatting() {
+        DurationParser parser = new DurationParser(846237551L, TimeUnit.MILLISECONDS);
+        parser.parse(TimeUnit.HOURS);
+        parser.parse(TimeUnit.SECONDS);
+        assertEquals(parser.get(TimeUnit.HOURS), 235);
+        assertEquals(parser.get(TimeUnit.SECONDS), 237);
+        parser.clear();
+        parser.parseAll();
+        assertEquals(parser.get(TimeUnit.HOURS), 19);
+    }
+
+    public void testStrictlyDecreasing() {
+        DurationParser parser = new DurationParser(846237551L, TimeUnit.MILLISECONDS);
+        parser.parse(TimeUnit.HOURS);
+        parser.parse(TimeUnit.SECONDS);
+        IllegalStateException exception = null;
+        try {
+            parser.parse(TimeUnit.MINUTES);
+        } catch (IllegalStateException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+    }
+
+    @Test
+    public void testFormattingWithGaps() {
+        String formatted = getFormattedDuration("%H hours, %s.%N seconds");
+        assertEquals(formatted, "235 hours, 237.55100 seconds");
+    }
+
+    private String getFormattedDuration(String format) {
+        long duration = 846237551L;
+        TimeUnit durationUnit = TimeUnit.MILLISECONDS;
+        return TimeUtils.getFormattedDuration(duration, durationUnit, format);
     }
 }
