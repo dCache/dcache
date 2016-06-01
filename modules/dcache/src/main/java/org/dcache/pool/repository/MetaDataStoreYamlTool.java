9fff7493b16e6ad754ca5624820a5e9eb191a043
Gerd Behrmann <behrmann@gmail.com>
2016-06-01 09:48:48 +0200

    pool: Close stores after use in meta data utilities
    Motivation:
    
    Neither \`dcache pool convert\` nor \`dcache pool yaml\` properly
    closed the store after use.
    
    Modification:
    
    Let MetaDataStore extend Closeable. Let the two utilities close
    the stores after use.
    
    Result:
    
    Fixed a problem in which the meta data store was not closed properly
    in the \`dcache pool convert\` and \`dcache pool yaml\` utilities.
    
    Target: trunk
    Require-notes: yes
    Require-book: no
    Request: 2.16
    Request: 2.15
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: https://rb.dcache.org/r/9362
    (cherry picked from commit 86c8c322cc1e9b260333519e99e73602e23ed97e)

--- modules/dcache/src/main/java/org/dcache/pool/repository/MetaDataStoreYamlTool.java
+++ modules/dcache/src/main/java/org/dcache/pool/repository/MetaDataStoreYamlTool.java
@@ -39,64 +39,65 @@ public class MetaDataStoreYamlTool
 
         File poolDir = new File(args[0]);
         FileStore fileStore = new DummyFileStore();
-        MetaDataStore metaStore =
-            createStore(Class.forName(args[1]).asSubclass(MetaDataStore.class), fileStore, poolDir);
-        metaStore.init();
+        try (MetaDataStore metaStore =
+                     createStore(Class.forName(args[1]).asSubclass(MetaDataStore.class), fileStore, poolDir)) {
+            metaStore.init();
 
-        PrintWriter out = new PrintWriter(System.out);
-        PrintWriter error = new PrintWriter(System.err);
-        for (PnfsId id: metaStore.index(MetaDataStore.IndexOption.META_ONLY)) {
-            try {
-                MetaDataRecord record = metaStore.get(id);
-                if (record == null) {
-                    continue;
-                }
-                FileAttributes attributes = record.getFileAttributes();
+            PrintWriter out = new PrintWriter(System.out);
+            PrintWriter error = new PrintWriter(System.err);
+            for (PnfsId id : metaStore.index(MetaDataStore.IndexOption.META_ONLY)) {
+                try {
+                    MetaDataRecord record = metaStore.get(id);
+                    if (record == null) {
+                        continue;
+                    }
+                    FileAttributes attributes = record.getFileAttributes();
 
-                out.format("%s:\n", id);
-                out.format("  state: %s\n", record.getState());
-                out.format("  sticky:\n");
-                for (StickyRecord sticky : record.stickyRecords()) {
-                    out.format("    %s: %d\n", sticky.owner(), sticky.expire());
-                }
-                if (attributes.isDefined(FileAttribute.STORAGECLASS)) {
-                    out.format("  storageclass: %s\n", attributes.getStorageClass());
-                }
-                if (attributes.isDefined(FileAttribute.CACHECLASS)) {
-                    out.format("  cacheclass: %s\n", attributes.getCacheClass());
-                }
-                if (attributes.isDefined(FileAttribute.STORAGEINFO)) {
-                    StorageInfo info = attributes.getStorageInfo();
-                    out.format("  bitfileid: %s\n", info.getBitfileId());
-                    out.format("  locations:\n");
-                    for (URI location : info.locations()) {
-                        out.format("    - %s\n", location);
+                    out.format("%s:\n", id);
+                    out.format("  state: %s\n", record.getState());
+                    out.format("  sticky:\n");
+                    for (StickyRecord sticky : record.stickyRecords()) {
+                        out.format("    %s: %d\n", sticky.owner(), sticky.expire());
                     }
-                }
-                if (attributes.isDefined(FileAttribute.HSM)) {
-                    out.format("  hsm: %s\n", attributes.getHsm());
-                }
-                if (attributes.isDefined(FileAttribute.SIZE)) {
-                    out.format("  filesize: %s\n", attributes.getSize());
-                }
-                if (attributes.isDefined(FileAttribute.FLAGS)) {
-                    out.format("  map:\n");
-                    for (Map.Entry<String, String> entry : attributes.getFlags().entrySet()) {
-                        out.format("    %s: %s\n", entry.getKey(), entry.getValue());
+                    if (attributes.isDefined(FileAttribute.STORAGECLASS)) {
+                        out.format("  storageclass: %s\n", attributes.getStorageClass());
                     }
+                    if (attributes.isDefined(FileAttribute.CACHECLASS)) {
+                        out.format("  cacheclass: %s\n", attributes.getCacheClass());
+                    }
+                    if (attributes.isDefined(FileAttribute.STORAGEINFO)) {
+                        StorageInfo info = attributes.getStorageInfo();
+                        out.format("  bitfileid: %s\n", info.getBitfileId());
+                        out.format("  locations:\n");
+                        for (URI location : info.locations()) {
+                            out.format("    - %s\n", location);
+                        }
+                    }
+                    if (attributes.isDefined(FileAttribute.HSM)) {
+                        out.format("  hsm: %s\n", attributes.getHsm());
+                    }
+                    if (attributes.isDefined(FileAttribute.SIZE)) {
+                        out.format("  filesize: %s\n", attributes.getSize());
+                    }
+                    if (attributes.isDefined(FileAttribute.FLAGS)) {
+                        out.format("  map:\n");
+                        for (Map.Entry<String, String> entry : attributes.getFlags().entrySet()) {
+                            out.format("    %s: %s\n", entry.getKey(), entry.getValue());
+                        }
+                    }
+                    if (attributes.isDefined(FileAttribute.RETENTION_POLICY)) {
+                        out.format("  retentionpolicy: %s\n", attributes.getRetentionPolicy());
+                    }
+                    if (attributes.isDefined(FileAttribute.ACCESS_LATENCY)) {
+                        out.format("  accesslatency: %s\n", attributes.getAccessLatency());
+                    }
+                } catch (CacheException e) {
+                    error.println("Failed to read " + id + ": " + e.getMessage());
                 }
-                if (attributes.isDefined(FileAttribute.RETENTION_POLICY)) {
-                    out.format("  retentionpolicy: %s\n", attributes.getRetentionPolicy());
-                }
-                if (attributes.isDefined(FileAttribute.ACCESS_LATENCY)) {
-                    out.format("  accesslatency: %s\n", attributes.getAccessLatency());
-                }
-            } catch (CacheException e) {
-                error.println("Failed to read " + id + ": " + e.getMessage());
             }
+            out.flush();
+            error.flush();
         }
-        out.flush();
-        error.flush();
     }
 
 }
