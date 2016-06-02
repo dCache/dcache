dCache/dcache

commit 8e4253dd3c85bbee77dc7302e0f9af0666302554

billing: re-implement population of aggregate tables
Motivation:

Billing relied on database triggers to populate aggregate daily summary
tables on inserts in per transaction based tables. Although the triggers have
been engineered to be executed once daily to minimize their impact on insert
rate into per transaction based tables, we still encountered issues when
transaction based tables are populated concurrently.

Modification:

This patch removes on insert triggers completely replacing them with a
background thread that populates aggregate tables once a day.

Result:

Eliminated impact on insert rate into per transaction based tables.

Target: trunk
Require-notes: no
Require-book: no
Request: 2.16
Acked-by: Gerd Behrmann <behrmann@gmail.com>
Acked-by: Paul Millar <paul.millar@desy.de>
Reviewed at https://rb.dcache.org/r/9313/

modules/dcache/src/main/java/org/dcache/services/billing/db/impl/datanucleus/DataNucleusBillingInfo.java
@@ -255,4 +255,29 @@ public class DataNucleusBillingInfo extends AbstractBillingInfoAccess {
     {
         this.pmf = pmf;
     }
+
+
+    @Override
+    public void aggregateDaily()
+    {
+        executeStoredProcedure("f_billing_daily_summary()");
+    }
+
+    private void executeStoredProcedure(String name)
+    {
+        PersistenceManager pm = pmf.getPersistenceManager();
+        Transaction tx = pm.currentTransaction();
+        try {
+            tx.begin();
+            Query query = pm.newQuery("javax.jdo.query.SQL","SELECT "+name);
+            query.execute();
+            tx.commit();
+        } finally {
+            try {
+                rollbackIfActive(tx);
+            } finally {
+                pm.close();
+            }
+        }
+    }
 }

modules/dcache/src/main/java/org/dcache/services/billing/db/IBillingInfoAccess.java
@@ -149,4 +149,6 @@ public interface IBillingInfoAccess {
      */
     <T> long remove(Class<T> type, String filter, String parameters,
                     Object... values);
+
+    void aggregateDaily();
 }

modules/dcache/src/main/resources/META-INF/persistence.xml
@@ -58,6 +58,7 @@
             <property name="datanucleus.persistenceByReachabilityAtCommit" value="false"/>
             <property name="datanucleus.rdbms.stringLengthExceededAction" value="TRUNCATE"/>
             <property name="datanucleus.query.jdoql.allowAll" value="true"/>
+            <property name="datanucleus.query.sql.allowAll" value="true"/>
             <property name="javax.jdo.option.Optimistic" value="true"/>
             <property name="javax.jdo.option.NontransactionalRead" value="false"/>
             <property name="javax.jdo.option.RetainValues" value="true"/>

modules/dcache/src/main/resources/org/dcache/services/billing/cells/billing.xml
@@ -8,6 +8,7 @@
        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
        xmlns:jee="http://www.springframework.org/schema/jee"
        xmlns:context="http://www.springframework.org/schema/context"
+       xmlns:task="http://www.springframework.org/schema/task"
        xmlns:util="http://www.springframework.org/schema/util"
        xsi:schemaLocation="http://www.springframework.org/schema/jee
                         http://www.springframework.org/schema/jee/spring-jee.xsd
@@ -15,6 +16,8 @@
                         http://www.springframework.org/schema/beans/spring-beans.xsd
                         http://www.springframework.org/schema/context
                         http://www.springframework.org/schema/context/spring-context.xsd
+			http://www.springframework.org/schema/task
+			http://www.springframework.org/schema/task/spring-task.xsd
                         http://www.springframework.org/schema/util
                         http://www.springframework.org/schema/util/spring-util.xsd">
 
@@ -101,5 +104,12 @@
       <property name="changeLog" value="classpath:${billing.db.schema.changelog}"/>
       <property name="shouldUpdate" value="${billing.db.schema.auto}"/>
     </bean>
+
+    <task:scheduled-tasks scheduler="scheduler">
+      <task:scheduled ref="jdbc-billing-info-access" method="aggregateDaily" initial-delay="10000" fixed-delay="#{ T(java.util.concurrent.TimeUnit).DAYS.toMillis(1) }"/>
+    </task:scheduled-tasks>
+
+    <task:scheduler id="scheduler" pool-size="1"/>
   </beans>
+
 </beans>

commit 95b114ef2f650e668fc1e0273e7d7de5b9272dbc

billing : fix issue wih HSQLDB backend
Motivation:

Commit 8e4253dd3c85bbee77dc7302e0f9af0666302554 caused billing to break when
running with HSQLDB backend.

Modification:

This patch fixes the issue by executing "call <procedure name>" if "select <procedure_name>"
has failed, thus covering HSQLDB (and any other RDBMS flavor that uses CALL to invoke stored
procedures).

Result:

works with HSQLDB

Acked-by: Albert Rossi <arossi@fnal.gov>
Acked-by: Gerd Behrmann <behrmann@gmail.com>
Target: trunk
Request: 2.16
Require-notes: no
Require-book: no
Fixes: #2500
Reviewed at https://rb.dcache.org/r/9367/

modules/dcache/src/main/java/org/dcache/services/billing/db/impl/datanucleus/DataNucleusBillingInfo.java
@@ -64,6 +64,7 @@ import org.datanucleus.FetchPlan;
 import java.util.Arrays;
 import java.util.Collection;
 import javax.jdo.JDOCanRetryException;
+import javax.jdo.JDODataStoreException;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Query;
@@ -269,8 +270,27 @@ public class DataNucleusBillingInfo extends AbstractBillingInfoAccess {
         Transaction tx = pm.currentTransaction();
         try {
             tx.begin();
+            /* TODO: the recommended recipe to execute a stored procedure
+             *       using DataNucleus JDO API is to call
+             *
+             *  Query query = pm.newQuery("STOREDPROC",name);
+             *
+             * http://www.datanucleus.org/products/datanucleus/jdo/stored_procedures.html
+             *
+             * Unfortunately currently this call translates into "CALL name" query
+             * on a DB backend and therefore does not work for postgresql
+             *
+             *  http://www.datanucleus.org/servlet/forum/viewthread_thread,7968
+             *
+             * until this is fixed, a hack is below
+             */
             Query query = pm.newQuery("javax.jdo.query.SQL","SELECT "+name);
-            query.execute();
+            try {
+                query.execute();
+            } catch (JDODataStoreException ignore) {
+                query = pm.newQuery("javax.jdo.query.SQL","CALL "+name);
+                query.execute();
+            }
             tx.commit();
         } finally {
             try {