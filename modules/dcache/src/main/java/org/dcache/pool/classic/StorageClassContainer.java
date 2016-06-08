dCache/dcache

commit 69cc5b9224282eabadc842fe5c930049373770d6

Resilience, Version 2, March 2016
Full commit of all modifications and additions, including junit tests.

modules/dcache/src/main/java/org/dcache/pool/classic/StorageClassContainer.java
@@ -258,7 +258,7 @@ public class StorageClassContainer
             append("\n");
     }
 
-    @Command(name = "queue activate",
+    @Command(name = "queue setIncluded",
             description = "Move a file from FAILED to ACTIVE.")
     class ActivateFileCommand implements Callable<String>
     {
@@ -277,7 +277,7 @@ public class StorageClassContainer
         }
     }
 
-    @Command(name = "queue activate class",
+    @Command(name = "queue setIncluded class",
             description = "Move files of a storage class from FAILED to ACTIVE.")
     class ActivateClassCommand implements Callable<String>
     {

commit 6f3dfc7950888740f618e4a1f70f53dc1285be09

pools: restore correct command names
Motivation:

Refactoring done in conjunction with commit 69cc5b9224282eabadc842fe5c930049373770d6
accidentally altered two command names in the pool StorageClassContainer.

Modification:

Restored to original name.

Result:

Commands are correctly named.

Target: master
Request: 2.16
Require-notes: yes
Require-book: no
Fixes: #2518
Acked-by: Gerd
Acked-by: Paul

modules/dcache/src/main/java/org/dcache/pool/classic/StorageClassContainer.java
@@ -272,7 +272,7 @@ public class StorageClassContainer
             append("\n");
     }
 
-    @Command(name = "queue setIncluded",
+    @Command(name = "queue activate",
             description = "Move a file from FAILED to ACTIVE.")
     class ActivateFileCommand implements Callable<String>
     {
@@ -291,7 +291,7 @@ public class StorageClassContainer
         }
     }
 
-    @Command(name = "queue setIncluded class",
+    @Command(name = "queue activate class",
             description = "Move files of a storage class from FAILED to ACTIVE.")
     class ActivateClassCommand implements Callable<String>
     {