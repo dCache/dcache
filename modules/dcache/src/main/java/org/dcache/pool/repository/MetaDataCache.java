dCache/dcache

commit 7d600c54a5b9b558ee46bf6992558f3b5e6b3b5b

pool: Refactor meta data store interface to allow bulk updates
Motivation:

Often we update several attributes together, yet these are individual
updates in the store and cause syncs to disk every time. Such updates
can already be made atomic by synchronizing on the entry, but there
is no way for the underlying storage to batch together the update to
persistent state.

Modification:

Introduced an update method to MetaDataRecord. The argument is a
callback that is given an updatable version of the meta data record.
The callback may make one or more changes to the record.

This change does not imply that other aspects of the meta data record are not
modifiable through the regular interface. The callback is only for attributes
that are typically updated together. It is even assymetric in the sense that
the linkcount is incremented through the callback, but decrement outside.
Although maybe not aesthetically pleasing, it reflects how the interface is
used.

For convenience, the callback has also access to a number of getters.

This patch doesn't actually utilize this to do any bulk updates to
the persistent state. That will be done in subsequent patches.

Result:

No user visible changes.

Target: trunk
Require-notes: no
Require-book: no
Acked-by: Paul Millar <paul.millar@desy.de>

Reviewed at https://rb.dcache.org/r/9402/

modules/dcache/src/main/java/org/dcache/pool/repository/MetaDataCache.java
@@ -159,24 +159,22 @@ public class MetaDataCache
         }
 
         @GuardedBy("this")
-        private void destroyIfRemoved()
+        private void destroy()
         {
-            if (_record.getLinkCount() == 0 && _record.getState() == EntryState.REMOVED) {
-                assert _entries.get(_id) == this;
-                try {
-                    CacheEntry entry = new CacheEntryImpl(_record);
-                    _record.setState(DESTROYED);
-                    _inner.remove(_id);
-                    _entries.remove(_id);
-                    _stateChangeListener.stateChanged(
-                            new StateChangeEvent(entry, entry, entry.getState(), DESTROYED));
-                } catch (DiskErrorCacheException | RuntimeException e) {
-                    _faultListener.faultOccurred(
-                            new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e));
-                } catch (CacheException e) {
-                    _faultListener.faultOccurred(
-                            new FaultEvent("repository", FaultAction.READONLY, "Internal repository error", e));
-                }
+            assert _entries.get(_id) == this;
+            try {
+                CacheEntry entry = new CacheEntryImpl(_record);
+                _record.update(r -> r.setState(DESTROYED));
+                _inner.remove(_id);
+                _entries.remove(_id);
+                _stateChangeListener.stateChanged(
+                        new StateChangeEvent(entry, entry, entry.getState(), DESTROYED));
+            } catch (DiskErrorCacheException | RuntimeException e) {
+                _faultListener.faultOccurred(
+                        new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e));
+            } catch (CacheException e) {
+                _faultListener.faultOccurred(
+                        new FaultEvent("repository", FaultAction.READONLY, "Internal repository error", e));
             }
         }
 
@@ -218,46 +216,11 @@ public class MetaDataCache
         }
 
         @Override
-        public void setFileAttributes(FileAttributes attributes) throws CacheException
-        {
-            try {
-                _record.setFileAttributes(attributes);
-            } catch (IllegalArgumentException e) {
-                throw e;
-            } catch (RuntimeException | DiskErrorCacheException e) {
-                _faultListener.faultOccurred(
-                        new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e));
-                throw e;
-            }
-        }
-
-        @Override
         public FileAttributes getFileAttributes() throws CacheException
         {
             try {
                 return _record.getFileAttributes();
             } catch (RuntimeException | DiskErrorCacheException e) {
-                _faultListener.faultOccurred(
-                        new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e));
-                throw e;
-            }
-        }
-
-        @Override
-        public synchronized void setState(EntryState state) throws CacheException
-        {
-            try {
-                if (_record.getState() != state) {
-                    CacheEntry oldEntry = new CacheEntryImpl(_record);
-                    _record.setState(state);
-                    CacheEntry newEntry = new CacheEntryImpl(_record);
-                    _stateChangeListener.stateChanged(
-                            new StateChangeEvent(oldEntry, newEntry, oldEntry.getState(), newEntry.getState()));
-                    destroyIfRemoved();
-                }
-            } catch (IllegalArgumentException | IllegalStateException e) {
-                throw e;
-            } catch (RuntimeException | CacheException e) {
                 _faultListener.faultOccurred(
                         new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e));
                 throw e;
@@ -342,22 +305,20 @@ public class MetaDataCache
         }
 
         @Override
-        public synchronized void decrementLinkCount()
+        public synchronized int decrementLinkCount()
         {
-            try {
-                _record.decrementLinkCount();
-                destroyIfRemoved();
-            } catch (RuntimeException e) {
-                _faultListener.faultOccurred(
-                        new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e));
+            int cnt = _record.decrementLinkCount();
+            if (cnt == 0 && _record.getState() == EntryState.REMOVED) {
+                destroy();
             }
+            return cnt;
         }
 
         @Override
-        public void incrementLinkCount()
+        public int incrementLinkCount()
         {
             try {
-                _record.incrementLinkCount();
+                return _record.incrementLinkCount();
             } catch (RuntimeException e) {
                 _faultListener.faultOccurred(
                         new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e));
@@ -408,26 +369,6 @@ public class MetaDataCache
         }
 
         @Override
-        public synchronized boolean setSticky(String owner, long validTill, boolean overwrite) throws CacheException
-        {
-            try {
-                CacheEntry oldEntry = new CacheEntryImpl(_record);
-                boolean changed = _record.setSticky(owner, validTill, overwrite);
-                if (changed) {
-                    CacheEntryImpl newEntry = new CacheEntryImpl(_record);
-                    _stateChangeListener.stickyChanged(new StickyChangeEvent(oldEntry, newEntry));
-                }
-                return changed;
-            } catch (IllegalArgumentException e) {
-                throw e;
-            } catch (RuntimeException | DiskErrorCacheException e) {
-                _faultListener.faultOccurred(
-                        new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e));
-                throw e;
-            }
-        }
-
-        @Override
         public Collection<StickyRecord> stickyRecords()
         {
             try {
@@ -435,6 +376,77 @@ public class MetaDataCache
             } catch (RuntimeException e) {
                 _faultListener.faultOccurred(
                         new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e));
+                throw e;
+            }
+        }
+
+        @Override
+        public synchronized <T> T update(Update<T> update) throws CacheException
+        {
+            try {
+                return _record.update(
+                        r -> update.apply(
+                                new UpdatableRecord()
+                                {
+                                    @Override
+                                    public boolean setSticky(String owner, long validTill,
+                                                             boolean overwrite) throws CacheException
+                                    {
+                                        CacheEntry oldEntry = new CacheEntryImpl(_record);
+                                        boolean changed = r.setSticky(owner, validTill, overwrite);
+                                        if (changed) {
+                                            CacheEntryImpl newEntry = new CacheEntryImpl(_record);
+                                            _stateChangeListener.stickyChanged(new StickyChangeEvent(oldEntry, newEntry));
+                                        }
+                                        return changed;
+                                    }
+
+                                    @Override
+                                    public Void setState(EntryState state) throws CacheException
+                                    {
+                                        if (r.getState() != state) {
+                                            CacheEntry oldEntry = new CacheEntryImpl(_record);
+                                            r.setState(state);
+                                            CacheEntry newEntry = new CacheEntryImpl(_record);
+                                            _stateChangeListener.stateChanged(
+                                                    new StateChangeEvent(oldEntry, newEntry, oldEntry.getState(),
+                                                                         newEntry.getState()));
+                                            if (r.getLinkCount() == 0 && state == EntryState.REMOVED) {
+                                                destroy();
+                                            }
+                                        }
+                                        return null;
+                                    }
+
+                                    @Override
+                                    public Void setFileAttributes(FileAttributes attributes) throws CacheException
+                                    {
+                                        return r.setFileAttributes(attributes);
+                                    }
+
+                                    @Override
+                                    public FileAttributes getFileAttributes() throws CacheException
+                                    {
+                                        return r.getFileAttributes();
+                                    }
+
+                                    @Override
+                                    public EntryState getState()
+                                    {
+                                        return r.getState();
+                                    }
+
+                                    @Override
+                                    public int getLinkCount()
+                                    {
+                                        return r.getLinkCount();
+                                    }
+                                }));
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                throw e;
+            } catch (RuntimeException | CacheException e) {
+                FaultEvent event = new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e);
+                _faultListener.faultOccurred(event);
                 throw e;
             }
         }

modules/dcache/src/main/java/org/dcache/pool/repository/ConsistentStore.java
@@ -164,7 +164,7 @@ public class ConsistentStore
                     throw e;
 
                 default:
-                    entry.setState(EntryState.BROKEN);
+                    entry.update(r -> r.setState(EntryState.BROKEN));
                     _log.error(AlarmMarkerFactory.getMarker(PredefinedAlarm.BROKEN_FILE,
                                     id.toString(),
                                     _poolName),
@@ -172,7 +172,7 @@ public class ConsistentStore
                     break;
                 }
             } catch (NoSuchAlgorithmException e) {
-                entry.setState(EntryState.BROKEN);
+                entry.update(r -> r.setState(EntryState.BROKEN));
                 _log.error(AlarmMarkerFactory.getMarker(PredefinedAlarm.BROKEN_FILE,
                                 id.toString(),
                                 _poolName),
@@ -301,23 +301,25 @@ public class ConsistentStore
 
             /* Update the pool meta data.
              */
-            entry.setFileAttributes(attributesInNameSpace);
-
             /* If not already precious or cached, we move the entry to
              * the target state of a newly uploaded file.
              */
             if (state != EntryState.CACHED && state != EntryState.PRECIOUS) {
                 EntryState targetState =
-                    _replicaStatePolicy.getTargetState(attributesInNameSpace);
+                        _replicaStatePolicy.getTargetState(attributesInNameSpace);
                 List<StickyRecord> stickyRecords =
-                    _replicaStatePolicy.getStickyRecords(attributesInNameSpace);
-
-                for (StickyRecord record: stickyRecords) {
-                    entry.setSticky(record.owner(), record.expire(), false);
-                }
-
-                entry.setState(targetState);
+                        _replicaStatePolicy.getStickyRecords(attributesInNameSpace);
+                entry.update(r -> {
+                    r.setFileAttributes(attributesInNameSpace);
+                    for (StickyRecord record : stickyRecords) {
+                        r.setSticky(record.owner(), record.expire(), false);
+                    }
+                    r.setState(targetState);
+                    return null;
+                });
                 _log.warn(String.format(MARKED_MSG, id, targetState));
+            } else {
+                entry.update(r -> r.setFileAttributes(attributesInNameSpace));
             }
 
             return entry;

modules/dcache/src/main/java/org/dcache/pool/repository/MetaDataRecord.java
@@ -42,17 +42,11 @@ public interface MetaDataRecord
      */
     long getSize();
 
-    void setFileAttributes(FileAttributes attributes) throws CacheException;
-
     FileAttributes getFileAttributes() throws CacheException;
-
-    void setState(EntryState state)
-        throws CacheException;
 
     EntryState getState();
 
-    File getDataFile()
-            ;
+    File getDataFile();
 
     long getCreationTime();
 
@@ -62,9 +56,9 @@ public interface MetaDataRecord
 
     void touch() throws CacheException;
 
-    void decrementLinkCount();
+    int decrementLinkCount();
 
-    void incrementLinkCount();
+    int incrementLinkCount();
 
     int getLinkCount();
 
@@ -86,27 +80,71 @@ public interface MetaDataRecord
     Collection<StickyRecord> removeExpiredStickyFlags() throws CacheException;
 
     /**
-     * Set sticky flag for a given owner and time. There is at most
-     * one flag per owner. If <code>overwrite</code> is true, then an
-     * existing record for <code>owner</code> will be replaced. If it
-     * is false, then the lifetime of an existing record will be
-     * extended if and only if the new lifetime is longer.
-     *
-     * A lifetime of -1 indicates that the flag never expires. A
-     * lifetime set in the past, for instance 0, expires immediately.
-     *
-     * @param owner flag owner
-     * @param validTill time milliseconds since 00:00:00 1 Jan. 1970.
-     * @param overwrite replace existing flag when true.
-     * @throws CacheException
-     * @return true if the collection returned by the stickyRecords
-     * method has changed due to this call.
-     */
-    boolean setSticky(String owner, long validTill, boolean overwrite)
-        throws CacheException;
-
-    /**
      * @return list of StickyRecords held by the file
      */
     Collection<StickyRecord> stickyRecords();
+
+    /**
+     * Bulk update one or more attributes.
+     *
+     * Callers provide a callback that is called with an UpdatableRecord which
+     * provides setters to attributes commonly updated together.
+     *
+     * The change will be applied atomically (in the sense that other threads will not
+     * see the change until the method returns). If the callback throws an exception,
+     * the changes applied prior to the exception are not rolled back.
+     *
+     * The callback may be called with a lock held or within a transaction. The
+     * callback should be fast and with minimal access to other resources to
+     * avoid the risk of causing deadlocks.
+     *
+     * @return The return value of {@code Update#apply}
+     * @param update
+     */
+    <T> T update(Update<T> update) throws CacheException;
+
+    /**
+     * Callback interface used by {@code update}.
+     */
+    interface Update<T>
+    {
+        T apply(UpdatableRecord record) throws CacheException;
+    }
+
+    /**
+     * Interface that provides means to modify attributes commonly modified
+     * together.
+     */
+    interface UpdatableRecord
+    {
+        /**
+         * Set sticky flag for a given owner and time. There is at most
+         * one flag per owner. If <code>overwrite</code> is true, then an
+         * existing record for <code>owner</code> will be replaced. If it
+         * is false, then the lifetime of an existing record will be
+         * extended if and only if the new lifetime is longer.
+         *
+         * A lifetime of -1 indicates that the flag never expires. A
+         * lifetime set in the past, for instance 0, expires immediately.
+         *
+         * @param owner flag owner
+         * @param validTill time milliseconds since 00:00:00 1 Jan. 1970.
+         * @param overwrite replace existing flag when true.
+         * @throws CacheException
+         * @return true if the collection returned by the stickyRecords
+         * method has changed due to this call.
+         */
+        boolean setSticky(String owner, long validTill, boolean overwrite)
+                throws CacheException;
+
+        Void setState(EntryState state) throws CacheException;
+
+        Void setFileAttributes(FileAttributes attributes) throws CacheException;
+
+        FileAttributes getFileAttributes() throws CacheException;
+
+        EntryState getState();
+
+        int getLinkCount();
+    }
 }

modules/dcache/src/main/java/org/dcache/pool/repository/meta/db/CacheRepositoryEntryImpl.java
@@ -30,7 +30,7 @@ import static com.google.common.collect.Iterables.*;
 /**
  * Berkeley DB aware implementation of CacheRepositoryEntry interface.
  */
-public class CacheRepositoryEntryImpl implements MetaDataRecord
+public class CacheRepositoryEntryImpl implements MetaDataRecord, MetaDataRecord.UpdatableRecord
 {
     private static final Logger _log =
         LoggerFactory.getLogger(CacheRepositoryEntryImpl.class);
@@ -113,22 +113,24 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     }
 
     @Override
-    public synchronized void decrementLinkCount()
+    public synchronized int decrementLinkCount()
     {
         if (_linkCount <= 0) {
             throw new IllegalStateException("Link count is already zero");
         }
         _linkCount--;
+        return _linkCount;
     }
 
     @Override
-    public synchronized void incrementLinkCount()
+    public synchronized int incrementLinkCount()
     {
         EntryState state = getState();
         if (state == EntryState.REMOVED || state == EntryState.DESTROYED) {
             throw new IllegalStateException("Entry is marked as removed");
         }
         _linkCount++;
+        return _linkCount;
     }
 
     @Override
@@ -206,7 +208,7 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     }
 
     @Override
-    public void setFileAttributes(FileAttributes attributes) throws CacheException
+    public Void setFileAttributes(FileAttributes attributes) throws CacheException
     {
         try {
             String id = _pnfsId.toString();
@@ -223,6 +225,7 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
         } catch (OperationFailureException e) {
             throw new CacheException("Meta data update failed: " + e.getMessage(), e);
         }
+        return null;
     }
 
     @Override
@@ -238,12 +241,13 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     }
 
     @Override
-    public synchronized void setState(EntryState state) throws CacheException
+    public Void setState(EntryState state) throws CacheException
     {
         if (_state != state) {
             _state = state;
             storeState();
         }
+        return null;
     }
 
     @Override
@@ -271,7 +275,7 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     }
 
     @Override
-    public synchronized boolean setSticky(String owner, long expire, boolean overwrite) throws CacheException
+    public boolean setSticky(String owner, long expire, boolean overwrite) throws CacheException
     {
         if (_state == EntryState.REMOVED) {
             throw new CacheException("Entry in removed state");
@@ -306,6 +310,12 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     public synchronized Collection<StickyRecord> stickyRecords()
     {
         return _sticky;
+    }
+
+    @Override
+    public synchronized <T> T update(Update<T> update) throws CacheException
+    {
+        return update.apply(this);
     }
 
     private synchronized void storeState() throws CacheException

modules/dcache/src/main/java/org/dcache/pool/repository/meta/file/CacheRepositoryEntryImpl.java
@@ -3,6 +3,8 @@ package org.dcache.pool.repository.meta.file;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.concurrent.GuardedBy;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -29,7 +31,7 @@ import org.dcache.pool.repository.v3.RepositoryException;
 import org.dcache.pool.repository.v3.entry.CacheRepositoryEntryState;
 import org.dcache.vehicles.FileAttributes;
 
-public class CacheRepositoryEntryImpl implements MetaDataRecord
+public class CacheRepositoryEntryImpl implements MetaDataRecord, MetaDataRecord.UpdatableRecord
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(CacheRepositoryEntryImpl.class);
     private final CacheRepositoryEntryState _state;
@@ -105,19 +107,21 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     }
 
     @Override
-    public synchronized void incrementLinkCount()
+    public synchronized int incrementLinkCount()
     {
         _linkCount++;
+        return _linkCount;
     }
 
     @Override
-    public synchronized void decrementLinkCount()
+    public synchronized int decrementLinkCount()
     {
 
         if (_linkCount <= 0) {
             throw new IllegalStateException("Link count is already  zero");
         }
         _linkCount--;
+        return _linkCount;
     }
 
     @Override
@@ -181,7 +185,7 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     }
 
     @Override
-    public synchronized void setState(EntryState state)
+    public synchronized Void setState(EntryState state)
         throws CacheException
     {
         try {
@@ -189,6 +193,7 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
         } catch (IOException e) {
             throw new DiskErrorCacheException(e.getMessage(), e);
         }
+        return null;
     }
 
     @Override
@@ -202,7 +207,7 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     }
 
     @Override
-    public synchronized boolean setSticky(String owner, long expire, boolean overwrite) throws CacheException {
+    public boolean setSticky(String owner, long expire, boolean overwrite) throws CacheException {
         try {
             if (_state.setSticky(owner, expire, overwrite)) {
                 return true;
@@ -226,7 +231,7 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     }
 
     @Override
-    public void setFileAttributes(FileAttributes attributes) throws CacheException {
+    public Void setFileAttributes(FileAttributes attributes) throws CacheException {
         try {
             if (attributes.isDefined(FileAttribute.STORAGEINFO)) {
                 setStorageInfo(StorageInfos.extractFrom(attributes));
@@ -236,6 +241,7 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
         } catch (IOException e) {
             throw new DiskErrorCacheException(_pnfsId + " " + e.getMessage(), e);
         }
+        return null;
     }
 
     @Override
@@ -284,5 +290,11 @@ public class CacheRepositoryEntryImpl implements MetaDataRecord
     @Override
     public synchronized Collection<StickyRecord> stickyRecords() {
         return _state.stickyRecords();
+    }
+
+    @Override
+    public synchronized <T> T update(Update<T> update) throws CacheException
+    {
+        return update.apply(this);
     }
 }

commit 13a1c7aac39fafb690ed0d74ae4be6f984af98d0

pool: Fix bug that disables pools on non-critical errors
Motivation:

MetaDataCache has a listener interface that notifies observes of changes and
critical faults. A bug introduced in 7d600c54a5b9b558ee46bf6992558f3b5e6b3b5b
causes it to misqualify a simple CacheException as a critical error, which in
turn disables the pool.

Modification:

Only classify DiskErroCacheException as a critical error.

Result:

Fixed a regression introduced in 7d600c54a5b9b558ee46bf6992558f3b5e6b3b5b.

Target: trunk
Require-notes: no
Require-book: no
Fixes: #2571
Acked-by: Albert Rossi <arossi@fnal.gov>
Acked-by: Paul Millar <paul.millar@desy.de>

Reviewed at https://rb.dcache.org/r/9443/

modules/dcache/src/main/java/org/dcache/pool/repository/MetaDataCache.java
@@ -429,7 +429,7 @@ public class MetaDataCache
                                 }));
             } catch (IllegalArgumentException | IllegalStateException e) {
                 throw e;
-            } catch (RuntimeException | CacheException e) {
+            } catch (RuntimeException | DiskErrorCacheException e) {
                 FaultEvent event = new FaultEvent("repository", FaultAction.DEAD, "Internal repository error", e);
                 _faultListener.faultOccurred(event);
                 throw e;