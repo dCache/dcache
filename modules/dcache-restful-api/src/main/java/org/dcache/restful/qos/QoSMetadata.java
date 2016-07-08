2fdbaf39c2bda7039f24031fdc70f1c6fbbfe84d
Marina Sahakyan <marina.sahakyan@desy.de>
2016-07-08 12:04:56 +0200

    dcache-restful-api: fix data type for cdmi_geographic_placement
    Motivation:
    
         cdmi_geographic_placement should be Array of strings according to specification
    
    Modification & Result:
    
    Targeet: master
    Require-book: no
    Require-notes: no
    Patch: https://rb.dcache.org/r/9520/
    Acted by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- modules/dcache-restful-api/src/main/java/org/dcache/restful/qos/QoSMetadata.java
+++ modules/dcache-restful-api/src/main/java/org/dcache/restful/qos/QoSMetadata.java
@@ -1,5 +1,6 @@
 package org.dcache.restful.qos;
 
+import java.util.List;
 
 /**
  * This class represents data describing specific QoS based on CDMI specification.
@@ -7,12 +8,11 @@ package org.dcache.restful.qos;
 public class QoSMetadata {
 
     private String cdmi_data_redundancy_provided;
-
-    private String cdmi_geographic_placement_provided;
+    private List<String> cdmi_geographic_placement_provided;
     private String cdmi_latency_provided;
 
     public QoSMetadata(String cdmi_data_redundancy_provided,
-                       String cdmi_geographic_placement_provided,
+                       List<String> cdmi_geographic_placement_provided,
                        String cdmi_latency_provided) {
         this.cdmi_data_redundancy_provided = cdmi_data_redundancy_provided;
         this.cdmi_geographic_placement_provided = cdmi_geographic_placement_provided;
@@ -28,20 +28,20 @@ public class QoSMetadata {
         this.cdmi_data_redundancy_provided = cdmi_data_redundancy_provided;
     }
 
-    public String getCdmi_geographic_placement_provided() {
-        return cdmi_geographic_placement_provided;
-    }
-
-    public void setCdmi_geographic_placement_provided(String cdmi_geographic_placement_provided) {
-        this.cdmi_geographic_placement_provided = cdmi_geographic_placement_provided;
-    }
-
     public String getCdmi_latency_provided() {
         return cdmi_latency_provided;
     }
 
     public void setCdmi_latency_provided(String cdmi_latency_provided) {
         this.cdmi_latency_provided = cdmi_latency_provided;
+    }
+
+    public List<String> getCdmi_geographic_placement_provided() {
+        return cdmi_geographic_placement_provided;
+    }
+
+    public void setCdmi_geographic_placement_provided(List<String> cdmi_geographic_placement_provided) {
+        this.cdmi_geographic_placement_provided = cdmi_geographic_placement_provided;
     }
 
 }
