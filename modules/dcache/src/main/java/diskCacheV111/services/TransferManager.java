dCache/dcache

commit dbf3bf3c17a922dfcf7556dfaf54e6f3b76fb21a

transfermanager: Merge CopyManager and RemoteTranferManager
Motivation:

The transfermanagers service breaks our conventions by having two well-known
cells and thus not fitting well into our *.service.* convention. In particular
we got *.service.copymanager properties without a corresponding copymanager
service.

Modification:

Collapses the two cells into a single RemoteTransferManagers cell. A number
of runtime configuration commands are dropped.

Result:

The transfermanagers service now only provides the RemoteTransferManager cell;
the older CopyManagers cell is removed as the functionality is folded into
RemoteTransferManagers. The properties *.service.copymanager* are marked as
obsolete. Several admin shell commands of the RemoteTransferManager cell have
been renamed or removed.

Target: trunk
Require-notes: yes
Require-book: no
Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
Patch: https://rb.dcache.org/r/9223/

modules/dcache/src/main/java/diskCacheV111/services/TransferManager.java
@@ -133,9 +133,9 @@ public abstract class TransferManager extends AbstractCellComponent
         return "remote ftp transactions will be logged to " + _tLogRoot;
     }
 
-    public static final String hh_set_max_transfers = "<#max transfers>";
+    public static final String hh_set_max_transfers_external = "<#max transfers>";
 
-    public String ac_set_max_transfers_$_1(Args args)
+    public String ac_set_max_transfers_external_$_1(Args args)
     {
         int max = Integer.parseInt(args.argv(0));
         if (max <= 0) {
@@ -145,48 +145,9 @@ public abstract class TransferManager extends AbstractCellComponent
         return "set maximum number of active transfers to " + max;
     }
 
-    public static final String hh_set_pool_timeout = "<#seconds>";
+    public static final String hh_ls_external = "[-l] [<#transferId>]";
 
-    public String ac_set_pool_timeout_$_1(Args args)
-    {
-        int timeout = Integer.parseInt(args.argv(0));
-        if (timeout <= 0) {
-            return "Error, pool timeout should be greater then 0 ";
-        }
-        _poolStub.setTimeout(timeout);
-        _poolStub.setTimeoutUnit(SECONDS);
-        return "set pool timeout to " + timeout + " seconds";
-    }
-
-    public static final String hh_set_pool_manager_timeout = "<#seconds>";
-
-    public String ac_set_pool_manager_timeout_$_1(Args args)
-    {
-        int timeout = Integer.parseInt(args.argv(0));
-        if (timeout <= 0) {
-            return "Error, pool manger timeout should be greater then 0 ";
-        }
-        _poolManager.setTimeout(timeout);
-        _poolManager.setTimeoutUnit(SECONDS);
-        return "set pool manager timeout to " + timeout + " seconds";
-    }
-
-    public static final String hh_set_pnfs_manager_timeout = "<#seconds>";
-
-    public String ac_set_pnfs_manager_timeout_$_1(Args args)
-    {
-        int timeout = Integer.parseInt(args.argv(0));
-        if (timeout <= 0) {
-            return "Error, pnfs manger timeout should be greater then 0 ";
-        }
-        _pnfsManager.setTimeout(timeout);
-        _pnfsManager.setTimeoutUnit(SECONDS);
-        return "set pnfs manager timeout to " + timeout + " seconds";
-    }
-
-    public static final String hh_ls = "[-l] [<#transferId>]";
-
-    public String ac_ls_$_0_1(Args args)
+    public String ac_ls_external_$_0_1(Args args)
     {
         boolean long_format = args.hasOption("l");
         if (args.argc() > 0) {
@@ -265,31 +226,16 @@ public abstract class TransferManager extends AbstractCellComponent
         }
     }
 
-    public static final String hh_set_io_queue = "<io-queue name >";
-
-    public String ac_set_io_queue_$_1(Args args)
-    {
-        String newIoQueueName = args.argv(0);
-        if (newIoQueueName.equals("null")) {
-            _ioQueueName = null;
-            return "io-queue is set to null";
-        }
-        _ioQueueName = newIoQueueName;
-        return "io_queue was set to " + _ioQueueName;
-    }
-
-    public void messageArrived(CellMessage envelope, DoorTransferFinishedMessage message)
+    public void messageArrived(DoorTransferFinishedMessage message)
     {
         long id = message.getId();
         TransferManagerHandler h = getHandler(id);
         if (h != null) {
             h.poolDoorMessageArrived(message);
-        } else {
-            log.error("cannot find handler with id={} for DoorTransferFinishedMessage", id);
         }
     }
 
-    public CancelTransferMessage messageArrived(CellMessage envelope, CancelTransferMessage message)
+    public CancelTransferMessage messageArrived(CancelTransferMessage message)
     {
         long id = message.getId();
         TransferManagerHandler h = getHandler(id);
@@ -313,7 +259,7 @@ public abstract class TransferManager extends AbstractCellComponent
         return message;
     }
 
-    public Object messageArrived(CellMessage envelope, TransferStatusQueryMessage message)
+    public Object messageArrived(TransferStatusQueryMessage message)
     {
         TransferManagerHandler handler = getHandler(message.getId());
 

modules/dcache-srm/src/main/java/diskCacheV111/srm/dcache/Storage.java
@@ -272,7 +272,6 @@ public final class Storage
     private CellStub _pnfsStub;
     private CellStub _poolManagerStub;
     private CellStub _spaceManagerStub;
-    private CellStub _copyManagerStub;
     private CellStub _transferManagerStub;
     private CellStub _pinManagerStub;
     private CellStub _billingStub;
@@ -459,12 +458,6 @@ public final class Storage
     public void setTransferManagerStub(CellStub transferManagerStub)
     {
         _transferManagerStub = transferManagerStub;
-    }
-
-    @Required
-    public void setCopyManagerStub(CellStub copyManagerStub)
-    {
-        _copyManagerStub = copyManagerStub;
     }
 
     @Required
@@ -1431,7 +1424,7 @@ public final class Storage
                                        config.getBuffer_size(),
                                        config.getTcp_buffer_size());
             copyRequest.setSubject(asDcacheUser(user).getSubject());
-            _copyManagerStub.sendAndWait(copyRequest);
+            _transferManagerStub.sendAndWait(copyRequest);
         } catch (TimeoutCacheException e) {
             _log.error("CopyManager is unavailable");
             throw new SRMInternalErrorException("CopyManager is unavailable: " +

modules/dcache-srm/src/main/resources/diskCacheV111/srm/srm.xml
@@ -77,13 +77,6 @@
     <property name="timeoutUnit" value="${srm.service.transfermanager.timeout.unit}" />
   </bean>
 
-  <bean id="copy-manager-stub" class="org.dcache.cells.CellStub">
-    <description>Copy manager communication stub</description>
-    <property name="destination" value="${srm.service.copymanager}" />
-    <property name="timeout" value="${srm.service.copymanager.timeout}" />
-    <property name="timeoutUnit" value="${srm.service.copymanager.timeout.unit}" />
-  </bean>
-
   <bean id="pin-manager-stub" class="org.dcache.cells.CellStub">
     <description>Pin manager communication stub</description>
     <property name="destination" value="${srm.service.pinmanager}" />
@@ -228,7 +221,6 @@
     <property name="poolMonitor" ref="pool-monitor"/>
     <property name="spaceManagerStub" ref="space-manager-stub"/>
     <property name="transferManagerStub" ref="transfer-manager-stub"/>
-    <property name="copyManagerStub" ref="copy-manager-stub"/>
     <property name="pinManagerStub" ref="pin-manager-stub"/>
     <property name="billingStub" ref="billing-stub"/>
     <property name="isSpaceManagerEnabled"

modules/dcache/src/main/java/diskCacheV111/doors/CopyManager.java
@@ -65,8 +65,8 @@ public class CopyManager extends AbstractCellComponent
         _localAddr = new InetSocketAddress(InetAddress.getLocalHost(), 0);
     }
 
-    public static final String hh_set_max_transfers = "<#max transfers>";
-    public String ac_set_max_transfers_$_1(Args args)
+    public static final String hh_set_max_transfers_internal = "<#max transfers>";
+    public String ac_set_max_transfers_internal_$_1(Args args)
     {
         int max = Integer.parseInt(args.argv(0));
         if (max <= 0) {
@@ -76,44 +76,8 @@ public class CopyManager extends AbstractCellComponent
         return "set maximum number of active transfers to " + max;
     }
 
-    public static final String hh_set_mover_timeout = "<#seconds>";
-    public String ac_set_mover_timeout_$_1(Args args)
-    {
-        int timeout = Integer.parseInt(args.argv(0));
-        if (timeout <= 0) {
-            return "Error, mover timeout should be greater than 0";
-        }
-        _moverTimeout = timeout;
-        _moverTimeoutUnit = TimeUnit.SECONDS;
-        return "set mover timeout to " + timeout +  " seconds";
-    }
-
-    public static final String hh_set_pool_timeout = "<#seconds>";
-    public String ac_set_pool_timeout_$_1(Args args)
-    {
-        int timeout = Integer.parseInt(args.argv(0));
-        if (timeout <= 0) {
-            return "Error, pool timeout should be greater than 0";
-        }
-        _poolStub.setTimeout(timeout);
-        _poolStub.setTimeoutUnit(TimeUnit.SECONDS);
-        return "set pool timeout to " + timeout +  " seconds";
-    }
-
-    public static final String hh_set_pool_manager_timeout = "<#seconds>";
-    public String ac_set_pool_manager_timeout_$_1(Args args)
-    {
-        int timeout = Integer.parseInt(args.argv(0));
-        if (timeout <= 0) {
-            return "Error, pool manger timeout should be greater than 0";
-        }
-        _poolManager.setTimeout(timeout);
-        _poolManager.setTimeoutUnit(TimeUnit.SECONDS);
-        return "set pool manager timeout to "+ timeout +  " seconds";
-    }
-
-    public static final String hh_ls = "[-l] [<#transferId>]";
-    public String ac_ls_$_0_1(Args args)
+    public static final String hh_ls_internal = "[-l] [<#transferId>]";
+    public String ac_ls_internal_$_0_1(Args args)
     {
         boolean long_format = args.hasOption("l");
         if (args.argc() > 0) {

modules/dcache/src/main/resources/diskCacheV111/doors/copy-manager.xml
@@ -1,46 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-
-<beans xmlns="http://www.springframework.org/schema/beans"
-       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-       xmlns:context="http://www.springframework.org/schema/context"
-       xmlns:util="http://www.springframework.org/schema/util"
-       xsi:schemaLocation="http://www.springframework.org/schema/beans
-                           http://www.springframework.org/schema/beans/spring-beans.xsd
-                           http://www.springframework.org/schema/context
-                           http://www.springframework.org/schema/context/spring-context.xsd
-                           http://www.springframework.org/schema/util
-                           http://www.springframework.org/schema/util/spring-util.xsd">
-
-  <context:property-placeholder/>
-
-  <bean id="poolmanager" class="org.dcache.cells.CellStub">
-    <property name="destination" value="${transfermanagers.service.poolmanager}"/>
-    <property name="timeout" value="${transfermanagers.service.poolmanager.timeout}"/>
-    <property name="timeoutUnit" value="${transfermanagers.service.poolmanager.timeout.unit}"/>
-  </bean>
-
-  <bean id="pnfshandler" class="diskCacheV111.util.PnfsHandler">
-    <constructor-arg>
-         <bean id="pnfsmanager" class="org.dcache.cells.CellStub">
-            <property name="destination" value="${transfermanagers.service.pnfsmanager}"/>
-            <property name="timeout" value="${transfermanagers.service.pnfsmanager.timeout}"/>
-            <property name="timeoutUnit" value="${transfermanagers.service.pnfsmanager.timeout.unit}"/>
-        </bean>
-    </constructor-arg>
-  </bean>
-
-  <bean id="pool" class="org.dcache.cells.CellStub">
-    <property name="timeout" value="${transfermanagers.service.pool.timeout}"/>
-    <property name="timeoutUnit" value="${transfermanagers.service.pool.timeout.unit}"/>
-  </bean>
-
-  <bean id="copyManager" class="diskCacheV111.doors.CopyManager"
-        init-method="init">
-    <property name="poolManager" ref="poolmanager"/>
-    <property name="pool" ref="pool"/>
-    <property name="pnfsHandler" ref="pnfshandler"/>
-    <property name="moverTimeout" value="${transfermanagers.limits.transfer-time}"/>
-    <property name="moverTimeoutUnit" value="${transfermanagers.limits.transfer-time.unit}"/>
-    <property name="maxTransfers" value="${transfermanagers.limits.internal-transfers}"/>
-  </bean>
-</beans>

modules/dcache/src/main/resources/diskCacheV111/services/transfer-manager.xml
@@ -40,6 +40,20 @@
     <property name="timeoutUnit" value="${transfermanagers.service.pool.timeout.unit}"/>
   </bean>
 
+  <bean id="copyManager" class="diskCacheV111.doors.CopyManager"
+        init-method="init">
+      <property name="poolManager" ref="poolmanager"/>
+      <property name="pool" ref="pool"/>
+      <property name="pnfsHandler">
+          <bean class="diskCacheV111.util.PnfsHandler">
+              <constructor-arg ref="pnfsmanager"/>
+          </bean>
+      </property>
+      <property name="moverTimeout" value="${transfermanagers.limits.transfer-time}"/>
+      <property name="moverTimeoutUnit" value="${transfermanagers.limits.transfer-time.unit}"/>
+      <property name="maxTransfers" value="${transfermanagers.limits.internal-transfers}"/>
+  </bean>
+
   <beans profile="db.enabled-false">
     <bean id="transferManager" class="diskCacheV111.services.RemoteTransferManager"
             destroy-method="cleanUp">

skel/share/defaults/dcache.properties
@@ -428,7 +428,6 @@ dcache.service.spacemanager = ${spacemanager.cell.name}
 dcache.service.pinmanager = ${pinmanager.cell.name}
 dcache.service.replica = ${replica.cell.name}
 dcache.service.transfermanager = ${transfermanagers.cell.name}
-dcache.service.copymanager = CopyManager
 dcache.service.topo = ${topo.cell.name}
 
 
@@ -973,3 +972,4 @@ dcache.description =
 (obsolete)dcache.broker.cells.config = See dcache.broker.scheme
 (obsolete)dcache.broker.messaging.port = Use dcache.broker.port
 (obsolete)dcache.authn.gsi.crl.cache.lifetime = No longer configurable
+(obsolete)dcache.service.copymanager =

skel/share/defaults/srm.properties
@@ -912,18 +912,6 @@ srm.service.transfermanager.timeout=24
 	DAYS)\
 srm.service.transfermanager.timeout.unit=HOURS
 
-# Cell address of copymanager service
-srm.service.copymanager=${dcache.service.copymanager}
-
-# Timeout for copymanager requests
-srm.service.copymanager.timeout=24
-(one-of?MILLISECONDS|\
-	SECONDS|\
-	MINUTES|\
-	HOURS|\
-	DAYS)\
-srm.service.copymanager.timeout.unit=HOURS
-
 # Cell address of billing service
 srm.service.billing = ${dcache.service.billing}
 
@@ -1395,3 +1383,6 @@ srm.request.maximum-client-assumed-bandwidth = 0
 (obsolete)srm.request.copy.max-by-same-user =
 (obsolete)srm.request.ls.max-by-same-user =
 (obsolete)srm.request.reserve-space.max-by-same-user =
+(obsolete)srm.service.copymanager=
+(obsolete)srm.service.copymanager.timeout=
+(obsolete)srm.service.copymanager.timeout.unit=

commit f90ced010b58e9a4f99ed80b2ad77bc16ffe975b

transfermanager: fix querying of 3rd-party transfer
Motivation:

Initiating a 3rd-party transfer via WebDAV shows NPE in transfer
manager.  Although the transfer succeeds, the progress reports contain
badly formed information.

This problem is a result of a regression introduced with commit
dbf3bf3c, where a seemingly innocuous change altered how the cell
message was routed.  When delivering a message, the dispatcher is
meant to select the `messageArrived` method with the most specific
(most sub-classed) signature.  Prior to dbf3bf3c this happened.

Modification:

Revert the change in the method signature.  Although this is not an
ideal change, it is the least dangerous change for back-porting to
2.16

Result:

WebDAV initiated 3rd-party transfers now provide valid progress
reports.

Target: master
Request: 2.16
Requires-notes: yes
Requires-book: no
Patch: https://rb.dcache.org/r/9593/
Acked-by: Gerd Behrmann

modules/dcache/src/main/java/diskCacheV111/services/TransferManager.java
@@ -252,7 +252,13 @@ public abstract class TransferManager extends AbstractCellComponent
         return message;
     }
 
-    public Object messageArrived(TransferStatusQueryMessage message)
+    // TransferStatusQueryMessage is a subclass of
+    // TransferManagerMessage, so the code relies on the
+    // messageArrived dispatch invoking the method with the most
+    // specific signature.  The unused "CellMessage envelope" argument
+    // of this method is required because two-argument methods are
+    // called preferentially.
+    public Object messageArrived(CellMessage envelope, TransferStatusQueryMessage message)
     {
         TransferManagerHandler handler = getHandler(message.getId());
 