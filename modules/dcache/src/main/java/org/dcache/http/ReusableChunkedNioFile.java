6c7692258c8e378f284ccb479d65f48211618b91
Gerd Behrmann <behrmann@gmail.com>
2016-08-22 14:51:19 +0200

    xrootd,pool: Upgrade to Netty 4.1
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Paul Millar <paul.millar@desy.de>
    
    Reviewed at https://rb.dcache.org/r/9668/

--- modules/dcache/src/main/java/org/dcache/http/ReusableChunkedNioFile.java
+++ modules/dcache/src/main/java/org/dcache/http/ReusableChunkedNioFile.java
@@ -1,35 +1,40 @@
 package org.dcache.http;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.stream.ChunkedInput;
+import io.netty.handler.stream.ChunkedNioFile;
 
 import java.nio.ByteBuffer;
 
 import org.dcache.pool.repository.RepositoryChannel;
 
 /*
- * Portions of this file based on JBoss Netty ChunkedNioFile which has the
+ * Portions of this file based on Netty's ChunkedNioFile which has the
  * following copyright:
  *
- * Copyright 2009 Red Hat, Inc.
- * Red Hat licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
+ * Copyright 2012 The Netty Project
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations
  * under the License.
- */
+*/
 public class ReusableChunkedNioFile implements ChunkedInput<ByteBuf>
 {
     private final RepositoryChannel _channel;
+    private final long _startOffset;
     private final long _endOffset;
     private final int _chunkSize;
+
     private volatile long _offset;
 
     public ReusableChunkedNioFile(RepositoryChannel channel,
@@ -58,16 +63,15 @@ public class ReusableChunkedNioFile implements ChunkedInput<ByteBuf>
 
         _channel = channel;
         _chunkSize = chunkSize;
-        _offset = offset;
+        _startOffset = _offset = offset;
         _endOffset = _offset + length;
     }
 
     /**
-     * With a normal ChunkedNioFile, netty at some point receives a
+     * With a normal ChunkedNioFile, Netty at some point receives a
      * "connection closed by peer" signal and closes the file, trying to
      * release the resources. As this closes the disk-file, the mover becomes
      * useless despite keep-alive. To avoid this, close here is a no-op.
-     *
      */
     @Override
     public void close() throws Exception {
@@ -79,25 +83,26 @@ public class ReusableChunkedNioFile implements ChunkedInput<ByteBuf>
         return _offset >= _endOffset || !_channel.isOpen();
     }
 
-    /**
-     * Method that is mostly copied from ChunkedNioFile#nextChunk but updates
-     * the mover information (last transferred, bytes transferred) and uses
-     * absolute position reading to avoid problems with multithreaded access.
-     *
-     * @return ChannelBuffer containing the read bytes
-     */
     @Override
     public ByteBuf readChunk(ChannelHandlerContext ctx) throws Exception
     {
-        long offset = _offset;
+        return readChunk(ctx.alloc());
+    }
 
+    /**
+     * Like {@link ChunkedNioFile#readChunk}, but uses position independent
+     * IO calls.
+     */
+    @Override
+    public ByteBuf readChunk(ByteBufAllocator allocator) throws Exception
+    {
+        long offset = _offset;
         if (offset >= _endOffset) {
             return null;
         }
 
         int length = (int) Math.min(_chunkSize, _endOffset - offset);
-
-        ByteBuf chunk = ctx.alloc().ioBuffer(length);
+        ByteBuf chunk = allocator.buffer(length);
         boolean release = true;
         try {
             ByteBuffer buffer = chunk.nioBuffer(0, length);
@@ -119,6 +124,18 @@ public class ReusableChunkedNioFile implements ChunkedInput<ByteBuf>
                 chunk.release();
             }
         }
+    }
+
+    @Override
+    public long length()
+    {
+        return _endOffset - _startOffset;
+    }
+
+    @Override
+    public long progress()
+    {
+        return _offset - _startOffset;
     }
 
     /**
