dCache/dcache

commit 0702d733bd534c529f203640169f29fe2e7b475b

nfs: discover open-stateid during layout return
Motivation:
Layoutreturn operation uses layout-stateid. Nevertheless, transfer objects
are mapped to open-stateid. Thus, layoutreturn does not files the
transfer object to stop the mover.

Modification:
Discover open-stateid and invalidate layout-stateid on layout return.

Result:
better spec compliance.

Acked-by: Paul Millar
Target: master, 3.0
Require-book: no
Require-notes: no

modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/NFSv41Door.java
@@ -609,9 +609,21 @@ public class NFSv41Door extends AbstractCellComponent implements
     @Override
     public void layoutReturn(CompoundContext context, stateid4 stateid) throws IOException {
 
-        _log.debug("Releasing device by stateid: {}", stateid);
+        final NFS4Client client;
+        if (context.getMinorversion() > 0) {
+            client = context.getSession().getClient();
+        } else {
+            // v4.0 client use proxy adapter, which calls layoutreturn
+            client = context.getStateHandler().getClientIdByStateId(stateid);
+        }
 
-        NfsTransfer transfer = _ioMessages.get(stateid);
+        final NFS4State layoutState = client.state(stateid);
+        final NFS4State openState = layoutState.getOpenState();
+
+        _log.debug("Releasing layout by stateid: {}, open-state: {}", stateid,
+                openState.stateid());
+
+        NfsTransfer transfer = _ioMessages.get(openState.stateid());
         if (transfer == null) {
             return;
         }
@@ -632,6 +644,9 @@ public class NFSv41Door extends AbstractCellComponent implements
                     transfer.getMoverId(), transfer.getPool(), e.getMessage());
             throw new NfsIoException(e.getMessage(), e);
         }
+
+        // any further use of this layout-stateid must fail with NFS4ERR_BAD_STATEID
+        client.releaseState(stateid);
     }
 
     /*

modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/proxy/ProxyIoClose.java
@@ -40,7 +40,8 @@ public class ProxyIoClose extends AbstractNFSv4Operation {
 
         context.getDeviceManager().layoutReturn(context, _args.opclose.open_stateid);
 
-        client.releaseState(_args.opclose.open_stateid);
+        // REVISIT: as we pass open-state id as layout state id, we dont need to invalidate it
+        // client.releaseState(_args.opclose.open_stateid);
         client.updateLeaseTime();
 
         res.open_stateid = Stateids.invalidStateId();

commit 7b9f49aa30170b48e06175df90a2d989060905fb

nfs41: fix open-stateid leak introduced in 0702d73
Motivation:
commit 0702d73 add discovery of open-state-id by layout state-id.
Nevertheless, it has introduced state-id leak for cases without transfers,
e.g. OPEN+CLOSE.

Modification:
ensure that NFS4Client#releaseState() get called

Result:
state id leak fixed

Acked-by: Paul Millar
Acked-by: Jürgen Starek
Target: master, 3.0
Require-book: no
Require-notes: no

modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/NFSv41Door.java
@@ -624,27 +624,24 @@ public class NFSv41Door extends AbstractCellComponent implements
                 openState.stateid());
 
         NfsTransfer transfer = _ioMessages.get(openState.stateid());
-        if (transfer == null) {
-            return;
-        }
+        if (transfer != null) {
+            _log.debug("Sending KILL to {}@{}", transfer.getMoverId(), transfer.getPool());
+            transfer.killMover(0, "killed by door: returning layout");
 
-        _log.debug("Sending KILL to {}@{}", transfer.getMoverId(), transfer.getPool());
-        transfer.killMover(0, "killed by door: returning layout");
-
-        try {
-            if(transfer.hasMover() && !transfer.waitForMover(500)) {
-                throw new DelayException("Mover not stopped");
+            try {
+                if (transfer.hasMover() && !transfer.waitForMover(500)) {
+                    throw new DelayException("Mover not stopped");
+                }
+            } catch (FileNotFoundCacheException e) {
+                // REVISIT: remove when pool will stop sending this exception
+                _log.info("Failed removed while being open mover: {}@{} : {}",
+                        transfer.getMoverId(), transfer.getPool(), e.getMessage());
+            } catch (CacheException | InterruptedException e) {
+                _log.info("Failed to kill mover: {}@{} : {}",
+                        transfer.getMoverId(), transfer.getPool(), e.getMessage());
+                throw new NfsIoException(e.getMessage(), e);
             }
-        } catch (FileNotFoundCacheException e){
-            // REVISIT: remove when pool will stop sending this exception
-            _log.info("Failed removed while being open mover: {}@{} : {}",
-                    transfer.getMoverId(), transfer.getPool(), e.getMessage());
-        } catch (CacheException | InterruptedException e) {
-            _log.info("Failed to kill mover: {}@{} : {}",
-                    transfer.getMoverId(), transfer.getPool(), e.getMessage());
-            throw new NfsIoException(e.getMessage(), e);
         }
-
         // any further use of this layout-stateid must fail with NFS4ERR_BAD_STATEID
         client.releaseState(stateid);
     }