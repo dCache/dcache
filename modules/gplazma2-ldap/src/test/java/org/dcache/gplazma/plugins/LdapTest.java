4e22dd0b6afc4fc161f759f95a365af32c8232be
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2016-12-21 12:34:37 +0100

    gplazma-ldap: rewrite junit tests in java
    Motivation:
    To update ldap plugin with enhancements from Bastian Tweddell let get rid
    of scala code. First of all, it makes compilation too slow and, second,
    ldap plugin is the only scala code we have.
    
    The next step is to rewrite/integrate Bastian's plugin.
    
    Modification:
    re-write ldap related junit tests in java by fully covering existing tests.
    
    Result:
    less scala code.
    
    Acked-by: Paul Millar
    Target: master
    Require-book: no
    Require-notes: no

---
+++ modules/gplazma2-ldap/src/test/java/org/dcache/gplazma/plugins/LdapTest.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2017 Deutsches Elektronen-Synchroton,
+ * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program (see the file COPYING.LIB for more
+ * details); if not, write to the Free Software Foundation, Inc.,
+ * 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.dcache.gplazma.plugins;
+
+import com.google.common.collect.Sets;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.dcache.auth.GidPrincipal;
+import org.dcache.auth.GroupNamePrincipal;
+import org.dcache.auth.UidPrincipal;
+import org.dcache.auth.UserNamePrincipal;
+import org.dcache.auth.attributes.HomeDirectory;
+import org.dcache.auth.attributes.RootDirectory;
+import org.dcache.gplazma.NoSuchPrincipalException;
+import org.dcache.gplazma.plugins.Ldap;
+import org.dcache.ldap4testing.EmbeddedServer;
+
+import static org.dcache.gplazma.plugins.Ldap.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ */
+public class LdapTest {
+
+    private static final GroupNamePrincipal ACTOR_GROUP_PRINCIPAL = new GroupNamePrincipal("actor");
+    private static final GidPrincipal ACTOR_GID_PRINCIPAL = new GidPrincipal(1001, false);
+
+    private static final GidPrincipal KERMIT_PRIMARY_GID_PRINCIPAL = new GidPrincipal(1000, true);
+    private static final UidPrincipal KERMIT_UID_PRINCIPAL = new UidPrincipal(1000);
+    private static final UserNamePrincipal KERMIT_PRINCIPAL = new UserNamePrincipal("kermit");
+
+    private static final UserNamePrincipal BERND_PRINCIPAL = new UserNamePrincipal("bernd");
+    private static final RootDirectory BERND_ROOT_DIRECTORY = new RootDirectory("/");
+    private static final HomeDirectory BERND_HOME_DIRECTORY = new HomeDirectory("/home/bernd");
+
+    private static final UserNamePrincipal NON_EXISTING_PRINCIPAL = new UserNamePrincipal("outlaw");
+    private static final UidPrincipal NON_EXISTING_UID_PRINCIPAL = new UidPrincipal(111);
+    private static final GidPrincipal NON_EXISTING_GID_PRINCIPAL = new GidPrincipal(111, true);
+
+    private EmbeddedServer ldapServer;
+    private Ldap plugin;
+
+    @Before
+    public void setUp() throws IOException {
+
+        InputStream initLdiff = ClassLoader.getSystemResourceAsStream("org/dcache/gplazma/plugins/ldap/init.ldif");
+        ldapServer = new EmbeddedServer(0, initLdiff);
+        ldapServer.start();
+
+        Properties properties = new Properties();
+        properties.put(Ldap.LDAP_URL(), "ldap://localhost:" + ldapServer.getSocketAddress().getPort());
+        properties.put(Ldap.LDAP_ORG(), "o=dcache,c=org");
+        properties.put(Ldap.LDAP_USER_FILTER(), "(uid=%s)");
+        properties.put(Ldap.LDAP_PEOPLE_TREE(), "people");
+        properties.put(Ldap.LDAP_GROUP_TREE(), "group");
+        properties.put(Ldap.LDAP_USER_HOME(), "%homeDirectory%");
+        properties.put(Ldap.LDAP_USER_ROOT(), "/");
+        properties.put(Ldap.LDAP_GROUP_MEMBER(), "uniqueMember");
+
+        properties.put(Ldap.LDAP_AUTH(), "simple");
+        properties.put(Ldap.LDAP_BINDDN(), "uid=kermit,ou=people,o=dcache,c=org");
+        properties.put(Ldap.LDAP_BINDPW(), "kermitTheFrog");
+
+        plugin = new Ldap(properties);
+    }
+
+    @Test
+    public void shouldReturnMatchingUidGid() {
+        Set<Principal> principals = Sets.newHashSet(KERMIT_PRINCIPAL);
+
+        plugin.map(principals);
+
+        assertThat("unexpected number of returned principals", principals, hasSize(4));
+        assertThat("expected USERNAME not found", principals, hasItem(KERMIT_PRINCIPAL));
+        assertThat("expected UID not found", principals, hasItem(KERMIT_UID_PRINCIPAL));
+        assertThat("expected GID not found", principals, hasItem(KERMIT_PRIMARY_GID_PRINCIPAL));
+        assertThat("expected GID not found", principals, hasItem(ACTOR_GID_PRINCIPAL));
+    }
+
+    @Test
+    public void shouldDoNothingForNonExisting() {
+        Set<Principal> principals = Sets.newHashSet(NON_EXISTING_PRINCIPAL);
+        plugin.map(principals);
+        assertThat("unexpected number of returned principals", principals, hasSize(1));
+        assertThat("expected USERNAME not found", principals, hasItem(NON_EXISTING_PRINCIPAL));
+    }
+
+    @Test
+    public void shouldReturnCorrectMappedUid() {
+        Principal mapped = plugin.map(KERMIT_PRINCIPAL);
+        assertEquals("unexpected mapping", KERMIT_UID_PRINCIPAL, mapped);
+    }
+
+    @Test(expected = NoSuchPrincipalException.class)
+    public void shouldFailMappingForNonExisting() {
+        plugin.map(NON_EXISTING_PRINCIPAL);
+    }
+
+    @Test
+    public void shouldReturnSetContaningUserNameOnReverseMapping() {
+        Set<Principal> rmap = plugin.reverseMap(KERMIT_UID_PRINCIPAL);
+        assertThat("Expceted principal not found", rmap, hasItem(KERMIT_PRINCIPAL));
+    }
+
+    @Test
+    public void shouldReturnSetContaningGroupNameOnReverseMapping() {
+        Set<Principal> rmap = plugin.reverseMap(new GidPrincipal(1001, false));
+        assertThat("Expceted principal not found", rmap, hasItem(ACTOR_GROUP_PRINCIPAL));
+    }
+
+    @Test
+    public void shouldReturnSerializableSetOnReverseMapping() {
+        Set<Principal> rmap = plugin.reverseMap(KERMIT_UID_PRINCIPAL);
+
+        assertThat("Expecting serializable set", instanceOf(Serializable.class));
+    }
+
+    @Test(expected = NoSuchPrincipalException.class)
+    public void shouldFailReverseMappingForNonExistingUid() {
+        plugin.reverseMap(NON_EXISTING_UID_PRINCIPAL);
+    }
+
+    @Test(expected = NoSuchPrincipalException.class)
+    public void shouldFailReverseMappingForNonExistingGid() {
+        plugin.reverseMap(NON_EXISTING_GID_PRINCIPAL);
+    }
+
+    @Test
+    public void shouldReturnUserHomeAndRoot() {
+        Set<Principal> principals = Sets.newHashSet(BERND_PRINCIPAL);
+        Set<Object> attrs = new HashSet<>();
+
+        plugin.session(principals, attrs);
+        assertThat("expected HOME attribute not found", attrs, hasItem(BERND_HOME_DIRECTORY));
+        assertThat("expected ROOT attribute not found", attrs, hasItem(BERND_ROOT_DIRECTORY));
+    }
+
+    @After
+    public void tearDown() {
+        ldapServer.stop();
+    }
+}
