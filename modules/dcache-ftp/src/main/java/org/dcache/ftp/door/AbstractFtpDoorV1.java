f90912b668a08288a62bb64d8c259da2726f3980
Paul Millar <paul.millar@desy.de>
2017-02-09 19:04:25 +0100

    ftp: show SIZE facts for directories
    Motivation:
    
    The Globus server supplies the SIZE fact for directories.
    
    Although supplying this information is not required by any specification
    or agreement, its omission may be causing problems with clients that are
    unable to parse MLSD output.  Since we have this information anyway,
    supplying it poses no additional overhead.
    
    Modification:
    
    Show size fact for directory items.
    
    Result:
    
    Potentially better compatibility with Globus clients.
    
    Target: master
    Request: 3.0
    Request: 2.16
    Requires-notes: yes
    Requires-book: no
    Patch: https://rb.dcache.org/r/10062/
    Acked-by: Dmitry Litvintsev

--- modules/dcache-ftp/src/main/java/org/dcache/ftp/door/AbstractFtpDoorV1.java
+++ modules/dcache-ftp/src/main/java/org/dcache/ftp/door/AbstractFtpDoorV1.java
@@ -4271,7 +4271,6 @@ public abstract class AbstractFtpDoorV1
             for (Fact fact: _currentFacts) {
                 switch (fact) {
                 case SIZE:
-                    attributes.add(SIMPLE_TYPE);
                     attributes.add(SIZE);
                     attributes.addAll(_pdp.getRequiredAttributes());
                     break;
@@ -4331,7 +4330,7 @@ public abstract class AbstractFtpDoorV1
                 for (Fact fact: _currentFacts) {
                     switch (fact) {
                     case SIZE:
-                        if (attr.isDefined(SIZE) && attr.getFileType() != FileType.DIR) {
+                        if (attr.isDefined(SIZE)) {
                             access = _pdp.canGetAttributes(_subject, attr, EnumSet.of(SIZE));
                             if (access == AccessType.ACCESS_ALLOWED) {
                                 printSizeFact(attr);
