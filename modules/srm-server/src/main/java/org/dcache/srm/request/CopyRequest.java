d73e230d9ce18423cdd6dd894176b93b562148c4
Paul Millar <paul.millar@desy.de>
2017-02-16 19:30:15 +0100

    srm: include remaining request lifetime in various responses
    Motivation:
    
    During an ATLAS stress-test of tape recalls, it was discovered that
    various sites had relatively short request lifetimes.  However, the SRM
    spec provides the opportunity for the server to inform the client (FTS,
    in this case) of what lifetime a request actually has.
    
    This gives the client an opportunity to discover a discrepency between
    desired lifetime and the lifetime of the job.
    
    Modification:
    
    Include the requests remaining lifetime in the response from the server.
    
    Target: master
    Requires-notes: yes
    Requires-book: no
    Request: 3.0
    Request: 2.16
    Request: 2.15
    Request: 2.14
    Request: 2.13
    Patch: https://rb.dcache.org/r/10077/
    Acked-by: Tigran Mkrtchyan

--- modules/srm-server/src/main/java/org/dcache/srm/request/CopyRequest.java
+++ modules/srm-server/src/main/java/org/dcache/srm/request/CopyRequest.java
@@ -87,6 +87,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.dcache.srm.SRM;
 import org.dcache.srm.SRMException;
@@ -787,6 +788,7 @@ public final class CopyRequest extends ContainerRequest<CopyFileRequest>
         response.setReturnStatus(getTReturnStatus());
         response.setRequestToken(getTRequestToken());
         response.setArrayOfFileStatuses(new ArrayOfTCopyRequestFileStatus(getArrayOfTCopyRequestFileStatuses()));
+        response.setRemainingTotalRequestTime(getRemainingLifetimeIn(TimeUnit.SECONDS));
         return response;
     }
 
@@ -841,6 +843,7 @@ public final class CopyRequest extends ContainerRequest<CopyFileRequest>
         response.setReturnStatus(getTReturnStatus());
         TCopyRequestFileStatus[] fileStatuses = getArrayOfTCopyRequestFileStatuses(fromurls, tourls);
         response.setArrayOfFileStatuses(new ArrayOfTCopyRequestFileStatus(fileStatuses));
+        response.setRemainingTotalRequestTime(getRemainingLifetimeIn(TimeUnit.SECONDS));
         return response;
     }
 
