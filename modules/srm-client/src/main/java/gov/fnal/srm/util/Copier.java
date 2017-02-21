dCache/dcache

commit 501ed182c1bb5fd8488331aa1586af85f5ebbe81

srm-client: Get rid of GlobusURL
Motivation:

Get rid of JGlobus dependencies.

Modification:

Replaces uses of GlobusURL with URI. SrmUrl is turned into a
URI factory that injects default ports like GlobusURL did.

Result:

Hopefully no user visible changes.

Target: trunk
Require-notes: yes
Require-book: no
Acked-by: Dmitry Litvintsev <litvinse@fnal.gov>
Acked-by: Paul Millar <paul.millar@desy.de>
Patch: https://rb.dcache.org/r/8605/

modules/srm-client/src/main/java/gov/fnal/srm/util/Copier.java
@@ -80,7 +80,6 @@ import eu.emi.security.authn.x509.impl.PEMCredential;
 import org.globus.ftp.exception.ServerException;
 import org.globus.ftp.exception.UnexpectedReplyCodeException;
 import org.globus.ftp.vanilla.Reply;
-import org.globus.util.GlobusURL;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -382,8 +381,8 @@ public class Copier implements Runnable {
     }
 
     public void copy(CopyJob job) throws Exception {
-        GlobusURL from =job.getSource();
-        GlobusURL to = job.getDestination();
+        java.net.URI from =job.getSource();
+        java.net.URI to = job.getDestination();
         int totype = SRMDispatcher.getUrlType(to);
 
         // handle directory
@@ -393,12 +392,12 @@ public class Copier implements Runnable {
             if(lastSlash != -1) {
                 filename = filename.substring(lastSlash);
             }
-            to = new GlobusURL(to.getURL().concat(filename));
+            to = to.resolve(filename);
         }
 
         dsay("copying " +job);
-        if(from.getProtocol().equals("dcap") ||
-                to.getProtocol().equals("dcap") ||
+        if(from.getScheme().equals("dcap") ||
+                to.getScheme().equals("dcap") ||
                 configuration.isUse_urlcopy_script() ) {
             try {
                 say("trying script copy");
@@ -417,11 +416,11 @@ public class Copier implements Runnable {
                 boolean to_protocol_supported=false;
                 if(script_protocols != null) {
                     for (String script_protocol : script_protocols) {
-                        if (script_protocol.equals(from.getProtocol())) {
+                        if (script_protocol.equals(from.getScheme())) {
                             from_protocol_supported = true;
                         }
 
-                        if (script_protocol.equals(to.getProtocol())) {
+                        if (script_protocol.equals(to.getScheme())) {
                             to_protocol_supported = true;
                         }
 
@@ -442,12 +441,12 @@ public class Copier implements Runnable {
             }
         }
 
-        if( (( from.getProtocol().equals("gsiftp") ||
-                from.getProtocol().equals("gridftp") ) &&
-                to.getProtocol().equals("file")) ||
-                (  from.getProtocol().equals("file") &&
-                        ( to.getProtocol().equals("gsiftp") ||
-                                to.getProtocol().equals("gridftp") ))
+        if( (( from.getScheme().equals("gsiftp") ||
+                from.getScheme().equals("gridftp") ) &&
+                to.getScheme().equals("file")) ||
+                (  from.getScheme().equals("file") &&
+                        ( to.getScheme().equals("gsiftp") ||
+                                to.getScheme().equals("gridftp") ))
         )
 
         {
@@ -459,13 +458,8 @@ public class Copier implements Runnable {
             }
             javaGridFtpCopy(from, to, credential, logger);
         } else {
-            URL fromURL;
-            URL toURL;
-            fromURL = new URL(from.getURL());
-            toURL = new URL(to.getURL());
-            javaUrlCopy(fromURL,toURL);
+            javaUrlCopy(from.toURL(), to.toURL());
         }
-
     }
 
     public String[] scriptCopyGetSupportedProtocols()
@@ -475,7 +469,7 @@ public class Copier implements Runnable {
         return ShellCommandExecuter.executeAndReturnOutput(command,logger);
     }
 
-    public void scriptCopy(GlobusURL from, GlobusURL to) throws Exception {
+    public void scriptCopy(java.net.URI from, java.net.URI to) throws Exception {
         String command = urlcopy;
         if(debug) {
             command = command+" -debug true";
@@ -513,8 +507,8 @@ public class Copier implements Runnable {
         }
 
         command = command+
-        " -src-protocol "+from.getProtocol();
-        if(from.getProtocol().equals("file")) {
+        " -src-protocol "+from.getScheme();
+        if(from.getScheme().equals("file")) {
             command = command+" -src-host-port localhost";
         }
         else {
@@ -523,8 +517,8 @@ public class Copier implements Runnable {
         }
         command = command+
         " -src-path "+from.getPath()+
-        " -dst-protocol "+to.getProtocol();
-        if(to.getProtocol().equals("file")) {
+        " -dst-protocol "+to.getScheme();
+        if(to.getScheme().equals("file")) {
             command = command+" -dst-host-port localhost";
         }
         else {
@@ -538,24 +532,24 @@ public class Copier implements Runnable {
             rc = ShellCommandExecuter.execute(command,logger);
         }
         if(rc == 0) {
-            say(" successfuly copied "+from.getURL()+" to "+to.getURL());
+            say(" successfuly copied "+from+" to "+to);
             num_completed_successfully++;
         }
         else {
-            esay(" failed to copy "+from.getURL()+" to "+to.getURL());
+            esay(" failed to copy "+from+" to "+to);
             esay(urlcopy+" return code = "+rc);
             throw new Exception(urlcopy+" return code = "+rc);
         }
     }
 
-    public void javaGridFtpCopy(GlobusURL src_url,
-                                GlobusURL dst_url,
+    public void javaGridFtpCopy(java.net.URI src_url,
+                                java.net.URI dst_url,
                                 X509Credential credential,
                                 Logger logger) throws Exception {
         String serverMode   = configuration.getServerMode();
         int numberOfStreams = configuration.getStreams_num();
-        if((src_url.getProtocol().equals("gsiftp")||src_url.getProtocol().equals("gridftp")) &&
-                dst_url.getProtocol().equals("file")) {
+        if((src_url.getScheme().equals("gsiftp")||src_url.getScheme().equals("gridftp")) &&
+                dst_url.getScheme().equals("file")) {
             //
             // case of read
             //
@@ -608,8 +602,8 @@ public class Copier implements Runnable {
             return;
         }
 
-        if(src_url.getProtocol().equals("file")&&
-                (dst_url.getProtocol().equals("gsiftp")||dst_url.getProtocol().equals("gridftp"))) {
+        if(src_url.getScheme().equals("file")&&
+                (dst_url.getScheme().equals("gsiftp")||dst_url.getScheme().equals("gridftp"))) {
             //
             // case of write
             //

modules/srm-client/src/main/java/org/dcache/srm/util/GridftpList.java
@@ -6,7 +6,7 @@
 
 package org.dcache.srm.util;
 
-import org.globus.util.GlobusURL;
+import java.net.URI;
 
 /**
  *
@@ -43,12 +43,11 @@ public class GridftpList {
             serverPassive = args[1].equalsIgnoreCase("true");
         }
 
-        GlobusURL directory_url = new GlobusURL(directory);
+        URI directory_url = new URI(directory);
 
-        if( ! directory_url.getProtocol().equals("gsiftp") &&
-              ! directory_url.getProtocol().equals("gridftp") ) {
-                  System.err.println("wrong protocol : "+
-                  directory_url.getProtocol());
+        if( ! directory_url.getScheme().equals("gsiftp") &&
+              ! directory_url.getScheme().equals("gridftp") ) {
+                  System.err.println("wrong protocol : " + directory_url.getScheme());
                  System.exit(1);
                     return;
         }

modules/srm-common/src/main/java/org/dcache/srm/util/GridftpClient.java
@@ -20,7 +20,6 @@ import org.globus.ftp.exception.UnexpectedReplyCodeException;
 import org.globus.ftp.vanilla.Reply;
 import org.globus.gsi.CredentialException;
 import org.globus.gsi.gssapi.GlobusGSSCredentialImpl;
-import org.globus.util.GlobusURL;
 import org.ietf.jgss.GSSCredential;
 import org.ietf.jgss.GSSException;
 import org.italiangrid.voms.credential.LoadCredentialsStrategy;
@@ -32,6 +31,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
@@ -761,16 +761,16 @@ public class GridftpClient
         String chsmValue = sMap.get("checksumValue");
         String cksmPrint = sMap.get("checksumPrint");
 
-        GlobusURL src_url = new GlobusURL(source);
-        GlobusURL dst_url = new GlobusURL(dest);
+        URI src_url = new URI(source);
+        URI dst_url = new URI(dest);
 
         LoadCredentialsStrategy loadCredentialsStrategy = new DefaultLoadCredentialsStrategy();
         X509Credential x509Credential =
                 loadCredentialsStrategy.loadCredentials(new CharArrayPasswordFinder(null));
 
-        if( ( src_url.getProtocol().equals("gsiftp") ||
-              src_url.getProtocol().equals("gridftp") ) &&
-            dst_url.getProtocol().equals("file")) {
+        if( ( src_url.getScheme().equals("gsiftp") ||
+              src_url.getScheme().equals("gridftp") ) &&
+            dst_url.getScheme().equals("file")) {
             GridftpClient client;
 
             client = new GridftpClient(src_url.getHost(),
@@ -787,9 +787,9 @@ public class GridftpClient
             return;
         }
 
-        if(  src_url.getProtocol().equals("file") &&
-             ( dst_url.getProtocol().equals("gsiftp") ||
-               dst_url.getProtocol().equals("gridftp") )
+        if(  src_url.getScheme().equals("file") &&
+             ( dst_url.getScheme().equals("gsiftp") ||
+               dst_url.getScheme().equals("gridftp") )
              ) {
             GridftpClient client;
             client = new GridftpClient(dst_url.getHost(),

modules/dcache/src/main/java/org/dcache/pool/movers/RemoteGsiftpTransferProtocol_1.java
@@ -72,12 +72,13 @@ import org.globus.ftp.Buffer;
 import org.globus.ftp.exception.ClientException;
 import org.globus.ftp.exception.ServerException;
 import org.globus.gsi.CredentialException;
-import org.globus.util.GlobusURL;
 import org.ietf.jgss.GSSException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
 import java.security.KeyStoreException;
 import java.security.MessageDigest;
@@ -115,6 +116,7 @@ public class RemoteGsiftpTransferProtocol_1
 
     private final static CellPath PNFS_MANAGER =
         new CellPath("PnfsManager");
+    public static final int DEFAULT_PORT = 2811;
 
     private final CellEndpoint _cell;
     private long _starttime;
@@ -148,14 +150,15 @@ public class RemoteGsiftpTransferProtocol_1
 
     private void createFtpClient(RemoteGsiftpTransferProtocolInfo protocolInfo)
             throws ServerException, ClientException,
-            CredentialException, GSSException, IOException, KeyStoreException
+            CredentialException, GSSException, IOException, KeyStoreException, URISyntaxException
     {
         if (_client != null) {
             return;
         }
 
-        GlobusURL url = new GlobusURL(protocolInfo.getGsiftpUrl());
-        _client = new GridftpClient(url.getHost(), url.getPort(),
+        URI url = new URI(protocolInfo.getGsiftpUrl());
+        int port = (url.getPort() == -1) ? DEFAULT_PORT : url.getPort();
+        _client = new GridftpClient(url.getHost(), port,
                                     protocolInfo.getTcpBufferSize(),
                                     protocolInfo.getCredential());
         _client.setStreamsNum(protocolInfo.getNumberOfStreams());
@@ -170,7 +173,7 @@ public class RemoteGsiftpTransferProtocol_1
                       IoMode access)
             throws CacheException, IOException,
             ServerException, ClientException,
-            CredentialException, GSSException, KeyStoreException
+            CredentialException, GSSException, KeyStoreException, URISyntaxException
     {
         _pnfsId = fileAttributes.getPnfsId();
         if (_log.isDebugEnabled()) {
@@ -242,7 +245,7 @@ public class RemoteGsiftpTransferProtocol_1
         throws CacheException
     {
         try {
-            GlobusURL src_url = new GlobusURL(protocolInfo.getGsiftpUrl());
+            URI src_url = new URI(protocolInfo.getGsiftpUrl());
             boolean emode = protocolInfo.isEmode();
             long size = _client.getSize(src_url.getPath());
             _log.debug(" received a file size info: " + size +
@@ -283,7 +286,7 @@ public class RemoteGsiftpTransferProtocol_1
                 _log.debug("PnfsId "+_pnfsId+" does not have checksums");
             }
 
-            GlobusURL dst_url =  new GlobusURL(protocolInfo.getGsiftpUrl());
+            URI dst_url =  new URI(protocolInfo.getGsiftpUrl());
             boolean emode = protocolInfo.isEmode();
 
             try {
@@ -340,7 +343,7 @@ public class RemoteGsiftpTransferProtocol_1
     {
         try {
             createFtpClient(remoteGsiftpProtocolInfo);
-            GlobusURL src_url =  new GlobusURL(remoteGsiftpProtocolInfo.getGsiftpUrl());
+            URI src_url =  new URI(remoteGsiftpProtocolInfo.getGsiftpUrl());
             _ftpCksm = _client.negotiateCksm(src_url.getPath());
             return ChecksumFactory.getFactory(ChecksumType.getChecksumType(_ftpCksm.type));
         } catch (NoSuchAlgorithmException | IllegalArgumentException e) {

modules/srm-client/src/main/java/gov/fnal/srm/util/Configuration.java
@@ -89,6 +89,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -1846,7 +1847,7 @@ public class Configuration {
     }
 
 
-    private String srmUrl;
+    private URI srmUrl;
     private String surls[];
 
     /** Creates a new instance of Configuration */
@@ -2542,7 +2543,7 @@ public class Configuration {
         }
 
         surls = args.getArguments().toArray(new String[numberOfArguments]);
-        srmUrl=args.argv(0);
+        srmUrl = new URI(args.argv(0));
 
         //
         // take care of protocol version for srm v2.2 functions
@@ -2645,7 +2646,7 @@ public class Configuration {
             protocols = readListOfOptions(protocols_list,",");
         }
         else if(ping) {
-            srmUrl = args.argv(0);
+            srmUrl = new URI(args.argv(0));
         }
         else if (setPermission) {
             setPermissionSurl = args.argv(0);
@@ -3210,11 +3211,11 @@ public class Configuration {
         this.spaceTokensList = spaceTokensList;
     }
 
-    public String getSrmUrl() {
+    public URI getSrmUrl() {
         return srmUrl;
     }
 
-    public void setSrmUrl(String srmUrl) {
+    public void setSrmUrl(URI srmUrl) {
         this.srmUrl = srmUrl;
     }
 }

modules/srm-client/src/main/java/gov/fnal/srm/util/CopyJob.java
@@ -73,7 +73,8 @@ COPYRIGHT STATUS:
 
 package gov.fnal.srm.util;
 
-import org.globus.util.GlobusURL;
+import java.net.URI;
+
 /**
  *
  * @author  timur
@@ -81,8 +82,8 @@ import org.globus.util.GlobusURL;
 
 public interface CopyJob {
 
-    public GlobusURL getSource() ;
-    public GlobusURL getDestination();
+    URI getSource() ;
+    URI getDestination();
 
-    public void done(boolean success, String error);
+    void done(boolean success, String error);
 }

modules/srm-client/src/main/java/gov/fnal/srm/util/Report.java
@@ -6,12 +6,11 @@
 
 package gov.fnal.srm.util;
 
-import org.globus.util.GlobusURL;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URI;
 
 /**
  *
@@ -28,9 +27,9 @@ public class Report {
     public static final String INITIAL_ERROR=
         "copy did not complete or status unknown";
     // source urls
-    GlobusURL from[];
+    URI from[];
     //destination urls
-    GlobusURL to[];
+    URI to[];
     //return codes
     int rc[];
     //errors
@@ -41,7 +40,7 @@ public class Report {
     private File reportFile;
 
     /** Creates a new instance of Report */
-    public Report(GlobusURL from[],GlobusURL to[], String reportFileName)  {
+    public Report(URI from[], URI to[], String reportFileName)  {
         if(from == null || to == null) {
             throw new NullPointerException(
             "from url array and to url array should not be null");
@@ -85,9 +84,9 @@ public class Report {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i <length; ++i) {
-            sb.append(from[i].getURL());
+            sb.append(from[i].toASCIIString());
             sb.append(' ');
-            sb.append(to[i].getURL());
+            sb.append(to[i].toASCIIString());
             sb.append(' ');
             sb.append(rc[i]);
             if(rc[i] != 0) {
@@ -99,14 +98,12 @@ public class Report {
         return sb.toString();
     }
 
-    public void setStatusBySourceUrl(GlobusURL srcURL,int returnCode,String errorDscr){
+    public void setStatusBySourceUrl(URI srcURL,int returnCode,String errorDscr){
         if(!isValidRC(returnCode)) {
             throw new IllegalArgumentException("illegal return code value : "+returnCode);
         }
         for(int i = 0; i <length; ++i) {
-            if(srcURL.getURL().equals(from[i].getURL())&&
-                    srcURL.getHost().equals(from[i].getHost()) &&
-                    srcURL.getProtocol().equals(from[i].getProtocol())){
+            if(srcURL.equals(from[i])) {
                 rc[i] = returnCode;
                 if(returnCode != 0) {
                     error[i] = errorDscr;
@@ -115,14 +112,12 @@ public class Report {
         }
     }
 
-    public void setStatusByDestinationUrl(GlobusURL dstURL,int returnCode,String errorDscr){
+    public void setStatusByDestinationUrl(URI dstURL,int returnCode,String errorDscr){
         if(!isValidRC(returnCode)) {
             throw new IllegalArgumentException("illegal return code value : "+returnCode);
         }
         for(int i = 0; i <length; ++i) {
-            if(dstURL.getURL().equals(to[i].getURL())&&
-                    dstURL.getHost().equals(to[i].getHost())&&
-                    dstURL.getProtocol().equals(to[i].getProtocol())){
+            if(dstURL.equals(to[i])){
                 rc[i] = returnCode;
                 if(returnCode != 0) {
                     error[i] = errorDscr;
@@ -130,21 +125,15 @@ public class Report {
                 return;
             }
         }
-        throw new IllegalArgumentException("record for dest="+
-                dstURL.getURL()+" not found");
+        throw new IllegalArgumentException("record for dest=" + dstURL + " not found");
     }
 
-    public void setStatusBySourceDestinationUrl(GlobusURL srcURL,GlobusURL dstURL,int returnCode,String errorDscr){
+    public void setStatusBySourceDestinationUrl(URI srcURL, URI dstURL,int returnCode,String errorDscr){
         if(!isValidRC(returnCode)) {
             throw new IllegalArgumentException("illegal return code value : "+returnCode);
         }
         for(int i = 0; i <length; ++i) {
-            if((dstURL.getURL().equals(to[i].getURL())&&
-                    dstURL.getHost().equals(to[i].getHost())&&
-                    dstURL.getProtocol().equals(to[i].getProtocol()))&&
-                    (srcURL.getURL().equals(from[i].getURL())&&
-                            srcURL.getHost().equals(from[i].getHost())&&
-                            srcURL.getProtocol().equals(from[i].getProtocol()))) {
+            if(dstURL.equals(to[i]) && srcURL.equals(from[i])) {
                 rc[i] = returnCode;
                 if(returnCode != 0) {
                     error[i] = errorDscr;
@@ -153,7 +142,7 @@ public class Report {
             }
         }
         throw new IllegalArgumentException("record for source="+
-                srcURL.getURL()+" and dest="+dstURL.getURL()+" not found");
+                srcURL+" and dest="+dstURL+" not found");
     }
 
     public boolean isValidRC(int rc) {
@@ -173,10 +162,10 @@ public class Report {
     public void reportErrors(PrintStream out) {
         for (int i = 0; i < length; ++i) {
             if (rc [i] != 0) {
-                out.append(from[i].getURL());
+                out.append(from[i].toASCIIString());
                 if (to[i] != from[i]) {
                     out.append(" -> ");
-                    out.append(to[i].getURL());
+                    out.append(to[i].toASCIIString());
                 }
                 out.append(": ");
                 out.append(error[i].replace('\n', ' '));

modules/srm-client/src/main/java/gov/fnal/srm/util/SRMAbortFilesClientV2.java
@@ -10,7 +10,6 @@
 package gov.fnal.srm.util;
 
 import org.apache.axis.types.URI;
-import org.globus.util.GlobusURL;
 import org.ietf.jgss.GSSCredential;
 import org.ietf.jgss.GSSException;
 
@@ -36,7 +35,7 @@ public class SRMAbortFilesClientV2 extends SRMClient {
     @Override
     public void connect() throws Exception {
         credential=getGssCredential();
-        isrm = new SRMClientV2((new GlobusURL(configuration.getSrmUrl())),
+        isrm = new SRMClientV2(configuration.getSrmUrl(),
                 credential,
                 configuration.getRetry_timeout(),
                 configuration.getRetry_num(),
@@ -66,7 +65,7 @@ public class SRMAbortFilesClientV2 extends SRMClient {
                 request.setRequestToken(requestToken);
                 ArrayOfAnyURI arrayOfSURLs = new ArrayOfAnyURI();
                 URI[] urlArray = new URI[1];
-                urlArray[0] =  new URI((new GlobusURL(configuration.getSrmUrl())).getURL());
+                urlArray[0] =  new URI(configuration.getSrmUrl().toASCIIString());
                 arrayOfSURLs.setUrlArray(urlArray);
                 request.setArrayOfSURLs(arrayOfSURLs);
                 SrmAbortFilesResponse response = isrm.srmAbortFiles(request);
@@ -125,7 +124,7 @@ public class SRMAbortFilesClientV2 extends SRMClient {
             ArrayOfAnyURI arrayOfSURLs = new ArrayOfAnyURI();
             URI[] urlArray = new URI[configuration.getSurls().length];
             for(int i=0; i<configuration.getSurls().length;i++) {
-                urlArray[i] = new URI((new GlobusURL(configuration.getSurls()[i])).getURL());
+                urlArray[i] = new URI((new java.net.URI(configuration.getSurls()[i])).toASCIIString());
             }
             arrayOfSURLs.setUrlArray(urlArray);
             request.setArrayOfSURLs(arrayOfSURLs);

commit 4a22f6e939de7be24b4e4d4d4a8d55b6da78688e

srm-client: use default GridFTP port if not specified
Motivation:

srmcp fails when transferring files into or out of DPM.  This is a
regression introduced with commit 501ed18.

Modification:

As DPM returns TURLs that do not contain a port number, the code must
ensure the default port number is inserted whenever creating a
GridFTPClient object.

Result:

srmcp works with DPM again.

Target: master
Request: 3.0
Requires-notes: no
Requires-srmclient-notes: yes
Requires-book: no
Patch: https://rb.dcache.org/r/10083/
Acked-by: Gerd Behrmann
Acked-by: Marina Sahakyan

modules/srm-client/src/main/java/gov/fnal/srm/util/Copier.java
@@ -580,8 +580,9 @@ public class Copier implements Runnable {
             }
             boolean emode = (numberOfStreams!=1);
             if(! dryRun ) {
+                int port = src_url.getPort();
                 GridftpClient client = new GridftpClient(src_url.getHost(),
-                                                         src_url.getPort(),
+                                                         port == -1 ? 8211 : port,
                                                          configuration.getBuffer_size(),
                                                          configuration.getGlobus_tcp_port_range() != null
                                                          ? PortRange.valueOf(configuration.getGlobus_tcp_port_range())
@@ -639,8 +640,9 @@ public class Copier implements Runnable {
             }
             boolean emode = (numberOfStreams!=1);
             if(! dryRun ) {
+                int port = dst_url.getPort();
                 GridftpClient client = new GridftpClient(dst_url.getHost(),
-                                                         dst_url.getPort(),
+                                                         port == -1 ? 2811 : port,
                                                          configuration.getBuffer_size(),
                                                          configuration.getGlobus_tcp_port_range() != null
                                                          ? PortRange.valueOf(configuration.getGlobus_tcp_port_range())

modules/srm-client/src/main/java/org/dcache/srm/util/GridftpList.java
@@ -58,8 +58,9 @@ public class GridftpList {
                     return;
         }
 
+        int port = directory_url.getPort();
         GridftpClient client = new GridftpClient(directory_url.getHost(),
-            directory_url.getPort(), PortRange.getGlobusTcpPortRange(), null, new String[0],
+            port == -1 ? 2811 : port, PortRange.getGlobusTcpPortRange(), null, new String[0],
                                                  "/etc/grid-security/certificates",
                                                  CrlCheckingMode.IF_VALID, NamespaceCheckingMode.EUGRIDPMA_GLOBUS,
                                                  OCSPCheckingMode.IF_AVAILABLE);

modules/srm-common/src/main/java/org/dcache/srm/util/GridftpClient.java
@@ -760,7 +760,8 @@ public class GridftpClient
             dst_url.getScheme().equals("file")) {
             GridftpClient client;
 
-            client = new GridftpClient(src_url.getHost(), src_url.getPort(), bs,
+            int port = src_url.getPort();
+            client = new GridftpClient(src_url.getHost(), port == -1 ? 2811 : port, bs,
                                        PortRange.getGlobusTcpPortRange(), x509Credential, new String[0],
                                        "/etc/grid-security/certificates", CrlCheckingMode.IF_VALID,
                                        NamespaceCheckingMode.EUGRIDPMA_GLOBUS, OCSPCheckingMode.IF_AVAILABLE);
@@ -781,7 +782,8 @@ public class GridftpClient
                dst_url.getScheme().equals("gridftp") )
              ) {
             GridftpClient client;
-            client = new GridftpClient(dst_url.getHost(), dst_url.getPort(), bs,
+            int port = dst_url.getPort();
+            client = new GridftpClient(dst_url.getHost(), port == -1 ? 2811 : port, bs,
                                        PortRange.getGlobusTcpPortRange(), x509Credential, new String[0],
                                        "/etc/grid-security/certificates", CrlCheckingMode.IF_VALID,
                                        NamespaceCheckingMode.EUGRIDPMA_GLOBUS, OCSPCheckingMode.IF_AVAILABLE);