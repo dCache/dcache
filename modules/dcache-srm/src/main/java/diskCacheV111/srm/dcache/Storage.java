dCache/dcache

commit 45a40c3e209b554abaaf81dcb8de1006f512bba0

ftp: add support for SITE USAGE command
Motivation:

There is a semi-standard mechanism to discover capacity usage in FTP:
the SITE USAGE command.  WLCG wishes to use this to discover
space-reservation information.

Modification:

Extract the existing code for caching this information in SRM
as utility methods for building such caches.

Update LoginBroker to inject spacemanager location.

Update the NettyLineBasedDoor to build caches for talking
with SpaceManager that are shared between all FTP sessions
and that are injected into AbstractFTP door.

Add support for SITE USAGE command in AbstractFTP door that
queries SpaceManager (via caches).

If the optional TOKEN argument is supplied then the value is
used to discover reservations associated with the current
user that have this description.  The path is ignored.

If the optional TOKEN argument is omitted then the path is
examined to discover if there is a WriteToken tag.  If so,
this is used.

Result:

The SITE USAGE command is implemented, supplying information
from SpaceManager using reservations with a description that
matches the TOKEN argument (if a TOKEN was given) or that are
bound to the supplied path (if TOKEN was omitted).

Target: master
Patch: https://rb.dcache.org/r/10226/
Request: 3.1
Request: 3.0
Request: 2.16
Require-notes: yes
Require-book: no
Acked-by: Tigran Mkrtchyan

modules/dcache-srm/src/main/java/diskCacheV111/srm/dcache/Storage.java
@@ -79,7 +79,6 @@ import com.google.common.collect.Ordering;
 import com.google.common.collect.Range;
 import com.google.common.util.concurrent.CheckedFuture;
 import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import eu.emi.security.authn.x509.X509Credential;
 import org.apache.axis.types.UnsignedLong;
@@ -103,11 +102,9 @@ import java.net.ProtocolFamily;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
-import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.HashMap;
@@ -125,12 +122,15 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicLong;
 
 import diskCacheV111.poolManager.PoolMonitorV5;
+
+import org.dcache.space.ReservationCaches;
+import org.dcache.space.ReservationCaches.GetSpaceTokensKey;
+
 import diskCacheV111.services.space.Space;
 import diskCacheV111.services.space.SpaceState;
 import diskCacheV111.services.space.message.ExtendLifetime;
 import diskCacheV111.services.space.message.GetFileSpaceTokensMessage;
 import diskCacheV111.services.space.message.GetSpaceMetaData;
-import diskCacheV111.services.space.message.GetSpaceTokens;
 import diskCacheV111.services.space.message.Release;
 import diskCacheV111.services.space.message.Reserve;
 import diskCacheV111.util.AccessLatency;
@@ -218,7 +218,6 @@ import org.dcache.srm.v2_2.TRetentionPolicy;
 import org.dcache.srm.v2_2.TRetentionPolicyInfo;
 import org.dcache.srm.v2_2.TReturnStatus;
 import org.dcache.srm.v2_2.TStatusCode;
-import org.dcache.util.CacheExceptionFactory;
 import org.dcache.util.NetworkUtils;
 import org.dcache.util.NetworkUtils.InetAddressScope;
 import org.dcache.util.URIs;
@@ -230,7 +229,6 @@ import org.dcache.util.list.DirectoryStream;
 import org.dcache.util.list.NullListPrinter;
 import org.dcache.vehicles.FileAttributes;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.collect.Iterables.filter;
 import static com.google.common.collect.Iterables.isEmpty;
 import static com.google.common.collect.Maps.filterKeys;
@@ -301,8 +299,6 @@ public final class Storage
     private int numDoorInRanSelection = 3;
 
     /**
-     * A loading cache for looking up space reservations by space token.
-     *
      * Used during  uploads to verify the availability of a space reservation. In case
      * of stale data, a TURL may be handed out to the client even though the reservation
      * doesn't exist or is full. In that case the upload to the TURL will fail. This is
@@ -310,109 +306,13 @@ public final class Storage
      * after handing out the TURL.
      */
     private final LoadingCache<String,Optional<Space>> spaces =
-            CacheBuilder.newBuilder()
-                    .maximumSize(1000)
-                    .expireAfterWrite(10, MINUTES)
-                    .refreshAfterWrite(30, SECONDS)
-                    .recordStats()
-                    .build(
-                            new CacheLoader<String, Optional<Space>>()
-                            {
-                                @Override
-                                public Optional<Space> load(String token)
-                                        throws CacheException, NoRouteToCellException, InterruptedException
-                                {
-                                    Space space =
-                                            _spaceManagerStub.sendAndWait(new GetSpaceMetaData(token)).getSpaces()[0];
-                                    return Optional.fromNullable(space);
-                                }
-
-                                @Override
-                                public ListenableFuture<Optional<Space>> reload(String token, Optional<Space> oldValue)
-                                {
-                                    final SettableFuture<Optional<Space>> future = SettableFuture.create();
-                                    CellStub.addCallback(
-                                            _spaceManagerStub.send(new GetSpaceMetaData(token)),
-                                            new AbstractMessageCallback<GetSpaceMetaData>()
-                                            {
-                                                @Override
-                                                public void success(GetSpaceMetaData message)
-                                                {
-                                                    future.set(Optional.fromNullable(message.getSpaces()[0]));
-                                                }
-
-                                                @Override
-                                                public void failure(int rc, Object error)
-                                                {
-                                                    CacheException exception = CacheExceptionFactory.exceptionOf(
-                                                            rc, Objects.toString(error, null));
-                                                    future.setException(exception);
-                                                }
-                                            }, _executor);
-                                    return future;
-                                }
-                            });
+            ReservationCaches.buildSpaceLookupCache(_spaceManagerStub, _executor);
 
     /**
      * A loading cache for looking up space tokens by owner and description.
      */
     private final LoadingCache<GetSpaceTokensKey, long[]> spaceTokens =
-            CacheBuilder.newBuilder()
-                    .maximumSize(1000)
-                    .expireAfterWrite(30, SECONDS)
-                    .refreshAfterWrite(10, SECONDS)
-                    .recordStats()
-                    .build(new CacheLoader<GetSpaceTokensKey, long[]>()
-                    {
-                        @Override
-                        public long[] load(GetSpaceTokensKey key) throws Exception
-                        {
-                            try {
-                                return _spaceManagerStub.sendAndWait(createRequest(key)).getSpaceTokens();
-                            } catch (TimeoutCacheException e) {
-                                throw new SRMInternalErrorException("Space manager timeout", e);
-                            } catch (InterruptedException e) {
-                                throw new SRMInternalErrorException("Operation interrupted", e);
-                            } catch (CacheException e) {
-                                _log.warn("GetSpaceTokens failed with rc={} error={}", e.getRc(), e.getMessage());
-                                throw new SRMException("GetSpaceTokens failed with rc=" + e.getRc() +
-                                                       " error=" + e.getMessage(), e);
-                            }
-                        }
-
-                        private GetSpaceTokens createRequest(GetSpaceTokensKey key)
-                        {
-                            GetSpaceTokens message = new GetSpaceTokens(key.description);
-                            message.setSubject(new Subject(true, key.principals,
-                                                           Collections.emptySet(), Collections.emptySet()));
-                            return message;
-                        }
-
-                        @Override
-                        public ListenableFuture<long[]> reload(GetSpaceTokensKey key, long[] oldValue) throws Exception
-                        {
-                            final SettableFuture<long[]> future = SettableFuture.create();
-                            CellStub.addCallback(
-                                    _spaceManagerStub.send(createRequest(key)),
-                                    new AbstractMessageCallback<GetSpaceTokens>()
-                                    {
-                                        @Override
-                                        public void success(GetSpaceTokens message)
-                                        {
-                                            future.set(message.getSpaceTokens());
-                                        }
-
-                                        @Override
-                                        public void failure(int rc, Object error)
-                                        {
-                                            CacheException exception = CacheExceptionFactory.exceptionOf(
-                                                    rc, Objects.toString(error, null));
-                                            future.setException(exception);
-                                        }
-                                    }, _executor);
-                            return future;
-                        }
-                    });
+            ReservationCaches.buildOwnerDescriptionLookupCache(_spaceManagerStub, _executor);
 
     public Storage()
     {
@@ -2518,39 +2418,4 @@ public final class Storage
         }
     }
 
-    private static class GetSpaceTokensKey
-    {
-        private final Set<Principal> principals;
-        private final String description;
-
-        public GetSpaceTokensKey(Set<Principal> principals, String description)
-        {
-            this.principals = checkNotNull(principals);
-            this.description = description;
-        }
-
-        @Override
-        public boolean equals(Object o)
-        {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-
-            GetSpaceTokensKey that = (GetSpaceTokensKey) o;
-            return principals.equals(that.principals) &&
-                   (description == null ? that.description == null : description.equals(that.description));
-
-        }
-
-        @Override
-        public int hashCode()
-        {
-            int result = principals.hashCode();
-            result = 31 * result + (description != null ? description.hashCode() : 0);
-            return result;
-        }
-    }
 }

modules/dcache-ftp/src/main/java/org/dcache/ftp/door/AbstractFtpDoorV1.java
@@ -67,9 +67,11 @@ COPYRIGHT STATUS:
 package org.dcache.ftp.door;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Range;
@@ -112,6 +114,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.HashMap;
@@ -124,13 +128,16 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import diskCacheV111.doors.FTPTransactionLog;
 import diskCacheV111.doors.LineBasedInterpreter;
+import diskCacheV111.services.space.Space;
 import diskCacheV111.util.CacheException;
 import diskCacheV111.util.CheckStagePermission;
 import diskCacheV111.util.ChecksumFactory;
@@ -199,6 +206,7 @@ import org.dcache.poolmanager.PoolManagerStub;
 import org.dcache.services.login.IdentityResolverFactory;
 import org.dcache.services.login.IdentityResolverFactory.IdentityResolver;
 import org.dcache.services.login.RemoteLoginStrategy;
+import org.dcache.space.ReservationCaches.GetSpaceTokensKey;
 import org.dcache.util.Args;
 import org.dcache.util.AsynchronousRedirectedTransfer;
 import org.dcache.util.CDCExecutorDecorator;
@@ -307,9 +315,75 @@ public abstract class AbstractFtpDoorV1
     protected InetSocketAddress _remoteSocketAddress;
     protected CellAddressCore _cellAddress;
     protected CellEndpoint _cellEndpoint;
+    private LoadingCache<GetSpaceTokensKey, long[]> _spaceDescriptionCache;
+    private LoadingCache<String,Optional<Space>> _spaceLookupCache;
     protected Executor _executor;
     private IdentityResolverFactory _identityResolverFactory;
     private IdentityResolver _identityResolver;
+
+    /**
+     * Simple class to allow easy accumulation of space usage.
+     */
+    private class SpaceAccount
+    {
+        private final long used;
+        private final long available;
+        private final long allocated;
+        private final long total;
+
+        public SpaceAccount(Space space)
+        {
+            used = space.getUsedSizeInBytes();
+            available = space.getAvailableSpaceInBytes();
+            allocated = space.getAllocatedSpaceInBytes();
+            total = space.getSizeInBytes();
+        }
+
+        public SpaceAccount(long used, long available, long allocated, long total)
+        {
+            this.used = used;
+            this.available = available;
+            this.allocated = allocated;
+            this.total = total;
+        }
+
+        public SpaceAccount()
+        {
+            used = 0L;
+            available = 0L;
+            allocated = 0L;
+            total = 0L;
+        }
+
+        public SpaceAccount plus(SpaceAccount other)
+        {
+            long combinedUsed = used + other.used;
+            long combinedAvailable = available + other.available;
+            long combinedAllocated = allocated + other.allocated;
+            long combinedTotal = total + other.total;
+            return new SpaceAccount(combinedUsed, combinedAvailable, combinedAllocated, combinedTotal);
+        }
+
+        public long getUsed()
+        {
+            return used;
+        }
+
+        public long getAvailable()
+        {
+            return available;
+        }
+
+        public long getAllocated()
+        {
+            return allocated;
+        }
+
+        public long getTotal()
+        {
+            return total;
+        }
+    }
 
     public enum ReplyType
     {
@@ -1341,6 +1415,16 @@ public abstract class AbstractFtpDoorV1
     public void setExecutor(Executor executor)
     {
         _executor = new CDCExecutorDecorator<>(executor);
+    }
+
+    public void setSpaceDescriptionCache(LoadingCache<GetSpaceTokensKey, long[]> cache)
+    {
+        _spaceDescriptionCache = cache;
+    }
+
+    public void setSpaceLookupCache(LoadingCache<String,Optional<Space>> cache)
+    {
+        _spaceLookupCache = cache;
     }
 
     public void setPoolManagerHandler(PoolManagerHandler poolManagerHandler)
@@ -2606,6 +2690,7 @@ public abstract class AbstractFtpDoorV1
             "SITE <SP> SYMLINKFROM <SP> <path> - Register symlink location; SYMLINKTO must follow\r\n" +
             "SITE <SP> SYMLINKTO <SP> <path> - Create symlink to <path>; SYMLINKFROM must be earlier command\r\n" +
             "SITE <SP> TASKID <SP> <id> - Provide server with an identifier\r\n" +
+            "SITE <SP> USAGE <SP> [TOKEN <SP> <token> <SP> ] <path>\r\n" +
             "SITE <SP> WHOAMI - Provides the username or uid of the user")
     public void ftp_site(String arg) throws FTPCommandException
     {
@@ -2649,6 +2734,10 @@ public abstract class AbstractFtpDoorV1
         } else if (args[0].equalsIgnoreCase("TASKID")) {
             checkFTPCommand(args.length >= 2, 501, "Syntax error in parameters or arguments.");
             doTaskid(arg.substring(6));
+        } else if (args[0].equalsIgnoreCase("USAGE")) {
+            checkFTPCommand(args.length >= 2, 501, "Missing arguments in command.");
+            boolean hasToken = args[1].equalsIgnoreCase("TOKEN");
+            doUsage(hasToken ? arg.substring(12) : arg.substring(6), hasToken);
         } else if (args[0].equalsIgnoreCase("WHOAMI")) {
             checkFTPCommand(args.length == 1, 501, "Invalid command arguments.");
             doWhoami();
@@ -2936,6 +3025,67 @@ public abstract class AbstractFtpDoorV1
         //     discoverable, provided this file still exists.  In future, we
         //     may want to record client-supplied identifiers in billing.
         reply("250 OK");
+    }
+
+    /**
+     * Process a "SITE USAGE" command.  The format is described here:
+     *
+     *     https://github.com/bbockelm/globus-gridftp-osg-extensions/
+     */
+    public void doUsage(String arg, boolean hasToken) throws FTPCommandException
+    {
+        String token;
+        String requestPath;
+
+        if (hasToken) {
+            List<String> args = Splitter.on(' ').limit(2).splitToList(arg);
+            checkFTPCommand(args.size() == 2, 501, "Missing path after TOKEN value");
+            token = args.get(0);
+            requestPath = args.get(1);
+        } else {
+            token = "default";
+            requestPath = arg;
+        }
+
+        try {
+            List<String> spaceIds;
+
+            if (token.equalsIgnoreCase("default")) {
+                FileAttributes attr = _pnfs.getFileAttributes(absolutePath(requestPath), EnumSet.of(FileAttribute.STORAGEINFO));
+                String id = attr.getStorageInfo().getMap().get("writeToken");
+                checkFTPCommand(id != null, 501, "Path is not under space management");
+                spaceIds = Collections.singletonList(id);
+            } else {
+                long[] ids = _spaceDescriptionCache.get(new GetSpaceTokensKey(_subject.getPrincipals(), token));
+                checkFTPCommand(ids.length > 0, 501, "Unknown TOKEN " + token);
+                spaceIds = Arrays.stream(ids).mapToObj(Long::toString).collect(Collectors.toList());
+            }
+
+            SpaceAccount combined = _spaceLookupCache.getAll(spaceIds).values().stream()
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .map(SpaceAccount::new)
+                    .reduce(new SpaceAccount(), (a,b) -> a.plus(b));
+
+            reply("250 USAGE " + (combined.getUsed() + combined.getAllocated()) +
+                    " FREE " + combined.getAvailable() +
+                    " TOTAL " + combined.getTotal());
+        } catch (CacheException e) {
+            switch (e.getRc()) {
+            case CacheException.FILE_NOT_FOUND:
+                throw new FTPCommandException(550, "File not found", e);
+            case CacheException.TIMEOUT:
+                throw new FTPCommandException(451, "Internal timeout", e);
+            case CacheException.NOT_DIR:
+                throw new FTPCommandException(550, "Not a directory", e);
+            default:
+                throw new FTPCommandException(451, "Operation failed: " + e.getMessage(), e);
+            }
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            Throwables.throwIfUnchecked(cause);
+            throw new FTPCommandException(451, "Failed to fetch space details: " + cause.getMessage(), e);
+        }
     }
 
     public void doWhoami()

modules/dcache-ftp/src/main/java/org/dcache/ftp/door/FtpInterpreterFactory.java
@@ -17,11 +17,15 @@
  */
 package org.dcache.ftp.door;
 
+import com.google.common.base.Optional;
+import com.google.common.cache.LoadingCache;
+
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executor;
 
 import diskCacheV111.doors.LineBasedInterpreter;
 import diskCacheV111.doors.NettyLineBasedInterpreterFactory;
+import diskCacheV111.services.space.Space;
 import diskCacheV111.util.ConfigurationException;
 
 import dmg.cells.nucleus.CellAddressCore;
@@ -30,6 +34,7 @@ import dmg.util.LineWriter;
 
 import org.dcache.poolmanager.PoolManagerHandler;
 import org.dcache.services.login.IdentityResolverFactory;
+import org.dcache.space.ReservationCaches.GetSpaceTokensKey;
 import org.dcache.util.Args;
 import org.dcache.util.OptionParser;
 
@@ -51,7 +56,9 @@ public abstract class FtpInterpreterFactory implements NettyLineBasedInterpreter
     public LineBasedInterpreter create(CellEndpoint endpoint, CellAddressCore myAddress,
                                        InetSocketAddress remoteAddress, InetSocketAddress proxyAddress, InetSocketAddress localAddress,
                                        LineWriter writer, Executor executor, PoolManagerHandler poolManagerHandler,
-                                       IdentityResolverFactory idResolverFactory)
+                                       IdentityResolverFactory idResolverFactory,
+                                       LoadingCache<GetSpaceTokensKey, long[]> spaceDescriptionCache,
+                                       LoadingCache<String,Optional<Space>> spaceLookupCache)
             throws Exception
     {
         AbstractFtpDoorV1 interpreter = createInterpreter();
@@ -65,6 +72,8 @@ public abstract class FtpInterpreterFactory implements NettyLineBasedInterpreter
         interpreter.setCellAddress(myAddress);
         interpreter.setPoolManagerHandler(poolManagerHandler);
         interpreter.setIdentityResolverFactory(idResolverFactory);
+        interpreter.setSpaceDescriptionCache(spaceDescriptionCache);
+        interpreter.setSpaceLookupCache(spaceLookupCache);
         interpreter.init();
         return interpreter;
     }

commit 00d63242047af29e28ebaa3175e1bb636ae8738f

srmmanager: fix NPE when querying spaces
Motivation:

Commit 45a40c3e broke srmmanager by instantiating the SpaceManager
caches before the CellStub for space-manager was injected.  This results
in a NullPointerException being thrown under certain circumstances.

Modification:

Move the instantiation of the caches into Spring and inject the
completed caches.

Result:

Fix unreleased regression, resulting in NPE.

Target: master
Require-notes: no
Require-book: no

modules/dcache-srm/src/main/java/diskCacheV111/srm/dcache/Storage.java
@@ -305,14 +305,12 @@ public final class Storage
      * however a failure path that would exist in any case, as the reservation may expire
      * after handing out the TURL.
      */
-    private final LoadingCache<String,Optional<Space>> spaces =
-            ReservationCaches.buildSpaceLookupCache(_spaceManagerStub, _executor);
+    private LoadingCache<String,Optional<Space>> spaces;
 
     /**
      * A loading cache for looking up space tokens by owner and description.
      */
-    private final LoadingCache<GetSpaceTokensKey, long[]> spaceTokens =
-            ReservationCaches.buildOwnerDescriptionLookupCache(_spaceManagerStub, _executor);
+    private LoadingCache<GetSpaceTokensKey, long[]> spaceTokens;
 
     public Storage()
     {
@@ -324,6 +322,18 @@ public final class Storage
     public void setExecutor(Executor executor)
     {
         _executor = executor;
+    }
+
+    @Required
+    public void setSpaceLookupCache(LoadingCache<String,Optional<Space>> cache)
+    {
+        spaces = cache;
+    }
+
+    @Required
+    public void setOwnerDescriptionLookupCache(LoadingCache<GetSpaceTokensKey, long[]> cache)
+    {
+        spaceTokens = cache;
     }
 
     @Required

modules/dcache-srm/src/main/resources/diskCacheV111/srm/srmmanager.xml
@@ -189,17 +189,17 @@
          =====================================================================================
       -->
 
+    <bean id="storage-executor" class="org.dcache.util.CDCExecutorServiceDecorator"
+          destroy-method="shutdown">
+        <constructor-arg>
+            <bean class="java.util.concurrent.Executors"
+                  factory-method="newCachedThreadPool"/>
+        </constructor-arg>
+    </bean>
+
     <bean id="storage" class="diskCacheV111.srm.dcache.Storage">
         <description>dCache plugin for SRM</description>
-        <property name="executor">
-            <bean class="org.dcache.util.CDCExecutorServiceDecorator"
-                  destroy-method="shutdown">
-                <constructor-arg>
-                    <bean class="java.util.concurrent.Executors"
-                          factory-method="newCachedThreadPool"/>
-                </constructor-arg>
-            </bean>
-        </property>
+        <property name="executor" ref="storage-executor"/>
         <property name="directoryListSource" ref="list-handler"/>
         <property name="loginBrokerSource" ref="login-broker-source"/>
         <property name="pnfsStub" ref="pnfs-stub"/>
@@ -222,6 +222,18 @@
         <property name="srmGetNotSupportedProtocols" value="${srmmanager.protocols.disallowed.get}"/>
         <property name="srmPreferredProtocols" value="${srmmanager.protocols.preferred}"/>
         <property name="verificationRequired" value="${srmmanager.enable.third-party.requiring-verification-by-default}"/>
+        <property name="spaceLookupCache">
+            <bean class="org.dcache.space.ReservationCaches" factory-method="buildSpaceLookupCache">
+                <constructor-arg index="0" ref="space-manager-stub"/>
+                <constructor-arg index="1" ref="storage-executor"/>
+            </bean>
+        </property>
+        <property name="ownerDescriptionLookupCache">
+            <bean class="org.dcache.space.ReservationCaches" factory-method="buildOwnerDescriptionLookupCache">
+                <constructor-arg index="0" ref="space-manager-stub"/>
+                <constructor-arg index="1" ref="storage-executor"/>
+            </bean>
+        </property>
     </bean>
 
     <bean id="config" class="diskCacheV111.srm.dcache.Configuration" depends-on="liquibase">