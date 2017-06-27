4ccd181edc62d9e3522d85c90be73372473caab7
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2017-06-27 12:48:02 +0200

    common: make AccessLatency and RetentionPolicy more enum like
    Motivation:
    as we can't painless replace them with real enums, make there code more
    enum like.
    
    Modification:
    remove dead code and dead comments. replace chain of 'if' statements with
    a loop over all defined constants.
    
    Result:
    cleaner code
    
    Acked-by: Paul Millar
    Target: master
    Require-book: no
    Require-notes: no

--- modules/common/src/main/java/diskCacheV111/util/AccessLatency.java
+++ modules/common/src/main/java/diskCacheV111/util/AccessLatency.java
@@ -1,57 +1,3 @@
-// $Id: AccessLatency.java,v 1.3 2007-08-03 15:46:04 timur Exp $
-// $Log: not supported by cvs2svn $
-// Revision 1.2  2007/01/10 23:03:02  timur
-// more consistent argument names
-//
-// Revision 1.1  2006/09/25 21:48:07  timur
-// move AccessLatency and RetentionPolicy classes to diskCacheV111.util
-//
-// Revision 1.4  2006/09/21 07:36:52  tigran
-// added equals method
-//
-// Revision 1.3  2006/09/19 00:37:47  timur
-// more work for space reservation
-//
-// Revision 1.2  2006/08/15 22:09:45  timur
-// got the messages to get through to space manager
-//
-// Revision 1.1  2006/07/19 05:09:08  timur
-// adding more Space Manager code
-//
-// Revision 1.1  2006/07/16 05:48:57  timur
-// new explicit space manger
-//
-// Revision 1.5  2006/04/26 17:17:56  timur
-// store the history of the state transitions in the database
-//
-// Revision 1.4  2006/04/21 22:58:28  timur
-// we do not need a thread running when we start a remote transfer, but we want to control the number of the transfers, I hope the code accomplishes this now, though an addition of the new job state
-//
-// Revision 1.3  2005/05/04 21:54:52  timur
-// new scheduling policy on restart for put and get request - do not schedule the request if the user does not update its status
-//
-// Revision 1.2  2005/03/30 22:42:11  timur
-// more database schema changes
-//
-// Revision 1.1  2005/01/14 23:07:15  timur
-// moving general srm code in a separate repository
-//
-// Revision 1.5  2004/11/17 21:56:49  timur
-// adding the option which allows to store the pending or running requests in memory, fixed a restore from database bug
-//
-// Revision 1.4  2004/11/09 08:04:48  tigran
-// added SerialVersion ID
-//
-// Revision 1.3  2004/08/06 19:35:25  timur
-// merging branch srm-branch-12_May_2004 into the trunk
-//
-// Revision 1.2.2.2  2004/06/18 22:20:53  timur
-// adding sql database storage for requests
-//
-// Revision 1.2.2.1  2004/06/16 19:44:34  timur
-// added cvs logging tags and fermi copyright headers at the top, removed Copier.java and CopyJob.java
-//
-
 /*
 COPYRIGHT STATUS:
   Dec 1st 2001, Fermi National Accelerator Laboratory (FNAL) documents and
@@ -117,17 +63,15 @@ COPYRIGHT STATUS:
   obligated to secure any necessary Government licenses before exporting
   documents or software obtained from this server.
  */
-
-/*
- * FileState.java
- *
- * Created on March 19, 2004, 2:51 PM
- */
-
 package diskCacheV111.util;
 
 import java.io.ObjectStreamException;
 import java.io.Serializable;
+
+/*
+ * FIXME: this class have to be converted into java enum, but we have to keep
+ * compatibility with existing pools.
+ */
 
 /**
  *
@@ -143,6 +87,11 @@ public final class AccessLatency implements Serializable {
     public static final AccessLatency ONLINE        = new AccessLatency("ONLINE",    1);
     public static final AccessLatency NEARLINE     = new AccessLatency("NEARLINE", 0);
 
+    private final static AccessLatency[] ALL_LATENCIES = {
+        ONLINE,
+        NEARLINE
+    };
+
     /**
      * Creates a new instance of FileState
      */
@@ -152,10 +101,10 @@ public final class AccessLatency implements Serializable {
     }
 
     public static AccessLatency[] getAllLatencies() {
-        return new AccessLatency[] {
-         ONLINE,
-         NEARLINE};
+        return ALL_LATENCIES;
     }
+
+    @Override
     public String toString() {
         return _name;
     }
@@ -172,40 +121,37 @@ public final class AccessLatency implements Serializable {
             throw new NullPointerException(" null state ");
         }
 
-        if(ONLINE._name.equalsIgnoreCase(latency)) {
-            return ONLINE;
+        for(AccessLatency al: getAllLatencies()) {
+            if (al._name.equalsIgnoreCase(latency)) {
+                return al;
+            }
         }
 
-        if(NEARLINE._name.equalsIgnoreCase(latency)) {
-            return NEARLINE;
-        }
-
-        try{
+        try {
             int id = Integer.parseInt(latency);
             return getAccessLatency(id);
-        }
-        catch(Exception e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Unknown AccessLatency");
         }
     }
 
     public static AccessLatency getAccessLatency(int id) throws IllegalArgumentException {
 
-        if(ONLINE._id == id) {
-            return ONLINE;
-        }
-
-        if(NEARLINE._id == id) {
-            return NEARLINE;
+        for (AccessLatency al : getAllLatencies()) {
+            if (al._id == id) {
+                return al;
+            }
         }
 
         throw new IllegalArgumentException("Unknown AccessLatency Id");
     }
 
+    @Override
     public boolean equals(Object obj) {
-		return ( obj instanceof AccessLatency) && ( ((AccessLatency)obj).getId() == this.getId() );
-	}
+        return (obj instanceof AccessLatency) && (((AccessLatency) obj).getId() == this.getId());
+    }
 
+    @Override
     public int hashCode() {
         return _name.hashCode();
     }
@@ -217,27 +163,4 @@ public final class AccessLatency implements Serializable {
     public Object readResolve() throws ObjectStreamException {
        return AccessLatency.getAccessLatency(getId());
     }
-
-    /*
-     * if we are to provide custom serialization
-     * we could do:
-    private void readObject(java.io.ObjectInputStream stream)
-        throws java.io.IOException, ClassNotFoundException {
-        int id = stream.readInt();
-        try {
-           AccessLatency al =  getAccessLatency(id);
-           this._id = id;
-           this._name = al._name;
-        }
-        catch ( IllegalArgumentException iae) {
-            throw new java.io.IOException(iae.getMessage());
-        }
-    }
-
-    private void writeObject(java.io.ObjectOutputStream stream)
-        throws java.io.IOException {
-        stream.write(_id);
-    }
-     */
-
 }
--- modules/common/src/main/java/diskCacheV111/util/RetentionPolicy.java
+++ modules/common/src/main/java/diskCacheV111/util/RetentionPolicy.java
@@ -1,63 +1,3 @@
-// $Id: RetentionPolicy.java,v 1.4 2007-08-03 15:46:04 timur Exp $
-// $Log: not supported by cvs2svn $
-// Revision 1.3  2007/01/12 21:07:51  timur
-// made correction for the CUSTODIAL space selection
-//
-// Revision 1.2  2007/01/10 23:03:02  timur
-// more consistent argument names
-//
-// Revision 1.1  2006/09/25 21:48:07  timur
-// move AccessLatency and RetentionPolicy classes to diskCacheV111.util
-//
-// Revision 1.5  2006/09/21 08:48:52  tigran
-// added equals method
-//
-// Revision 1.4  2006/09/19 00:37:48  timur
-// more work for space reservation
-//
-// Revision 1.3  2006/09/15 22:37:44  timur
-// started on storing the RetentionPolicy/AccessLatency in Pnfs
-//
-// Revision 1.2  2006/08/15 22:09:45  timur
-// got the messages to get through to space manager
-//
-// Revision 1.1  2006/07/19 05:09:09  timur
-// adding more Space Manager code
-//
-// Revision 1.1  2006/07/16 05:48:57  timur
-// new explicit space manger
-//
-// Revision 1.5  2006/04/26 17:17:56  timur
-// store the history of the state transitions in the database
-//
-// Revision 1.4  2006/04/21 22:58:28  timur
-// we do not need a thread running when we start a remote transfer, but we want to control the number of the transfers, I hope the code accomplishes this now, though an addition of the new job state
-//
-// Revision 1.3  2005/05/04 21:54:52  timur
-// new scheduling policy on restart for put and get request - do not schedule the request if the user does not update its status
-//
-// Revision 1.2  2005/03/30 22:42:11  timur
-// more database schema changes
-//
-// Revision 1.1  2005/01/14 23:07:15  timur
-// moving general srm code in a separate repository
-//
-// Revision 1.5  2004/11/17 21:56:49  timur
-// adding the option which allows to store the pending or running requests in memory, fixed a restore from database bug
-//
-// Revision 1.4  2004/11/09 08:04:48  tigran
-// added SerialVersion ID
-//
-// Revision 1.3  2004/08/06 19:35:25  timur
-// merging branch srm-branch-12_May_2004 into the trunk
-//
-// Revision 1.2.2.2  2004/06/18 22:20:53  timur
-// adding sql database storage for requests
-//
-// Revision 1.2.2.1  2004/06/16 19:44:34  timur
-// added cvs logging tags and fermi copyright headers at the top, removed Copier.java and CopyJob.java
-//
-
 /*
 COPYRIGHT STATUS:
   Dec 1st 2001, Fermi National Accelerator Laboratory (FNAL) documents and
@@ -123,17 +63,15 @@ COPYRIGHT STATUS:
   obligated to secure any necessary Government licenses before exporting
   documents or software obtained from this server.
  */
-
-/*
- * FileState.java
- *
- * Created on March 19, 2004, 2:51 PM
- */
-
 package diskCacheV111.util;
 
 import java.io.ObjectStreamException;
 import java.io.Serializable;
+
+/*
+ * FIXME: this class have to be converted into java enum, but we have to keep
+ * compatibility with existing pools.
+ */
 
 /**
  *
@@ -144,9 +82,15 @@ public final class RetentionPolicy implements Serializable {
     private final String _name;
     private final int _id;
 
-    public static final RetentionPolicy REPLICA        = new RetentionPolicy("REPLICA",    2);
+    public static final RetentionPolicy REPLICA    = new RetentionPolicy("REPLICA", 2);
     public static final RetentionPolicy OUTPUT     = new RetentionPolicy("OUTPUT", 1);
-    public static final RetentionPolicy CUSTODIAL     = new RetentionPolicy("CUSTODIAL", 0);
+    public static final RetentionPolicy CUSTODIAL  = new RetentionPolicy("CUSTODIAL", 0);
+
+    private final static RetentionPolicy[] ALL_POLICIES = {
+        REPLICA,
+        OUTPUT,
+        CUSTODIAL
+    };
 
     /**
      * Creates a new instance of FileState
@@ -156,16 +100,11 @@ public final class RetentionPolicy implements Serializable {
         _id = id;
     }
 
-    public static RetentionPolicy[] getAllPoliciess() {
-	    return getAllPolicies();
+    public static RetentionPolicy[] getAllPolicies() {
+            return ALL_POLICIES;
     }
 
-    public static RetentionPolicy[] getAllPolicies() {
-	    return new RetentionPolicy[] {
-		    REPLICA,
-		    OUTPUT,
-		    CUSTODIAL};
-    }
+    @Override
     public String toString() {
         return _name;
     }
@@ -182,38 +121,26 @@ public final class RetentionPolicy implements Serializable {
             throw new NullPointerException(" null state ");
         }
 
-        if(REPLICA._name.equalsIgnoreCase(policy)) {
-            return REPLICA;
+        for(RetentionPolicy rp: getAllPolicies()) {
+            if (rp._name.equalsIgnoreCase(policy)) {
+                return rp;
+            }
         }
 
-        if(OUTPUT._name.equalsIgnoreCase(policy)) {
-            return OUTPUT;
-        }
-
-        if(CUSTODIAL._name.equalsIgnoreCase(policy)) {
-            return CUSTODIAL;
-        }
-        try{
+        try {
             int id = Integer.parseInt(policy);
             return getRetentionPolicy(id);
-        }
-        catch(Exception e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Unknown Policy");
         }
     }
 
     public static RetentionPolicy getRetentionPolicy(int id) throws IllegalArgumentException {
 
-        if(REPLICA._id == id) {
-            return REPLICA;
-        }
-
-        if(OUTPUT._id == id) {
-            return OUTPUT;
-        }
-
-        if(CUSTODIAL._id == id) {
-            return CUSTODIAL;
+        for(RetentionPolicy rp: getAllPolicies()) {
+            if (rp._id == id) {
+                return rp;
+            }
         }
 
         throw new IllegalArgumentException("Unknown policy Id");
@@ -223,10 +150,12 @@ public final class RetentionPolicy implements Serializable {
         return getRetentionPolicy(value);
     }
 
+    @Override
     public boolean equals(Object obj) {
         return ( obj instanceof RetentionPolicy) && ( ((RetentionPolicy)obj).getId() == this.getId() );
     }
 
+    @Override
     public int hashCode() {
         return _name.hashCode();
     }
@@ -234,6 +163,4 @@ public final class RetentionPolicy implements Serializable {
     public Object readResolve() throws ObjectStreamException {
        return RetentionPolicy.getRetentionPolicy(getId());
     }
-
-
 }
