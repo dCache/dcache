dCache/dcache

commit ade17bb00e2a676151694b34eef9598c1feb5c37

pool: handle initial space allocation for existing files
Motivation:
if (hypothetically) pool opens an existing file for writing, then
space allocation will be confused as:
  - new file can have a different size
  - the file's existing space will be counted twice

Modification:
  - truncate existing file if needed
  - set initial space allocation to files actual size

Result:
pools existing behavior is unchanged, as we don't start movers on
existing files. However, hypothetical update of a file will not
miscalculate space allocation.

Acked-by: Paul Millar
Target: master
Require-book: no
Require-notes: no

modules/dcache/src/main/java/org/dcache/pool/repository/v5/WriteHandleImpl.java
@@ -120,8 +120,21 @@ class WriteHandleImpl implements ReplicaDescriptor
     }
 
     @Override
-    public RepositoryChannel createChannel() throws IOException {
-        return _entry.openChannel(FileStore.O_RW);
+    public synchronized RepositoryChannel createChannel() throws IOException {
+        RepositoryChannel channel = _entry.openChannel(FileStore.O_RW);
+        /*
+         * initial allocation matches file size. For new files it zero any way,
+         * for existing file allocation was already performed during initial write.
+         */
+         long currentSize = channel.size();
+        _allocated = currentSize;
+        if (_fileAttributes.isDefined(SIZE)) {
+            if (currentSize > _fileAttributes.getSize()) {
+                channel.truncate(_fileAttributes.getSize());
+                free(currentSize - _fileAttributes.getSize());
+            }
+        }
+        return channel;
     }
 
     /**
@@ -227,14 +240,16 @@ class WriteHandleImpl implements ReplicaDescriptor
     }
 
     /**
-     * Freeing space through a write handle is not supported. This
-     * method always throws IllegalStateException.
+     * Freeing allocated space.
+     *
+     * @param size number of bytes to free.
      */
     @Override
-    public void free(long size)
-        throws IllegalStateException
+    public synchronized void free(long size)
     {
-        throw new IllegalStateException("Space cannot be freed through a write handle");
+        checkState(size <= _allocated, "Can't free more space than allocated.");
+        _allocator.free(size);
+        _allocated -= size;
     }
 
     /**

commit 22365dcfb6fb2916dbbcd6b1168a8854d19c978e

pool: fix regression in accounting during file upload
Motivation:

A regression, introduced with commit ade17bb0, resulted in the pool
space accounting not reflecting the uploaded file size.  Instead, the
pool capacity usage increased by 50 MiB when the pool accepted any file
less than 50 MiB.

Modification:

The problem stems from the pool calling createChannel multiple times.
The additional code that sets the allocated to the current file size
resulted in the normal over-commit being lost.

To fix this, this patch introduces an additional state in
WriteHandleImpl so that _allocated is assigned only on the first open.

Result:

An unrelease regression was fixed and pools show the correct used
capacity after a file is uploaded.

Target: master
Require-notes: no
Require-book: no
Acked-by: Tigran Mkrtchyan (visually)

modules/dcache/src/main/java/org/dcache/pool/repository/v5/WriteHandleImpl.java
@@ -35,7 +35,7 @@ class WriteHandleImpl implements ReplicaDescriptor
 {
     enum HandleState
     {
-        OPEN, COMMITTED, CLOSED
+        INIT, OPEN, COMMITTED, CLOSED
     }
 
     private static final Logger _log =
@@ -99,7 +99,7 @@ class WriteHandleImpl implements ReplicaDescriptor
         _initialState = entry.getState();
         _targetState = checkNotNull(targetState);
         _stickyRecords = checkNotNull(stickyRecords);
-        _state = HandleState.OPEN;
+        _state = HandleState.INIT;
         _allocated = 0;
 
         checkState(_initialState != ReplicaState.FROM_CLIENT || _fileAttributes.isDefined(EnumSet.of(RETENTION_POLICY, ACCESS_LATENCY)));
@@ -114,26 +114,34 @@ class WriteHandleImpl implements ReplicaDescriptor
         }
     }
 
-    private synchronized boolean isOpen()
+    private synchronized boolean isAllocatable()
     {
-        return _state == HandleState.OPEN;
+        return _state == HandleState.INIT || _state == HandleState.OPEN;
     }
 
     @Override
     public synchronized RepositoryChannel createChannel() throws IOException {
         RepositoryChannel channel = _entry.openChannel(FileStore.O_RW);
+
         /*
          * initial allocation matches file size. For new files it zero any way,
          * for existing file allocation was already performed during initial write.
+         *
+         * This must happen only once since currently the pool calls
+         * createChannel twice.
          */
-         long currentSize = channel.size();
-        _allocated = currentSize;
-        if (_fileAttributes.isDefined(SIZE)) {
-            if (currentSize > _fileAttributes.getSize()) {
-                channel.truncate(_fileAttributes.getSize());
-                free(currentSize - _fileAttributes.getSize());
+        if (_state == HandleState.INIT) {
+             long currentSize = channel.size();
+            _allocated = currentSize;
+            if (_fileAttributes.isDefined(SIZE)) {
+                if (currentSize > _fileAttributes.getSize()) {
+                    channel.truncate(_fileAttributes.getSize());
+                    free(currentSize - _fileAttributes.getSize());
+                }
             }
+            setState(HandleState.OPEN);
         }
+
         return channel;
     }
 
@@ -152,7 +160,7 @@ class WriteHandleImpl implements ReplicaDescriptor
             wait();
         }
 
-        if (!isOpen()) {
+        if (!isAllocatable()) {
             throw new IllegalStateException("Handle is closed");
         }
 
@@ -189,7 +197,7 @@ class WriteHandleImpl implements ReplicaDescriptor
         try {
             _allocator.allocate(size);
         } catch (InterruptedException e) {
-            if (!isOpen()) {
+            if (!isAllocatable()) {
                 throw new IllegalStateException("Handle is closed");
             }
             throw e;
@@ -223,7 +231,7 @@ class WriteHandleImpl implements ReplicaDescriptor
         try {
             isAllocated = _allocator.allocateNow(size);
         } catch (InterruptedException e) {
-            if (!isOpen()) {
+            if (!isAllocatable()) {
                 throw new IllegalStateException("Handle is closed");
             }
             throw e;
@@ -312,9 +320,8 @@ class WriteHandleImpl implements ReplicaDescriptor
     public synchronized void commit()
         throws IllegalStateException, InterruptedException, CacheException
     {
-        if (_state != HandleState.OPEN) {
-            throw new IllegalStateException("Handle is closed");
-        }
+        checkState(_state != HandleState.INIT, "Channel not created");
+        checkState(_state == HandleState.OPEN, "Handle is closed");
 
         try {
             _entry.setLastAccessTime((_atime == null) ? System.currentTimeMillis() : _atime);
@@ -441,6 +448,7 @@ class WriteHandleImpl implements ReplicaDescriptor
         case CLOSED:
             throw new IllegalStateException("Handle is closed");
 
+        case INIT:
         case OPEN:
             fail();
             setState(HandleState.CLOSED);