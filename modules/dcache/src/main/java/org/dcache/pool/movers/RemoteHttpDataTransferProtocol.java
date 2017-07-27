dCache/dcache

commit d132d86052507b3f459c72c8b6788ba5deb5d638

webdav, dcache: Support for third party copy on the Webdav door using OpenId Credentials
Webdav supports requests with Bearer Token based on OpenId Credentials.
However, third-party-copy did not support OpenId Credentials. With this
commit, such a request can be accepted by the Webdav door. The door performs
a token-exchange with OpenId Provider to obtain a delegated token for
itself, which it can use (and refresh) to perform third-party-copy.

Three properties need to be setup, in order to use this feature
- webdav.oidc.client.ids : client ids of the regisered clients to OpenId
  provider for bearer token delegation via token exchange
- webdav.oidc.client.secrets : client secrets corresponding to client
  ids registered with the OpenId providers.

More info on OpenId Connect/OAuth2 token exchange here,
https://tools.ietf.org/html/draft-ietf-oauth-token-exchange-04#section-2

Ticket:
Acked-by: Paul Millar <paul.millar@desy.de>
Target: master
Require-book: no
Require-notes: no
Patch: https://rb.dcache.org/r/9599/

modules/dcache/src/main/java/org/dcache/pool/movers/RemoteHttpDataTransferProtocol.java
@@ -3,6 +3,7 @@ package org.dcache.pool.movers;
 import com.google.common.base.Optional;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
@@ -34,6 +35,8 @@ import diskCacheV111.vehicles.RemoteHttpDataTransferProtocolInfo;
 
 import dmg.cells.nucleus.CellEndpoint;
 
+import org.dcache.auth.OpenIdCredential;
+import org.dcache.auth.OpenIdCredentialRefreshable;
 import org.dcache.pool.movers.MoverChannel.AllocatorMode;
 import org.dcache.pool.repository.Allocator;
 import org.dcache.pool.repository.RepositoryChannel;
@@ -147,6 +150,8 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
      */
     private static final int MAX_REDIRECTIONS = 20;
 
+    private static final String AUTH_BEARER = "Bearer ";
+
     // REVISIT: we may wish to generate a value based on the algorithms dCache
     // supports
     private static final String WANT_DIGEST_VALUE = "adler32;q=1, md5;q=0.8";
@@ -219,13 +224,7 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
     private void receiveFile(final RemoteHttpDataTransferProtocolInfo info)
             throws ThirdPartyTransferFailedCacheException
     {
-        HttpGet get = new HttpGet(info.getUri());
-        get.addHeader("Want-Digest", WANT_DIGEST_VALUE);
-        info.getHeaders().forEach(get::addHeader);
-        get.setConfig(RequestConfig.custom()
-                              .setConnectTimeout(CONNECTION_TIMEOUT)
-                              .setSocketTimeout(SOCKET_TIMEOUT)
-                              .build());
+        HttpGet get = buildGetRequest(info);
 
         try (CloseableHttpResponse response = _client.execute(get)) {
             StatusLine statusLine = response.getStatusLine();
@@ -278,6 +277,17 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
         }
     }
 
+    private HttpGet buildGetRequest(RemoteHttpDataTransferProtocolInfo info) {
+        HttpGet get = new HttpGet(info.getUri());
+        get.addHeader("Want-Digest", WANT_DIGEST_VALUE);
+        addHeadersToRequest(info, get);
+        get.setConfig(RequestConfig.custom()
+                              .setConnectTimeout(CONNECTION_TIMEOUT)
+                              .setSocketTimeout(SOCKET_TIMEOUT)
+                              .build());
+        return get;
+    }
+
     private RepositoryChannel decorateForChecksumCalculation(RepositoryChannel
             baseChannel)
     {
@@ -321,7 +331,7 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
         URI location = info.getUri();
 
         for (int attempt = 0; attempt < MAX_REDIRECTIONS; attempt++) {
-            HttpPut put = buildPutRequest(location, info.getHeaders(), length);
+            HttpPut put = buildPutRequest(info, length);
 
             try (CloseableHttpResponse response = _client.execute(put)) {
                 StatusLine status = response.getStatusLine();
@@ -371,20 +381,20 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
                 "number of redirections; last location was " + location);
     }
 
-    private HttpPut buildPutRequest(URI location, Map<String,String> extraHeaders, long length)
+    private HttpPut buildPutRequest(RemoteHttpDataTransferProtocolInfo info, long length)
     {
-        HttpPut request = new HttpPut(location);
-        request.setConfig(RequestConfig.custom()
+        HttpPut put = new HttpPut(info.getUri());
+        put.setConfig(RequestConfig.custom()
                                   .setConnectTimeout(CONNECTION_TIMEOUT)
                                   .setExpectContinueEnabled(true)
                                   .setSocketTimeout(0)
                                   .build());
-        extraHeaders.forEach(request::addHeader);
-        request.setEntity(new InputStreamEntity(Channels.newInputStream(_channel), length));
+        addHeadersToRequest(info, put);
+        put.setEntity(new InputStreamEntity(Channels.newInputStream(_channel), length));
 
         // FIXME add SO_KEEPALIVE setting
 
-        return request;
+        return put;
     }
 
     private void verifyRemoteFile(RemoteHttpDataTransferProtocolInfo info)
@@ -416,7 +426,7 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
                 }
                 isFirstAttempt = false;
 
-                HttpHead head = buildHeadRequest(info.getUri(), info.getHeaders());
+                HttpHead head = buildHeadRequest(info);
                 try (CloseableHttpResponse response = _client.execute(head)) {
                     StatusLine status = response.getStatusLine();
 
@@ -454,16 +464,16 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
                 "to provide length after " + (t_max / 1_000) + "s");
     }
 
-    private HttpHead buildHeadRequest(URI location, Map<String,String> extraHeaders)
+    private HttpHead buildHeadRequest(RemoteHttpDataTransferProtocolInfo info)
     {
-        HttpHead request = new HttpHead(location);
-        request.addHeader("Want-Digest", WANT_DIGEST_VALUE);
-        request.setConfig(RequestConfig.custom()
+        HttpHead head = new HttpHead(info.getUri());
+        head.addHeader("Want-Digest", WANT_DIGEST_VALUE);
+        head.setConfig(RequestConfig.custom()
                                   .setConnectTimeout(CONNECTION_TIMEOUT)
                                   .setSocketTimeout(SOCKET_TIMEOUT)
                                   .build());
-        extraHeaders.forEach(request::addHeader);
-        return request;
+        addHeadersToRequest(info, head);
+        return head;
     }
 
     private void checkChecksums(RemoteHttpDataTransferProtocolInfo info,
@@ -531,12 +541,7 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
     private void deleteRemoteFile(String why, RemoteHttpDataTransferProtocolInfo info)
             throws ThirdPartyTransferFailedCacheException
     {
-        HttpDelete delete = new HttpDelete(info.getUri());
-        delete.setConfig(RequestConfig.custom()
-                                 .setConnectTimeout(CONNECTION_TIMEOUT)
-                                 .setSocketTimeout(SOCKET_TIMEOUT)
-                                 .build());
-        info.getHeaders().forEach(delete::addHeader);
+        HttpDelete delete = buildDeleteRequest(info);
 
         try (CloseableHttpResponse response = _client.execute(delete)) {
             StatusLine status = response.getStatusLine();
@@ -555,6 +560,27 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
         }
     }
 
+    private HttpDelete buildDeleteRequest(RemoteHttpDataTransferProtocolInfo info) {
+        HttpDelete delete = new HttpDelete(info.getUri());
+        delete.setConfig(RequestConfig.custom()
+                                 .setConnectTimeout(CONNECTION_TIMEOUT)
+                                 .setSocketTimeout(SOCKET_TIMEOUT)
+                                 .build());
+        addHeadersToRequest(info, delete);
+
+        return delete;
+    }
+
+    private void addHeadersToRequest(RemoteHttpDataTransferProtocolInfo info,
+                                    HttpRequest request)
+    {
+        info.getHeaders().forEach(request::addHeader);
+        if (info.hasTokenCredential()) {
+            request.addHeader("Authorization",
+                    AUTH_BEARER +
+                            new OpenIdCredentialRefreshable(info.getTokenCredential(), _client).getBearerToken());
+        }
+    }
 
     @Override
     public long getLastTransferred()

modules/common/pom.xml
@@ -35,6 +35,14 @@
         <groupId>eu.eu-emi.security</groupId>
         <artifactId>canl</artifactId>
     </dependency>
+    <dependency>
+      <groupId>org.json</groupId>
+      <artifactId>json</artifactId>
+    </dependency>
+      <dependency>
+          <groupId>org.apache.httpcomponents</groupId>
+          <artifactId>httpclient</artifactId>
+      </dependency>
   </dependencies>
 
     <build>

modules/common/src/main/java/org/dcache/auth/OpenIdClientSecret.java
@@ -1,0 +1,24 @@
+package org.dcache.auth;
+
+import java.io.Serializable;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class OpenIdClientSecret implements Serializable
+{
+    private final String id;
+    private final String secret;
+
+    public OpenIdClientSecret(String id, String secret) {
+        this.id = checkNotNull(id);
+        this.secret = checkNotNull(secret);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+}

modules/common/src/main/java/org/dcache/auth/OpenIdCredential.java
@@ -1,0 +1,44 @@
+package org.dcache.auth;
+
+/**
+ *  An OpenId Connect credential is composed of the following information.
+ *
+ *  {
+ *      "access_token": "eyJraWQMCJ9.N_eoiDrnPTv46ZNj",
+ *      "token_type": "Bearer",
+ *      "refresh_token": "eyJhbGciO.",
+ *      "expires_in": 3599,
+ *      "scope": "openid offline_access profile email",
+ *      "id_token": "Json Web Token"
+ *  }
+ *
+ *  http://openid.net/specs/openid-connect-core-1_0.html
+ *
+ *  This interface exposes the methods necessary to obtain these information from a parsed OpenId Connect
+ *  Credential.
+ *
+ *  The implementation of this interface is expected to hold information about the OpenId Connect credential
+ *  as well as the information necessary to perform token exchange via delegaton. It is also suggested to
+ *  hide the access token refresh within the implementation.
+ *
+ */
+public interface OpenIdCredential
+{
+    String getBearerToken();
+
+    String getAccessToken();
+
+    long getExpiresAt();
+
+    String getIssuedTokenType();
+
+    String getRefreshToken();
+
+    String getScope();
+
+    String getTokenType();
+
+    OpenIdClientSecret getClientCredential();
+
+    String getOpenidProvider();
+}

modules/common/src/main/java/org/dcache/auth/OpenIdCredentialRefreshable.java
@@ -1,0 +1,101 @@
+package org.dcache.auth;
+
+import com.google.common.base.Charsets;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class OpenIdCredentialRefreshable extends WrappingOpenIdCredential
+{
+    private static final Logger LOG =
+            LoggerFactory.getLogger(OpenIdCredentialRefreshable.class);
+    private final HttpClient client;
+
+    public OpenIdCredentialRefreshable(OpenIdCredential credential, HttpClient client) {
+        super(checkNotNull(credential, "OpenId Credential can't be null"));
+        this.client = checkNotNull(client, "Http Client can't be null");
+    }
+
+    @Override
+    public String getBearerToken()
+    {
+        if (timeToRefresh()) {
+            try {
+                refreshOpenIdCredentials();
+            } catch (IOException | AuthenticationException e) {
+                LOG.warn("Error Refreshing OpenId Bearer Token with {}: {}",
+                        credential.getOpenidProvider(), e.getMessage());
+            }
+        }
+        return credential.getBearerToken();
+    }
+
+    private synchronized void refreshOpenIdCredentials() throws IOException, AuthenticationException
+    {
+        HttpPost post = new HttpPost(credential.getOpenidProvider());
+        BasicScheme scheme = new BasicScheme(Charsets.UTF_8);
+        UsernamePasswordCredentials clientCreds = new UsernamePasswordCredentials(
+                credential.getClientCredential().getId(),
+                credential.getClientCredential().getSecret());
+
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("client_id", credential.getClientCredential().getId()));
+        params.add(new BasicNameValuePair("client_secret", credential.getClientCredential().getSecret()));
+        params.add(new BasicNameValuePair("grant_type", "refresh_token"));
+        params.add(new BasicNameValuePair("refresh_token", credential.getRefreshToken()));
+        params.add(new BasicNameValuePair("scope", credential.getScope()));
+        post.setEntity(new UrlEncodedFormEntity(params));
+        post.addHeader(scheme.authenticate(clientCreds, post, new BasicHttpContext()) );
+
+        HttpResponse response = client.execute(post);
+        if (response.getStatusLine().getStatusCode() == 200) {
+            updateCredential(parseResponseToJson(response));
+        } else {
+            throw new IOException(String.format("Error Refreshing OpenId Bearer Token [%s]: %s",
+                                                    response.getStatusLine().getStatusCode(),
+                                                    credential.getOpenidProvider()));
+        }
+    }
+
+    private boolean timeToRefresh() {
+        return (credential.getExpiresAt() - System.currentTimeMillis()) < 60*1000L;
+    }
+
+    private JSONObject parseResponseToJson(HttpResponse response) throws IOException
+    {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        response.getEntity().writeTo(os);
+        return new JSONObject(new String(os.toByteArray(), Charsets.UTF_8));
+    }
+
+    private void updateCredential(JSONObject json) throws IOException
+    {
+        try {
+            this.credential = StaticOpenIdCredential.copyOf(credential)
+                                                    .accessToken(json.getString("access_token"))
+                                                    .expiry(json.getLong("expires_in"))
+                                                    .build();
+        } catch (JSONException je) {
+            throw new IOException("Error Parsing response of OpenId Bearer Token Refresh: " + je.getMessage());
+        }
+    }
+}

modules/common/src/main/java/org/dcache/auth/StaticOpenIdCredential.java
@@ -1,0 +1,201 @@
+package org.dcache.auth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class StaticOpenIdCredential implements OpenIdCredential, Serializable
+{
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG =
+            LoggerFactory.getLogger(StaticOpenIdCredential.class);
+
+    private final String accessToken;
+    private final long expiresAt;
+    private final String issuedTokenType;
+    private final String refreshToken;
+    private final String scope;
+    private final String tokenType;
+
+    // Use to refresh Access Token
+    private final OpenIdClientSecret clientCredential;
+    private final String openidProvider;
+
+    private StaticOpenIdCredential(Builder builder) {
+        this.accessToken = builder._accessToken;
+        this.expiresAt = builder._expiresAt;
+        this.issuedTokenType = builder._issuedTokenType;
+        this.refreshToken = builder._refreshToken;
+        this.scope = builder._scope;
+        this.tokenType = builder._tokenType;
+        this.clientCredential = builder._clientCredential;
+        this.openidProvider = builder._urlOpenidProvider;
+    }
+
+    @Override
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    @Override
+    public long getExpiresAt() {
+        return expiresAt;
+    }
+
+    @Override
+    public String getIssuedTokenType() {
+        return issuedTokenType;
+    }
+
+    @Override
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    @Override
+    public String getScope() {
+        return scope;
+    }
+
+    @Override
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    @Override
+    public OpenIdClientSecret getClientCredential() {
+        return clientCredential;
+    }
+
+    @Override
+    public String getOpenidProvider() {
+        return openidProvider;
+    }
+
+    @Override
+    public String getBearerToken() {
+        return getAccessToken();
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+            return true;
+        if (!(o instanceof StaticOpenIdCredential))
+            return false;
+
+        StaticOpenIdCredential that = (StaticOpenIdCredential) o;
+
+        if (expiresAt != that.expiresAt)
+            return false;
+        if (!accessToken.equals(that.accessToken))
+            return false;
+        if (!issuedTokenType.equals(that.issuedTokenType))
+            return false;
+        if (!refreshToken.equals(that.refreshToken))
+            return false;
+        if (!scope.equals(that.scope))
+            return false;
+        if (!tokenType.equals(that.tokenType))
+            return false;
+        if (!clientCredential.equals(that.clientCredential))
+            return false;
+        return openidProvider.equals(that.openidProvider);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(accessToken,
+                            expiresAt,
+                            issuedTokenType,
+                            refreshToken,
+                            scope,
+                            tokenType,
+                            clientCredential, openidProvider);
+    }
+
+    public static Builder copyOf(OpenIdCredential credential)
+    {
+        return new Builder().accessToken(credential.getAccessToken())
+                            .expiry(credential.getExpiresAt())
+                            .refreshToken(credential.getRefreshToken())
+                            .issuedTokenType(credential.getIssuedTokenType())
+                            .scope(credential.getScope())
+                            .tokenType(credential.getTokenType())
+                            .clientCredential(credential.getClientCredential())
+                            .provider(credential.getOpenidProvider());
+    }
+
+    public static class Builder
+    {
+        private String _accessToken = null;
+        private long _expiresAt = 0L;
+        private String _issuedTokenType = null;
+        private String _refreshToken = null;
+        private String _scope = null;
+        private String _tokenType = null;
+        private OpenIdClientSecret _clientCredential = null;
+        private String _urlOpenidProvider = null;
+
+        public Builder()
+        {
+        }
+
+        public Builder accessToken(String accessToken)
+        {
+            this._accessToken = accessToken;
+            return this;
+        }
+
+        public Builder expiry(long expiresIn)
+        {
+            this._expiresAt = System.currentTimeMillis() + expiresIn*1000L;
+            return this;
+        }
+
+        public Builder refreshToken(String refreshToken)
+        {
+            this._refreshToken = refreshToken;
+            return this;
+        }
+
+        public Builder issuedTokenType(String issuedTokenType)
+        {
+            this._issuedTokenType = issuedTokenType;
+            return this;
+        }
+
+        public Builder scope(String scope)
+        {
+            this._scope = scope;
+            return this;
+        }
+
+        public Builder tokenType(String tokenType)
+        {
+            this._tokenType = tokenType;
+            return this;
+        }
+
+        public Builder clientCredential(OpenIdClientSecret clientCredential)
+        {
+            this._clientCredential = clientCredential;
+            return this;
+        }
+
+        public Builder provider(String url)
+        {
+            this._urlOpenidProvider = url;
+            return this;
+        }
+
+        public StaticOpenIdCredential build()
+        {
+            return new StaticOpenIdCredential(this);
+        }
+    }
+}

modules/common/src/main/java/org/dcache/auth/WrappingOpenIdCredential.java
@@ -1,0 +1,54 @@
+package org.dcache.auth;
+
+public class WrappingOpenIdCredential implements OpenIdCredential {
+    protected OpenIdCredential credential;
+
+    public WrappingOpenIdCredential(OpenIdCredential credential) {
+        this.credential = credential;
+    }
+
+    @Override
+    public String getBearerToken() {
+        return credential.getBearerToken();
+    }
+
+    @Override
+    public String getAccessToken() {
+        return credential.getAccessToken();
+    }
+
+    @Override
+    public long getExpiresAt() {
+        return credential.getExpiresAt();
+    }
+
+    @Override
+    public String getIssuedTokenType() {
+        return credential.getIssuedTokenType();
+    }
+
+    @Override
+    public String getRefreshToken() {
+        return credential.getRefreshToken();
+    }
+
+    @Override
+    public String getScope() {
+        return credential.getScope();
+    }
+
+    @Override
+    public String getTokenType() {
+        return credential.getTokenType();
+    }
+
+    @Override
+    public OpenIdClientSecret getClientCredential() {
+        return credential.getClientCredential();
+    }
+
+    @Override
+    public String getOpenidProvider() {
+        return credential.getOpenidProvider();
+    }
+}

modules/dcache-vehicles/pom.xml
@@ -39,6 +39,10 @@
           <version>3.0.0</version>
           <scope>compile</scope>
       </dependency>
+      <dependency>
+          <groupId>org.apache.httpcomponents</groupId>
+          <artifactId>httpclient</artifactId>
+      </dependency>
   </dependencies>
 
   <build>

commit 05026a1511549da6271318f986488f4049d7773d

pool: fix regression in HTTP third-party transfer with redirection
Motivation:

Unlike GET, the Apache HTTP client library does not follow redirection
on PUT; this logic is present within the pool.

Unfortunately, commit d132d860 introduced a regression where, after
receiving a redirection response, the pool will retry with the same URL
(i.e., does not follow the redirection).  This is repeated until the
loop-detection prevents any further attempts and fails the transfer.

Modification:

Use the updated location when the upload is redirected.

Result:

Third-party transfers work again when uploading to services that
redirect-on-PUT (such as dCache, if so configured).

Target: master
Request: 3.1
Require-notes: yes
Require-book: no
Patch: https://rb.dcache.org/r/10387/
Acked-by: Tigran Mkrtchyan

modules/dcache/src/main/java/org/dcache/pool/movers/RemoteHttpDataTransferProtocol.java
@@ -332,7 +332,7 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
         URI location = info.getUri();
 
         for (int attempt = 0; attempt < MAX_REDIRECTIONS; attempt++) {
-            HttpPut put = buildPutRequest(info, length);
+            HttpPut put = buildPutRequest(info, location, length);
 
             try (CloseableHttpResponse response = _client.execute(put)) {
                 StatusLine status = response.getStatusLine();
@@ -382,9 +382,10 @@ public class RemoteHttpDataTransferProtocol implements MoverProtocol,
                 "number of redirections; last location was " + location);
     }
 
-    private HttpPut buildPutRequest(RemoteHttpDataTransferProtocolInfo info, long length)
+    private HttpPut buildPutRequest(RemoteHttpDataTransferProtocolInfo info,
+            URI location, long length)
     {
-        HttpPut put = new HttpPut(info.getUri());
+        HttpPut put = new HttpPut(location);
         put.setConfig(RequestConfig.custom()
                                   .setConnectTimeout(CONNECTION_TIMEOUT)
                                   .setExpectContinueEnabled(true)