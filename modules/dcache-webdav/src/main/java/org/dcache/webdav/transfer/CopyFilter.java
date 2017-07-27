dCache/dcache

commit d132d86052507b3f459c72c8b6788ba5deb5d638

webdav, dcache: Support for third party copy on the Webdav door using OpenId Credentials
Webdav supports requests with Bearer Token based on OpenId Credentials.
However, third-party-copy did not support OpenId Credentials. With this
commit, such a request can be accepted by the Webdav door. The door performs
a token-exchange with OpenId Provider to obtain a delegated token for
itself, which it can use (and refresh) to perform third-party-copy.

Three properties need to be setup, in order to use this feature
- webdav.oidc.client.ids : client ids of the regisered clients to OpenId
  provider for bearer token delegation via token exchange
- webdav.oidc.client.secrets : client secrets corresponding to client
  ids registered with the OpenId providers.

More info on OpenId Connect/OAuth2 token exchange here,
https://tools.ietf.org/html/draft-ietf-oauth-token-exchange-04#section-2

Ticket:
Acked-by: Paul Millar <paul.millar@desy.de>
Target: master
Require-book: no
Require-notes: no
Patch: https://rb.dcache.org/r/9599/

modules/dcache-webdav/src/main/java/org/dcache/webdav/transfer/CopyFilter.java
@@ -17,8 +17,11 @@
  */
 package org.dcache.webdav.transfer;
 
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Joiner;
-import eu.emi.security.authn.x509.X509Credential;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.net.InternetDomainName;
 import io.milton.http.Filter;
 import io.milton.http.FilterChain;
 import io.milton.http.Request;
@@ -30,6 +33,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 
+import javax.annotation.PostConstruct;
 import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
 
@@ -39,9 +43,11 @@ import java.security.AccessController;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import diskCacheV111.util.CacheException;
@@ -51,19 +57,23 @@ import diskCacheV111.util.PermissionDeniedCacheException;
 import diskCacheV111.util.PnfsHandler;
 
 import org.dcache.acl.enums.AccessMask;
+import org.dcache.auth.BearerTokenCredential;
+import org.dcache.auth.OpenIdClientSecret;
 import org.dcache.auth.Subjects;
 import org.dcache.auth.attributes.Restriction;
 import org.dcache.cells.CellStub;
 import org.dcache.namespace.FileAttribute;
 import org.dcache.namespace.FileType;
 import org.dcache.vehicles.FileAttributes;
+import org.dcache.webdav.AuthenticationHandler;
 import org.dcache.webdav.PathMapper;
 import org.dcache.webdav.transfer.RemoteTransferHandler.Direction;
-import org.dcache.webdav.AuthenticationHandler;
 import org.dcache.webdav.transfer.RemoteTransferHandler.TransferType;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.concurrent.TimeUnit.MINUTES;
-import static org.dcache.namespace.FileAttribute.*;
+import static org.dcache.namespace.FileAttribute.PNFSID;
+import static org.dcache.namespace.FileAttribute.TYPE;
 
 /**
  * The CopyFilter adds support for initiating third-party copies via
@@ -100,6 +110,9 @@ public class CopyFilter implements Filter
 
     private static final String QUERY_KEY_ASKED_TO_DELEGATE = "asked-to-delegate";
     private static final String REQUEST_HEADER_CREDENTIAL = "Credential";
+    private static final String REQUEST_HEADER_AUTHORIZATION = "Authorization";
+    private static final String SCHEME_AUTHORIZATION_BEARER_WITH_SPACE = "BEARER ";
+
     private static final Set<AccessMask> READ_ACCESS_MASK =
             EnumSet.of(AccessMask.READ_DATA);
     private static final Set<AccessMask> WRITE_ACCESS_MASK =
@@ -107,12 +120,19 @@ public class CopyFilter implements Filter
     private static final Set<AccessMask> CREATE_ACCESS_MASK =
             EnumSet.of(AccessMask.ADD_FILE);
 
+    private ImmutableMap<String,String> _clientIds;
+    private ImmutableMap<String,String> _clientSecrets;
+    private ImmutableMap<String, OpenIdClientSecret> _oidcClientCredentials;
+
     /**
      * Describes where to fetch the delegated credential, if at all.
      */
     public enum CredentialSource {
         /* Get it from an SRM's GridSite credential store */
         GRIDSITE("gridsite"),
+
+        /* OpenID Connect Bearer Token */
+        OIDC("oidc"),
 
         /* Don't get a credential */
         NONE("none");
@@ -130,6 +150,7 @@ public class CopyFilter implements Filter
 
         public static CredentialSource forHeaderValue(String value)
         {
+            _log.debug("Source for Header {}", SOURCE_FOR_HEADER.get(value));
             return SOURCE_FOR_HEADER.get(value);
         }
 
@@ -176,6 +197,50 @@ public class CopyFilter implements Filter
     public void setRemoteTransferHandler(RemoteTransferHandler handler)
     {
         _remoteTransfers = handler;
+    }
+
+    public void setOidClientIds(ImmutableMap<String,String> clientIds)
+    {
+        checkArgument(clientIds.entrySet()
+                               .stream()
+                               .allMatch(e -> CharMatcher.ASCII.matchesAllOf(e.getValue())),
+                     "Client Ids must be ASCII Characters only");
+        _clientIds = clientIds;
+    }
+
+    public void setOidClientSecrets(ImmutableMap<String,String> clientSecrets)
+    {
+        checkArgument(clientSecrets.entrySet()
+                                   .stream()
+                                   .allMatch(e -> CharMatcher.ASCII.matchesAllOf(e.getValue())),
+                     "Client Secrets must be ASCII Characters only");
+        _clientSecrets = clientSecrets;
+    }
+
+    @PostConstruct
+    public void validateOidcClientParameters()
+    {
+        checkArgument(_clientIds.keySet().equals(_clientSecrets.keySet()),
+                        "Client Ids and Client Secrets must have the same set of hosts");
+
+        Map<Boolean,Set<String>> validatedProviders = _clientIds.keySet()
+                                                                .stream()
+                                                                .collect(
+                                                                        Collectors.groupingBy(InternetDomainName::isValid,
+                                                                                Collectors.toSet()));
+        checkArgument(!validatedProviders.containsKey(Boolean.FALSE),
+                String.format("Client credentials for invalid hostnames provided: %s",
+                        Joiner.on(", ")
+                                .join(validatedProviders.getOrDefault(Boolean.FALSE, new HashSet<>()))));
+
+        Set<String> hostnames = validatedProviders.get(Boolean.TRUE);
+        ImmutableMap.Builder<String, OpenIdClientSecret> builder = ImmutableMap.builder();
+        hostnames.stream()
+                 .forEach((host) -> builder.put(host, new OpenIdClientSecret(_clientIds.get(host),
+                                                                             _clientSecrets.get(host)))
+                 );
+
+        _oidcClientCredentials = builder.build();
     }
 
     @Override
@@ -274,9 +339,15 @@ public class CopyFilter implements Filter
         checkPath(path, direction);
 
         CredentialSource source = getCredentialSource(request, type);
-        X509Credential credential = fetchCredential(source);
-        if (credential == null && source != CredentialSource.NONE) {
-            redirectWithDelegation(response);
+        Object credential = fetchCredential(source);
+        if (credential == null) {
+            if (source == CredentialSource.GRIDSITE) {
+                redirectWithDelegation(response);
+            } else {
+                _log.error("Client failed OpenId Connect Token Exchange for COPY");
+                response.sendError(Status.SC_INTERNAL_SERVER_ERROR,
+                                   "client failed to perform OpenId Connect Token Delegation");
+            }
         } else {
             _remoteTransfers.acceptRequest(response.getOutputStream(),
                     request.getHeaders(), getSubject(), getRestriction(), path,
@@ -288,10 +359,15 @@ public class CopyFilter implements Filter
             throws ErrorResponseException
     {
         String headerValue = request.getHeaders().get(REQUEST_HEADER_CREDENTIAL);
+        CredentialSource source;
 
-        CredentialSource source = headerValue != null ?
-                CredentialSource.forHeaderValue(headerValue) :
-                type.getDefaultCredentialSource();
+        if (headerValue != null) {
+            source = CredentialSource.forHeaderValue(headerValue);
+        } else if (isAuthorizationHeaderBearer(request)) {
+            source = CredentialSource.OIDC;
+        } else {
+            source = type.getDefaultCredentialSource();
+        }
 
         if (source == null) {
             throw new ErrorResponseException(Status.SC_BAD_REQUEST,
@@ -309,12 +385,12 @@ public class CopyFilter implements Filter
         return source;
     }
 
-    private X509Credential fetchCredential(CredentialSource source)
+    private Object fetchCredential(CredentialSource source)
             throws InterruptedException, ErrorResponseException
     {
+        Subject subject = Subject.getSubject(AccessController.getContext());
         switch (source) {
         case GRIDSITE:
-            Subject subject = Subject.getSubject(AccessController.getContext());
             String dn = Subjects.getDn(subject);
             if (dn == null) {
                 throw new ErrorResponseException(Response.Status.SC_UNAUTHORIZED,
@@ -325,6 +401,18 @@ public class CopyFilter implements Filter
                     dn, Objects.toString(Subjects.getPrimaryFqan(subject), null),
                     20, MINUTES);
 
+        case OIDC:
+            BearerTokenCredential bearer = subject.getPrivateCredentials()
+                                                  .stream()
+                                                  .filter(BearerTokenCredential.class::isInstance)
+                                                  .map(BearerTokenCredential.class::cast)
+                                                  .findFirst()
+                                                  .orElseThrow(() ->
+                                                      new ErrorResponseException(Status.SC_UNAUTHORIZED,
+                                                              "User must authenticate with OpenID for " +
+                                                                      "OpenID delegation"));
+
+            return _credentialService.getDelegatedCredential(bearer.getToken(), _oidcClientCredentials);
         case NONE:
             return null;
 
@@ -490,5 +578,17 @@ public class CopyFilter implements Filter
     {
         HttpServletRequest servletRequest = ServletRequest.getRequest();
         return (Restriction) servletRequest.getAttribute(AuthenticationHandler.DCACHE_RESTRICTION_ATTRIBUTE);
+    }
+
+    private boolean isAuthorizationHeaderBearer(Request request)
+    {
+        return Strings.nullToEmpty(request.getHeaders()
+                                          .get(REQUEST_HEADER_AUTHORIZATION))
+                      .toUpperCase()
+                      .startsWith(SCHEME_AUTHORIZATION_BEARER_WITH_SPACE);
+    }
+
+    private static <T> Predicate<T> not(Predicate<T> t) {
+        return t.negate();
     }
 }

modules/dcache-webdav/src/main/java/org/dcache/webdav/transfer/RemoteTransferHandler.java
@@ -60,10 +60,13 @@ import diskCacheV111.vehicles.transferManager.TransferStatusQueryMessage;
 import dmg.cells.nucleus.CellMessageReceiver;
 import dmg.cells.nucleus.NoRouteToCellException;
 
+import org.dcache.auth.OpenIdCredential;
+import org.dcache.auth.StaticOpenIdCredential;
 import org.dcache.auth.attributes.Restriction;
 import org.dcache.cells.CellStub;
 import org.dcache.webdav.transfer.CopyFilter.CredentialSource;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.dcache.util.ByteUnit.MiB;
 import static org.dcache.webdav.transfer.CopyFilter.CredentialSource.*;
@@ -124,8 +127,8 @@ public class RemoteTransferHandler implements CellMessageReceiver
      */
     public enum TransferType {
         GSIFTP("gsiftp", 2811, GRIDSITE, EnumSet.noneOf(CredentialSource.class)),
-        HTTP(    "http",   80,     NONE, EnumSet.noneOf(CredentialSource.class)),
-        HTTPS(  "https",  443, GRIDSITE, EnumSet.of(NONE));
+        HTTP(  "http",   80,     NONE,   EnumSet.noneOf(CredentialSource.class)),
+        HTTPS( "https",  443,  GRIDSITE, EnumSet.of(OIDC));
 
         private static final ImmutableMap<String,TransferType> BY_SCHEME =
             ImmutableMap.of("gsiftp", GSIFTP, "http", HTTP, "https", HTTPS);
@@ -225,9 +228,11 @@ public class RemoteTransferHandler implements CellMessageReceiver
 
     public void acceptRequest(OutputStream out, Map<String,String> requestHeaders,
             Subject subject, Restriction restriction, FsPath path, URI remote,
-            X509Credential credential, Direction direction)
+            Object credential, Direction direction)
             throws ErrorResponseException, InterruptedException
     {
+        checkArgument(credential instanceof X509Credential || credential instanceof OpenIdCredential,
+                                            "Credential not supported for Third-Party Transfer");
         EnumSet<TransferFlag> flags = EnumSet.noneOf(TransferFlag.class);
         flags = addVerificationFlag(flags, requestHeaders);
         ImmutableMap<String,String> transferHeaders = buildTransferHeaders(requestHeaders);
@@ -338,6 +343,9 @@ public class RemoteTransferHandler implements CellMessageReceiver
         private final PrivateKey _privateKey;
         @Nullable
         private final X509Certificate[] _certificateChain;
+        @Nullable
+        private final OpenIdCredential _oidCredential;
+        private final CredentialSource _source;
         private final PrintWriter _out;
         private final EnumSet<TransferFlag> _flags;
         private final ImmutableMap<String,String> _transferHeaders;
@@ -349,7 +357,7 @@ public class RemoteTransferHandler implements CellMessageReceiver
         private boolean _finished;
 
         public RemoteTransfer(OutputStream out, Subject subject, Restriction restriction,
-                FsPath path, URI destination, @Nullable X509Credential credential,
+                FsPath path, URI destination, @Nullable Object credential,
                 EnumSet<TransferFlag> flags, ImmutableMap<String,String> transferHeaders,
                 Direction direction)
                 throws ErrorResponseException
@@ -359,12 +367,21 @@ public class RemoteTransferHandler implements CellMessageReceiver
             _path = path;
             _destination = destination;
             _type = TransferType.fromScheme(destination.getScheme());
-            if (credential != null) {
-                _privateKey = credential.getKey();
-                _certificateChain = credential.getCertificateChain();
+            if (credential instanceof X509Credential) {
+                _privateKey = ((X509Credential)credential).getKey();
+                _certificateChain = ((X509Credential)credential).getCertificateChain();
+                _source = CredentialSource.GRIDSITE;
+                _oidCredential = null;
+            } else if (credential instanceof OpenIdCredential) {
+                _privateKey = null;
+                _certificateChain = null;
+                _source = CredentialSource.OIDC;
+                _oidCredential = (OpenIdCredential) credential;
             } else {
                 _privateKey = null;
                 _certificateChain = null;
+                _source = null;
+                _oidCredential = null;
             }
             _out = new PrintWriter(out);
             _flags = flags;
@@ -381,7 +398,6 @@ public class RemoteTransferHandler implements CellMessageReceiver
 
             message.setSubject(_subject);
             message.setRestriction(_restriction);
-
             try {
                 _id = _transferManager.sendAndWait(message).getId();
                 return _id;
@@ -446,10 +462,17 @@ public class RemoteTransferHandler implements CellMessageReceiver
                         _transferHeaders);
 
             case HTTPS:
-                return new RemoteHttpsDataTransferProtocolInfo("RemoteHttpsDataTransfer",
-                        1, 1, address, _destination.toASCIIString(),
-                        _flags.contains(TransferFlag.REQUIRE_VERIFICATION),
-                        _transferHeaders, _privateKey, _certificateChain);
+                if (_source == CredentialSource.OIDC) {
+                    return new RemoteHttpsDataTransferProtocolInfo("RemoteHttpsDataTransfer",
+                            1, 1, address, _destination.toASCIIString(),
+                            _flags.contains(TransferFlag.REQUIRE_VERIFICATION),
+                            _transferHeaders, _oidCredential);
+                } else {
+                    return new RemoteHttpsDataTransferProtocolInfo("RemoteHttpsDataTransfer",
+                            1, 1, address, _destination.toASCIIString(),
+                            _flags.contains(TransferFlag.REQUIRE_VERIFICATION),
+                            _transferHeaders, _privateKey, _certificateChain);
+                }
             }
 
             throw new RuntimeException("Unexpected TransferType: " + _type);

modules/common/pom.xml
@@ -35,6 +35,14 @@
         <groupId>eu.eu-emi.security</groupId>
         <artifactId>canl</artifactId>
     </dependency>
+    <dependency>
+      <groupId>org.json</groupId>
+      <artifactId>json</artifactId>
+    </dependency>
+      <dependency>
+          <groupId>org.apache.httpcomponents</groupId>
+          <artifactId>httpclient</artifactId>
+      </dependency>
   </dependencies>
 
     <build>

modules/common/src/main/java/org/dcache/auth/OpenIdClientSecret.java
@@ -1,0 +1,24 @@
+package org.dcache.auth;
+
+import java.io.Serializable;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class OpenIdClientSecret implements Serializable
+{
+    private final String id;
+    private final String secret;
+
+    public OpenIdClientSecret(String id, String secret) {
+        this.id = checkNotNull(id);
+        this.secret = checkNotNull(secret);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+}

modules/common/src/main/java/org/dcache/auth/OpenIdCredential.java
@@ -1,0 +1,44 @@
+package org.dcache.auth;
+
+/**
+ *  An OpenId Connect credential is composed of the following information.
+ *
+ *  {
+ *      "access_token": "eyJraWQMCJ9.N_eoiDrnPTv46ZNj",
+ *      "token_type": "Bearer",
+ *      "refresh_token": "eyJhbGciO.",
+ *      "expires_in": 3599,
+ *      "scope": "openid offline_access profile email",
+ *      "id_token": "Json Web Token"
+ *  }
+ *
+ *  http://openid.net/specs/openid-connect-core-1_0.html
+ *
+ *  This interface exposes the methods necessary to obtain these information from a parsed OpenId Connect
+ *  Credential.
+ *
+ *  The implementation of this interface is expected to hold information about the OpenId Connect credential
+ *  as well as the information necessary to perform token exchange via delegaton. It is also suggested to
+ *  hide the access token refresh within the implementation.
+ *
+ */
+public interface OpenIdCredential
+{
+    String getBearerToken();
+
+    String getAccessToken();
+
+    long getExpiresAt();
+
+    String getIssuedTokenType();
+
+    String getRefreshToken();
+
+    String getScope();
+
+    String getTokenType();
+
+    OpenIdClientSecret getClientCredential();
+
+    String getOpenidProvider();
+}

modules/common/src/main/java/org/dcache/auth/OpenIdCredentialRefreshable.java
@@ -1,0 +1,101 @@
+package org.dcache.auth;
+
+import com.google.common.base.Charsets;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class OpenIdCredentialRefreshable extends WrappingOpenIdCredential
+{
+    private static final Logger LOG =
+            LoggerFactory.getLogger(OpenIdCredentialRefreshable.class);
+    private final HttpClient client;
+
+    public OpenIdCredentialRefreshable(OpenIdCredential credential, HttpClient client) {
+        super(checkNotNull(credential, "OpenId Credential can't be null"));
+        this.client = checkNotNull(client, "Http Client can't be null");
+    }
+
+    @Override
+    public String getBearerToken()
+    {
+        if (timeToRefresh()) {
+            try {
+                refreshOpenIdCredentials();
+            } catch (IOException | AuthenticationException e) {
+                LOG.warn("Error Refreshing OpenId Bearer Token with {}: {}",
+                        credential.getOpenidProvider(), e.getMessage());
+            }
+        }
+        return credential.getBearerToken();
+    }
+
+    private synchronized void refreshOpenIdCredentials() throws IOException, AuthenticationException
+    {
+        HttpPost post = new HttpPost(credential.getOpenidProvider());
+        BasicScheme scheme = new BasicScheme(Charsets.UTF_8);
+        UsernamePasswordCredentials clientCreds = new UsernamePasswordCredentials(
+                credential.getClientCredential().getId(),
+                credential.getClientCredential().getSecret());
+
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("client_id", credential.getClientCredential().getId()));
+        params.add(new BasicNameValuePair("client_secret", credential.getClientCredential().getSecret()));
+        params.add(new BasicNameValuePair("grant_type", "refresh_token"));
+        params.add(new BasicNameValuePair("refresh_token", credential.getRefreshToken()));
+        params.add(new BasicNameValuePair("scope", credential.getScope()));
+        post.setEntity(new UrlEncodedFormEntity(params));
+        post.addHeader(scheme.authenticate(clientCreds, post, new BasicHttpContext()) );
+
+        HttpResponse response = client.execute(post);
+        if (response.getStatusLine().getStatusCode() == 200) {
+            updateCredential(parseResponseToJson(response));
+        } else {
+            throw new IOException(String.format("Error Refreshing OpenId Bearer Token [%s]: %s",
+                                                    response.getStatusLine().getStatusCode(),
+                                                    credential.getOpenidProvider()));
+        }
+    }
+
+    private boolean timeToRefresh() {
+        return (credential.getExpiresAt() - System.currentTimeMillis()) < 60*1000L;
+    }
+
+    private JSONObject parseResponseToJson(HttpResponse response) throws IOException
+    {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        response.getEntity().writeTo(os);
+        return new JSONObject(new String(os.toByteArray(), Charsets.UTF_8));
+    }
+
+    private void updateCredential(JSONObject json) throws IOException
+    {
+        try {
+            this.credential = StaticOpenIdCredential.copyOf(credential)
+                                                    .accessToken(json.getString("access_token"))
+                                                    .expiry(json.getLong("expires_in"))
+                                                    .build();
+        } catch (JSONException je) {
+            throw new IOException("Error Parsing response of OpenId Bearer Token Refresh: " + je.getMessage());
+        }
+    }
+}

commit 56bc72919e959d55b9095a915e8a00ca8dcbe62e

webdav: fix more regressions with CredentialSource.NONE
Motivation:

A previous patch (committed as 7e5e81e1) attempted to fix the regression
introduced by commit d132d860 where the webdav service no longer
accepted the "Credential: none" header for HTTPS-based third-party
transfers.  Unfortunately that fix was incomplete and additional changes
proved necessary, which this patch provides.

Modification:

Relax conditions that prevented HTTPS-based third-party transfers
without delegation.

Result:

A regression is fixed that prevented third-party transfers without a
delegated credential (e.g., using HTTP basic authentication or a bearer
token like macaroons).

Target: master
Request: 3.1
Require-notes: yes
Require-book: no
Patch: https://rb.dcache.org/r/10386/
Acked-by: Tigran Mkrtchyan

modules/dcache-webdav/src/main/java/org/dcache/webdav/transfer/CopyFilter.java
@@ -353,7 +353,7 @@ public class CopyFilter implements Filter
 
         CredentialSource source = getCredentialSource(request, type);
         Object credential = fetchCredential(source);
-        if (credential == null) {
+        if (source != CredentialSource.NONE && credential == null) {
             if (source == CredentialSource.GRIDSITE) {
                 redirectWithDelegation(response);
             } else {

modules/dcache-webdav/src/main/java/org/dcache/webdav/transfer/RemoteTransferHandler.java
@@ -225,8 +225,10 @@ public class RemoteTransferHandler implements CellMessageReceiver
             Object credential, Direction direction)
             throws ErrorResponseException, InterruptedException
     {
-        checkArgument(credential instanceof X509Credential || credential instanceof OpenIdCredential,
-                                            "Credential not supported for Third-Party Transfer");
+        checkArgument(credential == null
+                || credential instanceof X509Credential
+                || credential instanceof OpenIdCredential,
+                "Credential not supported for Third-Party Transfer");
         EnumSet<TransferFlag> flags = EnumSet.noneOf(TransferFlag.class);
         flags = addVerificationFlag(flags, requestHeaders);
         ImmutableMap<String,String> transferHeaders = buildTransferHeaders(requestHeaders);