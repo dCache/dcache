dCache/dcache

commit 7e25b6b8f4aa997a4959cc3db681f31b8ff1f50d

pool: update abstractMover to use ChecksumChannel on write
there is no reason not to use ChecksumChannel. If it's impossible
to calculate checksum, channel will return null on getChecksum()
anyway.

Update NetyyMover to remove redundant code.
Update Nfs transfer service to provide required functionality
Update MoverProtocolMover to enable checksum channel.
The expected checksum will be taken from the mover, the
actual one  - from the channel.

A clenaup of dcap/ftp movers required to remove redundant code
related to checksum calculation.

Acked-by: Karsten Schwank
Target: master
Require-book: no
Require-notes: yes

modules/dcache/src/main/java/org/dcache/pool/movers/MoverProtocolMover.java
@@ -29,6 +29,7 @@ import dmg.cells.nucleus.CellPath;
 
 import org.dcache.pool.classic.TransferService;
 import org.dcache.pool.repository.ReplicaDescriptor;
+import org.dcache.pool.classic.ChecksumModule;
 import org.dcache.util.Checksum;
 
 /**
@@ -42,9 +43,10 @@ public class MoverProtocolMover extends AbstractMover<ProtocolInfo, MoverProtoco
     protected final MoverProtocol _moverProtocol;
 
     public MoverProtocolMover(ReplicaDescriptor handle, PoolIoFileMessage message, CellPath pathToDoor,
-                              TransferService<MoverProtocolMover> transferService, MoverProtocol moverProtocol)
+                    TransferService<MoverProtocolMover> transferService,
+                    MoverProtocol moverProtocol, ChecksumModule checksumModule)
     {
-        super(handle, message, pathToDoor, transferService);
+        super(handle, message, pathToDoor, transferService, checksumModule);
         _moverProtocol = moverProtocol;
     }
 
@@ -64,16 +66,6 @@ public class MoverProtocolMover extends AbstractMover<ProtocolInfo, MoverProtoco
     public long getLastTransferred()
     {
         return _moverProtocol.getLastTransferred();
-    }
-
-    @Override
-    public Set<Checksum> getActualChecksums()
-    {
-        if (_moverProtocol instanceof ChecksumMover) {
-            return Optional.fromNullable(((ChecksumMover) _moverProtocol).getActualChecksum()).asSet();
-        } else {
-            return Collections.emptySet();
-        }
     }
 
     @Override

modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/mover/NfsMover.java
@@ -1,6 +1,6 @@
 /* dCache - http://www.dcache.org/
  *
- * Copyright (C) 2013 Deutsches Elektronen-Synchrotron
+ * Copyright (C) 2013 - 2015 Deutsches Elektronen-Synchrotron
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -20,16 +20,10 @@ package org.dcache.chimera.nfsv41.mover;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Throwables;
-
 import java.io.IOException;
 import java.nio.channels.CompletionHandler;
-import java.util.Collections;
-import java.util.Set;
 
 import diskCacheV111.util.CacheException;
-import diskCacheV111.util.ChecksumFactory;
 import diskCacheV111.util.DiskErrorCacheException;
 import diskCacheV111.util.PnfsHandler;
 import diskCacheV111.vehicles.PoolIoFileMessage;
@@ -41,17 +35,11 @@ import org.dcache.nfs.v4.NFSv41Session;
 import org.dcache.nfs.v4.xdr.stateid4;
 import org.dcache.nfs.v4.xdr.verifier4;
 import org.dcache.pool.classic.Cancellable;
-import org.dcache.pool.movers.ChecksumChannel;
-import org.dcache.pool.movers.IoMode;
-
+import org.dcache.pool.classic.ChecksumModule;
 import org.dcache.pool.movers.MoverChannel;
 import org.dcache.pool.movers.MoverChannelMover;
 import org.dcache.pool.repository.ReplicaDescriptor;
-import org.dcache.pool.repository.RepositoryChannel;
-import org.dcache.util.Checksum;
 import org.dcache.vehicles.FileAttributes;
-
-import static com.google.common.base.Preconditions.checkState;
 
 public class NfsMover extends MoverChannelMover<NFS4ProtocolInfo, NfsMover> {
 
@@ -63,53 +51,14 @@ public class NfsMover extends MoverChannelMover<NFS4ProtocolInfo, NfsMover> {
     private volatile CompletionHandler<Void, Void> _completionHandler;
     private final verifier4 _bootVerifier;
 
-    private final ChecksumFactory _checksumFactory;
-    private ChecksumChannel _checksumChannel;
-
     public NfsMover(ReplicaDescriptor handle, PoolIoFileMessage message, CellPath pathToDoor,
-            NfsTransferService nfsTransferService, PnfsHandler pnfsHandler,
-            ChecksumFactory checksumFactory) {
-        super(handle, message, pathToDoor, nfsTransferService, MoverChannel.AllocatorMode.SOFT);
+            NfsTransferService nfsTransferService, PnfsHandler pnfsHandler, ChecksumModule checksumModule) {
+        super(handle, message, pathToDoor, nfsTransferService, MoverChannel.AllocatorMode.SOFT, checksumModule);
         _nfsIO = nfsTransferService.getNfsMoverHandler();
         org.dcache.chimera.nfs.v4.xdr.stateid4 legacyStateid =  getProtocolInfo().stateId();
         _state = new MoverState(new stateid4(legacyStateid.other, legacyStateid.seqid.value));
         _namespace = pnfsHandler;
         _bootVerifier = nfsTransferService.getBootVerifier();
-        _checksumFactory = checksumFactory;
-    }
-
-    @Override
-    public Set<Checksum> getActualChecksums() {
-        return (_checksumChannel == null)
-                ? Collections.<Checksum>emptySet()
-                : Optional.fromNullable(_checksumChannel.getChecksum()).asSet();
-    }
-
-    @Override
-    public Set<Checksum> getExpectedChecksums() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public synchronized RepositoryChannel openChannel() throws DiskErrorCacheException {
-        checkState(_checksumChannel == null);
-        RepositoryChannel channel = super.openChannel();
-        try {
-            if (getIoMode() == IoMode.WRITE && _checksumFactory != null) {
-                channel = _checksumChannel = new ChecksumChannel(channel, _checksumFactory);
-            }
-        } catch (Throwable t) {
-            /* This should only happen in case of JVM Errors or if the checksum digest cannot be
-             * instantiated (which, barring bugs, should never happen).
-             */
-            try {
-                channel.close();
-            } catch (IOException e) {
-                t.addSuppressed(e);
-            }
-            Throwables.propagate(t);
-        }
-        return channel;
     }
 
     public stateid4 getStateId() {

modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/mover/NfsTransferService.java
@@ -1,6 +1,5 @@
 package org.dcache.chimera.nfsv41.mover;
 
-import com.google.common.base.Function;
 import com.google.common.io.Files;
 import org.ietf.jgss.GSSException;
 import org.slf4j.Logger;
@@ -17,10 +16,8 @@ import java.net.InetSocketAddress;
 import java.net.SocketException;
 import java.nio.channels.CompletionHandler;
 import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
 
 import diskCacheV111.util.CacheException;
-import diskCacheV111.util.ChecksumFactory;
 import diskCacheV111.util.DiskErrorCacheException;
 import diskCacheV111.util.PnfsHandler;
 import diskCacheV111.vehicles.PoolIoFileMessage;
@@ -193,17 +190,7 @@ public class NfsTransferService extends AbstractCellComponent
     @Override
     public Mover<?> createMover(ReplicaDescriptor handle, PoolIoFileMessage message, CellPath pathToDoor) throws CacheException
     {
-        ChecksumFactory checksumFactory;
-        if (_checksumModule.hasPolicy(ChecksumModule.PolicyFlag.ON_TRANSFER)) {
-            try {
-                checksumFactory = _checksumModule.getPreferredChecksumFactory(handle);
-            } catch (NoSuchAlgorithmException e) {
-                throw new CacheException("Failed to instantiate NFS mover due to unsupported checksum type: " + e.getMessage(), e);
-            }
-        } else {
-            checksumFactory = null;
-        }
-        return new NfsMover(handle, message, pathToDoor, this, _pnfsHandler, checksumFactory);
+        return new NfsMover(handle, message, pathToDoor, this, _pnfsHandler, _checksumModule);
     }
 
     @Override

modules/dcache/src/main/java/org/dcache/pool/classic/MoverProtocolTransferService.java
@@ -33,7 +33,6 @@ import dmg.util.command.Command;
 import org.dcache.pool.FaultAction;
 import org.dcache.pool.FaultEvent;
 import org.dcache.pool.FaultListener;
-import org.dcache.pool.movers.ChecksumMover;
 import org.dcache.pool.movers.Mover;
 import org.dcache.pool.movers.MoverFactory;
 import org.dcache.pool.movers.MoverProtocol;
@@ -91,7 +90,8 @@ public class MoverProtocolTransferService extends AbstractCellComponent
         ProtocolInfo info = message.getProtocolInfo();
         try {
             MoverProtocol moverProtocol = createMoverProtocol(getMoverProtocolClass(info));
-            return new MoverProtocolMover(handle, message, pathToDoor, this, moverProtocol);
+            return new MoverProtocolMover(handle, message, pathToDoor, this,
+                    moverProtocol, _checksumModule);
         } catch (InvocationTargetException e) {
             throw new CacheException(27, "Could not create mover for " + info, e.getTargetException());
         } catch (ClassNotFoundException e) {
@@ -173,12 +173,6 @@ public class MoverProtocolTransferService extends AbstractCellComponent
                     switch (_mover.getIoMode()) {
                     case WRITE:
                         try {
-                            MoverProtocol moverProtocol = _mover.getMover();
-                            if (_checksumModule.hasPolicy(ChecksumModule.PolicyFlag.ON_TRANSFER)
-                                    && moverProtocol instanceof ChecksumMover) {
-                                ((ChecksumMover) moverProtocol).enableTransferChecksum(
-                                        _checksumModule.getPreferredChecksumFactory(_mover.getIoHandle()).getType());
-                            }
                             runMover(fileIoChannel);
                         } finally {
                             try {

modules/dcache/src/main/java/org/dcache/pool/movers/AbstractMover.java
@@ -17,21 +17,25 @@
  */
 package org.dcache.pool.movers;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
 import com.google.common.reflect.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.security.auth.Subject;
 
-import java.io.FileNotFoundException;
 import java.io.InterruptedIOException;
 import java.io.IOException;
 import java.nio.channels.CompletionHandler;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.Set;
 
 import diskCacheV111.util.CacheException;
 import diskCacheV111.util.DiskErrorCacheException;
-import diskCacheV111.util.FsPath;
+import diskCacheV111.util.ChecksumFactory;
 import diskCacheV111.vehicles.PoolAcceptFileMessage;
 import diskCacheV111.vehicles.PoolIoFileMessage;
 import diskCacheV111.vehicles.ProtocolInfo;
@@ -39,10 +43,12 @@ import diskCacheV111.vehicles.ProtocolInfo;
 import dmg.cells.nucleus.CellPath;
 
 import org.dcache.pool.classic.Cancellable;
+import org.dcache.pool.classic.ChecksumModule;
 import org.dcache.pool.classic.TransferService;
 import org.dcache.pool.repository.FileRepositoryChannel;
 import org.dcache.pool.repository.ReplicaDescriptor;
 import org.dcache.pool.repository.RepositoryChannel;
+import org.dcache.util.Checksum;
 import org.dcache.util.TryCatchTemplate;
 import org.dcache.vehicles.FileAttributes;
 
@@ -69,9 +75,12 @@ public abstract class AbstractMover<P extends ProtocolInfo, M extends AbstractMo
     protected final String _transferPath;
     protected volatile int _errorCode;
     protected volatile String _errorMessage = "";
+    private final ChecksumFactory _checksumFactory;
+    private volatile ChecksumChannel _checksumChannel;
 
     public AbstractMover(ReplicaDescriptor handle, PoolIoFileMessage message, CellPath pathToDoor,
-                         TransferService<M> transferService)
+                         TransferService<M> transferService,
+                         ChecksumModule checksumModule)
     {
         TypeToken<M> type = new TypeToken<M>(getClass()) {};
         checkArgument(type.isAssignableFrom(getClass()));
@@ -88,6 +97,7 @@ public abstract class AbstractMover<P extends ProtocolInfo, M extends AbstractMo
         _pathToDoor = pathToDoor;
         _handle = handle;
         _transferService = transferService;
+        _checksumFactory = getChecksumFactoryFor(checksumModule, handle);
     }
 
     @Override
@@ -240,16 +250,42 @@ public abstract class AbstractMover<P extends ProtocolInfo, M extends AbstractMo
      * @return An open RepositoryChannel to the replica of this mover
      * @throws DiskErrorCacheException If the file could not be opened
      */
-    public RepositoryChannel openChannel() throws DiskErrorCacheException
-    {
+    public RepositoryChannel openChannel() throws DiskErrorCacheException {
         RepositoryChannel channel;
         try {
             channel = new FileRepositoryChannel(_handle.getFile(), getIoMode().toOpenString());
+            if (getIoMode() == IoMode.WRITE) {
+                try {
+                    channel = _checksumChannel = new ChecksumChannel(channel, _checksumFactory);
+                } catch (Throwable t) {
+                    /* This should only happen in case of JVM Errors or if the checksum digest cannot be
+                     * instantiated (which, barring bugs, should never happen).
+                     */
+                    try {
+                        channel.close();
+                    } catch (IOException e) {
+                        t.addSuppressed(e);
+                    }
+                    Throwables.propagate(t);
+                }
+            }
         } catch (IOException e) {
             throw new DiskErrorCacheException(
                     "File could not be opened; please check the file system: " + e.getMessage(), e);
         }
         return channel;
+    }
+
+    @Override
+    public Set<Checksum> getActualChecksums() {
+        return (_checksumChannel == null)
+                ? Collections.<Checksum>emptySet()
+                : Optional.fromNullable(_checksumChannel.getChecksum()).asSet();
+    }
+
+    @Override
+    public Set<Checksum> getExpectedChecksums() {
+        return Collections.emptySet();
     }
 
     @Override
@@ -267,6 +303,18 @@ public abstract class AbstractMover<P extends ProtocolInfo, M extends AbstractMo
             sb.append((System.currentTimeMillis() - lastTransferTime) / 1000L);
         }
         return sb.toString();
+    }
+
+    private static ChecksumFactory getChecksumFactoryFor(ChecksumModule checksumModule, ReplicaDescriptor handle)
+    {
+        if (checksumModule.hasPolicy(ChecksumModule.PolicyFlag.ON_TRANSFER)) {
+            try {
+                return checksumModule.getPreferredChecksumFactory(handle);
+            } catch (NoSuchAlgorithmException | CacheException e) {
+                LOGGER.error("Failed to instantiate mover due to unsupported checksum type: " + e.getMessage(), e);
+            }
+        }
+        return null;
     }
 
     protected abstract String getStatus();

modules/dcache/src/main/java/org/dcache/pool/movers/ChecksumChannel.java
@@ -102,7 +102,10 @@ public class ChecksumChannel implements RepositoryChannel
     {
         _channel = inner;
         _checksumFactory = checksumFactory;
-        _digest = _checksumFactory.create();
+        _digest = _checksumFactory == null ? null : _checksumFactory.create();
+        if (_digest == null) {
+            _isChecksumViable = false;
+        }
     }
 
     @Override

modules/dcache/src/main/java/org/dcache/pool/movers/MoverChannelMover.java
@@ -1,6 +1,6 @@
 /* dCache - http://www.dcache.org/
  *
- * Copyright (C) 2013 Deutsches Elektronen-Synchrotron
+ * Copyright (C) 2013 - 2015 Deutsches Elektronen-Synchrotron
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -25,6 +25,7 @@ import diskCacheV111.vehicles.ProtocolInfo;
 
 import dmg.cells.nucleus.CellPath;
 
+import org.dcache.pool.classic.ChecksumModule;
 import org.dcache.pool.classic.TransferService;
 import org.dcache.pool.repository.ReplicaDescriptor;
 
@@ -41,9 +42,10 @@ public abstract class MoverChannelMover<P extends ProtocolInfo, M extends MoverC
     public MoverChannelMover(ReplicaDescriptor handle, PoolIoFileMessage message,
                              CellPath pathToDoor,
                              TransferService<M> transferService,
-                             MoverChannel.AllocatorMode allocatorMode)
+                             MoverChannel.AllocatorMode allocatorMode,
+                             ChecksumModule checksumModule)
     {
-        super(handle, message, pathToDoor, transferService);
+        super(handle, message, pathToDoor, transferService, checksumModule);
         _allocatorMode = allocatorMode;
     }
 

modules/dcache/src/main/java/org/dcache/pool/movers/NettyMover.java
@@ -17,88 +17,38 @@
  */
 package org.dcache.pool.movers;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Throwables;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Set;
 import java.util.UUID;
 
-import diskCacheV111.util.ChecksumFactory;
-import diskCacheV111.util.DiskErrorCacheException;
 import diskCacheV111.vehicles.PoolIoFileMessage;
 import diskCacheV111.vehicles.ProtocolInfo;
 
 import dmg.cells.nucleus.CellPath;
 
+import org.dcache.pool.classic.ChecksumModule;
 import org.dcache.pool.classic.TransferService;
 import org.dcache.pool.repository.ReplicaDescriptor;
-import org.dcache.pool.repository.RepositoryChannel;
-import org.dcache.util.Checksum;
-
-import static com.google.common.base.Preconditions.checkState;
 
 /**
  * A mover that served by AbstractNettyTransferServices.
  */
 public class NettyMover<P extends ProtocolInfo> extends MoverChannelMover<P, NettyMover<P>>
 {
-    private final ChecksumFactory checksumFactory;
     private final UUID uuid;
-    private ChecksumChannel checksumChannel;
+
 
     public NettyMover(ReplicaDescriptor handle,
                       PoolIoFileMessage message,
                       CellPath pathToDoor,
                       TransferService<NettyMover<P>> transferService,
-                      ChecksumFactory checksumFactory,
-                      UUID uuid)
+                      UUID uuid,
+                      ChecksumModule checksumModule)
     {
-        super(handle, message, pathToDoor, transferService, MoverChannel.AllocatorMode.HARD);
-        this.checksumFactory = checksumFactory;
+        super(handle, message, pathToDoor, transferService, MoverChannel.AllocatorMode.HARD, checksumModule);
         this.uuid = uuid;
     }
 
     public UUID getUuid()
     {
         return uuid;
-    }
-
-    @Override
-    public Set<Checksum> getActualChecksums()
-    {
-        return (checksumChannel == null)
-               ? Collections.<Checksum>emptySet()
-               : Optional.fromNullable(checksumChannel.getChecksum()).asSet();
-    }
-
-    @Override
-    public Set<Checksum> getExpectedChecksums()
-    {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public synchronized RepositoryChannel openChannel() throws DiskErrorCacheException
-    {
-        checkState(checksumChannel == null);
-        RepositoryChannel channel = super.openChannel();
-        try {
-            if (getIoMode() == IoMode.WRITE && checksumFactory != null) {
-                channel = checksumChannel = new ChecksumChannel(channel, checksumFactory);
-            }
-        } catch (Throwable t) {
-            /* This should only happen in case of JVM Errors or if the checksum digest cannot be
-             * instantiated (which, barring bugs, should never happen).
-             */
-            try {
-                channel.close();
-            } catch (IOException e) {
-                t.addSuppressed(e);
-            }
-            Throwables.propagate(t);
-        }
-        return channel;
     }
 }

commit 47157c37fc4f11d00949c35191472145c8fd413a

pool: fix regression in GridFTP OPTS CKSM command
Motivation:

The 'OPTS CKSM' command allows the client to request that the GridFTP
server calculates a specific type of checksum for subsequent uploads.

Additionally, the Globus transfer service requires MD5 checksum values.
Although it does not make this requirement explicit, the door identifies
the client and acts as if the client issued an 'OPTS CKSM MD5' command.

Commit 7e25b6b8f introduced a regression where, although the desired
checksum was calculated, the value was subsequently discarded and not
available to the client.

Modification:

Update pool to include checksums calculated by the mover.

Result:

The GridFTP 'OPTS CKSM' command is supported again.

Target: master
Request: 3.2
Request: 3.1
Request: 3.0
Request: 2.16
Require-notes: yes
Require-book: no
Patch: https://rb.dcache.org/r/10461
Acked-by: Albert Rossi

modules/dcache/src/main/java/org/dcache/pool/movers/MoverProtocolMover.java
@@ -22,6 +22,7 @@ import com.google.common.base.Optional;
 import javax.annotation.Nonnull;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import diskCacheV111.vehicles.PoolIoFileMessage;
@@ -68,6 +69,18 @@ public class MoverProtocolMover extends AbstractMover<ProtocolInfo, MoverProtoco
     public long getLastTransferred()
     {
         return _moverProtocol.getLastTransferred();
+    }
+
+    @Override
+    public Set<Checksum> getActualChecksums() {
+        if (_moverProtocol instanceof ChecksumMover) {
+            Set<Checksum> checksums = new HashSet<>();
+            checksums.addAll(super.getActualChecksums());
+            checksums.addAll(((ChecksumMover)_moverProtocol).getActualChecksums());
+            return checksums;
+        } else {
+            return super.getActualChecksums();
+        }
     }
 
     @Nonnull