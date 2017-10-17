4adcade245cc1ce0e5b0d0761ffdaaf66e7df0e3
Paul Millar <paul.millar@desy.de>
2017-10-17 16:00:45 +0200

    frontend: improve consistency in ObjectMapper
    Motivation:
    
    The ObjectMapper used in dCache frontend is in a somewhat confused
    state.  Before embarking on invasive changes, this patch tries to tidy
    up the current status.
    
    Modification:
    
    1. Remove @Produces annotations
    
       this class doesn't produce any output
    
    2. Get rid of explicit constructor -- it isn't needed
    
    3. Drop JAXB annotations -- we don't generate XML
    
    4. Rename combinedObjectMapper --> listObjectMapper since
       This no longer includes multiple annotation support.
    
    5. Remove the redundant serialization inclusion NON_NULL.
    
    6. Use a single style for generating an ObjectMapper.
    
    Result:
    
    No user or admin facing changes, but it should provide an easier
    starting point for future changes.
    
    Target: master
    Require-notes: no
    Require-book: no
    Patch: https://rb.dcache.org/r/10549/
    Acked-by: Marina Sahakyan

--- modules/dcache-frontend/src/main/java/org/dcache/restful/providers/ObjectMapperProvider.java
+++ modules/dcache-frontend/src/main/java/org/dcache/restful/providers/ObjectMapperProvider.java
@@ -1,72 +1,43 @@
 package org.dcache.restful.providers;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.AnnotationIntrospector;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
 
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
 /**
  * Fine-tuning how JSON we be presented by redefine the default Jackson behaviour.
  **/
-
-
 @Provider
-@Produces(MediaType.APPLICATION_JSON)
 public class ObjectMapperProvider implements ContextResolver<ObjectMapper>
 {
-    private final ObjectMapper defaultObjectMapper;
-    private final ObjectMapper combinedObjectMapper;
+    private final ObjectMapper defaultObjectMapper = createDefaultMapper();
+    private final ObjectMapper listObjectMapper = createListObjectMapper();
 
-    public ObjectMapperProvider()
-    {
-        defaultObjectMapper = createDefaultMapper();
-        combinedObjectMapper = createCombinedObjectMapper();
-    }
-
-    private static ObjectMapper createCombinedObjectMapper()
+    private static ObjectMapper createListObjectMapper()
     {
         return new ObjectMapper()
-                .configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false)
-                .configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-
-                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                .setSerializationInclusion(JsonInclude.Include.NON_DEFAULT)
-
-                .setAnnotationIntrospector(createJaxbJacksonAnnotationIntrospector());
+                .disable(SerializationFeature.WRITE_NULL_MAP_VALUES)
+                .enable(DeserializationFeature.UNWRAP_ROOT_VALUE)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
     }
 
     private static ObjectMapper createDefaultMapper()
     {
-        final ObjectMapper result = new ObjectMapper();
-        result.enable(SerializationFeature.INDENT_OUTPUT);
-        result.enable(SerializationFeature.WRITE_NULL_MAP_VALUES);
-        result.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        return result;
-    }
-
-    private static AnnotationIntrospector createJaxbJacksonAnnotationIntrospector()
-    {
-        final AnnotationIntrospector jaxbIntrospector = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
-        final AnnotationIntrospector jacksonIntrospector = new JacksonAnnotationIntrospector();
-
-        return AnnotationIntrospector.pair(jacksonIntrospector, jaxbIntrospector);
+        return new ObjectMapper()
+                .enable(SerializationFeature.INDENT_OUTPUT)
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
     @Override
     public ObjectMapper getContext(Class<?> type)
     {
         if (type == java.util.ArrayList.class) {
-            return combinedObjectMapper;
+            return listObjectMapper;
         } else {
             return defaultObjectMapper;
         }
