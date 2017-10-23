dCache/dcache

commit d1187b74ad2e3a73a46368c0a7b62c9195814bc1

frontend: use Spring to inject resource dependencies
Motivation:

Better handling of dependencies by using existing Spring framework.

Modification:

Note that jersey-spring integration supports both annotation and
XML-based injection.  For annotation based injection, Spring supporting
both its own annotation and a (useful) subset of JSR-330 annotations.

All the resource classes have been updated so they have their
dependencies (that were previously obtained via servlet attributes)
injected by Spring.

This patch adopts JSR-330 annotations rather than the XML-based approach
used elsewhere in dCache.  This approach requires less boilerplate
coding as there is no need to add setter methods or corresponding XML
description.  Field-members that are to be injected are annotated with
'@Inject'.

If there is a single bean of this type then this is sufficient;
otherwise, the '@Named' annotation is used to avoid any ambiguity.  This
attribute's value is the desired bean's 'id' attribute.

Result:

Coding is simpler and easier to maintain.

Target: master
Require-notes: no
Require-book: no
Patch: https://rb.dcache.org/r/10547/
Acked-by: Marina Sahakyan

modules/dcache-frontend/src/main/java/org/dcache/restful/resources/pool/PoolInfoResources.java
@@ -65,7 +65,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletContext;
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
@@ -94,13 +95,13 @@ import diskCacheV111.vehicles.PoolMoverKillMessage;
 import dmg.cells.nucleus.CellPath;
 import dmg.cells.nucleus.NoRouteToCellException;
 
+import org.dcache.cells.CellStub;
 import org.dcache.restful.providers.pool.PoolGroupInfo;
 import org.dcache.restful.providers.pool.PoolInfo;
 import org.dcache.restful.providers.pool.PoolKillMover;
 import org.dcache.restful.providers.pool.PoolModeUpdate;
 import org.dcache.restful.services.pool.PoolInfoService;
 import org.dcache.restful.util.HttpServletRequests;
-import org.dcache.restful.util.ServletContextHandlerAttributes;
 
 import static org.dcache.restful.providers.SuccessfulResponse.successfulResponse;
 
@@ -113,10 +114,14 @@ import static org.dcache.restful.providers.SuccessfulResponse.successfulResponse
 @Path("/pools")
 public final class PoolInfoResources {
     @Context
-    ServletContext ctx;
+    private HttpServletRequest request;
 
-    @Context
-    HttpServletRequest request;
+    @Inject
+    private PoolInfoService service;
+
+    @Inject
+    @Named("pool-stub")
+    private CellStub poolStub;
 
     /**
      * @return a list of pool group names
@@ -129,8 +134,7 @@ public final class PoolInfoResources {
         /*
          *  Allow pools and pool groups to be listed without privileges.
          */
-        return ServletContextHandlerAttributes.getPoolInfoService(ctx)
-                                              .listGroups();
+        return service.listPools();
     }
 
     /**
@@ -160,9 +164,6 @@ public final class PoolInfoResources {
         /*
          *  Allow access to aggregated data to be seen without privileges
          */
-        PoolInfoService service
-                        = ServletContextHandlerAttributes.getPoolInfoService(
-                        ctx);
         PoolGroupInfo info = new PoolGroupInfo();
 
         /*
@@ -246,9 +247,6 @@ public final class PoolInfoResources {
                     @DefaultValue("false")
                     @QueryParam("remove") boolean isRemove)
                     throws CacheException {
-        PoolInfoService service
-                        = ServletContextHandlerAttributes.getPoolInfoService(
-                        ctx);
         PoolInfo info = new PoolInfo();
 
         /*
@@ -338,12 +336,10 @@ public final class PoolInfoResources {
          *  Allow pools and pool groups to be listed without privileges.
          */
         if (group == null) {
-            return ServletContextHandlerAttributes.getPoolInfoService(ctx)
-                                                  .listPools();
+            return service.listPools();
         }
 
-        return ServletContextHandlerAttributes.getPoolInfoService(ctx)
-                                              .listPools(group);
+        return service.listPools(group);
     }
 
     /**
@@ -371,9 +367,7 @@ public final class PoolInfoResources {
             if (moverId != null) {
                 message = new PoolMoverKillMessage(pool, moverId,
                                                    update.getReason());
-                ServletContextHandlerAttributes.getPoolStub(ctx)
-                                               .sendAndWait(new CellPath(pool),
-                                                            message);
+                poolStub.sendAndWait(new CellPath(pool), message);
             }
         } catch (JSONException | IllegalArgumentException | JsonParseException
                         | JsonMappingException e) {
@@ -409,9 +403,7 @@ public final class PoolInfoResources {
             PoolV2Mode mode = new PoolV2Mode(update.mode());
             mode.setResilienceEnabled(update.isResilience());
             Message message = new PoolModifyModeMessage(pool, mode);
-            ServletContextHandlerAttributes.getPoolStub(ctx)
-                                           .sendAndWait(new CellPath(pool),
-                                                        message);
+            poolStub.sendAndWait(new CellPath(pool), message);
         } catch (JSONException | IllegalArgumentException | JsonParseException
                         | JsonMappingException e) {
             throw new BadRequestException(e);

modules/dcache-frontend/src/main/java/org/dcache/restful/qos/QosManagementNamespace.java
@@ -4,9 +4,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletContext;
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.NotAuthorizedException;
@@ -17,8 +20,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.ForbiddenException;
-import javax.ws.rs.BadRequestException;
 
 import java.net.InetSocketAddress;
 import java.net.URI;
@@ -26,17 +27,12 @@ import java.net.URISyntaxException;
 import java.util.EnumSet;
 import java.util.Set;
 
-import org.dcache.restful.util.ServletContextHandlerAttributes;
-
 import diskCacheV111.util.CacheException;
 import diskCacheV111.util.FileLocality;
 import diskCacheV111.util.FileNotFoundCacheException;
 import diskCacheV111.util.FsPath;
 import diskCacheV111.util.PermissionDeniedCacheException;
 import diskCacheV111.util.PnfsHandler;
-
-import org.dcache.poolmanager.RemotePoolMonitor;
-
 import diskCacheV111.vehicles.HttpProtocolInfo;
 
 import dmg.cells.nucleus.NoRouteToCellException;
@@ -46,6 +42,7 @@ import org.dcache.namespace.FileAttribute;
 import org.dcache.pinmanager.PinManagerCountPinsMessage;
 import org.dcache.pinmanager.PinManagerPinMessage;
 import org.dcache.pinmanager.PinManagerUnpinMessage;
+import org.dcache.poolmanager.PoolMonitor;
 import org.dcache.restful.util.HandlerBuilders;
 import org.dcache.restful.util.RequestUser;
 import org.dcache.vehicles.FileAttributes;
@@ -59,10 +56,18 @@ import org.dcache.vehicles.FileAttributes;
 public class QosManagementNamespace {
 
     @Context
-    ServletContext ctx;
+    private HttpServletRequest request;
 
-    @Context
-    HttpServletRequest request;
+    @Inject
+    private PoolMonitor poolMonitor;
+
+    @Inject
+    @Named("pinManagerStub")
+    private CellStub pinmanager;
+
+    @Inject
+    @Named("pnfs-stub")
+    private CellStub pnfsmanager;
 
     /** ID provided by the requestor eg. the SRM door or QoS.**/
 
@@ -90,8 +95,7 @@ public class QosManagementNamespace {
             FileAttributes fileAttributes = getFileAttributes(requestPath);
             FileLocality fileLocality =  getLocality(fileAttributes);
 
-            CellStub cellStub = ServletContextHandlerAttributes.getPinManager(ctx);
-            boolean isPinned = isPinned(fileAttributes, cellStub);
+            boolean isPinned = isPinned(fileAttributes, pinmanager);
 
 
             switch (fileLocality) {
@@ -164,17 +168,16 @@ public class QosManagementNamespace {
 
 
             FileAttributes fileAttributes = getFileAttributes(requestPath);
-            CellStub cellStub = ServletContextHandlerAttributes.getPinManager(ctx);
             FileLocality fileLocality =  getLocality(fileAttributes);
 
             switch (update) {
                 // change QoS to "disk+tape"
                 case QosManagement.DISK_TAPE:
-                    makeDiskAndTape( fileLocality, fileAttributes, cellStub);
+                    makeDiskAndTape(fileLocality, fileAttributes, pinmanager);
                     break;
                 // change QoS to "tape"
                 case QosManagement.TAPE:
-                    makeTape( fileLocality, fileAttributes, cellStub);
+                    makeTape(fileLocality, fileAttributes, pinmanager);
                     break;
                 default:
                     // error cases
@@ -203,7 +206,7 @@ public class QosManagementNamespace {
 
     public FileAttributes getFileAttributes(String requestPath) throws CacheException {
 
-        PnfsHandler handler = HandlerBuilders.roleAwarePnfsHandler(ctx, request);
+        PnfsHandler handler = HandlerBuilders.roleAwarePnfsHandler(pnfsmanager, request);
         FsPath path;
         if (requestPath == null || requestPath.isEmpty()) {
             path = FsPath.ROOT;
@@ -238,8 +241,7 @@ public class QosManagementNamespace {
 
     public FileLocality getLocality(FileAttributes fileAttributes) {
 
-        RemotePoolMonitor remotePoolMonitor = ServletContextHandlerAttributes.getRemotePoolMonitor(ctx);
-        return remotePoolMonitor.getFileLocality(fileAttributes, request.getRemoteHost());
+        return poolMonitor.getFileLocality(fileAttributes, request.getRemoteHost());
     }
 
 

modules/dcache-frontend/src/main/java/org/dcache/restful/resources/alarms/AlarmsResources.java
@@ -63,7 +63,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletContext;
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
@@ -89,7 +89,6 @@ import diskCacheV111.util.CacheException;
 import org.dcache.alarms.LogEntry;
 import org.dcache.restful.services.alarms.AlarmsInfoService;
 import org.dcache.restful.util.HttpServletRequests;
-import org.dcache.restful.util.ServletContextHandlerAttributes;
 
 import static org.dcache.restful.providers.SuccessfulResponse.successfulResponse;
 
@@ -102,10 +101,10 @@ import static org.dcache.restful.providers.SuccessfulResponse.successfulResponse
 @Path("/alarms")
 public final class AlarmsResources {
     @Context
-    ServletContext ctx;
+    private HttpServletRequest request;
 
-    @Context
-    HttpServletRequest request;
+    @Inject
+    private AlarmsInfoService service;
 
     /**
      * <p>Alarms.</p>
@@ -134,9 +133,7 @@ public final class AlarmsResources {
         }
 
         try {
-            return ServletContextHandlerAttributes.getAlarmsInfoService(ctx)
-                                                  .get(offset, limit,
-                                                       after, before, type);
+            return service.get(offset, limit, after, before, type);
         } catch (IllegalArgumentException | CacheException e) {
             throw new BadRequestException(e);
         } catch (Exception e) {
@@ -158,8 +155,7 @@ public final class AlarmsResources {
                             "Alarm service only accessible to admin users.");
         }
 
-        return ServletContextHandlerAttributes.getAlarmsInfoService(ctx)
-                                              .getMap();
+        return service.getMap();
     }
 
     /**
@@ -198,9 +194,6 @@ public final class AlarmsResources {
         try {
             LogEntry[] entries = new ObjectMapper().readValue(requestPayload,
                                                               LogEntry[].class);
-            AlarmsInfoService service =
-                            ServletContextHandlerAttributes.getAlarmsInfoService(ctx);
-
             List<LogEntry> list = Arrays.stream(entries)
                                         .collect(Collectors.toList());
 

modules/dcache-frontend/src/main/java/org/dcache/restful/resources/billing/BillingResources.java
@@ -61,7 +61,7 @@ package org.dcache.restful.resources.billing;
 
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletContext;
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.ForbiddenException;
@@ -86,7 +86,6 @@ import org.dcache.restful.providers.billing.BillingDataGridEntry;
 import org.dcache.restful.providers.billing.BillingRecords;
 import org.dcache.restful.services.billing.BillingInfoService;
 import org.dcache.restful.util.HttpServletRequests;
-import org.dcache.restful.util.ServletContextHandlerAttributes;
 import org.dcache.util.histograms.Histogram;
 
 /**
@@ -96,10 +95,10 @@ import org.dcache.util.histograms.Histogram;
 @Path("/billing")
 public class BillingResources {
     @Context
-    ServletContext ctx;
+    private HttpServletRequest request;
 
-    @Context
-    HttpServletRequest request;
+    @Inject
+    private BillingInfoService service;
 
     /**
      * <p>Request records for a given file.</p>
@@ -120,10 +119,7 @@ public class BillingResources {
                                                              + "to admin users.");
             }
 
-            return ServletContextHandlerAttributes.getBillingInfoService(ctx)
-                                                  .getRecords(pnfsid,
-                                                              before,
-                                                              after);
+            return service.getRecords(pnfsid, before, after);
         } catch (FileNotFoundCacheException e) {
             throw new NotFoundException(e);
         } catch (CacheException e) {
@@ -154,8 +150,7 @@ public class BillingResources {
          *  No admin privileges necessary for billing histogram data.
          */
         try {
-            return ServletContextHandlerAttributes.getBillingInfoService(ctx)
-                                                  .getHistogram(key);
+            return service.getHistogram(key);
         } catch (CacheException e) {
             throw new InternalServerErrorException(e);
         } catch (IllegalArgumentException e) {
@@ -174,8 +169,7 @@ public class BillingResources {
          *  No admin privileges necessary for billing histogram data.
          */
         try {
-            return ServletContextHandlerAttributes.getBillingInfoService(ctx)
-                                                  .getGrid();
+            return service.getGrid();
         } catch (CacheException e) {
             throw new InternalServerErrorException(e);
         }
@@ -191,8 +185,6 @@ public class BillingResources {
         List<Histogram> gridData = new ArrayList<>();
 
         try {
-            BillingInfoService service
-                            = ServletContextHandlerAttributes.getBillingInfoService(ctx);
             service.getGrid()
                    .getDataGrid()
                    .keySet()

modules/dcache-frontend/src/main/java/org/dcache/restful/resources/cells/CellInfoResources.java
@@ -61,7 +61,7 @@ package org.dcache.restful.resources.cells;
 
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletContext;
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
@@ -76,10 +76,9 @@ import java.util.stream.Collectors;
 
 import diskCacheV111.util.CacheException;
 
+import org.dcache.cells.json.CellData;
 import org.dcache.restful.services.cells.CellInfoService;
 import org.dcache.restful.util.HttpServletRequests;
-import org.dcache.restful.util.ServletContextHandlerAttributes;
-import org.dcache.cells.json.CellData;
 
 /**
  * <p>RESTful API to the {@link CellInfoService} service.</p>
@@ -89,11 +88,12 @@ import org.dcache.cells.json.CellData;
 @Component
 @Path("/cells")
 public final class CellInfoResources {
-    @Context
-    ServletContext ctx;
 
     @Context
-    HttpServletRequest request;
+    private HttpServletRequest request;
+
+    @Inject
+    private CellInfoService service;
 
     /**
      * <p>Get a list of current addresses for well-known cells.</p>
@@ -109,8 +109,7 @@ public final class CellInfoResources {
             throw new ForbiddenException(
                             "Cell info service only accessible to admin users.");
         }
-        return ServletContextHandlerAttributes.getCellInfoService(ctx)
-                                              .getAddresses();
+        return service.getAddresses();
     }
 
     /**
@@ -129,8 +128,7 @@ public final class CellInfoResources {
             throw new ForbiddenException(
                             "Cell info service only accessible to admin users.");
         }
-        return ServletContextHandlerAttributes.getCellInfoService(ctx)
-                                              .getCellData(address);
+        return service.getCellData(address);
     }
 
     /**
@@ -146,9 +144,6 @@ public final class CellInfoResources {
             throw new ForbiddenException(
                             "Cell info service only accessible to admin users.");
         }
-
-        CellInfoService service
-                        = ServletContextHandlerAttributes.getCellInfoService(ctx);
 
         return Arrays.stream(service.getAddresses())
                      .map(service::getCellData)

modules/dcache-frontend/src/main/java/org/dcache/restful/resources/namespace/FileResources.java
@@ -7,7 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletContext;
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
@@ -53,14 +54,12 @@ import org.dcache.pinmanager.PinManagerCountPinsMessage;
 import org.dcache.pinmanager.PinManagerPinMessage;
 import org.dcache.pinmanager.PinManagerUnpinMessage;
 import org.dcache.poolmanager.PoolMonitor;
-import org.dcache.poolmanager.RemotePoolMonitor;
 import org.dcache.restful.policyengine.MigrationPolicyEngine;
 import org.dcache.restful.providers.JsonFileAttributes;
 import org.dcache.restful.qos.QosManagement;
 import org.dcache.restful.util.HandlerBuilders;
 import org.dcache.restful.util.HttpServletRequests;
 import org.dcache.restful.util.RequestUser;
-import org.dcache.restful.util.ServletContextHandlerAttributes;
 import org.dcache.restful.util.namespace.NamespaceUtils;
 import org.dcache.util.list.DirectoryEntry;
 import org.dcache.util.list.DirectoryStream;
@@ -83,16 +82,33 @@ public class FileResources {
     private final String QOS_PIN_REQUEST_ID = "qos";
     private static final Logger LOG = LoggerFactory.getLogger(FileResources.class);
 
-
-    @Context
-    ServletContext ctx;
-
     /*
     This "request" parameter is used to get fully qualified name of the client
      * or the last proxy that sent the request. Later used for quering locality of the file.
      */
     @Context
-    HttpServletRequest request;
+    private HttpServletRequest request;
+
+    @Inject
+    private PoolMonitor poolMonitor;
+
+    @Inject
+    private PathMapper pathMapper;
+
+    @Inject
+    private ListDirectoryHandler listDirectoryHandler;
+
+    @Inject
+    @Named("pool-manager-stub")
+    private CellStub poolmanager;
+
+    @Inject
+    @Named("pinManagerStub")
+    private CellStub pinmanager;
+
+    @Inject
+    @Named("pnfs-stub")
+    private CellStub pnfsmanager;
 
     /**
      * The method offer to list the content of a directory or return metadata of
@@ -160,8 +176,7 @@ public class FileResources {
     {
         JsonFileAttributes fileAttributes = new JsonFileAttributes();
         Set<FileAttribute> attributes = EnumSet.allOf(FileAttribute.class);
-        PnfsHandler handler = HandlerBuilders.roleAwarePnfsHandler(ctx, request);
-        PathMapper pathMapper = ServletContextHandlerAttributes.getPathMapper(ctx);
+        PnfsHandler handler = HandlerBuilders.roleAwarePnfsHandler(pnfsmanager, request);
         FsPath path = pathMapper.asDcachePath(request, requestPath, ForbiddenException::new);
         try {
 
@@ -170,11 +185,11 @@ public class FileResources {
                                                    namespaceAttributes,
                                                    isLocality, isLocations,
                                                    false,
-                                                   request, ctx);
+                                                   request, poolMonitor);
             if (isQos) {
                 NamespaceUtils.addQoSAttributes(fileAttributes,
                                                 namespaceAttributes,
-                                                request, ctx);
+                                                request, poolMonitor, pinmanager);
             }
 
             // fill children list id it's a directory and listing is requested
@@ -194,8 +209,6 @@ public class FileResources {
 
                 List<JsonFileAttributes> children = new ArrayList<>();
 
-                ListDirectoryHandler listDirectoryHandler = ServletContextHandlerAttributes.getListDirectoryHandler(ctx);
-
                 DirectoryStream stream = listDirectoryHandler.list(
                         HttpServletRequests.roleAwareSubject(request),
                         HttpServletRequests.roleAwareRestriction(request),
@@ -214,12 +227,12 @@ public class FileResources {
                                                            entry.getFileAttributes(),
                                                            isLocality, isLocations,
                                                            false,
-                                                           request, ctx);
+                                                           request, poolMonitor);
                     childrenAttributes.setFileName(fName);
                     if (isQos) {
                         NamespaceUtils.addQoSAttributes(childrenAttributes,
                                                         entry.getFileAttributes(),
-                                                        request, ctx);
+                                                        request, poolMonitor, pinmanager);
                     }
                     children.add(childrenAttributes);
                 }
@@ -250,8 +263,7 @@ public class FileResources {
         try {
             JSONObject reqPayload = new JSONObject(requestPayload);
             String action = (String) reqPayload.get("action");
-            PnfsHandler handler = HandlerBuilders.roleAwarePnfsHandler(ctx, request);
-            PathMapper pathMapper = ServletContextHandlerAttributes.getPathMapper(ctx);
+            PnfsHandler handler = HandlerBuilders.roleAwarePnfsHandler(pnfsmanager, request);
             FsPath path = pathMapper.asDcachePath(request, requestPath, ForbiddenException::new);
 
             // FIXME: which attributes do we actually need?
@@ -261,7 +273,7 @@ public class FileResources {
                 case "mkdir":
                     String name = (String) reqPayload.get("name");
                     FsPath.checkChildName(name, BadRequestException::new);
-                    handler = HandlerBuilders.pnfsHandler(ctx, request); // FIXME: non-role identity to ensure correct ownership
+                    handler = HandlerBuilders.pnfsHandler(pnfsmanager, request); // FIXME: non-role identity to ensure correct ownership
                     handler.createPnfsDirectory(path.child(name).toString());
                     break;
 
@@ -274,10 +286,9 @@ public class FileResources {
                 case "qos":
                     String targetQos = reqPayload.getString("target");
                     JsonFileAttributes fileAttributes = new JsonFileAttributes();
-                    NamespaceUtils.addQoSAttributes(fileAttributes, attributes, request, ctx);
+                    NamespaceUtils.addQoSAttributes(fileAttributes, attributes, request, poolMonitor, pinmanager);
                     LOG.debug("Request received to change current QoS from {} to: {} for {}", fileAttributes.currentQos, targetQos, path);
-                    RemotePoolMonitor monitor = ServletContextHandlerAttributes.getRemotePoolMonitor(ctx);
-                    FileLocality locality = monitor.getFileLocality(attributes, request.getRemoteHost());
+                    FileLocality locality = poolMonitor.getFileLocality(attributes, request.getRemoteHost());
                     LOG.debug("The Locality of the file: {}", locality);
                     if (locality == FileLocality.NONE) {
                         throw new BadRequestException("Transition for directories not supported");
@@ -288,12 +299,8 @@ public class FileResources {
                             null, null, null,
                             URI.create("http://"+request.getRemoteHost()+"/"));
 
-                    CellStub cellStub = ServletContextHandlerAttributes.getPoolManger(ctx);
-                    CellStub pinmanager = ServletContextHandlerAttributes.getPinManager(ctx);
-
-                    PoolMonitor poolMonitor = ServletContextHandlerAttributes.getRemotePoolMonitor(ctx);
                     MigrationPolicyEngine migrationPolicyEngine =
-                            new MigrationPolicyEngine(attributes, cellStub, poolMonitor);
+                            new MigrationPolicyEngine(attributes, poolmanager, poolMonitor);
 
                     switch (targetQos) {
                     case QosManagement.DISK_TAPE:
@@ -351,8 +358,7 @@ public class FileResources {
     @Produces(MediaType.APPLICATION_JSON)
     public Response deleteFileEntry(@PathParam("value") String requestPath) throws CacheException {
 
-        PnfsHandler handler = HandlerBuilders.roleAwarePnfsHandler(ctx, request);
-        PathMapper pathMapper = ServletContextHandlerAttributes.getPathMapper(ctx);
+        PnfsHandler handler = HandlerBuilders.roleAwarePnfsHandler(pnfsmanager, request);
         FsPath path = pathMapper.asDcachePath(request, requestPath, ForbiddenException::new);
 
         try {

commit 66d1bac7faf16b5affdde2c36890bbbba410982c

dache-frontend: fix regression in pool info api
Motivation:

regression introduced by d1187b74ad2e3a73a46368c0a7b62c9195814bc1

Modification:

restore original getGroups()

Result:

Return to expected behavior.

Target: master
Require-notes: no
Require-book: no
Acked-by: Paul
Acked-by: Tigran

modules/dcache-frontend/src/main/java/org/dcache/restful/resources/pool/PoolInfoResources.java
@@ -134,7 +134,7 @@ public final class PoolInfoResources {
         /*
          *  Allow pools and pool groups to be listed without privileges.
          */
-        return service.listPools();
+        return service.listGroups();
     }
 
     /**