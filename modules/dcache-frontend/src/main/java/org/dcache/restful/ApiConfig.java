d101fc6483b24d64a20f83eb710883bf828b3688
Paul Millar <paul.millar@desy.de>
2017-11-22 17:39:24 +0100

    frontend: add global swagger annotations
    Motivation:
    
    Swagger will auto-generate documentation based on the JAX-RS annotation;
    however, that auto-generated documentation describes only basic
    structure.  Although judicious choices of option names goes a long way
    in describing the API, additional documentation is still needed before
    the documenation becomes genuinely useful.
    
    Modification:
    
    Add global definitions; these are independent of any API calls.
    
    Result:
    
    Improvided quality of REST API documentation.
    
    Target: master
    Require-notes: yes
    Require-book: yes
    Patch: https://rb.dcache.org/r/10704/
    Acked-by: Marina Sahakyan

---
+++ modules/dcache-frontend/src/main/java/org/dcache/restful/ApiConfig.java
@@ -0,0 +1,70 @@
+/*
+ * dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2017 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.restful;
+
+import io.swagger.annotations.BasicAuthDefinition;
+import io.swagger.annotations.Contact;
+import io.swagger.annotations.ExternalDocs;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.License;
+import io.swagger.annotations.SecurityDefinition;
+import io.swagger.annotations.SwaggerDefinition;
+
+import javax.ws.rs.ext.Provider;
+
+/**
+ * This interface exists only to give @SwaggerDefinition something to annotate
+ * that swagger will subsequently find.  See:
+ *
+ *     https://github.com/swagger-api/swagger-core/issues/2600
+ */
+@SwaggerDefinition(
+        info = @Info(
+            title = "The dCache REST interface",
+            description = "User and administration interaction with dCache",
+            version = "v1.0.1",
+            contact = @Contact(
+               name = "dCache support team",
+               email = "support@dCache.org"
+            ),
+            license = @License(
+                name = "Apache 2.0",
+                url = "http://www.apache.org/licenses/LICENSE-2.0"
+            )
+        ),
+        securityDefinition = @SecurityDefinition(
+                basicAuthDefinitions = {
+                    @BasicAuthDefinition(key = "basicAuth",
+                            description = "Username and password authentication "
+                                    + "with optional role assertion.  To assert "
+                                    + "roles, append '#' to the username followed "
+                                    + "by a comma-separated list of roles; e.g., "
+                                    + "a username of \"paul#admin\" is user "
+                                    + "\"paul\" asserting the \"admin\" role.")
+                }
+        ),
+        consumes = {"application/json"},
+        produces = {"application/json"},
+        externalDocs = @ExternalDocs(value = "Wiki", url = "https://github.com/dCache/dcache/wiki/Restful-API")
+)
+@Provider
+public interface ApiConfig
+{
+
+}
