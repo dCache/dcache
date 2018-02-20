b243c5d3faabb98c762723b44d8367ef359d798e
Albert Louis Rossi <arossi@fnal.gov>
2018-02-20 07:32:49 -0600

    dcache-frontend: change all JSON representations of pnfsid to String type
    Motivation:
    
    JSON representation of the pnfsid object requires a getter
    to display its value.   The PnfsId object (rather than its
    string representation) in fact appears in a number of
    JSON objects.   These objects are in turn accessed by
    the frontend client for their 'id' value.  Currently,
    with the changes made in #10735, that value is empty.
    
    Modification:
    
    Use custom PnfsId serializer on frontend which
    merely renders it as string.
    
    Result:
    
         { pnfsid: 0000650478D8F4DD4E6BB4A3801F7A7C965B }
    
    rather than:
    
         { pnfsid: { id: }}
    
    For all pnfsid attributes exposed through the RESTful API.
    
    Note:  all necessary adjustments to the dcache-view admin
    components have been made.
    
    Backward compatibility with pools and other services
    is maintained, but dcache-view admin is coupled to
    this version of the frontend API.
    
    Target: master
    Require-notes: no
    Require-book: no
    Acked-by: Paul

--- modules/dcache-frontend/src/main/java/org/dcache/restful/providers/ObjectMapperProvider.java
+++ modules/dcache-frontend/src/main/java/org/dcache/restful/providers/ObjectMapperProvider.java
@@ -1,9 +1,12 @@
 package org.dcache.restful.providers;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
@@ -14,12 +17,14 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class ObjectMapperProvider implements ContextResolver<ObjectMapper>
 {
+    private final static Module PNFSID_SERIALIZER = createPnfsIdSerializer();
     private final ObjectMapper defaultObjectMapper = createDefaultMapper();
     private final ObjectMapper listObjectMapper = createListObjectMapper();
 
     private static ObjectMapper createListObjectMapper()
     {
         return new ObjectMapper()
+                .registerModule(PNFSID_SERIALIZER)
                 .disable(SerializationFeature.WRITE_NULL_MAP_VALUES)
                 .enable(DeserializationFeature.UNWRAP_ROOT_VALUE)
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
@@ -29,8 +34,18 @@ public class ObjectMapperProvider implements ContextResolver<ObjectMapper>
     private static ObjectMapper createDefaultMapper()
     {
         return new ObjectMapper()
+                .registerModule(PNFSID_SERIALIZER)
                 .enable(SerializationFeature.INDENT_OUTPUT)
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    private static Module createPnfsIdSerializer()
+    {
+        Version version = new Version(1, 0, 0,
+                                      null, null, null);
+        SimpleModule pnfsIdModule = new SimpleModule("PnfsIdModule", version);
+        pnfsIdModule.addSerializer(new PnfsidSerializer());
+        return pnfsIdModule;
     }
 
     @Override
