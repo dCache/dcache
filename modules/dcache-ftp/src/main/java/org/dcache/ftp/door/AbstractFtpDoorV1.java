dCache/dcache

commit 59a67f4a13d173840b525ef715813603869951c7

ftp: Don't hold locking during proxy shut down
Motivation:

The Transfer object is a central component to transfers in dCache. Calls
synchronize to allow thread safe access and updates, but it is important
that the lock isn't held during blocking operations. The FTP door
violates that rule by holding the lock while shutting down its data
channel proxy.

Modification:

Don't hold the lock during proxy shut down.

Result:

In particular info calls no longer block while the adapter shuts down.
This could also block the System cell which in turn could cause other
features in dCache to block - such as tab completion in the admin
shell.

Target: trunk
Require-notes: yes
Require-book: no
Request: 2.13
Acked-by: Paul Millar <paul.millar@desy.de>
Patch: https://rb.dcache.org/r/8564/

modules/dcache-ftp/src/main/java/org/dcache/ftp/door/AbstractFtpDoorV1.java
@@ -76,7 +76,6 @@ import com.google.common.collect.Range;
 import com.google.common.collect.Sets;
 import com.google.common.net.InetAddresses;
 import com.google.common.primitives.Ints;
-import org.ietf.jgss.GSSException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -1136,33 +1135,37 @@ public abstract class AbstractFtpDoorV1
         }
 
         @Override
-        protected synchronized void onFinish() throws FTPCommandException
+        protected void onFinish() throws FTPCommandException
         {
             try {
-                /* Wait for adapter to shut down.
-                 */
-                if (_adapter != null) {
+                ProxyAdapter adapter;
+                synchronized (this) {
+                    adapter = _adapter;
+                }
+
+                if (adapter != null) {
                     LOGGER.info("Waiting for adapter to finish.");
-                    _adapter.join(300000); // 5 minutes
-                    if (_adapter.isAlive()) {
+                    adapter.join(300000); // 5 minutes
+                    if (adapter.isAlive()) {
                         throw new FTPCommandException(451, "FTP proxy did not shut down");
                     } else if (_adapter.hasError()) {
                         throw new FTPCommandException(451, "FTP proxy failed: " + _adapter.getError());
                     }
 
                     LOGGER.debug("Closing adapter");
-                    _adapter.close();
+                    adapter.close();
+                }
+
+                synchronized (this) {
                     _adapter = null;
-                }
-
-                if (_perfMarkerTask != null) {
-                    _perfMarkerTask.stop((GFtpProtocolInfo) getProtocolInfo());
-                }
-
-                if (_tLog != null) {
-                    _tLog.middle(getFileAttributes().getSize());
-                    _tLog.success();
-                    _tLog = null;
+                    if (_perfMarkerTask != null) {
+                        _perfMarkerTask.stop((GFtpProtocolInfo) getProtocolInfo());
+                    }
+                    if (_tLog != null) {
+                        _tLog.middle(getFileAttributes().getSize());
+                        _tLog.success();
+                        _tLog = null;
+                    }
                 }
 
                 notifyBilling(0, "");

commit 2445198a637a600cff713661f9606db33d852386

ftp: remove rare NullPointerException when proxying data
Motivation:

Commit 59a67f4a1 introduced a regression where a field member is
accessed outside of a synchronized block. Therefore it is theoretically
possible for the door to experience a NullPointerException when a
proxyed transfer has completed.

Modification:

Use the copy of the adapter object to avoid NPE.

Result:

A rare (unobserved) bug has been fixed

Target: master
Request: 4.0
Request: 3.2
Request: 3.1
Request: 3.0
Request: 2.16
Require-notes: yes
Require-book: no
Patch: https://rb.dcache.org/r/10765/
Acked-by: Tigran Mkrtchyan

modules/dcache-ftp/src/main/java/org/dcache/ftp/door/AbstractFtpDoorV1.java
@@ -1237,7 +1237,7 @@ public abstract class AbstractFtpDoorV1
                     LOGGER.info("Waiting for adapter to finish.");
                     adapter.join(300000); // 5 minutes
                     checkFTPCommand(!adapter.isAlive(), 451, "FTP proxy did not shut down");
-                    checkFTPCommand(!_adapter.hasError(), 451, "FTP proxy failed: %s", _adapter.getError());
+                    checkFTPCommand(!adapter.hasError(), 451, "FTP proxy failed: %s", adapter.getError());
                     LOGGER.debug("Closing adapter");
                     adapter.close();
                 }