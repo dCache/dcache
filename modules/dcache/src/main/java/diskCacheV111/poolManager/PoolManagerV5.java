dCache/dcache

commit 23cfdc51a5b0d9d4f7781c3cfcea69815ff938cb

pool: Fix interpretation of eager option of migration module
Migration module will try to reuse existing replicas on target pools. This is
how idempotence is achieved. When a pool with an existing replica fails to
process a request from migration module, the request will patiently be retried.
This can be surpressed using the -eager option. If used, this causes the
migration module to proceed to creating a new replica.

There is a flaw in the interpretation of not specifying -eager: If a pool is
down and not registered in pool manager, the pool information provided by pool
manager does not include those pools. Thus even though existing copies existing
on target pools, those pools are invisible to migration module. Thus despite
not being eager, a migration job would proceed with creating new replicas.

This patch fixes the problem by including offline pools in the query reply from
pool manager. The information is added in such a way that old pools without
this patch will not be affected by the change. If a pool is updated without
updating pool manager, the message will return an empty list for offline pools,
effectively maintaining the old semantics. This to fix the problem, both pool
manager and pools have to be updated, but the change is fully backwards
compatible.

The semantics of the -include-when and -exclude-when options is tricky as they
break idempotence. For offline pools, these expressions cannot be interpreted
and thus a migration job may block on an offline pool that would have been
excluded by these expressions had the pool been online. The proper fix is to
replace the -include-when and -exclude-when options with something that is
idempotent, but such a change would not be applicable to stable branches. The
present patch is kept minimal so it can be included on stable branches.

Target: trunk
Request: 2.10
Require-notes: yes
Require-book: yes
Acked-by: Albert Rossi <arossi@fnal.gov>
Patch: https://rb.dcache.org/r/7345

modules/dcache/src/main/java/diskCacheV111/poolManager/PoolManagerV5.java
@@ -465,17 +465,41 @@ public class PoolManagerV5
         return msg;
     }
 
+    private void getPoolInformation(
+            PoolSelectionUnit.SelectionPool pool,
+            Collection<PoolManagerPoolInformation> onlinePools,
+            Collection<String> offlinePools)
+    {
+        String name = pool.getName();
+        PoolCostInfo cost = _costModule.getPoolCostInfo(name);
+        if (!pool.isActive() || cost == null) {
+            offlinePools.add(name);
+        } else {
+            onlinePools.add(new PoolManagerPoolInformation(name, cost));
+        }
+    }
+
+    private void getPoolInformation(
+            Collection<PoolSelectionUnit.SelectionPool> pools,
+            Collection<PoolManagerPoolInformation> onlinePools,
+            Collection<String> offlinePools)
+    {
+        for (PoolSelectionUnit.SelectionPool pool: pools) {
+            getPoolInformation(pool, onlinePools, offlinePools);
+        }
+    }
+
     public PoolManagerGetPoolsByNameMessage
         messageArrived(PoolManagerGetPoolsByNameMessage msg)
     {
-        List<PoolManagerPoolInformation> pools = new ArrayList<>();
+        List<PoolManagerPoolInformation> onlinePools = new ArrayList<>();
+        List<String> offlinePools = new ArrayList<>();
         for (String name: msg.getPoolNames()) {
-            PoolManagerPoolInformation pool = _poolMonitor.getPoolInformation(name);
-            if (pool != null) {
-                pools.add(pool);
-            }
+            PoolSelectionUnit.SelectionPool pool = _selectionUnit.getPool(name);
+            getPoolInformation(pool, onlinePools, offlinePools);
         }
-        msg.setPools(pools);
+        msg.setPools(onlinePools);
+        msg.setOfflinePools(offlinePools);
         msg.setSucceeded();
         return msg;
     }
@@ -484,7 +508,13 @@ public class PoolManagerV5
         messageArrived(PoolManagerGetPoolsByLinkMessage msg)
     {
         try {
-            msg.setPools(_poolMonitor.getPoolsByLink(msg.getLink()));
+            List<PoolManagerPoolInformation> onlinePools = new ArrayList<>();
+            List<String> offlinePools = new ArrayList<>();
+            PoolSelectionUnit.SelectionLink link =
+                    _selectionUnit.getLinkByName(msg.getLink());
+            getPoolInformation(link.getPools(), onlinePools, offlinePools);
+            msg.setPools(onlinePools);
+            msg.setOfflinePools(offlinePools);
             msg.setSucceeded();
         } catch (NoSuchElementException e) {
             Collection<PoolManagerPoolInformation> empty =
@@ -500,10 +530,12 @@ public class PoolManagerV5
     {
         try {
             List<PoolManagerPoolInformation> pools = new ArrayList<>();
+            List<String> offlinePools = new ArrayList<>();
             for (String poolGroup : msg.getPoolGroups()) {
-                pools.addAll(_poolMonitor.getPoolsByPoolGroup(poolGroup));
+                getPoolInformation(_selectionUnit.getPoolsByPoolGroup(poolGroup), pools, offlinePools);
             }
             msg.setPools(pools);
+            msg.setOfflinePools(offlinePools);
             msg.setSucceeded();
         } catch (NoSuchElementException e) {
             Collection<PoolManagerPoolInformation> empty =

modules/dcache/src/main/java/diskCacheV111/poolManager/PoolMonitorV5.java
@@ -478,58 +478,6 @@ public class PoolMonitorV5
         return list ;
     }
 
-    @Nullable
-    private PoolManagerPoolInformation getPoolInformation(@Nonnull PoolSelectionUnit.SelectionPool pool)
-    {
-        String name = pool.getName();
-        PoolCostInfo cost = _costModule.getPoolCostInfo(name);
-        if (!pool.isActive() || cost == null) {
-            return null;
-        }
-        return new PoolManagerPoolInformation(name, cost);
-    }
-
-    @Nonnull
-    private Collection<PoolManagerPoolInformation>
-        getPoolInformation(@Nonnull Collection<PoolSelectionUnit.SelectionPool> pools)
-    {
-        List<PoolManagerPoolInformation> result = new ArrayList<>();
-        for (PoolSelectionUnit.SelectionPool pool: pools) {
-            PoolManagerPoolInformation poolInformation = getPoolInformation(pool);
-            if (poolInformation != null) {
-                result.add(poolInformation);
-            }
-        }
-        return result;
-    }
-
-    @Override @Nullable
-    public PoolManagerPoolInformation getPoolInformation(@Nonnull String name)
-    {
-        PoolSelectionUnit.SelectionPool pool = _selectionUnit.getPool(name);
-        return (pool == null) ? null : getPoolInformation(pool);
-    }
-
-    @Override @Nonnull
-    public Collection<PoolManagerPoolInformation>
-        getPoolsByLink(@Nonnull String linkName)
-        throws NoSuchElementException
-    {
-        PoolSelectionUnit.SelectionLink link =
-            _selectionUnit.getLinkByName(linkName);
-        return new ArrayList<>(getPoolInformation(link.getPools()));
-    }
-
-    @Override @Nonnull
-    public Collection<PoolManagerPoolInformation>
-        getPoolsByPoolGroup(@Nonnull String poolGroup)
-        throws NoSuchElementException
-    {
-        Collection<PoolSelectionUnit.SelectionPool> pools =
-            _selectionUnit.getPoolsByPoolGroup(poolGroup);
-        return new ArrayList<>(getPoolInformation(pools));
-    }
-
     public static Set<FileAttribute> getRequiredAttributesForFileLocality()
     {
         return EnumSet.of(STORAGEINFO, SIZE, LOCATIONS);

modules/dcache/src/main/java/diskCacheV111/vehicles/PoolManagerGetPoolsMessage.java
@@ -1,6 +1,7 @@
 package diskCacheV111.vehicles;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class PoolManagerGetPoolsMessage
     extends Message
@@ -8,14 +9,25 @@ public class PoolManagerGetPoolsMessage
     private static final long serialVersionUID = 4793574345114253473L;
 
     private Collection<PoolManagerPoolInformation> _pools;
+    private Collection<String> _offlinePools;
 
     public void setPools(Collection<PoolManagerPoolInformation> pools)
     {
         _pools = pools;
     }
 
+    public void setOfflinePools(Collection<String> pools)
+    {
+        _offlinePools = pools;
+    }
+
     public Collection<PoolManagerPoolInformation> getPools()
     {
         return _pools;
+    }
+
+    public Collection<String> getOfflinePools()
+    {
+        return (_offlinePools == null) ? Collections.<String>emptyList() : _offlinePools;
     }
 }

modules/dcache/src/main/java/org/dcache/pool/migration/PoolListFilter.java
@@ -20,9 +20,6 @@ public class PoolListFilter implements RefreshablePoolList
     private static final Logger _log =
         LoggerFactory.getLogger(PoolListFilter.class);
 
-    private static final ImmutableList<PoolManagerPoolInformation> EMPTY_LIST =
-        ImmutableList.of();
-
     private final Collection<Pattern> _exclude;
     private final Expression _excludeWhen;
     private final Collection<Pattern> _include;
@@ -32,6 +29,9 @@ public class PoolListFilter implements RefreshablePoolList
 
     private ImmutableList<PoolManagerPoolInformation> _cachedList;
     private ImmutableList<PoolManagerPoolInformation> _filteredList;
+
+    private ImmutableList<String> _cachedOfflinePools;
+    private ImmutableList<String> _filteredOfflinePools;
 
     public PoolListFilter(RefreshablePoolList poolList,
                           Collection<Pattern> exclude,
@@ -61,15 +61,41 @@ public class PoolListFilter implements RefreshablePoolList
     }
 
     @Override
-    synchronized public
-        ImmutableList<PoolManagerPoolInformation> getPools()
+    public ImmutableList<String> getOfflinePools()
     {
         if (!isValid()) {
-            return EMPTY_LIST;
+            return ImmutableList.of();
         }
         PoolManagerPoolInformation source = getSource();
         if (source == null) {
-            return EMPTY_LIST;
+            return ImmutableList.of();
+        }
+
+        ImmutableList<String> pools = _poolList.getOfflinePools();
+        if (!pools.equals(_cachedOfflinePools)) {
+            ImmutableList.Builder<String> filteredOfflinePools =
+                    ImmutableList.builder();
+            for (String pool: pools) {
+                if (!isExcluded(source, pool) && isIncluded(source, pool)) {
+                    filteredOfflinePools.add(pool);
+                }
+            }
+            _filteredOfflinePools = filteredOfflinePools.build();
+        }
+        _cachedOfflinePools = pools;
+        return _filteredOfflinePools;
+    }
+
+    @Override
+    public synchronized
+        ImmutableList<PoolManagerPoolInformation> getPools()
+    {
+        if (!isValid()) {
+            return ImmutableList.of();
+        }
+        PoolManagerPoolInformation source = getSource();
+        if (source == null) {
+            return ImmutableList.of();
         }
 
         ImmutableList<PoolManagerPoolInformation> list = _poolList.getPools();
@@ -95,6 +121,16 @@ public class PoolListFilter implements RefreshablePoolList
             }
         }
         return false;
+    }
+
+    private boolean isExcluded(PoolManagerPoolInformation source, String pool)
+    {
+        return matchesAny(_exclude, pool);
+    }
+
+    private boolean isIncluded(PoolManagerPoolInformation source, String pool)
+    {
+        return _include.isEmpty() || matchesAny(_include, pool);
     }
 
     private boolean isExcluded(PoolManagerPoolInformation source, PoolManagerPoolInformation pool)

modules/dcache/src/main/java/org/dcache/pool/migration/PoolListFromPoolManager.java
@@ -19,24 +19,34 @@ public abstract class PoolListFromPoolManager
     protected ImmutableList<PoolManagerPoolInformation> _pools =
         ImmutableList.of();
 
+    protected ImmutableList<String> _offlinePools =
+        ImmutableList.of();
+
     protected boolean _isValid;
 
     @Override
-    synchronized public boolean isValid()
+    public synchronized boolean isValid()
     {
         return _isValid;
     }
 
     @Override
-    synchronized public ImmutableList<PoolManagerPoolInformation> getPools()
+    public synchronized ImmutableList<String> getOfflinePools()
+    {
+        return _offlinePools;
+    }
+
+    @Override
+    public synchronized ImmutableList<PoolManagerPoolInformation> getPools()
     {
         return _pools;
     }
 
     @Override
-    synchronized public void success(PoolManagerGetPoolsMessage msg)
+    public synchronized void success(PoolManagerGetPoolsMessage msg)
     {
         _pools = ImmutableList.copyOf(msg.getPools());
+        _offlinePools = ImmutableList.copyOf(msg.getOfflinePools());
         _isValid = true;
     }
 

modules/dcache/src/main/java/org/dcache/pool/migration/RefreshablePoolList.java
@@ -20,7 +20,12 @@ public interface RefreshablePoolList
     boolean isValid();
 
     /**
-     * Returns information about pools in the list.
+     * Returns the names of offline pools in the list.
+     */
+    ImmutableList<String> getOfflinePools();
+
+    /**
+     * Returns information about online pools in the list.
      */
     ImmutableList<PoolManagerPoolInformation> getPools();
 

modules/dcache/src/main/java/org/dcache/pool/migration/Task.java
@@ -1,6 +1,6 @@
 package org.dcache.pool.migration;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
 import statemap.TransitionUndefinedException;
 
 import java.io.PrintWriter;
@@ -36,11 +36,10 @@ import org.dcache.services.pinmanager1.PinManagerMovePinMessage;
 import org.dcache.util.FireAndForgetTask;
 import org.dcache.util.ReflectionUtils;
 
-import static com.google.common.base.Predicates.compose;
-import static com.google.common.base.Predicates.in;
-import static com.google.common.base.Predicates.not;
+import static com.google.common.base.Predicates.*;
+import static com.google.common.collect.Collections2.transform;
 import static com.google.common.collect.Iterables.filter;
-import static com.google.common.collect.Lists.*;
+import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Queues.newArrayDeque;
 
 /**
@@ -205,18 +204,6 @@ public class Task
     }
 
     /**
-     * Returns the pool names in the associated pool list.
-     */
-    private Collection<String> getPools()
-    {
-        Collection<String> pools = new HashSet<>();
-        for (PoolManagerPoolInformation pool: _definition.poolList.getPools()) {
-            pools.add(pool.getName());
-        }
-        return pools;
-    }
-
-    /**
      * Returns a pool from the pool list using the pool selection
      * strategy.
      */
@@ -280,7 +267,14 @@ public class Task
      */
     private synchronized void setLocations(List<String> locations)
     {
-        _locations = newArrayDeque(filter(locations, in(getPools())));
+        Collection<String> onlinePools =
+                transform(_definition.poolList.getPools(), PoolManagerPoolInformation.GET_NAME);
+        if (_definition.isEager) {
+            _locations = newArrayDeque(filter(locations, in(onlinePools)));
+        } else {
+            ImmutableList<String> offlinePools = _definition.poolList.getOfflinePools();
+            _locations = newArrayDeque(filter(locations, or(in(onlinePools), in(offlinePools))));
+        }
     }
 
     /**

modules/dcache/src/main/java/org/dcache/poolmanager/PoolMonitor.java
@@ -1,16 +1,11 @@
 package org.dcache.poolmanager;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 import java.util.Collection;
-import java.util.NoSuchElementException;
 
 import diskCacheV111.poolManager.CostModule;
 import diskCacheV111.poolManager.PoolSelectionUnit;
 import diskCacheV111.pools.PoolCostInfo;
 import diskCacheV111.util.FileLocality;
-import diskCacheV111.vehicles.PoolManagerPoolInformation;
 import diskCacheV111.vehicles.ProtocolInfo;
 
 import org.dcache.vehicles.FileAttributes;
@@ -36,17 +31,6 @@ public interface PoolMonitor
             String linkGroup);
 
     Collection<PoolCostInfo> queryPoolsByLinkName(String linkName);
-
-    @Nullable
-    PoolManagerPoolInformation getPoolInformation(@Nonnull String name);
-
-    Collection<PoolManagerPoolInformation> getPoolsByLink(
-            String linkName)
-            throws NoSuchElementException;
-
-    Collection<PoolManagerPoolInformation> getPoolsByPoolGroup(
-            String poolGroup)
-            throws NoSuchElementException;
 
     FileLocality getFileLocality(FileAttributes attributes, String hostName);
 }

commit 39d6a6e1cdd1e0fe43acfd8337ff727ed141ec1f

poolmanager: fix migration command if named pool is removed
Motivation:

Commit 23cfdc51 introduced a regression by not considering that an incoming
PoolManagerGetPoolsByNameMessage request could target pools that are not just
offline, but are actually unknown.  This triggers a NPE.

Such a scenario could happen if a migration copy/move command is targeting specific
pools and one of those pools is removed from pool-manager.

Modification:

When information is requested about specific pools, filter out from the
response those pool names that are unknown.

Result:

migration move/copy commands that target specific pools do not trigger a
NullPointerException if one of the pools is removed from pool-manager.

Target: master
Request: 4.1
Request: 4.0
Request: 3.2
Request: 3.1
Request: 3.0
Request: 2.16
Require-notes: yes
Require-book: yes
Ticket: http://rt.dcache.org/Ticket/Display.html?id=9379
Patch: https://rb.dcache.org/r/10872/
Acked-by: Albert Rossi

modules/dcache/src/main/java/diskCacheV111/poolManager/PoolManagerV5.java
@@ -577,7 +577,9 @@ public class PoolManagerV5
         List<String> offlinePools = new ArrayList<>();
         for (String name: msg.getPoolNames()) {
             PoolSelectionUnit.SelectionPool pool = _selectionUnit.getPool(name);
-            getPoolInformation(pool, onlinePools, offlinePools);
+            if (pool != null) {
+                getPoolInformation(pool, onlinePools, offlinePools);
+            }
         }
         msg.setPools(onlinePools);
         msg.setOfflinePools(offlinePools);