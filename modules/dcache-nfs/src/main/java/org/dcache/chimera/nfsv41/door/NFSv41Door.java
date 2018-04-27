dCache/dcache

commit a11ec8d76b549e3e1de948e2848d73d86725029e

vehicles: introduce Pool class to glue pool name and address
Motivation:
dCache has a logical split between pool name and address. However as the pool
name and address usually used together, we have to use two variables and
keep them in sync.

Modification:
Introduce Pool class to represent pool name + address combination. Update
doors to use new Pool class.

Result:
more consistent pool handling
This change introduces incompatibility between doors and PoolManager.

Acked-by: Paul Millar
Target: master
Require-book: no
Require-notes: yes

modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/NFSv41Door.java
@@ -36,6 +36,7 @@ import diskCacheV111.vehicles.DoorRequestInfoMessage;
 import diskCacheV111.vehicles.DoorTransferFinishedMessage;
 import diskCacheV111.vehicles.IoDoorEntry;
 import diskCacheV111.vehicles.IoDoorInfo;
+import diskCacheV111.vehicles.Pool;
 import diskCacheV111.vehicles.PoolMoverKillMessage;
 import diskCacheV111.vehicles.PoolPassiveIoFileMessage;
 import diskCacheV111.vehicles.PoolStatusChangedMessage;
@@ -879,7 +880,7 @@ public class NFSv41Door extends AbstractCellComponent implements
 
             return _ioMessages.values()
                     .stream()
-                    .filter(d -> pool == null ? true : pool.matches(d.getPool()))
+                    .filter(d -> pool == null ? true : pool.matches(d.getPool().getName()))
                     .filter(d -> client == null ? true : client.matches(d.getClient().toString()))
                     .filter(d -> pnfsid == null ? true : pnfsid.matches(d.getPnfsId().toString()))
                     .map(Object::toString)
@@ -1032,8 +1033,8 @@ public class NFSv41Door extends AbstractCellComponent implements
                     }
                     String location = locations.iterator().next();
                     _log.debug("Using pre-existing WRITE pool {} for {}", location, getPnfsId());
-                    setPool(location);
-                    setPoolAddress(new CellAddressCore(location));
+                    // REVISIT: here we knoe that pool name and address are the same thing
+                    setPool(new Pool(location, new CellAddressCore(location)));
                     _redirectFuture = startMoverAsync(STAGE_REQUEST_TIMEOUT);
                 } else {
                     _log.debug("looking a {} pool for {}", (isWrite() ? "WRITE" : "READ"), getPnfsId());

modules/dcache-dcap/src/main/java/diskCacheV111/doors/DCapDoorInterpreterV3.java
@@ -52,6 +52,7 @@ import diskCacheV111.vehicles.IoDoorInfo;
 import diskCacheV111.vehicles.Message;
 import diskCacheV111.vehicles.PnfsCreateEntryMessage;
 import diskCacheV111.vehicles.PnfsFlagMessage;
+import diskCacheV111.vehicles.Pool;
 import diskCacheV111.vehicles.PoolAcceptFileMessage;
 import diskCacheV111.vehicles.PoolCheckFileMessage;
 import diskCacheV111.vehicles.PoolDeliverFileMessage;
@@ -1686,7 +1687,7 @@ public class DCapDoorInterpreterV3
     protected  class IoHandler  extends PnfsSessionHandler  {
         private String           _ioMode;
         private DCapProtocolInfo _protocolInfo;
-        private String           _pool         = "<unknown>" ;
+        private Pool             _pool;
         private Assumption       _assumption;
         private Integer          _moverId;
         private boolean          _isHsmRequest;
@@ -1770,7 +1771,7 @@ public class DCapDoorInterpreterV3
             return new IoDoorEntry(_sessionId,
                                    pnfsid,
                                    _subject,
-                                   _pool,
+                                   _pool == null? "<unknown>" : _pool.getName(),
                                    _status,
                                    _statusSince,
                                    _clientSocketAddress.getAddress().getHostAddress());
@@ -2094,7 +2095,7 @@ public class DCapDoorInterpreterV3
                 }
                 return;
             }
-            String pool = reply.getPoolName();
+            Pool pool = reply.getPool();
             if (pool == null) {
                 sendReply( "poolMgrGetPoolArrived" , 33 , "No pools available" ) ;
                 removeUs() ;
@@ -2113,14 +2114,14 @@ public class DCapDoorInterpreterV3
                     ((PoolMgrSelectReadPoolMsg) reply).getContext();
                 poolMessage =
                         new PoolDeliverFileMessage(
-                                pool,
+                                pool.getName(),
                                 _protocolInfo ,
                                 _fileAttributes,
                                 _assumption);
             }else if( reply instanceof PoolMgrSelectWritePoolMsg ){
                 poolMessage =
                         new PoolAcceptFileMessage(
-                                pool,
+                                pool.getName(),
                                 _protocolInfo ,
                                 _fileAttributes,
                                 _assumption,
@@ -2155,7 +2156,7 @@ public class DCapDoorInterpreterV3
                 return ;
             }
             try{
-                _poolMgrStub.start(reply.getPoolAddress(), poolMessage);
+                _poolMgrStub.start(pool.getAddress(), poolMessage);
                 _poolRequestDone = true ;
             }catch(RuntimeException ie){
                 sendReply( "poolMgrGetPoolArrived" , 2 ,
@@ -2256,11 +2257,11 @@ public class DCapDoorInterpreterV3
         public void removeUs() {
             Integer moverId = _moverId;
             if (moverId != null) {
-                PoolMoverKillMessage message = new PoolMoverKillMessage(_pool,
+                PoolMoverKillMessage message = new PoolMoverKillMessage(_pool.getName(),
                         moverId, "killed by door: " + _explanation);
                 message.setReplyRequired(false);
 
-                _cell.sendMessage(new CellMessage(new CellPath(_pool), message));
+                _cell.sendMessage(new CellMessage(_pool.getAddress(), message));
             }
             super.removeUs();
         }

modules/dcache-ftp/src/main/java/org/dcache/ftp/door/AbstractFtpDoorV1.java
@@ -1193,7 +1193,7 @@ public abstract class AbstractFtpDoorV1
             reply(_request, "150 Opening BINARY data connection for " + _path);
 
             if (isWrite() && _xferMode.equals("E") && _performanceMarkerPeriod > 0) {
-                _perfMarkerTask = new PerfMarkerTask(_request, getPoolAddress(),
+                _perfMarkerTask = new PerfMarkerTask(_request, getPool().getAddress(),
                         getMoverId(), _performanceMarkerPeriod / 2);
                 TIMER.schedule(_perfMarkerTask, _performanceMarkerPeriod, _performanceMarkerPeriod);
             }

modules/dcache-vehicles/src/main/java/diskCacheV111/vehicles/Pool.java
@@ -1,0 +1,41 @@
+package diskCacheV111.vehicles;
+
+import dmg.cells.nucleus.CellAddressCore;
+import java.io.Serializable;
+
+/**
+ * Object representing Pool name and associated address.
+ */
+public class Pool implements Serializable {
+
+    private static final long serialVersionUID = -5093980092916270836L;
+
+    private final String poolName;
+    private final CellAddressCore poolAddress;
+
+    public Pool(String poolName, CellAddressCore poolAddress) {
+        this.poolName = poolName;
+        this.poolAddress = poolAddress;
+    }
+
+    /**
+     * Get pool's name.
+     * @return pool's name.
+     */
+    public String getName() {
+        return poolName;
+    }
+
+    /**
+     * Get cell address of this pool.
+     * @return cell address of this pool.
+     */
+    public CellAddressCore getAddress() {
+        return poolAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "PoolName=" + poolName + " PoolAddress=" + poolAddress;
+    }
+}

modules/dcache-vehicles/src/main/java/diskCacheV111/vehicles/PoolMgrGetPoolMsg.java
@@ -6,8 +6,6 @@ import java.util.EnumSet;
 
 import diskCacheV111.util.PnfsId;
 
-import dmg.cells.nucleus.CellAddressCore;
-
 import org.dcache.namespace.FileAttribute;
 import org.dcache.vehicles.FileAttributes;
 
@@ -19,8 +17,7 @@ public class PoolMgrGetPoolMsg extends PoolManagerMessage
     private static final long serialVersionUID = 8907604668091102254L;
 
     private final FileAttributes _fileAttributes;
-    private String _poolName;
-    private CellAddressCore _poolAddress;
+    private Pool _pool;
 
     public PoolMgrGetPoolMsg(FileAttributes fileAttributes)
     {
@@ -48,24 +45,14 @@ public class PoolMgrGetPoolMsg extends PoolManagerMessage
 	return _fileAttributes.getPnfsId();
     }
 
-    public String getPoolName()
+    public Pool getPool()
     {
-	return _poolName;
+	return _pool;
     }
 
-    public void setPoolName(String poolName)
+    public void setPool(Pool pool)
     {
-	_poolName = poolName;
-    }
-
-    public void setPoolAddress(CellAddressCore poolAddress)
-    {
-        _poolAddress = poolAddress;
-    }
-
-    public CellAddressCore getPoolAddress()
-    {
-        return _poolAddress;
+	_pool = pool;
     }
 
     @Override
@@ -73,9 +60,8 @@ public class PoolMgrGetPoolMsg extends PoolManagerMessage
     {
         if (getReturnCode() == 0) {
             return "PnfsId=" + getPnfsId()
-                    + ";StorageInfo=" + getStorageInfo()
-                    + ((_poolName == null) ? "" : ";PoolName=" + _poolName)
-                    + ((_poolAddress == null) ? "" : "PoolAddress=" + _poolAddress);
+                    + ";StorageInfo=" + getStorageInfo() + ";"
+                    + ((_pool == null) ? "" : _pool);
         } else {
             return super.toString();
         }

modules/dcache/src/main/java/diskCacheV111/admin/LegacyAdminShell.java
@@ -979,7 +979,7 @@ public class LegacyAdminShell
             PoolMgrReplicateFileMsg select =
                     new PoolMgrReplicateFileMsg(fileAttributesMsg.getFileAttributes(), pinfo);
             select = _poolManager.sendAndWait(select, timeout);
-            return "p2p -> " + select.getPoolName();
+            return "p2p -> " + select.getPool().getName();
         }
     }
 

modules/dcache/src/main/java/diskCacheV111/doors/CopyManager.java
@@ -24,6 +24,7 @@ import diskCacheV111.vehicles.DCapClientPortAvailableMessage;
 import diskCacheV111.vehicles.DCapClientProtocolInfo;
 import diskCacheV111.vehicles.DCapProtocolInfo;
 import diskCacheV111.vehicles.DoorTransferFinishedMessage;
+import diskCacheV111.vehicles.Pool;
 import diskCacheV111.vehicles.ProtocolInfo;
 
 import dmg.cells.nucleus.AbstractCellComponent;
@@ -216,10 +217,10 @@ public class CopyManager extends AbstractCellComponent
         private void appendTransfer(StringBuilder sb, Transfer transfer)
         {
             PnfsId id = transfer.getPnfsId();
-            String pool = transfer.getPool();
+            Pool pool = transfer.getPool();
             Integer mover = transfer.getMoverId();
             sb.append("        PNFS-ID: ").append(id == null ? "Not yet known" : id).append('\n');
-            sb.append("        Pool: ").append(pool == null ? "Not yet selected" : pool);
+            sb.append("        Pool: ").append(pool == null ? "Not yet selected" : pool.getName());
             if (mover != null) {
                 sb.append('\n');
                 sb.append("        Mover: ").append(mover);

modules/dcache/src/main/java/diskCacheV111/poolManager/PoolManagerV5.java
@@ -829,8 +829,7 @@ public class PoolManagerV5
 
         protected void requestSucceeded(SelectedPool pool)
         {
-            _request.setPoolName(pool.name());
-            _request.setPoolAddress(pool.address());
+            _request.setPool(new diskCacheV111.vehicles.Pool(pool.name(), pool.address()));
             _request.setAssumption(pool.assumption());
             _request.setSucceeded();
             reply(_request);

commit 7487d0a90ae46c280eae4992735d8fd2fd319966

nfs: fix invalid types in equals (regression in a11ec8d76b)
Since introduction on Pool class, some operations which compare pool
name with string may have incorrect behaviour.

Acked-by: Paul Millar
Target: master
Require-book: no
Require-notes: no

modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/NFSv41Door.java
@@ -1373,7 +1373,8 @@ public class NFSv41Door extends AbstractCellComponent implements
     private synchronized long recallLayouts(String pool) {
 
         return _ioMessages.values().stream()
-                .filter(t -> pool.equals(t.getPool()))
+                .filter(t -> t.getPool() != null)
+                .filter(t -> pool.equals(t.getPool().getName()))
                 .filter(t -> t.getClient().getMinorVersion() > 0)
                 .peek(t -> {
                     t.recallLayout(_callbackExecutor);