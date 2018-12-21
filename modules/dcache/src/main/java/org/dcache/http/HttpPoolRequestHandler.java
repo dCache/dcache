dCache/dcache

commit 4fd18c5dbe9597292bb15746633204fe701503db

pool: grow file prior HTTP upload
Motivation:
when HTTP PUT requests sent to the dcache pool and file's length is
know, then we should pre-allocate file's size to let back-end store to
optimize block allocation. This is especially important for high latency
systems, like CEPH.

Modification:
Grow file size if 'Content-Length' header is provided for http PUT.

Result:
x100 performance boost for ceph pool.

Acked-by: Vincent Garonne
Acked-by: Paul Millar
Target: master, 4.2
Require-book: no
Require-notes: yes

modules/dcache/src/main/java/org/dcache/http/HttpPoolRequestHandler.java
@@ -39,6 +39,7 @@ import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalLong;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -50,6 +51,7 @@ import diskCacheV111.vehicles.HttpProtocolInfo;
 
 import dmg.util.HttpException;
 
+import org.dcache.namespace.FileAttribute;
 import org.dcache.pool.movers.NettyTransferService;
 import org.dcache.pool.repository.OutOfDiskException;
 import org.dcache.util.Checksum;
@@ -134,6 +136,15 @@ public class HttpPoolRequestHandler extends HttpRequestHandler
             return contentMd5.map(Checksums::parseContentMd5);
         } catch (IllegalArgumentException e) {
             throw new HttpException(BAD_REQUEST.code(), "Bad " + CONTENT_MD5 + " header: " + e);
+        }
+    }
+
+    private static OptionalLong contentLength(HttpRequest request) throws HttpException {
+        try {
+            Optional<String> contentLenght = Optional.ofNullable(request.headers().get(CONTENT_LENGTH));
+            return OptionalLong.of(Long.parseLong(contentLenght.get()));
+        } catch (NumberFormatException e) {
+            throw new HttpException(BAD_REQUEST.code(), "Bad " + CONTENT_LENGTH + " header: " + e);
         }
     }
 
@@ -384,6 +395,13 @@ public class HttpPoolRequestHandler extends HttpRequestHandler
 
             contentMd5Checksum(request).ifPresent(file::addChecksum);
 
+            OptionalLong contentLength = contentLength(request);
+            if (contentLength.isPresent()) {
+                file.truncate(contentLength.getAsLong());
+            } else if (file.getFileAttributes().isDefined(FileAttribute.SIZE)) {
+                file.truncate(file.getFileAttributes().getSize());
+            }
+
             file.getProtocolInfo().getWantedChecksum().ifPresent(file::addChecksumType);
             _wantedDigest = wantDigest(request).flatMap(Checksums::parseWantDigest);
             _wantedDigest.ifPresent(file::addChecksumType);
@@ -406,7 +424,7 @@ public class HttpPoolRequestHandler extends HttpRequestHandler
         } catch (IllegalArgumentException e) {
             exception = e;
             return context.writeAndFlush(createErrorResponse(BAD_REQUEST, e.getMessage()));
-        } catch (RuntimeException e) {
+        } catch (IOException | RuntimeException e) {
             exception = e;
             return context.writeAndFlush(createErrorResponse(INTERNAL_SERVER_ERROR, e.getMessage()));
         } finally {

commit 653bb697bbf24930decdd9717d0fe11c268c74ce

pool: fix HTTP chunked upload
Motivation:

Commit 4fd18c5d introduced a bug that affected chunked uploads.
Such uploads would result in an error response:

    500 No value present

Modification:

Avoid trying to get() an Optional that is empty.

Result:

An unreleased regression is fixed.

Target: master
Requires-notes: no
Requires-book: no
Request: 5.0
Request: 4.2
Patch: https://rb.dcache.org/r/11469/
Acked-by: Albert Rossi

modules/dcache/src/main/java/org/dcache/http/HttpPoolRequestHandler.java
@@ -139,10 +139,13 @@ public class HttpPoolRequestHandler extends HttpRequestHandler
         }
     }
 
-    private static OptionalLong contentLength(HttpRequest request) throws HttpException {
+    private static OptionalLong contentLength(HttpRequest request) throws HttpException
+    {
         try {
-            Optional<String> contentLenght = Optional.ofNullable(request.headers().get(CONTENT_LENGTH));
-            return OptionalLong.of(Long.parseLong(contentLenght.get()));
+            String contentLength = request.headers().get(CONTENT_LENGTH);
+            return contentLength == null
+                    ? OptionalLong.empty()
+                    : OptionalLong.of(Long.parseLong(contentLength));
         } catch (NumberFormatException e) {
             throw new HttpException(BAD_REQUEST.code(), "Bad " + CONTENT_LENGTH + " header: " + e);
         }