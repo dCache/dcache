dCache/dcache

commit 5ecd79a82ad6c45e63b11729241b83e62213425a

pool: add session to xrootd activity
Motivation:

Currently, there is nothing in the activity log file to group together
commands issued by the same TCP connection.  Normally this is the CDC
session; however, the session is not set in the pool.

Modification:

Add the CDC session, using a somewhat similar format to that used by the
xrootd door.

Unlike the xrootd door, the pool sends netty events outside of the event
queue.  Specificially, the reply to the client's CloseRequest is delayed
until after any post-processing has completed.  Although Netty supports
this, such events are executed outside of the pipeline.  This means that
the SessionHandler (which sets the CDC session in the xrootd door) is
ineffective.

As an alternative, the NettyTransferService binds the session identifier
to a channel attribute, which the AccessLogHandler has access with the
help of a handler that wraps it and injects the session value.

Result:

When enabled, the access-log for a pool shows all entries from the same
TCP connection as having the same session identifier.

Target: master
Require-notes: no
Require-book: no
Patch: https://rb.dcache.org/r/10354/
Acked-by: Albert Rossi

modules/dcache-xrootd/src/main/java/org/dcache/xrootd/plugins/ProxyAccessLogHandler.java
@@ -46,9 +46,9 @@ import static org.dcache.util.NetLoggerBuilder.Level.INFO;
 @ChannelHandler.Sharable
 public class ProxyAccessLogHandler extends AccessLogHandler
 {
-    private final AccessLogHandler handler;
+    private final ChannelHandler handler;
 
-    public ProxyAccessLogHandler(Logger logger, AccessLogHandler handler)
+    public ProxyAccessLogHandler(Logger logger, ChannelHandler handler)
     {
         super(logger);
         this.handler = handler;

modules/dcache-xrootd/src/main/java/org/dcache/xrootd/plugins/ProxyAccessLogHandlerFactory.java
@@ -20,9 +20,11 @@ package org.dcache.xrootd.plugins;
 
 import io.netty.channel.ChannelHandler;
 
+import org.dcache.util.ChannelCdcSessionHandlerWrapper;
+
 public class ProxyAccessLogHandlerFactory extends AccessLogHandlerFactory
 {
-    private final ProxyAccessLogHandler proxyAccessLogHandler = new ProxyAccessLogHandler(accessLogger, handler);
+    private final ChannelHandler proxyAccessLogHandler = new ChannelCdcSessionHandlerWrapper(new ProxyAccessLogHandler(accessLogger, handler));
 
     @Override
     public ChannelHandler createHandler()

modules/dcache-xrootd/src/main/java/org/dcache/xrootd/plugins/AccessLogHandlerFactory.java
@@ -21,11 +21,13 @@ import io.netty.channel.ChannelHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.dcache.util.ChannelCdcSessionHandlerWrapper;
+
 public class AccessLogHandlerFactory implements ChannelHandlerFactory
 {
     protected final Logger accessLogger = LoggerFactory.getLogger("org.dcache.access.xrootd");
 
-    protected final AccessLogHandler handler = new AccessLogHandler(accessLogger);
+    protected final ChannelHandler handler = new ChannelCdcSessionHandlerWrapper(new AccessLogHandler(accessLogger));
 
     @Override
     public String getName()

modules/dcache/src/main/java/org/dcache/pool/movers/NettyTransferService.java
@@ -57,6 +57,8 @@ import diskCacheV111.vehicles.PoolIoFileMessage;
 import diskCacheV111.vehicles.ProtocolInfo;
 
 import dmg.cells.nucleus.CDC;
+import dmg.cells.nucleus.CellAddressCore;
+import dmg.cells.nucleus.CellIdentityAware;
 import dmg.cells.nucleus.CellPath;
 import dmg.cells.nucleus.NoRouteToCellException;
 
@@ -67,6 +69,7 @@ import org.dcache.pool.classic.PostTransferService;
 import org.dcache.pool.classic.TransferService;
 import org.dcache.pool.repository.ReplicaDescriptor;
 import org.dcache.util.CDCThreadFactory;
+import org.dcache.util.ChannelCdcSessionHandlerWrapper;
 import org.dcache.util.Checksum;
 import org.dcache.util.ChecksumType;
 import org.dcache.util.NettyPortRange;
@@ -84,7 +87,7 @@ import static com.google.common.base.Preconditions.checkState;
  * the Netty channel to close.
  */
 public abstract class NettyTransferService<P extends ProtocolInfo>
-    implements TransferService<NettyMover<P>>, MoverFactory
+    implements TransferService<NettyMover<P>>, MoverFactory, CellIdentityAware
 {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(NettyTransferService.class);
@@ -136,6 +139,8 @@ public abstract class NettyTransferService<P extends ProtocolInfo>
     /** Communication stub for talking to doors. */
     protected CellStub doorStub;
 
+    private CellAddressCore address;
+
     public NettyTransferService(String name)
     {
         this.name = name;
@@ -151,6 +156,12 @@ public abstract class NettyTransferService<P extends ProtocolInfo>
     public void setThreads(int threads)
     {
         this.threads = threads;
+    }
+
+    @Override
+    public void setCellAddress(CellAddressCore address)
+    {
+        this.address = address;
     }
 
     @Required
@@ -259,6 +270,8 @@ public abstract class NettyTransferService<P extends ProtocolInfo>
                         protected void initChannel(Channel ch) throws Exception
                         {
                             NettyTransferService.this.initChannel(ch);
+                            ChannelCdcSessionHandlerWrapper.bindSessionToChannel(ch,
+                                    "pool:" + address + ":" + name + ":" + ch.id());
                         }
                     });
 

modules/dcache/src/main/java/org/dcache/util/ChannelCdcSessionHandlerWrapper.java
@@ -1,0 +1,239 @@
+/* dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2018 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.util;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelOutboundHandler;
+import io.netty.channel.ChannelPromise;
+import io.netty.util.AttributeKey;
+
+import java.net.SocketAddress;
+
+import dmg.cells.nucleus.CDC;
+
+
+/**
+ * This class wraps some ChannelDuplexHandler and ensures that, if the Netty
+ * Channel has a specific key then the value of this key is used as the CDC
+ * session.  This class is really a hack to work-around that CDC is a
+ * thread-local value that is not propagated when Netty schedules tasks outside
+ * of the event loop.  Such scheduled tasks current happen in the pool, as the
+ * response to a CloseRequest is delayed until after the mover has closed.
+ * There seems to be no easy way to propagate the CDC within Netty scheduled
+ * tasks.
+ */
+@Sharable
+public class ChannelCdcSessionHandlerWrapper implements ChannelOutboundHandler, ChannelInboundHandler
+{
+    public static final AttributeKey<String> SESSION = AttributeKey.newInstance("org.dcache.cells.cdc.session");
+
+    private final ChannelDuplexHandler _inner;
+
+    public ChannelCdcSessionHandlerWrapper(ChannelDuplexHandler inner)
+    {
+        _inner = inner;
+    }
+
+    /**
+     * Bind a particular session value to this channel.
+     */
+    public static void bindSessionToChannel(Channel channel, String session)
+    {
+        channel.attr(SESSION).set(session);
+    }
+
+    /*
+     * Decorate ChannelOutboundHandler methods.
+     */
+
+    @Override
+    public void bind(ChannelHandlerContext ctx, SocketAddress localAddress, ChannelPromise promise) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.bind(ctx, localAddress, promise);
+        }
+    }
+
+    @Override
+    public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress,
+            SocketAddress localAddress, ChannelPromise promise) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.connect(ctx, remoteAddress, localAddress, promise);
+        }
+    }
+
+    @Override
+    public void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.disconnect(ctx, promise);
+        }
+    }
+
+    @Override
+    public void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.close(ctx, promise);
+        }
+    }
+
+    @Override
+    public void deregister(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.deregister(ctx, promise);
+        }
+    }
+
+    @Override
+    public void read(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.read(ctx);
+        }
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.write(ctx, msg, promise);
+        }
+    }
+
+    @Override
+    public void flush(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.flush(ctx);
+        }
+    }
+
+    /*
+     * Decorate ChannelInboundHandler methods.
+     */
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.channelRegistered(ctx);
+        }
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.channelUnregistered(ctx);
+        }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.channelActive(ctx);
+        }
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.channelInactive(ctx);
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.channelRead(ctx, msg);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.channelReadComplete(ctx);
+        }
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.userEventTriggered(ctx, evt);
+        }
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.channelWritabilityChanged(ctx);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.exceptionCaught(ctx, cause);
+        }
+    }
+
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.handlerAdded(ctx);
+        }
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception
+    {
+        try (AutoCloseable oldCdc = withCdcSession(ctx)) {
+            _inner.handlerRemoved(ctx);
+        }
+    }
+
+    private AutoCloseable withCdcSession(ChannelHandlerContext ctx)
+    {
+        if (CDC.getSession() == null) {
+            String session = ctx.channel().attr(SESSION).get();
+            if (session != null) {
+                CDC captured = new CDC();
+                CDC.setSession(session);
+                return captured;
+            }
+        }
+        return () -> {};
+    }
+}

commit 137d2ed5657e68eec929e1ae892b9ae15a3cb0a4

xrootd: fix access logging when xrootd door is configured with HAproxy
Motivation:

Commit 5ecd79a82ad inadvertently broke access log support for HAproxy.

Modification:

Be sure to remove the wrapped ChannelHandler from the pipeline.

Result:

HAproxy logging for xrootd works as expected.

Target: master
Request: 5.0
Requires-notes: yes
Requires-book: no
Patch: https://rb.dcache.org/r/11549/
Acked-by: Albert Rossi
Acked-by: Vincent Garonne

modules/dcache-xrootd/src/main/java/org/dcache/xrootd/plugins/ProxyAccessLogHandler.java
@@ -19,6 +19,7 @@
 package org.dcache.xrootd.plugins;
 
 import com.google.common.net.HostAndPort;
+import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.haproxy.HAProxyCommand;
@@ -72,7 +73,7 @@ public class ProxyAccessLogHandler extends AccessLogHandler
                     log.add("socket.remote", remoteAddress);
                     log.add("socket.local", localAddress);
                     log.toLogger(logger);
-                    ctx.channel().pipeline().replace(this, null, handler);
+                    replaceWith(ctx, handler);
                 } else if (!Objects.equals(destinationAddress, localAddress.getAddress().getHostAddress())) {
                     /* The above check is a workaround for what looks like a bug in HAProxy - health checks
                      * should generate a LOCAL command, but it appears they do actually use PROXY.
@@ -86,11 +87,16 @@ public class ProxyAccessLogHandler extends AccessLogHandler
                             HostAndPort.fromParts(destinationAddress, proxyMessage.destinationPort()));
                     log.add("socket.local", localAddress);
                     log.toLogger(logger);
-                    ctx.channel().pipeline().replace(this, null, handler);
+                    replaceWith(ctx, handler);
                 }
             }
         }
         super.channelRead(ctx, msg);
+    }
+
+    protected void replaceWith(ChannelHandlerContext ctx, ChannelHandler handler)
+    {
+        ctx.channel().pipeline().replace(this, null, handler);
     }
 
     @Override

modules/dcache-xrootd/src/main/java/org/dcache/xrootd/plugins/ProxyAccessLogHandlerFactory.java
@@ -19,12 +19,22 @@
 package org.dcache.xrootd.plugins;
 
 import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
 
 import org.dcache.util.ChannelCdcSessionHandlerWrapper;
 
 public class ProxyAccessLogHandlerFactory extends AccessLogHandlerFactory
 {
-    private final ChannelHandler proxyAccessLogHandler = new ChannelCdcSessionHandlerWrapper(new ProxyAccessLogHandler(accessLogger, handler));
+    private final ChannelHandler proxyAccessLogHandler = new ChannelCdcSessionHandlerWrapper(
+                new ProxyAccessLogHandler(accessLogger, handler) {
+                    @Override
+                    protected void replaceWith(ChannelHandlerContext ctx, ChannelHandler handler)
+                    {
+                        ctx.pipeline().replace(ProxyAccessLogHandlerFactory.this.proxyAccessLogHandler,
+                                null, handler);
+                    }
+                }
+            );
 
     @Override
     public ChannelHandler createHandler()