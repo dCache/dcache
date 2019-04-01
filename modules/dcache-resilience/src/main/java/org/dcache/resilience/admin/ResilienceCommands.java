4b71475e85b5952d79575ed44bb09c222ff93d08
Albert Louis Rossi <arossi@fnal.gov>
2019-04-01 07:43:03 -0500

    resilience: update state on pool operations when restarted from admin command
    Motivation:
    
    The admin command 'pool ctrl'  provides for 'start' and 'shutdown'
    options which stop the processing of all pool operations.
    
    The 'start' option, however, neglects to update the state info
    on the pool operations when they are readded to the map and
    the map initialized.  This means that, barring state change
    updates from the PoolMonitor, these pools will appear
    perpetually uninitialized and will block any operation from
    running.
    
    Modification:
    
    Take the update code out of the MapInitializer and add it
    to the PoolOperationMap, where it is called both by the
    MapInitializer and the admin command.
    
    Result:
    
    Pool operations can successfully be restarted from the
    command line after they have been shutdown, without
    restarting resilience.
    
    Target: master
    Request: 5.0
    Request: 4.2
    Request: 4.1
    Request: 4.0
    Requires-notes: yes
    Requires-book: no
    Acked-by: Tigran

--- modules/dcache-resilience/src/main/java/org/dcache/resilience/admin/ResilienceCommands.java
+++ modules/dcache-resilience/src/main/java/org/dcache/resilience/admin/ResilienceCommands.java
@@ -1062,6 +1062,7 @@ public final class ResilienceCommands implements CellCommandListener {
                     new Thread(() -> {
                         poolOperationMap.loadPools();
                         poolOperationMap.initialize();
+                        poolOperationMap.updateInitialized();
                     }).start();
                     return "Consumer initialization and pool reload started.";
                 case SHUTDOWN:
