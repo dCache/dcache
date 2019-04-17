5b5d9510968cd2100d138e75d4cd61960fc14e3b
Marina Sahakyan <marina.sahakyan@desy.de>
2019-04-17 12:58:16 +0200

    alarms: sending alarms events in json format
    Motivation
    
    	Receiving alarm event in JSON format, makes info queryng easier.
    
    Target: master
    Request: 5.1
    Requires-notes: yes
    Requires-book: no
    Patch: https://rb.dcache.org/r/11704/
    Acked-by: Albert Rossi

---
+++ modules/dcache/src/main/java/org/dcache/alarms/logback/MarkerJsonLayout.java
@@ -0,0 +1,35 @@
+package org.dcache.alarms.logback;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.contrib.json.classic.JsonLayout;
+import org.dcache.alarms.LogEntry;
+
+import java.util.Map;
+
+
+public class MarkerJsonLayout extends JsonLayout {
+
+
+    public static final String CONTEXT_MARKER = "marker";
+
+
+    public MarkerJsonLayout() {
+        super();
+    }
+
+
+    @Override
+    protected void addCustomDataToJsonMap(Map<String, Object> map, ILoggingEvent event) {
+
+
+        LoggingEventConverter logginEventConv = new LoggingEventConverter();
+
+        LogEntry entry = logginEventConv.createEntryFromEvent(event);
+
+        map.put(CONTEXT_MARKER, entry);
+
+
+    }
+
+
+}
