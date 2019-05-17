dCache/dcache

commit 1d2f7ee1908afc77e8f068ea6b245ca93b3f944d

pool: add IO statistics to monitor storage performance
Motivation:

First, make IoStatistics generally available so that information about
the underlying storage system performance is available.  This has the
potential for providing useful insight into poor performance: whether
the problem is with the client/network delivering data slowly, or the
poor performance of the underlying storage device.  This monitoring may
also allow a more adaptive behaviour, such transfers based on
performance of the underlying performance.

Second, to demonstrate a generic idea on how new pool features may be
introduced cleanly, by allowing a RepositoryChannel to be optionally
cast to something implementation specific.

Modification:

Forwarding classes (ones that delegate operations to some other
instance) for ReplicaRecord and ReplicaStore are introduced.

Introduce specialist ReplicaStore and ReplicaRecord classes (based on
these forwarding classes) that allow optional injection of
IoStatisticsChannel: channels opened with the ENABLE_IO_STATISTICS open
option.  Without this flag, statistics are not gathered.

Finally, a handful of metrics are added to billing log files, both read
and write operations. These metrics are the mean instantaneous bandwidth
(data transferred / time taken, evaluated for each IO request), the
duration spent with at least one pending IO request, and the duration
spend with no pending IO request.

Result:

Billing can now log, for read and write operations, the mean bandwidth
of each IO request and time that the disk system was handling requests
and idling.  NB. the dCache configuration that describes the billing log
format must be updated before these values are actually recorded.

Target: master
Require-notes: yes
Require-book: yes
Patch: https://rb.dcache.org/r/10684/
Acked-by: Tigran Mkrtchyan

modules/dcache/src/main/java/org/dcache/pool/repository/ForwardingReplicaRecord.java
@@ -1,0 +1,133 @@
+/* dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2017 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.pool.repository;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.OpenOption;
+import java.util.Collection;
+import java.util.Set;
+
+import diskCacheV111.util.CacheException;
+import diskCacheV111.util.PnfsId;
+
+import org.dcache.vehicles.FileAttributes;
+
+/**
+ * A ReplicaRecord that forwards all activity to some delegate ReplicaRecord.
+ */
+public abstract class ForwardingReplicaRecord implements ReplicaRecord
+{
+    abstract protected ReplicaRecord delegate();
+
+    @Override
+    public PnfsId getPnfsId()
+    {
+        return delegate().getPnfsId();
+    }
+
+    @Override
+    public long getReplicaSize()
+    {
+        return delegate().getReplicaSize();
+    }
+
+    @Override
+    public FileAttributes getFileAttributes() throws CacheException
+    {
+        return delegate().getFileAttributes();
+    }
+
+    @Override
+    public ReplicaState getState()
+    {
+        return delegate().getState();
+    }
+
+    @Override
+    public URI getReplicaUri()
+    {
+        return delegate().getReplicaUri();
+    }
+
+    @Override
+    public RepositoryChannel openChannel(Set<? extends OpenOption> mode) throws IOException
+    {
+        return delegate().openChannel(mode);
+    }
+
+    @Override
+    public long getCreationTime()
+    {
+        return delegate().getCreationTime();
+    }
+
+    @Override
+    public long getLastAccessTime()
+    {
+        return delegate().getLastAccessTime();
+    }
+
+    @Override
+    public void setLastAccessTime(long time) throws CacheException
+    {
+        delegate().getLastAccessTime();
+    }
+
+    @Override
+    public int decrementLinkCount()
+    {
+        return delegate().decrementLinkCount();
+    }
+
+    @Override
+    public int incrementLinkCount()
+    {
+        return delegate().incrementLinkCount();
+    }
+
+    @Override
+    public int getLinkCount()
+    {
+        return delegate().getLinkCount();
+    }
+
+    @Override
+    public boolean isSticky()
+    {
+        return delegate().isSticky();
+    }
+
+    @Override
+    public Collection<StickyRecord> removeExpiredStickyFlags() throws CacheException
+    {
+        return delegate().removeExpiredStickyFlags();
+    }
+
+    @Override
+    public Collection<StickyRecord> stickyRecords()
+    {
+        return delegate().stickyRecords();
+    }
+
+    @Override
+    public <T> T update(Update<T> update) throws CacheException
+    {
+        return delegate().update(update);
+    }
+}

modules/dcache-vehicles/src/main/java/diskCacheV111/vehicles/MoverInfoMessage.java
@@ -1,5 +1,8 @@
 package diskCacheV111.vehicles;
 
+import java.time.Duration;
+import java.util.Optional;
+
 import diskCacheV111.util.PnfsId;
 
 import dmg.cells.nucleus.CellAddressCore;
@@ -13,6 +16,12 @@ public class MoverInfoMessage extends PnfsFileInfoMessage
     private boolean _fileCreated;
     private String _initiator = "<undefined>";
     private boolean _isP2p;
+    private double _meanReadBandwidth = Double.NaN;
+    private double _meanWriteBandwidth = Double.NaN;
+    private Duration _readActive;
+    private Duration _readIdle;
+    private Duration _writeActive;
+    private Duration _writeIdle;
 
     private static final long serialVersionUID = -7013160118909496211L;
     private String _transferPath;
@@ -87,6 +96,66 @@ public class MoverInfoMessage extends PnfsFileInfoMessage
         _transferPath = path;
     }
 
+    public void setMeanReadBandwidth(double value)
+    {
+        _meanReadBandwidth = value;
+    }
+
+    public double getMeanReadBandwidth()
+    {
+        return _meanReadBandwidth;
+    }
+
+    public void setMeanWriteBandwidth(double value)
+    {
+        _meanWriteBandwidth = value;
+    }
+
+    public double getMeanWriteBandwidth()
+    {
+        return _meanWriteBandwidth;
+    }
+
+    public void setReadIdle(Duration value)
+    {
+        _readIdle = value;
+    }
+
+    public Optional<Duration> getReadIdle()
+    {
+        return Optional.ofNullable(_readIdle);
+    }
+
+    public void setReadActive(Duration value)
+    {
+        _readActive = value;
+    }
+
+    public Optional<Duration> getReadActive()
+    {
+        return Optional.ofNullable(_readActive);
+    }
+
+    public void setWriteIdle(Duration value)
+    {
+        _writeIdle = value;
+    }
+
+    public Optional<Duration> getWriteIdle()
+    {
+        return Optional.ofNullable(_writeIdle);
+    }
+
+    public void setWriteActive(Duration value)
+    {
+        _writeActive = value;
+    }
+
+    public Optional<Duration> getWriteActive()
+    {
+        return Optional.ofNullable(_writeActive);
+    }
+
     @Override
     public String toString()
     {
@@ -98,6 +167,12 @@ public class MoverInfoMessage extends PnfsFileInfoMessage
                ", initiator='" + _initiator + '\'' +
                ", isP2p=" + _isP2p +
                ", transferPath='" + _transferPath + '\'' +
+               ", readBw='" + _meanReadBandwidth + '\'' +
+               ", writeBw='" + _meanWriteBandwidth + '\'' +
+               ", readIdle='" + _readIdle + '\'' +
+               ", readActive='" + _readActive + '\'' +
+               ", writeIdle='" + _writeIdle + '\'' +
+               ", writeActive='" + _writeActive + '\'' +
                "} " + super.toString();
     }
 

modules/dcache/src/main/java/org/dcache/pool/classic/DefaultPostTransferService.java
@@ -19,7 +19,6 @@ package org.dcache.pool.classic;
 
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import com.google.common.util.concurrent.Uninterruptibles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
@@ -44,6 +43,10 @@ import dmg.cells.nucleus.CellInfoProvider;
 import org.dcache.cells.CellStub;
 import org.dcache.pool.movers.Mover;
 import org.dcache.pool.repository.ReplicaDescriptor;
+import org.dcache.pool.statistics.DirectedIoStatistics;
+import org.dcache.pool.statistics.IoStatistics;
+import org.dcache.pool.statistics.IoStatisticsChannel;
+import org.dcache.pool.statistics.SnapshotStatistics;
 import org.dcache.util.CDCExecutorServiceDecorator;
 import org.dcache.util.FireAndForgetTask;
 import org.dcache.vehicles.FileAttributes;
@@ -134,6 +137,7 @@ public class DefaultPostTransferService extends AbstractCellComponent implements
     }
 
     public void sendBillingMessage(Mover<?> mover, long fileSize) {
+
         FileAttributes fileAttributes = mover.getFileAttributes();
 
         MoverInfoMessage info = new MoverInfoMessage(getCellAddress(), fileAttributes.getPnfsId());
@@ -150,8 +154,36 @@ public class DefaultPostTransferService extends AbstractCellComponent implements
         info.setBillingPath(mover.getBillingPath());
         info.setTransferPath(mover.getTransferPath());
 
-        _billing.notify(info);
+        MoverInfoMessage infoWithStats = mover.getChannel()
+                .flatMap(c -> c.optionallyAs(IoStatisticsChannel.class))
+                .map(c -> c.getStatistics())
+                .map(s -> updateIoStatistics(info, s))
+                .orElse(info);
+
+        _billing.notify(infoWithStats);
     }
+
+    private static MoverInfoMessage updateIoStatistics(MoverInfoMessage info, IoStatistics statistics)
+    {
+        DirectedIoStatistics reads = statistics.reads();
+        DirectedIoStatistics writes = statistics.writes();
+        SnapshotStatistics readStats = reads.statistics();
+        SnapshotStatistics writeStats = writes.statistics();
+
+        if (readStats.requestedBytes().getN() > 0) {
+            info.setMeanReadBandwidth(readStats.instantaneousBandwidth().getMean());
+            info.setReadActive(reads.active());
+            info.setReadIdle(reads.idle());
+        }
+
+        if (writeStats.requestedBytes().getN() > 0) {
+            info.setMeanWriteBandwidth(writeStats.instantaneousBandwidth().getMean());
+            info.setWriteActive(writes.active());
+            info.setWriteIdle(writes.idle());
+        }
+
+        return info;
+     }
 
     public void sendFinished(Mover<?> mover) {
         DoorTransferFinishedMessage finished =

modules/dcache/src/main/java/org/dcache/pool/movers/AbstractMover.java
@@ -33,6 +33,7 @@ import java.nio.file.StandardOpenOption;
 import java.nio.file.OpenOption;
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.Optional;
 import java.util.Set;
 
 import diskCacheV111.util.CacheException;
@@ -81,6 +82,7 @@ public abstract class AbstractMover<P extends ProtocolInfo, M extends AbstractMo
     protected volatile String _errorMessage = "";
     private final Set<ChecksumType> _checksumTypes;
     private volatile ChecksumChannel _checksumChannel;
+    private volatile Optional<RepositoryChannel> _channel = Optional.empty();
 
     public AbstractMover(ReplicaDescriptor handle, PoolIoFileMessage message, CellPath pathToDoor,
                          TransferService<M> transferService,
@@ -303,7 +305,16 @@ public abstract class AbstractMover<P extends ProtocolInfo, M extends AbstractMo
                     "File could not be opened; please check the file system: "
                     + messageOrClassName(e), e);
         }
+        if (!_channel.isPresent()) {
+            _channel = Optional.of(channel);
+        }
         return channel;
+    }
+
+    @Override
+    public Optional<RepositoryChannel> getChannel()
+    {
+        return _channel;
     }
 
     @Nonnull

modules/dcache/src/main/java/org/dcache/pool/movers/Mover.java
@@ -22,6 +22,7 @@ import javax.security.auth.Subject;
 
 import java.nio.channels.CompletionHandler;
 import java.nio.file.OpenOption;
+import java.util.Optional;
 import java.util.Set;
 
 import diskCacheV111.vehicles.ProtocolInfo;
@@ -30,6 +31,7 @@ import dmg.cells.nucleus.CellPath;
 
 import org.dcache.pool.classic.Cancellable;
 import org.dcache.pool.repository.ReplicaDescriptor;
+import org.dcache.pool.repository.RepositoryChannel;
 import org.dcache.util.Checksum;
 import org.dcache.util.ChecksumType;
 import org.dcache.vehicles.FileAttributes;
@@ -114,6 +116,13 @@ public interface Mover<T extends ProtocolInfo>
      * Provides a descriptor for the open repository entry of the file being transferred.
      */
     ReplicaDescriptor getIoHandle();
+
+    /**
+     * Provide the channel used for the transfer.  If no channel has been opened
+     * then the returned value is empty.
+     */
+    Optional<RepositoryChannel> getChannel();
+
 
     /**
      * Get set of options specifying how the file is opened. The READ

modules/dcache/src/main/java/org/dcache/pool/repository/ForwardingReplicaStore.java
@@ -1,0 +1,87 @@
+/* dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2017 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.pool.repository;
+
+import java.nio.file.OpenOption;
+import java.util.Set;
+
+import diskCacheV111.util.CacheException;
+import diskCacheV111.util.PnfsId;
+
+/**
+ * A ReplicaStore that delegates all operations to some delegate.
+ */
+public abstract class ForwardingReplicaStore implements ReplicaStore
+{
+    protected abstract ReplicaStore delegate();
+
+    @Override
+    public void init() throws CacheException
+    {
+        delegate().init();
+    }
+
+    @Override
+    public Set<diskCacheV111.util.PnfsId> index(IndexOption... options) throws CacheException
+    {
+        return delegate().index(options);
+    }
+
+    @Override
+    public ReplicaRecord get(PnfsId id) throws CacheException
+    {
+        return delegate().get(id);
+    }
+
+    @Override
+    public ReplicaRecord create(PnfsId id, Set<? extends OpenOption> flags)
+            throws DuplicateEntryException, CacheException
+    {
+        return delegate().create(id, flags);
+    }
+
+    @Override
+    public void remove(PnfsId id) throws CacheException
+    {
+        delegate().remove(id);
+    }
+
+    @Override
+    public boolean isOk()
+    {
+        return delegate().isOk();
+    }
+
+    @Override
+    public void close()
+    {
+        delegate().close();
+    }
+
+    @Override
+    public long getFreeSpace()
+    {
+        return delegate().getFreeSpace();
+    }
+
+    @Override
+    public long getTotalSpace()
+    {
+        return delegate().getTotalSpace();
+    }
+}

modules/dcache/src/main/java/org/dcache/pool/repository/ForwardingRepositoryChannel.java
@@ -23,6 +23,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.channels.WritableByteChannel;
+import java.util.Optional;
 
 /**
  * A {@link RepositoryChannel| implementation which forwards all its
@@ -125,4 +126,13 @@ public abstract class ForwardingRepositoryChannel implements RepositoryChannel {
         delegate().close();
     }
 
+    @Override
+    public <T> Optional<T> optionallyAs(Class<T> type)
+    {
+        if (type.isAssignableFrom(getClass())) {
+            return Optional.of(type.cast(this));
+        } else {
+            return delegate().optionallyAs(type);
+        }
+    }
 }

modules/dcache/src/main/java/org/dcache/pool/repository/RepositoryChannel.java
@@ -2,12 +2,14 @@ package org.dcache.pool.repository;
 
 import java.io.IOException;
 import java.io.SyncFailedException;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.ByteBuffer;
 import java.nio.channels.GatheringByteChannel;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.ScatteringByteChannel;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.channels.WritableByteChannel;
+import java.util.Optional;
 
 public interface RepositoryChannel
     extends GatheringByteChannel, ScatteringByteChannel, SeekableByteChannel
@@ -188,4 +190,14 @@ public interface RepositoryChannel
      */
     long transferFrom(ReadableByteChannel src, long position, long count)
         throws IOException;
+
+    /**
+     * Provide an optional interface that this channel might implement.  This
+     * allows the caller to interact with additional behaviour.
+     */
+    default <T> Optional<T> optionallyAs(Class<T> type)
+    {
+        T asType = type.isAssignableFrom(getClass()) ? type.cast(this) : null;
+        return Optional.ofNullable(asType);
+    }
 }

commit d2472e4edcb04724f3c4ff5a9a93d828c9a3c4f7

pool: fix storage of replica last access time
Motivation:

Commit 1d2f7ee1908afc77e8f068ea6b245ca93b3f944d introduced a regression
where changes to a file's last access time were lost.

Modification:

Fix typo in commit.

Result:

Updates to a replica's last access time are now stored.

Target: master
Request: 5.1
Request: 5.0
Request: 4.2
Request: 4.1
Requires-notes: yes
Requires-book: no
Closes: #4853
Patch: https://rb.dcache.org/r/11745/
Acked-by: Lea Morschel
Acked-by: Dmitry Litvintsev

modules/dcache/src/main/java/org/dcache/pool/repository/ForwardingReplicaRecord.java
@@ -86,7 +86,7 @@ public abstract class ForwardingReplicaRecord implements ReplicaRecord
     @Override
     public void setLastAccessTime(long time) throws CacheException
     {
-        delegate().getLastAccessTime();
+        delegate().setLastAccessTime(time);
     }
 
     @Override