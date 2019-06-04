2244b9a5ffa3772ed88636b0eaefeeb74b7c5e5e
Paul Millar <paul.millar@desy.de>
2019-06-04 11:01:20 +0200

    dcache: abstract common features of HTTP access logging
    Motivation:
    
    dCache currently does not log frontend activity in the access log file.
    With the increase of frontend usage, this omission is becoming more
    problematic.
    
    Modification:
    
    As a first step in adding support, this patch refactors the existing
    WebDAV logging into a generic logging base class and a concrete class
    that includes specific logging for the WebDAV door.
    
    A subsequent patch will add the frontend logging.
    
    Result:
    
    The order of fields logged in WebDAV access log entries has changed, but
    the presence (or absence) of fields has not changed.
    
    Target: master
    Requires-notes: yes
    Requires-book: no
    Patch: https://rb.dcache.org/r/11782/
    Acked-by: Albert Rossi

--- modules/dcache-webdav/src/main/java/org/dcache/webdav/LoggingHandler.java
+++ modules/dcache-webdav/src/main/java/org/dcache/webdav/LoggingHandler.java
@@ -1,119 +1,70 @@
 package org.dcache.webdav;
 
-import com.google.common.net.InetAddresses;
-import org.eclipse.jetty.http.HttpStatus;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Response;
-import org.eclipse.jetty.server.handler.HandlerWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.security.auth.Subject;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.security.cert.X509Certificate;
-
-import dmg.cells.nucleus.CDC;
-
+import org.dcache.http.AbstractLoggingHandler;
 import org.dcache.util.NetLoggerBuilder;
 import org.dcache.webdav.macaroons.MacaroonRequestHandler;
 import org.dcache.webdav.transfer.CopyFilter;
 
-import static org.dcache.http.AuthenticationHandler.DCACHE_SUBJECT_ATTRIBUTE;
 import static org.dcache.webdav.DcacheResourceFactory.TRANSACTION_ATTRIBUTE;
+import static org.dcache.util.NetLoggerBuilder.Level.WARN;
 
+/**
+ * WebDAV door specific logging.
+ */
+public class LoggingHandler extends AbstractLoggingHandler
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger("org.dcache.access.webdav");
 
-public class LoggingHandler extends HandlerWrapper {
-
-    private final Logger ACCESS_LOGGER =
-            LoggerFactory.getLogger("org.dcache.access.webdav");
-
-    private static final String X509_CERTIFICATE_ATTRIBUTE =
-            "javax.servlet.request.X509Certificate";
-
-    @Override
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        if (isStarted() && !baseRequest.isHandled()) {
-            super.handle(target, baseRequest, request, response);
-
-            NetLoggerBuilder log = new NetLoggerBuilder(logLevel(request, response),
-                    "org.dcache.webdav.request").omitNullValues();
-            log.add("session", CDC.getSession());
-            log.add("transaction", getTransaction(request));
-            log.add("request.method", request.getMethod());
-            log.add("request.url", request.getRequestURL());
-            log.add("request.macaroon-request", MacaroonRequestHandler.getMacaroonRequest(request));
-            log.add("response.code", response.getStatus());
-            log.add("response.reason", getReason(response));
-            log.add("response.macaroon-id", MacaroonRequestHandler.getMacaroonId(request));
-            log.add("location", response.getHeader("Location"));
-            InetAddress addr = InetAddresses.forString(request.getRemoteAddr());
-            log.add("socket.remote", new InetSocketAddress(addr, request.getRemotePort()));
-            log.add("user-agent", request.getHeader("User-Agent"));
-            log.add("tpc.credential", CopyFilter.getTpcCredential(request));
-            log.add("tpc.error", CopyFilter.getTpcError(request));
-            log.add("tpc.require-checksum", CopyFilter.getTpcRequireChecksumVerification(request));
-            log.add("tpc.source", CopyFilter.getTpcSource(request));
-            log.add("tpc.destination", CopyFilter.getTpcDestination(request));
-
-            log.add("user.dn", getCertificateName(request));
-            log.add("user.mapped", getSubject(request));
-            log.toLogger(ACCESS_LOGGER);
-        }
-    }
-
-    private static String getReason(HttpServletResponse response)
+    private static String getTransaction(HttpServletRequest request)
     {
-        if (response instanceof Response) {
-            return ((Response) response).getReason();
-        } else {
-            return HttpStatus.getMessage(response.getStatus());
-        }
-    }
-
-    private static NetLoggerBuilder.Level logLevel(HttpServletRequest request,
-            HttpServletResponse response)
-    {
-        int code = response.getStatus();
-        if (code >= 500) {
-            return NetLoggerBuilder.Level.ERROR;
-        } else if (code >= 400 || CopyFilter.getTpcError(request) != null) {
-            return NetLoggerBuilder.Level.WARN;
-        } else {
-            return NetLoggerBuilder.Level.INFO;
-        }
-    }
-
-    private static String getCertificateName(HttpServletRequest request) {
-
-        Object object = request.getAttribute(X509_CERTIFICATE_ATTRIBUTE);
-
-        if (object instanceof X509Certificate[]) {
-            X509Certificate[] chain = (X509Certificate[]) object;
-
-            if (chain.length >= 1) {
-                return chain[0].getSubjectX500Principal().getName();
-            }
-        }
-
-        return null;
-    }
-
-    private static String getTransaction(HttpServletRequest request) {
         Object object = request.getAttribute(TRANSACTION_ATTRIBUTE);
-
         return object == null ? null : String.valueOf(object);
     }
 
-    private static Subject getSubject(HttpServletRequest request) {
-        Object object = request.getAttribute(DCACHE_SUBJECT_ATTRIBUTE);
+    @Override
+    protected Logger accessLogger()
+    {
+        return LOGGER;
+    }
 
-        return (object instanceof Subject) ? (Subject) object : null;
+    @Override
+    protected String requestEventName()
+    {
+        return "org.dcache.webdav.request";
+    }
+
+    @Override
+    protected NetLoggerBuilder.Level logLevel(HttpServletRequest request,
+            HttpServletResponse response)
+    {
+        NetLoggerBuilder.Level level = super.logLevel(request, response);
+
+        if (level.ordinal() > WARN.ordinal() && CopyFilter.getTpcError(request) != null) {
+            level = WARN;
+        }
+
+        return level;
+    }
+
+    @Override
+    protected void describeOperation(NetLoggerBuilder log,
+            HttpServletRequest request, HttpServletResponse response)
+    {
+        super.describeOperation(log, request, response);
+
+        log.add("transaction", getTransaction(request));
+        log.add("request.macaroon-request", MacaroonRequestHandler.getMacaroonRequest(request));
+        log.add("response.macaroon-id", MacaroonRequestHandler.getMacaroonId(request));
+        log.add("tpc.credential", CopyFilter.getTpcCredential(request));
+        log.add("tpc.error", CopyFilter.getTpcError(request));
+        log.add("tpc.require-checksum", CopyFilter.getTpcRequireChecksumVerification(request));
+        log.add("tpc.source", CopyFilter.getTpcSource(request));
+        log.add("tpc.destination", CopyFilter.getTpcDestination(request));
     }
 }
---
+++ modules/dcache/src/main/java/org/dcache/http/AbstractLoggingHandler.java
@@ -0,0 +1,134 @@
+/* dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2019 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.http;
+
+import com.google.common.net.InetAddresses;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.server.handler.HandlerWrapper;
+import org.slf4j.Logger;
+
+import javax.security.auth.Subject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.security.cert.X509Certificate;
+
+import dmg.cells.nucleus.CDC;
+
+import org.dcache.util.NetLoggerBuilder;
+
+import static org.dcache.http.AuthenticationHandler.DCACHE_SUBJECT_ATTRIBUTE;
+
+/**
+ * This class act as a base logging class for logging servlet-based activity.
+ * It is expected that this class is subclassed to add protocol-specific
+ * logging.
+ */
+public abstract class AbstractLoggingHandler extends HandlerWrapper
+{
+    private static final String X509_CERTIFICATE_ATTRIBUTE =
+            "javax.servlet.request.X509Certificate";
+
+    /** The SLF4J Logger to which we send access log entries. */
+    protected abstract Logger accessLogger();
+
+    /** The name of the request events. */
+    protected abstract String requestEventName();
+
+    @Override
+    public void handle(String target, Request baseRequest,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        if (isStarted() && !baseRequest.isHandled()) {
+            super.handle(target, baseRequest, request, response);
+
+            NetLoggerBuilder.Level logLevel = logLevel(request, response);
+            NetLoggerBuilder log = new NetLoggerBuilder(logLevel, requestEventName())
+                    .omitNullValues();
+            describeOperation(log, request, response);
+            log.toLogger(accessLogger());
+        }
+    }
+
+    protected void describeOperation(NetLoggerBuilder log,
+            HttpServletRequest request, HttpServletResponse response)
+    {
+        log.add("session", CDC.getSession());
+        log.add("request.method", request.getMethod());
+        log.add("request.url", request.getRequestURL());
+        log.add("response.code", response.getStatus());
+        log.add("response.reason", getReason(response));
+        log.add("location", response.getHeader("Location"));
+        InetAddress addr = InetAddresses.forString(request.getRemoteAddr());
+        log.add("socket.remote", new InetSocketAddress(addr, request.getRemotePort()));
+        log.add("user-agent", request.getHeader("User-Agent"));
+
+        log.add("user.dn", getCertificateName(request));
+        log.add("user.mapped", getSubject(request));
+    }
+
+    private static String getReason(HttpServletResponse response)
+    {
+        if (response instanceof Response) {
+            return ((Response) response).getReason();
+        } else {
+            return HttpStatus.getMessage(response.getStatus());
+        }
+    }
+
+    protected NetLoggerBuilder.Level logLevel(HttpServletRequest request,
+            HttpServletResponse response)
+    {
+        int code = response.getStatus();
+        if (code >= 500) {
+            return NetLoggerBuilder.Level.ERROR;
+        } else if (code >= 400) {
+            return NetLoggerBuilder.Level.WARN;
+        } else {
+            return NetLoggerBuilder.Level.INFO;
+        }
+    }
+
+    private static String getCertificateName(HttpServletRequest request)
+    {
+        Object object = request.getAttribute(X509_CERTIFICATE_ATTRIBUTE);
+
+        if (object instanceof X509Certificate[]) {
+            X509Certificate[] chain = (X509Certificate[]) object;
+
+            if (chain.length >= 1) {
+                return chain[0].getSubjectX500Principal().getName();
+            }
+        }
+
+        return null;
+    }
+
+    private static Subject getSubject(HttpServletRequest request)
+    {
+        Object object = request.getAttribute(DCACHE_SUBJECT_ATTRIBUTE);
+        return (object instanceof Subject) ? (Subject) object : null;
+    }
+}
