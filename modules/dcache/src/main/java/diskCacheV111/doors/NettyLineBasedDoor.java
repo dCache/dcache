dCache/dcache

commit 388e6d2b6acb1c34e425a84d89f28e95b6aa44c7

dcap: fix premature close of kafka sender
Motivation:

Issue #4831 reported a stack-trace:

    08 May 2019 15:37:37 (DCap00-fndca4a-AAWIZk3V24A) \[door:DCap00-fndca4a-AAWIZk3V24A@dcap00-fndca4aDomain PnfsManager PnfsGetFileAttributes\] Uncaught exception in thread DCap00-fndca4a-io-2
    org.apache.kafka.common.KafkaException: Producer closed while send in progress
           at org.apache.kafka.clients.producer.KafkaProducer.doSend(KafkaProducer.java:864) ~[kafka-clients-2.1.0.jar:na]
           at org.apache.kafka.clients.producer.KafkaProducer.send(KafkaProducer.java:841) ~[kafka-clients-2.1.0.jar:na]
           at diskCacheV111.doors.DCapDoorInterpreterV3.sendAsynctoKafka(DCapDoorInterpreterV3.java:2553) ~[dcache-dcap-4.2.32-FNAL.jar:4.2.32-FNAL]
           at diskCacheV111.doors.DCapDoorInterpreterV3.postToBilling(DCapDoorInterpreterV3.java:2546) ~[dcache-dcap-4.2.32-FNAL.jar:4.2.32-FNAL]
           at diskCacheV111.doors.DCapDoorInterpreterV3.access$1500(DCapDoorInterpreterV3.java:104) ~[dcache-dcap-4.2.32-FNAL.jar:4.2.32-FNAL]
           at diskCacheV111.doors.DCapDoorInterpreterV3$SessionHandler.sendReply(DCapDoorInterpreterV3.java:875) ~[dcache-dcap-4.2.32-FNAL.jar:4.2.32-FNAL]
           at diskCacheV111.doors.DCapDoorInterpreterV3$SessionHandler.sendReply(DCapDoorInterpreterV3.java:856) ~[dcache-dcap-4.2.32-FNAL.jar:4.2.32-FNAL]
           at diskCacheV111.doors.DCapDoorInterpreterV3$PnfsSessionHandler.fileAttributesNotAvailable(DCapDoorInterpreterV3.java:1141) ~[dcache-dcap-4.2.32-FNAL.jar:4.2.32-FNAL]
           at diskCacheV111.doors.DCapDoorInterpreterV3$PnfsSessionHandler.pnfsGetFileAttributesArrived(DCapDoorInterpreterV3.java:1105) ~[dcache-dcap-4.2.32-FNAL.jar:4.2.32-FNAL]
           at diskCacheV111.doors.DCapDoorInterpreterV3.messageArrived(DCapDoorInterpreterV3.java:2527) ~[dcache-dcap-4.2.32-FNAL.jar:4.2.32-FNAL]
           at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method) ~[na:1.8.0_181]

modules/dcache/src/main/java/diskCacheV111/doors/NettyLineBasedDoor.java
@@ -243,6 +243,8 @@ public class NettyLineBasedDoor
     @Override
     public void stopped()
     {
+        interpreter.messagingClosed();
+
         channel.close().syncUninterruptibly();
 
         super.stopped();

modules/dcache-dcap/src/main/java/diskCacheV111/doors/DCapDoorInterpreterV3.java
@@ -2498,17 +2498,6 @@ public class DCapDoorInterpreterV3
     }
 
     public void close() {
-
-
-        /*The producer consists of a pool of buffer space that holds records that haven't yet been
-          transmitted to the server as well as a background I/O thread
-          that is responsible for turning these records into requests and transmitting them to the cluster.
-          Failure to close the producer after use will leak these resources. Hence we need to  close Kafka Producer
-         */
-        if (_settings.isKafkaEnabled()) {
-            _kafkaProducer.close();
-        }
-
         for(SessionHandler sh: _sessions.values()) {
             try {
                 sh.removeUs();
@@ -2518,6 +2507,18 @@ public class DCapDoorInterpreterV3
                  */
                 _log.error("failed to removed session: " + sh, e);
             }
+        }
+    }
+
+    protected void shutdownKafka() {
+        /*The producer consists of a pool of buffer space that holds records that haven't yet been
+          transmitted to the server as well as a background I/O thread
+          that is responsible for turning these records into requests and transmitting them to the cluster.
+          Failure to close the producer after use will leak these resources. Hence we need to  close Kafka Producer
+         */
+        if (_settings.isKafkaEnabled()) {
+            _log.debug("Shutting down kafka");
+            _kafkaProducer.close();
         }
     }
 

modules/dcache-dcap/src/main/java/diskCacheV111/doors/DcapLineBasedInterpreterAdapter.java
@@ -106,6 +106,12 @@ public class DcapLineBasedInterpreterAdapter
     }
 
     @Override
+    public void messagingClosed()
+    {
+        shutdownKafka();
+    }
+
+    @Override
     public void getInfo(PrintWriter pw)
     {
         pw.println("         User  : " + Subjects.getDisplayName(subject));

modules/dcache/src/main/java/diskCacheV111/doors/LineBasedDoor.java
@@ -210,6 +210,8 @@ public class LineBasedDoor
     @Override
     public void stopped()
     {
+        interpreter.messagingClosed();
+
         /* Closing the input stream will cause the FTP command
          * processing thread to shut down. In case the shutdown was
          * initiated by the FTP client, this will already have

modules/dcache/src/main/java/diskCacheV111/doors/LineBasedInterpreter.java
@@ -44,7 +44,19 @@ public interface LineBasedInterpreter
     void execute(String cmd) throws CommandExitException;
 
     /**
-     * Signals that the connection with the client is being terminated.
+     * Signals that the connection with the client is being terminated.  The
+     * implementation is still able to send and receive cell messages.
      */
     void shutdown();
+
+    /**
+     * Signals that this cell will receive no further cell messages.  Sending
+     * cell messages is also no longer supported.  The network connection to
+     * the client has not yet been closed, but the client might not accept any
+     * more data.
+     */
+    default void messagingClosed()
+    {
+        // do nothing.
+    }
 }

commit 5e1b8490c75c12d69d333fecf271d361337ac49e

ftp: avoid NPE on HA-Proxy probes
Motivation:

An HA-Proxy instance, if deployed, can probe endpoints to discover if
they are still alive.

The FTP door has an optimisation that detects such probes and does not
create the FTP command interpreter, since the FTP client (the HA-Proxy
instance) is calling on behalf of itself, and will not issue any FTP
requests.

Commit 388e6d2b6ac introduced a regression where such probes result in
the NullPointerException:

    java.lang.NullPointerException: null
        at diskCacheV111.doors.NettyLineBasedDoor.stopped(NettyLineBasedDoor.java:246)
        at dmg.cells.nucleus.CellAdapter.postRemoval(CellAdapter.java:765)
        at dmg.cells.nucleus.CellNucleus.shutdown(CellNucleus.java:1016)
        at dmg.cells.nucleus.CellGlue.lambda$doKill$6(CellGlue.java:451)
        at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)

Modification:

Check whether the interpreter was created before informing it will
receive no further cell messages.

Result:

A regression is fixed for FTP doors that are proxied by an HA-Proxy
instance.

Target: master
Request: 5.2
Request: 5.1
Request: 5.0
Request: 4.2
Requires-notes: yes
Requires-book: no
Patch: https://rb.dcache.org/r/11810/
Acked-by: Vincent Garonne

modules/dcache/src/main/java/diskCacheV111/doors/NettyLineBasedDoor.java
@@ -243,7 +243,9 @@ public class NettyLineBasedDoor
     @Override
     public void stopped()
     {
-        interpreter.messagingClosed();
+        if (interpreter != null) {
+            interpreter.messagingClosed();
+        }
 
         channel.close().syncUninterruptibly();
 