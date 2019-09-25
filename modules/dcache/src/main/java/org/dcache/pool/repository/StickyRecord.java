6a5c3dfc263e2fc38d89abe27c66e574a127ef27
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2019-09-25 11:04:22 +0200

    core: fix code formatting and javadoc of StickyRecord
    no code change
    
    Acked-by: Lea Morschel
    Target: master
    Requires-book: no
    Requires-notes: no

--- modules/dcache/src/main/java/org/dcache/pool/repository/StickyRecord.java
+++ modules/dcache/src/main/java/org/dcache/pool/repository/StickyRecord.java
@@ -10,65 +10,65 @@ public class StickyRecord implements Serializable {
     private static final DateFormat df = DateFormat.getInstance();
     public static final long NON_EXPIRING = -1;
 
-	private final String _owner;
-	private final long   _expire;
+    private final String _owner;
 
+    /**
+     * Timestamp in milliseconds since January 1, 1970 UTC specifying when this
+     * record will expire. The value -1 indicates that the record will never expire.
+     */
+    private final long _expire;
 
-	public StickyRecord(String owner, long expire)
-	{
-		_owner = owner;
-		_expire = expire;
-	}
+    /**
+     * Create a sticky record for given {@code owner} which will expire at a
+     * specified point in time. When the value of {@code expire} is -1, the record
+     * will never expire.
+     * @param owner The owner of this record.
+     * @param expire Timestamp in milliseconds since January 1, 1970 UTC.
+     */
+    public StickyRecord(String owner, long expire) {
+        _owner = owner;
+        _expire = expire;
+    }
 
-	public boolean isValid()
-	{
-		return isValidAt(System.currentTimeMillis());
-	}
+    public boolean isValid() {
+        return isValidAt(System.currentTimeMillis());
+    }
 
-	public boolean isValidAt(long time)
-	{
-		return isNonExpiring() || _expire > time;
-	}
+    public boolean isValidAt(long time) {
+        return isNonExpiring() || _expire > time;
+    }
 
-	public boolean isNonExpiring()
-	{
-	    return _expire == NON_EXPIRING;
-	}
+    public boolean isNonExpiring() {
+        return _expire == NON_EXPIRING;
+    }
 
-	public long expire()
-	{
-		return _expire;
-	}
+    public long expire() {
+        return _expire;
+    }
 
-	public String owner()
-	{
-		return _owner;
-	}
+    public String owner() {
+        return _owner;
+    }
 
-	@Override
-	public boolean equals(Object obj)
-	{
-		if( !(obj instanceof StickyRecord)) {
-		    return false;
-		}
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof StickyRecord)) {
+            return false;
+        }
 
-		StickyRecord other = (StickyRecord) obj;
+        StickyRecord other = (StickyRecord) obj;
 
-		return other.owner().equals(_owner) && other.expire() == _expire;
-	}
+        return other.owner().equals(_owner) && other.expire() == _expire;
+    }
 
+    @Override
+    public int hashCode() {
+        return _owner.hashCode();
+    }
 
-	@Override
-	public int hashCode()
-	{
-		return _owner.hashCode();
-	}
-
-
-	@Override
-	public synchronized String toString()
-	{
-	    String expiryLabel = isNonExpiring() ? "never expires" : "expires " + df.format(_expire);
-		return _owner + " : " + expiryLabel;
-	}
+    @Override
+    public synchronized String toString() {
+        String expiryLabel = isNonExpiring() ? "never expires" : "expires " + df.format(_expire);
+        return _owner + " : " + expiryLabel;
+    }
 }
