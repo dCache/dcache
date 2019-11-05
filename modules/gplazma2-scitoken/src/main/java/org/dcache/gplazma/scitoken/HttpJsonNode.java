dCache/dcache

commit f9226d3b19aa54af141e2a358330028779d63833

scitoken: do not use javax.activation.MimeType
Motivation:
The javax.activation.MimeType is deprecated in java-9 and removed in
java-11. It's time to move on.

Modification:
Use MediaType from guava.

Result:
Preparation for java 11 migration.

Acked-by: Paul Millar
Acked-by: Albert Rossi
Target: master
Require-book: no
Require-notes: no

modules/gplazma2-scitoken/src/main/java/org/dcache/gplazma/scitoken/HttpJsonNode.java
@@ -17,6 +17,7 @@
  */
 package org.dcache.gplazma.scitoken;
 
+import com.google.common.net.MediaType;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -27,9 +28,6 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.MissingNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.activation.MimeType;
-import javax.activation.MimeTypeParseException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -147,45 +145,22 @@ public class HttpJsonNode extends PreparationJsonNode
 
     private static Charset readCharset(HttpResponse response) throws IOException
     {
-        Charset charset = StandardCharsets.US_ASCII;
 
         Header contentType = response.getLastHeader("Content-Type");
         if (contentType == null || contentType.getValue() == null) {
-            return charset;
+            return StandardCharsets.US_ASCII;
         }
 
-        try {
-            MimeType type = new MimeType(contentType.getValue());
-            String baseType = type.getBaseType().toLowerCase();
-            switch (baseType) {
+        MediaType type = MediaType.parse(contentType.getValue());
+        String baseType = type.type().toLowerCase();
+        switch (baseType) {
             case "application/json":
             case "application/octet-stream": // basically "unknown"
                 break;
             default:
                 throw new IOException("Unexpected Content-Type: " + baseType);
-            }
-            String charsetParam = type.getParameter("charset");
-            if (charsetParam != null) {
-                switch (charsetParam.toLowerCase()) {
-                case "us-ascii":
-                    charset = StandardCharsets.US_ASCII;
-                    break;
-                case "utf-8":
-                case "utf8":
-                    charset = StandardCharsets.UTF_8;
-                    break;
-                case "iso-8859-1":
-                    charset = StandardCharsets.ISO_8859_1;
-                    break;
-                default:
-                    throw new IOException("Unknown charset " + charsetParam);
-                }
-            }
-        } catch (MimeTypeParseException e) {
-            throw new IOException("Failed to parse Content-Type value"
-                    + " \"" + contentType.getValue() + "\": " + e.getMessage());
         }
-        return charset;
+        return type.charset().or(StandardCharsets.US_ASCII);
     }
 
     @Override

commit 224ece37d0943d6ef961de48a19743b7c8f958c0

gplamza: fix SciToken fetching remote content
Motivation:

Commit f9226d3b broke SciToken support as it updated the MIME-Type
comparison so it checks only the type instead of the type and subtype.
For example, if the server replies with MIME type "application/json"
then the comparison is made with "application" only.

Modification:

Calculate the MIME-type without any parameters and use this value when
selecting the appropriate response.

Result:

An unreleased regression is fixed and SciToken support works again.

Target: master
Requires-notes: no
Requires-book: no
Closes: #5145
Patch: https://rb.dcache.org/r/12017
Acked-by: Albert Rossi

modules/gplazma2-scitoken/src/main/java/org/dcache/gplazma/scitoken/HttpJsonNode.java
@@ -152,7 +152,7 @@ public class HttpJsonNode extends PreparationJsonNode
         }
 
         MediaType type = MediaType.parse(contentType.getValue());
-        String baseType = type.type().toLowerCase();
+        String baseType = type.withoutParameters().toString().toLowerCase();
         switch (baseType) {
             case "application/json":
             case "application/octet-stream": // basically "unknown"