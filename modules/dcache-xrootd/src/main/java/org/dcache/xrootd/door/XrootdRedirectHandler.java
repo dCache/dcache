6c686376e8804db686b0d4c27cf183dedc41c670
Albert Louis Rossi <arossi@fnal.gov>
2019-11-18 09:47:42 -0600

    dcache-xrootd: refit checksum handling after xrootd4j bug fix
    Motivation:
    
    When multiple checksums are defined on a pool,
    xrdcp fails if -C is expressed as an argument.
    This is because it receives the first checksum
    in the natural ordering, because the xrootd
    value for cks.type is not being received.
    (see https://rb.dcache.org/r/12018 and
    GitHub https://github.com/dCache/dcache/issues/5147).
    
    Modfication:
    
    Make code depend on fixed releases of
    xrootd4j.  Adjust the construction
    of ChecksumInfo and also the use of
    getArgs->getPath on the pool.
    
    Result:
    
    xrdcp -C works again.
    
    Target: master
    Request: 5.2
    Request: 5.1
    Request: 5.0
    Request: 4.2
    Requires-notes: yes
    Requires-book: no
    Patch: https://rb.dcache.org/r/12019
    Depends-on: https://rb.dcache.org/r/12018
    Bug: #dCache/dcache/5147
    Acked-by: Paul

--- modules/dcache-xrootd/src/main/java/org/dcache/xrootd/door/XrootdRedirectHandler.java
+++ modules/dcache-xrootd/src/main/java/org/dcache/xrootd/door/XrootdRedirectHandler.java
@@ -33,7 +33,6 @@ import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.OptionalLong;
 import java.util.Set;
 import java.util.UUID;
@@ -60,7 +59,6 @@ import org.dcache.auth.attributes.RootDirectory;
 import org.dcache.cells.AbstractMessageCallback;
 import org.dcache.namespace.FileAttribute;
 import org.dcache.util.Checksum;
-import org.dcache.util.Checksums;
 import org.dcache.util.list.DirectoryEntry;
 import org.dcache.vehicles.PnfsListDirectoryMessage;
 import org.dcache.xrootd.core.XrootdException;
@@ -801,42 +799,15 @@ public class XrootdRedirectHandler extends ConcurrentXrootdRequestHandler
 
         case kXR_Qcksum:
             try {
-                ChecksumInfo info = new ChecksumInfo(msg.getArgs());
-                Set<Checksum> checksums = _door.getChecksums(createFullPath(info.getPath()),
+                ChecksumInfo info = new ChecksumInfo(msg.getPath(),
+                                                     msg.getOpaque());
+                Set<Checksum> checksums = _door.getChecksums(createFullPath(msg.getPath()),
                                                              msg.getSubject(),
                                                              _authz);
-                if (!checksums.isEmpty()) {
-                    Optional<String> type = info.getType();
-                    Optional<Checksum> result;
-
-                    if (type.isPresent()) {
-                        result = checksums.stream()
-                                          .filter((c) -> type.get()
-                                                             .equalsIgnoreCase(c.getType()
-                                                                                .getName()))
-                                          .findFirst();
-                    } else {
-                        result = Optional.of(Checksums.preferrredOrder().min(checksums));
-                    }
-
-                    /**
-                     * xrdcp expects lower case names for checksum algorithms
-                     * https://github.com/xrootd/xrootd/issues/459
-                     * TODO: remove toLowerCase() call when above issue is addressed
-                     */
-                    if (result.isPresent()) {
-                        Checksum checksum = result.get();
-                        return new QueryResponse(msg,checksum.getType().getName()
-                                                             .toLowerCase()
-                                                             + " "
-                                                             + checksum.getValue());
-                    }
-                }
+                return selectChecksum(info, checksums, msg);
             } catch (CacheException e) {
                 throw xrootdException(e);
             }
-            throw new XrootdException(kXR_Unsupported, "No checksum available for this file.");
-
         default:
             return unsupported(ctx, msg);
         }
