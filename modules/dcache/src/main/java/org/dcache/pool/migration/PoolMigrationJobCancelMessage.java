fe169d7b360cbfacf7a5a27f66b93060ab07fa87
Paul Millar <paul.millar@desy.de>
2019-11-20 23:25:22 +0100

    pool: improve messages when migration job is cancelled.
    Motivation:
    
    If a migration job is cancelled and this cancellation itself fails then
    the log entry can say simply "Failed to cancel task".  This is
    unsatisfactory for two reasons:
    
        1. it is unclear why cancelling the task failed.
    
        2. it is unclear what triggered the cancellation.
    
    Modification:
    
    Update the two places where "Failed to cancel task" is recorded to
    provide more information.
    
    Update the four places where cancellation can fail so message includes
    the reason for the cancellation.
    
    Introduce an explanation field when requesting a task is cancelled.
    Logging about cancellation problems include the reason why the task was
    cancelled.
    
    Include a reason for job cancellation in PoolMigrationJobCancelMessage,
    with Rebalancer updated to provide this information.
    
    Result:
    
    migration module provides better error messages about cancellation.
    
    Target: master
    Requires-notes: yes
    Requires-book: no
    Request: 6.0
    Request: 5.2
    Request: 5.1
    Request: 5.0
    Request: 4.2
    Closes: #5185
    Patch: https://rb.dcache.org/r/12064/
    Acked-by: Lea Morschel
    Acked-by: Albert Rossi

--- modules/dcache/src/main/java/org/dcache/pool/migration/PoolMigrationJobCancelMessage.java
+++ modules/dcache/src/main/java/org/dcache/pool/migration/PoolMigrationJobCancelMessage.java
@@ -1,18 +1,27 @@
 package org.dcache.pool.migration;
 
+import static java.util.Objects.requireNonNull;
+
 public class PoolMigrationJobCancelMessage extends PoolMigrationJobMessage
 {
     private static final long serialVersionUID = 7250151494463302009L;
     private final boolean _forced;
+    private final String _reason;
 
-    public PoolMigrationJobCancelMessage(String id, boolean forced)
+    public PoolMigrationJobCancelMessage(String id, boolean forced, String reason)
     {
         super(id);
         _forced = forced;
+        _reason = requireNonNull(reason);
     }
 
     public boolean isForced()
     {
         return _forced;
+    }
+
+    public String getReason()
+    {
+        return _reason;
     }
 }
