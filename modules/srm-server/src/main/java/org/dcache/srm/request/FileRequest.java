dCache/dcache

commit f817fa85aad6de00d4bbb345f3a169e0939f4f80

srm: refactor Job state-change
Motivation:

Currently, state change processing and notification is a bit of a mess
in SRM, with components tightly coupled.

Modification:

Refactor how the Job (and its subclasses) process a state change, using
a common method.

As an immediate benefit, the file-level state changes have a more
informative description when triggered by a request-level change.

Result:

SRM Job state-change processing is refactored, to form the basis for
further improvements.

Target: master
Requires-notes: no
Reqiures-book: no
Patch: https://rb.dcache.org/r/12061/
Acked-by: Tigran Mkrtchyan
Acked-by: Lea Morschel

modules/srm-server/src/main/java/org/dcache/srm/request/FileRequest.java
@@ -233,8 +233,11 @@ public abstract class FileRequest<R extends ContainerRequest> extends Job {
     }
 
     @Override
-    protected void stateChanged(State oldState)
+    protected void processStateChange(State newState, String description)
     {
+        super.processStateChange(newState, schedulerId);
+
+        // Notify container *after* this job's state is fully updated.
         try {
             getContainerRequest().fileRequestStateChanged(this);
         } catch (SRMInvalidRequestException ire) {

modules/srm-server/src/main/java/org/dcache/srm/request/BringOnlineFileRequest.java
@@ -340,10 +340,11 @@ public final class BringOnlineFileRequest extends FileRequest<BringOnlineRequest
     }
 
     @Override
-    protected void stateChanged(State oldState) {
-        State state = getState();
+    protected void processStateChange(State newState, String description)
+    {
+        State oldState = getState();
         LOGGER.debug("State changed from {} to {}", oldState, getState());
-        switch (state) {
+        switch (newState) {
         case READY:
             try {
                 getContainerRequest().resetRetryDeltaTime();
@@ -377,7 +378,8 @@ public final class BringOnlineFileRequest extends FileRequest<BringOnlineRequest
             }
             break;
         }
-        super.stateChanged(oldState);
+
+        super.processStateChange(newState, description);
     }
 
     @Override

modules/srm-server/src/main/java/org/dcache/srm/request/BringOnlineRequest.java
@@ -248,16 +248,16 @@ public final class BringOnlineRequest extends ContainerRequest<BringOnlineFileRe
     }
 
     @Override
-    protected void stateChanged(State oldState) {
-        State state = getState();
-        if (state.isFinal()) {
-            LOGGER.debug("Get request state changed to {}", state);
+    protected void processStateChange(State newState, String description)
+    {
+        if (newState.isFinal()) {
+            LOGGER.debug("Get request state changed to {}", newState);
             for (BringOnlineFileRequest fr: getFileRequests()) {
                 fr.wlock();
                 try {
                     if (!fr.getState().isFinal()) {
-                        LOGGER.debug("Changing fr#{} to {}", fr.getId(), state);
-                        fr.setState(state, "Changing file state because request state has changed.");
+                        LOGGER.debug("Changing fr#{} to {}", fr.getId(), newState);
+                        fr.setState(newState, "Request changed: " + description);
                     }
                 } catch (IllegalStateTransition e) {
                     LOGGER.error(e.getMessage());
@@ -266,6 +266,8 @@ public final class BringOnlineRequest extends ContainerRequest<BringOnlineFileRe
                 }
             }
         }
+
+        super.processStateChange(newState, description);
     }
 
     /**

modules/srm-server/src/main/java/org/dcache/srm/request/CopyFileRequest.java
@@ -596,11 +596,10 @@ public final class CopyFileRequest extends FileRequest<CopyRequest> implements D
     }
 
     @Override
-    protected void stateChanged(State oldState)
+    protected void processStateChange(State newState, String description)
     {
-        State state = getState();
-        if (state.isFinal()) {
-            if (getTransferId() != null && state != State.DONE) {
+        if (newState.isFinal()) {
+            if (getTransferId() != null && newState != State.DONE) {
                 getStorage().killRemoteTransfer(getTransferId());
                 String toFileId = getDestinationFileId();
                 if (toFileId != null) {
@@ -622,6 +621,8 @@ public final class CopyFileRequest extends FileRequest<CopyRequest> implements D
                 }
             }
         }
+
+        super.processStateChange(newState, description);
     }
 
     @Override

modules/srm-server/src/main/java/org/dcache/srm/request/CopyRequest.java
@@ -683,28 +683,29 @@ public final class CopyRequest extends ContainerRequest<CopyFileRequest>
     }
 
     @Override
-    protected void stateChanged(State oldState)
+    protected void processStateChange(State newState, String description)
     {
-        State state = getState();
-        if (state.isFinal()) {
+        if (newState.isFinal()) {
             TurlGetterPutter client = getRemoteTurlClient();
             if (client != null) {
                 LOGGER.debug("copyRequest TURL-fetching client is non null, stopping");
                 client.stop();
             }
-            LOGGER.debug("copy request state changed to {}", state);
+            LOGGER.debug("copy request state changed to {}", newState);
             for (CopyFileRequest request : getFileRequests()) {
                 try {
                     State frState = request.getState();
-                    if (!(frState.isFinal())) {
-                        LOGGER.debug("changing fr#{} to {}", request.getId(), state);
-                        request.setState(state, "Request now " + state);
+                    if (!frState.isFinal()) {
+                        LOGGER.debug("changing fr#{} to {}", request.getId(), newState);
+                        request.setState(newState, "Request changed: " + description);
                     }
                 } catch (IllegalStateTransition ist) {
                     LOGGER.error("Illegal State Transition : {}", ist.getMessage());
                 }
             }
         }
+
+        super.processStateChange(newState, description);
     }
 
     @Override

modules/srm-server/src/main/java/org/dcache/srm/request/GetFileRequest.java
@@ -363,10 +363,11 @@ public final class GetFileRequest extends FileRequest<GetRequest> {
     }
 
     @Override
-    protected void stateChanged(State oldState) {
-        State state = getState();
-        LOGGER.debug("State changed from {} to {}", oldState, getState());
-        switch (state) {
+    protected void processStateChange(State newState, String description)
+    {
+        State oldState = getState();
+        LOGGER.debug("State changed from {} to {}", oldState, newState);
+        switch (newState) {
         case READY:
             try {
                 getContainerRequest().resetRetryDeltaTime();
@@ -401,7 +402,7 @@ public final class GetFileRequest extends FileRequest<GetRequest> {
             }
         }
 
-        super.stateChanged(oldState);
+        super.processStateChange(newState, description);
     }
 
     private void computeTurl() throws SRMException

modules/srm-server/src/main/java/org/dcache/srm/request/GetRequest.java
@@ -227,18 +227,17 @@ public final class GetRequest extends ContainerRequest<GetFileRequest> {
     }
 
     @Override
-    protected void stateChanged(State oldState) {
-        State state = getState();
-        if(state.isFinal()) {
-            LOGGER.debug("Get request state changed to {}.", state);
+    protected void processStateChange(State newState, String description)
+    {
+        if (newState.isFinal()) {
+            LOGGER.debug("Get request state changed to {}.", newState);
             for (GetFileRequest request : getFileRequests()) {
                 request.wlock();
                 try {
                     State fr_state = request.getState();
-                    if(!fr_state.isFinal())
-                    {
-                        LOGGER.debug("Changing fr#{} to {}.", request.getId(), state);
-                        request.setState(state, "Changing file state because request state has changed.");
+                    if (!fr_state.isFinal()) {
+                        LOGGER.debug("Changing fr#{} to {}.", request.getId(), newState);
+                        request.setState(newState, "Request changed: " + description);
                     }
                 } catch (IllegalStateTransition ist) {
                     LOGGER.error(ist.getMessage());
@@ -246,9 +245,9 @@ public final class GetRequest extends ContainerRequest<GetFileRequest> {
                     request.wunlock();
                 }
             }
-
         }
 
+        super.processStateChange(newState, description);
     }
 
     public String[] getProtocols() {

modules/srm-server/src/main/java/org/dcache/srm/request/Job.java
@@ -72,6 +72,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.transaction.TransactionException;
 
 import javax.annotation.Nonnull;
+import javax.annotation.concurrent.GuardedBy;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -98,6 +99,7 @@ import org.dcache.srm.v2_2.TStatusCode;
 import org.dcache.util.TimeUtils;
 import org.dcache.util.TimeUtils.TimeUnitFormat;
 
+import static com.google.common.base.Preconditions.checkState;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 
@@ -285,22 +287,31 @@ public abstract class Job  {
                         "Illegal state transition from " + this.state + " to " + newState,
                         this.state, newState);
             }
-            State oldState = this.state;
-            this.state = newState;
-            lastStateTransitionTime = System.currentTimeMillis();
-
-            jobHistory.add( new JobHistory(nextLong(),newState,description,lastStateTransitionTime));
-
-            notifySchedulerOfStateChange(oldState, newState);
-
-            if (!newState.isFinal() && schedulerId == null) {
-                throw new IllegalStateTransition("Scheduler ID is null");
-            }
-            stateChanged(oldState);
+            processStateChange(newState, description);
             saveJob(state == State.RQUEUED);
         } finally {
             wunlock();
         }
+    }
+
+    /**
+     * Job-internal processing of a state change. Subclasses should override
+     * this method to do any internal processing.  When this method returns,
+     * the Job object is fully updated based on this state change.
+     */
+    @GuardedBy("lock")
+    protected void processStateChange(State newState, String description)
+    {
+        State oldState = this.state;
+        this.state = newState;
+        lastStateTransitionTime = System.currentTimeMillis();
+
+        jobHistory.add( new JobHistory(nextLong(),newState,description,lastStateTransitionTime));
+
+        notifySchedulerOfStateChange(oldState, newState);
+
+        checkState(newState.isFinal() || schedulerId != null,
+                "Scheduler ID is null");
     }
 
     private boolean isValidTransition(State currentState, State newState)
@@ -462,11 +473,6 @@ public abstract class Job  {
     }
 
     public abstract void run() throws SRMException, IllegalStateTransition;
-
-    //implementation should not block in this method
-    // this method should make sure that the job is saved in the
-    // job's storage (instance of Jon.JobStorage (possibly in a database )
-    protected abstract void stateChanged(State oldState);
 
     public TStatusCode getStatusCode() {
         rlock();

commit d06082415d3c95b0ed9e3c713d019f8a607cc15c

srmmanager: fix logged state transition description
Motivation:

Commit f817fa85 introduced a bug where, in some cases, file-level state
transitions were erronously logged with their scheduler ID as the
description/reason for the transition.

Modification:

Use the correct, descriptive text to indicate why a file-level job is
changing state.

Result:

An unreleased regression is fixed.

Target: master
Requires-notes: no
Requires-book: no
Patch: https://rb.dcache.org/r/12081/
Acked-by: Lea Morschel

modules/srm-server/src/main/java/org/dcache/srm/request/FileRequest.java
@@ -235,7 +235,7 @@ public abstract class FileRequest<R extends ContainerRequest> extends Job {
     @Override
     protected void processStateChange(State newState, String description)
     {
-        super.processStateChange(newState, schedulerId);
+        super.processStateChange(newState, description);
 
         // Notify container *after* this job's state is fully updated.
         try {