2231b12e451aaa80240eb54861a965c2468d3a99
Albert Louis Rossi <arossi@fnal.gov>
2020-05-26 07:33:06 -0500

    dcache-xrootd: make 'tried'/excluded hosts an optional feature
    Motivation:
    
    In https://rb.dcache.org/r/11955/
    master@261e78639548adadfaea38827c47efe92e1a2f41
    we introduced support for the xrootd cgi "tried=<host>,".
    Theoretically, this was in order to avoid, under
    an xrootd redirect, reasking for a pool that was
    experiencing some IO or disk failure on the node.
    
    Unfortunately, the xrootd client does not reserve
    this option only for IO or server failures, but
    will use it in case connectivity to the redirected
    endpoint fails.
    
    In this case, dCache does not behave in a friendly
    manner, because the PoolManager suspends access
    to the file (to all future connections/requests).
    
    We noticed this happening particularly during
    TLS testing.  The xrootd TPC client will retry
    against the source door if the pool it is
    trying to connect to does not support TLS but
    it thinks it should.
    
    Modification:
    
    We now introduce an option to enable this
    support, which is off by default.
    
    In the case of activation, the adminstrators
    are urged to make sure that all pools and doors
    that may become a TPC source for an 'xroots'
    (TLS) transfer have at least OPTIONAL capability
    for TLS.
    
    Result:
    
    Avoid locking up files unnecessarily.
    
    Target: master
    Request: 6.1
    Requires-notes: yes
    Requires-book: no
    Patch: https://rb.dcache.org/r/12387/
    Acked-by: Dmitry
    Acked-by: Lea

--- modules/dcache-xrootd/src/main/java/org/dcache/xrootd/door/XrootdRedirectHandler.java
+++ modules/dcache-xrootd/src/main/java/org/dcache/xrootd/door/XrootdRedirectHandler.java
@@ -31,6 +31,7 @@ import java.net.SocketAddress;
 import java.nio.channels.ClosedChannelException;
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.EnumSet;
 import java.util.HashMap;
@@ -260,7 +261,6 @@ public class XrootdRedirectHandler extends ConcurrentXrootdRequestHandler
         InetSocketAddress remoteAddress = getSourceAddress();
         SessionInfo sessionInfo = sessionInfo();
 
-
         Map<String,String> opaque;
 
         try {
@@ -311,11 +311,8 @@ public class XrootdRedirectHandler extends ConcurrentXrootdRequestHandler
             }
 
             _log.info("OPAQUE : {}", opaque);
-            Set<String> triedHosts
-                            = Arrays.stream(Strings.nullToEmpty(opaque.get("tried"))
-                                                   .split(","))
-                                    .collect(Collectors.toSet());
-            _log.info("TRIED : {}", triedHosts);
+            Set<String> triedHosts = extractTriedHosts(opaque);
+
             UUID uuid = UUID.randomUUID();
             opaque.put(UUID_PREFIX, uuid.toString());
             /*
@@ -623,6 +620,25 @@ public class XrootdRedirectHandler extends ConcurrentXrootdRequestHandler
         String error = String.format("Request metadata is invalid: %s: %s, %s.",
                                      req, fsPath, remoteHost);
         throw new CacheException(CacheException.THIRD_PARTY_TRANSFER_FAILED, error);
+    }
+
+    private Set<String> extractTriedHosts(Map<String, String> opaque)
+        throws XrootdException
+    {
+        String tried = Strings.emptyToNull(opaque.get("tried"));
+        if (tried == null) {
+            return Collections.EMPTY_SET;
+        } else if (!_door.isTriedHostsEnabled()) {
+            throw new XrootdException(kXR_InvalidRequest,
+                                      "tried hosts option not supported.");
+        }
+
+        Set<String> triedHosts
+                        = Arrays.stream(tried.split(","))
+                                .collect(Collectors.toSet());
+        _log.info("TRIED : {}", triedHosts);
+
+        return triedHosts;
     }
 
     private String getTpcClientId(XrootdSession session) {
