a1d8b9ababc9d7d174f304e9fbf776c7960c7220
Paul Millar <paul.millar@desy.de>
2020-07-02 19:56:05 +0200

    gplazma: automate filtering of principals
    Motivation:
    
    Reduce the risk of gPlazma leaking principals that then cause
    compatibility issues when upgrading or downgrading dCache.
    
    Modification:
    
    Add library to handle searching for principals based on their
    annotation.  With the current configuration, the scan takes some 200 ms
    on my laptop.
    
    Exceptions are allowed for principals that are not defined within dCache
    and so cannot be tagged with an annotation.  Currently the only
    exception is KerberosPrincipal.
    
    Add support for filtering door-supplied principals.  This should avoid
    problems where a door (mistakenly) sends principals that should come
    from gPlazma plugins.
    
    Replace existing ad-hoc filtering with annotation-based filtering, where
    only principals that have been tagged as suitable for dCache are
    allowed.
    
    Result:
    
    No user- or admin observable changes.
    
    dCache should now be more robust in maintaining compatibility with pool
    nodes running older versions of dCache and sites should be able to
    downgrade without problems.
    
    Target: master
    Requires-notes: no
    Requires-book: no
    Patch: https://rb.dcache.org/r/12446/
    Acked-by: Tigran Mkrtchyan

--- modules/dcache-gplazma/src/main/java/org/dcache/auth/Gplazma2LoginStrategy.java
+++ modules/dcache-gplazma/src/main/java/org/dcache/auth/Gplazma2LoginStrategy.java
@@ -1,12 +1,19 @@
 package org.dcache.auth;
 
+import com.google.common.base.Stopwatch;
 import com.google.common.base.Strings;
+import com.google.common.collect.Sets;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 
 import javax.security.auth.Subject;
+import javax.security.auth.kerberos.KerberosPrincipal;
 
 import java.io.File;
 import java.security.Principal;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
@@ -41,17 +48,44 @@ import org.dcache.gplazma.monitor.RecordingLoginMonitor;
 import org.dcache.util.Args;
 
 /**
- * A LoginStrategy that wraps a org.dcache.gplazma.GPlazma
- *
+ * A LoginStrategy that delegates login requests to an instance of
+ * org.dcache.gplazma.GPlazma.
  */
 public class Gplazma2LoginStrategy
     implements LoginStrategy, EnvironmentAware, CellCommandListener
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Gplazma2LoginStrategy.class);
+
+    // These are principals defined outside of dCache.
+    private static final Set<Class<?>> EXTERNAL_AUTHENTICATION_INPUT = Set.of(KerberosPrincipal.class);
+    private static final Set<Class<?>> EXTERNAL_AUTHENTICATION_OUTPUT = Set.of();
+
+    /** Principal classes allowed as input to gPlazma. */
+    private static final Set<Class<?>> AUTHENTICATION_INPUT;
+
+    /** Principal classes allowed in the response from gPlazma. */
+    private static final Set<Class<?>> AUTHENTICATION_OUTPUT;
+
     private String _configurationFile;
     private GPlazma _gplazma;
     private Map<String,Object> _environment = Collections.emptyMap();
     private PluginFactory _factory;
     private Function<FsPath, PrefixRestriction> _createPrefixRestriction;
+
+    static {
+        Stopwatch reflectionTimer = Stopwatch.createStarted();
+        Reflections principalPackages = new Reflections("org.dcache.auth",
+                "org.globus.gsi.gssapi.jaas");
+        AUTHENTICATION_INPUT = principalPackages.getTypesAnnotatedWith(AuthenticationInput.class);
+        AUTHENTICATION_INPUT.addAll(EXTERNAL_AUTHENTICATION_INPUT);
+        LOGGER.debug("AUTHENTICATION_INPUT: {}", AUTHENTICATION_INPUT);
+
+        AUTHENTICATION_OUTPUT = principalPackages.getTypesAnnotatedWith(AuthenticationOutput.class);
+        AUTHENTICATION_OUTPUT.addAll(EXTERNAL_AUTHENTICATION_OUTPUT);
+        LOGGER.debug("AUTHENTICATION_OUTPUT: {}", AUTHENTICATION_OUTPUT);
+
+        LOGGER.debug("gPlazma2 principal scanning completed: {}", reflectionTimer);
+    }
 
     @Required
     public void setConfigurationFile(String configurationFile)
@@ -135,25 +169,41 @@ public class Gplazma2LoginStrategy
                 .map(_createPrefixRestriction)
                 .forEach(loginAttributes::add);
 
-        // Filter IGTFStatusPrincipal and IGTFPolicyPrincipal until no longer
-        // need backwards compatibility with dCache v3.0 pools
-        Subject replyUser = new Subject();
-        Subject loggedInUser = gPlazmaLoginReply.getSubject();
-        replyUser.getPublicCredentials().addAll(loggedInUser.getPublicCredentials());
-        replyUser.getPrivateCredentials().addAll(loggedInUser.getPrivateCredentials());
-        Set<Principal> replyPrincipals = replyUser.getPrincipals();
-        loggedInUser.getPrincipals().stream()
-                .filter(p -> !(p instanceof IGTFStatusPrincipal))
-                .filter(p -> !(p instanceof IGTFPolicyPrincipal))
-                .forEach(replyPrincipals::add);
+        Subject replyUser = filterPrincipals(gPlazmaLoginReply.getSubject(),
+                AUTHENTICATION_OUTPUT, "LoginReply");
+
         return new LoginReply(replyUser, loginAttributes);
+    }
+
+    private Subject filterPrincipals(Subject in, Collection<Class<?>> allowed,
+            String name)
+    {
+        Set<Principal> inPrincipals = in.getPrincipals();
+
+        Set<Principal> outPrincipals = inPrincipals.stream()
+                .filter(p -> allowed.contains(p.getClass()))
+                .collect(Collectors.toSet());
+
+        if (outPrincipals.size() == inPrincipals.size()) {
+            return in;
+        }
+
+        LOGGER.debug("Filtered principals in {}: {}", name,
+                Sets.difference(inPrincipals, outPrincipals));
+
+        return new Subject(false, outPrincipals, in.getPublicCredentials(),
+                in.getPrivateCredentials());
     }
 
     @Override
     public LoginReply login(Subject subject) throws CacheException
     {
+        Subject filteredSubject = filterPrincipals(subject, AUTHENTICATION_INPUT,
+                "login subject");
+
         try {
-            return convertLoginReply(_gplazma.login(subject));
+            org.dcache.gplazma.LoginReply reply = _gplazma.login(filteredSubject);
+            return convertLoginReply(reply);
         } catch (AuthenticationException e) {
             // We deliberately hide the reason why the login failed from the
             // rest of dCache.  This is to prevent a brute-force attack
