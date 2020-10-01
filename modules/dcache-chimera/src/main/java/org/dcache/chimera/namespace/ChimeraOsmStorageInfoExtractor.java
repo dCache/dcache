baece52065d04e9009f052c75b2dde8fecdd0320
Paul Millar <paul.millar@desy.de>
2020-10-01 11:02:58 +0200

    pnfsmanager: drop support for LEGACY new files in ChimeraOsmStorageInfoExtractor
    Motivation:
    
    The LEGACY file state was added in 2016 with dCache v3.1.  Support for
    LEGACY in ChimeraOsmStorageInfoExtractor is to allow continued support
    for files that a client is (still) trying to upload after a pre-v3.1
    dCache was upgraded to v3.1 or later.
    
    After about four years, I think we can drop this LEGACY support in
    favour of simpler code.
    
    Modification:
    
    Drop the backwards compatibility support in
    ChimeraOsmStorageInfoExtractor for files in LEGACY state.
    
    The patch also restructures the code slightly, hopefully making it
    easier to read and maintain.
    
    Result:
    
    Drop support for files where the upload was initiated with a version of
    dCache v3.1 and that the client is still trying to upload the file.
    
    Target: master
    Requires-notes: yes
    Requires-book: no
    Patch: https://rb.dcache.org/r/12595/
    Acked-by: Tigran Mkrtchyan

--- modules/dcache-chimera/src/main/java/org/dcache/chimera/namespace/ChimeraOsmStorageInfoExtractor.java
+++ modules/dcache-chimera/src/main/java/org/dcache/chimera/namespace/ChimeraOsmStorageInfoExtractor.java
@@ -1,10 +1,13 @@
 package org.dcache.chimera.namespace;
 
 import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import diskCacheV111.util.AccessLatency;
@@ -21,7 +24,9 @@ import org.dcache.chimera.posix.Stat;
 import org.dcache.chimera.store.InodeStorageInformation;
 
 
-public class ChimeraOsmStorageInfoExtractor extends ChimeraHsmStorageInfoExtractor {
+public class ChimeraOsmStorageInfoExtractor extends ChimeraHsmStorageInfoExtractor
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChimeraOsmStorageInfoExtractor.class);
 
     public ChimeraOsmStorageInfoExtractor(AccessLatency defaultAL,
                                           RetentionPolicy defaultRP) {
@@ -29,44 +34,43 @@ public class ChimeraOsmStorageInfoExtractor extends ChimeraHsmStorageInfoExtract
     }
 
     @Override
-    public StorageInfo getFileStorageInfo(ExtendedInode inode) throws CacheException {
-
-        OSMStorageInfo info;
-
+    public StorageInfo getFileStorageInfo(ExtendedInode inode)
+            throws CacheException {
         try {
             Stat stat = inode.statCache();
 
-            boolean isNew = stat.getState() == FileState.CREATED || stat.getState() == FileState.LEGACY && stat.getSize() == 0 && !inode.getLevel(2).exists();
-
-            if (!isNew) {
-                ImmutableList<String> locations = inode.getLocations(StorageGenericLocation.TAPE);
-
-                if (locations.isEmpty()) {
-                    info = (OSMStorageInfo) getDirStorageInfo(inode);
-                } else {
-                    InodeStorageInformation inodeStorageInfo = inode.getStorageInfo();
-
-                    info = new OSMStorageInfo(inodeStorageInfo.storageGroup(),
-                            inodeStorageInfo.storageSubGroup());
-
-                    for (String location : locations) {
-                        try {
-                            info.addLocation(new URI(location));
-                        } catch (URISyntaxException e) {
-                            // bad URI
-                        }
-                    }
-                }
-            } else {
-                info = (OSMStorageInfo) getDirStorageInfo(inode);
+            if (stat.getState() == FileState.CREATED) {
+                StorageInfo info = getDirStorageInfo(inode);
+                info.setIsNew(true);
+                return info;
             }
 
-            info.setIsNew(isNew);
+            List<String> tapeLocations = inode.getLocations(StorageGenericLocation.TAPE);
 
+            if (tapeLocations.isEmpty()) {
+                StorageInfo info = getDirStorageInfo(inode);
+                info.setIsNew(false);
+                return info;
+            }
+
+            InodeStorageInformation inodeStorageInfo = inode.getStorageInfo();
+
+            StorageInfo info = new OSMStorageInfo(inodeStorageInfo.storageGroup(),
+                    inodeStorageInfo.storageSubGroup());
+
+            for (String location : tapeLocations) {
+                try {
+                    info.addLocation(new URI(location));
+                } catch (URISyntaxException e) {
+                    LOGGER.debug("Ignoring bad tape location {}: {}", location, e.toString());
+                }
+            }
+
+            info.setIsNew(false);
+            return info;
         } catch (ChimeraFsException e) {
             throw new CacheException(e.getMessage());
         }
-        return info;
     }
 
     @Override
