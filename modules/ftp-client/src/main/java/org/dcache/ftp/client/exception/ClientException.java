243978fece4268b40b109380d220ce7a453fe39d
Paul Millar <paul.millar@desy.de>
2020-10-13 20:08:22 +0200

    ftp-client: remove confusing field member
    Motivation:
    
    SpotBugs identified a field-member that is not used but obscures a
    field-member in the superclass with the same name.  At best, this is
    redundant but may cause confusion.
    
    SpotBugs Category: CORRECTNESS, Type: MF_CLASS_MASKS_FIELD
    
    Modification:
    
    Remove rogue field-member
    
    Result:
    
    No admin or user observable changes.
    
    Target: master
    Requires-notes: no
    Requires-book: no
    Patch: https://rb.dcache.org/r/12620/
    Acked-by: Lea Morschel

--- modules/ftp-client/src/main/java/org/dcache/ftp/client/exception/ClientException.java
+++ modules/ftp-client/src/main/java/org/dcache/ftp/client/exception/ClientException.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2006 University of Chicago
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -73,8 +73,6 @@ public class ClientException extends FTPException
         else return "";
     }
 
-
-    protected int code = UNSPECIFIED;
 
     public ClientException(int code, String message)
     {
