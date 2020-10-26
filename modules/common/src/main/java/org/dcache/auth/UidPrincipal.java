dCache/dcache

commit b4fe9166f4937c7898136f06aac2ffe30f3608a1

common: annotate principals based on their role during login.
Motivation:

In general, principals have one of three roles:

  a. they are discovered by the door and sent to gPlazma, aiding
     the login process;

  b. they are added by gPlazma plugins but are not used in dCache,
     carrying information between gPlazma plugins;

  c. they are added by gPlazma plugins and are used by dCache.

The principals delivered by gPlazma to dCache (category c.) must be very
stable as they are serialised and may be deserialised by older versions
of dCache.  There are two main use-case for this: pool nodes running
older versions of dCache and sites downgrading dCache.  In both cases,
older versions of dCache must be able to deserialise category c.
principals.

At the same time, gPlazma modules should be free to add whichever
principals to support enhanced authentication.  New authentication
mechanisms can result in new category a. and category b. principals.

Currently, there is no systematic handling of these principals: just
some ad-hoc rules that try to protect against some serialisation
problems.  This has proved ineffective, with sites experiencing problems
when downgrading.

Modification:

Add annotation to identify category a. principals (@AuthenticationInput)
and category c. principals (@AuthenticationOutput).  Any principal that
has no annotation is assumed to be a category b, principal.

Note: Origin is a somewhat special case.  It is both supplied to gPlazma
and some doors (e.g., WebDAV door) require that it is provided in the
LoginReply.  Therefore it is annotated both as @AuthenticationInput and
@AuthenticationOutput.

Result:

No user- or admin visible changes to dCache.

Target: master
Requires-notes: no
Requires-book: no
Patch: https://rb.dcache.org/r/12445/
Acked-by: Tigran Mkrtchyan

modules/common/src/main/java/org/dcache/auth/UidPrincipal.java
@@ -12,6 +12,7 @@ import java.security.Principal;
  * @see LoginUidPrincipal
  * @since 2.1
  */
+@AuthenticationOutput
 public class UidPrincipal implements Principal, Serializable
 {
     private static final long serialVersionUID = 1489893133915358418L;

modules/common/src/main/java/org/dcache/auth/AuthenticationInput.java
@@ -1,6 +1,6 @@
 /* dCache - http://www.dcache.org/
  *
- * Copyright (C) 2017 Deutsches Elektronen-Synchrotron
+ * Copyright (C) 2020 Deutsches Elektronen-Synchrotron
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -17,36 +17,18 @@
  */
 package org.dcache.auth;
 
-import java.io.Serializable;
-import java.security.Principal;
-
-import static java.util.Objects.requireNonNull;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
- * Identifies the Macaroon used to authorise the activity.
- * @since 3.2
+ * A tagging annotation that indicates the class provides input material to
+ * support the login process.  This is typically material that the door has
+ * discovered about the client and represented using principals.
  */
-@AuthenticationOutput
-public class MacaroonPrincipal implements Principal, Serializable
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AuthenticationInput
 {
-    private static final long serialVersionUID = 1L;
-
-    private final String _identifier;
-
-    public MacaroonPrincipal(String id)
-    {
-        _identifier = requireNonNull(id);
-    }
-
-    @Override
-    public String getName()
-    {
-        return _identifier;
-    }
-
-    @Override
-    public String toString()
-    {
-        return getClass().getSimpleName() + "[" + getName() + "]";
-    }
 }

modules/common/src/main/java/org/dcache/auth/AuthenticationOutput.java
@@ -1,0 +1,49 @@
+/* dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2020 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A tagging annotation that indicates the class provides information about the
+ * logged in user that is available to dCache.  Such annotated objects are
+ * subject to Java serialisation.  Changes must be forward- and backward
+ * compatible.
+ * <p>
+ * Forwards compatible means that older versions of dCache must be able to
+ * deserialise principals that were serialised by a newer version of dCache.
+ * This is needed as the site may downgrade (e.g., to recover quickly from some
+ * issue), or sites are running pool nodes with older versions of dCache.
+ * <p>
+ * Backwards compatible means that newer versions of dCache must be able to
+ * deserialise principals that were serialised by an older version of dCache.
+ * This is needed because login results are stored by some dCache components
+ * (e.g., diskCacheV111.srm.dcache.PersistentLoginUserManager).
+ * <p>
+ * These requirements are easiest to achieve if a class annotated
+ * AuthenticationOutput is never modified.  Therefore, care must be taken
+ * before annotating any principal with {@literal @AuthenticationOutput}
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AuthenticationOutput
+{
+}

modules/common/src/main/java/org/dcache/auth/DesiredRole.java
@@ -25,6 +25,7 @@ import java.security.Principal;
  * This principal in no way specifies whether the user is entitled to have
  * this role.
  */
+@AuthenticationInput
 public class DesiredRole implements Principal, Serializable
 {
     private static final long serialVersionUID = 1L;

modules/common/src/main/java/org/dcache/auth/EmailAddressPrincipal.java
@@ -9,6 +9,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 /**
  * @since 2.16
  */
+@AuthenticationOutput
 public class EmailAddressPrincipal implements Principal, Serializable
 {
     private static final long serialVersionUID = -5091924321331479809L;

modules/common/src/main/java/org/dcache/auth/FQANPrincipal.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
  * implement the Principal and Serializable interfaces.
  * @since 2.1
  */
+@AuthenticationOutput
 public class FQANPrincipal implements GroupPrincipal, Serializable
 {
     private static final long serialVersionUID = -4242349585261079835L;

modules/common/src/main/java/org/dcache/auth/FullNamePrincipal.java
@@ -10,6 +10,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 /**
  * @since 2.16
  */
+@AuthenticationOutput
 public class FullNamePrincipal implements Principal, Serializable
 {
     private static final long serialVersionUID = 1L;

modules/common/src/main/java/org/dcache/auth/GidPrincipal.java
@@ -18,6 +18,7 @@ import java.security.Principal;
  * @see LoginGidPrincipal
  * @since 2.1
  */
+@AuthenticationOutput
 public class GidPrincipal implements GroupPrincipal, Serializable
 {
     private static final long serialVersionUID = 7812225739755920892L;

commit 267d1c2f8c78f7f2e7958ec021ca2be5daa99ca1

common-auth: mark UidPrincipal as In/Out principal
fixes regression introduced in b4fe9166f4

Motivation:
As UidPrincipal used by NFS to 'login' when a user has more
than 16 secondary groups it have to be marked as AuthenticationInput as
well as AuthenticationOutput principal.

Modification:
mark UidPrincipal as In/Out principal

Result:
fixed regression introduced by limiting UidPrincipal to be only an
output principal.

Acked-by: Paul Millar
Target: master, 6.2
Require-book: no
Require-notes: yes

modules/common/src/main/java/org/dcache/auth/UidPrincipal.java
@@ -13,6 +13,7 @@ import java.security.Principal;
  * @since 2.1
  */
 @AuthenticationOutput
+@AuthenticationInput
 public class UidPrincipal implements Principal, Serializable
 {
     private static final long serialVersionUID = 1489893133915358418L;