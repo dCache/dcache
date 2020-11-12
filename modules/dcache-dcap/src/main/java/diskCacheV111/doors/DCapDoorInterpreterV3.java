d5572406f64bba3134505f709a588533c39ca7f2
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2020-11-12 11:36:25 +0100

    dcap: always set Subject when talking to PnfsManager
    EGI-SVG-2020-16939
    
    Motivation:
    The door should always set user subject when talks to PnfsManager
    (unless all permission checks done locally)
    
    Modification:
    Update PnfsSessionHandler#doLogin to set always set the subject
    used by PnfsHandler
    
    Result:
    PnfsManager can perform full permission check
    
    target: master, 7.0, 6.2, 6.1, 6.0, 5.2
    Requires-notes: yes
    Requires-book: no
    (cherry picked from commit 731146d91362c3b31c826e119e5a73e11de7070e)
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- modules/dcache-dcap/src/main/java/diskCacheV111/doors/DCapDoorInterpreterV3.java
+++ modules/dcache-dcap/src/main/java/diskCacheV111/doors/DCapDoorInterpreterV3.java
@@ -1036,10 +1036,8 @@ public class DCapDoorInterpreterV3
         {
             super.doLogin();
             _pnfs = new PnfsHandler(_cell, _settings.getPnfsManager());
-            if (_isUrl || _settings.isAuthorizationRequired()) {
-                _pnfs.setSubject(_subject);
-                _pnfs.setRestriction(_authz);
-            }
+            _pnfs.setSubject(_subject);
+            _pnfs.setRestriction(_authz);
         }
 
         @Override
