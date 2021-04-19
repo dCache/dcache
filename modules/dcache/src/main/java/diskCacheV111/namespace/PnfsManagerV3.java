dCache/dcache

commit 3dfed7e8b0f92db740946fad92ced8d7dbaf6071

dcache-core: add file attribute notification when AL/RP changes
Motivation:

As a first step toward integrating resilience more
fully into QoS management, PnfsManager needs to
send it a notification as to when access latency
or retention policy has been modified.

Modification:

Add a new topic relay for sending/forwarding
a PnfsSetFileAttributes message when it
contains access latency or retention policy.
Add a new post-processing method for
handling this and notifying if either
or both are set.

Result:

Other services can subscribe via dCache
messaging to this topic to receive updates.

Target: master
Requires-book: no
Requires-notes: no
Patch: https://rb.dcache.org/r/12022
Acked-by: Dmitry
Acked-by: Lea

modules/dcache/src/main/java/diskCacheV111/namespace/PnfsManagerV3.java
@@ -28,6 +28,7 @@ import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
@@ -37,7 +38,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-import diskCacheV111.namespace.NameSpaceProvider.Link;
+import diskCacheV111.util.AccessLatency;
 import diskCacheV111.util.CacheException;
 import diskCacheV111.util.FileNotFoundCacheException;
 import diskCacheV111.util.FsPath;
@@ -46,6 +47,7 @@ import diskCacheV111.util.MissingResourceCacheException;
 import diskCacheV111.util.NotDirCacheException;
 import diskCacheV111.util.PermissionDeniedCacheException;
 import diskCacheV111.util.PnfsId;
+import diskCacheV111.util.RetentionPolicy;
 import diskCacheV111.vehicles.DoorCancelledUploadNotificationMessage;
 import diskCacheV111.vehicles.Message;
 import diskCacheV111.vehicles.PnfsAddCacheLocationMessage;
@@ -110,8 +112,6 @@ import static org.dcache.acl.enums.AccessType.*;
 import static org.dcache.auth.Subjects.ROOT;
 import static org.dcache.auth.attributes.Activity.*;
 import static org.dcache.namespace.FileAttribute.*;
-import static org.dcache.namespace.FileType.DIR;
-import static org.dcache.namespace.FileType.REGULAR;
 
 public class PnfsManagerV3
     extends AbstractCellComponent
@@ -176,6 +176,12 @@ public class PnfsManagerV3
 
     private CellPath _cacheModificationRelay;
 
+    /**
+     * These involve changes
+     * to access latency and retention policy.
+     */
+    private CellPath _attributesRelay;
+
     private PermissionHandler _permissionHandler;
     private NameSpaceProvider _nameSpaceProvider;
 
@@ -236,6 +242,15 @@ public class PnfsManagerV3
             Strings.isNullOrEmpty(path) ? null : new CellPath(path);
         _log.info("CacheModificationRelay = {}",
                   (_cacheModificationRelay == null) ? "NONE" : _cacheModificationRelay.toString());
+    }
+
+    @Required
+    public void setFileAttributesRelay(String path)
+    {
+        _attributesRelay =
+                        Strings.isNullOrEmpty(path) ? null : new CellPath(path);
+        _log.info("attributesRelay = {}",
+                  (_attributesRelay == null) ? "NONE" : _attributesRelay.toString());
     }
 
     @Required
@@ -1750,6 +1765,13 @@ public class PnfsManagerV3
 
     private void postProcessMessage(CellMessage envelope, PnfsMessage message)
     {
+        if (_attributesRelay != null &&
+                        message instanceof PnfsSetFileAttributes &&
+                        message.getReturnCode() == 0) {
+            postProcessSetFileAttributes((PnfsSetFileAttributes)message);
+        } /* fall through because PnfsSetFileAttributes
+             is also postprocessed for locations */
+
         if (message instanceof PoolFileFlushedMessage && message.getReturnCode() == 0) {
             postProcessFlush(envelope, (PoolFileFlushedMessage) message);
         } else if (_cacheModificationRelay != null && message.getReturnCode() == 0) {
@@ -1757,6 +1779,21 @@ public class PnfsManagerV3
         } else if (message.getReplyRequired()) {
             envelope.revertDirection();
             sendMessage(envelope);
+        }
+    }
+
+    private void postProcessSetFileAttributes(PnfsSetFileAttributes message)
+    {
+        FileAttributes attributes = message.getFileAttributes();
+        Optional<AccessLatency> al = attributes.getAccessLatencyIfPresent();
+        Optional<RetentionPolicy> rp = attributes.getRetentionPolicyIfPresent();
+        if (al.isPresent() || rp. isPresent()) {
+            attributes = new FileAttributes();
+            attributes.setAccessLatency(al.orElse(null));
+            attributes.setRetentionPolicy(rp.orElse(null));
+            sendMessage(new CellMessage(_attributesRelay,
+                                        new PnfsSetFileAttributes(message.getPnfsId(),
+                                                                  attributes)));
         }
     }
 

modules/dcache-chimera/src/main/resources/diskCacheV111/namespace/pnfsmanager-chimera.xml
@@ -30,6 +30,7 @@
       <property name="threads" value="${pnfsmanager.limits.threads}"/>
       <property name="listThreads" value="${pnfsmanager.limits.list-threads}"/>
       <property name="cacheModificationRelay" value="${pnfsmanager.destination.cache-notification}"/>
+      <property name="fileAttributesRelay" value="${pnfsmanager.destination.file-attributes-notification}"/>
       <property name="logSlowThreshold" value="${pnfsmanager.limits.log-slow-threshold}"/>
       <property name="folding" value="${pnfsmanager.enable.folding}"/>
       <property name="directoryListLimit" value="${pnfsmanager.limits.list-chunk-size}"/>

skel/share/defaults/pnfsmanager.properties
@@ -198,6 +198,9 @@ pnfsmanager.inotify-generation.message-batch-size = \
 # Cell address to which to send cache location change notifications
 pnfsmanager.destination.cache-notification = CacheLocationTopic
 
+# Cell address to which to send QoS change notifications
+pnfsmanager.destination.file-attributes-notification = FileAttributesTopic
+
 # Cell address of space manager
 pnfsmanager.service.spacemanager = ${dcache.service.spacemanager}
 

skel/share/services/pnfsmanager.batch
@@ -12,6 +12,7 @@ check -strong pnfsmanager.default-access-latency
 check pnfsmanager.destination.flush-notification
 check pnfsmanager.destination.cache-notification
 check pnfsmanager.destination.cancel-upload-notification
+check pnfsmanager.destination.file-attributes-notification
 check -strong pnfsmanager.limits.threads
 check -strong pnfsmanager.limits.list-threads
 check -strong pnfsmanager.limits.list-chunk-size

commit fd7d355200848d30aab210e3a8a24ec77a8e66f0

pnfsmanager: fix NPE regression from 3dfed7e8b0
Motivation:

Commit 3dfed7e8b0 introduced a regression because it assumes that a
successful entry creation will have a non-null set of file attributes.

This is not true if a symbolic link is created, in which case there is
an NPE.

Modification:

Update code to check if value is non-null.

Result:

A regression is fixed the prevents creating a symbolic link.

Target: master
Request: 7.0
Request: 6.2
Request: 6.1
Requires-notes: yes
Requires-book: no
Patch: https://rb.dcache.org/r/12985/
Acked-by: Dmitry Litvintsev
Acked-by: Lea Morschel
Acked-by: Albert Rossi

modules/dcache/src/main/java/diskCacheV111/namespace/PnfsManagerV3.java
@@ -2026,6 +2026,9 @@ public class PnfsManagerV3
     private void postProcessSetFileAttributes(PnfsSetFileAttributes message)
     {
         FileAttributes attributes = message.getFileAttributes();
+        if (attributes == null) {
+            return;
+        }
         Optional<AccessLatency> al = attributes.getAccessLatencyIfPresent();
         Optional<RetentionPolicy> rp = attributes.getRetentionPolicyIfPresent();
         if (al.isPresent() || rp. isPresent()) {