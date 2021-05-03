34313248ff0db3c604a4f78fb8b4760ec5c4bff5
Paul Millar <paul.millar@desy.de>
2021-05-03 12:43:11 +0200

    webdav: use Jetty's built-in support for CORS
    Motivation:
    
    Issue #5883 is the latest of several bug-reports that come from dCache's
    custom CORS support.  In this case, the underlying problem is that the
    WebDAV door passes on CORS preflight requests to the down-stream servlet
    (Milton or frontend), in order to allow it to include additional
    headers.
    
    Since CORS requests are always unauthenticated, passing requests on to
    the downstream servlet will only work if the resource is public, or
    there is special code within those downstream serlvets to detect and
    support CORS requests.
    
    The precise problem described in issue #5883 comes from attempting to
    access a protected resource in dCacheView, which uses WebDAV for the
    actual data transfer.  The work-around in Milton to handle such requests
    is broken.
    
    Modification:
    
    This patch does two things:
    
    a) It hides CORS pre-flight requests from any downstream handlers.  All
       resources now receive a standard set of CORS headers.
    
    b) It removes the dCache-custom implementation of CORS in favour of
       Jetty's implementation: CrossOriginFilter.
    
    Unfortunately, as Jetty implemnets CORS as a Filter (not a Handler), we
    can't use Spring to create an instance.  Instead, this patch keeps the
    existing class (CrossOriginResourceSharingHandler), but now this acts
    both as a simple bridge between Handler and Filter and is responsible
    for instantiating and configuring CrossOriginFilter.
    
    Result:
    
    dCacheView is able to view and download files when authenticated and
    when the WebDAV door proxies the transfer.
    
    Target: master
    Requires-notes: yes
    Requires-book: no
    Request: 7.1
    Request: 7.0
    Request: 6.2
    Request: 6.1
    Request: 6.0
    Request: 5.2
    Closes: #5883
    Patch: https://rb.dcache.org/r/13017/
    Acked-by: Tigran Mkrtchyan

--- modules/dcache-webdav/src/main/java/org/dcache/webdav/DcacheResourceHandlerHelper.java
+++ modules/dcache-webdav/src/main/java/org/dcache/webdav/DcacheResourceHandlerHelper.java
@@ -1,6 +1,5 @@
 package org.dcache.webdav;
 
-import com.google.common.collect.ImmutableList;
 import io.milton.http.AuthenticationService;
 import io.milton.http.HandlerHelper;
 import io.milton.http.HttpManager;
@@ -15,8 +14,6 @@ import io.milton.http.exceptions.NotAuthorizedException;
 import io.milton.http.http11.Http11ResponseHandler;
 import io.milton.resource.Resource;
 
-import java.util.List;
-
 import org.dcache.webdav.transfer.CopyFilter;
 
 /**
@@ -25,11 +22,6 @@ import org.dcache.webdav.transfer.CopyFilter;
  */
 public class DcacheResourceHandlerHelper extends ResourceHandlerHelper
 {
-    // REVISIT can we generate a dummy DcacheFileResource and use that instead?
-    private static final List<String> OPTIONS_ALLOWED_METHODS_FOR_MISSING_ENTITY
-            = ImmutableList.of("GET", "HEAD", "PROPFIND", "OPTIONS", "DELETE",
-                    "MOVE", "GET", "HEAD", "PROPPATCH");
-
     public DcacheResourceHandlerHelper(HandlerHelper handlerHelper,
             UrlAdapter urlAdapter, Http11ResponseHandler responseHandler,
             AuthenticationService authenticationService)
@@ -49,23 +41,6 @@ public class DcacheResourceHandlerHelper extends ResourceHandlerHelper
             Resource resource = manager.getResourceFactory().getResource(host, url);
             /* Bypass check to see if file exists: our CopyFilter will handle the request */
             handler.processResource(manager, request, response, resource);
-        } else if (request.getMethod() == Request.Method.OPTIONS) {
-            Resource resource;
-            try {
-                resource = manager.getResourceFactory().getResource(host, url);
-            } catch (WebDavException e) {
-                // Treat all errors as if there was no such file.  We should
-                // always provide a successful response to a CORS request.
-                resource = null;
-            }
-            if (resource == null) {
-                // Milton ResourceHandlerHelper returns 404 for OPTIONS request
-                // targeting non-existing entity.  This breaks CORS uploads.
-                getResponseHandler().respondWithOptions(resource, response,
-                        request, OPTIONS_ALLOWED_METHODS_FOR_MISSING_ENTITY);
-            } else {
-                super.process(manager, request, response, handler);
-            }
         } else {
             super.process(manager, request, response, handler);
         }
