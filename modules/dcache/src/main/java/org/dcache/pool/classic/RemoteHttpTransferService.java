ac3a3e48fc39c14b953e1fed171976528da6f143
Paul Millar <paul.millar@desy.de>
2021-06-04 16:40:58 +0200

    pool: http-tpc add support for multiple trust stores
    Motivation:
    
    When dCache is acting as a client, it needs to decide whether or not to
    trust the remote server's certificate.  Currently this uses the same
    trust anchors as checking a client's (i.e., user's) certificates, which
    is an OpenSSL-style directory typically populated with the IGTF trust
    anchors.
    
    However, dCache pools may wish to trust a wider range of certificate
    authorities: the standard IGTF CAs and some additional ones.
    
    Modification:
    
    Add a X509TrustManager implementation that aggragates multiple "inner"
    X509TrustManager with the semantics that a certificate is trusted if any
    one of the inner X509TrustManager instances trusts the certificate.
    
    Update dCache to interpret the existing CA path as a colon-separated
    list of path elements (similar to the PATH environment variable).  Each
    path element is added as a trust anchor store.  If the path is a
    directory then the OpenSSL format is assumed, otherwise a single-file
    containing PEM files is assumed.
    
    Result:
    
    It is now possible to configure dCache to support multiple trust stores
    for HTTP-TPC.
    
    Target: master
    Request: 7.1
    See: #5927
    Requires-notes: yes
    Requires-book: no
    Patch: https://rb.dcache.org/r/13065
    Acked-by: Tigran Mkrtchyan

--- modules/dcache/src/main/java/org/dcache/pool/classic/RemoteHttpTransferService.java
+++ modules/dcache/src/main/java/org/dcache/pool/classic/RemoteHttpTransferService.java
@@ -17,14 +17,23 @@
  */
 package org.dcache.pool.classic;
 
+import com.google.common.base.Splitter;
 import eu.emi.security.authn.x509.OCSPParametes;
 import eu.emi.security.authn.x509.ProxySupport;
 import eu.emi.security.authn.x509.RevocationParameters;
-import eu.emi.security.authn.x509.X509CertChainValidator;
+import eu.emi.security.authn.x509.helpers.ssl.SSLTrustManager;
 import eu.emi.security.authn.x509.impl.OpensslCertChainValidator;
 import eu.emi.security.authn.x509.impl.ValidatorParams;
 
-import java.io.IOException;
+import javax.annotation.PostConstruct;
+import javax.net.ssl.X509TrustManager;
+
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import diskCacheV111.util.CacheException;
 import diskCacheV111.vehicles.ProtocolInfo;
@@ -34,19 +43,21 @@ import diskCacheV111.vehicles.RemoteHttpsDataTransferProtocolInfo;
 import org.dcache.pool.movers.MoverProtocol;
 import org.dcache.pool.movers.RemoteHttpDataTransferProtocol;
 import org.dcache.pool.movers.RemoteHttpsDataTransferProtocol;
-
-import static org.dcache.util.Files.checkDirectory;
+import org.dcache.security.trust.AggregateX509TrustManager;
 
 public class RemoteHttpTransferService extends SecureRemoteTransferService
 {
-    private OpensslCertChainValidator validator;
+    private final List<Runnable> onShutdownTasks = new ArrayList<>();
+
+    private X509TrustManager trustManager;
 
     @Override
     protected MoverProtocol createMoverProtocol(ProtocolInfo info) throws Exception
     {
         MoverProtocol moverProtocol;
         if (info instanceof RemoteHttpsDataTransferProtocolInfo) {
-            moverProtocol = new RemoteHttpsDataTransferProtocol(getCellEndpoint(), getValidator(), secureRandom);
+            moverProtocol = new RemoteHttpsDataTransferProtocol(getCellEndpoint(),
+                    trustManager, secureRandom);
         } else if (info instanceof RemoteHttpDataTransferProtocolInfo) {
             moverProtocol = new RemoteHttpDataTransferProtocol(getCellEndpoint());
         } else {
@@ -56,28 +67,35 @@ public class RemoteHttpTransferService extends SecureRemoteTransferService
         return moverProtocol;
     }
 
+    @PostConstruct
+    public void init()
+    {
+        FileSystem defaultFileSystem = FileSystems.getDefault();
+
+        var trustManagers = Splitter.on(':').omitEmptyStrings().splitToList(getCertificateAuthorityPath()).stream()
+                .map(defaultFileSystem::getPath)
+                .map(this::buildTrustManager)
+                .collect(Collectors.toList());
+
+        trustManager = new AggregateX509TrustManager(trustManagers);
+    }
+
+    private X509TrustManager buildTrustManager(Path path)
+    {
+        var ocspParameters = new OCSPParametes(getOcspCheckingMode());
+        var revocationParams = new RevocationParameters(getCrlCheckingMode(), ocspParameters);
+        var validatorParams = new ValidatorParams(revocationParams, ProxySupport.ALLOW);
+        long updateInterval = getCertificateAuthorityUpdateIntervalUnit().toMillis(getCertificateAuthorityUpdateInterval());
+        var validator = new OpensslCertChainValidator(path.toString(), true,
+                getNamespaceMode(), updateInterval, validatorParams, false);
+        onShutdownTasks.add(validator::dispose);
+        return new SSLTrustManager(validator);
+    }
+
     @Override
     public void shutdown()
     {
         super.shutdown();
-        synchronized (this) {
-            if (validator != null) {
-                validator.dispose();
-            }
-        }
-    }
-
-    private synchronized X509CertChainValidator getValidator() throws IOException
-    {
-        if (validator == null) {
-            checkDirectory(getCertificateAuthorityPath());
-            OCSPParametes ocspParameters = new OCSPParametes(getOcspCheckingMode());
-            ValidatorParams validatorParams =
-                    new ValidatorParams(new RevocationParameters(getCrlCheckingMode(), ocspParameters), ProxySupport.ALLOW);
-            long updateInterval = getCertificateAuthorityUpdateIntervalUnit().toMillis(getCertificateAuthorityUpdateInterval());
-            validator = new OpensslCertChainValidator(getCertificateAuthorityPath(), true, getNamespaceMode(), updateInterval, validatorParams,
-                                                      false);
-        }
-        return validator;
+        onShutdownTasks.forEach(Runnable::run);
     }
 }
