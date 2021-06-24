b9bf6d5dc867f15c6e83a46847726c865e1a228e
Paul Millar <paul.millar@desy.de>
2021-06-24 23:09:27 +0200

    gplazma: multimap now supports OP in 'oidc' predicate
    Motivation:
    
    The 'sub' claim identifies a person; however, the value is only
    guaranteed to be unique for values from the same OP.  The same 'sub'
    value asserted by different OPs could identify different people.
    
    Current, dCache only considers the 'sub' value, without considering
    which OP asserted the claim.  This could result in somebody logging in
    as someone else if two (trusted) OPs used the same 'sub' claim to
    identify different people.
    
    Modification:
    
    Update the OidcSubjectPrincipal class to record and use the OP's
    identity.
    
    Update places that create an OidcSubjectPrincipal object to include the
    OP's identity.
    
    Update multimap to support OP-specific 'oidc' predicates.
    
    For backwards compatibility, the current non-OP-specific 'oidc'
    predicates continue to work, but omit a warning that the configuration
    file should be updated.
    
    Add extra unit-tests to cover new behaviour; update existing unit-tests
    to include OP information.
    
    Update multimap's NDC when loading the configuration file.  The filename
    and line number are now included, while the door, gPlazma context have
    been removed.
    
    TheBook is updated to reflect the new format.
    
    Result:
    
    dCache may now be configured so that the multimap 'oidc' predicates
    match the 'sub' claim value from a specific OAuth2 Provider using the
    format SUBVALUE@OP, where 'OP' is the dCache-internal alias for the OP.
    Existing multimap configuration continues to work, but admin is warned
    to update the multimap configuration.
    
    Target: master
    Request: 7.1
    Request: 7.0
    Request: 6.2
    Request: 6.1
    Request: 6.0
    Request: 5.2
    Requires-notes: yes
    Requires-book: yes
    Closes: #5950
    Patch: https://rb.dcache.org/r/13093/
    Acked-by: Tigran Mkrtchyan

---
+++ modules/common/src/test/java/org/dcache/auth/OidcSubjectPrincipalTest.java
@@ -0,0 +1,122 @@
+/* dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2021 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.auth;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class OidcSubjectPrincipalTest
+{
+
+    @Test(expected=NullPointerException.class)
+    public void shouldRejectNullSubClaim()
+    {
+        new OidcSubjectPrincipal(null, "OP");
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void shouldRejectNullOP()
+    {
+        new OidcSubjectPrincipal("sub-claim", null);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldRejectNonASCIISubClaim()
+    {
+        new OidcSubjectPrincipal("\uD80C\uDC80", "OP");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldRejectTooLongSubClaim()
+    {
+        new OidcSubjectPrincipal(
+                  "01234567890123456789012345678901234567890123456789"
+                + "01234567890123456789012345678901234567890123456789"
+                + "01234567890123456789012345678901234567890123456789"
+                + "01234567890123456789012345678901234567890123456789"
+                + "01234567890123456789012345678901234567890123456789"
+                + "123456", "OP");
+    }
+
+    @Test
+    public void shouldReturnSubClaim()
+    {
+        OidcSubjectPrincipal p = new OidcSubjectPrincipal("sub-claim", "OP");
+
+        assertThat(p.getSubClaim(), is(equalTo("sub-claim")));
+    }
+
+    @Test
+    public void shouldReturnOP()
+    {
+        OidcSubjectPrincipal p = new OidcSubjectPrincipal("sub-claim", "OP");
+
+        assertThat(p.getOP(), is(equalTo("OP")));
+    }
+
+    @Test
+    public void shouldReturnReasonableName()
+    {
+        OidcSubjectPrincipal p = new OidcSubjectPrincipal("sub-claim", "OP");
+
+        assertThat(p.getName(), is(equalTo("sub-claim@OP")));
+    }
+
+    @Test
+    public void shouldBeEqualWithSameObject()
+    {
+        OidcSubjectPrincipal p = new OidcSubjectPrincipal("sub-claim", "OP");
+
+        assertThat(p, is(equalTo(p)));
+    }
+
+    @Test
+    public void shouldBeEqualWithOidcPrincipalWithSameStrings()
+    {
+        OidcSubjectPrincipal p = new OidcSubjectPrincipal("sub-claim", "OP");
+
+        assertThat(p, is(equalTo(new OidcSubjectPrincipal("sub-claim", "OP"))));
+        assertThat(p.hashCode(), is(equalTo(new OidcSubjectPrincipal("sub-claim", "OP").hashCode())));
+    }
+
+    @Test
+    public void shouldNotBeEqualWithOidcPrincipalWithDifferentSubClaim()
+    {
+        OidcSubjectPrincipal p = new OidcSubjectPrincipal("sub-claim", "OP");
+
+        assertThat(p, is(not(equalTo(new OidcSubjectPrincipal("different-sub-claim", "OP")))));
+    }
+
+    @Test
+    public void shouldNotBeEqualWithOidcPrincipalWithDifferentOP()
+    {
+        OidcSubjectPrincipal p = new OidcSubjectPrincipal("sub-claim", "OP");
+
+        assertThat(p, is(not(equalTo(new OidcSubjectPrincipal("sub-claim", "other-OP")))));
+    }
+
+    @Test
+    public void shouldHaveExpectedToString()
+    {
+        OidcSubjectPrincipal p = new OidcSubjectPrincipal("sub-claim", "OP");
+
+        assertThat(p.toString(), is(equalTo("OidcSubjectPrincipal[sub-claim@OP]")));
+    }
+}
