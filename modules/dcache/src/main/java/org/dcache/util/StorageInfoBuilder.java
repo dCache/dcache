b62ff9879a8391dfd3cf809ca989a1e7741c1826
Paul Millar <paul.millar@desy.de>
2021-08-11 22:58:18 +0200

    pool/pnfsmanager: don't accept a non-Enstore flush without locations
    Motivation:
    
    Currently, a pool will accept that a file was successfully flushed even
    if flush activity did not provide any URIs.  A flush without any URI
    makes no sense for most storage systesm because dCache will not have any
    way to recall/stage the file from tape later on.  This only makes sense
    for Enstore, as it directly modifies the namespace (bypassing dCache).
    
    Given dCache believes the file was successfully written to tape then
    dCache will mark the disk replica cache-only and, over time, it will be
    garbage collected.  Given dCache has no way of recalling the file from
    tape (if a non-Enstore storage is used), the file then becomes
    unavailable.
    
    This scenario can happen if (for example) the HSM is configured to use a
    script and that script returns successfully (return-code 0) but without
    printing anything on stdout.
    
    Depending on what ancillary information is available, it may prove
    impossible to track down the missing data (assuming it was written
    succesfully to tape), in which case this problem creates a possible
    route for data-loss.
    
    Modification:
    
    Update StorageInfo to alter the semantics of `isSetAddLocation`.
    Currently `isSetAddLocation` is true only for flush operations where
    `locations` returns a non-empty Set.  With the new semantics, the
    `isSetAddLocation` is true for any flush operation, including those
    where `locations` returns an empty Set.
    
    Note that, currently, ChimeraHsmStorageInfoExtractor#setStorageInfo does
    nothing if `isSetAddLocation` is true but `locations` returns an empty
    set.  Therefore, this change in semantics in `isSetAddLocation` has no
    effect if PnfsManager is not also updated.
    
    Add a framework that allows PnfsManager to fail a flush operation, so
    that the failed flush will moved that file into a failed state,
    requiring manual intervention from an operator before retrying.
    
    Add a check in ChimeraHsmStorageInfoExtractor that an update resulting
    from a flush operation is rejected if that update contains no new
    locations.  ChimeraEnstoreStorageInfoExtractor is updated to avoid this
    check: it is valid for an Enstore flush to contain no additional
    locations.
    
    Add unit-tests to verify correct behaviour of
    ChimeraEnstoreStorageInfoExtractor and ChimeraOsmStorageInfoExtractor.
    
    Result:
    
    Fix a problem where dCache accepts a HSM script that, when called to
    flush a file, returns successfully but without providing any URIs on
    stdout.  Such requests are considered failed, with the flush request
    blocked until retried explicitly with the `queue activate PNFSID`
    admin command.
    
    Target: master
    Requires-notes: yes
    Requires-book: no
    Bug: #6007
    Request: 7.1
    Request: 7.0
    Request: 6.2
    Acked-by: Tigran Mkrtchyan
    Acked-by: Lea Morschel
    Patch: https://rb.dcache.org/r/13130/

---
+++ modules/dcache/src/main/java/org/dcache/util/StorageInfoBuilder.java
@@ -0,0 +1,72 @@
+/*
+ * dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2021 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dcache.util;
+
+import java.net.URI;
+
+import diskCacheV111.vehicles.GenericStorageInfo;
+import diskCacheV111.vehicles.StorageInfo;
+
+/**
+ * A fluent class to build a StorageInfo.
+ */
+public class StorageInfoBuilder
+{
+    private final GenericStorageInfo info = new GenericStorageInfo();
+
+    public StorageInfoBuilder withLocation(String uri)
+    {
+        info.addLocation(URI.create(uri));
+        return this;
+    }
+
+    public StorageInfoBuilder withHsm(String hsm)
+    {
+        info.setHsm(hsm);
+        return this;
+    }
+
+    public StorageInfoBuilder withKey(String key, String value)
+    {
+        info.setKey(key, value);
+        return this;
+    }
+
+    public StorageInfoBuilder withIsSetAddLocation()
+    {
+        info.isSetAddLocation(true);
+        return this;
+    }
+
+    public StorageInfoBuilder withIsSetAddLocation(boolean value)
+    {
+        info.isSetAddLocation(value);
+        return this;
+    }
+
+    public StorageInfo build()
+    {
+        return info;
+    }
+
+    public static StorageInfoBuilder aStorageInfo()
+    {
+        return new StorageInfoBuilder();
+    }
+}
