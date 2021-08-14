dCache/dcache

commit 61a4e8dc4e4877316f3779dfc1a04acd40419067

Implementation of UID/GID quota system in dCache
Motivation:
----------

It has been a long standing request to implement quota system in dCache.
Experience with multi-user/multi-experiment dCache installation at Fermilab
showed that administrative overhead of maintaining pool groups per experiment
are just too high given proliferation of many small pool groups. Additionally
users complain about inability to control individual user space within each
pool group. These are not tape-backed pool groups. Additionally we received
multiple requests to control how much data a user can store on tape back-end.

Modification:
-------------

This patch introduces UID/GID quota system. The quota system operates on chimera
namespace. It periodically counts space usage broken down by Retention Policy by
UID and by GID and stores these counts in two chimera DB tables - t_user_quota
and t_group_quota. Besides space counts these tables hold quota limits for
each space count category by UID and by GID. A null for a limit means "no quota".
If there is no entries in these tables for a UID or GIDs means "no quota" for that
UID or GID.

    chimera=# \d t_user_quota
          Table "public.t_user_quota"
          Column      |  Type   | Modifiers
    ------------------+---------+-----------
     iuid             | integer | not null
     icustodial_used  | bigint  | not null
     ioutput_used     | bigint  | not null
     ireplica_used    | bigint  | not null
     icustodial_limit | bigint  |
     ioutput_limit    | bigint  |
     ireplica_limit   | bigint  |

Custodial Retention Policy corresponds to files going to tape, Replica corresponds to
disk-only files and Output currently is not used in dCache.

Queries executed by periodic updates may run significant time on chimera back-end.
Therefore default update frequency is once a day.

This means that quota enforcement is eventual. If user ran out of allocated quota

modules/chimera/src/main/java/org/dcache/chimera/JdbcFs.java
@@ -22,6 +22,8 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import diskCacheV111.util.RetentionPolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
@@ -50,10 +52,13 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.Optional;
 
 import org.dcache.acl.ACE;
 import org.dcache.acl.enums.RsType;
 import org.dcache.chimera.posix.Stat;
+import org.dcache.chimera.quota.QuotaHandler;
+
 import org.dcache.chimera.store.InodeStorageInformation;
 import org.dcache.util.Checksum;
 
@@ -149,6 +154,8 @@ public class JdbcFs implements FileSystemProvider {
                     .maximumSize(100000)
                     .build();
 
+    private QuotaHandler _quota;
+
     /**
      * current fs id
      */
@@ -167,6 +174,16 @@ public class JdbcFs implements FileSystemProvider {
      */
     private static final int MAX_NAME_LEN = 255;
 
+    /**
+     * switch quota check on/off
+     */
+    private boolean _quotaEnabled;
+
+    /**
+     * default retention policy
+     */
+    private RetentionPolicy _defaultRetentionPolicy;
+
     public JdbcFs(DataSource dataSource, PlatformTransactionManager txManager) throws SQLException, ChimeraFsException
     {
         this(dataSource, txManager, 0);
@@ -181,6 +198,21 @@ public class JdbcFs implements FileSystemProvider {
 
         // try to get database dialect specific query engine
         _sqlDriver = FsSqlDriver.getDriverInstance(dataSource);
+    }
+
+    public void setQuota(QuotaHandler quota) {
+        _quota = quota;
+    }
+
+    public void setQuotaEnabled(boolean enabled) {
+        _quotaEnabled = enabled;
+        if (_quotaEnabled) {
+            _quota.scheduleRefreshQuota();
+        }
+    }
+
+    public void setDefaultRetentionPolicy(RetentionPolicy rp) {
+        _defaultRetentionPolicy = rp;
     }
 
     private FsInode getWormID() throws ChimeraFsException {
@@ -398,6 +430,10 @@ public class JdbcFs implements FileSystemProvider {
                 }
 
                 int gid = (parentStat.getMode() & UnixPermission.S_ISGID) != 0 ? parentStat.getGid() : group;
+                if (_quotaEnabled) {
+                    RetentionPolicy rp = getRetentionPolicyFromParentTag(parent);
+                    checkQuota(owner, gid, rp);
+                }
                 FsInode inode = _sqlDriver.createFile(parent, name, owner, gid, mode, type);
                 _sqlDriver.copyAcl(parent, inode, RsType.FILE,
                                    EnumSet.of(INHERIT_ONLY_ACE, DIRECTORY_INHERIT_ACE, FILE_INHERIT_ACE),
@@ -438,6 +474,10 @@ public class JdbcFs implements FileSystemProvider {
                 }
                 Stat stat = parent.statCache();
                 int gid = (stat.getMode() & UnixPermission.S_ISGID) != 0 ? stat.getGid() : group;
+                if (_quotaEnabled) {
+                    RetentionPolicy rp = getRetentionPolicyFromParentTag(parent);
+                    checkQuota(owner, gid, rp);
+                }
                 FsInode inode = _sqlDriver.createFileWithId(parent, id, name, owner, gid, mode, type);
                 _sqlDriver.copyAcl(parent, inode, RsType.FILE,
                                    EnumSet.of(INHERIT_ONLY_ACE, DIRECTORY_INHERIT_ACE, FILE_INHERIT_ACE),
@@ -1525,6 +1565,31 @@ public class JdbcFs implements FileSystemProvider {
         public FsInode getParent()
         {
             return null;
+        }
+    }
+
+    private RetentionPolicy getRetentionPolicyFromParentTag(FsInode parent)
+    {
+        try {
+            Stat tagStat = statTag(parent, "RetentionPolicy");
+            byte[] data = new byte[(int) tagStat.getSize()];
+            getTag(parent, "RetentionPolicy", data, 0, data.length);
+            RetentionPolicy rp = RetentionPolicy.valueOf(new String(data));
+            return rp;
+        } catch (ChimeraFsException e) {
+            return _defaultRetentionPolicy;
+        }
+    }
+
+    private void checkQuota(int uid, int gid, RetentionPolicy rp)
+            throws QuotaChimeraFsException
+    {
+        LOGGER.debug("Checking quota for {} {} {}", uid, gid, rp);
+        if (!_quota.checkGroupQuota(gid, rp)) {
+            throw new QuotaChimeraFsException(String.format("%s group quota exceeded for gid=%d", rp, gid));
+        }
+        if (!_quota.checkUserQuota(uid, rp)) {
+            throw new QuotaChimeraFsException(String.format("%s user quota exceeded for uid=%d", rp, uid));
         }
     }
 }

modules/chimera/src/main/java/org/dcache/chimera/FsSqlDriver.java
@@ -16,6 +16,7 @@
  */
 package org.dcache.chimera;
 
+import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 import com.google.common.io.ByteStreams;
 import com.google.common.primitives.Ints;
@@ -40,6 +41,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -158,9 +160,16 @@ public class FsSqlDriver {
      * Update file system cache table.
      */
     void updateFsStat() {
-        _jdbc.update("UPDATE t_fstat SET "+
-                     "iusedFiles = (SELECT count(*) AS usedFiles FROM t_inodes WHERE itype=32768), "+
-                     "iusedSpace =  (SELECT SUM(isize) AS usedSpace FROM t_inodes WHERE itype=32768)");
+        try {
+            _jdbc.update("UPDATE t_fstat SET "+
+                    "iusedFiles = (SELECT count(*) AS usedFiles FROM t_inodes WHERE itype=32768), "+
+                    "iusedSpace =  (SELECT SUM(isize) AS usedSpace FROM t_inodes WHERE itype=32768)");
+        } catch (DataAccessException e) {
+            Throwable cause = Throwables.getRootCause(e);
+            if (cause instanceof SocketException) {
+                LOGGER.warn("FS statistics update interrupted {}.", e.getMessage());
+            }
+        }
     }
 
     /**

modules/chimera/src/main/java/org/dcache/chimera/PgSQL95FsSqlDriver.java
@@ -16,12 +16,15 @@
  */
 package org.dcache.chimera;
 
+import com.google.common.base.Throwables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.dao.DuplicateKeyException;
 
 import java.io.File;
+import java.net.SocketException;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -412,10 +415,17 @@ public class PgSQL95FsSqlDriver extends FsSqlDriver {
 
     @Override
     void updateFsStat() {
-        _jdbc.update("UPDATE t_fstat SET iusedFiles = t.usedFiles, "+
-                     "iusedSpace = t.usedSpace "+
-                     "FROM (SELECT count(*) AS usedFiles, "+
-                     "SUM(isize) AS usedSpace FROM t_inodes "+
-                     "WHERE itype=32768) t");
+        try {
+            _jdbc.update("UPDATE t_fstat SET iusedFiles = t.usedFiles, "+
+                    "iusedSpace = t.usedSpace "+
+                    "FROM (SELECT count(*) AS usedFiles, "+
+                    "SUM(isize) AS usedSpace FROM t_inodes "+
+                     "WHERE itype=32768) as t");
+        } catch (DataAccessException e) {
+            Throwable cause = Throwables.getRootCause(e);
+            if (cause instanceof SocketException) {
+                LOGGER.warn("FS stat update interrupted {}", e.getMessage());
+            }
+        }
     }
 }

modules/chimera/src/main/java/org/dcache/chimera/QuotaChimeraFsException.java
@@ -1,0 +1,75 @@
+/*
+COPYRIGHT STATUS:
+Dec 1st 2001, Fermi National Accelerator Laboratory (FNAL) documents and
+software are sponsored by the U.S. Department of Energy under Contract No.
+DE-AC02-76CH03000. Therefore, the U.S. Government retains a  world-wide
+non-exclusive, royalty-free license to publish or reproduce these documents
+and software for U.S. Government purposes.  All documents and software
+available from this server are protected under the U.S. and Foreign
+Copyright Laws, and FNAL reserves all rights.
+
+Distribution of the software available from this server is free of
+charge subject to the user following the terms of the Fermitools
+Software Legal Information.
+
+Redistribution and/or modification of the software shall be accompanied
+by the Fermitools Software Legal Information  (including the copyright
+notice).
+
+The user is asked to feed back problems, benefits, and/or suggestions
+about the software to the Fermilab Software Providers.
+
+Neither the name of Fermilab, the  URA, nor the names of the contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+DISCLAIMER OF LIABILITY (BSD):
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED  WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL FERMILAB,
+OR THE URA, OR THE U.S. DEPARTMENT of ENERGY, OR CONTRIBUTORS BE LIABLE
+FOR  ANY  DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY  OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE  POSSIBILITY OF SUCH DAMAGE.
+
+Liabilities of the Government:
+
+This software is provided by URA, independent from its Prime Contract
+with the U.S. Department of Energy. URA is acting independently from
+the Government and in its own private capacity and is not acting on
+behalf of the U.S. Government, nor as its contractor nor its agent.
+Correspondingly, it is understood and agreed that the U.S. Government
+has no connection to this software and in no manner whatsoever shall
+be liable for nor assume any responsibility or obligation for any claim,
+cost, or damages arising out of or resulting from the use of the software
+available from this server.
+
+Export Control:
+
+All documents and software available from this server are subject to U.S.
+export control laws.  Anyone downloading information from this server is
+obligated to secure any necessary Government licenses before exporting
+documents or software obtained from this server.
+*/
+
+package org.dcache.chimera;
+
+public class QuotaChimeraFsException extends ChimeraFsException {
+
+    private static final long serialVersionUID = 1L;
+
+
+    public QuotaChimeraFsException() {
+        super();
+    }
+
+    public QuotaChimeraFsException(String msg) {
+        super(msg);
+    }
+}

modules/chimera/src/main/java/org/dcache/chimera/quota/JdbcQuota.java
@@ -1,0 +1,221 @@
+/*
+COPYRIGHT STATUS:
+Dec 1st 2001, Fermi National Accelerator Laboratory (FNAL) documents and
+software are sponsored by the U.S. Department of Energy under Contract No.
+DE-AC02-76CH03000. Therefore, the U.S. Government retains a  world-wide
+non-exclusive, royalty-free license to publish or reproduce these documents
+and software for U.S. Government purposes.  All documents and software
+available from this server are protected under the U.S. and Foreign
+Copyright Laws, and FNAL reserves all rights.
+
+Distribution of the software available from this server is free of
+charge subject to the user following the terms of the Fermitools
+Software Legal Information.
+
+Redistribution and/or modification of the software shall be accompanied
+by the Fermitools Software Legal Information  (including the copyright
+notice).
+
+The user is asked to feed back problems, benefits, and/or suggestions
+about the software to the Fermilab Software Providers.
+
+Neither the name of Fermilab, the  URA, nor the names of the contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+DISCLAIMER OF LIABILITY (BSD):
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED  WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL FERMILAB,
+OR THE URA, OR THE U.S. DEPARTMENT of ENERGY, OR CONTRIBUTORS BE LIABLE
+FOR  ANY  DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY  OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE  POSSIBILITY OF SUCH DAMAGE.
+
+Liabilities of the Government:
+
+This software is provided by URA, independent from its Prime Contract
+with the U.S. Department of Energy. URA is acting independently from
+the Government and in its own private capacity and is not acting on
+behalf of the U.S. Government, nor as its contractor nor its agent.
+Correspondingly, it is understood and agreed that the U.S. Government
+has no connection to this software and in no manner whatsoever shall
+be liable for nor assume any responsibility or obligation for any claim,
+cost, or damages arising out of or resulting from the use of the software
+available from this server.
+
+Export Control:
+
+All documents and software available from this server are subject to U.S.
+export control laws.  Anyone downloading information from this server is
+obligated to secure any necessary Government licenses before exporting
+documents or software obtained from this server.
+*/
+
+package org.dcache.chimera.quota;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Map;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import diskCacheV111.util.RetentionPolicy;
+
+import org.dcache.util.FireAndForgetTask;
+
+public class JdbcQuota implements QuotaHandler {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(JdbcQuota.class);
+
+    private final QuotaSqlDriver sqlDriver;
+    private volatile Map<Integer, Quota> userQuotas;
+    private volatile Map<Integer, Quota> groupQuotas;
+    private ScheduledExecutorService  quotaRefreshExecutor;
+
+    public JdbcQuota(DataSource ds)
+            throws SQLException {
+        sqlDriver = QuotaSqlDriver.getDriverInstance(ds);
+        userQuotas = sqlDriver.getUserQuotas();
+        groupQuotas = sqlDriver.getGroupQuotas();
+    }
+
+    public void setQuotaRefreshExecutor(ScheduledExecutorService executor) {
+        quotaRefreshExecutor = executor;
+    }
+
+    public void scheduleRefreshQuota() {
+        ScheduledFuture<?> refreshUserQuota = quotaRefreshExecutor.
+                scheduleWithFixedDelay(
+                        new FireAndForgetTask(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    refreshUserQuotas();
+                                } catch(Exception ignore) {
+                                    LOGGER.warn("refreshUserQuotas failed {}",ignore.getMessage()) ;
+                                }
+                            }
+                        }),
+                        60000,
+                        60000,
+                        TimeUnit.MILLISECONDS);
+
+        ScheduledFuture<?> refreshGroupQuota = quotaRefreshExecutor.
+                scheduleWithFixedDelay(
+                        new FireAndForgetTask(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    refreshGroupQuotas();
+                                } catch (Exception ignore) {
+                                    LOGGER.warn("refreshGroupQuotas failed {}",ignore.getMessage()) ;
+                                }
+                            }
+                        }),
+                        60000,
+                        60000,
+                        TimeUnit.MILLISECONDS);
+    }
+
+
+    @Override
+    public Map<Integer, Quota> getUserQuotas()
+    {
+        return userQuotas;
+    }
+
+    @Override
+    public Map<Integer, Quota> getGroupQuotas()
+    {
+        return groupQuotas;
+    }
+
+    @Override
+    public void setUserQuota(Quota quota)
+    {
+        sqlDriver.setUserQuota(quota);
+    }
+
+    @Override
+    public void createUserQuota(Quota quota)
+    {
+        sqlDriver.createUserQuota(quota);
+    }
+
+    @Override
+    public void setGroupQuota(Quota quota)
+    {
+        sqlDriver.setGroupQuota(quota);
+    }
+
+    @Override
+    public void createGroupQuota(Quota quota)
+    {
+        sqlDriver.createGroupQuota(quota);
+    }
+
+    @Override
+    public boolean checkUserQuota(int uid, RetentionPolicy rp)
+    {
+        Quota quota = userQuotas.get(uid);
+        if (quota == null)  {
+            return true;
+        } else {
+            return quota.check(rp);
+        }
+    }
+
+    @Override
+    public boolean checkGroupQuota(int gid, RetentionPolicy rp)
+    {
+        Quota quota = groupQuotas.get(gid);
+        if (quota == null)  {
+            return true;
+        } else {
+            return quota.check(rp);
+        }
+    }
+
+    @Override
+    public void refreshUserQuotas()
+    {
+        LOGGER.debug("Running refreshUserQuotas.");
+        Map<Integer, Quota> tmp = sqlDriver.getUserQuotas();
+        userQuotas = tmp;
+    }
+
+    @Override
+    public void refreshGroupQuotas()
+    {
+        LOGGER.debug("Running refreshGroupQuotas.");
+        Map<Integer, Quota> tmp = sqlDriver.getGroupQuotas();
+        groupQuotas = tmp;
+    }
+
+    @Override
+    public void updateUserQuotas()
+    {
+        LOGGER.info("Running updateUserQuotas.");
+        sqlDriver.updateUserQuota();
+    }
+
+    @Override
+    public void updateGroupQuotas()
+    {
+        LOGGER.info("Running updateGroupQuotas.");
+        sqlDriver.updateGroupQuota();
+    }
+}

commit 68096908b5f508ed4fdeb9229d183a90211e2f9c

pnfsmanager: fix regression introduced with commit 61a4e8dc4e
Motivation:

Observer stack-trace while copying data into /tape in system-test:

  12 Aug 2021 10:09:49 (PnfsManager) [door:WebDAV-NA-sprocket@dCacheDomain:AAXJWEJG0qA WebDAV-NA-sprocket PnfsCreateEntry] Bug found when creating entry:
  java.lang.IllegalArgumentException: Unknown Policy: "CUSTODIAL
  "
          at diskCacheV111.util.RetentionPolicy.getRetentionPolicy(RetentionPolicy.java:148)
          at diskCacheV111.util.RetentionPolicy.valueOf(RetentionPolicy.java:164)
          at org.dcache.chimera.JdbcFs.getRetentionPolicyFromParentTag(JdbcFs.java:1577)
          at org.dcache.chimera.JdbcFs.lambda$createFile$9(JdbcFs.java:434)
          at org.dcache.chimera.JdbcFs.inTransaction(JdbcFs.java:229)
          at org.dcache.chimera.JdbcFs.createFile(JdbcFs.java:421)
          at org.dcache.chimera.JdbcFs.createFile(JdbcFs.java:357)
          at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
          at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
          at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
          at java.base/java.lang.reflect.Method.invoke(Method.java:566)
          at org.dcache.commons.stats.MonitoringProxy.invoke(MonitoringProxy.java:54)
          at com.sun.proxy.$Proxy33.createFile(Unknown Source)
          at org.dcache.chimera.FsInode.create(FsInode.java:261)
          at org.dcache.chimera.namespace.ExtendedInode.create(ExtendedInode.java:127)
          at org.dcache.chimera.namespace.ChimeraNameSpaceProvider.createFile(ChimeraNameSpaceProvider.java:321)
          at diskCacheV111.namespace.ForwardingNameSpaceProvider.createFile(ForwardingNameSpaceProvider.java:56)
          at diskCacheV111.namespace.MonitoringNameSpaceProvider.createFile(MonitoringNameSpaceProvider.java:181)

modules/chimera/src/main/java/org/dcache/chimera/JdbcFs.java
@@ -21,9 +21,11 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.io.ByteSource;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import diskCacheV111.util.RetentionPolicy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
@@ -63,7 +65,10 @@ import org.dcache.chimera.store.InodeStorageInformation;
 import org.dcache.util.Checksum;
 
 import static com.google.common.base.Preconditions.checkArgument;
+
 import java.nio.charset.StandardCharsets;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.dcache.acl.enums.AceFlags.*;
 import static org.dcache.chimera.FileSystemProvider.StatCacheOption.NO_STAT;
 import static org.dcache.chimera.FileSystemProvider.StatCacheOption.STAT;
@@ -1568,15 +1573,29 @@ public class JdbcFs implements FileSystemProvider {
         }
     }
 
+    // REVISIT: this method violates DRY by duplicating functionality from
+    // ChimeraHsmStorageInfoExtractor#getRetentionPolicy.
     private RetentionPolicy getRetentionPolicyFromParentTag(FsInode parent)
     {
+        String firstLine;
+
         try {
             Stat tagStat = statTag(parent, "RetentionPolicy");
             byte[] data = new byte[(int) tagStat.getSize()];
             getTag(parent, "RetentionPolicy", data, 0, data.length);
-            RetentionPolicy rp = RetentionPolicy.valueOf(new String(data));
-            return rp;
+            firstLine = ByteSource.wrap(data).asCharSource(UTF_8).readFirstLine();
         } catch (ChimeraFsException e) {
+            return _defaultRetentionPolicy;
+        } catch (IOException e) {
+            // This should not happen.
+            throw new RuntimeException("Unexpected IOException found", e);
+        }
+
+        try {
+            return RetentionPolicy.valueOf(firstLine);
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("Badly formatted RetentionPolicy tag in {}: {}", parent,
+                    e.getMessage());
             return _defaultRetentionPolicy;
         }
     }