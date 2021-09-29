fa7fef400348cbcb71134cd020d6d3a629eef995
Paul Millar <paul.millar@desy.de>
2021-09-29 10:32:23 +0200

    webdav: return 507 if insufficient space on GET request
    Motivation:
    
    dCache treats PoolManager reporting insufficient storage capacity on a
    PUT request by returning a 507 (Insufficient Storage) HTTP status code.
    
    PoolManager may also fail because of insufficient storage capacity on
    GET requests if pool-to-pool or staging is required.  If this happens
    then dCache logs the problem (in webdav) and returns the more generic
    500 (Internal Server Error) status code.
    
    Returning 507 on a GET request is allowed (and used, in practice) and
    provides more information.  In addition, the logging by the door should
    be consistent between GET and PUT requests.
    
    Modification:
    
    Factor-out a common method that describes how a CacheException is
    converted to the corresponding WebDavException.
    
    Apply this method to both GET and PUT requests.
    
    Result:
    
    dCache now responds with a 507 (Insufficient Storage) on GET request if
    dCache must do a pool-to-pool internal-transfer or stage the file but no
    pools have sufficient free capacity to support this.
    
    Target: master
    Requires-notes: yes
    Requires-book: no
    Request: 7.2
    Request: 7.1
    Request: 7.0
    Request: 6.2
    Closes: #6067
    Patch: https://rb.dcache.org/r/13186/
    Acked-by: Tigran Mkrtchyan

--- modules/dcache-webdav/src/main/java/org/dcache/webdav/WebDavException.java
+++ modules/dcache-webdav/src/main/java/org/dcache/webdav/WebDavException.java
@@ -1,6 +1,10 @@
 package org.dcache.webdav;
 
+import com.google.common.collect.ImmutableSet;
+import diskCacheV111.util.CacheException;
+import diskCacheV111.util.PermissionDeniedCacheException;
 import io.milton.resource.Resource;
+import javax.annotation.Nonnull;
 
 /**
  * Base class for WebDAV exceptions.
@@ -16,6 +20,15 @@ import io.milton.resource.Resource;
 public class WebDavException extends RuntimeException
 {
     private static final long serialVersionUID = -1251402018582832989L;
+
+    // FIXME update poolmanager to return the actual CacheException.
+    private static final ImmutableSet<String> FULL_POOL_MESSAGE = ImmutableSet.<String>builder()
+            .add("All pools full")
+            .add("All pools are full")
+            .add("Cost limit exceeded")
+            .add("Fallback cost exceeded")
+            .build();
+
     private final Resource _resource;
 
     public WebDavException(Resource resource)
@@ -38,5 +51,33 @@ public class WebDavException extends RuntimeException
     public Resource getResource()
     {
         return _resource;
+    }
+
+    /**
+     * Provide a common translation from (dCache) CacheException to the
+     * corresponding WebDAV exceptions that are common between GET and PUT
+     * requests.
+     * @param e The CacheException received.
+     * @param resource The resource targeted by the HTTP request.
+     * @return the corresponding WebDAV exception.
+     */
+    public static WebDavException of(@Nonnull CacheException e, Resource resource)
+    {
+        if (e instanceof PermissionDeniedCacheException) {
+            return WebDavExceptions.permissionDenied(resource);
+        }
+
+        switch (e.getRc()) {
+        case 192: // Pool-to-pool required, but destination cost exceeded.
+        case 194: // Pool-to-pool required, but source cost exceeded.
+            return new InsufficientStorageException("Unable to ready file for access",
+                    e, resource);
+        }
+
+        if (FULL_POOL_MESSAGE.contains(e.getMessage())) {
+            return new InsufficientStorageException(e.getMessage(), e, resource);
+        }
+
+        return new WebDavException(e.getMessage(), e, resource);
     }
 }
