dCache/dcache

commit 19fa7cf2425d084c6737bbc8e3944f21d770a3f9

chimera-vfs: return OK on write ACCESS check for existing files
Motivation:
Though xattrs have their own permission bits in the nfs world, the POSIX
filesystems relay only on a files read/write access mode. As a result,
NFS client won't send SETXATTR operation, if ACCESS check have not set
the write-allow bit.

Modification:
Update ChimeraVFS to return 'allow' write, if the user have write access
according to the posix permissions. Nevertheless, we sill check the file
state before changing the size as well as starting a write mover.

Result:
the nfs door sets 'write allow' bit on access check, to indicate that
permissions are granted, but reject IO operations on stored files.

Acked-by: Albert Rossi
Target: master
Require-book: no
Require-notes: no

modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/ChimeraVfs.java
@@ -20,6 +20,7 @@
 package org.dcache.chimera.nfsv41.door;
 
 import com.google.common.annotations.VisibleForTesting;
+import org.dcache.chimera.FileState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -329,7 +330,21 @@ public class ChimeraVfs implements VirtualFileSystem, AclCheckable {
             if (shouldRejectAttributeUpdates(fsInode, _fs)) {
                 throw new PermException("setStat not allowed.");
             }
-            fsInode.setStat(toChimeraStat(stat));
+
+            // OperationSETATTR have already checked for a valid open stateid
+            if (stat.isDefined(Stat.StatAttribute.SIZE)) {
+                // allow set size only for newly created files
+                if (_fs.stat(fsInode).getState() != FileState.CREATED) {
+                    throw new PermException("Can't change size of existing file");
+                }
+            }
+
+            org.dcache.chimera.posix.Stat chimeraStat = toChimeraStat(stat);
+            // convert empty setattr to noop
+            if (!chimeraStat.getDefinedAttributeses().isEmpty()) {
+                fsInode.setStat(chimeraStat);
+            }
+
         } catch (InvalidArgumentChimeraException e) {
             throw new InvalException(e.getMessage());
         } catch (IsDirChimeraException e) {
@@ -459,26 +474,11 @@ public class ChimeraVfs implements VirtualFileSystem, AclCheckable {
     }
 
     @VisibleForTesting
-    static boolean shouldRejectUpdates(FsInode fsInode, FileSystemProvider fs)
-                    throws ChimeraFsException {
-        switch (fsInode.type()) {
-            case SURI:
-                return !isRoot()
-                                && !fs.getInodeLocations(fsInode,
-                                                          StorageGenericLocation.TAPE)
-                                       .isEmpty();
-            case INODE:
-                return fsInode.getLevel() == 0
-                                && !fsInode.isDirectory()
-                                && (!fs.getInodeLocations(fsInode,
-                                                           StorageGenericLocation.TAPE)
-                                        .isEmpty()
-                                || !fs.getInodeLocations(fsInode,
-                                                          StorageGenericLocation.DISK)
-                                       .isEmpty());
-            default:
-                return false;
+    static boolean shouldRejectUpdates(FsInode fsInode, FileSystemProvider fs) throws ChimeraFsException {
+        if (!isRoot() && fsInode.type() == FsInodeType.SURI) {
+            return  !fs.getInodeLocations(fsInode, StorageGenericLocation.TAPE).isEmpty();
         }
+        return false;
     }
 
     @VisibleForTesting

commit c82b804288c78a28b268f332b46b39ad2cabcb68

nfs: fix tag update regression
Fixes: 19fa7cf2425

Motivation:
The directory tags inherit attributes from a directory to which it
tight, however, they are files, thus the file system object type have to
be adjusted. The FsInode#stat know how to do it, but
FileSystemProvider#stat doesn't.

Modification:
Update ChimeraVfs to use FsInode#stat to use the correct oject type.

Result:
the existing directory tags can be updated.

Fixes: #5955
Acked-by: Albert Rossi
Target: master, 7.2, 7.1, 7.0
Require-book: no
Require-notes: yes

modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/ChimeraVfs.java
@@ -346,7 +346,7 @@ public class ChimeraVfs implements VirtualFileSystem, AclCheckable {
             // OperationSETATTR have already checked for a valid open stateid
             if (stat.isDefined(Stat.StatAttribute.SIZE)) {
 
-                var chimeraStat = _fs.stat(fsInode);
+                var chimeraStat = fsInode.stat();
 
                 int type = chimeraStat.getMode() & UnixPermission.F_TYPE;
                 switch (type) {
@@ -360,7 +360,7 @@ public class ChimeraVfs implements VirtualFileSystem, AclCheckable {
                 }
 
                 // allow set size only for newly created files
-                if (chimeraStat.getState() != FileState.CREATED) {
+                if (fsInode.type() == FsInodeType.INODE && chimeraStat.getState() != FileState.CREATED) {
                     throw new PermException("Can't change size of existing file");
                 }
             }