69a3a515e7f1b0679aba5ab2669d9548b9dc8e02
Paul Millar <paul.millar@desy.de>
2021-11-26 13:21:22 +0100

    webdav: http-tpc update 'http-tpc ls' command to include state
    Motivation:
    
    Observed transfers that spend a long time (more than a second) where the
    pool is not shown.  This means _probably_ means that either the
    PnfsManager or PoolManager is being slow, but it's currently impossible
    to tell.
    
    Modification:
    
    Update the command to include the possibility of showing the transfer's
    state.
    
    Result:
    
    The WebDAV door's 'http-tpc ls' command now has the possibility to show
    the current state of the transfer.  This may be useful diagnosing
    transfers spending a lot of time deciding on which pool to transfer
    should take place.
    
    Target: master
    Request: 7.2
    Request: 7.1
    Request: 7.0
    Request: 6.2
    Requires-notes: yes
    Requires-book: no
    Patch: https://rb.dcache.org/r/13293/
    Acked-by: Lea Morschel

--- modules/dcache-webdav/src/main/java/org/dcache/webdav/transfer/RemoteTransferHandler.java
+++ modules/dcache-webdav/src/main/java/org/dcache/webdav/transfer/RemoteTransferHandler.java
@@ -315,6 +315,9 @@ public class RemoteTransferHandler implements CellMessageReceiver, CellCommandLi
               category = "Field options")
         boolean showNetwork;
 
+        @Option(name = "s", usage = "Show the current status.", category = "Field options")
+        boolean showState;
+
         @Option(name = "l", usage = "Show the local path of the transfer.",
               category = "Field options")
         boolean showLocalPath;
@@ -323,9 +326,8 @@ public class RemoteTransferHandler implements CellMessageReceiver, CellCommandLi
               category = "Field options")
         boolean showRemotePath;
 
-        @Option(name = "a", usage = "Show all available information about"
-              + " transfers.  This is equivalent to specifying \"-t -n -l"
-              + " -r\"", category = "Field options")
+        @Option(name = "a", usage = "Show all available information about transfers.  This is"
+                + " equivalent to specifying \"-l -n -r -s -t\".", category = "Field options")
         boolean showAll;
 
         @Option(name = "pool", usage = "Select transfers involving a pool that"
@@ -381,6 +383,7 @@ public class RemoteTransferHandler implements CellMessageReceiver, CellCommandLi
             if (showAll) {
                 showTiming = true;
                 showNetwork = true;
+                showState = true;
                 showLocalPath = true;
                 showRemotePath = true;
             }
@@ -402,6 +405,10 @@ public class RemoteTransferHandler implements CellMessageReceiver, CellCommandLi
                 output.space().header("Lifetime").left("lifetime")
                       .space().header("Queued").left("queued")
                       .space().header("Running").left("running");
+            }
+
+            if (showState) {
+                output.space().header("State").left("state");
             }
 
             output.space().header("Dirn").left("direction")
@@ -527,6 +534,10 @@ public class RemoteTransferHandler implements CellMessageReceiver, CellCommandLi
                   .value("direction", transfer._direction)
                   .value("host", transfer._destination.getHost())
                   .value("pool", transfer._pool.orElse("-"));
+
+            if (showState) {
+                row.value("state", TransferManagerHandler.describeState(transfer._lastState));
+            }
 
             if (showLocalPath) {
                 row.value("local-path", transfer._path.toString());
