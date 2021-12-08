dCache/dcache

commit 87408a17c04458894fdec86d344dd220ec67ba42

restful: add files listing based on label or virtual directory listing
Motivation:

Restful Api supports adding and removing labels to/from a file object.

there should be a restful call to list all files with a given label.
Or otherwise we call it virtual directory listing

Modification:

PnfsListDirectoryMessage has been adapted to support cases when path is the path to the given directory (real one)
or the path is a label

Result:

dCache admins can call listing virtual directory using:

curl  -v   -u    http://localhost:3880/api/v1/labels/cat/

where cat is the label.

Acked-by: Lea, Tigran
Patch: https://rb.dcache.org/r/13217/
Target: master,  7.2
Require-book: yes
Require-notes: yes

modules/dcache-vehicles/src/main/java/org/dcache/vehicles/PnfsListDirectoryMessage.java
@@ -24,6 +24,11 @@ public class PnfsListDirectoryMessage extends PnfsMessage {
 
     private static final long serialVersionUID = -5774904472984157638L;
 
+    public enum PathType
+    {
+        PATH, LABEL
+    }
+
     private final Glob _pattern;
     private final Integer _lower;
     private final Integer _upper;
@@ -38,6 +43,17 @@ public class PnfsListDirectoryMessage extends PnfsMessage {
      */
     private boolean _isFinal;
     private int _messageCount;
+
+
+
+    /**
+     * there are two possible values for this field     *
+     *  Path to directory to list
+     *  or path to virtual directory to list (which is the label value of a streamed files)
+     *  */
+    private static  PathType _pathType = PathType.PATH;
+
+
 
     /**
      * Constructs a new message.
@@ -135,6 +151,14 @@ public class PnfsListDirectoryMessage extends PnfsMessage {
     public void setSucceeded(int messageCount) {
         setSucceeded();
         _messageCount = messageCount;
+    }
+
+    public static PathType getPathType() {
+        return _pathType;
+    }
+
+    public static void setPathType(PathType pathType) {
+        _pathType = pathType;
     }
 
     public boolean isFinal() {

modules/dcache/src/main/java/diskCacheV111/namespace/PnfsManagerV3.java
@@ -2170,13 +2170,21 @@ public class PnfsManagerV3
                         : delay - envelope.getLocalAge();
             CellPath source = envelope.getSourcePath().revert();
             ListHandlerImpl handler =
-                  new ListHandlerImpl(source, envelope.getUOID(),
-                        msg, initialDelay, delay);
-            _nameSpaceProvider.list(msg.getSubject(), path,
-                  msg.getPattern(),
-                  msg.getRange(),
-                  msg.getRequestedAttributes(),
-                  handler);
+                new ListHandlerImpl(source, envelope.getUOID(),
+                                    msg, initialDelay, delay);
+
+            if (msg.getPathType() == PnfsListDirectoryMessage.PathType.LABEL) {
+                _nameSpaceProvider.listVirtualDirectory(msg.getSubject(), path.substring(1),
+                        msg.getRange(),
+                        msg.getRequestedAttributes(),
+                        handler);
+            } else {
+                _nameSpaceProvider.list(msg.getSubject(), path,
+                      msg.getPattern(),
+                      msg.getRange(),
+                      msg.getRequestedAttributes(),
+                      handler);
+            }
             msg.setSucceeded(handler.getMessageCount() + 1);
         } catch (FileNotFoundCacheException | NotDirCacheException e) {
             msg.setFailed(e.getRc(), e.getMessage());

modules/chimera/src/main/java/org/dcache/chimera/FsSqlDriver.java
@@ -2091,9 +2091,13 @@ public class FsSqlDriver {
 
                             Stat stat = toStat(rs);
                             //TODO get set _fs in constractor
+
                             FsInode inode = new FsInode(dir.getFs(), rs.getLong("fileid"),
                                   FsInodeType.INODE, 0);
-                            return new ChimeraDirectoryEntry(rs.getString("filename"), inode, stat);
+                            String path = inode2path(rs.getLong("fileid"), _root);
+                            return new ChimeraDirectoryEntry(path, inode, stat);
+
+
                         } catch (SQLException e) {
                             LOGGER.error("failed to fetch next entry: {}", e.getMessage());
                             return null;

modules/dcache-chimera/src/main/java/org/dcache/chimera/namespace/ChimeraNameSpaceProvider.java
@@ -1271,6 +1271,45 @@ public class ChimeraNameSpaceProvider
         }
     }
 
+
+    @Override
+    public void listVirtualDirectory(Subject subject, String path, Range<Integer> range,
+                     Set<FileAttribute> attrs, ListHandler handler)
+            throws CacheException
+    {
+        try {
+            int counter = 0;
+            try (DirectoryStreamB<ChimeraDirectoryEntry> dirStream = FsInode.getRoot(_fs)
+                    .virtualDirectoryStream(path)) {
+                for (ChimeraDirectoryEntry entry : dirStream) {
+                    try {
+                        String name = entry.getName();
+                        if (!name.equals(".") && !name.equals("..") &&
+                                range.contains(counter++)) {
+                            // FIXME: actually, ChimeraDirectoryEntry
+                            // already contains most of attributes
+                            FileAttributes fa =
+                                    attrs.isEmpty()
+                                            ? null
+                                            : getFileAttributes(new ExtendedInode(_fs, entry.getInode()), attrs);
+                            handler.addEntry(name, fa);
+                        }
+                    } catch (FileNotFoundChimeraFsException e) {
+                        /* Not an error; files may be deleted during the
+                         * list operation.
+                         */
+                    }
+                }
+            }
+
+        } catch (FileNotFoundChimeraFsException e) {
+            throw new FileNotFoundCacheException("No such file or directory: " + path);
+        } catch (IOException e) {
+            LOGGER.error("Exception in list: {}", e);
+            throw new CacheException(CacheException.UNEXPECTED_SYSTEM_EXCEPTION, e.getMessage());
+        }
+    }
+
     private ExtendedInode mkdir(Subject subject, ExtendedInode parent, String name, int uid,
           int gid, int mode)
           throws ChimeraFsException, CacheException {

modules/dcache-frontend/src/main/java/org/dcache/restful/DcacheRestApplication.java
@@ -11,6 +11,7 @@ import org.dcache.restful.resources.alarms.AlarmsResources;
 import org.dcache.restful.resources.billing.BillingResources;
 import org.dcache.restful.resources.cells.CellInfoResources;
 import org.dcache.restful.resources.doors.DoorsResources;
+import org.dcache.restful.resources.labels.LabelsResources;
 import org.dcache.restful.resources.namespace.FileResources;
 import org.dcache.restful.resources.pool.PoolGroupInfoResources;
 import org.dcache.restful.resources.pool.PoolInfoResources;
@@ -50,6 +51,10 @@ public class DcacheRestApplication extends ResourceConfig {
         register(SwaggerSerializers.class);
         register(EventResources.class);
         register(DoorsResources.class);
+
+
+        //register labels
+        register(LabelsResources.class);
 
         //register filters
         register(RequestUser.class);

modules/dcache-frontend/src/main/java/org/dcache/restful/resources/labels/LabelsResources.java
@@ -1,0 +1,215 @@
+/*
+ * dCache - http://www.dcache.org/
+ *
+ * Copyright (C) 2021 Deutsches Elektronen-Synchrotron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.dcache.restful.resources.labels;
+
+import com.google.common.collect.Range;
+import diskCacheV111.util.CacheException;
+import diskCacheV111.util.FileNotFoundCacheException;
+import diskCacheV111.util.FsPath;
+import diskCacheV111.util.PermissionDeniedCacheException;
+import dmg.cells.nucleus.NoRouteToCellException;
+import dmg.util.Exceptions;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import org.dcache.cells.CellStub;
+import org.dcache.http.PathMapper;
+import org.dcache.namespace.FileAttribute;
+import org.dcache.poolmanager.PoolMonitor;
+import org.dcache.restful.providers.JsonFileAttributes;
+import org.dcache.restful.util.HttpServletRequests;
+import org.dcache.restful.util.RequestUser;
+import org.dcache.restful.util.namespace.NamespaceUtils;
+import org.dcache.util.list.DirectoryEntry;
+import org.dcache.util.list.DirectoryStream;
+import org.dcache.util.list.ListDirectoryHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * RestFul API to  provide files/folders manipulation operations.
+ */
+@Api(value = "labels", authorizations = {@Authorization("basicAuth")})
+@Component
+@Path("/labels")
+public class LabelsResources {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LabelsResources.class);
+
+    /*
+     * Used to get fully qualified name of the client
+     * or the last proxy that sent the request.
+     * Later used for querying locality of the file.
+     */
+    @Context
+    private HttpServletRequest request;
+
+    @Inject
+    private PoolMonitor poolMonitor;
+
+    @Inject
+    private PathMapper pathMapper;
+
+    @Inject
+    private ListDirectoryHandler listDirectoryHandler;
+
+    @Inject
+    @Named("pool-manager-stub")
+    private CellStub poolmanager;
+
+    @Inject
+    @Named("pinManagerStub")
+    private CellStub pinmanager;
+
+    @Inject
+    @Named("pnfs-stub")
+    private CellStub pnfsmanager;
+
+
+    @GET
+    @ApiOperation(value = "Find metadata and optionally virtual directory contents.",
+          notes = "The method offers the possibility to list the content of a virtual"
+                + "directory for labels.")
+    @ApiResponses({
+          @ApiResponse(code = 401, message = "Unauthorized"),
+          @ApiResponse(code = 403, message = "Forbidden"),
+          @ApiResponse(code = 404, message = "Not Found"),
+          @ApiResponse(code = 500, message = "Internal Server Error"),
+    })
+    @Path("{path : .*}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonFileAttributes getFileAttributes(@ApiParam("Path of file or directory.")
+    @PathParam("path") String requestPath,
+          @ApiParam("Whether to include directory listing.")
+          @DefaultValue("false")
+          @QueryParam("children") boolean isList,
+          @ApiParam("List virtual dir.")
+          @DefaultValue("false")
+          @QueryParam("locality") boolean isLocality,
+          @ApiParam(value = "Whether to include replica locations.")
+          @QueryParam("locations") boolean isLocations,
+          @ApiParam(value = "Whether to include quality of service.")
+          @DefaultValue("false")
+          @QueryParam("qos") boolean isQos,
+          @ApiParam("Whether to include extended attributes.")
+          @QueryParam("xattr") boolean isXattr,
+          @ApiParam("Whether to include labels.")
+          @QueryParam("labels") boolean isLabels,
+          @ApiParam("Limit number of replies in directory listing.")
+          @QueryParam("limit") String limit,
+          @ApiParam("Number of entries to skip in directory listing.")
+          @QueryParam("offset") String offset) throws CacheException {
+        JsonFileAttributes fileAttributes = new JsonFileAttributes();
+        Set<FileAttribute> attributes =
+              NamespaceUtils.getRequestedAttributes(isLocality,
+                    isLocations,
+                    false,
+                    false);
+
+        FsPath path = pathMapper.asDcachePath(request, requestPath, ForbiddenException::new);
+
+        Range<Integer> range;
+        try {
+            int lower = (offset == null) ? 0 : Integer.parseInt(offset);
+            int ceiling = (limit == null) ? Integer.MAX_VALUE : Integer.parseInt(limit);
+            if (ceiling < 0 || lower < 0) {
+                throw new BadRequestException("limit and offset can not be less than zero.");
+            }
+            range = (Integer.MAX_VALUE - lower < ceiling) ? Range.atLeast(lower)
+                  : Range.closedOpen(lower, lower + ceiling);
+        } catch (NumberFormatException e) {
+            throw new BadRequestException("limit and offset must be an integer value.");
+        }
+        try {
+            List<JsonFileAttributes> children = new ArrayList<>();
+
+            DirectoryStream stream = listDirectoryHandler.listVirtualDirectory(
+                  HttpServletRequests.roleAwareSubject(request),
+                  HttpServletRequests.roleAwareRestriction(request),
+                  path,
+                  range,
+                  attributes);
+
+            for (DirectoryEntry entry : stream) {
+                String fPath = entry.getName();
+                String fName = entry.getName();
+
+
+                JsonFileAttributes childrenAttributes = new JsonFileAttributes();
+
+                NamespaceUtils.chimeraToJsonAttributes(fPath,
+                      childrenAttributes,
+                      entry.getFileAttributes(),
+                      isLocality, isLocations, isLabels,
+                      false, isXattr,
+                      request, poolMonitor);
+                childrenAttributes.setSourcePath(fPath);
+                childrenAttributes.setFileName(fName);
+
+                if (isQos) {
+                    NamespaceUtils.addQoSAttributes(childrenAttributes,
+                          entry.getFileAttributes(),
+                          request, poolMonitor, pinmanager);
+                }
+                children.add(childrenAttributes);
+            }
+
+            fileAttributes.setChildren(children);
+
+
+        } catch (FileNotFoundCacheException e) {
+            throw new NotFoundException(e);
+        } catch (PermissionDeniedCacheException e) {
+            if (RequestUser.isAnonymous()) {
+                throw new NotAuthorizedException(e);
+            } else {
+                throw new ForbiddenException(e);
+            }
+        } catch (CacheException | InterruptedException | NoRouteToCellException ex) {
+            LOGGER.warn(Exceptions.meaningfulMessage(ex));
+            throw new InternalServerErrorException(ex);
+        }
+        return fileAttributes;
+    }
+}

modules/dcache-frontend/src/main/resources/org/dcache/frontend/frontend.xml
@@ -563,6 +563,7 @@
     <bean class="org.dcache.restful.resources.doors.DoorsResources" scope="request"/>
     <bean class="org.dcache.restful.resources.identity.UserResource" scope="request"/>
     <bean class="org.dcache.restful.resources.namespace.FileResources" scope="request"/>
+    <bean class="org.dcache.restful.resources.labels.LabelsResources" scope="request"/>
     <bean class="org.dcache.restful.resources.namespace.IdResources" scope="request"/>
     <bean class="org.dcache.restful.resources.pool.PoolGroupInfoResources" scope="request"/>
     <bean class="org.dcache.restful.resources.pool.PoolInfoResources" scope="request">

modules/dcache/src/main/java/diskCacheV111/namespace/ForwardingNameSpaceProvider.java
@@ -155,6 +155,14 @@ public abstract class ForwardingNameSpaceProvider implements NameSpaceProvider {
         delegate().list(subject, path, glob, range, attrs, handler);
     }
 
+
+    @Override
+    public void listVirtualDirectory(Subject subject, String path, Range<Integer> range,
+                     Set<FileAttribute> attrs, ListHandler handler) throws CacheException
+    {
+        delegate().listVirtualDirectory(subject, path, range, attrs, handler);
+    }
+
     @Override
     public FsPath createUploadPath(Subject subject, FsPath path, FsPath rootPath,
           Long size, AccessLatency al, RetentionPolicy rp, String spaceToken,

commit 5c98be43ac79d037fb64d6086bcb0b1e9ea0a3a0

dcache-restful:fix the bug when setting PnfsListDirectoryMessage.PathType
Motivation

commit  https://github.com/dCache/dcache/commit/87408a17c04458894fdec86d344dd220ec67ba42

introduced a bug for the restful call on directory listing

the issue was that the PnfsListDirectoryMessage.PathType was set to LABELS and for some cases has been to updated back
to value "PATH",
which was wrong for the for the following call for non LABEL path:

curl  -v   -u admin:dickerelch   http://localhost:3880/api/v1/namespace/?children=true&offset=10&limit=100&qos=true

followed by

curl  -v   -u admin:dickerelch   http://localhost:3880/api/v1/labels/cat

then the same call to curl  -v   -u admin:dickerelch   http://localhost:3880/api/v1/namespace/?children=true&offset=10&limit=100&qos=true

was failing since the PathType=LABEL

Target: master
Acked-by: Paul

modules/dcache-vehicles/src/main/java/org/dcache/vehicles/PnfsListDirectoryMessage.java
@@ -51,7 +51,7 @@ public class PnfsListDirectoryMessage extends PnfsMessage {
      *  Path to directory to list
      *  or path to virtual directory to list (which is the label value of a streamed files)
      *  */
-    private static  PathType _pathType = PathType.PATH;
+    private PathType _pathType = PathType.PATH;
 
 
 
@@ -153,11 +153,11 @@ public class PnfsListDirectoryMessage extends PnfsMessage {
         _messageCount = messageCount;
     }
 
-    public static PathType getPathType() {
+    public PathType getPathType() {
         return _pathType;
     }
 
-    public static void setPathType(PathType pathType) {
+    public void setPathType(PathType pathType) {
         _pathType = pathType;
     }
 

modules/dcache/src/main/java/diskCacheV111/namespace/PnfsManagerV3.java
@@ -2175,9 +2175,10 @@ public class PnfsManagerV3
 
             if (msg.getPathType() == PnfsListDirectoryMessage.PathType.LABEL) {
                 _nameSpaceProvider.listVirtualDirectory(msg.getSubject(), path.substring(1),
-                        msg.getRange(),
-                        msg.getRequestedAttributes(),
-                        handler);
+                      msg.getRange(),
+                      msg.getRequestedAttributes(),
+                      handler);
+
             } else {
                 _nameSpaceProvider.list(msg.getSubject(), path,
                       msg.getPattern(),