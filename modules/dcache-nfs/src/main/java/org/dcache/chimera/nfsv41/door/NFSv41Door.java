5a001af33339992ac78300db66cc6ec4923da046
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2022-01-21 10:55:24 +0100

    nfs4.1: recall write layout on close
    Motivation:
    Due to race in the Linux kernel client, a server might see CLOSE before
    the last WRITE operation have been processed by a data server. Thus,
    recall the layout (enforce dirty page flushing) and return a NFS4ERR_DELAY.
    
    Modification:
    update open-state disposition call back to trigger layout recall for valid
    clients.
    
    Result:
    Triggering layout return if a valid pNFS client closes file withour
    returning layout.
    
    Acked-by: Paul Millar
    Acked-by: Dmitry Litvintsev
    Target: master
    Require-book: no
    Require-notes: yes

--- modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/NFSv41Door.java
+++ modules/dcache-nfs/src/main/java/org/dcache/chimera/nfsv41/door/NFSv41Door.java
@@ -744,7 +744,29 @@ public class NFSv41Door extends AbstractCellComponent implements
                         /*
                          * Cleanup transfer when state invalidated.
                          */
-                        t.shutdownMover();
+                        if (t.hasMover()) {
+                            if (client.getMinorVersion() > 0 && client.isLeaseValid()
+                                  && client.getCB() != null) {
+                                /*
+                                 * Due to race in the Linux kernel client, a server might see CLOSE before
+                                 * the last WRITE operation have been processed by a data server. Thus,
+                                 * recall the layout (enforce dirty page flushing) and return a NFS4ERR_DELAY.
+                                 *
+                                 * see: https://bugzilla.redhat.com/show_bug.cgi?id=1901524
+                                 */
+                                _log.warn(
+                                      "Deploying work-around for buggy client {} issuing CLOSE before LAYOUT_RETURN for transfer {}@{} of {}",
+                                      t.getMoverId(), t.getPool(), t.getPnfsId(),
+                                      t.getClient().getRemoteAddress());
+                                t.recallLayout(_callbackExecutor);
+                                throw new DelayException("Close before layoutreturn");
+                            } else {
+                                _log.warn("Removing orphan mover: {}@{} for {} by {}",
+                                      t.getMoverId(), t.getPool(), t.getPnfsId(),
+                                      t.getClient().getRemoteAddress());
+                                t.shutdownMover();
+                            }
+                        }
                         if (t.isWrite()) {
                             /* write request keep in the message map to
                              * avoid re-creates and trigger errors.
