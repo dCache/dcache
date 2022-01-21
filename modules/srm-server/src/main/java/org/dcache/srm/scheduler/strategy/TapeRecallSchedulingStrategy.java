2671c9ffae70abca03381d17ff583375602d1810
lemora <lea.morschel@desy.de>
2022-01-21 16:45:17 +0100

    srm-server: log trs tape queue states when tape requests are added
    Motivation:
    
    The tape recall scheduler groups jobs by tape and activates them according to configurable criteria.
    Active and pending tapes along with the number of associated jobs are of interest in order to understand the TRS state and scheduling options. This information is currently logged when a new tape is activated, which, depending on configuration, might happen a long time after jobs have been grouped by tape and are waiting for a minimum cooldown period to have passed before being selectable for activation.
    
    Modification:
    
    Log the pending and active tapes with associated job counts whenever a new batch of jobs is tape-associated. Refactor logging for DRYness.
    
    Result:
    
    Log tape queue states when new jobs are tape-associated.
    
    Target: master
    Target: 7.2
    Requires-notes: no
    Requires-book: no
    Patch: https://rb.dcache.org/r/13385/
    Acked-by: Tigran Mkrtchyan

--- modules/srm-server/src/main/java/org/dcache/srm/scheduler/strategy/TapeRecallSchedulingStrategy.java
+++ modules/srm-server/src/main/java/org/dcache/srm/scheduler/strategy/TapeRecallSchedulingStrategy.java
@@ -103,12 +103,7 @@ public class TapeRecallSchedulingStrategy implements SchedulingStrategy {
         }
 
         if (activatedTapes) {
-            if (!tapesWithJobs.isEmpty()) {
-                LOGGER.info("Pending tapes: {}", describeTapesWithJobsMap(tapesWithJobs));
-            }
-            if (!activeTapesWithJobs.isEmpty()) {
-                LOGGER.info("Active tapes: {}", describeTapesWithJobsMap(activeTapesWithJobs));
-            }
+            logTapesWithJobsState();
         }
 
         if (immediateJobQueue.isEmpty()) {
@@ -415,6 +410,10 @@ public class TapeRecallSchedulingStrategy implements SchedulingStrategy {
         Map<String, TapefileInfo> newTapeFileInfos = tapeInformant.getTapefileInfos(fileids);
         LOGGER.info("Retrieved tape info on {}/{} files", newTapeFileInfos.size(), fileids.size());
 
+        if (newTapeFileInfos.isEmpty()) {
+            return;
+        }
+
         Iterator<SchedulingItemJob> iterator = newJobs.iterator();
         SchedulingItemJob job;
         String fileid;
@@ -435,6 +434,8 @@ public class TapeRecallSchedulingStrategy implements SchedulingStrategy {
             }
         }
         sortTapeRequestQueues(changedTapeQueues);
+
+        logTapesWithJobsState();
     }
 
     /**
@@ -523,6 +524,15 @@ public class TapeRecallSchedulingStrategy implements SchedulingStrategy {
               e -> sb.append("(").append(e.getKey()).append(", ").append(e.getValue().size())
                     .append(") "));
         return sb.toString();
+    }
+
+    private void logTapesWithJobsState() {
+        if (!tapesWithJobs.isEmpty()) {
+            LOGGER.info("Pending tapes: {}", describeTapesWithJobsMap(tapesWithJobs));
+        }
+        if (!activeTapesWithJobs.isEmpty()) {
+            LOGGER.info("Active tapes: {}", describeTapesWithJobsMap(activeTapesWithJobs));
+        }
     }
 
 }
