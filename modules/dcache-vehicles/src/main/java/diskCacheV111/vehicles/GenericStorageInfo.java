dCache/dcache

commit 5d603029b210de2a970bb57c57e9e79a17aea0af

vehicles - clarify semantics of the StorageInfo#locations method.
Motivation:

A recent patch showed the semantics of the StorageInfo#locations method
was unclear.  In particular, the current code allowed modification of
the returned List; however, the JavaDoc didn't make it clear whether
this was allowed or not.

Modification:

Update JavaDoc to make the desired semantics clear.

Update implementation to ensure correct behaviour.

Result:

No user- or admin observable changes.  Code is hopefully clearer and
easier to maintain.

Target: master
Requires-notes: no
Requires-book: yes
Patch: https://rb.dcache.org/r/13406/
Acked-by: Marina Sahakyan

modules/dcache-vehicles/src/main/java/diskCacheV111/vehicles/GenericStorageInfo.java
@@ -7,6 +7,7 @@ import diskCacheV111.util.RetentionPolicy;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -30,6 +31,7 @@ public class GenericStorageInfo
 
     private Map<String, String> _keyHash = new HashMap<>();
     private List<URI> _locations = new ArrayList<>();
+    private List<URI> _locationsRO = Collections.unmodifiableList(_locations);
     private boolean _setHsm;
     private boolean _setStorageClass;
     private boolean _setBitFileId;
@@ -164,7 +166,7 @@ public class GenericStorageInfo
 
     @Override
     public List<URI> locations() {
-        return _locations;
+        return _locationsRO;
     }
 
     @Override

modules/dcache-vehicles/src/main/java/diskCacheV111/vehicles/StorageInfo.java
@@ -20,6 +20,8 @@ import java.util.Map;
  * data
  * <p>
  * example: osm://desy-main/?store=h1&bfid=1234 osm://desy-copy/?store=h1_d&bfid=5678
+ * <p>
+ * It is not guaranteed that a class implementing this interface is thread-safe.
  */
 public interface StorageInfo
       extends Serializable, Cloneable {
@@ -58,8 +60,16 @@ public interface StorageInfo
     String getBitfileId();
 
     /**
+     * Provide an unmodifiable view of the available locations for this file.  Direct modification
+     * of the returned List is not supported.
+     * <p>
+     * The returned object is a view, not a snapshot.  There are methods on StorageInfo that modify
+     * the list of locations for this file.  Any such change is reflected in subsequent queries of
+     * the returned List.
      * @return list of know locations
      * @since 1.8
+     * @see #clearLocations()
+     * @see #addLocation(java.net.URI)
      */
     List<URI> locations();
 

commit 1869af4fafe22edc56a83eb23cbd9f33369bcea7

vehicles: fix regression in GenericStorageInfo
Motivation:

Commit 5d603029b2 introduced a regression when handling a deserliased
StorageInfo from an earlier version of dCache.  Under these
circumstances, the new field is not initialised and is `null`.

Modification:

Remove the _locationsRO field.  This is an example of premature
optimisation.

Currently, GenericStorageInfo contains a memory optimisation for the
common case where a file has no tape locations.  This implementation is
broken (works-by-accident).

This patch fixes this, so the optimisation is applied uniformly and all
functions work at expected.

Remove various tests the check whether `_locations` is `null`.  There is
no way that `_locations` can be `null`, so these tests are redundant and
only make the code harder to read.

Result:

An unreleased regression is fixed.

Target: master
Requires-notes: no
Requires-book: no
Patch: https://rb.dcache.org/r/13427/
Acked-by: Tigran Mkrtchyan

modules/dcache-vehicles/src/main/java/diskCacheV111/vehicles/GenericStorageInfo.java
@@ -30,8 +30,11 @@ public class GenericStorageInfo
     private RetentionPolicy _retentionPolicy = StorageInfo.DEFAULT_RETENTION_POLICY;
 
     private Map<String, String> _keyHash = new HashMap<>();
-    private List<URI> _locations = new ArrayList<>();
-    private List<URI> _locationsRO = Collections.unmodifiableList(_locations);
+
+    // Use a singleton instance in order to reduce memory footprint for a common case, where a file
+    // has no tape locations.
+    private List<URI> _locations = List.of();
+
     private boolean _setHsm;
     private boolean _setStorageClass;
     private boolean _setBitFileId;
@@ -68,6 +71,9 @@ public class GenericStorageInfo
 
     @Override
     public void addLocation(URI newLocation) {
+        if (_locations.isEmpty()) {
+            _locations = new ArrayList<>();
+        }
         _locations.add(newLocation);
     }
 
@@ -158,20 +164,17 @@ public class GenericStorageInfo
 
     @Override
     public boolean isStored() {
-        /*
-         * FIXME: _locations!= null is needed to read old SI files
-         */
-        return _locations != null && !_locations.isEmpty();
+        return !_locations.isEmpty();
     }
 
     @Override
     public List<URI> locations() {
-        return _locationsRO;
+        return _locations.isEmpty() ? _locations : Collections.unmodifiableList(_locations);
     }
 
     @Override
     public void clearLocations() {
-        _locations.clear();
+        _locations = List.of();
     }
 
     @Override
@@ -232,10 +235,8 @@ public class GenericStorageInfo
                 sb.append(key).append('=').append(value).append(';');
             }
         }
-        if (_locations != null) {
-            for (URI location : _locations) {
-                sb.append(location).append(';');
-            }
+        for (URI location : _locations) {
+            sb.append(location).append(';');
         }
         return sb.toString();
     }
@@ -338,7 +339,9 @@ public class GenericStorageInfo
         try {
             GenericStorageInfo copy = (GenericStorageInfo) super.clone();
             copy._keyHash = new HashMap<>(_keyHash);
-            copy._locations = new ArrayList<>(_locations);
+            copy._locations = _locations.isEmpty()
+                    ? List.of()
+                    : new ArrayList<>(_locations);
             return copy;
         } catch (CloneNotSupportedException e) {
             throw new RuntimeException("Failed to clone storage info: " +
@@ -431,7 +434,6 @@ public class GenericStorageInfo
         }
 
         if (_locations == null || _locations.isEmpty()) {
-            // immutable list is ok as only PnfsManager initially populates the locations.
             _locations = List.of();
         }
     }