dCache/dcache

commit 3351df28abfb6aada436723c0a01af42409c0827

frontend: make srr resources public
Motivation:
The access to Storage Resource Reporting is limited to localhost only.
Turned out, that the majority of site admins want to see it pubic.

Modification:
control SRR resource restriction with

```
frontend.srr.public=true|false
```

property. The default is false, to provide backward compatibility with
existing implementation.

Result:
The srr endpoint is publicly accessible, if desired.

Fixes: #6181
Acked-by: Paul Millar
Acked-by: Lea Morschel
Target: master, 8.0, 7.2, 7.1, 7.0, 6.2
Require-book: yes
Require-notes: yes

modules/dcache-frontend/src/main/java/org/dcache/restful/resources/srr/SrrResource.java
@@ -64,6 +64,11 @@ public class SrrResource {
 
     private boolean spaceReservationEnabled;
 
+    /**
+     * If false, then SRR requests are accepted only from loopback interface.
+     */
+    private boolean isPublic;
+
     public void setQuality(String quality) {
         this.quality = quality;
     }
@@ -86,6 +91,10 @@ public class SrrResource {
 
     public void setArchitecture(String architecture) {
         this.architecture = architecture;
+    }
+
+    public void setIsPublic(boolean isPublic) {
+        this.isPublic = isPublic;
     }
 
     public void setGroupMapping(String mapping) {
@@ -115,9 +124,11 @@ public class SrrResource {
     @Path("/")
     public Response getSrr() throws InterruptedException, CacheException, NoRouteToCellException {
 
-        InetAddress remoteAddress = InetAddresses.forUriString(request.getRemoteAddr());
-        if (!remoteAddress.isLoopbackAddress()) {
-            throw new ForbiddenException();
+        if (isPublic) {
+            InetAddress remoteAddress = InetAddresses.forUriString(request.getRemoteAddr());
+            if (!remoteAddress.isLoopbackAddress()) {
+                throw new ForbiddenException();
+            }
         }
 
         SrrRecord record = SrrBuilder.builder()

modules/dcache-frontend/src/main/resources/org/dcache/frontend/frontend.xml
@@ -597,6 +597,7 @@
         <property name="architecture" value="${info-provider.dcache-architecture}" />
         <property name="quality" value="${info-provider.dcache-quality-level}" />
         <property name="doorTag" value="${storage-descriptor.door.tag}" />
+        <property name="isPublic" value="${frontend.srr.public}" />
     </bean>
 
     <beans profile="macaroons-true">

skel/share/defaults/frontend.properties
@@ -616,6 +616,10 @@ frontend.version.swagger-ui = @version.swagger-ui@
 #  cms-user:/cms,default:/cms,default:/atlas
 frontend.srr.shares =
 
+#
+# Should SRR information be restricted to localhost only
+#
+(one-of?true|false)frontend.srr.public=false
 
 (obsolete)frontend.dcache-view.endpoints.webapi = Use frontend.static!dcache-view.endpoints.webapi instead
 (obsolete)frontend.dcache-view.endpoints.webdav = Use frontend.static!dcache-view.endpoints.webdav instead

commit 5001ee852bbb4eb2ffca26b3349f603b37b23787

frontend: fix problem with making SRR resources public
Motivation:
Commit 3351df28ab introduced an option that allows making the SRR resources public. The patch contained a mistake, which inverts the logic of the property, so that public access is allowed when `frontend.srr.public` option is set to false, not true.

Modification:
Invert the faulty logic so that now `frontend.srr.public=true` means "allow public access" and false does not.

Result:
The SSR public access property works as intended.

Target: master
Request: 8.0, 7.2, 7.1, 7.0, 6.2
Fixes: #6181
Requires-notes: yes
Requires-book: no
Patch: https://rb.dcache.org/r/13488/
Acked-by: Tigran Mkrtchyan

modules/dcache-frontend/src/main/java/org/dcache/restful/resources/srr/SrrResource.java
@@ -124,7 +124,7 @@ public class SrrResource {
     @Path("/")
     public Response getSrr() throws InterruptedException, CacheException, NoRouteToCellException {
 
-        if (isPublic) {
+        if (!isPublic) {
             InetAddress remoteAddress = InetAddresses.forUriString(request.getRemoteAddr());
             if (!remoteAddress.isLoopbackAddress()) {
                 throw new ForbiddenException();