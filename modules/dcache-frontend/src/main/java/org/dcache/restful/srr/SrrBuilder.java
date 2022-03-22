dCache/dcache

commit 9c85dab02cb294e1ed0db6d80bb266a45bde295a

srr: limit publisched endpoints to GLOBAL scope
Motivation:
When SRR publishes a door, then it expected that the door is accessible
by others. Thus we should publish only globally accessible interfaces.

Modification:
Update SrrBuilder to filter doors with GLOBAL scope.

Result:
The link-local and local host interfaces are not published.

Ticket: #10249
Acked-by: Paul Millar
Target: master, 7.2, 7.1, 7.0, 6.2
Require-book: no
Require-notes: yes

modules/dcache-frontend/src/main/java/org/dcache/restful/srr/SrrBuilder.java
@@ -20,6 +20,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.dcache.cells.CellStub;
 import org.dcache.poolmanager.PoolMonitor;
+import org.dcache.util.NetworkUtils.InetAddressScope;
 import org.dcache.util.Version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -237,6 +238,7 @@ public class SrrBuilder {
 
         return loginBrokerSubscriber.doors().stream()
               .filter(doorTagFilter)
+              .filter(i -> i.supports(InetAddressScope.GLOBAL))
               .map(d -> {
                         Storageendpoint endpoint = new Storageendpoint()
                               .withName(id + "#" + d.getProtocolFamily() + "@" + d.getAddresses().get(0)

commit c33f75b45e261a53b45311b64e945a7c68a7f7db

srr: don't publish link local addresses (v2)
Fixes: 9c85dab02

Motivation:
The LoginBrokerInfo contains all addresses published by a door,
for example, doors GLOBAL and LinkLocal IPv6 addresses. The corresponding
`LoginBrokerInfo.supports(InetAddressScope.GLOBAL)` call will return
`true`, however, the following `LoginBrokerInfo.getAddresses().get(0)`
might return any of addresses. Thus, a LinkLocal address might be
published.

Modification:
Update SrrBuilder to filter out any non global addresses before
publishing.

Result:
No LinkLocal addresses for endpoint URL;

Fixes: #6534
Acked-by: Lea Morschel
Acked-by: Paul Millar
Target: master, 8.0, 7.2
Require-book: no
Require-notes: yes

modules/dcache-frontend/src/main/java/org/dcache/restful/srr/SrrBuilder.java
@@ -20,6 +20,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.dcache.cells.CellStub;
 import org.dcache.poolmanager.PoolMonitor;
+import org.dcache.util.NetworkUtils;
 import org.dcache.util.NetworkUtils.InetAddressScope;
 import org.dcache.util.Version;
 import org.slf4j.Logger;
@@ -240,12 +241,23 @@ public class SrrBuilder {
               .filter(doorTagFilter)
               .filter(i -> i.supports(InetAddressScope.GLOBAL))
               .map(d -> {
+                        /*
+                         * LoginBrokerInfo entry might contain multiple addresses (IPv4, IPv6, LinkLocal).
+                         * Thus we should tape only the one with global scope.
+                         *
+                         * REVISIT: this functionality probably should go into LoginBrokerInfo
+                         */
+                        var globalAddress = d.getAddresses().stream()
+                              .filter(a -> NetworkUtils.InetAddressScope.of(a).ordinal() >= InetAddressScope.GLOBAL.ordinal())
+                              .findAny()
+                              .orElseThrow(RuntimeException::new);
+
                         Storageendpoint endpoint = new Storageendpoint()
-                              .withName(id + "#" + d.getPreferredProtocolFamily() + "@" + d.getAddresses().get(0)
+                              .withName(id + "#" + d.getPreferredProtocolFamily() + "@" + globalAddress
                                     .getCanonicalHostName() + "-" + d.getPort())
                               .withInterfacetype(d.getPreferredProtocolFamily())
                               .withInterfaceversion(d.getProtocolVersion())
-                              .withEndpointurl(d.getPreferredProtocolFamily() + "://" + d.getAddresses().get(0)
+                              .withEndpointurl(d.getPreferredProtocolFamily() + "://" + globalAddress
                                     .getCanonicalHostName() + ":" + d.getPort() + d.getRoot())
                               .withAssignedshares(Collections.singletonList("all"));
 