7f1cdae61b351ad42a3809bc6e73a2e67c64d3dc
Paul Millar <paul.millar@desy.de>
2022-09-06 13:57:57 +0200

    gplazma: fix year in copyright statement
    Motivation:
    
    Copyright statement contains the wrong year.
    
    Modification:
    
    Remove rogue character.
    
    Result:
    
    No user- or admin observable changes.  The copyright statement is now
    correct.
    
    Target: master
    Requires-notes: no
    Requires-book: no
    Patch: https://rb.dcache.org/r/13659/
    Acked-by: Lea Morschel

--- modules/common/src/main/java/org/dcache/util/files/ParsableFile.java
+++ modules/common/src/main/java/org/dcache/util/files/ParsableFile.java
@@ -1,6 +1,6 @@
 /* dCache - http://www.dcache.org/
  *
- * Copyright (C) 20212-2022 Deutsches Elektronen-Synchrotron
+ * Copyright (C) 2021-2022 Deutsches Elektronen-Synchrotron
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
