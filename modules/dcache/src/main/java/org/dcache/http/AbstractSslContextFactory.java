b63079bd046d6803115589a918f58a0156f991f3
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2023-01-06 10:25:26 +0100

    pool: reload host certificate if changed
    Motivation:
    When host certificate is changed dCache should reload the host
    certificate and key to avoid service reload. Unfortunately, the
    https mover created with a specific SslContext, thus never reloads
    host credential, even if the under laying factory supposed to do so.
    
    Modification:
    Update HttpsTransferService to use Callable<SslContext> instead of
    SslContext. Introduce JdkSslContextFactory that created JDK SSLContext
    that is used by p2p transfers.
    
    Result:
    Updated host certificate is available to https mover. Fixed p2p
    transfers over HTTPS.
    
    Fixes: #6929, #6939
    Acked-by: Paul Millar
    Acked-by: Marina Sahakyan
    Target: master, 8.2
    Require-book: no
    Require-notes: yes
    (cherry picked from commit aa348d48fad1d77eab694bc1458b08012cf1fcf9)
    (cherry picked from commit fc604fc0b9e01c4036c8c261a86ed90a8c68ab96)
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- modules/dcache/src/main/java/org/dcache/http/AbstractSslContextFactory.java
+++ modules/dcache/src/main/java/org/dcache/http/AbstractSslContextFactory.java
@@ -1,6 +1,6 @@
 /* dCache - http://www.dcache.org/
  *
- * Copyright (C) 2021 Deutsches Elektronen-Synchrotron
+ * Copyright (C) 2021 - 2023 Deutsches Elektronen-Synchrotron
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -17,19 +17,28 @@
  */
 package org.dcache.http;
 
+import dmg.cells.nucleus.CDC;
 import eu.emi.security.authn.x509.CrlCheckingMode;
 import eu.emi.security.authn.x509.OCSPCheckingMode;
 import java.nio.file.Path;
-import org.springframework.beans.factory.FactoryBean;
+import java.util.concurrent.Callable;
+import javax.annotation.PostConstruct;
+import org.dcache.ssl.CanlContextFactory;
 import org.springframework.beans.factory.annotation.Required;
 
-abstract class AbstractSslContextFactoryBean<C> implements FactoryBean<C> {
+abstract class AbstractSslContextFactory<C> implements Callable<C> {
 
     protected Path serverCertificatePath;
     protected Path serverKeyPath;
     protected Path serverCaPath;
     protected CrlCheckingMode crlCheckingMode;
     protected OCSPCheckingMode ocspCheckingMode;
+
+    private final Class type;
+
+    protected AbstractSslContextFactory(Class<? extends Object> type) {
+        this.type = type;
+    }
 
     @Required
     public void setServerCertificatePath(Path serverCertificatePath) {
@@ -56,8 +65,24 @@ abstract class AbstractSslContextFactoryBean<C> implements FactoryBean<C> {
         this.ocspCheckingMode = ocspCheckingMode;
     }
 
+    private Callable<C> contextHolder;
+
+    @PostConstruct
+    public void init() throws Exception {
+        contextHolder = CanlContextFactory.custom()
+              .withCertificateAuthorityPath(serverCaPath)
+              .withCrlCheckingMode(crlCheckingMode)
+              .withOcspCheckingMode(ocspCheckingMode)
+              .withCertificatePath(serverCertificatePath)
+              .withKeyPath(serverKeyPath)
+              .withLazy(false)
+              .withLoggingContext(new CDC()::restore)
+              .withLoggingContext(new CDC()::restore)
+              .buildWithCaching(type);
+    }
+
     @Override
-    public boolean isSingleton() {
-        return false;
+    public C call() throws Exception {
+        return contextHolder.call();
     }
 }
