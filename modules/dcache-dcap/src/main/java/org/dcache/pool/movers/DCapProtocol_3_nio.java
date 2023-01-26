6ce991fd5a4fcf8bcb85b7fe1da5696896fd92c7
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2023-01-26 17:52:09 +0100

    pool: remove active dcap mover support
    Motivation:
    As DCAP door always start mover in passive mode the active mover support
    become a dead-code, thus should be removed.
    
    Modification:
    remove active dcap mover support
    
    Result:
    dead code removed.
    
    Acked-by: Albert Rossi
    Target: master
    Require-book: no
    Require-notes: yes

--- modules/dcache-dcap/src/main/java/org/dcache/pool/movers/DCapProtocol_3_nio.java
+++ modules/dcache-dcap/src/main/java/org/dcache/pool/movers/DCapProtocol_3_nio.java
@@ -273,69 +273,31 @@ public class DCapProtocol_3_nio implements MoverProtocol, ChecksumMover, CellArg
 
         _sessionId = dcapProtocolInfo.getSessionId();
 
-        if (!dcapProtocolInfo.isPassive()) {
+        try (Listen listen = factory.acquireListen(bufferSize.getRecvBufferSize())) {
+            InetAddress localAddress = NetworkUtils.
+                  getLocalAddress(dcapProtocolInfo.getSocketAddress().getAddress());
+            InetSocketAddress socketAddress = new InetSocketAddress(localAddress,
+                  listen.getPort());
 
-            socketChannel = SocketChannel.open();
-            socketChannel.configureBlocking(true);
+            byte[] challenge = UUID.randomUUID().toString().getBytes();
+            PoolPassiveIoFileMessage<byte[]> msg = new PoolPassiveIoFileMessage<>("pool",
+                  socketAddress, challenge);
+            msg.setId(dcapProtocolInfo.getSessionId());
+            _log.info("waiting for client to connect ({}:{})", localAddress,
+                  listen.getPort());
 
-            Socket socket = socketChannel.socket();
-            socket.setKeepAlive(true);
-            socket.setTcpNoDelay(true);
-            if (bufferSize.getRecvBufferSize() > 0) {
-                socket.setReceiveBufferSize(bufferSize
-                      .getRecvBufferSize());
-            }
-            if (bufferSize.getSendBufferSize() > 0) {
-                socket.setSendBufferSize(bufferSize
-                      .getSendBufferSize());
-            }
+            CellPath cellpath = dcapProtocolInfo.door();
+            _cell.sendMessage(new CellMessage(cellpath, msg));
+            DCapProrocolChallenge dcapChallenge = new DCapProrocolChallenge(_sessionId,
+                  challenge);
+            socketChannel = listen.getSocket(dcapChallenge);
+        }
 
-            socketChannel.connect(dcapProtocolInfo.getSocketAddress());
-
-            if (_logSocketIO.isDebugEnabled()) {
-                _logSocketIO.debug("Socket OPEN remote = {}:{} local = {}:{}",
-                      socket.getInetAddress(), socket.getPort(),
-                      socket.getLocalAddress(), socket.getLocalPort());
-            }
-            _log.info("Using : Buffer Sizes (send/recv/io) : {}/{}/{}", socket.getSendBufferSize(),
-                  socket.getReceiveBufferSize(),
-                  _bigBuffer.capacity());
-            _log.info("Connected to {}", dcapProtocolInfo.getSocketAddress());
-
-            //
-            // send the sessionId and our (for now) 0 byte security challenge.
-            //
-            _bigBuffer.clear();
-            _bigBuffer.putInt(_sessionId).putInt(0);
-            _bigBuffer.flip();
-            socketChannel.write(_bigBuffer);
-        } else { // passive connection
-            try (Listen listen = factory.acquireListen(bufferSize.getRecvBufferSize())) {
-                InetAddress localAddress = NetworkUtils.
-                      getLocalAddress(dcapProtocolInfo.getSocketAddress().getAddress());
-                InetSocketAddress socketAddress = new InetSocketAddress(localAddress,
-                      listen.getPort());
-
-                byte[] challenge = UUID.randomUUID().toString().getBytes();
-                PoolPassiveIoFileMessage<byte[]> msg = new PoolPassiveIoFileMessage<>("pool",
-                      socketAddress, challenge);
-                msg.setId(dcapProtocolInfo.getSessionId());
-                _log.info("waiting for client to connect ({}:{})", localAddress,
-                      listen.getPort());
-
-                CellPath cellpath = dcapProtocolInfo.door();
-                _cell.sendMessage(new CellMessage(cellpath, msg));
-                DCapProrocolChallenge dcapChallenge = new DCapProrocolChallenge(_sessionId,
-                      challenge);
-                socketChannel = listen.getSocket(dcapChallenge);
-            }
-
-            Socket socket = socketChannel.socket();
-            socket.setKeepAlive(true);
-            socket.setTcpNoDelay(true);
-            if (bufferSize.getSendBufferSize() > 0) {
-                socket.setSendBufferSize(bufferSize.getSendBufferSize());
-            }
+        Socket socket = socketChannel.socket();
+        socket.setKeepAlive(true);
+        socket.setTcpNoDelay(true);
+        if (bufferSize.getSendBufferSize() > 0) {
+            socket.setSendBufferSize(bufferSize.getSendBufferSize());
         }
 
         //
