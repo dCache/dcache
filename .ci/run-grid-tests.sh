c425a62257315a46d6a8c3f82e82dbf5c438850f
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2023-08-24 15:21:45 +0200

    ci: add first robot test

--- .ci/run-grid-tests.sh
+++ .ci/run-grid-tests.sh
@@ -2,3 +2,33 @@
 
 . /init-grid-ui.sh
 
+yum install -y -q git-core python-pip boost-python
+
+# version that works with centos7
+pip install robotframework==3.2.2
+
+git clone --depth 1 https://github.com/dCache/Grid-tools-functional-test-suite.git
+cd Grid-tools-functional-test-suite
+
+export DFTS_SUT=store-door-svc.$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace).svc.cluster.local
+export HTTP_PORT=2880
+export SRM_PORT=8443
+export GSIDCAP_PORT=22128
+export GSIFTP_PORT=2811
+export DCAP_PORT=22125
+export REMOTE_DIR=/data/g2/
+export WORKSPACE=`pwd`
+
+
+# test groups to run
+TESTS="DccpTests GlobusurlcpTests"
+
+# robot returns the number of failed tests in its return code.
+# So we add up the retvals using ERRORS as an accumulator
+declare -i ERRORS=0
+
+for name in $TESTS; do
+  robot -o ${name}_output --variable SRM_VERSION:2 --name ${name} -x ${name}-junit.xml ${name}.robot || ERRORS+=$?
+done
+
+exit $ERRORS
