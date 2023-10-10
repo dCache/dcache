dCache/dcache

commit 0b4140b454b819de55c7c412539294537ff0beb8

dcache-bulk: refine container executor model
Motivation:

In https://rb.dcache.org/r/14115
master@8a5c358af45586383d87873952407c646f81f4c6

the container executor model was made more like SRM
by using an unbounded cached executor.  While this
brought Bulk performance in line with SRM, it also
drives memory usage to be very near to physical
memory.  In JFR, after a sustained usage/submission
of about 1700 requests of 10K targets each over 6
hours, one can observe this warning (see attached).

However, a totally unbounded executor is not necessary.
All that is required is an executor which has enough
slots to accommodate the in-flight semaphore.

Modification:

Split the pooled executor into a main and a callback
executor, each initialized with max threads equal
to the in-flight semaphore value.  Use the callback
executor exclusively for message/activity callbacks
(instead of direct executor).

Result:

Same performance as before, but with a total memory
footprint at a little more than half available
physical memory instead of being very close to it.

Target: master
Request: 9.2
Requires-notes: yes
Patch: https://rb.dcache.org/r/14118/
Acked-by: Tigran
Acked-by: Lea

modules/dcache-bulk/src/main/java/org/dcache/services/bulk/job/BulkRequestContainerJob.java
@@ -73,7 +73,6 @@ import com.google.common.base.Throwables;
 import com.google.common.collect.Range;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.MoreExecutors;
 import diskCacheV111.util.CacheException;
 import diskCacheV111.util.FsPath;
 import diskCacheV111.util.NamespaceHandlerAware;
@@ -476,7 +475,7 @@ public final class BulkRequestContainerJob
                         remove();
                     }
                 }
-            }, MoreExecutors.directExecutor());
+            }, callbackExecutor);
         }
 
         /**
@@ -510,7 +509,7 @@ public final class BulkRequestContainerJob
                         remove();
                     }
                 }
-            }, MoreExecutors.directExecutor());
+            }, callbackExecutor);
         }
 
         /**
@@ -563,7 +562,7 @@ public final class BulkRequestContainerJob
             try {
                 activityFuture = activity.perform(ruid, id == null ? seqNo : id, path, attributes);
                 if (async) {
-                    activityFuture.addListener(() -> handleCompletion(), executor);
+                    activityFuture.addListener(() -> handleCompletion(), callbackExecutor);
                 }
             } catch (BulkServiceException | UnsupportedOperationException e) {
                 LOGGER.error("{}, perform failed for {}: {}", ruid, target, e.getMessage());
@@ -665,6 +664,7 @@ public final class BulkRequestContainerJob
     private SignalAware callback;
     private Thread runThread;
     private ExecutorService executor;
+    private ExecutorService callbackExecutor;
     private Semaphore dirListSemaphore;
     private Semaphore inFlightSemaphore;
 
@@ -833,6 +833,10 @@ public final class BulkRequestContainerJob
 
     public void setListHandler(ListDirectoryHandler listHandler) {
         this.listHandler = listHandler;
+    }
+
+    public void setCallbackExecutor(ExecutorService callbackExecutor) {
+        this.callbackExecutor = callbackExecutor;
     }
 
     public void setExecutor(ExecutorService executor) {

modules/dcache-bulk/src/main/java/org/dcache/services/bulk/manager/ConcurrentRequestManager.java
@@ -92,6 +92,7 @@ import org.dcache.services.bulk.store.BulkTargetStore;
 import org.dcache.services.bulk.util.BulkRequestTarget;
 import org.dcache.services.bulk.util.BulkRequestTarget.State;
 import org.dcache.services.bulk.util.BulkServiceStatistics;
+import org.dcache.util.BoundedCachedExecutor;
 import org.dcache.util.FireAndForgetTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -310,9 +311,14 @@ public final class ConcurrentRequestManager implements BulkRequestManager {
     private ExecutorService processorExecutorService;
 
     /**
+     * Thread dedicated to job callbacks.
+     */
+    private ExecutorService callbackExecutor;
+
+    /**
      * Thread dedicated to jobs.
      */
-    private ExecutorService pooledExecutorService;
+    private ExecutorService executorService;
 
     /**
      * Records number of jobs and requests processed.
@@ -357,7 +363,6 @@ public final class ConcurrentRequestManager implements BulkRequestManager {
         schedulerProvider.initialize();
         processor = new ConcurrentRequestProcessor(schedulerProvider.getRequestScheduler());
         processorExecutorService = Executors.newSingleThreadScheduledExecutor();
-        pooledExecutorService = Executors.newCachedThreadPool();
         processorFuture = processorExecutorService.submit(processor);
     }
 
@@ -421,8 +426,18 @@ public final class ConcurrentRequestManager implements BulkRequestManager {
     }
 
     @Required
+    public void setCallbackExecutor(BoundedCachedExecutor callbackExecutor) {
+        this.callbackExecutor = callbackExecutor;
+    }
+
+    @Required
     public void setCompletionHandler(BulkRequestCompletionHandler completionHandler) {
         this.completionHandler = completionHandler;
+    }
+
+    @Required
+    public void setExecutor(BoundedCachedExecutor pooledExecutor) {
+        this.executorService = pooledExecutor;
     }
 
     @Required
@@ -518,12 +533,13 @@ public final class ConcurrentRequestManager implements BulkRequestManager {
         String key = job.getTarget().getKey();
         LOGGER.trace("submitting job {} to executor, target {}.", key,
               job.getTarget());
-        job.setExecutor(pooledExecutorService);
+        job.setExecutor(executorService);
+        job.setCallbackExecutor(callbackExecutor);
         job.setCallback(this);
         try {
             if (isJobValid(job)) { /* possibly cancelled in flight */
                 job.update(State.RUNNING);
-                pooledExecutorService.submit(new FireAndForgetTask(job));
+                executorService.submit(new FireAndForgetTask(job));
             }
         } catch (RuntimeException e) {
             job.getTarget().setErrorObject(e);

modules/dcache-bulk/src/main/resources/org/dcache/services/bulk/bulk.xml
@@ -243,6 +243,16 @@
     <property name="submissionHandler" ref="request-handler"/>
     <property name="schedulerProvider" ref="scheduler-provider"/>
     <property name="maxActiveRequests" value="${bulk.limits.container-processing-threads}"/>
+    <property name="executor">
+      <bean class="org.dcache.util.BoundedCachedExecutor">
+        <constructor-arg value="${bulk.limits.in-flight-semaphore}"/>
+      </bean>
+    </property>
+    <property name="callbackExecutor">
+      <bean class="org.dcache.util.BoundedCachedExecutor">
+        <constructor-arg value="${bulk.db.connections.max}"/>
+      </bean>
+    </property>
     <property name="timeout" value="${bulk.limits.sweep-interval}"/>
     <property name="timeoutUnit" value="${bulk.limits.sweep-interval.unit}"/>
   </bean>

commit ce9734a123c0dd26c09ff7c7965c2ee6885f5c3b

dcache-bulk: fix cancellation issues
Motivation:

Changes introduced by:

https://rb.dcache.org/r/14115
master@8a5c358af45586383d87873952407c646f81f4c6

and

https://rb.dcache.org/r/14118/
master@0b4140b454b819de55c7c412539294537ff0beb8

inadvertently introduced a regression in
request cancellation.   In addition, the
semaphore release was not always taking
place.

Modification:

The semaphore issue is addressed by
selective modifications of synchronization
on the `running` map.

The problem with cancellation (hanging
perpetually in the CANCELLING state)
was solved by eliminating the re-entrance
on the synchronization block when
cancelling running tasks.  We have
also modified the `cancel` procedure
to interrupt the main thread first and
then set container state.  We also
changed the main target loops not
to throw the interrupted exception
but simply to stop processing.

Result:

Testing of cancellations for
both recursive and non-recursive
tasks show correctness of semaphore
count and completion of the cancellation.

Target: master
Request: 9.2 (a definite bug!)
Patch: https://rb.dcache.org/r/14127/
Depends-on: #14126
Requires-notes: yes (if 9.2 released before this is applied)
Acked-by: Tigran

modules/dcache-bulk/src/main/java/org/dcache/services/bulk/job/BulkRequestContainerJob.java
@@ -94,6 +94,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 import javax.security.auth.Subject;
@@ -203,16 +204,12 @@ public final class BulkRequestContainerJob
      */
     abstract class ContainerTask implements Runnable {
         final Consumer<Throwable> errorHandler = e -> uncaughtException(Thread.currentThread(), e);
-        final long seqNo;
+        final long seqNo = taskCounter.getAndIncrement();
+        final AtomicBoolean holdingPermit = new AtomicBoolean(false);
 
         Future taskFuture;
-        boolean holdingPermit;
         ExecutorService taskExecutor;
         Semaphore taskSemaphore;
-
-        ContainerTask() {
-            seqNo = taskCounter.getAndIncrement();
-        }
 
         public void run() {
             try {
@@ -232,44 +229,58 @@ public final class BulkRequestContainerJob
         void cancel() {
             if (taskFuture != null) {
                 taskFuture.cancel(true);
+                LOGGER.debug("{} - task future cancelled {}.", ruid, seqNo);
             }
+
             remove();
         }
 
         void expandDepthFirst(Long id, PID pid, FsPath path, FileAttributes dirAttributes)
-              throws BulkServiceException, CacheException, InterruptedException {
+              throws BulkServiceException, CacheException {
             LOGGER.debug("{} - expandDepthFirst, {}, {}, {}, {}", ruid, id, pid, path, dirAttributes);
-            new DirListTask(id, pid, path, dirAttributes).submitAsync();
+            try {
+                new DirListTask(id, pid, path, dirAttributes).submitAsync();
+            } catch (InterruptedException e) {
+                LOGGER.trace("{} - expandDepthFirst {} interrupted.", ruid, id);
+            }
         }
 
         void submitAsync() throws InterruptedException {
-            checkForRequestCancellation();
-
-            if (!holdingPermit) {
+            /*
+             *  Acquisition must be done outside the synchronized block (running),
+             *  else there could be a deadlock.
+             */
+            if (holdingPermit.compareAndSet(false, true)) {
                 taskSemaphore.acquire();
-                holdingPermit = true;
             }
 
             synchronized (running) {
-                running.put(seqNo, this);
-                LOGGER.debug("{} - submitAsync {}, task count is now {}.", ruid, seqNo, running.size());
-            }
+                if (jobTarget.isTerminated()) {
+                    taskSemaphore.release();
+                    return;
+                }
 
-            taskFuture = taskExecutor.submit(this);
+                running.put(seqNo, this);
+                LOGGER.debug("{} - submitAsync {}, task count is now {}.", ruid, seqNo,
+                      running.size());
+
+                taskFuture = taskExecutor.submit(this);
+            }
         }
 
         void remove() {
+            if (holdingPermit.compareAndSet(true, false)) {
+                taskSemaphore.release();
+            }
+
             synchronized (running) {
                 running.remove(seqNo);
                 LOGGER.debug("{} - remove task {}, task count now {}.", ruid, seqNo, running.size());
-            }
 
-            if (holdingPermit) {
-                taskSemaphore.release();
-                holdingPermit = false;
+                if (running.isEmpty()) {
+                    checkTransitionToDirs();
+                }
             }
-
-            checkTransitionToDirs();
         }
 
         abstract void doInner() throws Throwable;
@@ -392,10 +403,12 @@ public final class BulkRequestContainerJob
         void cancel() {
             if (activityFuture != null) {
                 activityFuture.cancel(true);
+                LOGGER.debug("{} - activity future cancelled for task {}.", ruid, seqNo);
             }
 
             if (target != null) {
                 activity.cancel(target);
+                LOGGER.debug("{} - target cancelled for task {}.", ruid, seqNo);
             }
 
             super.cancel();
@@ -441,7 +454,7 @@ public final class BulkRequestContainerJob
         /**
          *  (1) symlink resolution on initial targets; bypassed for discovered targets.
          */
-        private void resolvePath() throws InterruptedException {
+        private void resolvePath() {
             LOGGER.debug("{} - resolvePath, resolving {}", ruid, target.getPath());
             PnfsResolveSymlinksMessage message = new PnfsResolveSymlinksMessage(
                   target.getPath().toString(), null);
@@ -464,14 +477,8 @@ public final class BulkRequestContainerJob
                 @Override
                 public void failure(int rc, Object error) {
                     LOGGER.error("{} - resolvePath, callback failure for {}.", ruid, target);
-                    try {
-                        storeOrUpdate(CacheExceptionFactory.exceptionOf(
-                              rc, Objects.toString(error, null)));
-                    } catch (InterruptedException e) {
-                        errorHandler.accept(e);
-                    } finally {
-                        remove();
-                    }
+                    storeOrUpdate(CacheExceptionFactory.exceptionOf(
+                          rc, Objects.toString(error, null)));
                 }
             }, callbackExecutor);
         }
@@ -479,7 +486,7 @@ public final class BulkRequestContainerJob
         /**
          *  (2) retrieval of required file attributes.
          */
-        private void fetchAttributes() throws InterruptedException {
+        private void fetchAttributes() {
             LOGGER.debug("{} - fetchAttributes for path {}", ruid, target.getPath());
             PnfsGetFileAttributes message = new PnfsGetFileAttributes(target.getPath().toString(),
                   MINIMALLY_REQUIRED_ATTRIBUTES);
@@ -498,14 +505,8 @@ public final class BulkRequestContainerJob
                 @Override
                 public void failure(int rc, Object error) {
                     LOGGER.error("{} - fetchAttributes, callback failure for {}.", ruid, target);
-                    try {
-                        storeOrUpdate(CacheExceptionFactory.exceptionOf(
-                              rc, Objects.toString(error, null)));
-                    } catch (InterruptedException e) {
-                        errorHandler.accept(e);
-                    } finally {
-                        remove();
-                    }
+                    storeOrUpdate(CacheExceptionFactory.exceptionOf(
+                          rc, Objects.toString(error, null)));
                 }
             }, callbackExecutor);
         }
@@ -552,8 +553,8 @@ public final class BulkRequestContainerJob
 
             storeOrUpdate(null);
 
-            if (hasBeenCancelled(this)) {
-                LOGGER.debug("{} - performActivity hasBeenCancelled for {}.", ruid, path);
+            if (hasBeenSpecificallyCancelled(this)) {
+                LOGGER.debug("{} - performActivity hasBeenSpecificallyCancelled for {}.", ruid, path);
                 remove();
             }
 
@@ -610,11 +611,11 @@ public final class BulkRequestContainerJob
             }
         }
 
-        private void storeOrUpdate(Throwable error) throws InterruptedException {
+        private void storeOrUpdate(Throwable error) {
             LOGGER.debug("{} - storeOrUpdate {}.", ruid, target);
 
-            if (hasBeenCancelled(this)) {
-                LOGGER.debug("{} - storeOrUpdate, hasBeenCancelled {}.", ruid, target.getPath());
+            if (hasBeenSpecificallyCancelled(this)) {
+                LOGGER.debug("{} - storeOrUpdate, hasBeenSpecificallyCancelled {}.", ruid, target.getPath());
                 return;
             }
 
@@ -626,12 +627,16 @@ public final class BulkRequestContainerJob
                  *  If this is an insert (id == null), the target id will be updated to what is
                  *  returned from the database.
                  */
+                checkForRequestCancellation();
                 targetStore.storeOrUpdate(target);
                 LOGGER.debug("{} - storeOrUpdate, target id {}", ruid, target.getId());
             } catch (BulkStorageException e) {
                 LOGGER.error("{}, could not store or update target from result {}, {}, {}: {}.", ruid,
                       target.getId(), target.getPath(), target.getAttributes(), e.toString());
                 error = e;
+            } catch (InterruptedException e) {
+                remove();
+                return;
             }
 
             if (error != null) {
@@ -692,22 +697,36 @@ public final class BulkRequestContainerJob
     }
 
     public void cancel() {
-        containerState = ContainerState.STOP;
-
-        jobTarget.cancel();
-
-        LOGGER.debug("{} - cancel:  target state is now {}.", ruid, jobTarget.getState());
-
         interruptRunThread();
 
-        synchronized (running) {
-            LOGGER.debug("{} - cancel:  running {}.", ruid, running.size());
-            running.values().forEach(ContainerTask::cancel);
-            LOGGER.debug("{} - cancel:  running targets cancelled.", ruid);
-            running.clear();
+        /*
+         *  Thread may already have exited.
+         *
+         *  Update terminates job target.
+         */
+        containerState = ContainerState.STOP;
+        update(CANCELLED);
+
+        LOGGER.debug("{} - cancel, running {}.", ruid, running.size());
+
+        /*
+         * Drain running tasks.  Calling task cancel removes the task from the map.
+         */
+        while (true) {
+            ContainerTask task;
+            synchronized (running) {
+                if (running.isEmpty()) {
+                    break;
+                }
+
+                task = running.values().iterator().next();
+            }
+
+            task.cancel();
+
+            LOGGER.debug("{} - cancel: task {} cancelled.", ruid, task.seqNo);
         }
 
-        LOGGER.debug("{} - cancel:  calling cancel all on target store.", ruid);
         targetStore.cancelAll(rid);
 
         signalStateChange();
@@ -773,7 +792,7 @@ public final class BulkRequestContainerJob
         containerState = ContainerState.PROCESS_FILES;
     }
 
-    public synchronized boolean isReady() {
+    public boolean isReady() {
         switch (jobTarget.getState()) {
             case READY:
             case CREATED:
@@ -819,7 +838,12 @@ public final class BulkRequestContainerJob
             update(CANCELLED);
         }
         setRunThread(null);
-        checkTransitionToDirs();
+
+        synchronized (running) {
+            if (running.isEmpty()) {
+                checkTransitionToDirs();
+            }
+        }
     }
 
     public void setDirListSemaphore(Semaphore dirListSemaphore) {
@@ -879,39 +903,43 @@ public final class BulkRequestContainerJob
     public void update(State state) {
         if (jobTarget.setState(state)) {
             try {
-                targetStore.update(jobTarget.getId(), jobTarget.getState(), jobTarget.getErrorType(),
+                targetStore.update(jobTarget.getId(), jobTarget.getState(),
+                      jobTarget.getErrorType(),
                       jobTarget.getErrorMessage());
             } catch (BulkStorageException e) {
                 LOGGER.error("{}, updateJobState: {}", ruid, e.toString());
             }
+
             signalStateChange();
         }
     }
 
     private void checkForRequestCancellation() throws InterruptedException {
-        if (isRunThreadInterrupted() || containerState == ContainerState.STOP
-              || jobTarget.isTerminated()) {
+        if (containerState == ContainerState.STOP) {
             throw new InterruptedException();
+        }
+
+        if (isRunThreadInterrupted()) {
+            throw new InterruptedException();
+        }
+
+        synchronized (running) {
+            if (jobTarget.isTerminated()) {
+                throw new InterruptedException();
+            }
         }
     }
 
     private void checkTransitionToDirs() {
-        synchronized (running) {
-            if (!running.isEmpty()) {
-                LOGGER.debug("{} - checkTransitionToDirs, running {}", ruid, running.size());
-                return;
-            }
-        }
-
-        synchronized (this) {
-            if (containerState == ContainerState.WAIT) {
-                containerState = ContainerState.PROCESS_DIRS;
-                executor.submit(this);
-            }
+        LOGGER.debug("{} - checkTransitionToDirs: {}", ruid, containerState);
+        if (containerState == ContainerState.WAIT) {
+            containerState = ContainerState.PROCESS_DIRS;
+            LOGGER.debug("{} - checkTransitionToDirs is now {}", ruid, containerState);
+            executor.submit(this);
         }
     }
 
-    private boolean hasBeenCancelled(TargetTask task) {
+    private boolean hasBeenSpecificallyCancelled(TargetTask task) {
         synchronized (cancelledPaths) {
             BulkRequestTarget target = task.target;
             if (cancelledPaths.remove(target.getPath().toString())) {
@@ -924,7 +952,7 @@ public final class BulkRequestContainerJob
                         targetStore.update(target.getId(), CANCELLED, null, null);
                     }
                 } catch (BulkServiceException | UnsupportedOperationException e) {
-                    LOGGER.error("hasBeenCancelled {}, failed for {}: {}", ruid, target.getPath(),
+                    LOGGER.error("hasBeenSpecificallyCancelled {}, failed for {}: {}", ruid, target.getPath(),
                           e.toString());
                 }
                 return true;
@@ -945,7 +973,7 @@ public final class BulkRequestContainerJob
         return runThread != null && runThread.isInterrupted();
     }
 
-    private void processDirTargets() throws InterruptedException {
+    private void processDirTargets() {
         if (dirs.isEmpty()) {
             LOGGER.debug("{} - processDirTargets, nothing to do.", ruid);
             return;
@@ -960,13 +988,20 @@ public final class BulkRequestContainerJob
          *  Process serially in this thread
          */
         for (DirTarget dirTarget : sorted) {
-            new TargetTask(toTarget(dirTarget.id, dirTarget.pid, dirTarget.path,
-                  Optional.of(dirTarget.attributes), CREATED, null),
-                  TaskState.HANDLE_DIR_TARGET).performSync();
+            try {
+                new TargetTask(toTarget(dirTarget.id, dirTarget.pid, dirTarget.path,
+                      Optional.of(dirTarget.attributes), CREATED, null),
+                      TaskState.HANDLE_DIR_TARGET).performSync();
+            } catch (InterruptedException e) {
+                /*
+                 *  Cancel most likely called; stop processing.
+                 */
+                break;
+            }
         }
     }
 
-    private void processFileTargets() throws InterruptedException {
+    private void processFileTargets() {
         List<BulkRequestTarget> requestTargets = targetStore.getInitialTargets(rid, true);
 
         LOGGER.debug("{} - processFileTargets, initial size {}.", ruid, requestTargets.size());
@@ -979,7 +1014,14 @@ public final class BulkRequestContainerJob
         }
 
         for (BulkRequestTarget target : requestTargets) {
-            new TargetTask(target, TaskState.RESOLVE_PATH).submitAsync();
+            try {
+                new TargetTask(target, TaskState.RESOLVE_PATH).submitAsync();
+            } catch (InterruptedException e) {
+                /*
+                 *  Cancel most likely called; stop processing.
+                 */
+                break;
+            }
         }
     }
 

modules/dcache-bulk/src/main/java/org/dcache/services/bulk/util/BulkRequestTarget.java
@@ -260,7 +260,7 @@ public final class BulkRequestTarget {
         return startedAt;
     }
 
-    public State getState() {
+    public synchronized State getState() {
         return state;
     }
 