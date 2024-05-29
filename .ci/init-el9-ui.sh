ea976307ef4cd4e2789c23f04af35f5045ec97e3
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2024-05-29 20:39:06 +0200

    ci: split el9 wn initialization from s2 tests
    (cherry picked from commit cce2da5a98304c77ee7fa8404b057c1d68654ff5)
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- .ci/init-el9-ui.sh
+++ .ci/init-el9-ui.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#
+# Required to support grid tools that sill use SHA1
+#
+update-crypto-policies --set DEFAULT:SHA1
+
 AUTOCA_URL=https://ci.dcache.org/ca
 
 dnf -q install -y epel-release which
@@ -27,24 +32,3 @@ python3 ./autoca-client -n -k userkey.pem -c usercert.pem ${AUTOCA_URL} "Kermit 
 voms-proxy-init -cert=usercert.pem -key=userkey.pem -voms=desy
 voms-proxy-info -all
 
-# SLEEP_SOR is the sleep-time when polling an SRM GetStatusOf...Request
-export SLEEP_SOR=2
-export S2_SUPRESS_PROGRESS=1
-
-export SRM_HOST=store-door-svc.$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace).svc.cluster.local
-export SRM_PORT=8443
-export SRM_DATAPATH=/data/s2
-export SRM_ENDPOINT="srm://${SRM_HOST}:${SRM_PORT}/srm/managerv2?SFN=${SRM_DATAPATH}"
-
-# s3 test depends on needs USER env to be set
-export USER=dcache-ci
-
-RC=0
-for i in /usr/share/s2/testing/scripts/protos/srm/2.2/{avail,basic,usecase}
-do
-  S2_LOGS_DIR=/ /usr/bin/xrunner.py -d $i
-  if [ $? -ne 0 ]; then
-    RC=1
-  fi
-done
-exit $RC
