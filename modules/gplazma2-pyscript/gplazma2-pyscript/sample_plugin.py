7b2b3dfd10cab244383f3004c6e9636e90ce4282
Anton Schwarz <39237289+ThePhisch@users.noreply.github.com>
2024-08-09 15:38:32 +0200

    gplazma: add gplazma2-jython module for auth using Python (#7627)
    Adds a new gplazma2 module to the project which uses Jython to parse and
    execute Python files (only version 2.7 supported) within the dcache Java
    runtime. This enables site admins to quickly script authentication plugins
    without needing to understand or write the native dcache Java code.
    
    This commit includes a sample "sample_plugin.py" auth plugin which is used
    during testing and serves as a reference.
    
    Implementation Details
    
    Jython limits the Python version to version 2.7. The Python files are currently
    stored directly within the source folder, i.e.
    `modules/gplazma2-jython/gplazma2-jython/sample_plugin.py`. Multiple files can
    be inserted which will be executed sequentially but in no particular order.
    
    Each Python file shall be structured so as to contain one function
    ´py_auth(public_credentials, private_credentials, principals)´ which returns a
    boolean whether the authentication was successful. Returning ´False´ will
    trigger an ´AuthenticationException´ in Java. Each parameter passed to ´py_auth´ will be a Python set.
    
    - ´private_credentials´ is a set of strings, integers or floats (implemented in
      Java as ´String || Number´
    - ´public_credentials´ is the same as ´private_credentials´
    - ´principals´ will be a set of strings in the form ´<principal_type>:<value>´,
      i.e. a pair of strings separated by a colon. Consider
      ´org.dcache.auth.Subjects#principalsFromArgs´to see how the principal type
      string maps to the principals classes.
    
    The conversion from a set of principals to a set of strings is done entirely
    within the ´authenticate´ method of the ´JythonPlugin´-class. The Python file
    only ever sees a set of strings, while someone using the plugin will only ever
    need to pass a set of principals from within Java. The conversion is done using
    existing functions.
    
    Tests
    
    A test class ´JythonPluginTest´ has been added that makes use of the internal
    logic of the sample ´sample_plugin.py´. The logic is simple and is repeated in
    the Java class and the Python file as a comment.
    
    
    Signed-off-by: Anton Schwarz <anton.schwarz@desy.de>

---
+++ modules/gplazma2-pyscript/gplazma2-pyscript/sample_plugin.py
@@ -0,0 +1,39 @@
+def py_auth(public_credentials, private_credentials, principals):
+    """
+    Function py_auth, sample implementation for a gplazma2-pyscript plugin
+    :param public_credentials: set of public auth credentials
+    :param private_credentials: set of private auth credentials
+    :param principals: set of principals (string values)
+    :return: boolean whether authentication is permitted
+
+    Note that the set of principals may only be added to, existing principals
+    may never be removed!
+
+    Note that the principals are handled as strings in Python! Consider
+    org.dcache.auth.Subjects#principalsFromArgs how these are converted back
+    into principals in Java.
+
+    In case authentication is denied, we return None.
+
+    In this sample implementation, the following logic is used:
+    AUTHENTICATE:
+        Condition:
+        - Either "Tom" is in the public credentials
+        - Or "Rose" is in the set of private credentials
+        Result:
+        - the username principal "Joad" is added (as string "username:joad")
+        - we return True
+    REFUSE:
+        Condition:
+        - Either "Connie" is in either one of the credentials
+        - No passing condition from above is met
+        Result:
+        - we return False
+    """
+    if ("Connie" in public_credentials) or ("Connie" in private_credentials):
+        return False
+    elif ("Tom" in public_credentials) or ("Rose" in private_credentials):
+        principals.add("username:Joad")
+        return True
+    else:
+        return False
