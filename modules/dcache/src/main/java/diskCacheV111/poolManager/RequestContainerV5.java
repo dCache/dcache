b6a528ca20107f6037306f16aa7985d4f70749a2
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2024-09-16 12:32:59 +0200

    poolmanager: cancel stage when restore is canceled
    Motivation:
    if user cancels the stage request via tape-api, then all in-progress
    stage requests are still processed. This is due to the lack of
    missing message that can be send from pool manager to a pool.
    
    Modification:
    
    Add (currently just mimic) such message and trigger it when a stage
    request is triggered in pool manager.
    
    Result:
    in-the-flight stage request is canceled when pool manager request is
    canceled.
    
    Acked-by: Albert Rossi
    Target: master
    Require-book: no
    Require-notes: yes

--- modules/dcache/src/main/java/diskCacheV111/poolManager/RequestContainerV5.java
+++ modules/dcache/src/main/java/diskCacheV111/poolManager/RequestContainerV5.java
@@ -738,6 +738,16 @@ public class RequestContainerV5
               .toArray(RestoreHandlerInfo[]::new);
     }
 
+    @VisibleForTesting
+    void fail(String id, int errorCode, String errorMessage) {
+        synchronized (_handlerHash) {
+            PoolRequestHandler rph = _handlerHash.get(id);
+            if (rph != null) {
+                rph.fail(errorCode, errorMessage);
+            }
+        }
+    }
+
     public void messageArrived(CellMessage envelope,
           PoolMgrSelectReadPoolMsg request)
           throws PatternSyntaxException, IOException {
@@ -1161,6 +1171,20 @@ public class RequestContainerV5
             _currentRc = code;
             _currentRm = message;
             updateStatus("Failed: " + message);
+
+            switch (_state) {
+                case ST_WAITING_FOR_STAGING:
+                    _stageCandidate.ifPresent(c -> {
+                        // FIXME: we need a new message here
+                        var cancelMessage = new CellMessage(c.address(), "rh kill " + _fileAttributes.getPnfsId());
+                        sendMessage(cancelMessage);
+                    });
+                case ST_WAITING_FOR_POOL_2_POOL:
+                    // FIXME: cancel p2p
+                    break;
+                default:
+                    // noop
+            }
 
             answerRequests();
             nextStep(RequestState.ST_OUT);
