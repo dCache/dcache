2400de1d01b1f0800f689d41eb1fd0a7fcf70aaa
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2024-09-23 15:17:36 +0200

    scripts: accept java 17-21
    Motivation:
    dCache master build with `--release=17` can run under java-21. Let
    script reflect that.
    
    Modification:
    Update script to allow java 17-21 runtime.
    
    Result:
    accept java 17-21 for runtime
    
    Acked-by: Karen Hoyos
    Target: master
    Require-book: no
    Require-notes: yes

--- skel/share/lib/loadConfig.sh
+++ skel/share/lib/loadConfig.sh
@@ -54,11 +54,11 @@ findJava()
 
 isJavaVersionOk()
 {
-    # Should match 11-19, which will let java major versions 11 to 19 through.
+    # Should match 17-21 which will let java major versions 17 to 21 through.
     # Extract just the version number from 'java -version'
     version=$($JAVA -version 2>&1 | awk -F '"' '/version/ {print $2}')
     # Use grep to match against '^1[1-9].*$'
-    echo "$version" | grep -q "^1[1-9].*$"
+    echo "$version" | grep -q "^1[7-9].*$\|^2[0-1].*$"
     # Return result from the above grep (0 if successful, 1 if failed)
     return $?
 }
@@ -182,7 +182,7 @@ loadConfig()
 
 # Get java location
 if ! findJava || ! isJavaVersionOk; then
-    echo "Could not find usable Java VM. Please set JAVA_HOME to the path to Java 11"
+    echo "Could not find usable Java VM. Please set JAVA_HOME to the path to Java 17-21"
     echo "or newer."
     exit 1
 fi
