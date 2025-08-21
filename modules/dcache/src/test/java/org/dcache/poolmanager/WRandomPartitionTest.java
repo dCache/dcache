a778f9763cbce8ba3876d4eaf3a440e02c4c4040
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2025-08-21 12:25:41 +0200

    poolmanager: update wrandom partition to respect gap
    Motivation:
    The wrandom partition ignores pool gap, thus can select a full pull.
    Issue: #7863
    
    Modification:
    Update WRandomPartition to skip pools that will run into gap if accept
    the file. Added unit test to ensure the behavior.
    
    Result:
    full pulls are skipped by wrandom partition.
    
    Acked-by: Dmitry Litvintsev
    Target: master, 11.0, 10.2, 10.1, 10.0, 9.2
    Require-book: no
    Require-notes: yes

---
+++ modules/dcache/src/test/java/org/dcache/poolmanager/WRandomPartitionTest.java
@@ -0,0 +1,57 @@
+package org.dcache.poolmanager;
+
+import com.google.common.collect.ImmutableMap;
+import diskCacheV111.pools.PoolCostInfo;
+import diskCacheV111.util.CacheException;
+import dmg.cells.nucleus.CellAddressCore;
+import org.hamcrest.MatcherAssert;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.AdditionalMatchers;
+
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.*;
+
+public class WRandomPartitionTest {
+
+
+    @Test(expected = CostException.class)
+    public void shouldFailIfAllocatesIntoGap() throws CacheException {
+
+        var wrandom = new WRandomPartition(Map.of());
+        var pools = IntStream.range(0, 10).mapToObj(i -> {
+                    var cost = new PoolCostInfo("pool" + i, "default-queue");
+                    cost.setSpaceUsage(10_000L, 3000L, 0L, 0L);
+                    cost.getSpaceInfo().setParameter(0.0d, 2500L);
+
+                    return new PoolInfo(new CellAddressCore("pool" + i), cost, ImmutableMap.of());
+                }
+        ).toList();
+
+        long fileSize = 1000L;
+        var selectedPool = wrandom.selectWritePool(null, pools, null, fileSize);
+    }
+
+    @Test
+    public void shouldSelectValidPool() throws CacheException {
+
+        var wrandom = new WRandomPartition(Map.of());
+        var pools = IntStream.range(0, 10).mapToObj(i -> {
+                    var cost = new PoolCostInfo("pool" + i, "default-queue");
+                    cost.setSpaceUsage(10_000L, 5000L, 0L, 0L);
+                    cost.getSpaceInfo().setParameter(0.0d, 2500L);
+
+                    return new PoolInfo(new CellAddressCore("pool" + i), cost, ImmutableMap.of());
+                }
+        ).toList();
+
+        long fileSize = 1000L;
+        var selectedPool = wrandom.selectWritePool(null, pools, null, fileSize);
+
+        var spaceInfo = selectedPool.info().getCostInfo().getSpaceInfo();
+        assertTrue("selected pool has no sufficient space", spaceInfo.getFreeSpace() + spaceInfo.getRemovableSpace() - fileSize > spaceInfo.getGap());
+    }
+
+}
