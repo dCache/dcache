3954e2b22188cf51d17ed14cd7b57e7fe176d232
chubnastia <ch.nastia20@gmail.com>
2026-08-03 14:45:13 +0200

    change dummy script and add debug

--- .ci/dummy-hsm.sh
+++ .ci/dummy-hsm.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-
 # Dummy HSM script for use-case-to-tape and use-case-diskandtape
-# Arguments passed by dCache: <action> <pnfsid> <filepath> [options]
-
 ACTION="$1"
 PNFSID="$2"
+FILEPATH="$3"
+
+echo "$(date -Iseconds) CALLED: $0 $*" >> /tmp/dummy-hsm-calls.log
 
 case "$ACTION" in
     put)
-        # Print dummy storage URI to stdout for dCache registration
         echo "dummy://dummy/?store=default&group=default&bfid=${PNFSID:-000000000000}"
         exit 0
         ;;
-    get|remove|*)
+    get)
+        # write dummy content back so dCache has an actual disk copy
+        echo "dummy restored content for ${PNFSID}" > "$FILEPATH"
         exit 0
+        ;;
+    remove)
+        exit 0
+        ;;
+    *)
+        exit 1
         ;;
 esac
